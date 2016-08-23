08ec10112dc7302648421d42cddbabf2d00a21f1
Jacob Vosmaer <jacob@gitlab.com>
2016-08-23 14:02:54 +0200

    Fix backend URL parsing

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ var printVersion = flag.Bool("version", false, "Print version and exit")
 var listenAddr = flag.String("listenAddr", "localhost:8181", "Listen address for HTTP server")
 var listenNetwork = flag.String("listenNetwork", "tcp", "Listen 'network' (tcp, tcp4, tcp6, unix)")
 var listenUmask = flag.Int("listenUmask", 0, "Umask for Unix socket")
-var authBackend = URLFlag("authBackend", upstream.DefaultBackend, "Authentication/authorization backend")
+var authBackend = flag.String("authBackend", upstream.DefaultBackend.String(), "Authentication/authorization backend")
 var authSocket = flag.String("authSocket", "", "Optional: Unix domain socket to dial authBackend at")
 var pprofListenAddr = flag.String("pprofListenAddr", "", "pprof listening address, e.g. 'localhost:6060'")
 var documentRoot = flag.String("documentRoot", "public", "Path to static files content")
@@ -53,6 +53,12 @@ func main() {
 	if *printVersion {
 		fmt.Println(version)
 		os.Exit(0)
+	}
+
+	backendURL, err := parseAuthBackend(*authBackend)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "invalid authBackend: %v\n", err)
+		os.Exit(1)
 	}
 
 	log.Printf("Starting %s", version)
@@ -83,7 +89,7 @@ func main() {
 	}
 
 	up := upstream.NewUpstream(
-		*authBackend,
+		backendURL,
 		*authSocket,
 		Version,
 		*documentRoot,
--- main_test.go
+++ main_test.go
@@ -736,6 +736,43 @@ func TestGetGitPatch(t *testing.T) {
 	}
 }
 
+func TestParseAuthBackend(t *testing.T) {
+	failures := []string{
+		"",
+		"ftp://localhost",
+		"https://example.com",
+	}
+	for _, example := range failures {
+		if _, err := parseAuthBackend(example); err == nil {
+			t.Errorf("error expected for %q", example)
+		}
+	}
+
+	successes := []struct{ input, host, scheme string }{
+		{"http://localhost:8080", "localhost:8080", "http"},
+		{"localhost:3000", "localhost:3000", "http"},
+		{"http://localhost", "localhost", "http"},
+		{"localhost", "localhost", "http"},
+	}
+
+	for _, example := range successes {
+		result, err := parseAuthBackend(example.input)
+		if err != nil {
+			t.Errorf("parse %q: %v", example.input, err)
+			break
+		}
+
+		if result.Host != example.host {
+			t.Errorf("expected %q, got %q", example.host, result.Host)
+		}
+
+		if result.Scheme != example.scheme {
+			t.Errorf("expected %q, got %q", example.scheme, result.Scheme)
+
+		}
+	}
+}
+
 func setupStaticFile(fpath, content string) error {
 	cwd, err := os.Getwd()
 	if err != nil {
