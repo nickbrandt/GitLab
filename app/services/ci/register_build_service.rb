04e4d8e0a36363ba3c7f80b9a17a52ca09df0b23
Kamil Trzcinski <ayufan@ayufan.eu>
2017-01-25 14:23:47 +0100

    Return struct instead of multiple values

--- app/services/ci/register_build_service.rb
+++ app/services/ci/register_build_service.rb
@@ -7,6 +7,8 @@ module Ci
 
     attr_reader :runner
 
+    Result = Struct.new(:build, :valid?)
+
     def initialize(runner)
       @runner = runner
     end
@@ -30,10 +32,10 @@ module Ci
         build.run!
       end
 
-      [build, true]
+      Result.new(build, true)
 
     rescue StateMachines::InvalidTransition, ActiveRecord::StaleObjectError
-      [nil, false]
+      Result.new(build, false)
     end
 
     private
