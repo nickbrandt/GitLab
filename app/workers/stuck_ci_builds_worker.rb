30489a27a60c10e2d718066a63e6bd9e3a9760df
Tomasz Maczukin <tomasz@maczukin.pl>
2017-02-07 14:00:46 +0100

    Port of 27523-make-stuck-build-detection-more-performant to EE
    - Update stuck and outdated builds cleanup worker
    - Update default configuration of stuck_ci_builds_worker cron interval
    - Add exclusive lease for stuck_ci_builds_worker
    - Add Gitlab::OptimisticLocking for build dropping
    - Improve builds search query

--- app/workers/stuck_ci_builds_worker.rb
+++ app/workers/stuck_ci_builds_worker.rb
@@ -2,19 +2,54 @@ class StuckCiBuildsWorker
   include Sidekiq::Worker
   include CronjobQueue
 
-  BUILD_STUCK_TIMEOUT = 1.day
+  BUILD_RUNNING_OUTDATED_TIMEOUT = 1.hour
+  BUILD_PENDING_OUTDATED_TIMEOUT = 1.day
+  BUILD_PENDING_STUCK_TIMEOUT = 1.hour
 
   def perform
     return if Gitlab::Geo.secondary?
-    Rails.logger.info 'Cleaning stuck builds'
+    return unless try_obtain_lease
 
-    builds = Ci::Build.joins(:project).running_or_pending.where('ci_builds.updated_at < ?', BUILD_STUCK_TIMEOUT.ago)
-    builds.find_each(batch_size: 50).each do |build|
-      Rails.logger.debug "Dropping stuck #{build.status} build #{build.id} for runner #{build.runner_id}"
-      build.drop
+    Rails.logger.info "#{self.class}: Cleaning stuck builds"
+
+    drop       :running, BUILD_RUNNING_OUTDATED_TIMEOUT
+    drop       :pending, BUILD_PENDING_OUTDATED_TIMEOUT
+    drop_stuck :pending, BUILD_PENDING_STUCK_TIMEOUT
+  end
+
+  private
+
+  def try_obtain_lease
+    Gitlab::ExclusiveLease.new(
+      'stuck_ci_builds_worker_lease',
+      timeout: 30.minutes
+    ).try_obtain
+  end
+
+  def drop(status, timeout)
+    search(status, timeout) do |build|
+      drop_build :outdated, build, status, timeout
     end
+  end
 
-    # Update builds that failed to drop
-    builds.update_all(status: 'failed')
+  def drop_stuck(status, timeout)
+    search(status, timeout) do |build|
+      return unless build.stuck?
+      drop_build :stuck, build, status, timeout
+    end
+  end
+
+  def search(status, timeout)
+    builds = Ci::Build.where(status: status).where('ci_builds.updated_at < ?', timeout.ago)
+    builds.joins(:project).includes(project: :namespace).includes(:tags).includes(:runner).find_each(batch_size: 50).each do |build|
+      yield(build)
+    end
+  end
+
+  def drop_build(type, build, status, timeout)
+    Rails.logger.info "#{self.class}: Dropping #{type} build #{build.id} for runner #{build.runner_id} (status: #{status}, timeout: #{timeout})"
+    Gitlab::OptimisticLocking.retry_lock(build, 3) do |b|
+      b.drop
+    end
   end
 end
