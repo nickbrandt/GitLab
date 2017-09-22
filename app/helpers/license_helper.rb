0dd98053d453ba0965b66057da160b3261fdec38
Douwe Maan <douwe@gitlab.com>
2017-09-22 11:24:07 +0000

    Merge branch '3498-error-500-due-to-promote-group-web-hooks' into 'master'
    Resolve "Error 500 due to promote group Web hooks"
    
    Closes #3498
    
    See merge request gitlab-org/gitlab-ee!2972

--- app/helpers/license_helper.rb
+++ app/helpers/license_helper.rb
@@ -68,8 +68,9 @@ module LicenseHelper
   end
 
   def upgrade_plan_url
-    if  @project.group
-      group_billings_path(@project.group)
+    group = @project&.group || @group
+    if group
+      group_billings_path(group)
     else
       profile_billings_path
     end
