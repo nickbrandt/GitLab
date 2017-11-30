5f1eeaf08cca8d01a5b1559ded8c145ee90d4280
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-11-30 13:23:39 -0200

    Add table to track upload deletion events

---
+++ db/migrate/20171130151759_create_geo_upload_deleted_events.rb
@@ -0,0 +1,16 @@
+class CreateGeoUploadDeletedEvents < ActiveRecord::Migration
+  DOWNTIME = false
+
+  def change
+    create_table :geo_upload_deleted_events, id: :bigserial do |t|
+      # If a upload is deleted, we need to retain this entry
+      t.references :upload, index: true, foreign_key: false, null: false
+      t.string :path, null: false
+      t.string :checksum, null: false, limit: 64
+      t.integer :model_id, null: false
+      t.string :model_type, null: false
+    end
+
+    add_column :geo_event_log, :upload_deleted_event_id, :integer, limit: 8
+  end
+end
