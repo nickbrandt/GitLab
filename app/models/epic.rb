4d487917b36c71c2b0ba26b1de3e51f10cbf6983
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-12-11 19:52:43 -0200

    Resolve conflicts on app/models/epic.rb

--- app/models/epic.rb
+++ app/models/epic.rb
@@ -1,7 +1,6 @@
 # Placeholder class for model that is implemented in EE
 # It reserves '&' as a reference prefix, but the table does not exists in CE
 class Epic < ActiveRecord::Base
-<<<<<<< HEAD
   prepend EE::Epic
 
   def self.reference_prefix
@@ -10,16 +9,5 @@ class Epic < ActiveRecord::Base
 
   def self.reference_prefix_escaped
     '&amp;'
-||||||| merged common ancestors
-  # TODO: this will be implemented as part of #3853
-  def to_reference
-=======
-  def self.reference_prefix
-    '&'
-  end
-
-  def self.reference_prefix_escaped
-    '&amp;'
->>>>>>> ce/10-3-stable
   end
 end
