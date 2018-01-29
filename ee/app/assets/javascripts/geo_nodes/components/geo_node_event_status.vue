4d1ab825fffdc250009acd0531916cf62dec123d
Eric Eastwood <contact@ericeastwood.com>
2018-01-29 16:22:00 -0600

    Fix Vue lint conflicts with geo nodes
    Changes introduced in
    https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/4206
    
    Conflicting with 10.5 Vue lint changes
    https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/3983

--- ee/app/assets/javascripts/geo_nodes/components/geo_node_event_status.vue
+++ ee/app/assets/javascripts/geo_nodes/components/geo_node_event_status.vue
@@ -1,59 +1,40 @@
 <script>
-import { formatDate } from '~/lib/utils/datetime_utility';
-import timeAgoMixin from '~/vue_shared/mixins/timeago';
-import tooltip from '~/vue_shared/directives/tooltip';
+  import { formatDate } from '~/lib/utils/datetime_utility';
+  import timeAgoMixin from '~/vue_shared/mixins/timeago';
+  import tooltip from '~/vue_shared/directives/tooltip';
 
-export default {
-  props: {
-    eventId: {
-      type: Number,
-      required: true,
+  export default {
+    directives: {
+      tooltip,
     },
-    eventTimeStamp: {
-      type: Number,
-      required: true,
+    mixins: [
+      timeAgoMixin,
+    ],
+    props: {
+      eventId: {
+        type: Number,
+        required: true,
+      },
+      eventTimeStamp: {
+        type: Number,
+        required: true,
+      },
     },
-  },
-  mixins: [
-    timeAgoMixin,
-  ],
-  directives: {
-    tooltip,
-  },
-  computed: {
-    timeStamp() {
-      return new Date(this.eventTimeStamp * 1000);
+    computed: {
+      timeStamp() {
+        return new Date(this.eventTimeStamp * 1000);
+      },
+      timeStampString() {
+        return formatDate(this.timeStamp);
+      },
     },
-    timeStampString() {
-      return formatDate(this.timeStamp);
-    },
-  },
-};
+  };
 </script>
 
 <template>
   <div
     class="node-detail-value"
   >
-<<<<<<< HEAD
-    <strong>
-      {{eventId}}
-    </strong>
-    <span
-      v-tooltip
-      class="event-status-timestamp"
-      data-placement="bottom"
-      :title="timeStampString"
-||||||| parent of c3efd8e0d8... Merge branch 'kp-fix-geo-admin-bugs' into 'master'
-    <strong>
-      {{ eventId }}
-    </strong>
-    <span
-      v-tooltip
-      class="event-status-timestamp"
-      data-placement="bottom"
-      :title="timeStampString"
-=======
     <template v-if="eventTimeStamp">
       <strong>
         {{ eventId }}
@@ -70,17 +51,8 @@ export default {
     </template>
     <strong
       v-else
->>>>>>> c3efd8e0d8... Merge branch 'kp-fix-geo-admin-bugs' into 'master'
     >
-<<<<<<< HEAD
-      ({{timeFormated(timeStamp)}})
-    </span>
-||||||| parent of c3efd8e0d8... Merge branch 'kp-fix-geo-admin-bugs' into 'master'
-      ({{ timeFormated(timeStamp) }})
-    </span>
-=======
       {{ __('Not available') }}
     </strong>
->>>>>>> c3efd8e0d8... Merge branch 'kp-fix-geo-admin-bugs' into 'master'
   </div>
 </template>
--- ee/app/assets/javascripts/geo_nodes/components/geo_node_sync_settings.vue
+++ ee/app/assets/javascripts/geo_nodes/components/geo_node_sync_settings.vue
@@ -1,72 +1,32 @@
 <script>
-import { s__ } from '~/locale';
-import { timeIntervalInWords } from '~/lib/utils/datetime_utility';
-import tooltip from '~/vue_shared/directives/tooltip';
-import icon from '~/vue_shared/components/icon.vue';
+  import { s__ } from '~/locale';
+  import { timeIntervalInWords } from '~/lib/utils/datetime_utility';
+  import tooltip from '~/vue_shared/directives/tooltip';
+  import icon from '~/vue_shared/components/icon.vue';
 
-import { TIME_DIFF } from '../constants';
+  import { TIME_DIFF } from '../constants';
 
-export default {
-  props: {
-    namespaces: {
-      type: Array,
-      required: true,
+  export default {
+    directives: {
+      tooltip,
     },
-    lastEvent: {
-      type: Object,
-      required: true,
+    components: {
+      icon,
     },
-    cursorLastEvent: {
-      type: Object,
-      required: true,
-    },
-<<<<<<< HEAD
-  },
-  directives: {
-    tooltip,
-  },
-  components: {
-    icon,
-  },
-  computed: {
-    syncType() {
-      return this.namespaces.length > 0 ? s__('GeoNodes|Selective') : s__('GeoNodes|Full');
-    },
-    syncLagInSeconds() {
-      return this.lagInSeconds(this.lastEvent.timeStamp, this.cursorLastEvent.timeStamp);
-    },
-    syncStatusIcon() {
-      return this.statusIcon(this.syncLagInSeconds);
-    },
-    syncStatusEventInfo() {
-      return this.statusEventInfo(
-        this.lastEvent.id,
-        this.cursorLastEvent.id,
-        this.syncLagInSeconds,
-      );
-||||||| parent of c3efd8e0d8... Merge branch 'kp-fix-geo-admin-bugs' into 'master'
-
-    computed: {
-      syncType() {
-        return this.namespaces.length > 0 ? s__('GeoNodes|Selective') : s__('GeoNodes|Full');
+    props: {
+      namespaces: {
+        type: Array,
+        required: true,
       },
-      syncLagInSeconds() {
-        return this.lagInSeconds(this.lastEvent.timeStamp, this.cursorLastEvent.timeStamp);
+      lastEvent: {
+        type: Object,
+        required: true,
       },
-      syncStatusIcon() {
-        return this.statusIcon(this.syncLagInSeconds);
+      cursorLastEvent: {
+        type: Object,
+        required: true,
       },
-      syncStatusEventInfo() {
-        return this.statusEventInfo(
-          this.lastEvent.id,
-          this.cursorLastEvent.id,
-          this.syncLagInSeconds,
-        );
-      },
-      syncStatusTooltip() {
-        return this.statusTooltip(this.syncLagInSeconds);
-      },
-=======
+    },
 
     computed: {
       syncType() {
@@ -91,77 +51,20 @@ export default {
       syncStatusTooltip() {
         return this.statusTooltip(this.syncLagInSeconds);
       },
->>>>>>> c3efd8e0d8... Merge branch 'kp-fix-geo-admin-bugs' into 'master'
     },
-    syncStatusTooltip() {
-      return this.statusTooltip(this.syncLagInSeconds);
-    },
-  },
-  methods: {
-    lagInSeconds(lastEventTimeStamp, cursorLastEventTimeStamp) {
-      let eventDateTime;
-      let cursorDateTime;
+    methods: {
+      lagInSeconds(lastEventTimeStamp, cursorLastEventTimeStamp) {
+        let eventDateTime;
+        let cursorDateTime;
 
-      if (lastEventTimeStamp && lastEventTimeStamp > 0) {
-        eventDateTime = new Date(lastEventTimeStamp * 1000);
-      }
-
-      if (cursorLastEventTimeStamp && cursorLastEventTimeStamp > 0) {
-        cursorDateTime = new Date(cursorLastEventTimeStamp * 1000);
-      }
-
-<<<<<<< HEAD
-      return (cursorDateTime - eventDateTime) / 1000;
-    },
-    statusIcon(syncLag) {
-      if (syncLag <= TIME_DIFF.FIVE_MINS) {
-        return 'retry';
-      } else if (syncLag > TIME_DIFF.FIVE_MINS &&
-                 syncLag <= TIME_DIFF.HOUR) {
-        return 'warning';
-      }
-      return 'status_failed';
-    },
-    statusEventInfo(lastEventId, cursorLastEventId, lagInSeconds) {
-      const timeAgoStr = timeIntervalInWords(lagInSeconds);
-      const pendingEvents = lastEventId - cursorLastEventId;
-      return `${timeAgoStr} (${pendingEvents} events)`;
-    },
-    statusTooltip(lagInSeconds) {
-      if (lagInSeconds <= TIME_DIFF.FIVE_MINS) {
-        return '';
-      } else if (lagInSeconds > TIME_DIFF.FIVE_MINS &&
-                 lagInSeconds <= TIME_DIFF.HOUR) {
-        return s__('GeoNodeSyncStatus|Node is slow, overloaded, or it just recovered after an outage.');
-      }
-      return s__('GeoNodeSyncStatus|Node is failing or broken.');
-||||||| parent of c3efd8e0d8... Merge branch 'kp-fix-geo-admin-bugs' into 'master'
-        return (cursorDateTime - eventDateTime) / 1000;
-      },
-      statusIcon(syncLag) {
-        if (syncLag <= TIME_DIFF.FIVE_MINS) {
-          return 'retry';
-        } else if (syncLag > TIME_DIFF.FIVE_MINS &&
-                  syncLag <= TIME_DIFF.HOUR) {
-          return 'warning';
+        if (lastEventTimeStamp && lastEventTimeStamp > 0) {
+          eventDateTime = new Date(lastEventTimeStamp * 1000);
         }
-        return 'status_failed';
-      },
-      statusEventInfo(lastEventId, cursorLastEventId, lagInSeconds) {
-        const timeAgoStr = timeIntervalInWords(lagInSeconds);
-        const pendingEvents = lastEventId - cursorLastEventId;
-        return `${timeAgoStr} (${pendingEvents} events)`;
-      },
-      statusTooltip(lagInSeconds) {
-        if (lagInSeconds <= TIME_DIFF.FIVE_MINS) {
-          return '';
-        } else if (lagInSeconds > TIME_DIFF.FIVE_MINS &&
-                  lagInSeconds <= TIME_DIFF.HOUR) {
-          return s__('GeoNodeSyncStatus|Node is slow, overloaded, or it just recovered after an outage.');
+
+        if (cursorLastEventTimeStamp && cursorLastEventTimeStamp > 0) {
+          cursorDateTime = new Date(cursorLastEventTimeStamp * 1000);
         }
-        return s__('GeoNodeSyncStatus|Node is failing or broken.');
-      },
-=======
+
         return (cursorDateTime - eventDateTime) / 1000;
       },
       statusIcon(syncLag) {
@@ -188,10 +91,8 @@ export default {
         }
         return s__('GeoNodeSyncStatus|Node is failing or broken.');
       },
->>>>>>> c3efd8e0d8... Merge branch 'kp-fix-geo-admin-bugs' into 'master'
     },
-  },
-};
+  };
 </script>
 
 <template>
@@ -204,7 +105,7 @@ export default {
       data-placement="bottom"
       :title="syncStatusTooltip"
     >
-      <strong>{{syncType}}</strong>
+      <strong>{{ syncType }}</strong>
       <icon
         name="retry"
         css-classes="sync-status-icon prepend-left-5"
@@ -213,7 +114,7 @@ export default {
         v-if="!eventTimestampEmpty"
         class="sync-status-event-info prepend-left-5"
       >
-        {{syncStatusEventInfo}}
+        {{ syncStatusEventInfo }}
       </span>
     </span>
   </div>
