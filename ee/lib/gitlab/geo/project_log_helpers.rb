ebd80d1306e2871274b757df4477dba44c983748
Nick Thomas <nick@gitlab.com>
2018-04-09 11:40:59 +0100

    Resolve conflict in ee/lib/gitlab/geo/project_log_helpers.rb

--- ee/lib/gitlab/geo/project_log_helpers.rb
+++ ee/lib/gitlab/geo/project_log_helpers.rb
@@ -8,15 +8,10 @@ module Gitlab
           class: self.class.name,
           project_id: project.id,
           project_path: project.full_path,
-<<<<<<< HEAD
-          message: message
-        }
-=======
           storage_version: project.storage_version,
           message: message,
           job_id: get_sidekiq_job_id
         }.compact
->>>>>>> 89d997230bb... Merge branch '4994-geo-log-jid-for-sync-related-jobs' into 'master'
       end
     end
   end
