254e9724fdeebeee3720ea0a944ae401c2adf3cb
James Lopez <james@gitlab.com>
2018-04-10 15:04:12 +0000

    Fix "Migrations altering Appearances need to clear cache"

--- ee/db/migrate/20180314145917_add_header_and_footer_banners_to_appearances_table.rb
+++ ee/db/migrate/20180314145917_add_header_and_footer_banners_to_appearances_table.rb
@@ -12,5 +12,11 @@ class AddHeaderAndFooterBannersToAppearancesTable < ActiveRecord::Migration
 
     add_column :appearances, :message_background_color, :text
     add_column :appearances, :message_font_color, :text
+
+    say 'Flushing `current_appearance` cache...'
+
+    # Flush the cache for `current_appearance` right after the migration
+    # See https://gitlab.com/gitlab-org/gitlab-ee/issues/5571
+    Rails.cache.delete('current_appearance')
   end
 end
