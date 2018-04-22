1978e6d32f5f609dfc4bba71c865b68cef415dba
James Edwards-Jones <jedwardsjones@gitlab.com>
2018-04-23 00:09:20 +0100

    Adds Group SAML test button
    Makes it easier to try out different configurations
    while testing Group SAML settings.

--- ee/spec/features/groups/saml_providers_spec.rb
+++ ee/spec/features/groups/saml_providers_spec.rb
@@ -17,6 +17,10 @@ describe 'SAML provider settings' do
     click_button('Save changes')
   end
 
+  def test_sso
+    click_link('Test SAML SSO')
+  end
+
   def stub_saml_config
     stub_licensed_features(group_saml: true)
     allow(Devise).to receive(:omniauth_providers).and_return(%i(group_saml))
@@ -70,6 +74,35 @@ describe 'SAML provider settings' do
         login_url = find('label', text: 'GitLab single sign on URL').find('~* a').text
 
         expect(login_url).to end_with "/groups/#{group.full_path}/-/saml/sso"
+      end
+    end
+
+    describe 'test button' do
+      let!(:saml_provider) { create(:saml_provider, group: group) }
+
+      before do
+        sign_in(user)
+        allow_any_instance_of(OmniAuth::Strategies::GroupSaml).to receive(:callback_url) { callback_path }
+      end
+
+      it 'POSTs to the SSO path for the group' do
+        visit group_saml_providers_path(group)
+
+        test_sso
+
+        expect(current_path).to eq callback_path
+      end
+
+      it 'sets RelayState to redirect back to settings page' do
+        visit group_saml_providers_path(group)
+
+        allow_any_instance_of(OmniAuth::Strategies::GroupSaml).to receive(:mock_request_call) do |group_saml_strategy|
+          group_saml_strategy.mock_redirect_relay_state
+        end
+
+        test_sso
+
+        expect(current_path).to eq group_saml_providers_path(group)
       end
     end
   end
