c5449d4a90aab2949b032c1f82a350e5284d686f
Filipa Lacerda <filipa@gitlab.com>
2018-05-08 09:27:51 +0100

    Fixes conflicts for app/serializers/project_mirror_entity.rb

--- app/serializers/project_mirror_entity.rb
+++ app/serializers/project_mirror_entity.rb
@@ -1,9 +1,6 @@
 class ProjectMirrorEntity < Grape::Entity
-<<<<<<< HEAD
   prepend ::EE::ProjectMirrorEntity
 
-=======
->>>>>>> 632244e7ad4a77dc5bf7ef407812b875d20569bb
   expose :id
 
   expose :remote_mirrors_attributes do |project|
