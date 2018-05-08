c56917a977ded1475170aa9aa4ab9a9dba6ec7e7
Filipa Lacerda <filipa@gitlab.com>
2018-05-08 08:50:42 +0100

    Fixes conflict for db/migrate/20180503141722_add_remote_mirror_available_overridden_to_projects.rb

--- db/migrate/20180503141722_add_remote_mirror_available_overridden_to_projects.rb
+++ db/migrate/20180503141722_add_remote_mirror_available_overridden_to_projects.rb
@@ -10,10 +10,6 @@ class AddRemoteMirrorAvailableOverriddenToProjects < ActiveRecord::Migration
   end
 
   def down
-<<<<<<< HEAD
     # ee/db/migrate/20171017130239_add_remote_mirror_available_overridden_to_projects_ee.rb will remove the column.
-=======
-    remove_column(:projects, :remote_mirror_available_overridden) if column_exists?(:projects, :remote_mirror_available_overridden)
->>>>>>> 632244e7ad4a77dc5bf7ef407812b875d20569bb
   end
 end
