47bb295e676b507108d524d0c45ccee3c940c0f5
Douwe Maan <douwe@gitlab.com>
2018-06-06 14:14:05 +0000

    Merge branch 'bvl-validate-label-for-project-before-save' into 'master'
    Validate classification label on create & update
    
    Closes #5336
    
    See merge request gitlab-org/gitlab-ee!5976

--- ee/app/services/ee/projects/update_service.rb
+++ ee/app/services/ee/projects/update_service.rb
@@ -2,7 +2,7 @@ module EE
   module Projects
     module UpdateService
       extend ::Gitlab::Utils::Override
-
+      include ValidatesClassificationLabel
       include CleanupApprovers
 
       override :execute
@@ -16,7 +16,21 @@ module EE
         should_remove_old_approvers = params.delete(:remove_old_approvers)
         wiki_was_enabled = project.wiki_enabled?
 
+<<<<<<< HEAD
         result = super
+=======
+        limit = params.delete(:repository_size_limit)
+        result = super do
+          # Repository size limit comes as MB from the view
+          project.repository_size_limit = ::Gitlab::Utils.try_megabytes_to_bytes(limit) if limit
+
+          if changing_storage_size?
+            project.change_repository_storage(params.delete(:repository_storage))
+          end
+
+          validate_classification_label(project, :external_authorization_classification_label)
+        end
+>>>>>>> e14fec7a5c1... Merge branch 'bvl-validate-label-for-project-before-save' into 'master'
 
         if result[:status] == :success
           cleanup_approvers(project) if should_remove_old_approvers
