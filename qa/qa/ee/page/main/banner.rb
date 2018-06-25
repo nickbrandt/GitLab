a265a1a4e6ada49060d8a8da5d8025e3e8377a42
Stan Hu <stanhu@gmail.com>
2018-06-25 15:22:21 -0700

    Fix QA selector comparison for Geo read-only message

--- qa/qa/ee/page/main/banner.rb
+++ qa/qa/ee/page/main/banner.rb
@@ -4,7 +4,7 @@ module QA
       module Main
         class Banner < QA::Page::Base
           view 'ee/app/helpers/ee/application_helper.rb' do
-            element :read_only_message, 'You are on a secondary, <b>read-only</b> Geo node.'
+            element :read_only_message, 'You are on a secondary (read-only) Geo node.'
           end
 
           def has_secondary_read_only_banner?
