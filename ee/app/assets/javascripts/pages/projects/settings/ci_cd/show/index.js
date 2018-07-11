211e40d6f0de686d688fb2a99accd303b24dc45e
Mayra Cabrera <mcabrera@gitlab.com>
2018-07-11 15:34:04 -0500

    Refactor EE specific JS file for CI/CD settings
    Re-use CE specific CI/CD JS file

--- ee/app/assets/javascripts/pages/projects/settings/ci_cd/show/index.js
+++ ee/app/assets/javascripts/pages/projects/settings/ci_cd/show/index.js
@@ -1,44 +1,7 @@
-/* eslint-disable no-new */
-
-import initSettingsPanels from '~/settings_panels';
-import SecretValues from '~/behaviors/secret_values';
-import AjaxVariableList from '~/ci_variable_list/ajax_variable_list';
+import '~/pages/projects/settings/ci_cd/show/index';
 import ProtectedEnvironmentCreate from 'ee/protected_environments/protected_environment_create';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Initialize expandable settings panels
-  initSettingsPanels();
-
-  new ProtectedEnvironmentCreate();
-
-  const runnerToken = document.querySelector('.js-secret-runner-token');
-  if (runnerToken) {
-    const runnerTokenSecretValue = new SecretValues({
-      container: runnerToken,
-    });
-    runnerTokenSecretValue.init();
-  }
-
-  const variableListEl = document.querySelector('.js-ci-variable-list-section');
   // eslint-disable-next-line no-new
-  new AjaxVariableList({
-    container: variableListEl,
-    saveButton: variableListEl.querySelector('.js-secret-variables-save-button'),
-    errorBox: variableListEl.querySelector('.js-ci-variable-error-box'),
-    saveEndpoint: variableListEl.dataset.saveEndpoint,
-  });
-
-  // hide extra auto devops settings based on data-attributes
-  const autoDevOpsSettings = document.querySelector('.js-auto-devops-settings');
-  const autoDevOpsExtraSettings = document.querySelector('.js-extra-settings');
-
-  autoDevOpsSettings.addEventListener('click', event => {
-    const { target } = event;
-    if (target.classList.contains('js-toggle-extra-settings')) {
-      autoDevOpsExtraSettings.classList.toggle(
-        'hidden',
-        !!(target.dataset && target.dataset.hideExtraSettings),
-      );
-    }
-  });
+  new ProtectedEnvironmentCreate();
 });
