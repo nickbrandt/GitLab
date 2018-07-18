4a7463e87f5ce44bc81e2d1b2322fe2f2a773628
Mayra Cabrera <mcabrera@gitlab.com>
2018-07-18 14:05:55 -0500

    Fixes update action for ProtectedEnvironment

--- ee/app/services/protected_environments/update_service.rb
+++ ee/app/services/protected_environments/update_service.rb
@@ -2,6 +2,7 @@ module ProtectedEnvironments
   class UpdateService < BaseService
     def execute(protected_environment)
       protected_environment.update(params)
+      protected_environment
     end
   end
 end
