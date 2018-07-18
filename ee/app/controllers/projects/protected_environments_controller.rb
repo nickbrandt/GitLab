6c82f21480008b38773c46e3896e60c838ae7c4f
Mayra Cabrera <mcabrera@gitlab.com>
2018-07-18 09:36:30 -0500

    Remove useless html_safe on ProtectedEnvironments controller

--- ee/app/controllers/projects/protected_environments_controller.rb
+++ ee/app/controllers/projects/protected_environments_controller.rb
@@ -8,7 +8,7 @@ class Projects::ProtectedEnvironmentsController < Projects::ApplicationControlle
     if protected_environment.persisted?
       flash[:notice] = s_('ProtectedEnvironment|Your environment has been protected.')
     else
-      flash[:alert] = protected_environment.errors.full_messages.join(', ').html_safe
+      flash[:alert] = protected_environment.errors.full_messages.join(', ')
     end
 
     redirect_to project_settings_ci_cd_path(@project)
