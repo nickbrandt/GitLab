c0306a13a171242c50fa67efe36b40414b4432b5
Filipa Lacerda <filipa@gitlab.com>
2018-07-19 08:47:01 +0000

    Merge branch '6940-mr-reports-alignment' into 'master'
    Resolve "bad alignment in merge request widget (code climate, security report, etc.)"
    
    Closes #6940
    
    See merge request gitlab-org/gitlab-ee!6580

--- ee/app/assets/javascripts/vue_shared/security_reports/components/report_section.vue
+++ ee/app/assets/javascripts/vue_shared/security_reports/components/report_section.vue
@@ -146,7 +146,7 @@ export default {
         :status="statusIconName"
       />
       <div
-        class="media-body space-children d-flex"
+        class="media-body space-children d-flex flex-align-self-center"
       >
         <span
           class="js-code-text code-text"
