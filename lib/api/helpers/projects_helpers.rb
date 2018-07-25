0f5e814518a3791bf26f381d0e6c57b5d8eb9751
Tiago Botelho <tiagonbotelho@hotmail.com>
2018-07-25 12:06:56 +0100

    Moves Project API EE logic to live under the ee/ folder

--- lib/api/helpers/projects_helpers.rb
+++ lib/api/helpers/projects_helpers.rb
@@ -29,13 +29,12 @@ module API
           end
 
           params :optional_project_params_ee do
-            optional :repository_storage, type: String, desc: 'Which storage shard the repository is on. Available only to admins'
-            optional :approvals_before_merge, type: Integer, desc: 'How many approvers should approve merge request by default'
-            optional :external_authorization_classification_label, type: String, desc: 'The classification label for the project'
-            optional :mirror, type: Boolean, desc: 'Enables pull mirroring in a project'
-            optional :mirror_trigger_builds, type: Boolean, desc: 'Pull mirroring triggers builds'
           end
+        end
 
+        prepend EE::API::Helpers::ProjectsHelpers
+
+        helpers do
           params :optional_project_params do
             use :optional_project_params_ce
             use :optional_project_params_ee
