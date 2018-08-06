19927144a40ea1a7f5627afacfebc0fcd5d90297
Jose Vargas <jvargas@gitlab.com>
2018-08-06 14:58:17 -0500

    Fix template_helper

--- lib/gitlab/template_helper.rb
+++ lib/gitlab/template_helper.rb
@@ -2,8 +2,8 @@ module Gitlab
   module TemplateHelper
     include Gitlab::Utils::StrongMemoize
 
-    def prepare_template_environment(file)
-      return unless file
+    def prepare_template_environment(file_path)
+      return unless file_path.present?
 
       FileUtils.mkdir_p(File.dirname(import_upload_path))
       FileUtils.copy_entry(file_path, import_upload_path)
