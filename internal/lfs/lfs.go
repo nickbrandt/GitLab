40b5f3d4b7acfedc9a6c8aa5049222d95a1cf69e
Alessio Caiazza <acaiazza@gitlab.com>
2018-08-14 13:03:33 +0000

    Accelerate Maven artifact repository uploads

--- internal/lfs/lfs.go
+++ internal/lfs/lfs.go
@@ -6,65 +6,38 @@ package lfs
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"net/url"
-	"path/filepath"
-	"strings"
 
 	"gitlab.com/gitlab-org/gitlab-workhorse/internal/api"
 	"gitlab.com/gitlab-org/gitlab-workhorse/internal/filestore"
-	"gitlab.com/gitlab-org/gitlab-workhorse/internal/helper"
 )
 
-func PutStore(a *api.API, h http.Handler) http.Handler {
-	return handleStoreLFSObject(a, h)
+type object struct {
+	size int64
+	oid  string
 }
 
-func handleStoreLFSObject(myAPI *api.API, h http.Handler) http.Handler {
-	return myAPI.PreAuthorizeHandler(func(w http.ResponseWriter, r *http.Request, a *api.Response) {
-		opts := filestore.GetOpts(a)
-		opts.TempFilePrefix = a.LfsOid
+func (l *object) Verify(fh *filestore.FileHandler) error {
+	if fh.Size != l.size {
+		return fmt.Errorf("LFSObject: expected size %d, wrote %d", l.size, fh.Size)
+	}
 
-		// backward compatible api check - to be removed on next release
-		if a.StoreLFSPath != "" {
-			opts.LocalTempPath = a.StoreLFSPath
-		}
-		// end of backward compatible api check
+	if fh.SHA256() != l.oid {
+		return fmt.Errorf("LFSObject: expected sha256 %s, got %s", l.oid, fh.SHA256())
+	}
 
-		fh, err := filestore.SaveFileFromReader(r.Context(), r.Body, r.ContentLength, opts)
-		if err != nil {
-			helper.Fail500(w, r, fmt.Errorf("handleStoreLFSObject: copy body to tempfile: %v", err))
-			return
-		}
+	return nil
+}
 
-		if fh.Size != a.LfsSize {
-			helper.Fail500(w, r, fmt.Errorf("handleStoreLFSObject: expected size %d, wrote %d", a.LfsSize, fh.Size))
-			return
-		}
+type uploadPreparer struct{}
 
-		if fh.SHA256() != a.LfsOid {
-			helper.Fail500(w, r, fmt.Errorf("handleStoreLFSObject: expected sha256 %s, got %s", a.LfsOid, fh.SHA256()))
-			return
-		}
+func (l *uploadPreparer) Prepare(a *api.Response) (*filestore.SaveFileOpts, filestore.UploadVerifier, error) {
+	opts := filestore.GetOpts(a)
+	opts.TempFilePrefix = a.LfsOid
 
-		data := url.Values{}
-		for k, v := range fh.GitLabFinalizeFields("file") {
-			data.Set(k, v)
-		}
+	return opts, &object{oid: a.LfsOid, size: a.LfsSize}, nil
+}
 
-		// Hijack body
-		body := data.Encode()
-		r.Body = ioutil.NopCloser(strings.NewReader(body))
-		r.ContentLength = int64(len(body))
-		r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-		// backward compatible API header - to be removed on next release
-		if opts.IsLocal() {
-			r.Header.Set("X-GitLab-Lfs-Tmp", filepath.Base(fh.LocalPath))
-		}
-		// end of backward compatible API header
-
-		// And proxy the request
-		h.ServeHTTP(w, r)
-	}, "/authorize")
+func PutStore(a *api.API, h http.Handler) http.Handler {
+	return filestore.BodyUploader(a, h, &uploadPreparer{})
 }
