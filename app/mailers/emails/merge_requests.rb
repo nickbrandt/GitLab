dfd21f50724ca88a6eda02b8a29f663e27863c14
Bob Van Landuyt <bob@gitlab.com>
2018-09-24 15:07:40 +0000

    Merge branch 'security-11-3-6881-project-group-approvers-leaks-private-group-info' into 'security-11-3-ee'
    [11.3] Project group approvers leaks private group info
    
    See merge request gitlab/gitlab-ee!676

--- app/mailers/emails/merge_requests.rb
+++ app/mailers/emails/merge_requests.rb
@@ -66,7 +66,7 @@ module Emails
     end
 
     def add_merge_request_approver_email(recipient_id, merge_request_id, updated_by_user_id, reason = nil)
-      setup_merge_request_mail(merge_request_id, recipient_id)
+      setup_merge_request_mail(merge_request_id, recipient_id, present: true)
 
       @updated_by = User.find(updated_by_user_id)
       mail_answer_thread(@merge_request, merge_request_thread_options(updated_by_user_id, recipient_id, reason))
