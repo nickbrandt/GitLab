68bf4a836526be83ebae0af614ffa86833c939c5
Ash McKenzie <amckenzie@gitlab.com>
2018-10-01 14:42:27 +1000

    Geo SSH push to secondary specs
    - Break out HTTP & push specs into separate files
    - Also remove repository_replication_spec.rb as it's more accurately covered by http_push_spec.rb

--- qa/qa/specs/features/browser_ui/ee_geo/ssh_push_to_secondary_spec.rb
+++ qa/qa/specs/features/browser_ui/ee_geo/ssh_push_to_secondary_spec.rb
@@ -2,15 +2,21 @@
 
 module QA
   context :geo, :orchestrated, :geo do
-    describe 'GitLab Geo HTTP push secondary' do
-      let(:file_name) { 'README.md' }
-      let(:file_content_primary) { 'This is a Geo project!  Commit from primary.' }
-      let(:file_content_secondary) { 'This is a Geo project!  Commit from secondary.' }
+    describe 'GitLab SSH push to secondary' do
+      it "is proxy'd to the primary and ultimately replicated to the secondary" do
+        file_name = 'README.md'
+        key_title = "key for ssh tests #{Time.now.to_f}"
+        file_content_primary = 'This is a Geo project!  Commit from primary.'
+        file_content_secondary = 'This is a Geo project!  Commit from secondary.'
 
-      it 'is redirected to the primary and ultimately replicated to the secondary' do
         Runtime::Browser.visit(:geo_primary, QA::Page::Main::Login) do
           # Visit the primary node and login
           Page::Main::Login.act { sign_in_using_credentials }
+
+          # Create a new SSH key for the user
+          key = Factory::Resource::SSHKey.fabricate! do |resource|
+            resource.title = key_title
+          end
 
           # Create a new Project
           project = Factory::Resource::Project.fabricate! do |project|
@@ -18,22 +24,18 @@ module QA
             project.description = 'Geo test project'
           end
 
-          # Perform a git push over HTTP directly to the primary
+          # Perform a git push over SSH directly to the primary
+          #
+          # This push is required to ensure we have the primary credentials
+          # written out to the .netrc
           Factory::Repository::ProjectPush.fabricate! do |push|
+            push.ssh_key = key
             push.project = project
             push.file_name = file_name
             push.file_content = "# #{file_content_primary}"
-            push.commit_message = 'Add README.md'
+            push.commit_message = "Add #{file_name}"
           end
           project.visit!
-
-          # Validate git push worked and file exists with content
-          Page::Project::Show.perform do |show|
-            show.wait_for_repository_replication
-
-            expect(page).to have_content file_name
-            expect(page).to have_content file_content_primary
-          end
 
           Runtime::Browser.visit(:geo_secondary, QA::Page::Main::Login) do
             # Visit the secondary node and login
@@ -43,43 +45,44 @@ module QA
               expect(banner).to have_secondary_read_only_banner
             end
 
-            Page::Main::Menu.perform { |menu| menu.go_to_projects }
+            # Ensure the SSH key has replicated
+            Page::Main::Menu.act { go_to_profile_settings }
+            Page::Profile::Menu.perform do |menu|
+              menu.click_ssh_keys
+              menu.wait_for_key_to_replicate(key_title)
+            end
 
+            expect(page).to have_content(key_title)
+            expect(page).to have_content(key.fingerprint)
+
+            # Ensure project has replicated
+            Page::Main::Menu.perform { |menu| menu.go_to_projects }
             Page::Dashboard::Projects.perform do |dashboard|
               dashboard.wait_for_project_replication(project.name)
               dashboard.go_to_project(project.name)
             end
 
-            # Validate the content has been sync'd from the primary
-            Page::Project::Show.perform do |show|
-              show.wait_for_repository_replication_with(file_content_primary)
-
-              expect(page).to have_content file_name
-              expect(page).to have_content file_content_primary
-            end
-
-            # Grab the HTTP URI for the secondary and store as 'location'
+            # Grab the SSH URI for the secondary and store as 'location'
             location = Page::Project::Show.act do
-              choose_repository_clone_http
+              choose_repository_clone_ssh
               repository_location
             end
 
-            # Perform a git push over HTTP at the secondary
+            # Perform a git push over SSH at the secondary
             Factory::Repository::Push.fabricate! do |push|
               push.new_branch = false
-              push.repository_http_uri = location.uri
+              push.ssh_key = key
+              push.repository_ssh_uri = location.uri
               push.file_name = file_name
               push.file_content = "# #{file_content_secondary}"
-              push.commit_message = 'Update README.md'
+              push.commit_message = "Update #{file_name}"
             end
 
             # Validate git push worked and new content is visible
             Page::Project::Show.perform do |show|
               show.wait_for_repository_replication_with(file_content_secondary)
-              show.refresh
 
-              expect(page).to have_content file_name
-              expect(page).to have_content file_content_secondary
+              expect(page).to have_content(file_content_secondary)
             end
           end
         end
