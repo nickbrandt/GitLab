c7a89fa48679d7284a6f4ba00a7a05838e4ceca0
Stan Hu <stanhu@gmail.com>
2018-10-10 23:44:26 -0700

    Resolve conflicts in spec/javascripts/boards/mock_data.js

--- spec/javascripts/boards/mock_data.js
+++ spec/javascripts/boards/mock_data.js
@@ -1,14 +1,11 @@
 import BoardService from '~/boards/services/board_service';
 
-<<<<<<< HEAD
 export const boardObj = {
   id: 1,
   name: 'test',
   milestone_id: null,
 };
 
-=======
->>>>>>> upstream/master
 export const listObj = {
   id: 300,
   position: 0,
