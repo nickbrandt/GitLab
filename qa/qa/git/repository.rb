ba993f49f7d1e6dc64a662716b1dc4c6964a09ef
Lin Jen-Shin <godfat@godfat.org>
2018-10-23 20:20:14 +0800

    Resolve qa/qa/git/repository.rb

--- qa/qa/git/repository.rb
+++ qa/qa/git/repository.rb
@@ -61,11 +61,8 @@ module QA
       def configure_identity(name, email)
         run(%Q{git config user.name #{name}})
         run(%Q{git config user.email #{email}})
-<<<<<<< HEAD
-=======
 
         add_credentials_to_netrc
->>>>>>> upstream/master
       end
 
       def commit_file(name, contents, message)
@@ -116,19 +113,6 @@ module QA
 
       attr_reader :uri, :username, :password, :known_hosts_file, :private_key_file
 
-<<<<<<< HEAD
-      def ssh_key_set?
-        !private_key_file.nil?
-      end
-
-      def run(command_str)
-        command = [env_vars, command_str, '2>&1'].compact.join(' ')
-        warn "DEBUG: command=[#{command}]" if Runtime::Env.debug?
-
-        output, _ = Open3.capture2(command)
-        output = output.chomp.gsub(/\s+$/, '')
-        warn "DEBUG: output=[#{output}]" if Runtime::Env.debug?
-=======
       def debug?
         Runtime::Env.respond_to?(:verbose?) && Runtime::Env.verbose?
       end
@@ -144,7 +128,6 @@ module QA
         output, _ = Open3.capture2(command)
         output = output.chomp.gsub(/\s+$/, '')
         warn "DEBUG: output=[#{output}]" if debug?
->>>>>>> upstream/master
 
         output
       end
