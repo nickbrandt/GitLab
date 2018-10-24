8728de58f5c3fc15fc1522b251b6fc62cdc54b8d
Sam Beckham <sbeckham@gitlab.com>
2018-10-24 07:46:48 +0000

    Adds the security dashboard link

--- ee/app/helpers/ee/groups_helper.rb
+++ ee/app/helpers/ee/groups_helper.rb
@@ -2,6 +2,11 @@ module EE
   module GroupsHelper
     extend ::Gitlab::Utils::Override
 
+    override :group_overview_nav_link_paths
+    def group_overview_nav_link_paths
+      super + %w(groups/security/dashboard#show)
+    end
+
     override :group_nav_link_paths
     def group_nav_link_paths
       if ::Gitlab::CurrentSettings.should_check_namespace_plan? && can?(current_user, :admin_group, @group)
