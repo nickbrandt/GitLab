93e9966a8630772c9683248e2de64bc5595ba616
Winnie Hellmann <winnie@gitlab.com>
2018-10-26 17:43:00 +0000

    Resolve conflict in ee_switch_mr_widget_options.js

--- app/assets/javascripts/vue_merge_request_widget/ee_switch_mr_widget_options.js
+++ app/assets/javascripts/vue_merge_request_widget/ee_switch_mr_widget_options.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 import MRWidgetOptions from 'ee/vue_merge_request_widget/mr_widget_options.vue';
-=======
-import MRWidgetOptions from './mr_widget_options.vue';
->>>>>>> upstream/master
 
 export default MRWidgetOptions;
