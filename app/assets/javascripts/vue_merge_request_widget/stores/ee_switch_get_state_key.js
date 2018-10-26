071c4d46393db07fa7a3b1778a17d668296ada92
Winnie Hellmann <winnie@gitlab.com>
2018-10-26 17:43:36 +0000

    Resolve conflict in ee_switch_get_state_key.js

--- app/assets/javascripts/vue_merge_request_widget/stores/ee_switch_get_state_key.js
+++ app/assets/javascripts/vue_merge_request_widget/stores/ee_switch_get_state_key.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 import getStateKey from 'ee/vue_merge_request_widget/stores/get_state_key';
-=======
-import getStateKey from './get_state_key';
->>>>>>> upstream/master
 
 export default getStateKey;
