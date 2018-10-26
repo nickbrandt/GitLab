600e1b0e22bc38d1d6fb55683cb6288c2bc41e69
Winnie Hellmann <winnie@gitlab.com>
2018-10-26 17:44:27 +0000

    Resolve conflict in ee_switch_state_maps.js

--- app/assets/javascripts/vue_merge_request_widget/stores/ee_switch_state_maps.js
+++ app/assets/javascripts/vue_merge_request_widget/stores/ee_switch_state_maps.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 import stateMaps from 'ee/vue_merge_request_widget/stores/state_maps';
-=======
-import stateMaps from './state_maps';
->>>>>>> upstream/master
 
 export default stateMaps;
