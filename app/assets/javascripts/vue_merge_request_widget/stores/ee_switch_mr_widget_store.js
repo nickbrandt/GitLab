c0864caba470696cb8a71e8f22c9c61fbe8b5cc3
Winnie Hellmann <winnie@gitlab.com>
2018-10-26 17:44:07 +0000

    Resolve conflict in ee_switch_mr_widget_store.js

--- app/assets/javascripts/vue_merge_request_widget/stores/ee_switch_mr_widget_store.js
+++ app/assets/javascripts/vue_merge_request_widget/stores/ee_switch_mr_widget_store.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 import MergeRequestStore from 'ee/vue_merge_request_widget/stores/mr_widget_store';
-=======
-import MergeRequestStore from './mr_widget_store';
->>>>>>> upstream/master
 
 export default MergeRequestStore;
