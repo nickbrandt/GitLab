ba8932b2acbb0cf1d8ab26cdca618908a73b3782
Winnie Hellmann <winnie@gitlab.com>
2018-10-26 17:43:19 +0000

    Resolve conflict in ee_switch_mr_widget_service.js

--- app/assets/javascripts/vue_merge_request_widget/services/ee_switch_mr_widget_service.js
+++ app/assets/javascripts/vue_merge_request_widget/services/ee_switch_mr_widget_service.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 import MRWidgetService from 'ee/vue_merge_request_widget/services/mr_widget_service';
-=======
-import MRWidgetService from './mr_widget_service';
->>>>>>> upstream/master
 
 export default MRWidgetService;
