fb83534f3ec01cc754057ba6d4ac6720693f57e7
Brett Walker <bwalker@gitlab.com>
2018-10-29 11:19:42 -0500

    Placeholder for 'ee/config/routes/operations.rb'
    A route was added before the corresponding MR was
    merged.  So add the placeholder so we can keep 'routes.rb' in sync with CE/EE

---
+++ ee/config/routes/operations.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Placeholder for https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/7341
+# Added to resolve https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/8131
+
