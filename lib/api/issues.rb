52c22b4aa2b56035eb9879e99e73d568cfb8b31e
Heinrich Lee Yu <hleeyu@gmail.com>
2018-10-30 15:27:28 +0800

    Allow filtering by weight in issues API

--- lib/api/issues.rb
+++ lib/api/issues.rb
@@ -60,7 +60,7 @@ module API
       end
 
       params :issue_params_ee do
-        optional :weight, type: Integer, desc: 'The weight of the issue'
+        optional :weight, type: [Integer, String], integer_none_any: true, desc: 'The weight of the issue'
       end
 
       params :issue_params do
