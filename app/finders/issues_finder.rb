4e7c2bfdeca7092fc90fd671a55aa8a15819ca36
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2018-10-31 15:59:24 -0300

    Port of da-rubocop-whitelist-active-record-none to EE

--- app/finders/issues_finder.rb
+++ app/finders/issues_finder.rb
@@ -137,7 +137,6 @@ class IssuesFinder < IssuableFinder
     current_user.blank?
   end
 
-  # rubocop: disable CodeReuse/ActiveRecord
   def by_assignee(items)
     if filter_by_no_assignee?
       items.unassigned
@@ -151,5 +150,4 @@ class IssuesFinder < IssuableFinder
       items
     end
   end
-  # rubocop: enable CodeReuse/ActiveRecord
 end
