b593693155db51e4e166cd0c26f17cbb04054a5a
Filipa Lacerda <filipa@gitlab.com>
2018-10-31 11:50:49 +0000

    Fix conflicts for spec/javascripts/vue_mr_widget/mock_data.js

--- spec/javascripts/vue_mr_widget/mock_data.js
+++ spec/javascripts/vue_mr_widget/mock_data.js
@@ -218,7 +218,6 @@ export default {
   diverged_commits_count: 0,
   only_allow_merge_if_pipeline_succeeds: false,
   commit_change_content_path: '/root/acets-app/merge_requests/22/commit_change_content',
-<<<<<<< HEAD
   codeclimate: {
     head_path: 'head.json',
     base_path: 'base.json',
@@ -231,9 +230,6 @@ export default {
     '/help/user/project/merge_requests/index#interacting-with-security-reports-ultimate',
   merge_commit_path:
     'http://localhost:3000/root/acets-app/commit/53027d060246c8f47e4a9310fb332aa52f221775',
-=======
-  merge_commit_path: 'http://localhost:3000/root/acets-app/commit/53027d060246c8f47e4a9310fb332aa52f221775',
->>>>>>> upstream/master
   troubleshooting_docs_path: 'help'
 };
 // Codeclimate
