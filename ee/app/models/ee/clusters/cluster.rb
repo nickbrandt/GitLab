d448994758ef5c96cb77146d115a1a30c8938b29
Thong Kuah <tkuah@gitlab.com>
2018-11-06 17:11:31 +1300

    Removes un-necessary self inside method

--- ee/app/models/ee/clusters/cluster.rb
+++ ee/app/models/ee/clusters/cluster.rb
@@ -10,12 +10,12 @@ module EE
       end
 
       def unique_environment_scope
-        if project && project.clusters.where(environment_scope: environment_scope).where.not(id: self.id).exists?
+        if project && project.clusters.where(environment_scope: environment_scope).where.not(id: id).exists?
           errors.add(:base, "cannot add duplicated environment scope")
           return false
         end
 
-        if group && group.clusters.where(environment_scope: environment_scope).where.not(id: self.id).exists?
+        if group && group.clusters.where(environment_scope: environment_scope).where.not(id: id).exists?
           errors.add(:base, 'cannot add duplicated environment scope')
           return false
         end
