3199d159bb0c81d443afb7d0b085ca258344cc55
Sanad Liaquat <sliaquat@gitlab.com>
2018-11-06 12:06:36 +0500

    Moved and removed a test

--- qa/qa/specs/features/browser_ui/1_manage/ee_group/group_ldap_sync_spec.rb
+++ qa/qa/specs/features/browser_ui/1_manage/ee_group/group_ldap_sync_spec.rb
@@ -2,26 +2,7 @@
 
 module QA
   context 'Manage', :orchestrated, :ldap_tls, :ldap_no_tls do
-    describe 'LDAP Sync' do
-      it 'syncs admin users' do
-        Runtime::Browser.visit(:gitlab, Page::Main::Login)
-
-        Page::Main::Login.perform do |login_page|
-          login_page.sign_in_using_ldap_credentials(username: 'adminuser1', password: 'password')
-        end
-
-        Page::Main::Menu.perform do |menu|
-          expect(menu).to have_personal_area
-
-          # The ldap_sync_worker_cron job is set to run every minute
-          admin_synchronised = menu.wait(max: 65, time: 1, reload: true) do
-            menu.has_admin_area_link?
-          end
-
-          expect(admin_synchronised).to be_truthy
-        end
-      end
-
+    describe 'LDAP Group Sync' do
       it 'Has LDAP user synced using group cn method' do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
 
--- qa/qa/specs/features/browser_ui/1_manage/login/log_into_gitlab_via_ldap_spec.rb
+++ qa/qa/specs/features/browser_ui/1_manage/login/log_into_gitlab_via_ldap_spec.rb
@@ -3,15 +3,22 @@
 module QA
   context 'Manage', :orchestrated, :ldap_no_tls, :ldap_tls do
     describe 'LDAP login' do
-      it 'user logs into GitLab using LDAP credentials' do
+      it 'Login with LDAP and sync admin users' do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
-        Page::Main::Login.act { sign_in_using_credentials }
 
-        # TODO, since `Signed in successfully` message was removed
-        # this is the only way to tell if user is signed in correctly.
-        #
+        Page::Main::Login.perform do |login_page|
+          login_page.sign_in_using_ldap_credentials(username: 'adminuser1', password: 'password')
+        end
+
         Page::Main::Menu.perform do |menu|
           expect(menu).to have_personal_area
+
+          # The ldap_sync_worker_cron job is set to run every minute
+          admin_synchronised = menu.wait(max: 65, time: 1, reload: true) do
+            menu.has_admin_area_link?
+          end
+
+          expect(admin_synchronised).to be_truthy
         end
       end
     end
