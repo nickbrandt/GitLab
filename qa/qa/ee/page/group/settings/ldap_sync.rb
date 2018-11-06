7483ed5bdc10b143126b49786dca03ea4b4fe11b
Sanad Liaquat <sliaquat@gitlab.com>
2018-11-06 10:58:19 +0500

    Added another test and some refactoring

--- qa/qa/ee/page/group/settings/ldap_sync.rb
+++ qa/qa/ee/page/group/settings/ldap_sync.rb
@@ -11,6 +11,7 @@ module QA
             view 'ee/app/views/ldap_group_links/_form.html.haml' do
               element :ldap_group_cn_select
               element :add_sync_button
+              element :ldap_user_filter_text_field
             end
 
             def set_sync_method(method)
@@ -20,6 +21,10 @@ module QA
             def set_group_cn(group_cn)
               click_element :ldap_group_cn_select
               search_and_select(group_cn)
+            end
+
+            def set_user_filter(user_filter)
+              fill_element :ldap_user_filter_text_field, user_filter
             end
 
             def click_add_sync_button
--- qa/qa/specs/features/browser_ui/1_manage/ee_group/group_ldap_sync_spec.rb
+++ qa/qa/specs/features/browser_ui/1_manage/ee_group/group_ldap_sync_spec.rb
@@ -1,46 +1,16 @@
 # frozen_string_literal: true
 
 module QA
-  context 'Manage', :orchestrated, :ldap_tls do
+  # context 'Manage', :orchestrated, :ldap_tls do
+  context 'Manage' do
     describe 'LDAP Group Sync' do
-      it 'Has LDAP user synced with group' do
+      it 'Has LDAP user synced using group cn method' do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
 
-        # Login and Logout enguser3
-        Page::Main::Login.perform do |login_page|
-          login_page.sign_in_using_ldap_credentials(username: 'enguser3', password: 'password')
-        end
+        # Login users to create them
+        login_logout_users(['enguser3', 'enguser2'])
 
-        Page::Main::Menu.perform do |menu|
-          expect(menu).to have_personal_area
-        end
-
-        Page::Main::Menu.perform(&:sign_out)
-
-        # Login and Logout enguser2
-        Page::Main::Login.perform do |login_page|
-          login_page.sign_in_using_ldap_credentials(username: 'enguser2', password: 'password')
-        end
-
-        Page::Main::Menu.perform do |menu|
-          expect(menu).to have_personal_area
-        end
-
-        Page::Main::Menu.perform(&:sign_out)
-
-        # Login enguser1
-        Page::Main::Login.perform do |login_page|
-          login_page.sign_in_using_ldap_credentials(username: 'enguser1', password: 'password')
-        end
-
-        Page::Main::Menu.perform do |menu|
-          expect(menu).to have_personal_area
-        end
-
-        # Create a sand box group
-        Resource::Sandbox.fabricate_via_browser_ui! do |resource|
-          resource.path = "Synched-engineering-group-#{SecureRandom.hex(4)}"
-        end
+        create_sandbox_group_with_user(user: 'enguser1', group_name:'Synched-engineering-group')
 
         EE::Page::Group::Menu.perform(&:go_to_ldap_sync_settings)
 
@@ -52,11 +22,64 @@ module QA
 
         EE::Page::Group::Menu.perform(&:go_to_members)
 
+        verify_users_synched(['ENG User 2', 'ENG User 3'])
+
+      end
+
+      it 'Has LDAP user synced using user filter method' do
+        Runtime::Browser.visit(:gitlab, Page::Main::Login)
+
+        # Login users to create them
+        login_logout_users(['hruser3', 'hruser2'])
+
+        create_sandbox_group_with_user(user: 'hruser1', group_name:'Synched-human-resources-group')
+
+        EE::Page::Group::Menu.perform(&:go_to_ldap_sync_settings)
+
+        EE::Page::Group::Settings::LDAPSync.perform do |page|
+          page.set_user_filter('(&(objectClass=person)(cn=HR*))')
+          page.click_add_sync_button
+        end
+
+        EE::Page::Group::Menu.perform(&:go_to_members)
+
+        verify_users_synched(['HR User 2', 'HR User 3'])
+      end
+
+      def login_logout_users(users)
+        users.each do |user|
+          Page::Main::Login.perform do |login_page|
+            login_page.sign_in_using_ldap_credentials(username: user, password: 'password')
+          end
+
+          Page::Main::Menu.perform do |menu|
+            expect(menu).to have_personal_area
+          end
+
+          Page::Main::Menu.perform(&:sign_out)
+        end
+      end
+
+      def create_sandbox_group_with_user(user: nil, group_name: nil)
+        Page::Main::Login.perform do |login_page|
+          login_page.sign_in_using_ldap_credentials(username: user, password: 'password')
+        end
+
+        Page::Main::Menu.perform do |menu|
+          expect(menu).to have_personal_area
+        end
+
+        Resource::Sandbox.fabricate_via_browser_ui! do |resource|
+          resource.path = "#{group_name}-#{SecureRandom.hex(4)}"
+        end
+      end
+
+      def verify_users_synched(expected_users)
         users_synchronised = false
         EE::Page::Group::Members.perform do |page|
           page.click_sync_now
           users_synchronised = page.with_retry(reload: true) do
-            page.has_content?('ENG User 2') && page.has_content?('ENG User 3')
+            expected_users.map { |user| page.has_content?(user) }.reduce(true) { |a, b| a && b }
           end
         end
 
