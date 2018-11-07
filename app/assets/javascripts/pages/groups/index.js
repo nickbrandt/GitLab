658c2647e30d5968ff55e5591de7d9b2f9140503
Luke Bennett <lbennett@gitlab.com>
2018-11-07 05:06:40 +0000

    Promote the GitLab.com Gold trial on a users default dashboard
    Show a dismissable callout on a users dashboard for a free trial of
    the GitLab.com Gold plan. Hide the callout from users who have
    dismissed the callout, are already on a trial or are already on the
    gold plan

--- app/assets/javascripts/pages/groups/index.js
+++ app/assets/javascripts/pages/groups/index.js
@@ -1,7 +1,7 @@
 import PersistentUserCallout from '~/persistent_user_callout';
 import initGkeDropdowns from '~/projects/gke_cluster_dropdowns';
 
-function initGcpSignupCallout() {
+function initCallout() {
   const callout = document.querySelector('.gcp-signup-offer');
 
   if (callout) new PersistentUserCallout(callout); // eslint-disable-line no-new
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
   ];
 
   if (newClusterViews.indexOf(page) > -1) {
-    initGcpSignupCallout();
+    initCallout();
     initGkeDropdowns();
   }
 });
