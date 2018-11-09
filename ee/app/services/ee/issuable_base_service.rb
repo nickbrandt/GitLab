cd5932328f17e08b7d73b03bb728961bace749d6
Felipe Artur <felipefac@gmail.com>
2018-11-09 14:42:42 -0200

    Resolve: Guest can set weight of a new issue
    Delete weight parameter when guest is creating issuables.

--- ee/app/services/ee/issuable_base_service.rb
+++ ee/app/services/ee/issuable_base_service.rb
@@ -3,7 +3,13 @@ module EE
     private
 
     def filter_params(issuable)
-      params.delete(:weight) unless issuable.supports_weight?
+      # This security check is repeated here to avoid multiple backports,
+      # this should be refactored to be reused from the base class.
+      ability_name = :"admin_#{issuable.to_ability_name}"
+
+      unless issuable.supports_weight? && can?(current_user, ability_name, issuable)
+        params.delete(:weight)
+      end
 
       super
     end
