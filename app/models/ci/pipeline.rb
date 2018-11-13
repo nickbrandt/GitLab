4feeb5f3558bc5721142a6f5718c6cff490a8e11
Yorick Peterse <yorickpeterse@gmail.com>
2018-11-13 17:29:14 +0100

    Refactor how a few ActiveRecord enums are defined
    In a few models we define ActiveRecord enums that are redefined in EE
    using the following pattern:
    
        enum :some_enum, {
          ...
        }.merge(EE_ENUM_VALUES)
    
    This particular approach is problematic to deal with, because it
    requires that we `prepend` and EE module _before_ defining the enum.
    This typically translates to the `prepend` being the first line in the
    model in EE, but this can easily lead to merge conflicts when developers
    add more `include` and/or `prepend` lines.
    
    As part of https://gitlab.com/gitlab-org/gitlab-ee/issues/8244 and
    https://gitlab.com/gitlab-org/gitlab-ee/issues/8241 we are moving
    `prepend` to the last line in a file, reducing the chances of running
    into merge conflicts. This poses a bit of a problem with the pattern
    above, because this pattern does not allow us to move the `prepend`
    further down a file.
    
    To resolve this problem, we simply move the Hash value of the enum to a
    separate class method. This method is defined in a separate module where
    necessary, allowing us to use it like so:
    
        enum :failure_reasons, ::SomeModelEnums.failure_reasons
    
    The method in turn is defined in a very straightforward manner:
    
        module SomeModelEnums
          def self.failure_reasons
            {
              ...
            }
          end
        end
    
    This makes it easy for EE to add values without requiring the `prepend`
    to be placed before the `enum` is defined.
    
    For more information, see the following issues and merge requests:
    
    * https://gitlab.com/gitlab-org/gitlab-ee/issues/8244
    * https://gitlab.com/gitlab-org/gitlab-ee/issues/8241
    * https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/8424

--- app/models/ci/pipeline.rb
+++ app/models/ci/pipeline.rb
@@ -69,17 +69,9 @@ module Ci
 
     after_create :keep_around_commits, unless: :importing?
 
-    enum_with_nil source: {
-      unknown: nil,
-      push: 1,
-      web: 2,
-      trigger: 3,
-      schedule: 4,
-      api: 5,
-      external: 6,
-      pipeline: 7,
-      chat: 8
-    }
+    # We use `Ci::PipelineEnums.sources` here so that EE can more easily extend
+    # this `Hash` with new values.
+    enum_with_nil source: ::Ci::PipelineEnums.sources
 
     enum_with_nil config_source: {
       unknown_source: nil,
@@ -87,10 +79,9 @@ module Ci
       auto_devops_source: 2
     }
 
-    enum failure_reason: {
-      unknown_failure: 0,
-      config_error: 1
-    }.merge(EE_FAILURE_REASONS)
+    # We use `Ci::PipelineEnums.failure_reasons` here so that EE can more easily
+    # extend this `Hash` with new values.
+    enum failure_reason: ::Ci::PipelineEnums.failure_reasons
 
     state_machine :status, initial: :created do
       event :enqueue do
