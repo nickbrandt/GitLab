7a6443d4fdf0875f2161d2ec14f78509a3121894
Jan Provaznik <jprovaznik@gitlab.com>
2018-11-13 13:50:38 +0100

    Authorize user when listing board resources
    Make sure that user is authorized to read users/milestones
    on board users/milestone index actions.

---
+++ ee/app/controllers/concerns/ee/boards_responses.rb
@@ -0,0 +1,17 @@
+module EE
+  module BoardsResponses
+    extend ActiveSupport::Concern
+
+    def authorize_read_parent
+      ability = board.group_board? ? :read_group : :read_project
+
+      authorize_action_for!(board.parent, ability)
+    end
+
+    def authorize_read_milestone
+      ability = board.group_board? ? :read_group : :read_milestone
+
+      authorize_action_for!(board.parent, ability)
+    end
+  end
+end
