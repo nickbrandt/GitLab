0a8db3dc2f15196d91e06e4d3fede92d7679716e
Francisco Javier López <fjlopez@gitlab.com>
2018-11-13 11:35:37 +0100

    Refactor Gitlab::Checks::ChangeAccess and EE::Gitlab::Checks::ChangeAccess
    This class handles the validations of git pushes. It is
    quite big and all the logic is inside one file. This commit
    refactors the code and split it into different files.

---
+++ ee/lib/ee/gitlab/checks/diff_check.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+module EE
+  module Gitlab
+    module Checks
+      module DiffCheck
+        extend ActiveSupport::Concern
+        extend ::Gitlab::Utils::Override
+
+        private
+
+        override :should_run_diff_validations?
+        def should_run_diff_validations?
+          super || validate_path_locks? || push_rule_checks_commit?
+        end
+
+        def validate_path_locks?
+          strong_memoize(:validate_path_locks) do
+            project.feature_available?(:file_locks) &&
+              project.any_path_locks? &&
+              project.default_branch == branch_name # locks protect default branch only
+          end
+        end
+
+        def push_rule_checks_commit?
+          return false unless push_rule
+
+          push_rule.file_name_regex.present? || push_rule.prevent_secrets
+        end
+
+        override :validations_for_diff
+        def validations_for_diff
+          super.tap do |validations|
+            validations.push(path_locks_validation) if validate_path_locks?
+            validations.push(file_name_validation) if push_rule
+          end
+        end
+
+        def path_locks_validation
+          lambda do |diff|
+            path = diff.new_path || diff.old_path
+
+            lock_info = project.find_path_lock(path)
+
+            if lock_info && lock_info.user != user_access.user
+              return "The path '#{lock_info.path}' is locked by #{lock_info.user.name}"
+            end
+          end
+        end
+
+        def file_name_validation
+          lambda do |diff|
+            begin
+              if (diff.renamed_file || diff.new_file) && blacklisted_regex = push_rule.filename_blacklisted?(diff.new_path)
+                return nil unless blacklisted_regex.present?
+
+                "File name #{diff.new_path} was blacklisted by the pattern #{blacklisted_regex}."
+              end
+            rescue ::PushRule::MatchError => e
+              raise ::Gitlab::GitAccess::UnauthorizedError, e.message
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ ee/lib/ee/gitlab/checks/push_rules/tag_check.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module EE
+  module Gitlab
+    module Checks
+      module PushRules
+        class TagCheck < ::Gitlab::Checks::BaseChecker
+          def validate!
+            return unless newrev && oldrev && push_rule
+
+            logger.log_timed("Checking if you are allowed to delete a tag...") do
+              if tag_deletion_denied_by_push_rule?
+                raise ::Gitlab::GitAccess::UnauthorizedError, 'You cannot delete a tag'
+              end
+            end
+          end
+
+          private
+
+          def tag_deletion_denied_by_push_rule?
+            push_rule.deny_delete_tag &&
+              !updated_from_web? &&
+              deletion? &&
+              tag_exists?
+          end
+        end
+      end
+    end
+  end
+end
