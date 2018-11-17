5aa5681e48950f8747b985caa12ba7c8c40c6a81
Peter Leitzen <pleitzen@gitlab.com>
2018-11-17 10:25:03 +0100

    Extract PrometheusConfigService

--- ee/app/services/clusters/applications/prometheus_update_service.rb
+++ ee/app/services/clusters/applications/prometheus_update_service.rb
@@ -11,17 +11,13 @@ module Clusters
       def execute
         app.make_updating!
 
-        response = helm_api.get_config_map(config_map_name)
-        config = extract_config(response)
+        values = helm_api
+          .get_config_map(config_map_name)
+          .yield_self { |response| extract_config(response) }
+          .yield_self { |config| update_config(config) }
+          .yield_self { |config| config.to_yaml }
 
-        data =
-          if has_alerts?
-            generate_alert_manager(config)
-          else
-            reset_alert_manager(config)
-          end
-
-        helm_api.update(upgrade_command(data.to_yaml))
+        helm_api.update(upgrade_command(values))
 
         ::ClusterWaitForAppUpdateWorker.perform_in(::ClusterWaitForAppUpdateWorker::INTERVAL, app.name, app.id)
       rescue ::Kubeclient::HttpError => ke
@@ -36,120 +32,14 @@ module Clusters
         ::Gitlab::Kubernetes::ConfigMap.new(app.name, app.files).config_map_name
       end
 
-      def reset_alert_manager(config)
-        config = set_alert_manager_enabled(config, false)
-        config.delete("alertmanagerFiles")
-        config["serverFiles"]["alerts"] = {}
-
-        config
-      end
-
-      def generate_alert_manager(config)
-        config = set_alert_manager_enabled(config, true)
-        config = set_alert_manager_files(config)
-
-        set_alert_manager_groups(config)
-      end
-
-      def set_alert_manager_enabled(config, enabled)
-        config["alertmanager"]["enabled"] = enabled
-
-        config
-      end
-
-      def set_alert_manager_files(config)
-        config["alertmanagerFiles"] = {
-          "alertmanager.yml" => {
-            "receivers" => alert_manager_receivers_params,
-            "route" => alert_manager_route_params
-          }
-        }
-
-        config
-      end
-
-      def set_alert_manager_groups(config)
-        config["serverFiles"]["alerts"]["groups"] ||= []
-
-        environments_with_alerts.each do |env_name, alerts|
-          index = config["serverFiles"]["alerts"]["groups"].find_index do |group|
-            group["name"] == env_name
-          end
-
-          if index
-            config["serverFiles"]["alerts"]["groups"][index]["rules"] = alerts
-          else
-            config["serverFiles"]["alerts"]["groups"] << {
-              "name" => env_name,
-              "rules" => alerts
-            }
-          end
-        end
-
-        config
-      end
-
-      def alert_manager_receivers_params
-        [
-          {
-            "name" => "gitlab",
-            "webhook_configs" => [
-              {
-                "url" => notify_url,
-                "send_resolved" => true
-              }
-            ]
-          }
-        ]
-      end
-
-      def alert_manager_route_params
-        {
-          "receiver" => "gitlab",
-          "group_wait" => "30s",
-          "group_interval" => "5m",
-          "repeat_interval" => "4h"
-        }
-      end
-
-      def notify_url
-        ::Gitlab::Routing.url_helpers.notify_namespace_project_prometheus_alerts_url(
-          namespace_id: project.namespace.path,
-          project_id: project.path,
-          format: :json
-        )
-      end
-
       def extract_config(response)
         YAML.safe_load(response.data[:'values.yaml'])
       end
 
-      def has_alerts?
-        environments_with_alerts.values.flatten.any?
-      end
-
-      def environments_with_alerts
-        @environments_with_alerts ||=
-          environments.each_with_object({}) do |environment, hsh|
-            name = rule_name(environment)
-            hsh[name] = alerts(environment)
-          end
-      end
-
-      def rule_name(environment)
-        "#{environment.name}.rules"
-      end
-
-      def alerts(environment)
-        environment.prometheus_alerts.map do |alert|
-          alert.to_param.tap do |hash|
-            hash['expr'] %= Gitlab::Prometheus::QueryVariables.call(environment)
-          end
-        end
-      end
-
-      def environments
-        project.environments_for_scope(cluster.environment_scope)
+      def update_config(config)
+        PrometheusConfigService
+          .new(project, cluster)
+          .execute(config)
       end
     end
   end
