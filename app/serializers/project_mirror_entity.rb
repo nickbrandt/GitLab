97cf87ea1b4a17b6b4607dcf95a8f59262e6ab08
Nick Thomas <nick@gitlab.com>
2018-11-20 16:42:53 +0000

    CE->EE: Resolve conflicts in app/serializers/project_mirror_entity.rb

--- app/serializers/project_mirror_entity.rb
+++ app/serializers/project_mirror_entity.rb
@@ -3,11 +3,8 @@
 class ProjectMirrorEntity < Grape::Entity
   prepend ::EE::ProjectMirrorEntity
 
-<<<<<<< HEAD
   expose :id
 
-=======
->>>>>>> upstream/master
   expose :remote_mirrors_attributes, using: RemoteMirrorEntity do |project|
     project.remote_mirrors
   end
