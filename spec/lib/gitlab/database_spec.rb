d4518791a1844e5dd179468309d6255d512c87b9
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2018-11-26 17:42:18 -0200

    Move EE specific code from Gitbab::Database to ee/
    This commit move EE specific code from GitLab::Database
    into ee/, and remove unused methods.

--- spec/lib/gitlab/database_spec.rb
+++ spec/lib/gitlab/database_spec.rb
@@ -443,51 +443,9 @@ describe Gitlab::Database do
     end
   end
 
-  describe '#disable_prepared_statements' do
-    it 'disables prepared statements' do
-      config = {}
-
-      expect(ActiveRecord::Base.configurations).to receive(:[])
-        .with(Rails.env)
-        .and_return(config)
-
-      expect(ActiveRecord::Base).to receive(:establish_connection)
-        .with({ 'prepared_statements' => false })
-
-      described_class.disable_prepared_statements
-
-      expect(config['prepared_statements']).to eq(false)
-    end
-  end
-
   describe '.read_only?' do
-    context 'with Geo enabled' do
-      before do
-        allow(Gitlab::Geo).to receive(:enabled?) { true }
-        allow(Gitlab::Geo).to receive(:current_node) { geo_node }
-      end
-
-      context 'is Geo secondary node' do
-        let(:geo_node) { create(:geo_node) }
-
-        it 'returns true' do
-          expect(described_class.read_only?).to be_truthy
-        end
-      end
-
-      context 'is Geo primary node' do
-        let(:geo_node) { create(:geo_node, :primary) }
-
-        it 'returns false when is Geo primary node' do
-          expect(described_class.read_only?).to be_falsey
-        end
-      end
-    end
-
-    context 'with Geo disabled' do
-      it 'returns false' do
-        expect(described_class.read_only?).to be_falsey
-      end
+    it 'returns false' do
+      expect(described_class.read_only?).to be_falsey
     end
   end
 
