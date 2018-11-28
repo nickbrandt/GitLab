5cca659d7ae531de0aeeec3ec13eb6406ee4451b
Reuben Pereira <rpereira@gitlab.com>
2018-11-28 22:58:27 +0000

    [master] Stored XSS in Operation Page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181112103239) do
+ActiveRecord::Schema.define(version: 20181116100917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
