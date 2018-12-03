c0bc3aaae2005c924ab917f4947ef717380d0c5c
Steve Azzopardi <steveazz@outlook.com>
2018-12-03 10:47:25 +0100

    Resolve conflicts in spec/migrations/delete_inconsistent_internal_id_records_spec.rb
    Resolve conflict for https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/8664

--- spec/migrations/delete_inconsistent_internal_id_records_spec.rb
+++ spec/migrations/delete_inconsistent_internal_id_records_spec.rb
@@ -95,16 +95,10 @@ describe DeleteInconsistentInternalIdRecords, :migration do
 
   context 'for milestones (by group)' do
     # milestones (by group) is a little different than most of the other models
-<<<<<<< HEAD
-    let!(:group1) { create(:group) }
-    let!(:group2) { create(:group) }
-    let!(:group3) { create(:group) }
-=======
     let(:groups)  { table(:namespaces) }
     let(:group1) { groups.create(name: 'Group 1', type: 'Group', path: 'group_1') }
     let(:group2) { groups.create(name: 'Group 2', type: 'Group', path: 'group_2') }
     let(:group3) { groups.create(name: 'Group 2', type: 'Group', path: 'group_3') }
->>>>>>> upstream/master
 
     let(:internal_id_query) { ->(group) { InternalId.where(usage: InternalId.usages['milestones'], namespace: group) } }
 
