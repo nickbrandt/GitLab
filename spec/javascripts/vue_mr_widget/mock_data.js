f8d5e735bd27148b2a14f9987da083f0dde2e9da
Paul Slaughter <pslaughter@gitlab.com>
2018-12-03 09:58:51 -0600

    Resolve CE to EE of mr/mock_data

--- spec/javascripts/vue_mr_widget/mock_data.js
+++ spec/javascripts/vue_mr_widget/mock_data.js
@@ -232,7 +232,6 @@ export default {
     'http://localhost:3000/root/acets-app/commit/53027d060246c8f47e4a9310fb332aa52f221775',
   troubleshooting_docs_path: 'help',
 };
-<<<<<<< HEAD
 // Codeclimate
 export const headIssues = [
   {
@@ -402,8 +401,6 @@ export const codequalityParsedIssues = [
     urlPath: 'foo/Gemfile.lock',
   },
 ];
-=======
->>>>>>> upstream/master
 
 export const mockStore = {
   pipeline: { id: 0 },
