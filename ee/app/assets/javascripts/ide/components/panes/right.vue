aacdc35da53089ac2a734005063a47d362bcfd32
Paul Slaughter <pslaughter@gitlab.com>
2018-12-04 08:51:02 +0000

    Add empty and disabled web terminal tab

--- ee/app/assets/javascripts/ide/components/panes/right.vue
+++ ee/app/assets/javascripts/ide/components/panes/right.vue
@@ -1,15 +1,29 @@
 <script>
+import { __ } from '~/locale';
 import RightPane from '~/ide/components/panes/right.vue';
+import TerminalView from '../terminal/view.vue';
 
 export default {
   name: 'EERightPane',
   components: {
     RightPane,
   },
+  data() {
+    return {
+      // this will come from Vuex store in https://gitlab.com/gitlab-org/gitlab-ee/issues/5426
+      isTerminalEnabled: false,
+    };
+  },
   computed: {
     extensionTabs() {
-      // This is empty for now, but it will be used in: https://gitlab.com/gitlab-org/gitlab-ee/issues/5426
-      return [];
+      return [
+        {
+          show: this.isTerminalEnabled,
+          title: __('Terminal'),
+          views: [{ name: 'terminal', keepAlive: true, component: TerminalView }],
+          icon: 'terminal',
+        },
+      ];
     },
   },
 };
