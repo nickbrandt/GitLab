bb5194f5f78dfce16652b93a834e2b4f434f6d6c
Francisco Javier López <fjlopez@gitlab.com>
2018-12-05 21:46:44 +0000

    Update create_pipeline_service.rb

--- app/services/ci/create_pipeline_service.rb
+++ app/services/ci/create_pipeline_service.rb
@@ -17,11 +17,7 @@ module Ci
                 Gitlab::Ci::Pipeline::Chain::Create,
                 EE::Gitlab::Ci::Pipeline::Chain::Limit::Activity].freeze
 
-<<<<<<< HEAD
     def execute(source, ignore_skip_ci: false, save_on_errors: true, trigger_request: nil, schedule: nil, merge_request: nil, mirror_update: false, &block)
-=======
-    def execute(source, ignore_skip_ci: false, save_on_errors: true, trigger_request: nil, schedule: nil, merge_request: nil, &block)
->>>>>>> upstream/master
       @pipeline = Ci::Pipeline.new
 
       command = Gitlab::Ci::Pipeline::Chain::Command.new(
