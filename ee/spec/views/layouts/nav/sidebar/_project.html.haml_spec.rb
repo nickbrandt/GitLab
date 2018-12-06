e78c35bce509abffd51462d54879b6423a947cb5
Matija Čupić <matteeyah@gmail.com>
2018-12-06 23:15:21 +0100

    Stub feature_flags feature check for project

--- ee/spec/views/layouts/nav/sidebar/_project.html.haml_spec.rb
+++ ee/spec/views/layouts/nav/sidebar/_project.html.haml_spec.rb
@@ -14,28 +14,34 @@ describe 'layouts/nav/sidebar/_project' do
   end
 
   describe 'Operations main link' do
-    describe 'when ci/cd is disabled' do
+    let(:user) { create(:user) }
+
+    before do
+      stub_licensed_features(feature_flags: true)
+
+      project.project_feature.update(builds_access_level: feature)
+
+      project.team.add_developer(user)
+      sign_in(user)
+    end
+
+    context 'when ci/cd is disabled' do
       let(:feature) { ProjectFeature::DISABLED }
 
       it 'links to feature flags page' do
-        allow(view).to receive(:can?).with(:read_environment).and_return(false)
-        allow(view).to receive(:can?).with(:read_cluster).and_return(false)
-        allow(view).to receive(:can?).with(:read_feature_flag).and_return(true)
         render
 
-        expect(rendered).to have_link('Operations', :href=> project_feature_flags_path(project))
+        expect(rendered).to have_link('Operations', href: project_feature_flags_path(project))
       end
     end
 
-    describe 'when ci/cd is enabled' do
+    context 'when ci/cd is enabled' do
       let(:feature) { ProjectFeature::ENABLED }
 
-      it 'links to metrics page' do 
-        allow(view).to receive(:can?).and_return(true)
-
+      it 'links to metrics page' do
         render
 
-        expect(rendered).to have_link('Operations', :href=> metrics_project_environments_path(project))        
+        expect(rendered).to have_link('Operations', href: metrics_project_environments_path(project))
       end
     end
   end
