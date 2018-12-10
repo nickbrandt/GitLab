5ec1e539cc943ca1c4149d3d0c053fc23c3bf0f4
Stan Hu <stanhu@gmail.com>
2018-12-10 14:11:56 -0800

    Fix DiffCheck failing due to invalid string argument
    Hot patch for #55137

--- lib/gitlab/checks/diff_check.rb
+++ lib/gitlab/checks/diff_check.rb
@@ -12,6 +12,7 @@ module Gitlab
       }.freeze
 
       def validate!
+        return if deletion? || newrev.nil?
         return unless should_run_diff_validations?
         return if commits.empty?
         return unless uses_raw_delta_validations?
