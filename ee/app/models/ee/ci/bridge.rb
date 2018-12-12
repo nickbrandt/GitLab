d426d249c7abb6938e667f5806399638a9d35f4f
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-12-12 11:29:22 +0100

    Update comments about design choices about bridge jobs

--- ee/app/models/ee/ci/bridge.rb
+++ ee/app/models/ee/ci/bridge.rb
@@ -15,11 +15,15 @@ module EE
               bridge.schedule_downstream_pipeline!
 
               # 1. schedule pipeline creation async
-              # 2. scheduled pipeline calls-back to change state to running
-              #    when it gets created successfully
-              # 3. if no downstream pipeline can not be created because of
-              #    various reasons like lack of access then we change state to
-              #    failed with a reason
+              # 2. scheduled pipeline calls-back to change state to success or
+              #    running when it gets created successfully
+              # 3. if no downstream pipeline can be created because of various
+              #    reasons, like lack of access, then we change state of this
+              #    job to failed with a reason
+              # 4. Status of this job depends on the trigger specification,
+              #    it can `wait` for the status, `depend` on the status or
+              #    be just instant trigger without status attribution.
+              # 5. In the first iteration it supports no status attribution.
             end
           end
 
