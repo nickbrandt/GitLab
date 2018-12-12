d131ba8e714b2b0f4cbbc3dc5c33d47e35e1f971
Robert Speicher <rspeicher@gmail.com>
2018-12-12 18:54:59 +0000

    Merge branch 'if-8805-smartcard_support_for_older_nginx' into 'master'
    Support older NGINX version forwarding the client certificate for smartcard auth
    
    See merge request gitlab-org/gitlab-ee!8784
    
    (cherry picked from commit 5919727500b72ec6b5e37baef6596ccc640abe7c)
    
    4f0418a2 Support certificate header in multiple format for smartcard auth

--- ee/spec/requests/smartcard_controller_spec.rb
+++ ee/spec/requests/smartcard_controller_spec.rb
@@ -76,6 +76,32 @@ describe SmartcardController, type: :request  do
         end
       end
 
+      context 'certificate header formats from NGINX' do
+        shared_examples 'valid certificate header' do
+          it 'authenticates user' do
+            expect(Gitlab::Auth::Smartcard::Certificate).to receive(:new).with(expected_certificate).and_call_original
+
+            subject
+
+            expect(request.env['warden']).to be_authenticated
+          end
+        end
+
+        let(:expected_certificate) { "-----BEGIN CERTIFICATE-----\nrow\nrow\n-----END CERTIFICATE-----" }
+
+        context 'escaped format' do
+          let(:certificate_headers) { { 'X-SSL-CLIENT-CERTIFICATE': '-----BEGIN%20CERTIFICATE-----%0Arow%0Arow%0A-----END%20CERTIFICATE-----' } }
+
+          it_behaves_like 'valid certificate header'
+        end
+
+        context 'deprecated format' do
+          let(:certificate_headers) { { 'X-SSL-CLIENT-CERTIFICATE': '-----BEGIN CERTIFICATE----- row row -----END CERTIFICATE-----' } }
+
+          it_behaves_like 'valid certificate header'
+        end
+      end
+
       context 'missing certificate headers' do
         let(:certificate_headers) { nil }
 
