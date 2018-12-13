132fc9d0be0128733d72460e9c9559003a354bde
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-12-13 12:11:49 +0100

    Add description of insufficient permissions failure reason

--- ee/app/presenters/ee/commit_status_presenter.rb
+++ ee/app/presenters/ee/commit_status_presenter.rb
@@ -5,7 +5,8 @@ module EE
 
     prepended do
       EE_CALLOUT_FAILURE_MESSAGES = const_get(:CALLOUT_FAILURE_MESSAGES).merge(
-        protected_environment_failure: 'The environment this job is deploying to is protected. Only users with permission may successfully run this job'
+        protected_environment_failure: 'The environment this job is deploying to is protected. Only users with permission may successfully run this job.',
+        insufficient_permissions: 'This job could not be executed because of insufficient permissions.'
       ).freeze
 
       EE::CommitStatusPresenter.private_constant :EE_CALLOUT_FAILURE_MESSAGES
