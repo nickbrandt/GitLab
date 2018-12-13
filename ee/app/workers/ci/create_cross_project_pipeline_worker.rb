12bdf876a291ca3ddf8c0a3adb116ac87ae979aa
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-12-13 12:12:15 +0100

    Fix namespace in cross project pipeline worker

--- ee/app/workers/ci/create_cross_project_pipeline_worker.rb
+++ ee/app/workers/ci/create_cross_project_pipeline_worker.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
-class CreateCrossProjectPipelineWorker
-  include ::ApplicationWorker
-  include ::PipelineQueue
+module Ci
+  class CreateCrossProjectPipelineWorker
+    include ::ApplicationWorker
+    include ::PipelineQueue
 
-  def perform(bridge_id)
-    ::Ci::Bridge.find(bridge_id).try do |bridge|
-      ::Ci::CreateCrossProjectPipelineService
-        .new(bridge.project, bridge.user)
-        .execute(bridge)
+    def perform(bridge_id)
+      ::Ci::Bridge.find(bridge_id).try do |bridge|
+        ::Ci::CreateCrossProjectPipelineService
+          .new(bridge.project, bridge.user)
+          .execute(bridge)
+      end
     end
   end
 end
