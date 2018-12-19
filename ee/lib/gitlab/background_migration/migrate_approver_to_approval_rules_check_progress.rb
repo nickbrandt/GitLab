fc73f55162c898dd590c2d895c94cceb79797a32
Mark Chao <mchao@gitlab.com>
2018-12-19 16:40:33 +0800

    Update progress checker
    Check presence of type of jobs in queue

--- ee/lib/gitlab/background_migration/migrate_approver_to_approval_rules_check_progress.rb
+++ ee/lib/gitlab/background_migration/migrate_approver_to_approval_rules_check_progress.rb
@@ -6,35 +6,17 @@ module Gitlab
       RESCHEDULE_DELAY = 1.day
 
       def perform
-        if remaining('MergeRequest') == 0 && remaining('Project') == 0
-          Feature.enable(:approval_rule)
-        else
+        if remaining?
           BackgroundMigrationWorker.perform_in(RESCHEDULE_DELAY, self.class.name)
+        else
+          Feature.enable(:approval_rule)
         end
       end
 
       private
 
-      def remaining(class_name)
-        target_type = ActiveRecord::Base.connection.quote(class_name)
-
-        sql_old_schema = <<-SQL.strip_heredoc
-          SELECT count(*) FROM (
-            SELECT target_id FROM "approvers" WHERE "approvers"."target_type" = #{target_type}
-            UNION
-            SELECT target_id FROM "approver_groups" WHERE "approver_groups"."target_type" = #{target_type}
-          ) AS target_count
-        SQL
-
-        sql_new_schema = <<-SQL.strip_heredoc
-          SELECT count(distinct #{class_name.foreign_key}) from approval_#{class_name.underscore}_rules
-        SQL
-
-        count(sql_old_schema) - count(sql_new_schema)
-      end
-
-      def count(sql)
-        ActiveRecord::Base.connection.exec_query(sql).first['count']
+      def remaining?
+        Gitlab::BackgroundMigration.exists?('MigrateApproverToApprovalRulesInBatch')
       end
     end
   end
