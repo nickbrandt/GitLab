f0620b76f15c9ce864e804eb0dc82bb5a665a1b7
Reuben Pereira <rpereira@gitlab.com>
2018-12-26 10:38:50 +0000

    [11.4] Add a shared secret to prevent abuse of the alert endpoint

---
+++ ee/db/migrate/20181026085436_add_alert_manager_token_to_clusters_application_prometheus.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AddAlertManagerTokenToClustersApplicationPrometheus < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  def change
+    add_column :clusters_applications_prometheus, :encrypted_alert_manager_token, :string
+    add_column :clusters_applications_prometheus, :encrypted_alert_manager_token_iv, :string
+  end
+end
