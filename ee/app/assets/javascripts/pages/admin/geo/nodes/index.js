6468b7ed2a76b1d5800db3d292fe7dfea848e29f
Michael Kozono <mkozono@gmail.com>
2019-01-02 15:30:47 -0800

    Geo: Add hashed storage warnings
    Hashed storage is now a requirement of Geo. This is
    one part of deprecating non-hashed storage for Geo.

--- ee/app/assets/javascripts/pages/admin/geo/nodes/index.js
+++ ee/app/assets/javascripts/pages/admin/geo/nodes/index.js
@@ -1,3 +1,9 @@
 import initGeoNodes from 'ee/geo_nodes';
+import PersistentUserCallout from '~/persistent_user_callout';
 
 document.addEventListener('DOMContentLoaded', initGeoNodes);
+document.addEventListener('DOMContentLoaded', () => {
+  const callout = document.querySelector('.user-callout');
+
+  if (callout) new PersistentUserCallout(callout); // eslint-disable-line no-new
+});
