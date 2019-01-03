18aa2fb66450b9390c344995af5f4284abae09b0
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2019-01-03 14:53:32 +0100

    Remove Rubocop override in cross-pipeline creation worker

--- ee/app/workers/ci/create_cross_project_pipeline_worker.rb
+++ ee/app/workers/ci/create_cross_project_pipeline_worker.rb
@@ -5,14 +5,12 @@ module Ci
     include ::ApplicationWorker
     include ::PipelineQueue
 
-    # rubocop:disable CodeReuse/ActiveRecord
     def perform(bridge_id)
-      ::Ci::Bridge.find_by(id: bridge_id).try do |bridge|
+      ::Ci::Bridge.find_by_id(bridge_id).try do |bridge|
         ::Ci::CreateCrossProjectPipelineService
           .new(bridge.project, bridge.user)
           .execute(bridge)
       end
     end
-    # rubocop:enable CodeReuse/ActiveRecord
   end
 end
