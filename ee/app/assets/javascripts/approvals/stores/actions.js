69d562a65df547118f5b41b5232852507bb9f976
Paul Slaughter <pslaughter@gitlab.com>
2019-01-05 00:54:58 -0600

    FE store for MR approvals settings
    **Note:**
    - This contains the service stub which is removed in a later commit
    - Adds modal modules which will ref components in a later commit

--- ee/app/assets/javascripts/approvals/stores/actions.js
+++ ee/app/assets/javascripts/approvals/stores/actions.js
@@ -2,6 +2,7 @@ import createFlash from '~/flash';
 import { __ } from '~/locale';
 import * as types from './mutation_types';
 import service from '../services/approvals_service_stub';
+import { mapApprovalRuleRequest, mapApprovalRulesResponse } from '../mappers';
 
 export const setSettings = ({ commit }, settings) => {
   commit(types.SET_SETTINGS, settings);
@@ -22,13 +23,49 @@ export const receiveRulesError = () => {
 
 export const fetchRules = ({ state, dispatch }) => {
   if (state.isLoading) {
-    return;
+    return Promise.resolve();
   }
 
   dispatch('requestRules');
 
-  service
+  return service
     .getProjectApprovalRules()
-    .then(response => dispatch('receiveRulesSuccess', response.data))
+    .then(response => dispatch('receiveRulesSuccess', mapApprovalRulesResponse(response.data)))
     .catch(() => dispatch('receiveRulesError'));
 };
+
+export const postRuleSuccess = ({ dispatch }) => {
+  dispatch('createModal/close');
+  dispatch('fetchRules');
+};
+
+export const postRuleError = () => {
+  createFlash(__('An error occurred while updating approvers'));
+};
+
+export const postRule = ({ dispatch }, rule) =>
+  service
+    .postProjectApprovalRule(mapApprovalRuleRequest(rule))
+    .then(() => dispatch('postRuleSuccess'))
+    .catch(() => dispatch('postRuleError'));
+
+export const putRule = ({ dispatch }, { id, ...newRule }) =>
+  service
+    .putProjectApprovalRule(id, mapApprovalRuleRequest(newRule))
+    .then(() => dispatch('postRuleSuccess'))
+    .catch(() => dispatch('postRuleError'));
+
+export const deleteRuleSuccess = ({ dispatch }) => {
+  dispatch('deleteModal/close');
+  dispatch('fetchRules');
+};
+
+export const deleteRuleError = () => {
+  createFlash(__('An error occurred while deleting the approvers group'));
+};
+
+export const deleteRule = ({ dispatch }, id) =>
+  service
+    .deleteProjectApprovalRule(id)
+    .then(() => dispatch('deleteRuleSuccess'))
+    .catch(() => dispatch('deleteRuleError'));
