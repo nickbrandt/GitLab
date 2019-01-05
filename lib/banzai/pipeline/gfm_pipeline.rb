71b88607d66fd0dfe6eef96c40e93a3fc573ac12
Brett Walker <bwalker@gitlab.com>
2019-01-04 20:54:02 -0600

    Properly process footnotes in markdown
    All the ids and classes were stripped.  Add them back in
    and make ids unique

--- lib/banzai/pipeline/gfm_pipeline.rb
+++ lib/banzai/pipeline/gfm_pipeline.rb
@@ -32,6 +32,7 @@ module Banzai
           Filter::AutolinkFilter,
           Filter::ExternalLinkFilter,
           Filter::SuggestionFilter,
+          Filter::FootnoteFilter,
 
           *reference_filters,
 
