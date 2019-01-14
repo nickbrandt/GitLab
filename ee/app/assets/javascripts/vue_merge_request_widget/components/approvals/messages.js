5eaea35c63202e6e287353aeee860e9df2665365
Paul Slaughter <pslaughter@gitlab.com>
2019-01-14 01:43:53 -0600

    Display approvals summary and action button

--- ee/app/assets/javascripts/vue_merge_request_widget/components/approvals/messages.js
+++ ee/app/assets/javascripts/vue_merge_request_widget/components/approvals/messages.js
@@ -4,3 +4,5 @@ export const FETCH_LOADING = __('Checking approval status');
 export const FETCH_ERROR = s__(
   'mrWidget|An error occurred while retrieving approval data for this merge request.',
 );
+export const APPROVE_ERROR = s__('mrWidget|An error occurred while submitting your approval.');
+export const UNAPPROVE_ERROR = s__('mrWidget|An error occurred while removing your approval.');
--- ee/app/assets/javascripts/vue_merge_request_widget/components/approvals/multiple_rule/approvals.vue
+++ ee/app/assets/javascripts/vue_merge_request_widget/components/approvals/multiple_rule/approvals.vue
@@ -1,14 +1,23 @@
 <script>
+import { GlButton, GlLoadingIcon } from '@gitlab/ui';
 import createFlash from '~/flash';
+import { s__ } from '~/locale';
+import UserAvatarList from '~/vue_shared/components/user_avatar/user_avatar_list.vue';
+import eventHub from '~/vue_merge_request_widget/event_hub';
 import MrWidgetContainer from '~/vue_merge_request_widget/components/mr_widget_container.vue';
 import MrWidgetIcon from '~/vue_merge_request_widget/components/mr_widget_icon.vue';
-import { FETCH_LOADING, FETCH_ERROR } from '../messages';
+import ApprovalsSummary from './approvals_summary.vue';
+import { FETCH_LOADING, FETCH_ERROR, APPROVE_ERROR, UNAPPROVE_ERROR } from '../messages';
 
 export default {
   name: 'MRWidgetMultipleRuleApprovals',
   components: {
+    UserAvatarList,
     MrWidgetContainer,
     MrWidgetIcon,
+    ApprovalsSummary,
+    GlButton,
+    GlLoadingIcon,
   },
   props: {
     mr: {
@@ -23,39 +32,45 @@ export default {
   data() {
     return {
       fetchingApprovals: true,
+      isApproving: false,
     };
   },
   computed: {
-    canApprove() {
-      return this.mr.isOpen;
+    approvedBy() {
+      return this.mr.approvals.approved_by.map(x => x.user);
     },
-    canUnapprove() {
-      return this.mr.state !== 'merged';
+    userHasApproved() {
+      return this.mr.approvals.user_has_approved;
+    },
+    userCanApprove() {
+      return this.mr.approvals.user_can_approve;
+    },
+    showApprove() {
+      return !this.userHasApproved && this.userCanApprove && this.mr.isOpen;
+    },
+    showUnapprove() {
+      return this.userHasApproved && !this.userCanApprove && this.mr.state !== 'merged';
     },
     action() {
-      if (!this.mr.userCanApprove) {
-        return null;
-      } else if (this.mr.userHasApproved && this.canUnapprove) {
+      if (this.showApprove && this.mr.approvals.approved) {
+        return {
+          text: s__('mrWidget|Approve additionally'),
+          variant: 'primary',
+          inverted: true,
+          action: () => this.approve(),
+        };
+      } else if (this.showApprove) {
+        return {
+          text: s__('mrWidget|Approve'),
+          variant: 'primary',
+          action: () => this.approve(),
+        };
+      } else if (this.showUnapprove) {
         return {
           text: s__('mrWidget|Revoke approval'),
           variant: 'warning',
           inverted: true,
-          action: () => {},
-        };
-      } else if (!this.mr.userHasApproved && this.canApprove) {
-        if (this.mr.approvals.approved) {
-          return {
-            text: s__('mrWidget|Approve additionally'),
-            variant: 'primary',
-            inverted: true,
-            action: () => {},
-          };
-        }
-
-        return {
-          text: s__('mrWidget|Approve'),
-          variant: 'primary',
-          action: () => {},
+          action: () => this.unapprove(),
         };
       }
 
@@ -71,6 +86,34 @@ export default {
       })
       .catch(() => createFlash(FETCH_ERROR));
   },
+  methods: {
+    approve() {
+      this.onApprovalRequest();
+      this.service
+        .approveMergeRequest()
+        .then(data => this.onApprovalSuccess(data))
+        .catch(() => this.onApprovalError(APPROVE_ERROR));
+    },
+    unapprove() {
+      this.onApprovalRequest();
+      this.service
+        .unapproveMergeRequest()
+        .then(data => this.onApprovalSuccess(data))
+        .catch(() => this.onApprovalError(UNAPPROVE_ERROR));
+    },
+    onApprovalRequest() {
+      this.isApproving = true;
+    },
+    onApprovalSuccess(data) {
+      this.mr.setApprovals(data);
+      eventHub.$emit('MRWidgetUpdateRequested');
+      this.isApproving = false;
+    },
+    onApprovalError(msg) {
+      createFlash(msg);
+      this.isApproving = false;
+    },
+  },
   FETCH_LOADING,
 };
 </script>
@@ -79,9 +122,25 @@ export default {
   <mr-widget-container>
     <div class="js-mr-approvals d-flex align-items-center">
       <mr-widget-icon name="approval" />
-      <div v-if="fetchingApprovals" class="media-body">
-        <span class="approvals-loading-text"> {{ $options.FETCH_LOADING }} </span>
-      </div>
+      <div v-if="fetchingApprovals">{{ $options.FETCH_LOADING }}</div>
+      <template v-else>
+        <gl-button
+          v-if="action"
+          :variant="action.variant"
+          :class="{ 'btn-inverted': action.inverted }"
+          size="sm"
+          class="mr-3"
+          @click="action.action"
+        >
+          <gl-loading-icon v-if="isApproving" inline />
+          {{ action.text }}
+        </gl-button>
+        <approvals-summary
+          :approvals-left="mr.approvals.approvals_left"
+          :rules-left="mr.approvals.approval_rules_left"
+          :approvers="approvedBy"
+        />
+      </template>
     </div>
   </mr-widget-container>
 </template>
