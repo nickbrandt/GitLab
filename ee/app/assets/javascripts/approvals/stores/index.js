869a6434c8cd21e6de41e9265a195588f095cfc1
Paul Slaughter <pslaughter@gitlab.com>
2019-01-14 13:34:59 -0600

    Update approvals store with getters

--- ee/app/assets/javascripts/approvals/stores/index.js
+++ ee/app/assets/javascripts/approvals/stores/index.js
@@ -3,12 +3,14 @@ import modalModule from '~/vuex_shared/modules/modal';
 import state from './state';
 import mutations from './mutations';
 import * as actions from './actions';
+import * as getters from './getters';
 
 export default () =>
   new Vuex.Store({
     state: state(),
     mutations,
     actions,
+    getters,
     modules: {
       createModal: modalModule(),
       deleteModal: modalModule(),
