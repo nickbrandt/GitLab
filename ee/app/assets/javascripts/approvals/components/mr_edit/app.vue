82d41f1ece24eecad7fc9e4ef0a229eec29f2bd7
Paul Slaughter <pslaughter@gitlab.com>
2019-01-16 16:01:18 -0600

    Create components for mr_edit approvals
    **Note:**
    - These components take advantage of the base app
       and rules components.
    - It includes a hidden inputs component which maps
       the store state to form inputs which will be
       submitted by the overall MR edit form.

---
+++ ee/app/assets/javascripts/approvals/components/mr_edit/app.vue
@@ -0,0 +1,20 @@
+<script>
+import App from '../app.vue';
+import MrRules from './mr_rules.vue';
+import MrRulesHiddenInputs from './mr_rules_hidden_inputs.vue';
+
+export default {
+  components: {
+    App,
+    MrRules,
+    MrRulesHiddenInputs,
+  },
+};
+</script>
+
+<template>
+  <app>
+    <mr-rules slot="rules" />
+    <mr-rules-hidden-inputs slot="footer" />
+  </app>
+</template>
