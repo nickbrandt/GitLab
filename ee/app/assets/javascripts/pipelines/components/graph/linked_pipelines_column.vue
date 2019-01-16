a0cfd40486333561ae1ebeba934395a4f658fea4
Filipa Lacerda <filipa@gitlab.com>
2019-01-16 17:24:17 +0000

    Updates changelog entry

--- ee/app/assets/javascripts/pipelines/components/graph/linked_pipelines_column.vue
+++ ee/app/assets/javascripts/pipelines/components/graph/linked_pipelines_column.vue
@@ -40,11 +40,7 @@ export default {
           active: pipeline.isExpanded,
           'left-connector': pipeline.isExpanded && graphPosition === 'left',
         }"
-        :pipeline-id="pipeline.id"
-        :project-name="pipeline.project.name"
-        :pipeline-status="pipeline.details.status"
-        :pipeline-path="pipeline.path"
-        :is-loading="pipeline.isLoading"
+        :pipeline="pipeline"
         @pipelineClicked="$emit('linkedPipelineClick', pipeline, index)"
       />
     </ul>
