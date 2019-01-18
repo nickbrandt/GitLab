ee229401f39c50f5930800274e7c09d7414a237e
Felipe Artur <fcardozo@gitlab.com>
2019-01-18 14:01:39 +0000

    Add notes filters for epics

--- ee/app/models/ee/user_preference.rb
+++ ee/app/models/ee/user_preference.rb
@@ -8,6 +8,8 @@ module EE
       validates :roadmap_epics_state, allow_nil: true, inclusion: {
         in: ::Epic.states.values, message: "%{value} is not a valid epic state"
       }
+
+      validates :epic_notes_filter, inclusion: { in: ::UserPreference::NOTES_FILTERS.values }, presence: true
     end
   end
 end
