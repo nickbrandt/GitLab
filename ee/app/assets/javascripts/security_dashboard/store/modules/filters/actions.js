7b56662ddc8928412729d6d74afbcda794534425
Sam Beckham <sbeckham@gitlab.com>
2019-01-21 12:32:26 +0000

    Adds snowplough tracking to the GSD filters

--- ee/app/assets/javascripts/security_dashboard/store/modules/filters/actions.js
+++ ee/app/assets/javascripts/security_dashboard/store/modules/filters/actions.js
@@ -1,7 +1,13 @@
+import Stats from 'ee/stats';
 import * as types from './mutation_types';
 
 export const setFilter = ({ commit }, payload) => {
   commit(types.SET_FILTER, payload);
+
+  Stats.trackEvent(document.body.dataset.page, 'set_filter', {
+    label: payload.filterId,
+    value: payload.optionId,
+  });
 };
 
 export const setFilterOptions = ({ commit }, payload) => {
