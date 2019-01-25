76671ce7d40673e03ec94a1a5b348adfead66fab
Yorick Peterse <yorickpeterse@gmail.com>
2019-01-25 16:12:52 +0000

    Merge branch 'security-fix-protected-branches-creation-access-rights-v3-11-5' into 'security-11-5-ee'
    [11.5] Check access rights when creating/updating ProtectedRefs
    
    See merge request gitlab/gitlab-ee!812
    
    (cherry picked from commit fc7d940eeb74c4d6f1979636e6ef15af591b11c8)
    
    5d78f7b5 Check access rights when creating/updating ProtectedRefs

--- ee/app/services/ee/protected_branches/access_level_params.rb
+++ ee/app/services/ee/protected_branches/access_level_params.rb
@@ -8,14 +8,6 @@ module EE
         ce_style_access_level + ee_style_access_levels
       end
 
-      def group_ids
-        ids_for('group_id')
-      end
-
-      def user_ids
-        ids_for('user_id')
-      end
-
       private
 
       override :use_default_access_level?
@@ -25,10 +17,6 @@ module EE
 
       def ee_style_access_levels
         params[:"allowed_to_#{type}"] || []
-      end
-
-      def ids_for(key)
-        ee_style_access_levels.select { |level| level[key] }.flat_map(&:values)
       end
     end
   end
