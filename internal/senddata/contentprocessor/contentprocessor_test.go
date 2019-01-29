39dd7332c8e7a24ddd3d2e17446b5b06b983688d
Francisco Javier López <fjlopez@gitlab.com>
2019-01-29 12:36:27 +0100

    Fixed svg recognition to get the proper content type

--- internal/senddata/contentprocessor/contentprocessor_test.go
+++ internal/senddata/contentprocessor/contentprocessor_test.go
@@ -74,6 +74,12 @@ func TestSetProperContentTypeAndDisposition(t *testing.T) {
 			body:               testhelper.LoadFile(t, "testdata/image.svg"),
 		},
 		{
+			desc:               "Partial SVG type",
+			contentType:        "image/svg+xml",
+			contentDisposition: "attachment",
+			body:               "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" viewBox=\"0 0 330 82\"><title>SVG logo combined with the W3C logo, set horizontally</title><desc>The logo combines three entities displayed horizontall</desc><metadata>",
+		},
+		{
 			desc:               "Application type",
 			contentType:        "application/pdf",
 			contentDisposition: "attachment",
