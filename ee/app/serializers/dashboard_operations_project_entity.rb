94b8f2837c10b624349868ecd4c358780cbae4bf
Matija Čupić <matteeyah@gmail.com>
2019-02-01 17:15:00 +0100

    Expose upstream/downstream pipelines

--- ee/app/serializers/dashboard_operations_project_entity.rb
+++ ee/app/serializers/dashboard_operations_project_entity.rb
@@ -10,20 +10,18 @@ class DashboardOperationsProjectEntity < Grape::Entity
   end
 
   expose :last_pipeline, if: -> (*) { last_pipeline } do |dashboard_project, options|
-    new_request = EntityRequest.new(
-      current_user: request.current_user,
-      project: dashboard_project.project
-    )
-
     PipelineEntity.represent(last_pipeline, options.merge(request: new_request))
   end
 
-  expose :last_deployment, if: -> (*) { last_deployment? } do |dashboard_project, options|
-    new_request = EntityRequest.new(
-      current_user: request.current_user,
-      project: dashboard_project.project
-    )
+  expose :upstream_pipeline, if: -> (*) { upstream_pipeline } do |dashboard_project, options|
+    PipelineEntity.represent(upstream_pipeline, options.merge(request: new_request))
+  end
 
+  expose :downstream_pipelines, if: -> (*) { downstream_pipelines } do |dashboard_project, options|
+    PipelineEntity.represent(downstream_pipelines, options.merge(request: new_request))
+  end
+
+  expose :last_deployment, if: -> (*) { last_deployment? } do |dashboard_project, options|
     DeploymentEntity.represent(dashboard_project.last_deployment,
                                options.merge(request: new_request))
   end
@@ -41,8 +39,23 @@ class DashboardOperationsProjectEntity < Grape::Entity
 
   alias_method :dashboard_project, :object
 
+  def new_request
+    EntityRequest.new(
+      current_user: request.current_user,
+      project: dashboard_project.project
+    )
+  end
+
+  def upstream_pipeline
+    last_pipeline&.source_pipeline&.source_pipeline
+  end
+
+  def downstream_pipelines
+    last_pipeline&.sourced_pipelines&.map(&:source_pipeline)
+  end
+
   def last_pipeline
-    dashboard_project.project.commit.last_pipeline
+    dashboard_project.project.commit&.last_pipeline
   end
 
   def last_deployment?
