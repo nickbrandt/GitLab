323e64eeb15c757ce1d4865032dcb5f38ba29d20
James Lopez <james@jameslopez.es>
2019-02-08 16:42:00 +0100

    Update spec and fix Danger

--- ee/spec/requests/api/scim_spec.rb
+++ ee/spec/requests/api/scim_spec.rb
@@ -3,18 +3,18 @@
 require 'spec_helper'
 
 describe API::Scim do
+  let(:user) { create(:user) }
+  let(:identity) { create(:group_saml_identity, user: user) }
+  let(:group) { identity.saml_provider.group }
+  let(:token) { create(:personal_access_token, user: user) }
+
+  before do
+    stub_licensed_features(group_saml: true)
+
+    group.add_owner(user)
+  end
+
   describe 'GET api/scim/v2/groups/:group/Users' do
-    let(:user) { create(:user) }
-    let(:identity) { create(:group_saml_identity, user: user) }
-    let(:group) { identity.saml_provider.group }
-    let(:token) { create(:personal_access_token, user: user) }
-
-    before do
-      stub_licensed_features(group_saml: true)
-
-      group.add_owner(user)
-    end
-
     it 'responds with a 200' do
       get api("scim/v2/groups/#{group.full_path}/Users", user, oauth_access_token: token, version: '')
 
