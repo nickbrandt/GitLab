7337810faaaa7f15bad65306ebba6d68a97e0252
Mark Lapierre <mlapierre@gitlab.com>
2019-02-14 12:00:25 -0500

    Move server responding check to scenario before hook
    This checks that the server is responding before any other actions,
    including before trying to add a license.

--- qa/qa/ee/strategy.rb
+++ qa/qa/ee/strategy.rb
@@ -11,6 +11,10 @@ module QA
         return unless ENV['EE_LICENSE']
 
         QA::Runtime::Browser.visit(:gitlab, QA::Page::Main::Login) do
+          # The login page could take some time to load the first time it is visited.
+          # We visit the login page and wait for it to properly load only once before the tests.
+          QA::Page::Main::Login.perform(&:assert_page_loaded)
+
           EE::Resource::License.fabricate!(ENV['EE_LICENSE'])
         end
       end
