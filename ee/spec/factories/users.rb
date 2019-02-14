5a3a20e82a9dd84a502401449a87cea28eb74f13
Rémy Coutable <remy@rymai.me>
2019-02-14 18:55:55 +0100

    Extract EE-specific code from spec/factories/users.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

---
+++ ee/spec/factories/users.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+FactoryBot.modify do
+  factory :user do
+    trait :auditor do
+      auditor true
+    end
+  end
+
+  factory :omniauth_user do
+    transient do
+      saml_provider nil
+    end
+  end
+end
+
+FactoryBot.define do
+  factory :auditor, parent: :user, traits: [:auditor]
+  factory :external_user, parent: :user, traits: [:external]
+end
