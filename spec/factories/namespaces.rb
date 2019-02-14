4721cfe875a4309ed7bac5ff257b18437aabacc4
Rémy Coutable <remy@rymai.me>
2019-02-14 18:54:30 +0100

    Extract EE-specific code from spec/factories/namespaces.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- spec/factories/namespaces.rb
+++ spec/factories/namespaces.rb
@@ -19,34 +19,5 @@ FactoryBot.define do
         owner.namespace = namespace
       end
     end
-
-    trait :with_build_minutes do
-      namespace_statistics factory: :namespace_statistics, shared_runners_seconds: 400.minutes.to_i
-    end
-
-    trait :with_build_minutes_limit do
-      shared_runners_minutes_limit 500
-    end
-
-    trait :with_not_used_build_minutes_limit do
-      namespace_statistics factory: :namespace_statistics, shared_runners_seconds: 300.minutes.to_i
-      shared_runners_minutes_limit 500
-    end
-
-    trait :with_used_build_minutes_limit do
-      namespace_statistics factory: :namespace_statistics, shared_runners_seconds: 1000.minutes.to_i
-      shared_runners_minutes_limit 500
-    end
-
-    # EE-only
-    transient do
-      plan nil
-    end
-
-    before :create do |namespace, evaluator|
-      if evaluator.plan.present?
-        namespace.plan = create(evaluator.plan)
-      end
-    end
   end
 end
