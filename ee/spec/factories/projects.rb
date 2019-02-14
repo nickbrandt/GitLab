1abf34cb3411259381a96a5cb20c8477bcb95114
Rémy Coutable <remy@rymai.me>
2019-02-14 18:54:50 +0100

    Extract EE-specific code from spec/factories/projects.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

---
+++ ee/spec/factories/projects.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+FactoryBot.modify do
+  factory :project do
+    transient do
+      last_update_at nil
+      last_successful_update_at nil
+      retry_count 0
+    end
+
+    after(:create) do |project, evaluator|
+      import_state = project.import_state
+      if import_state
+        import_state.last_successful_update_at = evaluator.last_successful_update_at
+        import_state.retry_count = evaluator.retry_count
+
+        case import_state.status.to_sym
+        when :scheduled
+          import_state.last_update_scheduled_at = Time.now
+        when :started
+          import_state.last_update_started_at = Time.now
+        when :finished
+          timestamp = evaluator.last_update_at || Time.now
+          import_state.last_update_at = timestamp
+          import_state.last_successful_update_at = timestamp
+        when :failed
+          import_state.last_update_at = evaluator.last_update_at || Time.now
+        end
+        import_state.save!
+      end
+    end
+
+    trait :import_none do
+      import_status :none
+    end
+
+    trait :import_hard_failed do
+      import_status :failed
+      last_update_at { Time.now - 1.minute }
+      retry_count { Gitlab::Mirror::MAX_RETRY + 1 }
+    end
+
+    trait :disabled_mirror do
+      mirror false
+      import_url { generate(:url) }
+      mirror_user_id { creator_id }
+    end
+
+    trait :mirror do
+      mirror true
+      import_url { generate(:url) }
+      mirror_user_id { creator_id }
+    end
+
+    trait :random_last_repository_updated_at do
+      last_repository_updated_at { rand(1.year).seconds.ago }
+    end
+  end
+end
