nickbrandt/GitLab

commit 0a8db3dc2f15196d91e06e4d3fede92d7679716e

Refactor Gitlab::Checks::ChangeAccess and EE::Gitlab::Checks::ChangeAccess
This class handles the validations of git pushes. It is
quite big and all the logic is inside one file. This commit
refactors the code and split it into different files.

ee/lib/ee/gitlab/checks/diff_check.rb
@@ -1,0 +1,67 @@
+# frozen_string_literal: true
+
+module EE
+  module Gitlab
+    module Checks
+      module DiffCheck
+        extend ActiveSupport::Concern
+        extend ::Gitlab::Utils::Override
+
+        private
+
+        override :should_run_diff_validations?
+        def should_run_diff_validations?
+          super || validate_path_locks? || push_rule_checks_commit?
+        end
+
+        def validate_path_locks?
+          strong_memoize(:validate_path_locks) do
+            project.feature_available?(:file_locks) &&
+              project.any_path_locks? &&
+              project.default_branch == branch_name # locks protect default branch only
+          end
+        end
+
+        def push_rule_checks_commit?
+          return false unless push_rule
+
+          push_rule.file_name_regex.present? || push_rule.prevent_secrets
+        end
+
+        override :validations_for_diff
+        def validations_for_diff
+          super.tap do |validations|
+            validations.push(path_locks_validation) if validate_path_locks?
+            validations.push(file_name_validation) if push_rule
+          end
+        end
+
+        def path_locks_validation
+          lambda do |diff|
+            path = diff.new_path || diff.old_path
+
+            lock_info = project.find_path_lock(path)
+
+            if lock_info && lock_info.user != user_access.user
+              return "The path '#{lock_info.path}' is locked by #{lock_info.user.name}"
+            end
+          end
+        end
+
+        def file_name_validation
+          lambda do |diff|
+            begin
+              if (diff.renamed_file || diff.new_file) && blacklisted_regex = push_rule.filename_blacklisted?(diff.new_path)
+                return nil unless blacklisted_regex.present?
+
+                "File name #{diff.new_path} was blacklisted by the pattern #{blacklisted_regex}."
+              end
+            rescue ::PushRule::MatchError => e
+              raise ::Gitlab::GitAccess::UnauthorizedError, e.message
+            end
+          end
+        end
+      end
+    end
+  end
+end

ee/spec/lib/gitlab/checks/diff_check_spec.rb
@@ -1,0 +1,95 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Gitlab::Checks::DiffCheck do
+  include_context 'push rules checks context'
+
+  describe '#validate!' do
+    context 'file name rules' do
+      # Notice that the commit used creates a file named 'README'
+      context 'file name regex check' do
+        let!(:push_rule) { create(:push_rule, file_name_regex: 'READ*') }
+
+        it_behaves_like 'check ignored when push rule unlicensed'
+
+        it "returns an error if a new or renamed filed doesn't match the file name regex" do
+          expect { subject.validate! }.to raise_error(Gitlab::GitAccess::UnauthorizedError, "File name README was blacklisted by the pattern READ*.")
+        end
+
+        it 'returns an error if the regex is invalid' do
+          push_rule.file_name_regex = '+'
+
+          expect { subject.validate! }.to raise_error(Gitlab::GitAccess::UnauthorizedError, /\ARegular expression '\+' is invalid/)
+        end
+      end
+
+      context 'blacklisted files check' do
+        let(:push_rule) { create(:push_rule, prevent_secrets: true) }
+
+        it_behaves_like 'check ignored when push rule unlicensed'
+
+        it "returns true if there is no blacklisted files" do
+          new_rev = nil
+
+          white_listed =
+            [
+              'readme.txt', 'any/ida_rsa.pub', 'any/id_dsa.pub', 'any_2/id_ed25519.pub',
+              'random_file.pdf', 'folder/id_ecdsa.pub', 'docs/aws/credentials.md', 'ending_withhistory'
+            ]
+
+          white_listed.each do |file_path|
+            old_rev = 'be93687618e4b132087f430a4d8fc3a609c9b77c'
+            old_rev = new_rev if new_rev
+            new_rev = project.repository.create_file(user, file_path, "commit #{file_path}", message: "commit #{file_path}", branch_name: "master")
+
+            allow(project.repository).to receive(:new_commits).and_return(
+              project.repository.commits_between(old_rev, new_rev)
+            )
+
+            expect(subject.validate!).to be_truthy
+          end
+        end
+
+        it "returns an error if a new or renamed filed doesn't match the file name regex" do
+          new_rev = nil
+
+          black_listed =
+            [
+              'aws/credentials', '.ssh/personal_rsa', 'config/server_rsa', '.ssh/id_rsa', '.ssh/id_dsa',
+              '.ssh/personal_dsa', 'config/server_ed25519', 'any/id_ed25519', '.ssh/personal_ecdsa', 'config/server_ecdsa',
+              'any_place/id_ecdsa', 'some_pLace/file.key', 'other_PlAcE/other_file.pem', 'bye_bug.history', 'pg_sql_history'
+            ]
+
+          black_listed.each do |file_path|
+            old_rev = 'be93687618e4b132087f430a4d8fc3a609c9b77c'
+            old_rev = new_rev if new_rev
+            new_rev = project.repository.create_file(user, file_path, "commit #{file_path}", message: "commit #{file_path}", branch_name: "master")
+
+            allow(subject).to receive(:commits).and_return(
+              project.repository.commits_between(old_rev, new_rev)
+            )
+
+            expect { subject.validate! }.to raise_error(Gitlab::GitAccess::UnauthorizedError, /File name #{file_path} was blacklisted by the pattern/)
+          end
+        end
+      end
+    end
+
+    context 'file lock rules' do
+      let(:project) { create(:project, :repository) }
+
+      it 'returns an error if the changes update a path locked by another user' do
+        path_lock = create(:path_lock, path: 'README', project: project)
+
+        expect { subject.validate! }.to raise_error(Gitlab::GitAccess::UnauthorizedError, "The path 'README' is locked by #{path_lock.user.name}")
+      end
+
+      it 'memoizes the validate_path_locks? call' do
+        expect(project).to receive(:any_path_locks?).once.and_call_original
+
+        2.times { subject.validate! }
+      end
+    end
+  end
+end

lib/gitlab/checks/diff_check.rb
@@ -1,0 +1,100 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module Checks
+    class DiffCheck < BaseChecker
+      include Gitlab::Utils::StrongMemoize
+      prepend EE::Gitlab::Checks::DiffCheck
+
+      LOG_MESSAGES = {
+        validate_file_paths: "Validating diffs' file paths...",
+        diff_content_check: "Validating diff contents..."
+      }.freeze
+
+      def validate!
+        return unless should_run_diff_validations?
+        return if commits.empty?
+        return unless uses_raw_delta_validations?
+
+        file_paths = []
+        process_raw_deltas do |diff|
+          file_paths << (diff.new_path || diff.old_path)
+
+          validate_diff(diff)
+        end
+
+        validate_file_paths(file_paths)
+      end
+
+      private
+
+      def should_run_diff_validations?
+        newrev && oldrev && !deletion? && validate_lfs_file_locks?
+      end
+
+      def validate_lfs_file_locks?
+        strong_memoize(:validate_lfs_file_locks) do
+          project.lfs_enabled? && project.any_lfs_file_locks?
+        end
+      end
+
+      def uses_raw_delta_validations?
+        validations_for_diff.present? || path_validations.present?
+      end
+
+      def validate_diff(diff)
+        validations_for_diff.each do |validation|
+          if error = validation.call(diff)
+            raise ::Gitlab::GitAccess::UnauthorizedError, error
+          end
+        end
+      end
+
+      # Method overwritten in EE to inject custom validations
+      def validations_for_diff
+        []
+      end
+
+      def path_validations
+        validate_lfs_file_locks? ? [lfs_file_locks_validation] : []
+      end
+
+      def process_raw_deltas
+        logger.log_timed(LOG_MESSAGES[:diff_content_check]) do
+          # n+1: https://gitlab.com/gitlab-org/gitlab-ee/issues/3593
+          ::Gitlab::GitalyClient.allow_n_plus_1_calls do
+            commits.each do |commit|
+              logger.check_timeout_reached
+
+              commit.raw_deltas.each do |diff|
+                yield(diff)
+              end
+            end
+          end
+        end
+      end
+
+      def validate_file_paths(file_paths)
+        logger.log_timed(LOG_MESSAGES[__method__]) do
+          path_validations.each do |validation|
+            if error = validation.call(file_paths)
+              raise ::Gitlab::GitAccess::UnauthorizedError, error
+            end
+          end
+        end
+      end
+
+      # rubocop: disable CodeReuse/ActiveRecord
+      def lfs_file_locks_validation
+        lambda do |paths|
+          lfs_lock = project.lfs_file_locks.where(path: paths).where.not(user_id: user_access.user.id).take
+
+          if lfs_lock
+            return "The path '#{lfs_lock.path}' is locked in Git LFS by #{lfs_lock.user.name}"
+          end
+        end
+      end
+      # rubocop: enable CodeReuse/ActiveRecord
+    end
+  end
+end

spec/lib/gitlab/checks/diff_check_spec.rb
@@ -1,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Gitlab::Checks::DiffCheck do
+  include_context 'change access checks context'
+
+  describe '#validate!' do
+    let(:owner) { create(:user) }
+    let!(:lock) { create(:lfs_file_lock, user: owner, project: project, path: 'README') }
+
+    before do
+      allow(project.repository).to receive(:new_commits).and_return(
+        project.repository.commits_between('be93687618e4b132087f430a4d8fc3a609c9b77c', '54fcc214b94e78d7a41a9a8fe6d87a5e59500e51')
+      )
+    end
+
+    context 'with LFS not enabled' do
+      before do
+        allow(project).to receive(:lfs_enabled?).and_return(false)
+      end
+
+      it 'skips the validation' do
+        expect(subject).not_to receive(:validate_diff)
+        expect(subject).not_to receive(:validate_file_paths)
+
+        subject.validate!
+      end
+    end
+
+    context 'with LFS enabled' do
+      before do
+        allow(project).to receive(:lfs_enabled?).and_return(true)
+      end
+
+      context 'when change is sent by a different user' do
+        it 'raises an error if the user is not allowed to update the file' do
+          expect { subject.validate! }.to raise_error(Gitlab::GitAccess::UnauthorizedError, "The path 'README' is locked in Git LFS by #{lock.user.name}")
+        end
+      end
+
+      context 'when change is sent by the author of the lock' do
+        let(:user) { owner }
+
+        it "doesn't raise any error" do
+          expect { subject.validate! }.not_to raise_error
+        end
+      end
+    end
+  end
+end

ee/lib/ee/gitlab/checks/base_checker.rb
@@ -1,0 +1,20 @@
+# frozen_string_literal: true
+
+module EE
+  module Gitlab
+    module Checks
+      module BaseChecker
+        extend ActiveSupport::Concern
+        include ::Gitlab::Utils::StrongMemoize
+
+        private
+
+        def push_rule
+          strong_memoize(:push_rule) do
+            project.push_rule
+          end
+        end
+      end
+    end
+  end
+end

ee/lib/ee/gitlab/checks/change_access.rb
@@ -1,243 +1,17 @@
+# frozen_string_literal: true
+
 module EE
   module Gitlab
     module Checks
       module ChangeAccess
         extend ActiveSupport::Concern
         extend ::Gitlab::Utils::Override
-        include PathLocksHelper
-        include ::Gitlab::Utils::StrongMemoize
 
-        ERROR_MESSAGES = {
-          push_rule_branch_name: "Branch name does not follow the pattern '%{branch_name_regex}'",
-          push_rule_committer_not_verified: "Committer email '%{committer_email}' is not verified.",
-          push_rule_committer_not_allowed: "You cannot push commits for '%{committer_email}'. You can only push commits that were committed with one of your own verified emails."
-        }.freeze
+        override :ref_level_checks
+        def ref_level_checks
+          super
 
-        LOG_MESSAGES = {
-          push_rule_tag_check: "Checking if you are allowed to delete a tag...",
-          push_rule_branch_check: "Checking if branch follows the naming patterns defined by the project...",
-          push_rule_commits_check: "Checking if commits follow defined push rules...",
-          file_size_check: "Checking if any files are larger than the allowed size..."
-        }.freeze
-
-        override :exec
-        def exec
-          return true if skip_authorization
-
-          super(skip_commits_check: true)
-
-          push_rule_check
-          file_size_check
-          # Check of commits should happen as the last step
-          # given they're expensive in terms of performance
-          commits_check
-
-          true
-        end
-
-        private
-
-        def file_size_check
-          return if push_rule.nil? || push_rule.max_file_size.zero?
-
-          logger.log_timed(LOG_MESSAGES[__method__]) do
-            max_file_size = push_rule.max_file_size
-            blobs = project.repository.new_blobs(newrev, dynamic_timeout: logger.time_left)
-
-            large_blob = blobs.find do |blob|
-              ::Gitlab::Utils.bytes_to_megabytes(blob.size) > max_file_size
-            end
-
-            if large_blob
-              raise ::Gitlab::GitAccess::UnauthorizedError, %Q{File "#{large_blob.path}" is larger than the allowed size of #{max_file_size} MB}
-            end
-          end
-        end
-
-        def push_rule
-          project.push_rule
-        end
-
-        def push_rule_check
-          return unless newrev && oldrev && project.feature_available?(:push_rules)
-
-          if tag_name
-            push_rule_tag_check
-          else
-            push_rule_branch_check
-          end
-        end
-
-        def push_rule_tag_check
-          logger.log_timed(LOG_MESSAGES[__method__]) do
-            if tag_deletion_denied_by_push_rule?
-              raise ::Gitlab::GitAccess::UnauthorizedError, 'You cannot delete a tag'
-            end
-          end
-        end
-
-        def push_rule_branch_check
-          logger.log_timed(LOG_MESSAGES[__method__]) do
-            unless branch_name_allowed_by_push_rule?
-              message = ERROR_MESSAGES[:push_rule_branch_name] % { branch_name_regex: push_rule.branch_name_regex }
-              raise ::Gitlab::GitAccess::UnauthorizedError.new(message)
-            end
-          end
-
-          commit_validation = push_rule.try(:commit_validation?)
-          # if newrev is blank, the branch was deleted
-          return if deletion? || !commit_validation
-
-          logger.log_timed(LOG_MESSAGES[:push_rule_commits_check]) do
-            commits.each do |commit|
-              logger.check_timeout_reached
-
-              push_rule_commit_check(commit)
-            end
-          end
-        rescue ::PushRule::MatchError => e
-          raise ::Gitlab::GitAccess::UnauthorizedError, e.message
-        end
-
-        def branch_name_allowed_by_push_rule?
-          return true if skip_branch_name_push_rule?
-
-          push_rule.branch_name_allowed?(branch_name)
-        end
-
-        def skip_branch_name_push_rule?
-          push_rule.nil? ||
-            deletion? ||
-            branch_name.blank? ||
-            branch_name == project.default_branch
-        end
-
-        def tag_deletion_denied_by_push_rule?
-          push_rule.try(:deny_delete_tag) &&
-            !updated_from_web? &&
-            deletion? &&
-            tag_exists?
-        end
-
-        def push_rule_commit_check(commit)
-          if push_rule.try(:commit_validation?)
-            error = check_commit(commit)
-            raise ::Gitlab::GitAccess::UnauthorizedError, error if error
-          end
-        end
-
-        # If commit does not pass push rule validation the whole push should be rejected.
-        # This method should return nil if no error found or a string if error.
-        # In case of errors - all other checks will be canceled and push will be rejected.
-        def check_commit(commit)
-          unless push_rule.commit_message_allowed?(commit.safe_message)
-            return "Commit message does not follow the pattern '#{push_rule.commit_message_regex}'"
-          end
-
-          if push_rule.commit_message_blocked?(commit.safe_message)
-            return "Commit message contains the forbidden pattern '#{push_rule.commit_message_negative_regex}'"
-          end
-
-          unless push_rule.author_email_allowed?(commit.committer_email)
-            return "Committer's email '#{commit.committer_email}' does not follow the pattern '#{push_rule.author_email_regex}'"
-          end
-
-          unless push_rule.author_email_allowed?(commit.author_email)
-            return "Author's email '#{commit.author_email}' does not follow the pattern '#{push_rule.author_email_regex}'"
-          end
-
-          committer_error_message = committer_check(commit)
-          return committer_error_message if committer_error_message
-
-          if !updated_from_web? && !push_rule.commit_signature_allowed?(commit)
-            return "Commit must be signed with a GPG key"
-          end
-
-          # Check whether author is a GitLab member
-          if push_rule.member_check
-            unless ::User.find_by_any_email(commit.author_email).present?
-              return "Author '#{commit.author_email}' is not a member of team"
-            end
-
-            if commit.author_email.casecmp(commit.committer_email) == -1
-              unless ::User.find_by_any_email(commit.committer_email).present?
-                return "Committer '#{commit.committer_email}' is not a member of team"
-              end
-            end
-          end
-
-          nil
-        end
-
-        def committer_check(commit)
-          unless push_rule.committer_allowed?(commit.committer_email, user_access.user)
-            committer_is_current_user = commit.committer == user_access.user
-
-            if committer_is_current_user && !commit.committer.verified_email?(commit.committer_email)
-              ERROR_MESSAGES[:push_rule_committer_not_verified] % { committer_email: commit.committer_email }
-            else
-              ERROR_MESSAGES[:push_rule_committer_not_allowed] % { committer_email: commit.committer_email }
-            end
-          end
-        end
-
-        override :should_run_commit_validations?
-        def should_run_commit_validations?
-          super || validate_path_locks? || push_rule_checks_commit?
-        end
-
-        def push_rule_checks_commit?
-          return false unless push_rule
-
-          push_rule.file_name_regex.present? || push_rule.prevent_secrets
-        end
-
-        override :validations_for_commit
-        def validations_for_commit(commit)
-          validations = super
-
-          validations.push(path_locks_validation) if validate_path_locks?
-          validations.concat(push_rule_commit_validations(commit))
-        end
-
-        def push_rule_commit_validations(commit)
-          return [] unless push_rule
-
-          [file_name_validation]
-        end
-
-        def validate_path_locks?
-          strong_memoize(:validate_path_locks) do
-            project.feature_available?(:file_locks) &&
-              newrev && oldrev && project.any_path_locks? &&
-              project.default_branch == branch_name # locks protect default branch only
-          end
-        end
-
-        def path_locks_validation
-          lambda do |diff|
-            path = diff.new_path || diff.old_path
-
-            lock_info = project.find_path_lock(path)
-
-            if lock_info && lock_info.user != user_access.user
-              return "The path '#{lock_info.path}' is locked by #{lock_info.user.name}"
-            end
-          end
-        end
-
-        def file_name_validation
-          lambda do |diff|
-            begin
-              if (diff.renamed_file || diff.new_file) && blacklisted_regex = push_rule.filename_blacklisted?(diff.new_path)
-                return nil unless blacklisted_regex.present?
-
-                "File name #{diff.new_path} was blacklisted by the pattern #{blacklisted_regex}."
-              end
-            rescue ::PushRule::MatchError => e
-              raise ::Gitlab::GitAccess::UnauthorizedError, e.message
-            end
-          end
+          PushRuleCheck.new(self).validate!
         end
       end
     end

ee/lib/ee/gitlab/checks/push_rule_check.rb
@@ -1,0 +1,21 @@
+# frozen_string_literal: true
+
+module EE
+  module Gitlab
+    module Checks
+      class PushRuleCheck < ::Gitlab::Checks::BaseChecker
+        def validate!
+          return unless push_rule
+
+          if tag_name
+            PushRules::TagCheck.new(change_access).validate!
+          else
+            PushRules::BranchCheck.new(change_access).validate!
+          end
+
+          PushRules::FileSizeCheck.new(change_access).validate!
+        end
+      end
+    end
+  end
+end

ee/lib/ee/gitlab/checks/push_rules/branch_check.rb
@@ -1,0 +1,43 @@
+# frozen_string_literal: true
+
+module EE
+  module Gitlab
+    module Checks
+      module PushRules
+        class BranchCheck < ::Gitlab::Checks::BaseChecker
+          ERROR_MESSAGE = "Branch name does not follow the pattern '%{branch_name_regex}'".freeze
+          LOG_MESSAGE = "Checking if branch follows the naming patterns defined by the project...".freeze
+
+          def validate!
+            return unless newrev && oldrev && push_rule
+
+            logger.log_timed(LOG_MESSAGE) do
+              unless branch_name_allowed_by_push_rule?
+                message = ERROR_MESSAGE % { branch_name_regex: push_rule.branch_name_regex }
+                raise ::Gitlab::GitAccess::UnauthorizedError.new(message)
+              end
+            end
+
+            PushRules::CommitCheck.new(change_access).validate!
+          rescue ::PushRule::MatchError => e
+            raise ::Gitlab::GitAccess::UnauthorizedError, e.message
+          end
+
+          private
+
+          def branch_name_allowed_by_push_rule?
+            return true if skip_branch_name_push_rule?
+
+            push_rule.branch_name_allowed?(branch_name)
+          end
+
+          def skip_branch_name_push_rule?
+            deletion? ||
+              branch_name.blank? ||
+              branch_name == project.default_branch
+          end
+        end
+      end
+    end
+  end
+end

commit 7fad50ba479cf0292561c1c6247171ade28ba025

Disable commit checks when no push rules are active
The refactoring in 0a8db3dc2f15196d91e06e4d3fede92d7679716e
inadvertently caused commit checks to become active regardless of
whether any rules were actually active. This significantly slowed pushes
and caused repositories with large number of commits to time out.

We fix this by using `push_rule_checks_commit?` since the existence of a
`PushRule` is not sufficient to determine whether any rules are
active. This also eliminates the need to override
`should_run_diff_validations?`, since the set of validations will only
contain the active ones.

Closes https://gitlab.com/gitlab-org/gitlab-ce/issues/57067

ee/lib/ee/gitlab/checks/diff_check.rb
@@ -9,11 +9,6 @@ module EE
 
         private
 
-        override :should_run_diff_validations?
-        def should_run_diff_validations?
-          super || validate_path_locks? || push_rule_checks_commit?
-        end
-
         def validate_path_locks?
           strong_memoize(:validate_path_locks) do
             project.feature_available?(:file_locks) &&
@@ -32,7 +27,7 @@ module EE
         def validations_for_diff
           super.tap do |validations|
             validations.push(path_locks_validation) if validate_path_locks?
-            validations.push(file_name_validation) if push_rule
+            validations.push(file_name_validation) if push_rule_checks_commit?
           end
         end
 

ee/spec/lib/gitlab/checks/diff_check_spec.rb
@@ -6,6 +6,16 @@ describe Gitlab::Checks::DiffCheck do
   include_context 'push rules checks context'
 
   describe '#validate!' do
+    context 'no push rules active' do
+      set(:push_rule) { create(:push_rule) }
+
+      it "does not attempt to check commits" do
+        expect(subject).not_to receive(:process_commits)
+
+        subject.validate!
+      end
+    end
+
     context 'file name rules' do
       # Notice that the commit used creates a file named 'README'
       context 'file name regex check' do