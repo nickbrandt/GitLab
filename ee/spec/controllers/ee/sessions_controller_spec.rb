a832e312ae420b159ab4853f595bc49218ced9c9
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2019-02-19 16:00:48 -0300

    Use JSONWebToken::HMACToken to encode state into Geo::Oauth::LoginState
    This change is necessary to prevent replay attacks.

--- ee/spec/controllers/ee/sessions_controller_spec.rb
+++ ee/spec/controllers/ee/sessions_controller_spec.rb
@@ -15,43 +15,41 @@ describe SessionsController do
       set(:primary_node) { create(:geo_node, :primary) }
       set(:secondary_node) { create(:geo_node) }
 
-      let(:salt) { 'MTAwZDhjYmQxNzUw' }
-      let(:login_state) { Gitlab::Geo::Oauth::LoginState.new(salt: salt, return_to: '/') }
-
       before do
         stub_current_geo_node(secondary_node)
-        allow(SecureRandom).to receive(:hex).and_return(salt)
+      end
+
+      shared_examples 'a valid oauth authentication redirect' do
+        it 'redirects to the correct oauth_geo_auth_url' do
+          get(:new)
+
+          redirect_uri = URI.parse(response.location)
+          redirect_params = CGI.parse(redirect_uri.query)
+
+          expect(response).to have_gitlab_http_status(302)
+          expect(response).to redirect_to %r(\A#{primary_node.url}oauth/geo/auth)
+          expect(redirect_params['state'].first).to end_with(':/')
+        end
       end
 
       context 'with a tampered HOST header' do
-        it 'prevents open redirect attack' do
+        before do
           request.headers['HOST'] = 'http://this.is.not.my.host'
-
-          get(:new)
-
-          expect(response).to have_gitlab_http_status(302)
-          expect(response).to redirect_to(oauth_geo_auth_url(host: secondary_node.url, state: login_state.encode))
         end
+
+        it_behaves_like 'a valid oauth authentication redirect'
       end
 
       context 'with a tampered X-Forwarded-Host header' do
-        it 'prevents open redirect attack' do
+        before do
           request.headers['X-Forwarded-Host'] = 'http://this.is.not.my.host'
-
-          get(:new)
-
-          expect(response).to have_gitlab_http_status(302)
-          expect(response).to redirect_to(oauth_geo_auth_url(host: secondary_node.url, state: login_state.encode))
         end
+
+        it_behaves_like 'a valid oauth authentication redirect'
       end
 
       context 'without a tampered header' do
-        it 'redirects to oauth_geo_auth_url' do
-          get(:new)
-
-          expect(response).to have_gitlab_http_status(302)
-          expect(response).to redirect_to(oauth_geo_auth_url(host: secondary_node.url, state: login_state.encode))
-        end
+        it_behaves_like 'a valid oauth authentication redirect'
       end
     end
   end
