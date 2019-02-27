buzzdeee/gitlab-ce

Add project http fetch statistics API
The API get projects/:id/traffic/fetches allows user with write
access to the repository to get the number of clones for the
last 30 days.

lib/api/api.rb
@@ -137,6 +137,7 @@ module API
     mount ::API::ProjectSnapshots
     mount ::API::ProjectSnippets
     mount ::API::ProjectTemplates
+    mount ::API::ProjectTraffics
     mount ::API::ProtectedBranches
     mount ::API::ProtectedTags
     mount ::API::Repositories

nickbrandt/GitLab

lib/api/api.rb
@@ -151,6 +151,7 @@ module API
     mount ::API::Projects
     mount ::API::ProjectSnapshots
     mount ::API::ProjectSnippets
+    mount ::API::ProjectStatistics
     mount ::API::ProjectTemplates
     mount ::API::ProtectedBranches
     mount ::API::ProtectedTags