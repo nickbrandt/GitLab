a5d06643847af87f47f28cb859bf25175dd9683c
Heinrich Lee Yu <heinrich@gitlab.com>
2019-03-01 05:12:39 +0800

    EE-port for 54924-clean-up-data
    Fixes merge conflict in db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190222110418) do
+ActiveRecord::Schema.define(version: 20190228092516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
