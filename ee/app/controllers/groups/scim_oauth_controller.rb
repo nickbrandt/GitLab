1b350d994cee3a0f905a0b22615da5020c0b9e22
James Lopez <james@jameslopez.es>
2019-03-01 15:51:54 +0100

    Update controller and group

--- ee/app/controllers/groups/scim_oauth_controller.rb
+++ ee/app/controllers/groups/scim_oauth_controller.rb
@@ -1,20 +1,38 @@
 # frozen_string_literal: true
 
 class Groups::ScimOauthController < Groups::ApplicationController
-  before_action :require_top_level_group
-  before_action :authorize_manage_saml!
-  before_action :check_group_saml_available!
-  before_action :check_group_saml_configured
+  # before_action :require_top_level_group
+  # before_action :authorize_manage_saml!
+  # before_action :check_group_saml_available!
+  # before_action :check_group_saml_configured
+  skip_before_filter :verify_authenticity_token
+
 
   def show
-    @saml_provider = @group.saml_provider || @group.build_saml_provider
+    scim_token = ScimOauthAccessToken.find_by_group_id(@group.id)
+
+    respond_to do |format|
+      format.json do
+        if scim_token
+          render json: ScimOauthAccessTokenEntity.new(scim_token).as_json
+        else
+          render json: {}
+        end
+      end
+    end
   end
 
   def create
-    @saml_provider = @group.build_saml_provider(saml_provider_params)
+    scim_token = ScimOauthAccessToken.safe_find_or_create_by(group: @group)
 
-    @saml_provider.save
-
-    render :show
+    respond_to do |format|
+      format.json do
+        if scim_token&.valid?
+          render json: ScimOauthAccessTokenEntity.new(scim_token).as_json
+        else
+          render json: { errors: scim_token&.errors&.full_messages }, status: :unprocessable_entity
+        end
+      end
+    end
   end
 end
