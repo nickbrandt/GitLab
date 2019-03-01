b0722b7bf6dfe98342d4bb4bb45979ff34beee47
Phil Hughes <me@iamphill.com>
2019-03-01 15:52:40 +0000

    Refactor some issuable JS code to move EE code out of CE files
    Closes https://gitlab.com/gitlab-org/gitlab-ee/issues/10020, https://gitlab.com/gitlab-org/gitlab-ee/issues/10089

--- app/assets/javascripts/pages/projects/issues/form.js
+++ app/assets/javascripts/pages/projects/issues/form.js
@@ -2,13 +2,12 @@
 
 import $ from 'jquery';
 import GLForm from '~/gl_form';
-import IssuableForm from '~/issuable_form';
+import IssuableForm from 'ee_else_ce/issuable_form';
 import LabelsSelect from '~/labels_select';
 import MilestoneSelect from '~/milestone_select';
 import ShortcutsNavigation from '~/behaviors/shortcuts/shortcuts_navigation';
 import IssuableTemplateSelectors from '~/templates/issuable_template_selectors';
 import initSuggestions from '~/issuable_suggestions';
-import WeightSelect from 'ee/weight_select';
 
 export default () => {
   new ShortcutsNavigation();
@@ -21,6 +20,4 @@ export default () => {
   if (gon.features.graphql) {
     initSuggestions();
   }
-
-  new WeightSelect();
 };
