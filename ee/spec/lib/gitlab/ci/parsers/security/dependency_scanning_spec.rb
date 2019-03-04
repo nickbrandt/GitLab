95de7bfed4d5d686bc4669771ffd48fa0860f604
Lucas Charles <me@lucascharles.me>
2019-03-04 10:03:52 +0000

    Create Merge Request From Vulnerability Solution
    Fixes https://gitlab.com/gitlab-org/gitlab-ee/issues/9224
    
     - Add `MergeRequests::CreateFromVulnerabilityDataService`
     - Add new Vulnerabilities::Feedback#feedback_type of 'merge_request'

--- ee/spec/lib/gitlab/ci/parsers/security/dependency_scanning_spec.rb
+++ ee/spec/lib/gitlab/ci/parsers/security/dependency_scanning_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper'
 
 describe Gitlab::Ci::Parsers::Security::DependencyScanning do
+  using RSpec::Parameterized::TableSyntax
+
   describe '#parse!' do
     let(:project) { artifact.project }
     let(:pipeline) { artifact.job.pipeline }
@@ -10,7 +12,11 @@ describe Gitlab::Ci::Parsers::Security::DependencyScanning do
     let(:report) { Gitlab::Ci::Reports::Security::Report.new(artifact.file_type) }
     let(:parser) { described_class.new }
 
-    where(report_format: %i(dependency_scanning dependency_scanning_deprecated))
+    where(:report_format, :occurrence_count, :identifier_count, :scanner_count, :fingerprint, :version) do
+      :dependency_scanning             | 4 | 7 | 2 | '2773f8cc955346ab1f756b94aa310db8e17c0944' | '1.3'
+      :dependency_scanning_deprecated  | 4 | 7 | 2 | '2773f8cc955346ab1f756b94aa310db8e17c0944' | '1.3'
+      :dependency_scanning_remediation | 2 | 3 | 1 | '228998b5db51d86d3b091939e2f5873ada0a14a1' | '2.0'
+    end
 
     with_them do
       let(:artifact) { create(:ee_ci_job_artifact, report_format) }
@@ -22,17 +28,36 @@ describe Gitlab::Ci::Parsers::Security::DependencyScanning do
       end
 
       it "parses all identifiers and occurrences" do
-        expect(report.occurrences.length).to eq(4)
-        expect(report.identifiers.length).to eq(7)
-        expect(report.scanners.length).to eq(2)
+        expect(report.occurrences.length).to eq(occurrence_count)
+        expect(report.identifiers.length).to eq(identifier_count)
+        expect(report.scanners.length).to eq(scanner_count)
       end
 
       it "generates expected location fingerprint" do
-        expect(report.occurrences.first[:location_fingerprint]).to eq('2773f8cc955346ab1f756b94aa310db8e17c0944')
+        expect(report.occurrences.first[:location_fingerprint]).to eq(fingerprint)
       end
 
       it "generates expected metadata_version" do
-        expect(report.occurrences.first[:metadata_version]).to eq('1.3')
+        expect(report.occurrences.first[:metadata_version]).to eq(version)
+      end
+    end
+
+    context "when vulnerabilities have remediations" do
+      let(:artifact) { create(:ee_ci_job_artifact, :dependency_scanning_remediation) }
+
+      before do
+        artifact.each_blob do |blob|
+          parser.parse!(blob, report)
+        end
+      end
+
+      it "generates occurrence with expected remediation" do
+        occurrence = report.occurrences.last
+        raw_metadata = JSON.parse!(occurrence[:raw_metadata])
+
+        expect(occurrence[:name]).to eq("Authentication bypass via incorrect DOM traversal and canonicalization in saml2-js")
+        expect(raw_metadata["remediations"].first["summary"]).to eq("Upgrade saml2-js")
+        expect(raw_metadata["remediations"].first["diff"]).to start_with("ZGlmZiAtLWdpdCBhL3lhcm4")
       end
     end
   end
