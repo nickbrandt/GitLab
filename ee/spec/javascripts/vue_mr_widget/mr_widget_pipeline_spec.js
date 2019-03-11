410f0587b0e60cc1fbbddbccf61806f304a7d4c5
Filipa Lacerda <filipa@gitlab.com>
2019-03-11 17:29:49 +0000

    Moves EE specific tests into EE file for mr_widget_pipeline_spec

---
+++ ee/spec/javascripts/vue_mr_widget/mr_widget_pipeline_spec.js
@@ -0,0 +1,58 @@
+import Vue from 'vue';
+import pipelineComponent from '~/vue_merge_request_widget/components/mr_widget_pipeline.vue';
+import mountComponent from 'spec/helpers/vue_mount_component_helper';
+import mockData from 'spec/vue_mr_widget/mock_data';
+import mockLinkedPipelines from 'ee_spec/pipelines/graph/linked_pipelines_mock_data';
+
+describe('MRWidgetPipeline', () => {
+  let vm;
+  let Component;
+
+  beforeEach(() => {
+    Component = Vue.extend(pipelineComponent);
+  });
+
+  afterEach(() => {
+    vm.$destroy();
+  });
+
+  describe('computed', () => {
+    describe('when upstream pipelines are passed', () => {
+      beforeEach(() => {
+        vm = mountComponent(Component, {
+          pipeline: Object.assign({}, mockData.pipeline, {
+            triggered_by: mockLinkedPipelines.triggered_by,
+          }),
+          hasCi: true,
+          ciStatus: 'success',
+          troubleshootingDocsPath: 'help',
+        });
+      });
+
+      it('should coerce triggeredBy into a collection', () => {
+        expect(vm.triggeredBy.length).toBe(1);
+      });
+
+      it('should render the linked pipelines mini list', () => {
+        expect(vm.$el.querySelector('.linked-pipeline-mini-list.is-upstream')).not.toBeNull();
+      });
+    });
+
+    describe('when downstream pipelines are passed', () => {
+      beforeEach(() => {
+        vm = mountComponent(Component, {
+          pipeline: Object.assign({}, mockData.pipeline, {
+            triggered: mockLinkedPipelines.triggered,
+          }),
+          hasCi: true,
+          ciStatus: 'success',
+          troubleshootingDocsPath: 'help',
+        });
+      });
+
+      it('should render the linked pipelines mini list', () => {
+        expect(vm.$el.querySelector('.linked-pipeline-mini-list.is-downstream')).not.toBeNull();
+      });
+    });
+  });
+});
--- spec/javascripts/vue_mr_widget/components/mr_widget_pipeline_spec.js
+++ spec/javascripts/vue_mr_widget/components/mr_widget_pipeline_spec.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import pipelineComponent from '~/vue_merge_request_widget/components/mr_widget_pipeline.vue';
 import mountComponent from 'spec/helpers/vue_mount_component_helper';
 import mockData from '../mock_data';
-import mockLinkedPipelines from 'ee_spec/pipelines/graph/linked_pipelines_mock_data'; // eslint-disable-line import/order
 
 describe('MRWidgetPipeline', () => {
   let vm;
@@ -192,44 +191,6 @@ describe('MRWidgetPipeline', () => {
         });
 
         expect(vm.$el.querySelector('.js-mini-pipeline-graph')).toEqual(null);
-      });
-    });
-
-    describe('when upstream pipelines are passed', () => {
-      beforeEach(() => {
-        vm = mountComponent(Component, {
-          pipeline: Object.assign({}, mockData.pipeline, {
-            triggered_by: mockLinkedPipelines.triggered_by,
-          }),
-          hasCi: true,
-          ciStatus: 'success',
-          troubleshootingDocsPath: 'help',
-        });
-      });
-
-      it('should coerce triggeredBy into a collection', () => {
-        expect(vm.triggeredBy.length).toBe(1);
-      });
-
-      it('should render the linked pipelines mini list', () => {
-        expect(vm.$el.querySelector('.linked-pipeline-mini-list.is-upstream')).not.toBeNull();
-      });
-    });
-
-    describe('when downstream pipelines are passed', () => {
-      beforeEach(() => {
-        vm = mountComponent(Component, {
-          pipeline: Object.assign({}, mockData.pipeline, {
-            triggered: mockLinkedPipelines.triggered,
-          }),
-          hasCi: true,
-          ciStatus: 'success',
-          troubleshootingDocsPath: 'help',
-        });
-      });
-
-      it('should render the linked pipelines mini list', () => {
-        expect(vm.$el.querySelector('.linked-pipeline-mini-list.is-downstream')).not.toBeNull();
       });
     });
   });
