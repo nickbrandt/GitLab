3098334021526c8af3818df0c803b0e3d84df889
Filipa Lacerda <filipa@gitlab.com>
2019-03-12 11:11:33 +0000

    Removes EE differences for env app
    Creates EE mixin for env app
    Moves EE tests to a EE file

--- ee/spec/javascripts/environments/environments_app_spec.js
+++ ee/spec/javascripts/environments/environments_app_spec.js
@@ -3,7 +3,7 @@ import MockAdapter from 'axios-mock-adapter';
 import axios from '~/lib/utils/axios_utils';
 import environmentsComponent from '~/environments/components/environments_app.vue';
 import mountComponent from 'spec/helpers/vue_mount_component_helper';
-import { environment, folder } from './mock_data';
+import { environment, folder } from 'spec/environments/mock_data';
 
 describe('Environment', () => {
   const mockData = {
@@ -116,7 +116,82 @@ describe('Environment', () => {
           }, 0);
         });
       });
+
+      describe('deploy boards', () => {
+        it('should render arrow to open deploy boards', done => {
+          setTimeout(() => {
+            expect(
+              component.$el.querySelector('.deploy-board-icon.ic-chevron-right'),
+            ).toBeDefined();
+            done();
+          }, 0);
+        });
+      });
     });
+
+    // ee-only start
+    describe('canary callout', () => {
+      it('should render banner underneath second environment', done => {
+        mock.onGet(mockData.endpoint).reply(
+          200,
+          {
+            environments: [environment, environment],
+            stopped_count: 1,
+            available_count: 0,
+          },
+          {
+            'X-nExt-pAge': '2',
+            'x-page': '1',
+            'X-Per-Page': '1',
+            'X-Prev-Page': '',
+            'X-TOTAL': '37',
+            'X-Total-Pages': '2',
+          },
+        );
+
+        component = mountComponent(EnvironmentsComponent, mockData);
+
+        setTimeout(() => {
+          expect(
+            component.$el
+              .querySelector('.canary-deployment-callout')
+              .getAttribute('data-js-canary-promo-key'),
+          ).toBe('1');
+          done();
+        }, 0);
+      });
+
+      it('should render banner underneath first environment', done => {
+        mock.onGet(mockData.endpoint).reply(
+          200,
+          {
+            environments: [environment],
+            stopped_count: 1,
+            available_count: 0,
+          },
+          {
+            'X-nExt-pAge': '2',
+            'x-page': '1',
+            'X-Per-Page': '1',
+            'X-Prev-Page': '',
+            'X-TOTAL': '37',
+            'X-Total-Pages': '2',
+          },
+        );
+
+        component = mountComponent(EnvironmentsComponent, mockData);
+
+        setTimeout(() => {
+          expect(
+            component.$el
+              .querySelector('.canary-deployment-callout')
+              .getAttribute('data-js-canary-promo-key'),
+          ).toBe('0');
+          done();
+        }, 0);
+      });
+    });
+    // ee-only end
   });
 
   describe('unsuccessfull request', () => {
