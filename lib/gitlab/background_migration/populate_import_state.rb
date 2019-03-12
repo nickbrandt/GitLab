b1c86666895fd3449fa992f4880e286537791e42
Yorick Peterse <yorickpeterse@gmail.com>
2019-03-12 14:16:09 +0100

    Move EE code out of two background migrations
    This moves the EE specific queries to EE modules that we prepend into
    the background migrations. This requires some duplication of the SQL
    queries, but there's sadly no way around this.

--- lib/gitlab/background_migration/populate_import_state.rb
+++ lib/gitlab/background_migration/populate_import_state.rb
@@ -15,8 +15,8 @@ module Gitlab
         Rails.logger.info("#{self.class.name} - Moving import attributes data to project mirror data table: #{start_id} - #{end_id}")
 
         ActiveRecord::Base.connection.execute <<~SQL
-          INSERT INTO project_mirror_data (project_id, status, jid, last_update_at, last_successful_update_at, last_error)
-          SELECT id, import_status, import_jid, mirror_last_update_at, mirror_last_successful_update_at, import_error
+          INSERT INTO project_mirror_data (project_id, status, jid, last_error)
+          SELECT id, import_status, import_jid, import_error
           FROM projects
           WHERE projects.import_status != 'none'
           AND projects.id BETWEEN #{start_id} AND #{end_id}
@@ -37,3 +37,5 @@ module Gitlab
     end
   end
 end
+
+Gitlab::BackgroundMigration::PopulateImportState.prepend(EE::Gitlab::BackgroundMigration::PopulateImportState)
