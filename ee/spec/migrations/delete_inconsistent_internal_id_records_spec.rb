f747f5a65fc89ab0d38460431c1c8bd7fbdaa1b9
Rémy Coutable <remy@rymai.me>
2019-03-14 11:59:10 +0100

    Reduce differences with CE in a migration spec
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- ee/spec/migrations/delete_inconsistent_internal_id_records_spec.rb
+++ ee/spec/migrations/delete_inconsistent_internal_id_records_spec.rb
@@ -1,22 +1,36 @@
 # frozen_string_literal: true
-# rubocop:disable RSpec/FactoriesInMigrationSpecs
+
 require 'spec_helper'
 require Rails.root.join('db', 'post_migrate', '20180723130817_delete_inconsistent_internal_id_records.rb')
 
 describe DeleteInconsistentInternalIdRecords, :migration do
-  context 'for milestones (by group)' do
-    # milestones (by group) is a little different than most of the other models
+  context 'for epics (by group)' do
     let(:groups) { table(:namespaces) }
+    let(:users) { table(:users) }
     let(:group1) { groups.create(name: 'Group 1', type: 'Group', path: 'group_1') }
     let(:group2) { groups.create(name: 'Group 2', type: 'Group', path: 'group_2') }
     let(:group3) { groups.create(name: 'Group 2', type: 'Group', path: 'group_3') }
+    let!(:user) { users.create!(email: 'email@email.com', name: 'foo', username: 'foo', projects_limit: 0) }
 
-    let(:internal_id_query) { ->(group) { InternalId.where(usage: InternalId.usages['milestones'], namespace: group) } }
+    let(:internal_id_query) { ->(group) { InternalId.where(usage: InternalId.usages['epics'], namespace_id: group.id) } }
 
     before do
-      3.times { create(:milestone, group_id: group1.id) }
-      3.times { create(:milestone, group_id: group2.id) }
-      3.times { create(:milestone, group_id: group3.id) }
+      # we use state enum in Epic but state field was added after this migration
+      epics = table(:epics)
+
+      epics.belongs_to(:group)
+      epics.include(AtomicInternalId)
+      epics.has_internal_id(:iid, scope: :group, init: ->(s) { s&.group&.epics&.maximum(:iid) })
+
+      epics.create!(title: 'Epic 1', title_html: 'Epic 1', group_id: group1.id, author_id: user.id)
+      epics.create!(title: 'Epic 2', title_html: 'Epic 2', group_id: group1.id, author_id: user.id)
+      epics.create!(title: 'Epic 3', title_html: 'Epic 3', group_id: group1.id, author_id: user.id)
+      epics.create!(title: 'Epic 4', title_html: 'Epic 4', group_id: group2.id, author_id: user.id)
+      epics.create!(title: 'Epic 5', title_html: 'Epic 5', group_id: group2.id, author_id: user.id)
+      epics.create!(title: 'Epic 6', title_html: 'Epic 6', group_id: group2.id, author_id: user.id)
+      epics.create!(title: 'Epic 7', title_html: 'Epic 7', group_id: group3.id, author_id: user.id)
+      epics.create!(title: 'Epic 8', title_html: 'Epic 8', group_id: group3.id, author_id: user.id)
+      epics.create!(title: 'Epic 9', title_html: 'Epic 9', group_id: group3.id, author_id: user.id)
 
       internal_id_query.call(group1).first.tap do |iid|
         iid.last_value = iid.last_value - 2
@@ -31,11 +45,11 @@ describe DeleteInconsistentInternalIdRecords, :migration do
       end
     end
 
-    it "deletes inconsistent issues" do
+    it "deletes inconsistent records" do
       expect { migrate! }.to change { internal_id_query.call(group1).size }.from(1).to(0)
     end
 
-    it "retains consistent issues" do
+    it "retains consistent records" do
       expect { migrate! }.not_to change { internal_id_query.call(group2).size }
     end
 
