8575ee72bfc1c5af48571695b22e89c3012bddc3
Mark Florian <mflorian@gitlab.com>
2019-03-19 18:24:44 +0800

    Use more explicit/less confusing function name
    It takes options objects (id/name key/value pairs), not `Map`s.

--- ee/app/assets/javascripts/security_dashboard/store/modules/filters/state.js
+++ ee/app/assets/javascripts/security_dashboard/store/modules/filters/state.js
@@ -1,19 +1,19 @@
 import { SEVERITY_LEVELS, REPORT_TYPES, BASE_FILTERS } from './constants';
 
-const mapToArray = map => Object.entries(map).map(([id, name]) => ({ id, name }));
+const optionsObjectToArray = obj => Object.entries(obj).map(([id, name]) => ({ id, name }));
 
 export default () => ({
   filters: [
     {
       name: 'Severity',
       id: 'severity',
-      options: [BASE_FILTERS.severity, ...mapToArray(SEVERITY_LEVELS)],
+      options: [BASE_FILTERS.severity, ...optionsObjectToArray(SEVERITY_LEVELS)],
       selection: new Set(['all']),
     },
     {
       name: 'Report type',
       id: 'report_type',
-      options: [BASE_FILTERS.report_type, ...mapToArray(REPORT_TYPES)],
+      options: [BASE_FILTERS.report_type, ...optionsObjectToArray(REPORT_TYPES)],
       selection: new Set(['all']),
     },
     {
