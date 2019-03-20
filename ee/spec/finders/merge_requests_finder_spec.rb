e384dd6f8d6295a5ec3c82a775825a8517ca1e4c
Rémy Coutable <remy@rymai.me>
2019-03-20 10:27:05 +0100

    [EE] Extract EE-specific code from merge_requests_finder_spec.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- ee/spec/finders/merge_requests_finder_spec.rb
+++ ee/spec/finders/merge_requests_finder_spec.rb
@@ -5,4 +5,16 @@ describe MergeRequestsFinder do
     let!(:subject) { create(:merge_request, source_project: project) }
     let(:project_params) { { project_id: project.id } }
   end
+
+  describe '#execute' do
+    include_context 'MergeRequestsFinder multiple projects with merge requests context'
+
+    it 'ignores filtering by weight' do
+      params = { project_id: project1.id, scope: 'authored', state: 'opened', weight: Issue::WEIGHT_ANY }
+
+      merge_requests = described_class.new(user, params).execute
+
+      expect(merge_requests).to contain_exactly(merge_request1)
+    end
+  end
 end
