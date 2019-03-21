f0878ee8e91af94867dfa38704c733bbc502610f
James Lopez <james@jameslopez.es>
2019-03-21 10:56:42 +0100

    Fix spec failure

--- ee/app/serializers/scim_oauth_access_token_entity.rb
+++ ee/app/serializers/scim_oauth_access_token_entity.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class ScimOauthAccessTokenEntity < Grape::Entity
+  include ::API::Helpers::RelatedResourcesHelpers
+
+  SCIM_PATH = '/api/scim/v2/groups'
+
   expose :scim_api_url do |scim|
-    Gitlab::Routing.url_helpers.group_scim_oauth_url(scim.group)
+    expose_url("#{SCIM_PATH}/#{scim.group.full_path}")
   end
 
   expose :token, as: :scim_token
