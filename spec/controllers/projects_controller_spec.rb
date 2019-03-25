f4ef97402ff30c83d78070a6d36120e098f12755
Małgorzata Ksionek <meksionek@gmail.com>
2019-03-25 11:08:20 +0100

    Add cr remarks

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -406,7 +406,7 @@ describe ProjectsController do
               id: project.id,
               project: params
             }
-        end.not_to change {project.namespace}
+        end.not_to change {project.reload.namespace}
       end
 
       def update_project(**parameters)
