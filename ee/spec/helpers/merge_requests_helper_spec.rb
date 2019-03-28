1f22f70b3ba146a7c8e25bc8a2e9d093dc049e24
Rémy Coutable <remy@rymai.me>
2019-03-28 11:33:38 +0100

    [EE] Reduce the diff with CE in spec/helpers/merge_requests_helper_spec.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

---
+++ ee/spec/helpers/merge_requests_helper_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe MergeRequestsHelper do
+  describe '#render_items_list' do
+    it "returns one item in the list" do
+      expect(render_items_list(["user"])).to eq("user")
+    end
+
+    it "returns two items in the list" do
+      expect(render_items_list(%w(user user1))).to eq("user and user1")
+    end
+
+    it "returns three items in the list" do
+      expect(render_items_list(%w(user user1 user2))).to eq("user, user1 and user2")
+    end
+  end
+end
--- spec/helpers/merge_requests_helper_spec.rb
+++ spec/helpers/merge_requests_helper_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe MergeRequestsHelper do
@@ -41,20 +43,6 @@ describe MergeRequestsHelper do
       let(:target_title) { "#{project.full_path}:#{merge_request.target_branch}" }
 
       it { is_expected.to eq([source_title, target_title]) }
-    end
-  end
-
-  describe '#render_items_list' do
-    it "returns one item in the list" do
-      expect(render_items_list(["user"])).to eq("user")
-    end
-
-    it "returns two items in the list" do
-      expect(render_items_list(%w(user user1))).to eq("user and user1")
-    end
-
-    it "returns three items in the list" do
-      expect(render_items_list(%w(user user1 user2))).to eq("user, user1 and user2")
     end
   end
 
