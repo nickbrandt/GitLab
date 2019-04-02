2a9ab657d0f38aeb7f38dd604c1c422d3159d61f
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2019-04-02 19:24:00 -0300

    Use FDW to find unsynced projects when feature flag is enabled

--- ee/app/finders/geo/project_registry_finder.rb
+++ ee/app/finders/geo/project_registry_finder.rb
@@ -72,18 +72,11 @@ module Geo
         .execute
     end
 
-    # rubocop: disable CodeReuse/ActiveRecord
-    def find_unsynced_projects(batch_size:)
-      relation =
-        if use_legacy_queries?
-          legacy_find_unsynced_projects
-        else
-          fdw_find_unsynced_projects
-        end
-
-      relation.limit(batch_size)
+    def find_unsynced_projects(shard_name:, batch_size:)
+      finder_klass_for_unsynced_projects
+        .new(current_node: current_node, shard_name: shard_name, batch_size: batch_size)
+        .execute
     end
-    # rubocop: enable CodeReuse/ActiveRecord
 
     # rubocop: disable CodeReuse/ActiveRecord
     def find_projects_updated_recently(batch_size:)
@@ -100,39 +93,12 @@ module Geo
 
     protected
 
-    #
-    # FDW accessors
-    #
-
-    # @return [ActiveRecord::Relation<Geo::Fdw::Project>]
-    # rubocop: disable CodeReuse/ActiveRecord
-    def fdw_find_unsynced_projects
-      Geo::Fdw::Project.joins("LEFT OUTER JOIN project_registry ON project_registry.project_id = #{fdw_project_table.name}.id")
-        .where(project_registry: { project_id: nil })
-    end
-    # rubocop: enable CodeReuse/ActiveRecord
-
     # @return [ActiveRecord::Relation<Geo::Fdw::Project>]
     # rubocop: disable CodeReuse/ActiveRecord
     def fdw_find_projects_updated_recently
       Geo::Fdw::Project.joins("INNER JOIN project_registry ON project_registry.project_id = #{fdw_project_table.name}.id")
           .merge(Geo::ProjectRegistry.dirty)
           .merge(Geo::ProjectRegistry.retry_due)
-    end
-    # rubocop: enable CodeReuse/ActiveRecord
-
-    #
-    # Legacy accessors (non FDW)
-    #
-
-    # @return [ActiveRecord::Relation<Project>] list of unsynced projects
-    # rubocop: disable CodeReuse/ActiveRecord
-    def legacy_find_unsynced_projects
-      legacy_left_outer_join_registry_ids(
-        current_node.projects,
-        Geo::ProjectRegistry.pluck(:project_id),
-        Project
-      )
     end
     # rubocop: enable CodeReuse/ActiveRecord
 
@@ -169,6 +135,14 @@ module Geo
 
     def use_legacy_queries_for_selective_sync?
       fdw_disabled? || selective_sync? && !Gitlab::Geo::Fdw.enabled_for_selective_sync?
+    end
+
+    def finder_klass_for_unsynced_projects
+      if use_legacy_queries_for_selective_sync?
+        Geo::LegacyProjectUnsyncedFinder
+      else
+        Geo::ProjectUnsyncedFinder
+      end
     end
 
     def finder_klass_for_synced_registries
--- ee/spec/finders/geo/project_registry_finder_spec.rb
+++ ee/spec/finders/geo/project_registry_finder_spec.rb
@@ -380,43 +380,6 @@ describe Geo::ProjectRegistryFinder, :geo do
   end
 
   shared_examples 'finds all the things' do |method_prefix|
-    describe '#find_unsynced_projects' do
-      it 'delegates to the correct method' do
-        expect(subject).to receive("#{method_prefix}_find_unsynced_projects".to_sym).and_call_original
-
-        subject.find_unsynced_projects(batch_size: 10)
-      end
-
-      it 'returns projects without an entry on the tracking database' do
-        project_not_synced = create(:project)
-        create(:geo_project_registry, :synced, :repository_dirty, project: project_1_in_synced_group)
-
-        projects = subject.find_unsynced_projects(batch_size: 10)
-
-        expect(projects).to match_ids(project_not_synced)
-      end
-
-      context 'with selective sync' do
-        before do
-          secondary.update!(selective_sync_type: 'namespaces', namespaces: [synced_group])
-        end
-
-        it 'delegates to #legacy_find_unsynced_projects' do
-          expect(subject).to receive(:legacy_find_unsynced_projects).and_call_original
-
-          subject.find_unsynced_projects(batch_size: 10)
-        end
-
-        it 'returns untracked projects in the synced group' do
-          create(:geo_project_registry, :sync_failed, project: project_1_in_synced_group)
-
-          projects = subject.find_unsynced_projects(batch_size: 10)
-
-          expect(projects).to match_ids(project_2_in_synced_group)
-        end
-      end
-    end
-
     describe '#find_projects_updated_recently' do
       it 'delegates to the correct method' do
         expect(subject).to receive("#{method_prefix}_find_projects_updated_recently".to_sym).and_call_original
@@ -520,6 +483,13 @@ describe Geo::ProjectRegistryFinder, :geo do
 
     include_examples 'counts all the things', 'legacy'
     include_examples 'finds all the things', 'legacy'
+  end
+
+  describe '#find_unsynced_projects', :delete do
+    include_examples 'delegates to the proper finder',
+      Geo::LegacyProjectUnsyncedFinder,
+      Geo::ProjectUnsyncedFinder,
+      :find_unsynced_projects, [shard_name: 'default', batch_size: 100]
   end
 
   describe '#find_failed_project_registries', :delete do
