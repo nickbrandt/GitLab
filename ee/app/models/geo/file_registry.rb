3cf0f1c92607616f9e5c1015f5614c20425958cd
Toon Claes <toon@gitlab.com>
2019-04-02 16:55:28 +0200

    Add methods to filter on status
    Add some methods which you can use to filter Geo::FileRegistries on
    status, or check what's a particular registry has (used in the Geo
    Admin panel).

--- ee/app/models/geo/file_registry.rb
+++ ee/app/models/geo/file_registry.rb
@@ -5,6 +5,8 @@ class Geo::FileRegistry < Geo::BaseRegistry
 
   scope :lfs_objects, -> { where(file_type: :lfs) }
   scope :attachments, -> { where(file_type: Geo::FileService::DEFAULT_OBJECT_TYPES) }
+  scope :never, -> { where(retry_count: nil) }
+  scope :fresh, -> { order(created_at: :desc) }
 
   self.inheritance_column = 'file_type'
 
@@ -14,5 +16,31 @@ class Geo::FileRegistry < Geo::BaseRegistry
     elsif Geo::FileService::DEFAULT_OBJECT_TYPES.include?(file_type.to_sym)
       Geo::UploadRegistry
     end
+  end
+
+  def self.with_status(status)
+    case status
+    when 'synced'
+      synced
+    when 'never'
+      never
+    when 'failed'
+      failed
+    else
+      self
+    end
+  end
+
+  # Returns a synchronization state based on existing attribute values
+  #
+  # It takes into account things like if a successful replication has been done
+  # if there are pending actions or existing errors
+  #
+  # @return [Symbol] :never, :failed:, :pending or :synced
+  def synchronization_state
+    return :synced if success?
+    return :never if success.nil? && retry_count.nil?
+
+    :failed
   end
 end
