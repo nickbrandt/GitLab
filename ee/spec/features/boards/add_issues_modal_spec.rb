2841265392d3035651deca8c667116c21879b7d3
charlieablett <cablett@gitlab.com>
2019-04-09 07:29:48 +1200

    Remove unneeded spec variables

--- ee/spec/features/boards/add_issues_modal_spec.rb
+++ ee/spec/features/boards/add_issues_modal_spec.rb
@@ -6,12 +6,10 @@ describe 'Issue Boards add issue modal', :js do
   let(:project) { create(:project, :public) }
   let(:board) { create(:board, project: project) }
   let(:user) { create(:user) }
-  let!(:planning) { create(:label, project: project, name: 'Planning') }
+
   let!(:label) { create(:label, project: project) }
-  let!(:list1) { create(:list, board: board, label: planning, position: 0) }
-  let!(:list2) { create(:list, board: board, label: label, position: 1) }
+  let!(:list) { create(:list, board: board, label: label, position: 0) }
   let!(:issue) { create(:issue, project: project, title: 'abc', description: 'def') }
-  let!(:issue2) { create(:issue, project: project, title: 'hij', description: 'klm') }
 
   before do
     project.add_maintainer(user)
