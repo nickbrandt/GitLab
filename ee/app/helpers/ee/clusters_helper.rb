67c0d52080b2d19b005e4e1848fd6c9ef745ecfb
Reuben Pereira <rpereira@gitlab.com>
2019-04-10 12:19:46 +0000

    Remove conditional from clusters/_health partial
    - Since the condition is now being checked in the clusters/show view.

--- ee/app/helpers/ee/clusters_helper.rb
+++ ee/app/helpers/ee/clusters_helper.rb
@@ -8,5 +8,10 @@ module EE
     def has_multiple_clusters?
       clusterable.feature_available?(:multiple_clusters)
     end
+
+    override :show_cluster_health_graphs?
+    def show_cluster_health_graphs?(cluster)
+      cluster.project_type? && cluster.project.feature_available?(:cluster_health)
+    end
   end
 end
