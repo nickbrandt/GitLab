a09448a4cf8765cae8280269e344e3537385d107
James Lopez <james@gitlab.com>
2019-04-11 20:19:13 +0000

    Merge branch 'winh-hide-scoped-labels-message-ee' into 'master'
    Hide scoped labels help text without corresponding license
    
    Closes #11095
    
    See merge request gitlab-org/gitlab-ee!10737
    
    (cherry picked from commit 9886230b771c66facf0c4ce7936b7cda8b98c086)
    
    811f0f31 Add failing test for scoped labels hint
    b3024c2d Hide scoped labels help text without corresponding license

---
+++ ee/spec/views/shared/labels/_create_label_help_text.html.haml_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'app/views/shared/labels/_create_label_help_text.html.haml' do
+  include ApplicationHelper
+
+  shared_examples 'scoped labels' do
+    context 'when license has scoped labels feature' do
+      before do
+        stub_licensed_features(scoped_labels: true)
+      end
+
+      it 'displays scoped labels hint' do
+        render 'shared/labels/create_label_help_text'
+
+        expect(rendered).to have_content 'scoped label'
+      end
+    end
+
+    context 'when license does not have scoped labels feature' do
+      before do
+        stub_licensed_features(scoped_labels: false)
+      end
+
+      it 'does not display scoped labels hint' do
+        render 'shared/labels/create_label_help_text'
+
+        expect(rendered).not_to have_content 'scoped label'
+      end
+    end
+  end
+
+  context 'for project label' do
+    before do
+      project = create(:project)
+      @label = project.labels.new
+    end
+
+    include_examples 'scoped labels'
+  end
+
+  context 'for group label' do
+    before do
+      group = create(:group)
+      @label = group.labels.new
+    end
+
+    include_examples 'scoped labels'
+  end
+end
