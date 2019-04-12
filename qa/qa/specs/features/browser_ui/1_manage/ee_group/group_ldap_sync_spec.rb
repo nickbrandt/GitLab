a54fca6798c49ad7cd9e972912729d185c98444d
Sanad Liaquat <sliaquat@gitlab.com>
2019-04-12 16:45:49 +0500

    Fix LDAP specs

--- qa/qa/specs/features/browser_ui/1_manage/ee_group/group_ldap_sync_spec.rb
+++ qa/qa/specs/features/browser_ui/1_manage/ee_group/group_ldap_sync_spec.rb
@@ -14,7 +14,10 @@ module QA
 
         # Create an admin personal access token and use it for the remaining API calls
         @original_personal_access_token = Runtime::Env.personal_access_token
-        Page::Main::Menu.perform(&:sign_out)
+
+        Page::Main::Menu.perform do |menu|
+          menu.sign_out if menu.has_personal_area?
+        end
 
         Page::Main::Login.perform(&:sign_in_using_admin_credentials)
         Runtime::Env.personal_access_token = Resource::PersonalAccessToken.fabricate!.access_token
