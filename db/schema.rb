935203671da652d17532ec28b92a46130a974e92
Vladimir Shushlin <v.shushlin@gmail.com>
2019-04-12 19:43:13 +0300

    Add Let's Encrypt application settings
    Store Let's Encrypt account email in application settings
    Also add explicit terms of service consent

--- db/schema.rb
+++ db/schema.rb
@@ -218,6 +218,8 @@ ActiveRecord::Schema.define(version: 20190408163745) do
     t.integer "first_day_of_week", default: 0, null: false
     t.boolean "elasticsearch_limit_indexing", default: false, null: false
     t.integer "default_project_creation", default: 2, null: false
+    t.string "lets_encrypt_notification_email"
+    t.boolean "lets_encrypt_terms_of_service_accepted", default: false, null: false
     t.string "geo_node_allowed_ips", default: "0.0.0.0/0, ::/0"
     t.index ["custom_project_templates_group_id"], name: "index_application_settings_on_custom_project_templates_group_id", using: :btree
     t.index ["file_template_project_id"], name: "index_application_settings_on_file_template_project_id", using: :btree
