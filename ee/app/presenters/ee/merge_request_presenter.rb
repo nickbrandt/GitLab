b78ec13c0525be3f8deccd15b1f144b202bbfae1
Paul Slaughter <pslaughter@gitlab.com>
2019-04-17 13:19:38 -0500

    Add presenter to approvals_multiple_rule
    Also:
    - Move project settings path to presenter
    - Add consistent conditions to presenter
    - Clean up presenter spec

--- ee/app/presenters/ee/merge_request_presenter.rb
+++ ee/app/presenters/ee/merge_request_presenter.rb
@@ -6,31 +6,37 @@ module EE
     prepend VisibleApprovableForRule
 
     def approvals_path
-      if approval_feature_available?
-        approvals_project_merge_request_path(project, merge_request)
+      if expose_mr_approval_path?
+        expose_url(approvals_project_merge_request_path(project, merge_request))
       end
     end
 
     def api_approvals_path
-      if approval_feature_available?
+      if expose_mr_approval_path?
         expose_url(api_v4_projects_merge_requests_approvals_path(id: project.id, merge_request_iid: merge_request.iid))
       end
     end
 
     def api_approval_settings_path
-      if approval_feature_available?
+      if expose_mr_approval_path?
         expose_url(api_v4_projects_merge_requests_approval_settings_path(id: project.id, merge_request_iid: merge_request.iid))
       end
     end
 
-    def api_approve_path
+    def api_project_approval_settings_path
       if approval_feature_available?
+        expose_url(api_v4_projects_approval_settings_path(id: project.id))
+      end
+    end
+
+    def api_approve_path
+      if expose_mr_approval_path?
         expose_url(api_v4_projects_merge_requests_approve_path(id: project.id, merge_request_iid: merge_request.iid))
       end
     end
 
     def api_unapprove_path
-      if approval_feature_available?
+      if expose_mr_approval_path?
         expose_url(api_v4_projects_merge_requests_unapprove_path(id: project.id, merge_request_iid: merge_request.iid))
       end
     end
@@ -45,6 +51,12 @@ module EE
 
     def approver_groups
       ::ApproverGroup.filtered_approver_groups(merge_request.approver_groups, current_user)
+    end
+
+    private
+
+    def expose_mr_approval_path?
+      approval_feature_available? && merge_request.iid
     end
   end
 end
