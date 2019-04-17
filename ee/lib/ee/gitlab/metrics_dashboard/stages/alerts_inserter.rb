eb6b9e44272a25b94bf735eae1608bec118415fb
syasonik <syasonik@gitlab.com>
2019-04-18 00:05:43 +0800

    Reduce cognitive complexity of AlertsInserter

--- ee/lib/ee/gitlab/metrics_dashboard/stages/alerts_inserter.rb
+++ ee/lib/ee/gitlab/metrics_dashboard/stages/alerts_inserter.rb
@@ -11,10 +11,9 @@ module EE
             alerts = metrics_with_alerts
 
             for_metrics do |metric|
-              next unless metric_id = metric[:metric_id]
-              next unless alerts.include?(metric_id)
+              next unless alerts.include?(metric[:metric_id])
 
-              metric[:alert_path] = alert_path(metric_id, project, environment)
+              metric[:alert_path] = alert_path(metric[:metric_id], project, environment)
             end
           end
 
