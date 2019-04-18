1a5a2af1474ee5d0d911d03892622134e581d0f5
Phil Hughes <me@iamphill.com>
2019-04-18 16:15:34 +0100

    Fixes stylelint in EE specific boards SCSS
    Closes https://gitlab.com/gitlab-org/gitlab-ee/issues/10942

--- ee/app/assets/javascripts/boards/components/boards_list_selector/assignees_list_item.vue
+++ ee/app/assets/javascripts/boards/components/boards_list_selector/assignees_list_item.vue
@@ -25,13 +25,13 @@ export default {
 
 <template>
   <li class="filter-dropdown-item" @click="handleItemClick">
-    <button class="btn btn-link dropdown-user" type="button">
-      <div class="avatar-container s32">
+    <button class="btn btn-link dropdown-user d-flex py-2 px-4" type="button">
+      <div class="avatar-container s32 flex-shrink-0">
         <img :alt="avatarAltText" :src="item.avatar_url" class="avatar s32 lazy" />
       </div>
-      <div class="dropdown-user-details">
-        <div :title="item.name">{{ item.name }}</div>
-        <div :title="item.username" class="dropdown-light-content">@{{ item.username }}</div>
+      <div class="text-truncate dropdown-user-details">
+        <div class="text-truncate">{{ item.name }}</div>
+        <div class="text-truncate dropdown-light-content">@{{ item.username }}</div>
       </div>
     </button>
   </li>
