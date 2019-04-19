570b1885f29940eddb197aae0d09d0c03848cb82
Lin Jen-Shin <godfat@godfat.org>
2019-04-20 02:17:43 +0800

    Fix updating/deleting insights
    See https://gitlab.com/gitlab-org/gitlab-ee/issues/11099

--- lib/gitlab/database/migration_helpers.rb
+++ lib/gitlab/database/migration_helpers.rb
@@ -906,8 +906,8 @@ module Gitlab
       end
 
       def remove_foreign_key_if_exists(*args)
-        if foreign_key_exists?(args)
-          remove_foreign_key(args)
+        if foreign_key_exists?(*args)
+          remove_foreign_key(*args)
         end
       end
 
