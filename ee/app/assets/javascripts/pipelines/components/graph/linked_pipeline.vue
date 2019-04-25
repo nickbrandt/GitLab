ab0a8f49a641a2e4bd4aa28faa0cfdacf7a6d0b2
sarahghp <sarah.groff.palermo@gmail.com>
2019-04-25 17:15:29 -0400

    Addresses second issue

--- ee/app/assets/javascripts/pipelines/components/graph/linked_pipeline.vue
+++ ee/app/assets/javascripts/pipelines/components/graph/linked_pipeline.vue
@@ -52,7 +52,7 @@ export default {
       @click="onClickLinkedPipeline"
     >
       <gl-loading-icon v-if="pipeline.isLoading" class="js-linked-pipeline-loading d-inline" />
-      <ci-status v-else :status="pipelineStatus" class="js-linked-pipeline-status" />
+      <ci-status v-else :status="pipelineStatus" cssClasses="position-top-0" class="js-linked-pipeline-status" />
 
       <span class="str-truncated align-bottom"> {{ projectName }} &#8226; #{{ pipeline.id }} </span>
     </gl-button>
