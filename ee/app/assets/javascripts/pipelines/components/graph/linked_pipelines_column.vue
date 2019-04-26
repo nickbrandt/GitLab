a59b673fd4578bd7e0dd308f211e1b7f6ba226d6
sarahghp <sarah.groff.palermo@gmail.com>
2019-04-26 12:17:53 -0400

    Fix failing formatting

--- ee/app/assets/javascripts/pipelines/components/graph/linked_pipelines_column.vue
+++ ee/app/assets/javascripts/pipelines/components/graph/linked_pipelines_column.vue
@@ -36,10 +36,17 @@ export default {
     <div class="stage-name linked-pipelines-column-title">{{ columnTitle }}</div>
     <div class="cross-project-triangle"></div>
     <ul>
-      <linked-pipeline v-for='(pipeline, index) in linkedPipelines' :key='pipeline.id' :class='{
-      'flat-connector-before': index === 0 && graphPosition === 'right', active:
-      pipeline.isExpanded, 'left-connector': pipeline.isExpanded && graphPosition === 'left', }'
-      :pipeline='pipeline' @pipelineClicked='$emit('linkedPipelineClick', pipeline, index)' />
+      <linked-pipeline
+        v-for="(pipeline, index) in linkedPipelines"
+        :key="pipeline.id"
+        :class="{
+          'flat-connector-before': index === 0 && graphPosition === 'right',
+          active: pipeline.isExpanded,
+          'left-connector': pipeline.isExpanded && graphPosition === 'left',
+        }"
+        :pipeline="pipeline"
+        @pipelineClicked="$emit('linkedPipelineClick', pipeline, index)"
+      />
     </ul>
   </div>
 </template>
