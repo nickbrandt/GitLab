0bd38602098a4f5020a27057fad09780b16e5b28
Ash McKenzie <amckenzie@gitlab.com>
2019-04-30 17:43:01 +1200

    Ensure Issue & MR note_html cannot be imported

--- spec/lib/gitlab/import_export/project_tree_restorer_spec.rb
+++ spec/lib/gitlab/import_export/project_tree_restorer_spec.rb
@@ -59,20 +59,20 @@ describe Gitlab::ImportExport::ProjectTreeRestorer do
       end
 
       context 'when importing a project with cached_markdown_version and note_html' do
-        let!(:issue) { Issue.find_by(description: 'Aliquam enim illo et possimus.') }
-        let(:note1) { issue.notes.select {|n| n.note.match(/Quo reprehenderit aliquam qui dicta impedit cupiditate eligendi/)}.first }
-        let(:note2) { issue.notes.select {|n| n.note.match(/Est reprehenderit quas aut aspernatur autem recusandae voluptatem/)}.first }
-
-        it 'does not import the note_html' do
-          expect(note1.note_html).to match(/Quo reprehenderit aliquam qui dicta impedit cupiditate eligendi/)
+        context 'for an Issue' do
+          it 'does not import note_html' do
+            note_content = 'Quo reprehenderit aliquam qui dicta impedit cupiditate eligendi'
+            issue_note = Issue.find_by(description: 'Aliquam enim illo et possimus.').notes.select { |n| n.note.match(/#{note_content}/)}.first
+            expect(issue_note.note_html).to match(/#{note_content}/)
+          end
         end
 
-        it 'does not set the old cached_markdown_version' do
-          expect(note2.cached_markdown_version).not_to eq(121212)
-        end
-
-        it 'does not import the note_html' do
-          expect(note2.note_html).to match(/Est reprehenderit quas aut aspernatur autem recusandae voluptatem/)
+        context 'for a Merge Request' do
+          it 'does not import note_html' do
+            note_content = 'Sit voluptatibus eveniet architecto quidem'
+            merge_request_note = MergeRequest.find_by(title: 'MR1').notes.select { |n| n.note.match(/#{note_content}/)}.first
+            expect(merge_request_note.note_html).to match(/#{note_content}/)
+          end
         end
       end
 
