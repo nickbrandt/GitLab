24d024215487067ddf0669cac3129e921977b8b4
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2019-04-29 23:21:53 -0300

    Refactor specs to reuse shared examples
    Remove duplicatin between spec files.

--- ee/spec/finders/geo/job_artifact_registry_finder_spec.rb
+++ ee/spec/finders/geo/job_artifact_registry_finder_spec.rb
@@ -20,25 +20,6 @@ describe Geo::JobArtifactRegistryFinder, :geo do
     stub_artifacts_object_storage
   end
 
-  it 'responds to file registry finder methods' do
-    file_registry_finder_methods = %i{
-      syncable
-      count_syncable
-      count_synced
-      count_failed
-      count_synced_missing_on_primary
-      count_registry
-      find_unsynced
-      find_migrated_local
-      find_retryable_failed_registries
-      find_retryable_synced_missing_on_primary_registries
-    }
-
-    file_registry_finder_methods.each do |method|
-      expect(subject).to respond_to(method)
-    end
-  end
-
   shared_examples 'counts all the things' do
     describe '#count_syncable' do
       let!(:job_artifact_1) { create(:ci_job_artifact, project: synced_project) }
@@ -582,34 +563,5 @@ describe Geo::JobArtifactRegistryFinder, :geo do
     end
   end
 
-  # Disable transactions via :delete method because a foreign table
-  # can't see changes inside a transaction of a different connection.
-  context 'FDW', :geo_fdw, :delete do
-    context 'with use_fdw_queries_for_selective_sync disabled' do
-      before do
-        stub_feature_flags(use_fdw_queries_for_selective_sync: false)
-      end
-
-      include_examples 'counts all the things'
-      include_examples 'finds all the things'
-    end
-
-    context 'with use_fdw_queries_for_selective_sync enabled' do
-      before do
-        stub_feature_flags(use_fdw_queries_for_selective_sync: true)
-      end
-
-      include_examples 'counts all the things'
-      include_examples 'finds all the things'
-    end
-  end
-
-  context 'Legacy' do
-    before do
-      stub_fdw_disabled
-    end
-
-    include_examples 'counts all the things'
-    include_examples 'finds all the things'
-  end
+  it_behaves_like 'a file registry finder'
 end
