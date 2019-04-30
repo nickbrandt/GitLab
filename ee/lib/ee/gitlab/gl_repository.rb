fb5aa88c1a9f1a5c5f679835893a4c76a2ef0f3d
Bob Van Landuyt <bob@vanlanduyt.co>
2019-04-30 11:37:07 +0200

    Make Gitlab::GlRepository#types an instance method
    Having this as an instance method makes it easier to override in the
    prepended `EE` module.
    
    If we try to override this method on the module itself, it would not
    be overridden correctly, depending on the load order.

--- ee/lib/ee/gitlab/gl_repository.rb
+++ ee/lib/ee/gitlab/gl_repository.rb
@@ -3,6 +3,7 @@
 module EE
   module Gitlab
     module GlRepository
+      extend ::Gitlab::Utils::Override
       extend ActiveSupport::Concern
 
       DESIGN = ::Gitlab::GlRepository::RepoType.new(
@@ -15,10 +16,9 @@ module EE
         DESIGN.name.to_s => DESIGN
       }.freeze
 
-      class_methods do
-        def types
-          super.merge(EE_TYPES)
-        end
+      override :types
+      def types
+        super.merge(EE_TYPES)
       end
     end
   end
--- ee/spec/lib/gitlab/gl_repository_spec.rb
+++ ee/spec/lib/gitlab/gl_repository_spec.rb
@@ -9,4 +9,12 @@ describe Gitlab::GlRepository do
       expect(described_class.parse("design-#{project.id}")).to eq([project, EE::Gitlab::GlRepository::DESIGN])
     end
   end
+
+  describe '.types' do
+    it 'contains both the EE and CE repository types' do
+      expected_types = Gitlab::GlRepository::TYPES.merge(EE::Gitlab::GlRepository::EE_TYPES)
+
+      expect(described_class.types).to eq(expected_types)
+    end
+  end
 end
