c0298a69b504825b2bf0a5246b1f6203a8a4140b
Sean McGivern <sean@gitlab.com>
2019-05-06 10:18:34 +0100

    Fix updating boards through API
    There were a couple of issues here:
    
    1. Because an empty `labels` key was in the params, it was blowing up as
       ActiveRecord's expecting a board's labels to be an association.
    2. The service wouldn't allow removing labels, assignee, etc.

--- ee/spec/requests/api/boards_spec.rb
+++ ee/spec/requests/api/boards_spec.rb
@@ -4,7 +4,7 @@ describe API::Boards do
   set(:user) { create(:user) }
   set(:board_parent) { create(:project, :public, creator_id: user.id, namespace: user.namespace ) }
   set(:milestone) { create(:milestone, project: board_parent) }
-  set(:board) { create(:board, project: board_parent, milestone: milestone) }
+  set(:board) { create(:board, project: board_parent, milestone: milestone, assignee: user) }
 
   it_behaves_like 'multiple and scoped issue boards', "/projects/:id/boards"
 
