ef9ef3a2a5ea49795f37f06e997b05b936220d49
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2019-05-07 19:43:29 +0000

    Merge branch 'sh-fix-use-path-for-approval-endpoitns' into 'master'
    Use path instead of a URL for accessing approval settings
    
    Closes gitlab-ce#61558
    
    See merge request gitlab-org/gitlab-ee!12414
    
    (cherry picked from commit 1712d0fe8ef7dc09db3d22b9ac12eaa9b6ffddd2)
    
    d344d1b3 Use path instead of a URL for accessing approval settings

--- ee/app/presenters/ee/merge_request_presenter.rb
+++ ee/app/presenters/ee/merge_request_presenter.rb
@@ -7,37 +7,37 @@ module EE
 
     def approvals_path
       if expose_mr_approval_path?
-        expose_url(approvals_project_merge_request_path(project, merge_request))
+        expose_path(approvals_project_merge_request_path(project, merge_request))
       end
     end
 
     def api_approvals_path
       if expose_mr_approval_path?
-        expose_url(api_v4_projects_merge_requests_approvals_path(id: project.id, merge_request_iid: merge_request.iid))
+        expose_path(api_v4_projects_merge_requests_approvals_path(id: project.id, merge_request_iid: merge_request.iid))
       end
     end
 
     def api_approval_settings_path
       if expose_mr_approval_path?
-        expose_url(api_v4_projects_merge_requests_approval_settings_path(id: project.id, merge_request_iid: merge_request.iid))
+        expose_path(api_v4_projects_merge_requests_approval_settings_path(id: project.id, merge_request_iid: merge_request.iid))
       end
     end
 
     def api_project_approval_settings_path
       if approval_feature_available?
-        expose_url(api_v4_projects_approval_settings_path(id: project.id))
+        expose_path(api_v4_projects_approval_settings_path(id: project.id))
       end
     end
 
     def api_approve_path
       if expose_mr_approval_path?
-        expose_url(api_v4_projects_merge_requests_approve_path(id: project.id, merge_request_iid: merge_request.iid))
+        expose_path(api_v4_projects_merge_requests_approve_path(id: project.id, merge_request_iid: merge_request.iid))
       end
     end
 
     def api_unapprove_path
       if expose_mr_approval_path?
-        expose_url(api_v4_projects_merge_requests_unapprove_path(id: project.id, merge_request_iid: merge_request.iid))
+        expose_path(api_v4_projects_merge_requests_unapprove_path(id: project.id, merge_request_iid: merge_request.iid))
       end
     end
 
