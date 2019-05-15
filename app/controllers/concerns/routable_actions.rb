ngen-brett/gitlab-ce

Refactor RoutableActions to allow for additional checks

app/controllers/concerns/routable_actions.rb
@@ -9,13 +9,19 @@ module RoutableActions
       ensure_canonical_path(routable, requested_full_path)
       routable
     else
-      if not_found_or_authorized_proc
-        not_found_or_authorized_proc.call(routable)
-      end
+      perform_not_found_checks(routable, [not_found_or_authorized_proc])
 
       route_not_found unless performed?
 
       nil
+    end
+  end
+
+  def perform_not_found_checks(routable, checks)
+    checks.compact.each do |check|
+      break if performed?
+
+      check.call(routable)
     end
   end
 

nickbrandt/GitLab

app/controllers/concerns/routable_actions.rb
@@ -9,13 +9,19 @@ module RoutableActions
       ensure_canonical_path(routable, requested_full_path)
       routable
     else
-      if not_found_or_authorized_proc
-        not_found_or_authorized_proc.call(routable)
-      end
+      perform_not_found_actions(routable, [not_found_or_authorized_proc])
 
       route_not_found unless performed?
 
       nil
+    end
+  end
+
+  def perform_not_found_actions(routable, actions)
+    actions.compact.each do |action|
+      break if performed?
+
+      action.call(routable)
     end
   end
 