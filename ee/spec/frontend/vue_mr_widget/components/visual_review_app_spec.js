ef2381ace8de4000e2638598f316c40621a7f982
Sarah Groff Hennigh-Palermo <sarah.groff.palermo@gmail.com>
2019-05-20 17:08:28 +0000

    Basic implementation, cancel broken
    Trying to figure things out touches many files

---
+++ ee/spec/frontend/vue_mr_widget/components/visual_review_app_spec.js
@@ -0,0 +1,82 @@
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
+import VisualReviewAppLink from 'ee/vue_merge_request_widget/components/visual_review_app_link.vue';
+import { GlButton, GlModal } from '@gitlab/ui';
+
+const localVue = createLocalVue();
+
+describe('Visual Review App Link', () => {
+  const Component = localVue.extend(VisualReviewAppLink);
+  let wrapper;
+  let propsData;
+
+  beforeEach(() => {
+    propsData = {
+      cssClass: 'button cool-button best-button',
+      appMetadata: {
+        issueId: 1,
+        sourceProjectId: 20,
+        appUrl: 'http://gitlab.example.com',
+      },
+      link: 'http://example.com',
+    };
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe('renders link and text', () => {
+    beforeEach(() => {
+      wrapper = mount(Component, {
+        propsData,
+        localVue,
+      });
+    });
+
+    it('renders Review text', () => {
+      expect(wrapper.find(GlButton).text()).toBe('Review');
+    });
+
+    it('renders provided cssClass as class attribute', () => {
+      expect(wrapper.find(GlButton).attributes('class')).toEqual(
+        expect.stringContaining(propsData.cssClass),
+      );
+    });
+  });
+
+  describe('renders the modal', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(Component, {
+        propsData,
+        localVue,
+      });
+    });
+
+    it('with expected project Id', () => {
+      expect(wrapper.find(GlModal).text()).toEqual(
+        expect.stringContaining(`data-project-id='${propsData.appMetadata.sourceProjectId}'`),
+      );
+    });
+
+    it('with expected discussion Id', () => {
+      expect(wrapper.find(GlModal).text()).toEqual(
+        expect.stringContaining(`data-discussion-id='${propsData.appMetadata.issueId}'`),
+      );
+    });
+
+    it('with expected appUrl', () => {
+      expect(wrapper.find(GlModal).text()).toEqual(
+        expect.stringContaining(`data-mr-url='${propsData.appMetadata.appUrl}'`),
+      );
+    });
+
+    it('with review app link', () => {
+      expect(
+        wrapper
+          .find(GlModal)
+          .find('a')
+          .attributes('href'),
+      ).toEqual(propsData.link);
+    });
+  });
+});
