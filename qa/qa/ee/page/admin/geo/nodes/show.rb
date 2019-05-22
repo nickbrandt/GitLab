9674e4947ad43a5734a57bfa6a87a6152f2176fd
Michael Kozono <mkozono@gmail.com>
2019-05-22 16:40:24 +0000

    Fix Geo QA for 11.11.0 RC1

--- qa/qa/ee/page/admin/geo/nodes/show.rb
+++ qa/qa/ee/page/admin/geo/nodes/show.rb
@@ -8,11 +8,11 @@ module QA
           module Nodes
             class Show < QA::Page::Base
               view 'ee/app/views/admin/geo/nodes/index.html.haml' do
-                element :new_node_link, /link_to .*New node/ # rubocop:disable QA/ElementWithPattern
+                element :new_node_link
               end
 
               def new_node!
-                click_link 'New node'
+                click_element :new_node_link
               end
             end
           end
--- qa/qa/specs/features/browser_ui/ee_geo/rename_replication_spec.rb
+++ qa/qa/specs/features/browser_ui/ee_geo/rename_replication_spec.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 module QA
-  # Failure issue: https://gitlab.com/gitlab-org/quality/nightly/issues/99
-  context 'Geo', :orchestrated, :geo, :quarantine do
+  context 'Geo', :orchestrated, :geo do
     describe 'GitLab Geo project rename replication' do
       it 'user renames project' do
         # create the project and push code
