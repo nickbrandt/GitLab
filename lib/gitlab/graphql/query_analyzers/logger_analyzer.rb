8a6fd0014c4e96f50b52aca938aae97ad78810be
charlieablett <cablett@gitlab.com>
2019-05-22 17:13:06 +1200

    Apply reviewer feedback
    - Move complexity and depth calls from `initial_value` to `final_value`
    - Log variables as json
    - test readability
    - code and test structure/styling
    - static query analyzers
    - call `as_json` on `provided_variables`
    - add exception tracking
    - Add changelog file
    - Call analyzers from LoggerAnalyzer
    - Add exception handling

--- lib/gitlab/graphql/query_analyzers/logger_analyzer.rb
+++ lib/gitlab/graphql/query_analyzers/logger_analyzer.rb
@@ -4,68 +4,66 @@ module Gitlab
   module Graphql
     module QueryAnalyzers
       class LoggerAnalyzer
-        # Called before initializing the analyzer.
-        # Returns true to run this analyzer, or false to skip it.
+        COMPLEXITY_ANALYZER = GraphQL::Analysis::QueryComplexity.new { |query, complexity_value| complexity_value }
+        DEPTH_ANALYZER = GraphQL::Analysis::QueryDepth.new { |query, depth_value| depth_value }
+
         def analyze?(query)
           Feature.enabled?(:graphql_logging, default_enabled: true)
         end
 
-        # Called before the visit.
-        # Returns the initial value for `memo`
         def initial_value(query)
-          {
-            time_started: Gitlab::Metrics::System.monotonic_time,
+          variables = process_variables(query.provided_variables)
+          default_initial_values(query).merge({
             query_string: query.query_string,
-            variables: process_variables(query.provided_variables),
-            complexity: nil,
-            depth: nil,
-            duration: nil
-          }
+            variables: variables
+          })
+        rescue => e
+          Gitlab::Sentry.track_exception(e)
+          default_initial_values(query)
         end
 
-        # This is like the `reduce` callback.
-        # The return value is passed to the next call as `memo`
         def call(memo, visit_type, irep_node)
-          memo = set_complexity(memo)
-          set_depth(memo)
+          memo
         end
 
-        # Called when we're done the whole visit.
-        # The return value may be a GraphQL::AnalysisError (or an array of them).
-        # Or, you can use this hook to write to a log, etc
         def final_value(memo)
-          memo[:duration] = "#{duration(memo[:time_started]).round(1)}ms"
-          GraphqlLogger.info(memo.except!(:time_started))
-          memo
+          return if memo.nil?
+
+          analyzers = [COMPLEXITY_ANALYZER, DEPTH_ANALYZER]
+          complexity, depth = GraphQL::Analysis.analyze_query(memo[:query], analyzers)
+
+          memo[:depth] = depth
+          memo[:complexity] = complexity
+          memo[:duration] = duration(memo[:time_started]).round(1)
+
+          GraphqlLogger.info(memo.except!(:time_started, :query))
+        rescue => e
+          Gitlab::Sentry.track_exception(e)
         end
 
         private
 
         def process_variables(variables)
-          if variables.respond_to?(:to_unsafe_h)
-            variables.to_unsafe_h
+          if variables.respond_to?(:to_s)
+            variables.to_s
           else
             variables
           end
         end
 
-        def set_complexity(memo)
-          GraphQL::Analysis::QueryComplexity.new do |query, complexity_value|
-            memo[:complexity] = complexity_value
-          end
-          memo
-        end
-
-        def set_depth(memo)
-          GraphQL::Analysis::QueryDepth.new do |query, depth_value|
-            memo[:depth] = depth_value
-          end
-          memo
-        end
-
         def duration(time_started)
           nanoseconds = Gitlab::Metrics::System.monotonic_time - time_started
           nanoseconds * 1000000
+        end
+
+        def default_initial_values(query)
+          {
+            time_started: Gitlab::Metrics::System.monotonic_time,
+            query_string: nil,
+            query: query,
+            variables: nil,
+            duration: nil
+          }
         end
       end
     end
--- spec/lib/gitlab/graphql/query_analyzers/logger_analyzer_spec.rb
+++ spec/lib/gitlab/graphql/query_analyzers/logger_analyzer_spec.rb
@@ -4,22 +4,6 @@ require 'spec_helper'
 
 describe Gitlab::Graphql::QueryAnalyzers::LoggerAnalyzer do
   subject { described_class.new }
-  let(:query_string) { "abc" }
-  let(:provided_variables) { { a: 1, b: 2, c: 3 } }
-  let!(:now) { Gitlab::Metrics::System.monotonic_time }
-  let(:complexity) { 4 }
-  let(:depth) { 2 }
-  let(:initial_values) do
-    { time_started: now,
-      query_string: query_string,
-      variables: provided_variables,
-      complexity: nil,
-      depth: nil,
-      duration: nil }
-  end
-  before do
-    allow(Gitlab::Metrics::System).to receive(:monotonic_time).and_return(now)
-  end
 
   describe '#analyze?' do
     context 'feature flag disabled' do
@@ -27,40 +11,15 @@ describe Gitlab::Graphql::QueryAnalyzers::LoggerAnalyzer do
         stub_feature_flags(graphql_logging: false)
       end
 
-      specify do
+      it 'disables the analyzer' do
         expect(subject.analyze?(anything)).to be_falsey
       end
     end
 
     context 'feature flag enabled by default' do
-      specify do
+      it 'enables the analyzer' do
         expect(subject.analyze?(anything)).to be_truthy
       end
-    end
-  end
-
-  describe '#initial_value' do
-    it 'assembles a hash with initial values' do
-      query = OpenStruct.new(query_string: query_string, provided_variables: provided_variables)
-
-      expect(subject.initial_value(query)).to eq initial_values
-    end
-  end
-
-  describe '#call' do
-    before do
-      # some statements to fudge the complexity and depth
-    end
-
-    it 'sets the complexity and depth' do
-      expected_hash = { time_started: now,
-                        query_string: query_string,
-                        variables: provided_variables,
-                        complexity: nil,
-                        depth: depth,
-                        duration: complexity }
-
-      expect(subject.call(initial_values, nil, nil)).to eq expected_hash
     end
   end
 end
