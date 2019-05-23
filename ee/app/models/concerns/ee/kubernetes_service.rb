5b877268e0c46f10f20a9c9d2f8afbc03e76497e
Tiger <twatson@gitlab.com>
2019-05-23 12:01:21 -0500

    EE - Move deployments and pod logs to Environment
    This enables rollout statuses, deployment boards and pod logs
    for group and project level clusters. Previously there was
    no way to determine which project (and therefore kubernetes
    namespace) to connect to, moving this logic onto Environment
    means the assoicated project can be used to look up the
    correct namespace.

--- ee/app/models/concerns/ee/kubernetes_service.rb
+++ ee/app/models/concerns/ee/kubernetes_service.rb
@@ -6,71 +6,36 @@ module EE
 
     LOGS_LIMIT = 500.freeze
 
-    def rollout_status(environment)
-      result = with_reactive_cache do |data|
-        project = environment.project
-
-        deployments = filter_by_project_environment(data[:deployments], project.full_path_slug, environment.slug)
-        pods = filter_by_project_environment(data[:pods], project.full_path_slug, environment.slug) if data[:pods]&.any?
-
-        legacy_deployments = filter_by_legacy_label(data[:deployments], project.full_path_slug, environment.slug)
-
-        ::Gitlab::Kubernetes::RolloutStatus.from_deployments(*deployments, pods: pods, legacy_deployments: legacy_deployments)
-      end
-      result || ::Gitlab::Kubernetes::RolloutStatus.loading
-    end
-
-    def calculate_reactive_cache
+    def calculate_reactive_cache_for(environment)
       result = super
-      result[:deployments] = read_deployments if result
+      result[:deployments] = read_deployments(environment.deployment_namespace) if result
 
       result
     end
 
-    def reactive_cache_updated
-      super
+    def rollout_status(environment, data)
+      project = environment.project
 
-      if first_project
-        ::Gitlab::EtagCaching::Store.new.tap do |store|
-          store.touch(
-            ::Gitlab::Routing.url_helpers.project_environments_path(first_project, format: :json))
-        end
-      end
+      deployments = filter_by_project_environment(data[:deployments], project.full_path_slug, environment.slug)
+      pods = filter_by_project_environment(data[:pods], project.full_path_slug, environment.slug) if data[:pods]&.any?
+
+      legacy_deployments = filter_by_legacy_label(data[:deployments], project.full_path_slug, environment.slug)
+
+      ::Gitlab::Kubernetes::RolloutStatus.from_deployments(*deployments, pods: pods, legacy_deployments: legacy_deployments)
     end
 
-    def read_deployments
-      return [] unless first_project
-
-      kubeclient.get_deployments(namespace: kubernetes_namespace_for(first_project)).as_json
-    rescue KubeException => err
-      raise err unless err.error_code == 404
-
-      []
-    end
-
-    def read_pod_logs(pod_name, container: nil)
-      return [] unless first_project
-
-      kubeclient.get_pod_log(pod_name, kubernetes_namespace_for(first_project), container: container, tail_lines: LOGS_LIMIT).as_json
-    rescue ::Kubeclient::HttpError => err
-      raise err unless err.error_code == 404
-
+    def read_pod_logs(pod_name, namespace, container: nil)
+      kubeclient.get_pod_log(pod_name, namespace, container: container, tail_lines: LOGS_LIMIT).as_json
+    rescue Kubeclient::ResourceNotFoundError
       []
     end
 
     private
 
-    ##
-    # TODO: KubernetesService is soon to be removed (https://gitlab.com/gitlab-org/gitlab-ce/issues/39217),
-    # after which we can retrieve the project from the cluster in all cases.
-    #
-    # This currently only works for project-level clusters, this is likely to be fixed as part of
-    # https://gitlab.com/gitlab-org/gitlab-ce/issues/61156, which will require logic to select
-    # a project from a cluster based on an environment.
-    def first_project
-      return project unless respond_to?(:cluster)
-
-      cluster.first_project if cluster.project_type?
+    def read_deployments(namespace)
+      kubeclient.get_deployments(namespace: namespace).as_json
+    rescue Kubeclient::ResourceNotFoundError
+      []
     end
   end
 end
--- ee/spec/models/ee/clusters/platforms/kubernetes_spec.rb
+++ ee/spec/models/ee/clusters/platforms/kubernetes_spec.rb
@@ -1,66 +1,78 @@
 require 'spec_helper'
 
-describe Clusters::Platforms::Kubernetes, :use_clean_rails_memory_store_caching do
+describe Clusters::Platforms::Kubernetes do
   include KubernetesHelpers
-  include ReactiveCachingHelpers
 
-  describe '#calculate_reactive_cache' do
-    subject { service.calculate_reactive_cache }
-
-    let(:cluster) { create(:cluster, :project, enabled: true, platform_kubernetes: service) }
+  describe '#rollout_status' do
     let(:service) { create(:cluster_platform_kubernetes, :configured) }
-    let(:namespace) { cluster.kubernetes_namespace_for(cluster.first_project) }
+    let(:environment) { create(:environment) }
+    let(:cache_data) { Hash(deployments: deployments, pods: pods) }
+    let(:pods) { [kube_pod] }
+    let(:deployments) { [kube_deployment] }
+    let(:legacy_deployments) { [kube_deployment] }
 
-    context 'when kubernetes responds with valid pods and deployments' do
-      before do
-        stub_kubeclient_pods(namespace)
-        stub_kubeclient_deployments(namespace)
-      end
+    subject { service.rollout_status(environment, cache_data) }
 
-      it { is_expected.to eq(pods: [kube_pod], deployments: [kube_deployment]) }
-
-      context 'on a cluster that is not project level' do
-        let(:cluster) { create(:cluster, :group, platform_kubernetes: service) }
-
-        it { is_expected.to eq(pods: [], deployments: []) }
-      end
+    before do
+      allow(service).to receive(:filter_by_project_environment).with(pods, any_args).and_return(pods)
+      allow(service).to receive(:filter_by_project_environment).with(deployments, any_args).and_return(deployments)
+      allow(service).to receive(:filter_by_legacy_label).with(deployments, any_args).and_return(legacy_deployments)
     end
 
-    context 'when kubernetes responds with 404s' do
-      before do
-        stub_kubeclient_pods(namespace, status: 404)
-        stub_kubeclient_deployments(namespace, status: 404)
-      end
+    it 'requests the rollout status' do
+      expect(::Gitlab::Kubernetes::RolloutStatus).to receive(:from_deployments).with(*deployments, pods: pods, legacy_deployments: legacy_deployments)
 
-      it { is_expected.to eq(pods: [], deployments: []) }
+      subject
+    end
+
+    context 'no pod data provided' do
+      let(:pods) { [] }
+
+      it 'requests the rollout status without pod information' do
+        expect(::Gitlab::Kubernetes::RolloutStatus).to receive(:from_deployments).with(*deployments, pods: nil, legacy_deployments: legacy_deployments)
+
+        subject
+      end
     end
   end
 
   describe '#read_pod_logs' do
-    subject { service.read_pod_logs(pod_name) }
-
-    let(:pod_name) { 'foo' }
-    let(:cluster) { create(:cluster, :project, enabled: true, platform_kubernetes: service) }
+    let(:cluster) { create(:cluster, :project, platform_kubernetes: service) }
     let(:service) { create(:cluster_platform_kubernetes, :configured) }
-    let(:namespace) { cluster.kubernetes_namespace_for(cluster.first_project) }
+    let(:pod_name) { 'pod-1' }
+    let(:namespace) { 'app' }
+
+    subject { service.read_pod_logs(pod_name, namespace) }
 
     context 'when kubernetes responds with valid logs' do
       before do
         stub_kubeclient_logs(pod_name, namespace)
       end
 
-      it 'returns logs' do
-        expect(subject.body).to eq("\"Log 1\\nLog 2\\nLog 3\"")
+      shared_examples 'successful log request' do
+        it { expect(subject.body).to eq("\"Log 1\\nLog 2\\nLog 3\"") }
       end
 
-      context 'on a cluster that is not project level' do
+      context 'on a project level cluster' do
+        let(:cluster) { create(:cluster, :project, platform_kubernetes: service) }
+
+        include_examples 'successful log request'
+      end
+
+      context 'on a group level cluster' do
         let(:cluster) { create(:cluster, :group, platform_kubernetes: service) }
 
-        it { is_expected.to be_empty }
+        include_examples 'successful log request'
+      end
+
+      context 'on an instance level cluster' do
+        let(:cluster) { create(:cluster, :instance, platform_kubernetes: service) }
+
+        include_examples 'successful log request'
       end
     end
 
-    context 'when kubernetes response with 500s' do
+    context 'when kubernetes responds with 500s' do
       before do
         stub_kubeclient_logs(pod_name, namespace, status: 500)
       end
@@ -74,6 +86,63 @@ describe Clusters::Platforms::Kubernetes, :use_clean_rails_memory_store_caching 
       end
 
       it { is_expected.to be_empty }
+    end
+  end
+
+  describe '#calculate_reactive_cache_for' do
+    let(:cluster) { create(:cluster, :project, platform_kubernetes: service) }
+    let(:service) { create(:cluster_platform_kubernetes, :configured) }
+    let(:namespace) { 'app' }
+    let(:environment) { instance_double(Environment, deployment_namespace: namespace) }
+
+    subject { service.calculate_reactive_cache_for(environment) }
+
+    before do
+      allow(service).to receive(:read_pods).and_return([])
+    end
+
+    context 'when kubernetes responds with valid deployments' do
+      before do
+        stub_kubeclient_deployments(namespace)
+      end
+
+      shared_examples 'successful deployment request' do
+        it { is_expected.to include(deployments: [kube_deployment]) }
+      end
+
+      context 'on a project level cluster' do
+        let(:cluster) { create(:cluster, :project, platform_kubernetes: service) }
+
+        include_examples 'successful deployment request'
+      end
+
+      context 'on a group level cluster' do
+        let(:cluster) { create(:cluster, :group, platform_kubernetes: service) }
+
+        include_examples 'successful deployment request'
+      end
+
+      context 'on an instance level cluster' do
+        let(:cluster) { create(:cluster, :instance, platform_kubernetes: service) }
+
+        include_examples 'successful deployment request'
+      end
+    end
+
+    context 'when kubernetes responds with 500s' do
+      before do
+        stub_kubeclient_deployments(namespace, status: 500)
+      end
+
+      it { expect { subject }.to raise_error(::Kubeclient::HttpError) }
+    end
+
+    context 'when kubernetes responds with 404s' do
+      before do
+        stub_kubeclient_deployments(namespace, status: 404)
+      end
+
+      it { is_expected.to include(deployments: []) }
     end
   end
 end
