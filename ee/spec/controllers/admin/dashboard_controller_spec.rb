da70aa07229c8bd2bff6f6ded102204fd32f86bb
Małgorzata Ksionek <meksionek@gmail.com>
2019-05-24 13:10:29 +0200

    Move ee-specific parts from Admin::DashboardController

---
+++ ee/spec/controllers/admin/dashboard_controller_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Admin::DashboardController do
+  describe '#index' do
+    it "allows an admin user to access the page" do
+      sign_in(create(:user, :admin))
+
+      get :index
+
+      expect(response).to have_gitlab_http_status(200)
+    end
+
+    it "does not allow an auditor user to access the page" do
+      sign_in(create(:user, :auditor))
+
+      get :index
+
+      expect(response).to have_gitlab_http_status(404)
+    end
+
+    it "does not allow a regular user to access the page" do
+      sign_in(create(:user))
+
+      get :index
+
+      expect(response).to have_gitlab_http_status(404)
+    end
+  end
+end
