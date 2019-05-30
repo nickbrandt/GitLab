8afdc2c969bdc1d590c11ecd709cbf442d41059b
Mark Florian <mflorian@gitlab.com>
2019-05-30 16:14:03 +0800

    Add Dependency List table and row components
    This forms part of the [Dependency List MVC][1].
    
    These are the main components of the feature, as they present all
    dependency-related information to the user.
    
    [1]: https://gitlab.com/gitlab-org/gitlab-ee/issues/10075

---
+++ ee/app/assets/javascripts/dependencies/components/dependencies_table_row.vue
@@ -0,0 +1,55 @@
+<script>
+import { GlSkeletonLoading } from '@gitlab/ui';
+
+export default {
+  name: 'DependenciesTableRow',
+  components: {
+    GlSkeletonLoading,
+  },
+  props: {
+    dependency: {
+      type: Object,
+      required: false,
+      default: null,
+    },
+    isLoading: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="gl-responsive-table-row p-2">
+    <gl-skeleton-loading
+      v-if="isLoading"
+      :lines="1"
+      class="d-flex flex-column justify-content-center"
+    />
+    <template v-else>
+      <div class="table-section section-20 section-wrap">
+        <div class="table-mobile-header" role="rowheader">{{ s__('Dependencies|Component') }}</div>
+        <div class="table-mobile-content">{{ dependency.name }}</div>
+      </div>
+
+      <div class="table-section section-15">
+        <div class="table-mobile-header" role="rowheader">{{ s__('Dependencies|Version') }}</div>
+        <div class="table-mobile-content">{{ dependency.version }}</div>
+      </div>
+
+      <div class="table-section section-20 section-wrap">
+        <div class="table-mobile-header" role="rowheader">{{ s__('Dependencies|Packager') }}</div>
+        <div class="table-mobile-content">{{ dependency.packager }}</div>
+      </div>
+
+      <div class="table-section flex-grow-1 section-wrap">
+        <div class="table-mobile-header" role="rowheader">{{ s__('Dependencies|Location') }}</div>
+        <div class="table-mobile-content">
+          <a :href="dependency.location.blob_path">{{ dependency.location.path }}</a>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
---
+++ ee/spec/frontend/dependencies/components/dependencies_table_row_spec.js
@@ -0,0 +1,56 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import DependenciesTableRow from 'ee/dependencies/components/dependencies_table_row.vue';
+import { makeDependency } from './utils';
+
+describe('DependenciesTableRow component', () => {
+  let wrapper;
+
+  const factory = (props = {}) => {
+    const localVue = createLocalVue();
+
+    wrapper = shallowMount(localVue.extend(DependenciesTableRow), {
+      localVue,
+      sync: false,
+      propsData: { ...props },
+    });
+  };
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe('when passed no props', () => {
+    beforeEach(() => {
+      factory();
+    });
+
+    it('matches the snapshot', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+  });
+
+  describe('when loading', () => {
+    beforeEach(() => {
+      factory({
+        isLoading: true,
+      });
+    });
+
+    it('matches the snapshot', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+  });
+
+  describe('when a dependency is loaded', () => {
+    beforeEach(() => {
+      factory({
+        isLoading: false,
+        dependency: makeDependency(),
+      });
+    });
+
+    it('matches the snapshot', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+  });
+});
