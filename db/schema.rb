83d78687d5812ae5e1847db56fdf48e684848c90
Stan Hu <stanhu@gmail.com>
2019-05-29 21:30:04 -0700

    Fix db/schema.rb ordering

--- db/schema.rb
+++ db/schema.rb
@@ -219,9 +219,9 @@ ActiveRecord::Schema.define(version: 20190529142545) do
     t.boolean "elasticsearch_limit_indexing", default: false, null: false
     t.integer "default_project_creation", default: 2, null: false
     t.string "geo_node_allowed_ips", default: "0.0.0.0/0, ::/0"
+    t.boolean "dns_rebinding_protection_enabled", default: true, null: false
     t.index ["custom_project_templates_group_id"], name: "index_application_settings_on_custom_project_templates_group_id", using: :btree
     t.index ["file_template_project_id"], name: "index_application_settings_on_file_template_project_id", using: :btree
-    t.boolean "dns_rebinding_protection_enabled", default: true, null: false
     t.index ["usage_stats_set_by_user_id"], name: "index_application_settings_on_usage_stats_set_by_user_id", using: :btree
   end
 
