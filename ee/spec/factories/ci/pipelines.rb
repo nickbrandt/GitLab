adb1ec1955f10d4623da30efca997e258f89313f
Tetiana Chupryna <tchupryna@gitlab.com>
2019-06-01 23:40:08 +0300

    Add traits for jobs with reports

--- ee/spec/factories/ci/pipelines.rb
+++ ee/spec/factories/ci/pipelines.rb
@@ -7,19 +7,21 @@ FactoryBot.define do
       config_source :webide_source
     end
 
+    %i[sast codequality dependency_scanning container_scanning dast performance license_management].each do |report_type|
+      trait "with_#{report_type}_report".to_sym do
+        status :success
+
+        after(:build) do |pipeline, evaluator|
+          pipeline.builds << build(:ee_ci_build, report_type, pipeline: pipeline, project: pipeline.project)
+        end
+      end
+    end
+
     trait :with_dependency_list_report do
       status :success
 
       after(:build) do |pipeline, evaluator|
         pipeline.builds << build(:ee_ci_build, :dependency_list, pipeline: pipeline, project: pipeline.project)
-      end
-    end
-
-    trait :with_license_management_report do
-      status :success
-
-      after(:build) do |pipeline, evaluator|
-        pipeline.builds << build(:ee_ci_build, :license_management, pipeline: pipeline, project: pipeline.project)
       end
     end
 
