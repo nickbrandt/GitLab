aefa6568715e326574ce1acfd956c64169639d09
Brett Walker <bwalker@gitlab.com>
2019-06-03 08:37:56 +0000

    Add support for querying epics with GraphQL
    wrapped into a Group query

---
+++ ee/spec/graphql/types/permission_types/epic_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Types::PermissionTypes::Epic do
+  it do
+    expected_permissions = [:read_epic, :read_epic_iid, :update_epic, :destroy_epic,
+                            :admin_epic, :create_epic, :create_note, :award_emoji]
+
+    expected_permissions.each do |permission|
+      expect(described_class).to have_graphql_field(permission)
+    end
+  end
+end
---
+++ ee/spec/presenters/epic_issue_presenter_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe EpicIssuePresenter do
+  include Gitlab::Routing.url_helpers
+
+  let(:user)         { create(:user) }
+  let(:group)        { create(:group) }
+  let(:project)      { create(:project, group: group) }
+  let(:epic)         { create(:epic, group: group)}
+  let(:issue)        { create(:issue, project: project) }
+  let!(:epic_issue)  { create(:epic_issue, issue: issue, epic: epic) }
+  let(:target_issue) { epic.issues_readable_by(user).first }
+  let(:presenter)    { described_class.new(target_issue, current_user: user) }
+
+  before do
+    stub_licensed_features(epics: true)
+    group.add_developer(user)
+  end
+
+  describe '#group_epic_issue_path' do
+    it 'returns correct path' do
+      expect(presenter.group_epic_issue_path(user)).to eq "/groups/#{group.name}/-/epics/#{epic.iid}/issues/#{target_issue.epic_issue_id}"
+    end
+
+    it 'returns nil without proper permission' do
+      unauth_user = create(:user)
+
+      expect(presenter.group_epic_issue_path(unauth_user)).to be_nil
+    end
+  end
+end
