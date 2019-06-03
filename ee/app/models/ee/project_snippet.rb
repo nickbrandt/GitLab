83482288084085d8af7bc9d5901c0fcab8de2dc8
Markus Koller <mkoller@gitlab.com>
2019-06-03 17:07:40 +0200

    Use bulk-indexing API for project associations
    This greatly speeds up initial indexing by batching requests to the
    Elasticsearch API. For now this only affects the associated records
    of projects, while the projects themselves are still indexed one-by-one.

--- ee/app/models/ee/project_snippet.rb
+++ ee/app/models/ee/project_snippet.rb
@@ -8,6 +8,10 @@ module EE
       document_type 'doc'
       index_name [Rails.application.class.parent_name.downcase, Rails.env].join('-')
       include Elastic::SnippetsSearch
+
+      # FIXME: Re-include to avoid a SystemStackError in elasticsearch-model
+      # https://github.com/elastic/elasticsearch-rails/issues/144
+      include Elasticsearch::Model
     end
   end
 end
