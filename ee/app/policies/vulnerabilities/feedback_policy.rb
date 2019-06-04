90970775946eccac2bdc8e4a1cf430468b6385e9
Avielle Wolfe <aviellewolfe@gmail.com>
2019-06-04 19:07:09 -0400

    Update uses of feedback type enum

--- ee/app/policies/vulnerabilities/feedback_policy.rb
+++ ee/app/policies/vulnerabilities/feedback_policy.rb
@@ -4,9 +4,9 @@ module Vulnerabilities
   class FeedbackPolicy < BasePolicy
     delegate { @subject.project }
 
-    condition(:issue) { @subject.issue? }
-    condition(:merge_request) { @subject.merge_request? }
-    condition(:dismissal) { @subject.dismissal? }
+    condition(:issue) { @subject.issue_type? }
+    condition(:merge_request) { @subject.merge_request_type? }
+    condition(:dismissal) { @subject.dismissal_type? }
 
     rule { issue & ~can?(:create_issue) }.prevent :create_vulnerability_feedback
 
