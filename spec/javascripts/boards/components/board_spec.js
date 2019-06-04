86cebe11b74ae2191412eb963f88ae3980967a4e
Winnie Hellmann <winnie@gitlab.com>
2019-06-04 15:32:23 +0000

    Resolve EE-specific import in board_spec.js

--- spec/javascripts/boards/components/board_spec.js
+++ spec/javascripts/boards/components/board_spec.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import '~/boards/services/board_service';
 import Board from '~/boards/components/board';
 import '~/boards/models/list';
-import { mockBoardService } from 'spec/boards/mock_data';
+import { mockBoardService } from '../mock_data';
 
 describe('Board component', () => {
   let vm;
