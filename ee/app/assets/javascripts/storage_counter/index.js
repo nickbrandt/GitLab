e0191c18c7e1b5b761a7aa12fd65607f5389dd5d
Filipa Lacerda <filipa@gitlab.com>
2019-06-06 19:43:10 +0000

    Updates empty text message
    This commits updates the text in the
    empty state to reflect it being used
    in the user profile and the group settings

---
+++ ee/app/assets/javascripts/storage_counter/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import createDefaultClient from '~/lib/graphql';
+import App from './components/app.vue';
+
+Vue.use(VueApollo);
+
+export default () => {
+  const el = document.getElementById('js-storage-counter-app');
+  const { namespacePath } = el.dataset;
+
+  const apolloProvider = new VueApollo({
+    defaultClient: createDefaultClient(),
+  });
+
+  return new Vue({
+    el,
+    apolloProvider,
+    render(h) {
+      return h(App, {
+        props: {
+          namespacePath,
+        },
+      });
+    },
+  });
+};
---
+++ ee/spec/javascripts/storage_counter/components/project_spec.js
@@ -0,0 +1,65 @@
+import { shallowMount } from '@vue/test-utils';
+import { GlButton } from '@gitlab/ui';
+import Project from 'ee/storage_counter/components/project.vue';
+import ProjectAvatar from '~/vue_shared/components/project_avatar/default.vue';
+import { numberToHumanSize } from '~/lib/utils/number_utils';
+
+let wrapper;
+const data = {
+  id: '8',
+  fullPath: 'h5bp/html5-boilerplate',
+  nameWithNamespace: 'H5bp / Html5 Boilerplate',
+  avatarUrl: null,
+  webUrl: 'http://localhost:3001/h5bp/html5-boilerplate',
+  name: 'Html5 Boilerplate',
+  statistics: {
+    commitCount: 0,
+    storageSize: 1293346,
+    repositorySize: 0,
+    lfsObjectsSize: 0,
+    buildArtifactsSize: 1272375,
+    packagesSize: 0,
+  },
+};
+
+function factory(project) {
+  wrapper = shallowMount(Project, {
+    propsData: {
+      project,
+    },
+  });
+}
+
+describe('Storage Counter project component', () => {
+  beforeEach(() => {
+    factory(data);
+  });
+
+  it('renders project avatar', () => {
+    expect(wrapper.contains(ProjectAvatar)).toBe(true);
+  });
+
+  it('renders project name', () => {
+    expect(wrapper.text()).toContain(data.nameWithNamespace);
+  });
+
+  it('renders formatted storage size', () => {
+    expect(wrapper.text()).toContain(numberToHumanSize(data.statistics.storageSize));
+  });
+
+  describe('toggle row', () => {
+    describe('on click', () => {
+      it('toggles isOpen', () => {
+        expect(wrapper.vm.isOpen).toEqual(false);
+
+        wrapper.find(GlButton).vm.$emit('click');
+
+        expect(wrapper.vm.isOpen).toEqual(true);
+
+        wrapper.find(GlButton).vm.$emit('click');
+
+        expect(wrapper.vm.isOpen).toEqual(false);
+      });
+    });
+  });
+});
