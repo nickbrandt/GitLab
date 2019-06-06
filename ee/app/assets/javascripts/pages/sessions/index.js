597917b64df3de2b212397e83e7eb9f82341aa5b
Ash McKenzie <amckenzie@gitlab.com>
2019-06-06 16:01:55 +1000

    Add trackEvent when accessing the Register tab

---
+++ ee/app/assets/javascripts/pages/sessions/index.js
@@ -0,0 +1,4 @@
+import '~/pages/sessions/index';
+import initSignInRegisterTracking from './sign_in_register_tracking';
+
+document.addEventListener('DOMContentLoaded', initSignInRegisterTracking);
---
+++ ee/app/assets/javascripts/pages/sessions/sign_in_register_tracking.js
@@ -0,0 +1,5 @@
+import Stats from 'ee/stats';
+
+export default () => {
+  Stats.bindTrackableContainer('#signin-container');
+};
