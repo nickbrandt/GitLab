3dcfd7a4ae4144511a5e464d3615ea2cbfbfa4a5
Walmyr Lima <walmyr@gitlab.com>
2019-06-06 15:35:54 +0200

    Change required elements on page object view
    The elements that were set to required can't be required because
    due to the randomness of tests execution, sometimes a previous
    test may change the issue filter, which might hide the elements.
    
    In the other hand, the new required field should always be visible
    after an issue is created.

--- qa/qa/page/project/issue/show.rb
+++ qa/qa/page/project/issue/show.rb
@@ -15,12 +15,12 @@ module QA
           end
 
           view 'app/assets/javascripts/notes/components/comment_form.vue' do
-            element :comment_button, required: true
-            element :comment_input, required: true
+            element :comment_button
+            element :comment_input
           end
 
           view 'app/assets/javascripts/notes/components/discussion_filter.vue' do
-            element :discussion_filter
+            element :discussion_filter, required: true
             element :filter_options
           end
 
