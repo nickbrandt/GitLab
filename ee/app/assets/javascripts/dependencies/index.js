d7f592b83b58c2d49410ca54361649071e07eaa7
Mark Florian <553096-markrian@users.noreply.gitlab.com>
2019-06-07 05:40:21 +0000

    Add Dependency List app component
    This forms part of the [Dependency List MVC][1].
    
    This is the root component of the feature, and composes the table,
    actions and alert components [as designed][1].
    
    [1]: https://gitlab.com/gitlab-org/gitlab-ee/issues/10075

---
+++ ee/app/assets/javascripts/dependencies/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import DependenciesApp from './components/app.vue';
+import createStore from './store';
+
+export default () => {
+  const el = document.querySelector('#js-dependencies-app');
+  const { endpoint, emptyStateSvgPath, documentationPath } = el.dataset;
+
+  const store = createStore();
+
+  return new Vue({
+    el,
+    store,
+    components: {
+      DependenciesApp,
+    },
+    render(createElement) {
+      return createElement(DependenciesApp, {
+        props: {
+          endpoint,
+          emptyStateSvgPath,
+          documentationPath,
+        },
+      });
+    },
+  });
+};
