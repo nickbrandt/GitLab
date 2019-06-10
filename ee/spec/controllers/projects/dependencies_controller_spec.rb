d11a0cbba96c4c85bb4ed5fbcd8d8a89c851c04a
Mark Florian <mflorian@gitlab.com>
2019-06-10 12:09:10 +0800

    Add test for unauthorized user

--- ee/spec/controllers/projects/dependencies_controller_spec.rb
+++ ee/spec/controllers/projects/dependencies_controller_spec.rb
@@ -3,18 +3,15 @@
 require 'spec_helper'
 
 describe Projects::DependenciesController do
-  set(:project) { create(:project, :repository, :public) }
+  set(:project) { create(:project, :repository, :private) }
   set(:user) { create(:user) }
 
   subject { get :show, params: { namespace_id: project.namespace, project_id: project } }
 
-  before do
-    project.add_developer(user)
-  end
-
   describe 'GET show' do
     context 'with authorized user' do
       before do
+        project.add_developer(user)
         sign_in(user)
       end
 
@@ -41,10 +38,28 @@ describe Projects::DependenciesController do
     end
 
     context 'with unauthorized user' do
-      it 'returns 404' do
+      before do
+        sign_in(user)
+      end
+
+      context 'when feature is available' do
+        before do
+          stub_licensed_features(dependency_list: true)
+        end
+
+        it 'returns 404' do
+          subject
+
+          expect(response).to have_gitlab_http_status(404)
+        end
+      end
+    end
+
+    context 'with anonymous user' do
+      it 'returns 302' do
         subject
 
-        expect(response).to have_gitlab_http_status(404)
+        expect(response).to have_gitlab_http_status(302)
       end
     end
   end
