2ee5fa5f42045ce97aeb4ff5ef9693561f428837
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2019-06-11 21:25:38 -0300

    Remove legacy queries to find registries for expired uploads
    This commit removes the legacy to find registries for expired
    uploads by project since we made Foreign Data Wrapper (FDW) a
    hard requirement for Geo on GitLab 12.0.

--- ee/app/finders/geo/expire_uploads_finder.rb
+++ ee/app/finders/geo/expire_uploads_finder.rb
@@ -9,31 +9,9 @@ module Geo
     end
 
     def find_file_registries_uploads(project)
-      if Gitlab::Geo::Fdw.enabled?
-        Gitlab::Geo::Fdw::UploadRegistryQueryBuilder.new
-          .for_model(project)
-          .with_type(UPLOAD_TYPE)
-      else
-        legacy_find_file_registries_uploads(project)
-      end
+      Gitlab::Geo::Fdw::UploadRegistryQueryBuilder.new
+        .for_model(project)
+        .with_type(UPLOAD_TYPE)
     end
-
-    private
-
-    # rubocop:disable CodeReuse/ActiveRecord
-    def legacy_find_file_registries_uploads(project)
-      upload_ids = Upload.for_model(project).pluck_primary_key
-      return Geo::FileRegistry.none if upload_ids.empty?
-
-      values_sql = upload_ids.map { |id| "(#{id})" }.join(',')
-
-      Geo::FileRegistry.joins(<<~SQL)
-        JOIN (VALUES #{values_sql})
-          AS uploads (id)
-          ON uploads.id = file_registry.file_id
-         AND file_registry.file_type='#{UPLOAD_TYPE}'
-      SQL
-    end
-    # rubocop:enable CodeReuse/ActiveRecord
   end
 end
