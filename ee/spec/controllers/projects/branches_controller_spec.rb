e9b8f2e44602ac853dffb414fc0d7df957f625df
Stan Hu <stanhu@gmail.com>
2019-06-12 09:10:40 -0700

    Simplify spec based on review comments

--- ee/spec/controllers/projects/branches_controller_spec.rb
+++ ee/spec/controllers/projects/branches_controller_spec.rb
@@ -3,12 +3,10 @@
 require 'spec_helper'
 
 describe Projects::BranchesController do
-  let(:project) { create(:project, :repository) }
-  let(:user) { create(:user) }
+  set(:project) { create(:project, :repository) }
+  let(:user) { project.owner }
 
   before do
-    project.add_maintainer(user)
-
     allow(project).to receive(:branches).and_return(['master'])
     controller.instance_variable_set(:@project, project)
 
@@ -16,26 +14,26 @@ describe Projects::BranchesController do
   end
 
   describe 'GET #index' do
-    let(:import_state) { create(:import_state, next_execution_timestamp: Time.now, last_update_at: Time.now, last_successful_update_at: Time.now) }
+    context 'for mirrored projects with diverged branch' do
+      render_views
 
-    render_views
+      before do
+        create(:import_state, :mirror, :finished, project: project, last_successful_update_at: Time.now)
+        allow(project.repository).to receive(:diverged_from_upstream?) { true }
+      end
 
-    before do
-      project.update!(mirror: true, import_state: import_state, import_url: 'https://import.url', mirror_user: user)
-      allow(project.repository).to receive(:diverged_from_upstream?) { true }
-    end
+      it 'renders the diverged from upstream partial' do
+        get :index,
+            format: :html,
+            params: {
+              namespace_id: project.namespace,
+              project_id: project,
+              state: 'all'
+            }
 
-    it 'renders the diverged from upstream partial' do
-      get :index,
-          format: :html,
-          params: {
-            namespace_id: project.namespace,
-            project_id: project,
-            state: 'all'
-          }
-
-      expect(controller).to render_template('projects/branches/_diverged_from_upstream')
-      expect(response.body).to match(/diverged from upstream/)
+        expect(controller).to render_template('projects/branches/_diverged_from_upstream')
+        expect(response.body).to match(/diverged from upstream/)
+      end
     end
   end
 end
