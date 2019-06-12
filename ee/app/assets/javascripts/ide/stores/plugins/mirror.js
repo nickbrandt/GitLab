865ad4aae390060bebf4ad97a7a6ece96b9d0448
Paul Slaughter <pslaughter@gitlab.com>
2019-06-12 08:47:31 +0000

    Create IDE terminal sync store module
    **Notes:**
    This interfaces with the `mirror` service and maintains
    a state for it's current status.

--- ee/app/assets/javascripts/ide/stores/plugins/mirror.js
+++
@@ -1,8 +0,0 @@
-import eventHub from '~/ide/eventhub';
-import mirror from '../../lib/mirror';
-
-export default function createMirrorPlugin() {
-  return store => {
-    eventHub.$on('editor.save', () => mirror.upload(store.state));
-  };
-}
