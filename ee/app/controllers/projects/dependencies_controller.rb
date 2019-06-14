350972650c0c5d1bd0af04718036e66fe60ac7ce
Mark Florian <mflorian@gitlab.com>
2019-06-14 14:42:18 +0800

    Disable dependency_list feature by default

--- ee/app/controllers/projects/dependencies_controller.rb
+++ ee/app/controllers/projects/dependencies_controller.rb
@@ -5,7 +5,8 @@ module Projects
     before_action :check_feature_enabled!
 
     def check_feature_enabled!
-      render_404 unless project.feature_available?(:dependency_list)
+      render_404 unless ::Feature.enabled?(:dependency_list, default_enabled: false) &&
+        project.feature_available?(:dependency_list)
     end
   end
 end
--- ee/app/models/ee/ci/build.rb
+++ ee/app/models/ee/ci/build.rb
@@ -81,7 +81,8 @@ module EE
       end
 
       def collect_dependency_list_reports!(dependency_list_report)
-        if project.feature_available?(:dependency_list)
+        flag_enabled = ::Feature.enabled?(:dependency_list, default_enabled: false)
+        if flag_enabled && project.feature_available?(:dependency_list)
           dependency_list = ::Gitlab::Ci::Parsers::Security::DependencyList.new(project, sha)
 
           each_report(::Ci::JobArtifact::DEPENDENCY_LIST_REPORT_FILE_TYPES) do |file_type, blob|
