b08475fd087479544b4fedee22798534a1cf87e4
Patrick Bajao <ebajao@gitlab.com>
2019-06-14 09:00:24 +0800

    Update internal API spec to cover feature

--- ee/spec/requests/api/internal_spec.rb
+++ ee/spec/requests/api/internal_spec.rb
@@ -3,11 +3,12 @@ require 'spec_helper'
 
 describe API::Internal do
   describe "POST /internal/allowed" do
+    set(:user) { create(:user) }
+    set(:key) { create(:key, user: user) }
+    let(:secret_token) { Gitlab::Shell.secret_token }
+
     context "for design repositories" do
-      set(:user) { create(:user) }
       set(:project) { create(:project) }
-      set(:key) { create(:key, user: user) }
-      let(:secret_token) { Gitlab::Shell.secret_token }
       let(:gl_repository) { EE::Gitlab::GlRepository::DESIGN.identifier_for_subject(project) }
 
       it "does not allow access" do
@@ -21,6 +22,49 @@ describe API::Internal do
              })
 
         expect(response).to have_gitlab_http_status(401)
+      end
+    end
+
+    context "project alias" do
+      context "project matches a project alias" do
+        let(:project) { create(:project, :public, :repository) }
+        let(:project_alias) { create(:project_alias, project: project) }
+
+        before do
+          post(
+            api("/internal/allowed"),
+            params: {
+              action: "git-upload-pack",
+              key_id: key.id,
+              project: project_alias.name,
+              protocol: 'ssh',
+              secret_token: secret_token
+            }
+          )
+        end
+
+        it "allows access" do
+          expect(response).to have_gitlab_http_status(200)
+        end
+      end
+
+      context "project doesn't match a project alias" do
+        before do
+          post(
+            api("/internal/allowed"),
+            params: {
+              action: "git-upload-pack",
+              key_id: key.id,
+              project: "some-project",
+              protocol: 'ssh',
+              secret_token: secret_token
+            }
+          )
+        end
+
+        it "does not allow access because project can't be found" do
+          expect(response).to have_gitlab_http_status(404)
+        end
       end
     end
   end
