868bb32a71bc8f2452980f846d1b2c3242bbaf3b
Fatih Acet <acetfatih@gmail.com>
2019-06-14 15:11:56 +0200

    Milestone bulk editing for group issues
    WIP: With this commit we will allow milestone
    bulk editing for group issues. This version will
    only support milestones.

---
+++ app/assets/javascripts/issuable_init_bulk_update_sidebar.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import IssuableBulkUpdateSidebar from './issuable_bulk_update_sidebar';
+import issuableBulkUpdateActions from './issuable_bulk_update_actions';
+
+export default {
+  bulkUpdateSidebar: null,
+
+  init(prefixId) {
+    const userCanBulkUpdate = $('.issues-bulk-update').length > 0;
+    const alreadyInitialized = Boolean(this.bulkUpdateSidebar);
+
+    if (userCanBulkUpdate && !alreadyInitialized) {
+      issuableBulkUpdateActions.init({ prefixId });
+
+      this.bulkUpdateSidebar = new IssuableBulkUpdateSidebar();
+    }
+  }
+}
--- app/assets/javascripts/pages/groups/issues/index.js
+++ app/assets/javascripts/pages/groups/issues/index.js
@@ -1,10 +1,14 @@
 import projectSelect from '~/project_select';
 import initFilteredSearch from '~/pages/search/init_filtered_search';
+import issuableInitBulkUpdateSidebar from '~/issuable_init_bulk_update_sidebar';
 import { FILTERED_SEARCH } from '~/pages/constants';
 import IssuableFilteredSearchTokenKeys from 'ee_else_ce/filtered_search/issuable_filtered_search_token_keys';
 
 document.addEventListener('DOMContentLoaded', () => {
   IssuableFilteredSearchTokenKeys.addExtraTokensForIssues();
+
+  // FIXME: Hardcoded prefix
+  issuableInitBulkUpdateSidebar.init('issue_');
 
   initFilteredSearch({
     page: FILTERED_SEARCH.ISSUES,
