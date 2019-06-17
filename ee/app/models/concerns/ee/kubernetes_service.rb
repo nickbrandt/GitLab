84f8862b1ed580d9122ac055d6dfa716e6847872
Thong Kuah <tkuah@gitlab.com>
2019-06-17 15:58:35 +1200

    Select deployments that only has the app label
    As Auto DevOps deployments both label and annotations, that will be
    included in the deploy board so need to warn at all.

--- ee/app/models/concerns/ee/kubernetes_service.rb
+++ ee/app/models/concerns/ee/kubernetes_service.rb
@@ -13,7 +13,7 @@ module EE
         deployments = filter_by_project_environment(data[:deployments], project.full_path_slug, environment.slug)
         pods = filter_by_project_environment(data[:pods], project.full_path_slug, environment.slug) if data[:pods]&.any?
 
-        legacy_deployments = filter_by_label(data[:deployments], { app: environment.slug })
+        legacy_deployments = filter_by_legacy_label(data[:deployments], project.full_path_slug, environment.slug)
 
         ::Gitlab::Kubernetes::RolloutStatus.from_deployments(*deployments, pods: pods, legacy_deployments: legacy_deployments)
       end
