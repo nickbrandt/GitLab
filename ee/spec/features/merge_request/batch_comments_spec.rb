6e16468feb5e92d3fffe677338151fae51e83eb7
Michel Engelen <michel.engelen@diva-e.com>
2019-06-17 10:29:37 +0200

    fixed problems with the rspec tests

--- ee/spec/features/merge_request/batch_comments_spec.rb
+++ ee/spec/features/merge_request/batch_comments_spec.rb
@@ -137,25 +137,25 @@ describe 'Merge request > Batch comments', :js do
       end
     end
 
-    context 'discussion is unresolved' do
+    context 'thread is unresolved' do
       let!(:active_discussion) { create(:diff_note_on_merge_request, noteable: merge_request, project: project).to_discussion }
 
       before do
         visit_diffs
       end
 
-      it 'publishes comment right away and resolves the discussion' do
+      it 'publishes comment right away and resolves the thread' do
         expect(active_discussion.resolved?).to eq(false)
 
         write_reply_to_discussion(button_text: 'Add comment now', resolve: true)
 
         page.within '.line-resolve-all-container' do
-          expect(page).to have_content('1/1 discussion resolved')
+          expect(page).to have_content('1/1 thread resolved')
           expect(page).to have_selector('.line-resolve-btn.is-active')
         end
       end
 
-      it 'publishes review and resolves the discussion' do
+      it 'publishes review and resolves the thread' do
         expect(active_discussion.resolved?).to eq(false)
 
         write_reply_to_discussion(resolve: true)
@@ -168,7 +168,7 @@ describe 'Merge request > Batch comments', :js do
         wait_for_requests
 
         page.within '.line-resolve-all-container' do
-          expect(page).to have_content('1/1 discussion resolved')
+          expect(page).to have_content('1/1 thread resolved')
           expect(page).to have_selector('.line-resolve-btn.is-active')
         end
 
@@ -176,7 +176,7 @@ describe 'Merge request > Batch comments', :js do
       end
     end
 
-    context 'discussion is resolved' do
+    context 'thread is resolved' do
       let!(:active_discussion) { create(:diff_note_on_merge_request, :resolved, noteable: merge_request, project: project).to_discussion }
 
       before do
@@ -187,18 +187,18 @@ describe 'Merge request > Batch comments', :js do
         page.find('.js-diff-comment-avatar').click
       end
 
-      it 'publishes comment right away and unresolves the discussion' do
+      it 'publishes comment right away and unresolves the thread' do
         expect(active_discussion.resolved?).to eq(true)
 
         write_reply_to_discussion(button_text: 'Add comment now', unresolve: true)
 
         page.within '.line-resolve-all-container' do
-          expect(page).to have_content('0/1 discussion resolved')
+          expect(page).to have_content('0/1 thread resolved')
           expect(page).to have_selector('.line-resolve-btn.is-disabled')
         end
       end
 
-      it 'publishes review and unresolves the discussion' do
+      it 'publishes review and unresolves the thread' do
         expect(active_discussion.resolved?).to eq(true)
 
         wait_for_requests
@@ -213,7 +213,7 @@ describe 'Merge request > Batch comments', :js do
         wait_for_requests
 
         page.within '.line-resolve-all-container' do
-          expect(page).to have_content('0/1 discussion resolved')
+          expect(page).to have_content('0/1 thread resolved')
           expect(page).to have_selector('.line-resolve-btn.is-disabled')
         end
 
@@ -259,11 +259,11 @@ def write_reply_to_discussion(button_text: 'Start a review', text: 'Line is wron
     fill_in('note_note', with: text)
 
     if resolve
-      page.check('Resolve discussion')
+      page.check('Resolve thread')
     end
 
     if unresolve
-      page.check('Unresolve discussion')
+      page.check('Unresolve thread')
     end
 
     click_button(button_text)
