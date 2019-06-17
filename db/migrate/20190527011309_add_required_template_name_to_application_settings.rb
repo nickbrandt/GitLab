mehulagg/gitlab_test

Implement required templates
Implements the backend logic for required templates.

db/migrate/20190527011309_add_required_template_name_to_application_settings.rb
@@ -1,0 +1,15 @@
+# frozen_string_literal: true
+
+# See http://doc.gitlab.com/ce/development/migration_style_guide.html
+# for more information on how to write migrations for GitLab.
+
+class AddRequiredTemplateNameToApplicationSettings < ActiveRecord::Migration[5.1]
+  include Gitlab::Database::MigrationHelpers
+
+  # Set this constant to true if this migration requires downtime.
+  DOWNTIME = false
+
+  def change
+    add_column :application_settings, :required_ci_template, :string, null: true
+  end
+end

db/schema.rb
@@ -225,6 +225,7 @@ ActiveRecord::Schema.define(version: 20190603124955) do
     t.integer "elasticsearch_replicas", default: 1, null: false
     t.text "encrypted_lets_encrypt_private_key"
     t.text "encrypted_lets_encrypt_private_key_iv"
+    t.string "required_ci_template"
     t.boolean "dns_rebinding_protection_enabled", default: true, null: false
     t.index ["custom_project_templates_group_id"], name: "index_application_settings_on_custom_project_templates_group_id", using: :btree
     t.index ["file_template_project_id"], name: "index_application_settings_on_file_template_project_id", using: :btree

ee/lib/gitlab/ci/config/required/processor.rb
@@ -1,0 +1,44 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module Ci
+    class Config
+      module Required
+        class Processor
+          RequiredError = Class.new(StandardError)
+
+          def initialize(config)
+            @config = config
+          end
+
+          def perform
+            return @config unless ::License.feature_available?(:required_template_inclusion)
+            return @config unless required_ci_template_name
+
+            merge_required_template
+          end
+
+          def merge_required_template
+            raise RequiredError, "Required template '#{required_ci_template_name}' not found!" unless required_template
+
+            @config.deep_merge(required_template_hash)
+          end
+
+          private
+
+          def required_template_hash
+            YAML.safe_load(required_template.content).deep_symbolize_keys
+          end
+
+          def required_template
+            ::TemplateFinder.build(:gitlab_ci_ymls, nil, name: required_ci_template_name).execute
+          end
+
+          def required_ci_template_name
+            ::Gitlab::CurrentSettings.current_application_settings.required_ci_template
+          end
+        end
+      end
+    end
+  end
+end

ee/spec/lib/gitlab/ci/config/required/processor_spec.rb
@@ -1,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Gitlab::Ci::Config::Required::Processor do
+  subject { described_class.new(config).perform }
+
+  let(:config) { { image: 'ruby:2.5.3' } }
+
+  context 'when feature is available' do
+    before do
+      stub_licensed_features(required_template_inclusion: true)
+
+      stub_application_setting(required_ci_template: required_ci_template_name)
+    end
+
+    context 'when template is set' do
+      context 'when template can not be found' do
+        let(:required_ci_template_name) { 'invalid_template_name' }
+
+        it 'raises an error' do
+          expect { subject }.to raise_error(Gitlab::Ci::Config::Required::Processor::RequiredError)
+        end
+      end
+
+      context 'when template can be found' do
+        let(:required_ci_template_name) { 'Android' }
+
+        it 'merges the template content with the config' do
+          expect(subject).to include(image: 'openjdk:8-jdk')
+        end
+      end
+    end
+
+    context 'when template is not set' do
+      let(:required_ci_template_name) { nil }
+
+      it 'returns the unmodified config' do
+        expect(subject).to eq(config)
+      end
+    end
+  end
+
+  context 'when feature is not available' do
+    before do
+      stub_licensed_features(required_template_inclusion: false)
+    end
+
+    it 'returns the unmodified config' do
+      expect(subject).to eq(config)
+    end
+  end
+end

nickbrandt/GitLab

db/migrate/20190527011309_add_required_template_name_to_application_settings.rb
@@ -1,0 +1,15 @@
+# frozen_string_literal: true
+
+# See http://doc.gitlab.com/ce/development/migration_style_guide.html
+# for more information on how to write migrations for GitLab.
+
+class AddRequiredTemplateNameToApplicationSettings < ActiveRecord::Migration[5.1]
+  include Gitlab::Database::MigrationHelpers
+
+  # Set this constant to true if this migration requires downtime.
+  DOWNTIME = false
+
+  def change
+    add_column :application_settings, :required_instance_ci_template, :string, null: true
+  end
+end

db/schema.rb
@@ -225,6 +225,7 @@ ActiveRecord::Schema.define(version: 20190611161641) do
     t.integer "elasticsearch_replicas", default: 1, null: false
     t.text "encrypted_lets_encrypt_private_key"
     t.text "encrypted_lets_encrypt_private_key_iv"
+    t.string "required_instance_ci_template"
     t.boolean "dns_rebinding_protection_enabled", default: true, null: false
     t.boolean "default_project_deletion_protection", default: false, null: false
     t.boolean "lock_memberships_to_ldap", default: false, null: false

ee/lib/gitlab/ci/config/required/processor.rb
@@ -1,0 +1,44 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module Ci
+    class Config
+      module Required
+        class Processor
+          RequiredError = Class.new(StandardError)
+
+          def initialize(config)
+            @config = config
+          end
+
+          def perform
+            return @config unless ::License.feature_available?(:required_ci_templates)
+            return @config unless required_ci_template_name
+
+            merge_required_template
+          end
+
+          def merge_required_template
+            raise RequiredError, "Required template '#{required_ci_template_name}' not found!" unless required_template
+
+            @config.deep_merge(required_template_hash)
+          end
+
+          private
+
+          def required_template_hash
+            Gitlab::Config::Loader::Yaml.new(required_template.content).load!
+          end
+
+          def required_template
+            ::TemplateFinder.build(:gitlab_ci_ymls, nil, name: required_ci_template_name).execute
+          end
+
+          def required_ci_template_name
+            ::Gitlab::CurrentSettings.current_application_settings.required_instance_ci_template
+          end
+        end
+      end
+    end
+  end
+end

ee/spec/lib/gitlab/ci/config/required/processor_spec.rb
@@ -1,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Gitlab::Ci::Config::Required::Processor do
+  subject { described_class.new(config).perform }
+
+  let(:config) { { image: 'ruby:2.5.3' } }
+
+  context 'when feature is available' do
+    before do
+      stub_licensed_features(required_ci_templates: true)
+
+      stub_application_setting(required_instance_ci_template: required_ci_template_name)
+    end
+
+    context 'when template is set' do
+      context 'when template can not be found' do
+        let(:required_ci_template_name) { 'invalid_template_name' }
+
+        it 'raises an error' do
+          expect { subject }.to raise_error(Gitlab::Ci::Config::Required::Processor::RequiredError)
+        end
+      end
+
+      context 'when template can be found' do
+        let(:required_ci_template_name) { 'Android' }
+
+        it 'merges the template content with the config' do
+          expect(subject).to include(image: 'openjdk:8-jdk')
+        end
+      end
+    end
+
+    context 'when template is not set' do
+      let(:required_ci_template_name) { nil }
+
+      it 'returns the unmodified config' do
+        expect(subject).to eq(config)
+      end
+    end
+  end
+
+  context 'when feature is not available' do
+    before do
+      stub_licensed_features(required_ci_templates: false)
+    end
+
+    it 'returns the unmodified config' do
+      expect(subject).to eq(config)
+    end
+  end
+end