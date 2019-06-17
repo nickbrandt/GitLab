b7e260a01ab8ae79e0bcb34942e1b0917cde56cb
Justin Boyson <jboyson@gitlab.com>
2019-06-17 20:43:45 +0000

    Fix broken filter by approvers
    This was fixed in CE, and the EE reference no longer makes sense.
    So remove the reference to EE since it is basically a straight
    passthrough.
    And remove the ee version of visual_token_value.js since it is not used
    anymore.

---
+++ ee/app/assets/javascripts/filtered_search/constants.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+import { TOKEN_TYPES as CE_TOKEN_TYPES } from '~/filtered_search/constants';
+
+export const TOKEN_TYPES = [...CE_TOKEN_TYPES, 'approver'];
