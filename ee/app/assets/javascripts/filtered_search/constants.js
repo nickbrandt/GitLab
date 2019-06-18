a47fb242a47d03e1d21b7309cbd2f70aa37f0a9f
Arun Kumar Mohan <arunmohandm@gmail.com>
2019-06-18 14:44:44 +0000

    Rename TOKEN_TYPES to USER_TOKEN_TYPES

--- ee/app/assets/javascripts/filtered_search/constants.js
+++ ee/app/assets/javascripts/filtered_search/constants.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
-import { TOKEN_TYPES as CE_TOKEN_TYPES } from '~/filtered_search/constants';
+import { USER_TOKEN_TYPES as CE_USER_TOKEN_TYPES } from '~/filtered_search/constants';
 
-export const TOKEN_TYPES = [...CE_TOKEN_TYPES, 'approver'];
+export const USER_TOKEN_TYPES = [...CE_USER_TOKEN_TYPES, 'approver'];
