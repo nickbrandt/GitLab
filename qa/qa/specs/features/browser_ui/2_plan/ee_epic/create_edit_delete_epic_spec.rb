95a4159cf3c4602a64da18458bac56c1bdca160b
Walmyr Lima <walmyr@gitlab.com>
2019-06-18 18:03:26 +0200

    Split test that does too much into multiple tests
    The new tests are also more isolated.
    
    One of the new tests is commented out since it depends on an open
    issue.

--- qa/qa/specs/features/browser_ui/2_plan/ee_epic/create_edit_delete_epic_spec.rb
+++ qa/qa/specs/features/browser_ui/2_plan/ee_epic/create_edit_delete_epic_spec.rb
@@ -1,84 +1,107 @@
 # frozen_string_literal: true
 
 module QA
-  # Failure issue: https://gitlab.com/gitlab-org/quality/staging/issues/19
-  context 'Plan', :quarantine do
-    describe 'Epics Creation' do
-      before(:all) do
+  context 'Plan' do
+    describe 'Epics Management' do
+      before do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
-        Page::Main::Login.act { sign_in_using_credentials }
+        Page::Main::Login.perform(&:sign_in_using_credentials)
       end
 
-      it 'user creates, edits, deletes epic' do
-        issue = Resource::Issue.fabricate_via_api! do |issue|
-          issue.title = 'Issue for epics tests'
-          issue.labels = []
-        end
-
+      it 'creates, edits, and deletes an epic' do
+        epic_title = 'Epic created via GUI'
         epic = EE::Resource::Epic.fabricate_via_browser_ui! do |epic|
-          epic.group = issue.project.group
-          epic.title = 'My First Epic'
+          epic.title = epic_title
         end
 
-        expect(page).to have_content('My First Epic')
+        expect(page).to have_content(epic_title)
 
-        # Edit Epics
-        EE::Page::Group::Epic::Show.act { click_edit_button }
-
+        EE::Page::Group::Epic::Show.perform(&:click_edit_button)
         EE::Page::Group::Epic::Edit.perform do |edit_page|
-          edit_page.set_description('My Edited Epic Description')
-          edit_page.set_title('My Edited Epic')
+          edited_title = 'Epic edited via GUI'
+          edit_page.set_title(edited_title)
           edit_page.save_changes
-          expect(edit_page).to have_content('My Edited Epic')
+
+          expect(edit_page).to have_content(edited_title)
         end
 
-        # Add/Remove Issues to/from Epics
+        epic.visit!
+        EE::Page::Group::Epic::Show.perform(&:click_edit_button)
+        EE::Page::Group::Epic::Edit.perform do |edit_page|
+          edit_page.delete_epic
+
+          expect(edit_page).to have_content('The epic was successfully deleted')
+        end
+      end
+
+      it 'adds/removes issue to/from epic' do
+        create_resources_and_visit_epic_page
+
         EE::Page::Group::Epic::Show.perform do |show_page|
-          show_page.add_issue_to_epic(issue.web_url)
+          show_page.add_issue_to_epic(@issue.web_url)
           expect(show_page).to have_content('added issue')
-          expect(show_page).to have_content('My Edited Epic')
 
           show_page.remove_issue_from_epic
           expect(show_page).to have_content('removed issue')
         end
+      end
 
-        # Comment on Epics
-        EE::Page::Group::Epic::Show.act { add_comment_to_epic('My Epic Comments') }
+      it 'comments on epic' do
+        create_resources_and_visit_epic_page
 
-        expect(page).to have_content('My Epic Comments')
+        comment = 'My Epic Comment'
+        EE::Page::Group::Epic::Show.perform do |show_page|
+          show_page.add_comment_to_epic(comment)
+        end
 
-        # Add Issue to Epic using quick actions
-        issue.visit!
+        expect(page).to have_content(comment)
+      end
+
+      it 'closes and reopens an epic' do
+        create_resources_and_visit_epic_page
+
+        EE::Page::Group::Epic::Show.perform(&:close_reopen_epic)
+
+        expect(page).to have_content('Closed')
+
+        EE::Page::Group::Epic::Show.perform(&:close_reopen_epic)
+
+        expect(page).to have_content('Open')
+      end
+
+      it 'adds/removes issue to/from epic using quick actions' do
+        create_resources_and_visit_epic_page
+
+        @issue.visit!
 
         Page::Project::Issue::Show.perform do |show_page|
           show_page.wait_for_related_issues_to_load
-          show_page.comment("/epic #{epic.web_url}")
+          show_page.comment("/epic #{@epic_web_url}")
           show_page.comment("/remove_epic")
           expect(show_page).to have_content('removed from epic')
         end
 
-        epic.visit!
+        page.visit @epic_web_url
 
         expect(page).to have_content('added issue', count: 2)
         expect(page).to have_content('removed issue', count: 2)
+      end
 
-        # Close Epic
-        EE::Page::Group::Epic::Show.act { close_reopen_epic }
-
-        expect(page).to have_content('Closed')
-
-        # Reopen Epic
-        EE::Page::Group::Epic::Show.act { close_reopen_epic }
-
-        expect(page).to have_content('Open')
-
-        # Delete Epics
-        EE::Page::Group::Epic::Show.act { click_edit_button }
-
-        EE::Page::Group::Epic::Edit.perform do |edit_page|
-          edit_page.delete_epic
-          expect(edit_page).to have_content('The epic was successfully deleted')
+      def create_resources_and_visit_epic_page
+        @issue = Resource::Issue.fabricate_via_api! do |issue|
+          issue.title = 'Issue created via API'
+          issue.labels = []
         end
+
+        @epic = EE::Resource::Epic.fabricate_via_api! do |epic|
+          epic.group = @issue.project.group.id
+          epic.title = 'Epic created via API'
+        end
+
+        # Epics have no web_url exposed via the API, and so we build it here.
+        # Related issue: https://gitlab.com/gitlab-org/gitlab-ee/issues/11241
+        @epic_web_url = "#{@issue.project.group.web_url}/-/epics/1"
+        page.visit @epic_web_url
       end
     end
   end
