2aaef05788f3497d0d7675619da866c7f79bb02d
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2019-06-19 17:42:33 -0300

    Remove legacy query to find job artifacts that sync failed
    This commit removes the legacy queries to find job artifacts
    that sync failed since we made Foreign Data Wrapper (FDW) a
    hard requirement for Geo on GitLab 12.0.

--- ee/app/finders/geo/job_artifact_registry_finder.rb
+++ ee/app/finders/geo/job_artifact_registry_finder.rb
@@ -11,7 +11,7 @@ module Geo
     end
 
     def count_failed
-      job_artifacts_failed.count
+      registries_for_job_artifacts.merge(Geo::JobArtifactRegistry.failed).count
     end
 
     def count_synced_missing_on_primary
@@ -109,14 +109,6 @@ module Geo
           .job_artifacts
           .inner_join_job_artifact_registry
           .syncable
-      end
-    end
-
-    def job_artifacts_failed
-      if use_legacy_queries_for_selective_sync?
-        legacy_finder.job_artifacts_failed
-      else
-        registries_for_job_artifacts.merge(Geo::JobArtifactRegistry.failed)
       end
     end
 
