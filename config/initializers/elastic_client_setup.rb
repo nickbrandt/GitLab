33471d9ffbc88f7d4dec2c2512fb9ce8bb4e895c
Mark Chao <mchao@gitlab.com>
2019-06-21 17:30:27 +0800

    ES: proxy object setup for elasticsearch-rails
    We no longer include Elasticsearch::Model, which would set up
    those included modules. Therefore we have to do this manually.
    
    Allow __transform to handle versions

--- config/initializers/elastic_client_setup.rb
+++ config/initializers/elastic_client_setup.rb
@@ -5,9 +5,40 @@
 require 'gitlab/current_settings'
 
 Gitlab.ee do
+  require 'elasticsearch/model'
+
+  ### Modified from elasticsearch-model/lib/elasticsearch/model.rb
+
+  [
+    Elasticsearch::Model::Client::ClassMethods,
+    Elasticsearch::Model::Naming::ClassMethods,
+    Elasticsearch::Model::Indexing::ClassMethods,
+    Elasticsearch::Model::Searching::ClassMethods
+  ].each do |mod|
+    Elasticsearch::Model::Proxy::ClassMethodsProxy.include mod
+  end
+
+  [
+    Elasticsearch::Model::Client::InstanceMethods,
+    Elasticsearch::Model::Naming::InstanceMethods,
+    Elasticsearch::Model::Indexing::InstanceMethods,
+    Elasticsearch::Model::Serializing::InstanceMethods
+  ].each do |mod|
+    Elasticsearch::Model::Proxy::InstanceMethodsProxy.include mod
+  end
+
+  Elasticsearch::Model::Proxy::InstanceMethodsProxy.class_eval <<-CODE, __FILE__, __LINE__ + 1
+    def as_indexed_json(options={})
+      target.respond_to?(:as_indexed_json) ? target.__send__(:as_indexed_json, options) : super
+    end
+  CODE
+
+  ### Monkey patches
+
   Elasticsearch::Model::Response::Records.prepend GemExtensions::Elasticsearch::Model::Response::Records
   Elasticsearch::Model::Adapter::Multiple::Records.prepend GemExtensions::Elasticsearch::Model::Adapter::Multiple::Records
   Elasticsearch::Model::Indexing::InstanceMethods.prepend GemExtensions::Elasticsearch::Model::Indexing::InstanceMethods
+  Elasticsearch::Model::Adapter::ActiveRecord::Importing.prepend GemExtensions::Elasticsearch::Model::Adapter::ActiveRecord::Importing
 
   module Elasticsearch
     module Model
