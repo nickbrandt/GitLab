4c68f71e5add73cb3cac6cc1776cd98cd5581bce
egrieff <egrieff@gitlab.com>
2019-06-21 15:46:45 +0100

    Remove comment and add auth for issues controller

--- ee/app/controllers/groups/issues_controller.rb
+++ ee/app/controllers/groups/issues_controller.rb
@@ -2,4 +2,13 @@
 
 class Groups::IssuesController < Groups::ApplicationController
   include IssuableActions
+
+  before_action :authorize_admin_group!
+  before_action :verify_group_issues_bulk_edit_enabled!
+
+  private
+
+  def verify_group_issues_bulk_edit_enabled!
+    render_404 unless @group.feature_available?(:group_issues_bulk_edit)
+  end
 end
