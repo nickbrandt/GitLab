1d2434ba80a45a01cfbeea2a41a2dbfb71dfd4d8
Walmyr Lima <walmyr@gitlab.com>
2019-06-24 11:39:36 +0200

    Quarantine ee geo end-to-end test

--- qa/qa/specs/features/browser_ui/ee_geo/attachment_replication_spec.rb
+++ qa/qa/specs/features/browser_ui/ee_geo/attachment_replication_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module QA
-  context 'Geo', :orchestrated, :geo do
+  # https://gitlab.com/gitlab-org/quality/nightly/issues/109
+  context 'Geo', :orchestrated, :geo, :quarantine do
     describe 'GitLab Geo attachment replication' do
       let(:file_to_attach) { File.absolute_path(File.join('spec', 'fixtures', 'banana_sample.gif')) }
 
