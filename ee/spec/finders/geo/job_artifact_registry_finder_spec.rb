d32c03d356815c2e4c607b1b51c280f2f8d65cd8
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2019-06-24 19:57:14 -0300

    Refactor specs for file registry finders
    This commit removes code duplicate between these specs

--- ee/spec/finders/geo/job_artifact_registry_finder_spec.rb
+++ ee/spec/finders/geo/job_artifact_registry_finder_spec.rb
@@ -563,25 +563,5 @@ describe Geo::JobArtifactRegistryFinder, :geo_fdw do
     end
   end
 
-  it 'responds to file registry finder methods' do
-    file_registry_finder_methods = %i{
-      syncable
-      count_syncable
-      count_synced
-      count_failed
-      count_synced_missing_on_primary
-      count_registry
-      find_unsynced
-      find_migrated_local
-      find_retryable_failed_registries
-      find_retryable_synced_missing_on_primary_registries
-    }
-
-    file_registry_finder_methods.each do |method|
-      expect(subject).to respond_to(method)
-    end
-  end
-
-  include_examples 'counts all the things'
-  include_examples 'finds all the things'
+  it_behaves_like 'a file registry finder'
 end
