30ba66cebdf735806561767d95d789085c9e07a2
Małgorzata Ksionek <mksionek@gitlab.com>
2019-06-25 16:39:49 +0200

    Add specs for group fetching

--- spec/lib/gitlab/cycle_analytics/issue_stage_spec.rb
+++ spec/lib/gitlab/cycle_analytics/issue_stage_spec.rb
@@ -36,4 +36,40 @@ describe Gitlab::CycleAnalytics::IssueStage do
       expect(result.map { |event| event[:title] }).to contain_exactly(issue_1.title, issue_2.title, issue_3.title)
     end
   end
+
+  context 'when group is given' do
+    let(:user) { create(:user) }
+    let(:group) { create(:group) }
+    let(:project_2) { create(:project, group: group) }
+    let(:project_3) { create(:project, group: group) }
+    let!(:issue_2_1) { create(:issue, project: project_2, created_at: 90.minutes.ago) }
+    let!(:issue_2_2) { create(:issue, project: project_3, created_at: 60.minutes.ago) }
+    let!(:issue_2_3) { create(:issue, project: project_2, created_at: 60.minutes.ago) }
+    let(:stage) { described_class.new(options: { from: 2.days.ago, current_user: user, group: group }) }
+
+    before do
+      group.add_owner(user)
+      issue_2_1.metrics.update!(first_associated_with_milestone_at: 60.minutes.ago)
+      issue_2_2.metrics.update!(first_added_to_board_at: 30.minutes.ago)
+    end
+
+    describe '#group_median' do
+      around do |example|
+        Timecop.freeze { example.run }
+      end
+
+      it 'counts median from issues with metrics' do
+        expect(stage.group_median).to eq(ISSUES_MEDIAN)
+      end
+    end
+
+    describe '#events' do
+      it 'exposes merge requests that close issues' do
+        result = stage.events
+
+        expect(result.count).to eq(2)
+        expect(result.map { |event| event[:title] }).to contain_exactly(issue_2_1.title, issue_2_2.title)
+      end
+    end
+  end
 end
