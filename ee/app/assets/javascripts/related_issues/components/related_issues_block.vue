0f3c3c97329d1c082310ca0aa85baa3eefb57aa2
Martin Hanzel <mhanzel@gitlab.com>
2019-06-27 06:12:22 +0000

    Make related issue cards conform to the UI design
    Original design available here:
    https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/12493

--- ee/app/assets/javascripts/related_issues/components/related_issues_block.vue
+++ ee/app/assets/javascripts/related_issues/components/related_issues_block.vue
@@ -260,20 +260,21 @@ export default {
               event-namespace="relatedIssue"
               @relatedIssueRemoveRequest="$emit('relatedIssueRemoveRequest', $event)"
             >
-              <issue-weight
-                v-if="issue.weight"
-                slot="weight"
-                :weight="issue.weight"
-                class="item-weight d-flex align-items-center ml-2 mr-0"
-                tag-name="span"
-              />
-              <issue-due-date
-                v-if="issue.due_date"
-                slot="dueDate"
-                :date="issue.due_date"
-                tooltip-placement="top"
-                css-class="item-due-date d-flex align-items-center ml-2 mr-0"
-              />
+              <span v-if="issue.weight" slot="weight" class="order-md-1">
+                <issue-weight
+                  :weight="issue.weight"
+                  class="item-weight d-flex align-items-center"
+                  tag-name="span"
+                />
+              </span>
+
+              <span v-if="issue.due_date" slot="dueDate" class="order-md-1">
+                <issue-due-date
+                  :date="issue.due_date"
+                  tooltip-placement="top"
+                  css-class="item-due-date d-flex align-items-center"
+                />
+              </span>
             </related-issuable-item>
           </li>
         </ul>
