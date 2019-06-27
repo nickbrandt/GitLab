36c8d608c7f4533afa65d3c15c895475f3f9c0fb
Avielle Wolfe <aviellewolfe@gmail.com>
2019-06-27 19:09:39 -0400

    Spike: fill out project security controller

--- ee/spec/controllers/projects/security/vulnerabilities_controller_spec.rb
+++ ee/spec/controllers/projects/security/vulnerabilities_controller_spec.rb
@@ -40,6 +40,20 @@ describe Projects::Security::VulnerabilitiesController do
       context 'when user has developer access' do
         context 'when no page request' do
           it "returns a list of vulnerabilities" do
+            user = create(:user)
+            project = create(:project)
+            project.add_developer(user)
+            create_vulnerabilities(3, project)
+
+            sign_in(user)
+            stub_licensed_features(security_dashboard: true)
+
+            get :index, params: { namespace_id: project.namespace, project_id: project }, format: :json
+
+            expect(response).to have_gitlab_http_status(200)
+            expect(json_response).to be_an(Array)
+            expect(json_response.length).to eq 3
+            expect(response).to match_response_schema('vulnerabilities/occurrence_list', dir: 'ee')
           end
         end
 
@@ -77,5 +91,25 @@ describe Projects::Security::VulnerabilitiesController do
     #     it 'returns empty history if there are no vulnerabilities within last 90 days' do
     #     it 'returns filtered history if filters are enabled' do
     # end
+  end
+end
+
+def create_vulnerabilities(count, project, options = {})
+  report_type = options[:report_type] || :sast
+  pipeline = create(:ci_pipeline, :success, project: project)
+  vulnerabilities = create_list(:vulnerabilities_occurrence, count, report_type: report_type, pipelines: [pipeline], project: project)
+  return vulnerabilities unless options[:with_feedback]
+
+  vulnerabilities.each do |occurrence|
+    create(:vulnerability_feedback, report_type, :dismissal,
+            pipeline: pipeline,
+            project: project_dev,
+            project_fingerprint: occurrence.project_fingerprint)
+
+    create(:vulnerability_feedback, report_type, :issue,
+            pipeline: pipeline,
+            issue: create(:issue, project: project),
+            project: project_dev,
+            project_fingerprint: occurrence.project_fingerprint)
   end
 end
