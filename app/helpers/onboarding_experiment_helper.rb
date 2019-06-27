9e05fcb8dc2984eab71a5e4d1c65d3c85d1e86b5
Ash McKenzie <amckenzie@gitlab.com>
2019-06-27 13:48:04 +1000

    Add new OnboardingExperimentHelper modules
    OnboardingExperimentHelpers take care of
    determining if the current_user should or should
    not see the new onboarding feature.

---
+++ app/helpers/onboarding_experiment_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module OnboardingExperimentHelper
+  def allow_access_to_onboarding?
+    ::Gitlab.com? && Feature.enabled?(:user_onboarding)
+  end
+end
+
+OnboardingExperimentHelper.prepend(EE::OnboardingExperimentHelper)
