3f8a817d0064e758d4533e2bab133765d89e8ed2
Michael Kozono <mkozono@gmail.com>
2019-06-28 12:48:37 -0700

    Check LFS object file integrity after download

--- ee/spec/lib/gitlab/geo/lfs_transfer_spec.rb
+++ ee/spec/lib/gitlab/geo/lfs_transfer_spec.rb
@@ -5,7 +5,7 @@ describe Gitlab::Geo::LfsTransfer do
 
   set(:primary_node) { create(:geo_node, :primary) }
   set(:secondary_node) { create(:geo_node) }
-  set(:lfs_object) { create(:lfs_object, :with_file) }
+  set(:lfs_object) { create(:lfs_object, :with_file, :correct_oid) }
 
   subject do
     described_class.new(lfs_object)
@@ -28,7 +28,7 @@ describe Gitlab::Geo::LfsTransfer do
 
     context 'when the HTTP response is successful' do
       it 'returns a successful result' do
-        content = SecureRandom.random_bytes(10)
+        content = lfs_object.file.read
         size = content.bytesize
         expect(FileUtils).to receive(:mv).with(anything, lfs_object.file.path).and_call_original
         response = double(:response, success?: true)
@@ -93,6 +93,18 @@ describe Gitlab::Geo::LfsTransfer do
 
         expect(result.success).to eq(false)
         expect(result.bytes_downloaded).to eq(0)
+      end
+    end
+
+    context 'when the checksum of the downloaded file does not match' do
+      it 'returns a failed result' do
+        bad_content = 'corrupted!!!'
+        response = double(:response, success?: true)
+        expect(Gitlab::HTTP).to receive(:get).and_yield(bad_content).and_return(response)
+
+        result = subject.download_from_primary
+
+        expect_result(result, success: false, bytes_downloaded: bad_content.bytesize, primary_missing_file: false)
       end
     end
   end
