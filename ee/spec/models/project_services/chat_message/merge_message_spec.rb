d5cfc015217bc49a96c7475324eb400f9f114a60
Timothy Jones <tim@zmthy.net>
2019-07-03 14:06:35 -0400

    Handle all approval messages in merge chat message

--- ee/spec/models/project_services/chat_message/merge_message_spec.rb
+++ ee/spec/models/project_services/chat_message/merge_message_spec.rb
@@ -27,14 +27,53 @@ RSpec.describe ChatMessage::MergeMessage do
     }
   end
 
-  context 'approval' do
+  context 'approved' do
     before do
       args[:object_attributes][:action] = 'approved'
     end
 
-    it 'returns a message regarding approval of merge requests' do
+    it 'returns a message regarding completed approval of merge requests' do
       expect(subject.pretext).to eq(
         'Test User (test.user) approved <http://somewhere.com/merge_requests/100|!100 *Merge Request title*> '\
+        'in <http://somewhere.com|project_name>')
+      expect(subject.attachments).to be_empty
+    end
+  end
+
+  context 'unapproved' do
+    before do
+      args[:object_attributes][:action] = 'unapproved'
+    end
+
+    it 'returns a message regarding revocation of completed approval of merge requests' do
+      expect(subject.pretext).to eq(
+        'Test User (test.user) unapproved <http://somewhere.com/merge_requests/100|!100 *Merge Request title*> '\
+        'in <http://somewhere.com|project_name>')
+      expect(subject.attachments).to be_empty
+    end
+  end
+
+  context 'approval' do
+    before do
+      args[:object_attributes][:action] = 'approval'
+    end
+
+    it 'returns a message regarding added approval of merge requests' do
+      expect(subject.pretext).to eq(
+        'Test User (test.user) added their approval to <http://somewhere.com/merge_requests/100|!100 *Merge Request title*> '\
+        'in <http://somewhere.com|project_name>')
+      expect(subject.attachments).to be_empty
+    end
+  end
+
+  context 'unapproval' do
+    before do
+      args[:object_attributes][:action] = 'unapproval'
+    end
+
+    it 'returns a message regarding revoking approval of merge requests' do
+      expect(subject.pretext).to eq(
+        'Test User (test.user) removed their approval from <http://somewhere.com/merge_requests/100|!100 *Merge Request title*> '\
         'in <http://somewhere.com|project_name>')
       expect(subject.attachments).to be_empty
     end
