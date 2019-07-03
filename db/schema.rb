4242d239ecf9877a296170d1a09e87e7a240ecb1
Gabriel Mazetto <gabriel@gitlab.com>
2019-07-03 16:09:26 +0000

    [EE Backport] Add Grafana to Admin > Monitoring menu when enabled

--- db/schema.rb
+++ db/schema.rb
@@ -224,8 +224,10 @@ ActiveRecord::Schema.define(version: 20190628185004) do
     t.string "required_instance_ci_template"
     t.boolean "dns_rebinding_protection_enabled", default: true, null: false
     t.boolean "default_project_deletion_protection", default: false, null: false
+    t.boolean "grafana_enabled", default: false, null: false
     t.boolean "lock_memberships_to_ldap", default: false, null: false
     t.boolean "time_tracking_limit_to_hours", default: false, null: false
+    t.string "grafana_url", default: "/-/grafana", null: false
     t.index ["custom_project_templates_group_id"], name: "index_application_settings_on_custom_project_templates_group_id", using: :btree
     t.index ["file_template_project_id"], name: "index_application_settings_on_file_template_project_id", using: :btree
     t.index ["usage_stats_set_by_user_id"], name: "index_application_settings_on_usage_stats_set_by_user_id", using: :btree
