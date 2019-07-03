4e2de04ab9ad75e5c4ca302ec060cf93dd26168b
egrieff <egrieff@gitlab.com>
2019-07-03 12:23:29 +0100

    Remove support for merge request bulk update

--- ee/config/routes/group.rb
+++ ee/config/routes/group.rb
@@ -78,12 +78,6 @@ constraints(::Constraints::GroupUrlConstrainer.new) do
       end
     end
 
-    resources :merge_requests, only: [] do
-      collection do
-        post :bulk_update
-      end
-    end
-
     resources :todos, only: [:create]
     resources :boards, only: [:create, :update, :destroy] do
       collection do
