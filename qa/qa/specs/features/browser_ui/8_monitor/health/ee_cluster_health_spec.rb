61265d9b32e7c4849dc3e73ae1f56265e8682c57
Peter Leitzen <pleitzen@gitlab.com>
2019-07-05 02:12:49 +0000

    Add end-to-end test showing Cluster health graphs
    This test may take some time as we install Kubernetes, Prometheus and
    wait for the "Cluster health" graphs to appear.

---
+++ qa/qa/specs/features/browser_ui/8_monitor/health/ee_cluster_health_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module QA
+  context 'Monitor' do
+    describe 'Cluster health graphs', :orchestrated, :kubernetes do
+      before do
+        @cluster = Service::KubernetesCluster.new.create!
+      end
+
+      after do
+        @cluster&.remove!
+      end
+
+      it 'installs Kubernetes and Prometheus' do
+        login
+
+        create_project
+
+        create_kubernetes_cluster
+
+        verify_cluster_health_graphs
+      end
+
+      private
+
+      def login
+        Runtime::Browser.visit(:gitlab, Page::Main::Login)
+        Page::Main::Login.perform(&:sign_in_using_credentials)
+      end
+
+      def create_project
+        @project = Resource::Project.fabricate_via_api! do |p|
+          p.name = 'cluster-health'
+          p.description = 'Cluster health'
+        end
+      end
+
+      def create_kubernetes_cluster
+        Resource::KubernetesCluster.fabricate_via_browser_ui! do |c|
+          c.project = @project
+          c.cluster = @cluster
+          c.install_helm_tiller = true
+          c.install_prometheus = true
+        end
+      end
+
+      def verify_cluster_health_graphs
+        Page::Project::Operations::Kubernetes::Show.perform do |cluster|
+          cluster.refresh
+          expect(cluster).to have_cluster_health_title
+
+          cluster.wait_for_cluster_health
+        end
+      end
+    end
+  end
+end
