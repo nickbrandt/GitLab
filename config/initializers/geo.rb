fb85fd9d31f4fa5d30e780491d89dca6725da624
Robert Speicher <rspeicher@gmail.com>
2019-07-08 14:33:35 -0500

    Wrap geo initializer in `ee` block

--- config/initializers/geo.rb
+++ config/initializers/geo.rb
@@ -1,13 +1,15 @@
-if File.exist?(Rails.root.join('config/database_geo.yml'))
-  Rails.application.configure do
-    config.geo_database = config_for(:database_geo)
+Gitlab.ee do
+  if File.exist?(Rails.root.join('config/database_geo.yml'))
+    Rails.application.configure do
+      config.geo_database = config_for(:database_geo)
+    end
   end
-end
 
-begin
-  if Gitlab::Geo.connected? && Gitlab::Geo.primary?
-    Gitlab::Geo.current_node&.update_clone_url!
+  begin
+    if Gitlab::Geo.connected? && Gitlab::Geo.primary?
+      Gitlab::Geo.current_node&.update_clone_url!
+    end
+  rescue => e
+    warn "WARNING: Unable to check/update clone_url_prefix for Geo: #{e}"
   end
-rescue => e
-  warn "WARNING: Unable to check/update clone_url_prefix for Geo: #{e}"
 end
