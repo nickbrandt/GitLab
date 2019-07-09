de7ad8d33862ba53a0c4195bb8f2184b19f14a3e
Ryan Cobb <rcobb@gitlab.com>
2019-07-09 15:46:44 -0600

    Clean up specs

--- ee/app/models/ee/prometheus_metric_enums.rb
+++ ee/app/models/ee/prometheus_metric_enums.rb
@@ -10,7 +10,8 @@ module EE
       override :groups
       def groups
         super.merge(
-          # Start at 100 to avoid collisions with CE values
+          # Start at -100 to avoid collisions with CE values
+          # built-in groups
           cluster_health: -100
         )
       end
@@ -19,6 +20,7 @@ module EE
       def group_details
         super.merge(
           # keys can collide with CE values! please ensure you are not redefining a key that already exists in app/models/prometheus_metric_enums.rb#group_details
+          # built-in groups
           cluster_health: {
             group_title: _('Cluster Health'),
             required_metrics: %w(container_memory_usage_bytes container_cpu_usage_seconds_total),
