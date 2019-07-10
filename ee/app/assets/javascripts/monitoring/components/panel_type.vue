e77210b03cc9d336788efbfd00ca8a54cfaa8349
Jose Ivan Vargas <jvargas@gitlab.com>
2019-07-10 21:12:36 +0000

    Add support for additional panel types
    This introduces the single stat chart component aside from
    the area chart component

---
+++ ee/app/assets/javascripts/monitoring/components/panel_type.vue
@@ -0,0 +1,39 @@
+<script>
+import CePanelType from '~/monitoring/components/panel_type.vue';
+import AlertWidget from './alert_widget.vue';
+import CustomMetricsFormFields from 'ee/custom_metrics/components/custom_metrics_form_fields.vue';
+
+export default {
+  components: {
+    AlertWidget,
+    CustomMetricsFormFields,
+  },
+  extends: CePanelType,
+  props: {
+    alertsEndpoint: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    prometheusAlertsAvailable: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      allAlerts: {},
+    };
+  },
+  methods: {
+    setAlerts(alertPath, alertAttributes) {
+      if (alertAttributes) {
+        this.$set(this.allAlerts, alertPath, alertAttributes);
+      } else {
+        this.$delete(this.allAlerts, alertPath);
+      }
+    },
+  },
+};
+</script>
