c112a15466d9754cc4c9adf21ee3f4df159e3ceb
Mark Florian <553096-markrian@users.noreply.gitlab.com>
2019-07-10 09:47:03 +0000

    Display vulnerabilities in Dependency List
    This adds a "Status" column to the Dependency List, and allows the user
    to see basic information about the vulnerabilities detected for each of
    a project's dependencies.
    
    This contributes towards the larger [feature][1] for adding dependency
    scanning results to the Dependency List, behind the
    `dependency_list_vulnerabilities` feature flag.
    
    [1]: https://gitlab.com/gitlab-org/gitlab-ee/issues/10077

--- ee/app/assets/javascripts/dependencies/components/constants.js
+++ ee/app/assets/javascripts/dependencies/components/constants.js
@@ -6,3 +6,8 @@ export const DANGER_ALERT_CLASS = 'danger_message';
 
 export const WARNING_TEXT_CLASS = 'text-warning-900';
 export const DANGER_TEXT_CLASS = 'text-danger-900';
+
+// Limit the number of vulnerabilities to display so as to avoid jank.
+// In practice, this limit will probably never be reached, since the
+// largest number of vulnerabilities we've seen one dependency have is 20.
+export const MAX_DISPLAYED_VULNERABILITIES_PER_DEPENDENCY = 50;
