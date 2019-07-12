a5c9e82f9a4290979bf2fa96cfc81ad41e97a02f
Winnie Hellmann <winnie@gitlab.com>
2019-07-12 19:59:07 +0000

    Move functions for multiple boards in BoardService to core

--- ee/app/assets/javascripts/boards/services/board_service.js
+++ ee/app/assets/javascripts/boards/services/board_service.js
@@ -1,25 +1,7 @@
-/* eslint-disable class-methods-use-this */
-
 import BoardService from '~/boards/services/board_service';
 import boardsStore from '~/boards/stores/boards_store';
 
 export default class BoardServiceEE extends BoardService {
-  allBoards() {
-    return boardsStore.allBoards();
-  }
-
-  recentBoards() {
-    return boardsStore.recentBoards();
-  }
-
-  createBoard(board) {
-    return boardsStore.createBoard(board);
-  }
-
-  deleteBoard({ id }) {
-    return boardsStore.deleteBoard({ id });
-  }
-
   static updateWeight(endpoint, weight = null) {
     return boardsStore.updateWeight(endpoint, weight);
   }
