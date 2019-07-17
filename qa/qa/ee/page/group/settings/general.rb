8359fece4cf496c10aafabcbefcd6ae847568c67
Sanad Liaquat <sliaquat@gitlab.com>
2019-07-17 13:46:10 +0500

    Fix & Un-quarantine project templates spec
    Also Add steps to validate that template selection succeeded.

--- qa/qa/ee/page/group/settings/general.rb
+++ qa/qa/ee/page/group/settings/general.rb
@@ -15,6 +15,14 @@ module QA
               element :save_changes_button
             end
 
+            def current_custom_project_template
+              expand_section(:custom_project_templates)
+
+              within_element(:custom_project_template_select) do
+                current_selection
+              end
+            end
+
             def choose_custom_project_template(path)
               expand_section(:custom_project_templates)
 
