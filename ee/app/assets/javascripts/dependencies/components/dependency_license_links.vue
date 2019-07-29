2f07fec84bb9a9de617d8e88e0d90a23b136cb4d
Dave Pisek <dpisek@gitlab.com>
2019-07-29 23:35:06 +1000

    Add dependencies licenses to security dashboard
    This commit adds the frontend changes needed to display dependencies
    licenses on the security dashboard table.
    
    It also adds spacing in between the ependencies-table columns.

---
+++ ee/app/assets/javascripts/dependencies/components/dependency_license_links.vue
@@ -0,0 +1,104 @@
+<script>
+import { uniqueId } from 'underscore';
+
+import { sprintf, s__ } from '~/locale';
+import { GlButton, GlLink, GlModal, GlModalDirective, GlIntersperse } from '@gitlab/ui';
+
+// If there are more licenses than this count, a counter will be displayed for the remaining licenses
+// eg.: VISIBLE_LICENSE_COUNT = 2; licenses = ['MIT', 'GNU', 'GPL'] -> 'MIT, GNU and 1 more'
+const VISIBLE_LICENSES_COUNT = 2;
+const MODAL_ID_PREFIX = 'dependency-license-link-modal-';
+
+export default {
+  components: {
+    GlIntersperse,
+    GlButton,
+    GlLink,
+    GlModal,
+  },
+  directives: {
+    GlModalDirective,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    licenses: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    allLicenses() {
+      return Array.isArray(this.licenses) ? this.licenses : [];
+    },
+    visibleLicenses() {
+      return this.allLicenses.slice(0, VISIBLE_LICENSES_COUNT);
+    },
+    remainingLicensesCount() {
+      return this.allLicenses.length - VISIBLE_LICENSES_COUNT;
+    },
+    hasLicensesInModal() {
+      return this.remainingLicensesCount > 0;
+    },
+    lastSeparator() {
+      return ` ${s__('SeriesFinalConjunction|and')} `;
+    },
+    modalId() {
+      return uniqueId(MODAL_ID_PREFIX);
+    },
+    modalActionText() {
+      return s__('Modal|Close');
+    },
+    modalButtonText() {
+      const { remainingLicensesCount } = this;
+      return sprintf(s__('Dependencies|%{remainingLicensesCount} more'), {
+        remainingLicensesCount,
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <gl-intersperse :last-separator="lastSeparator" class="js-license-links-license-list">
+      <span
+        v-for="license in visibleLicenses"
+        :key="license.name"
+        class="js-license-links-license-list-item"
+      >
+        <gl-link v-if="license.url" :href="license.url" target="_blank">{{ license.name }}</gl-link>
+        <template v-else>{{ license.name }}</template>
+      </span>
+      <gl-button
+        v-if="hasLicensesInModal"
+        v-gl-modal-directive="modalId"
+        variant="link"
+        class="align-baseline js-license-links-modal-trigger"
+        >{{ modalButtonText }}</gl-button
+      >
+    </gl-intersperse>
+    <div class="js-license-links-modal">
+      <gl-modal
+        v-if="hasLicensesInModal"
+        :title="title"
+        :modal-id="modalId"
+        :ok-title="modalActionText"
+        ok-only
+        ok-variant="secondary"
+      >
+        <h5>{{ __('Licenses') }}</h5>
+        <ul class="list-unstyled">
+          <li v-for="license in licenses" :key="license.name" class="js-license-links-modal-item">
+            <gl-link v-if="license.url" :href="license.url" target="_blank">{{
+              license.name
+            }}</gl-link>
+            <span v-else>{{ license.name }}</span>
+          </li>
+        </ul>
+      </gl-modal>
+    </div>
+  </div>
+</template>
--- ee/spec/frontend/dependencies/components/utils.js
+++ ee/spec/frontend/dependencies/components/utils.js
@@ -6,6 +6,7 @@ export const makeDependency = (changes = {}) => ({
     blob_path: '/a-group/a-project/blob/da39a3ee5e6b4b0d3255bfef95601890afd80709/yarn.lock',
     path: 'yarn.lock',
   },
+  licenses: [],
   ...changes,
 });
 
