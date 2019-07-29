9712ab5a6ee02b2e220369aa23fa34dfef84f882
Avielle Wolfe <awolfe@gitlab.com>
2019-07-29 11:59:31 +0000

    Extract SecurityDashboardsPermissions module
    This commit extracts the controller logic for security dashboard
    permissions so that we can reuse it in the upcoming project security
    dashboard controllers.

--- ee/spec/controllers/groups/security/vulnerabilities_controller_spec.rb
+++ ee/spec/controllers/groups/security/vulnerabilities_controller_spec.rb
@@ -11,46 +11,13 @@ describe Groups::Security::VulnerabilitiesController do
     let(:vulnerable_params) { { group_id: group } }
   end
 
-  before do
-    sign_in(user)
+  it_behaves_like SecurityDashboardsPermissions do
+    let(:vulnerable) { group }
+    let(:security_dashboard_action) { get :index, params: { group_id: group }, format: :json }
   end
 
-  describe 'access for all actions' do
-    context 'when security dashboard feature is disabled' do
-      it 'returns 404' do
-        stub_licensed_features(security_dashboard: false)
-
-        get :index, params: { group_id: group }, format: :json
-
-        expect(response).to have_gitlab_http_status(404)
-      end
-    end
-
-    context 'when security dashboard feature is enabled' do
-      before do
-        stub_licensed_features(security_dashboard: true)
-      end
-
-      context 'when user has guest access' do
-        it 'denies access' do
-          group.add_guest(user)
-
-          get :index, params: { group_id: group }, format: :json
-
-          expect(response).to have_gitlab_http_status(403)
-        end
-      end
-
-      context 'when user has developer access' do
-        it 'grants access' do
-          group.add_developer(user)
-
-          get :index, params: { group_id: group }, format: :json
-
-          expect(response).to have_gitlab_http_status(200)
-        end
-      end
-    end
+  before do
+    sign_in(user)
   end
 
   describe 'GET index.json' do
