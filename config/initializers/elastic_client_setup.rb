405d161329ed7be17d2a01296c65784abc1207c5
Mark Chao <mchao@gitlab.com>
2019-07-31 15:28:31 +0800

    ES: Fix client loading and refreshing
    Ensure various `client` method always provide up-to-date client

--- config/initializers/elastic_client_setup.rb
+++ config/initializers/elastic_client_setup.rb
@@ -39,43 +39,8 @@ Gitlab.ee do
   Elasticsearch::Model::Adapter::Multiple::Records.prepend GemExtensions::Elasticsearch::Model::Adapter::Multiple::Records
   Elasticsearch::Model::Indexing::InstanceMethods.prepend GemExtensions::Elasticsearch::Model::Indexing::InstanceMethods
   Elasticsearch::Model::Adapter::ActiveRecord::Importing.prepend GemExtensions::Elasticsearch::Model::Adapter::ActiveRecord::Importing
-
-  module Elasticsearch
-    module Model
-      module Client
-        # This mutex is only used to synchronize *creation* of a new client, so
-        # all including classes can share the same client instance
-        CLIENT_MUTEX = Mutex.new
-
-        cattr_accessor :cached_client
-        cattr_accessor :cached_config
-
-        module ClassMethods
-          # Override the default ::Elasticsearch::Model::Client implementation to
-          # return a client configured from application settings. All including
-          # classes will use the same instance, which is refreshed automatically
-          # if the settings change.
-          #
-          # _client is present to match the arity of the overridden method, where
-          # it is also not used.
-          #
-          # @return [Elasticsearch::Transport::Client]
-          def client(_client = nil)
-            store = ::Elasticsearch::Model::Client
-
-            store::CLIENT_MUTEX.synchronize do
-              config = Gitlab::CurrentSettings.elasticsearch_config
-
-              if store.cached_client.nil? || config != store.cached_config
-                store.cached_client = ::Gitlab::Elastic::Client.build(config)
-                store.cached_config = config
-              end
-            end
-
-            store.cached_client
-          end
-        end
-      end
-    end
-  end
+  Elasticsearch::Model::Client::InstanceMethods.prepend GemExtensions::Elasticsearch::Model::Client
+  Elasticsearch::Model::Client::ClassMethods.prepend GemExtensions::Elasticsearch::Model::Client
+  Elasticsearch::Model::ClassMethods.prepend GemExtensions::Elasticsearch::Model::Client
+  Elasticsearch::Model.singleton_class.prepend GemExtensions::Elasticsearch::Model::Client
 end
