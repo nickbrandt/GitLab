9230dcb053cea884e2aa8ce7d6fd27b0176341b1
Małgorzata Ksionek <mksionek@gitlab.com>
2019-07-31 12:24:22 +0200

    Add validations

--- ee/app/models/allowed_email_domain.rb
+++ ee/app/models/allowed_email_domain.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 class AllowedEmailDomain < ApplicationRecord
+  RESERVED_DOMAINS = [
+    '*@gmail.com',
+    '*@yahoo.com',
+    '*@hotmail.com',
+    '*@aol.com',
+    '*@msn.com',
+    '*@hotmail.co.uk',
+   	'*@hotmail.fr'
+  ].freeze
+
+  validates :domain, exclusion: { in: RESERVED_DOMAINS,
+    message: _('The domain you entered is not allowed.') }
+
+  validates :domain, format: { with: /\*\@\w*\.*./,
+    message: _('The domain you entered is not allowed.') }
+
   belongs_to :group, class_name: 'Group', foreign_key: :namespace_id
 end
