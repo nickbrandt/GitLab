d0422e288b33d317867a3384cad1c339fe9df7cb
Gosia Ksionek <mksionek@gitlab.com>
2019-08-01 07:03:34 +0000

    Add controllers changes
    Add new controllers and specs

---
+++ ee/app/controllers/groups/cycle_analytics/events_controller.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+class Groups::CycleAnalytics::EventsController < Groups::ApplicationController
+  include ActionView::Helpers::DateHelper
+  include ActionView::Helpers::TextHelper
+  include CycleAnalyticsParams
+
+  before_action :authorize_group_cycle_analytics!
+
+  def issue
+    render_events(:issue)
+  end
+
+  def plan
+    render_events(:plan)
+  end
+
+  def code
+    render_events(:code)
+  end
+
+  def test
+    render_events(:test)
+  end
+
+  def review
+    render_events(:review)
+  end
+
+  def staging
+    render_events(:staging)
+  end
+
+  def production
+    render_events(:production)
+  end
+
+  private
+
+  def render_events(stage)
+    respond_to do |format|
+      format.html
+      format.json { render json: { events: cycle_analytics[stage].events } }
+    end
+  end
+
+  def cycle_analytics
+    @cycle_analytics ||= ::CycleAnalytics::GroupLevel.new(group: group, options: options(cycle_analytics_params))
+  end
+
+  def cycle_analytics_params
+    return {} unless params[:cycle_analytics].present?
+
+    params[:cycle_analytics].permit(:start_date, :branch_name, project_ids: [])
+  end
+
+  def authorize_group_cycle_analytics!
+    unless can?(current_user, :read_group_cycle_analytics, group)
+      render_403
+    end
+  end
+end
---
+++ ee/spec/controllers/groups/cycle_analytics/events_controller_spec.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec::Matchers.define :nested_hash_including do |path_to_hash, value|
+  match { |actual| actual.dig(*path_to_hash) == value }
+end
+
+describe Groups::CycleAnalytics::EventsController do
+  let(:group) { create(:group) }
+  let(:project) { create(:project, namespace: group) }
+  let(:user) { create(:user) }
+  let(:group_service) { instance_double(::CycleAnalytics::GroupLevel) }
+  let(:events_service) { double }
+
+  before do
+    stub_licensed_features(cycle_analytics_for_groups: true)
+
+    sign_in(user)
+  end
+
+  describe 'cycle analytics' do
+    context 'with proper permission' do
+      before do
+        group.add_owner(user)
+      end
+
+      it 'calls service' do
+        expect(events_service).to receive(:events)
+        expect(group_service).to receive(:[]).and_return(events_service)
+        expect(::CycleAnalytics::GroupLevel).to receive(:new).and_return(group_service)
+
+        get(:issue,
+            params: {
+              group_id: group.name
+            },
+            format: :json)
+
+        expect(response).to be_success
+      end
+
+      it 'calls service with specific params' do
+        expect(events_service).to receive(:events)
+        expect(group_service).to receive(:[]).and_return(events_service)
+        expect(::CycleAnalytics::GroupLevel).to receive(:new)
+          .with(nested_hash_including([:options, :projects], [project.id.to_s]))
+          .and_return(group_service)
+
+        get(:issue,
+            params: {
+              group_id: group.name,
+              cycle_analytics: {
+                project_ids: [project.id]
+              }
+            },
+            format: :json)
+
+        expect(response).to be_success
+      end
+    end
+
+    context 'as guest' do
+      before do
+        group.add_guest(user)
+      end
+
+      it 'returns 403' do
+        get(:issue,
+            params: {
+              group_id: group.name
+            },
+            format: :json)
+
+        expect(response.status).to eq(403)
+      end
+    end
+  end
+end
