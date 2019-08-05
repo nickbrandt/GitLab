d19f8fccea1061cde439933afd5eb9bcc1b96102
Alex Buijs <abuijs@gitlab.com>
2019-08-05 11:41:44 +0200

    Change migration to rename concurrently
    In order to prevent downtime, rename columns concurrently

--- db/migrate/20190801193427_rename_application_settings_snowplow_collector_uri_column.rb
+++ db/migrate/20190801193427_rename_application_settings_snowplow_collector_uri_column.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
-# See http://doc.gitlab.com/ce/development/migration_style_guide.html
-# for more information on how to write migrations for GitLab.
-
 class RenameApplicationSettingsSnowplowCollectorUriColumn < ActiveRecord::Migration[5.2]
   include Gitlab::Database::MigrationHelpers
 
   DOWNTIME = false
 
-  def change
-    rename_column :application_settings, :snowplow_collector_uri, :snowplow_collector_hostname
+  disable_ddl_transaction!
+
+  def up
+    rename_column_concurrently :application_settings, :snowplow_collector_uri, :snowplow_collector_hostname
+  end
+
+  def down
+    cleanup_concurrent_column_rename :application_settings, :snowplow_collector_hostname, :snowplow_collector_uri
   end
 end
