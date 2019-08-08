3248030e262a6f150e7150532cbcca4c4ea21f18
Walmyr Lima <walmyr@gitlab.com>
2019-08-08 16:54:11 +0200

    Add testability on vue components

--- ee/app/assets/javascripts/roadmap/components/epic_item_timeline.vue
+++ ee/app/assets/javascripts/roadmap/components/epic_item_timeline.vue
@@ -116,7 +116,7 @@ export default {
 </script>
 
 <template>
-  <span :style="itemStyles" class="epic-timeline-cell">
+  <span :style="itemStyles" class="epic-timeline-cell" data-qa-selector="epic_timeline_cell">
     <div class="timeline-bar-wrapper">
       <a
         v-if="showTimelineBar"
