d43494db22c548f3ef69e29f182f990efd1a1f39
Małgorzata Ksionek <mksionek@gitlab.com>
2019-08-08 09:51:26 +0200

    Update migration file

--- db/migrate/20190723153247_create_allowed_email_domains_for_groups.rb
+++ db/migrate/20190723153247_create_allowed_email_domains_for_groups.rb
@@ -4,12 +4,10 @@
 # for more information on how to write migrations for GitLab.
 
 class CreateAllowedEmailDomainsForGroups < ActiveRecord::Migration[5.2]
-  include Gitlab::Database::MigrationHelpers
-
   # Set this constant to true if this migration requires downtime.
   DOWNTIME = false
 
-  def up
+  def change
     create_table :allowed_email_domains do |t|
       t.timestamps_with_timezone null: false
       t.references :group, references: :namespace,
@@ -20,9 +18,5 @@ class CreateAllowedEmailDomainsForGroups < ActiveRecord::Migration[5.2]
       t.foreign_key :namespaces, column: :group_id, on_delete: :cascade
       t.string :domain, null: false, limit: 255
     end
-  end
-
-  def down
-    drop_table :allowed_email_domains
   end
 end
