98e5001a80bf8d4f5f978f881a5c369eabb4503b
Mark Chao <mchao@gitlab.com>
2019-08-15 16:17:00 +0800

    ES: index snippet content only up to 1 MB
    To avoid indexing timeout.
    1 MB is chosen because it's repo file index limit,
    and snippet will be migrated from database to repo.

--- ee/lib/elastic/latest/snippet_instance_proxy.rb
+++ ee/lib/elastic/latest/snippet_instance_proxy.rb
@@ -3,6 +3,8 @@
 module Elastic
   module Latest
     class SnippetInstanceProxy < ApplicationInstanceProxy
+      MAX_INDEX_SIZE = 1.megabyte
+
       def as_indexed_json(options = {})
         # We don't use as_json(only: ...) because it calls all virtual and serialized attributes
         # https://gitlab.com/gitlab-org/gitlab-ee/issues/349
@@ -20,6 +22,10 @@ module Elastic
           :visibility_level
         ].each do |attr|
           data[attr.to_s] = safely_read_attribute_for_elasticsearch(attr)
+        end
+
+        if data['content'].bytesize > MAX_INDEX_SIZE
+          data['content'] = data['content'].mb_chars.limit(MAX_INDEX_SIZE).to_s # rubocop: disable CodeReuse/ActiveRecord
         end
 
         # ES6 is now single-type per index, so we implement our own typing
