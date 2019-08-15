f290556806e4a920c24a0231b95e6f9ac99f8b24
Rubén Dávila <ruben@gitlab.com>
2019-08-15 01:39:36 -0500

    Add method to customize redirect path after sign up

--- ee/app/controllers/trials_controller.rb
+++ ee/app/controllers/trials_controller.rb
@@ -1,6 +1,11 @@
-class TrialsController < RegistrationsController
+# frozen_string_literal: true
 
+class TrialsController < RegistrationsController
   private
+
+  def after_sign_up_path_for(user)
+    root_path
+  end
 
   def sign_up_params
     extra_params = params.require(:user).permit(:skip_confirmation)
