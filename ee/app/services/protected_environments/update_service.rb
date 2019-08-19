83841c21ac77bfa7fb5dca6afe0a2710796b2001
Krasimir Angelov <krasio@codingspree.net>
2019-08-19 18:45:12 +0300

    Sanitize input when creating/updating protected environments
    Remove group ids that are not in the project's invited groups.
    
    Related to https://gitlab.com/gitlab-org/gitlab-ee/issues/11649.

--- ee/app/services/protected_environments/update_service.rb
+++ ee/app/services/protected_environments/update_service.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 module ProtectedEnvironments
-  class UpdateService < BaseService
+  class UpdateService < ProtectedEnvironments::BaseService
     def execute(protected_environment)
-      protected_environment.update(params)
+      protected_environment.update(sanitized_params)
     end
   end
 end
