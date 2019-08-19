ef12b20543c44591c8bfa64342a8ad6a3ff28dc4
Fabio Pitino <fpitino@gitlab.com>
2019-08-19 16:05:09 +0200

    Introduce JobActivity limit for alive jobs
    To protect from accidentally launching too many CI pipelines
    we introduce a new JobActivity limit which compare the count
    of jobs in alive pipelines created in the past hour.

---
+++ ee/lib/ee/gitlab/ci/pipeline/chain/limit/job_activity.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module EE
+  module Gitlab
+    module Ci
+      module Pipeline
+        module Chain
+          module Limit
+            module JobActivity
+              extend ::Gitlab::Utils::Override
+              include ::Gitlab::Ci::Pipeline::Chain::Helpers
+              include ::Gitlab::OptimisticLocking
+
+              attr_reader :limit
+              private :limit
+
+              def initialize(*)
+                super
+
+                @limit = Pipeline::Quota::JobActivity
+                  .new(project.namespace, pipeline.project)
+              end
+
+              override :perform!
+              def perform!
+                return unless limit.exceeded?
+
+                retry_optimistic_lock(pipeline) do
+                  pipeline.drop!(:job_activity_limit_exceeded)
+                end
+              end
+
+              override :break?
+              def break?
+                limit.exceeded?
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
