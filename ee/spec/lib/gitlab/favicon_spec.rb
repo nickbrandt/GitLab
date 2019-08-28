d1cca89298b0a9c1b4e4948d6b3ad8e8631a54fb
Ash McKenzie <amckenzie@gitlab.com>
2019-08-28 11:53:46 +1000

    Use new RailsHelpers module

--- ee/spec/lib/gitlab/favicon_spec.rb
+++ ee/spec/lib/gitlab/favicon_spec.rb
@@ -3,9 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Gitlab::Favicon, :request_store do
+  include RailsHelpers
+
   describe '.main' do
     it 'has green favicon for development' do
-      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('development'))
+      stub_rails_env('development')
       expect(described_class.main).to match_asset_path 'favicon-green.png'
     end
   end
