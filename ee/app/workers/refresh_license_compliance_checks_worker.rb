480208061f8bd89b25436b48ca7e54dc43c8aa4f
mo khan <mo.khan@gmail.com>
2019-08-29 17:04:09 -0600

    Filter down MR to ones with license check rules

--- ee/app/workers/refresh_license_compliance_checks_worker.rb
+++ ee/app/workers/refresh_license_compliance_checks_worker.rb
@@ -10,11 +10,7 @@ class RefreshLicenseComplianceChecksWorker
     project_approval_rule = license_compliance_rule_for(project)
     return if project_approval_rule.nil?
 
-    merge_requests = project
-      .merge_requests
-      .opened
-      .includes(:approval_rules, :head_pipeline)
-    merge_requests.find_each do |merge_request|
+    merge_requests_for(project).find_each do |merge_request|
       merge_request_rule = license_compliance_rule_for(merge_request)
       next if merge_request_rule.nil?
 
@@ -30,6 +26,17 @@ class RefreshLicenseComplianceChecksWorker
   end
 
   private
+
+  def merge_requests_for(project)
+    project
+      .merge_requests
+      .opened
+      .includes(:approval_rules, :head_pipeline)
+      .where(approval_merge_request_rules: {
+        name: ApprovalRuleLike::DEFAULT_NAME_FOR_LICENSE_REPORT,
+        rule_type: ApprovalMergeRequestRule.rule_types[:report_approver]
+      })
+  end
 
   def license_compliance_rule_for(target)
     rule_for(target: target, name: ApprovalRuleLike::DEFAULT_NAME_FOR_LICENSE_REPORT)
