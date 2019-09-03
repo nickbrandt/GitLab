1c3121e5429e2a89fc346d1a143c18604e158a75
Thong Kuah <tkuah@gitlab.com>
2019-09-03 11:25:44 +0000

    Add facade which will always return nil in CE
    This method will be overridden in EE

--- ee/app/presenters/ee/group_clusterable_presenter.rb
+++ ee/app/presenters/ee/group_clusterable_presenter.rb
@@ -8,5 +8,18 @@ module EE
     def metrics_cluster_path(cluster, params = {})
       metrics_group_cluster_path(clusterable, cluster, params)
     end
+
+    override :environments_cluster_path
+    def environments_cluster_path(cluster)
+      return super unless can_read_cluster_environments?
+
+      environments_group_cluster_path(clusterable, cluster)
+    end
+
+    private
+
+    def can_read_cluster_environments?
+      can?(current_user, :read_cluster_environments, clusterable)
+    end
   end
 end
--- ee/spec/presenters/group_clusterable_presenter_spec.rb
+++ ee/spec/presenters/group_clusterable_presenter_spec.rb
@@ -5,6 +5,8 @@ require 'spec_helper'
 describe GroupClusterablePresenter do
   include Gitlab::Routing.url_helpers
 
+  set(:user) { create(:user) }
+
   let(:presenter) { described_class.new(group) }
   let(:cluster) { create(:cluster, :provided_by_gcp, :group) }
   let(:group) { cluster.group }
@@ -13,5 +15,29 @@ describe GroupClusterablePresenter do
     subject { presenter.metrics_cluster_path(cluster) }
 
     it { is_expected.to eq(metrics_group_cluster_path(group, cluster)) }
+  end
+
+  describe '#environments_cluster_path' do
+    subject { presenter.environments_cluster_path(cluster) }
+
+    before do
+      group.add_maintainer(user)
+
+      allow(presenter).to receive(:current_user).and_return(user)
+
+      stub_licensed_features(cluster_deployments: feature_available)
+    end
+
+    context 'cluster_deployments feature is available' do
+      let(:feature_available) { true }
+
+      it { is_expected.to eq(environments_group_cluster_path(group, cluster)) }
+    end
+
+    context 'cluster_deployments feature is not available' do
+      let(:feature_available) { false }
+
+      it { is_expected.to be_nil }
+    end
   end
 end
