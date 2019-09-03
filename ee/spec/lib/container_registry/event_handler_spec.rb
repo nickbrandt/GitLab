91652b13b6f6779d5b7884d022916e979c22632a
Valery Sizov <valery@gitlab.com>
2019-09-03 18:49:39 +0300

    Fixes Docker Registry Replication
    Fixes event system

--- ee/spec/lib/container_registry/event_handler_spec.rb
+++ ee/spec/lib/container_registry/event_handler_spec.rb
@@ -7,8 +7,12 @@ describe ContainerRegistry::EventHandler do
 
   let(:container_repository) { create(:container_repository) }
 
-  let(:event_target) do
+  let(:event_target_for_push) do
     { 'mediaType' => 'application/vnd.docker.distribution.manifest.v2+json', 'repository' => container_repository.path }
+  end
+
+  let(:event_target_for_delete) do
+    { 'tag' => 'latest', 'repository' => container_repository.path }
   end
 
   set(:primary_node)   { create(:geo_node, :primary) }
@@ -18,15 +22,22 @@ describe ContainerRegistry::EventHandler do
     stub_current_geo_node(primary_node)
   end
 
-  it 'creates event' do
-    push_event = { action: 'push', target: event_target }.with_indifferent_access
+  it 'creates event for push' do
+    push_event = { action: 'push', target: event_target_for_push }.with_indifferent_access
 
     expect { described_class.new([push_event]).execute }
         .to change { ::Geo::ContainerRepositoryUpdatedEvent.count }.by(1)
   end
 
-  it 'ignores non-push events' do
-    pull_event = { action: 'pull', target: event_target }.with_indifferent_access
+  it 'creates event for delete' do
+    delete_event = { action: 'delete', target: event_target_for_delete }.with_indifferent_access
+
+    expect { described_class.new([delete_event]).execute }
+        .to change { ::Geo::ContainerRepositoryUpdatedEvent.count }.by(1)
+  end
+
+  it 'ignores pull events' do
+    pull_event = { action: 'pull', target: {} }.with_indifferent_access
 
     expect { described_class.new([pull_event]).execute }
         .to change { ::Geo::ContainerRepositoryUpdatedEvent.count }.by(0)
