473bf37c43256aaf686bb2b985915254bdf565c5
Rubén Dávila <ruben@gitlab.com>
2019-09-05 10:21:58 -0500

    Add API client to talk with subscription portal

--- ee/app/services/gitlab_subscriptions/create_lead_service.rb
+++ ee/app/services/gitlab_subscriptions/create_lead_service.rb
@@ -3,7 +3,19 @@
 module GitlabSubscriptions
   class CreateLeadService
     def execute(company_params)
-      { success: true }
+      response = subscription_app_client.create_trial_account(company_params)
+
+      if response.success
+        { success: true }
+      else
+        { success: false, errors: response.data&.errors }
+      end
+    end
+
+    private
+
+    def subscription_app_client
+      Gitlab::SubscriptionPortal::Client.new
     end
   end
 end
---
+++ ee/lib/gitlab/subscription_portal/client.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module SubscriptionPortal
+    class Client
+      def create_trial_account(params)
+        parse_response(Gitlab::HTTP.post("#{base_url}/trials",
+                                         body: params.to_json,
+                                         headers: headers))
+      end
+
+      private
+
+      def base_url
+        EE::SUBSCRIPTIONS_URL
+      end
+
+      def headers
+        {
+          "Accept" => 'application/json',
+          "X-Admin-Email" => ENV['SUBSCRIPTION_PORTAL_ADMIN_EMAIL'],
+          "X-Admin-Token" => ENV['SUBSCRIPTION_PORTAL_ADMIN_TOKEN']
+        }
+      end
+
+      def parse_response(http_response)
+        response = Hashie::Mash.new(success: false)
+
+        case http_response.response
+        when Net::HTTPSuccess
+          response.success = true
+          response.data = JSON.parse(response.body)
+        when Net::HTTPUnprocessableEntity
+          response.data = JSON.parse(response.body) rescue nil
+        else
+          response.data = { errors: "HTTP status code: #{http_response.code}" }
+        end
+
+        response
+      end
+    end
+  end
+end
