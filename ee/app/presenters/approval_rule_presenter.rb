e8d813ac51b38645defe4f163f472e95eb61d728
Nick Thomas <nick@gitlab.com>
2019-09-06 17:51:11 +0100

    Hide approvers if a rule has any hidden groups
    An approval rule that contains a private group can disclose information
    about the membership of that group via the list of approvers for that
    rule, which is constructed from all members of all groups, plus each
    individual user included in the rule.
    
    To avoid this information disclosure, hide all approvers in a rule
    where even one of the groups is hidden to the viewer. This removes more
    information than is strictly necessary, but is a simple fix for a hard
    problem - right now, we don't track which approvers come from which
    group, so it's difficult to be more precise, and this is something of
    an edge case anyway.

--- ee/app/presenters/approval_rule_presenter.rb
+++ ee/app/presenters/approval_rule_presenter.rb
@@ -1,12 +1,26 @@
 # frozen_string_literal: true
 
 class ApprovalRulePresenter < Gitlab::View::Presenter::Delegated
+  include Gitlab::Utils::StrongMemoize
+
+  # Hide all approvers if any of them might come from a hidden group. This
+  # represents an abundance of caution, but we can't tell which approvers come
+  # from a hidden group and which don't, from here, so this is the simplest
+  # thing we can do
+  def approvers
+    return [] if contains_hidden_groups?
+
+    super
+  end
+
   def groups
     group_query_service.visible_groups
   end
 
   def contains_hidden_groups?
-    group_query_service.contains_hidden_groups?
+    strong_memoize(:contains_hidden_groups) do
+      group_query_service.contains_hidden_groups?
+    end
   end
 
   private
