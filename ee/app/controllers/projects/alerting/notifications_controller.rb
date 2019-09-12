8ef2d7150573264324f2b6275c9d8646d255304f
Vitali Tatarintev <vtatarintev@gitlab.com>
2019-09-12 14:41:06 +0200

    Call NotifyService from NotificationsController
    Connecting the dots by calling
    `Projects::Alerting::NotifyService`
    from `Projects::Alerting::NotificationsController`

--- ee/app/controllers/projects/alerting/notifications_controller.rb
+++ ee/app/controllers/projects/alerting/notifications_controller.rb
@@ -11,7 +11,10 @@ module Projects
       before_action :check_generic_alert_endpoint_feature_flag!
 
       def create
-        head :ok
+        token = extract_alert_manager_token(request)
+        result = notify_service.execute(token)
+
+        head(response_status(result))
       end
 
       private
@@ -23,6 +26,26 @@ module Projects
 
       def check_generic_alert_endpoint_feature_flag!
         render_404 unless Feature.enabled?(:generic_alert_endpoint, @project)
+      end
+
+      def extract_alert_manager_token(request)
+        Doorkeeper::OAuth::Token.from_bearer_authorization(request)
+      end
+
+      def notify_service
+        Projects::Alerting::NotifyService
+          .new(project, current_user, params.permit!)
+      end
+
+      def response_status(result)
+        case result.http_status
+        when 401
+          :unauthorized
+        when 403
+          :forbidden
+        else
+          :ok
+        end
       end
     end
   end
--- ee/spec/controllers/projects/alerting/notifications_controller_spec.rb
+++ ee/spec/controllers/projects/alerting/notifications_controller_spec.rb
@@ -7,6 +7,13 @@ describe Projects::Alerting::NotificationsController do
   set(:environment) { create(:environment, project: project) }
 
   describe 'POST #create' do
+    let(:service_response) { ServiceResponse.success }
+    let(:notify_service) { instance_double(Projects::Alerting::NotifyService, execute: service_response) }
+
+    before do
+      allow(Projects::Alerting::NotifyService).to receive(:new).and_return notify_service
+    end
+
     def make_request(opts = {})
       post :create, params: project_params, session: { as: :json }
     end
@@ -16,10 +23,50 @@ describe Projects::Alerting::NotificationsController do
         stub_feature_flags(generic_alert_endpoint: true)
       end
 
-      it 'responds with ok' do
-        make_request
+      context 'when notification service succeeds' do
+        it 'responds with ok' do
+          make_request
 
-        expect(response).to have_gitlab_http_status(:ok)
+          expect(response).to have_gitlab_http_status(:ok)
+        end
+      end
+
+      context 'when notification service fails' do
+        let(:service_response) { ServiceResponse.error(message: 'Unauthorized', http_status: 401) }
+
+        it 'responds with the service response' do
+          make_request
+
+          expect(response).to have_gitlab_http_status(:unauthorized)
+        end
+      end
+
+      context 'bearer token' do
+        context 'when set' do
+          it 'extracts bearer token' do
+            request.headers['HTTP_AUTHORIZATION'] = 'Bearer some token'
+
+            expect(notify_service).to receive(:execute).with('some token')
+
+            make_request
+          end
+
+          it 'pass nil if cannot extract a non-bearer token' do
+            request.headers['HTTP_AUTHORIZATION'] = 'some token'
+
+            expect(notify_service).to receive(:execute).with(nil)
+
+            make_request
+          end
+        end
+
+        context 'when missing' do
+          it 'passes nil' do
+            expect(notify_service).to receive(:execute).with(nil)
+
+            make_request
+          end
+        end
       end
     end
 
