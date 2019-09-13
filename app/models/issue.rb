0f85bf0a511c4ac7270908b40b21a8ab00966528
Eugenia Grieff <egrieff@gitlab.com>
2019-09-13 13:08:19 +0000

    Indicate on Issue Status if an Issue was Duplicated
    - Also includes db migration to track duplicated reference

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -27,6 +27,7 @@ class Issue < ApplicationRecord
 
   belongs_to :project
   belongs_to :moved_to, class_name: 'Issue'
+  belongs_to :duplicated_to, class_name: 'Issue'
   belongs_to :closed_by, class_name: 'User'
 
   has_internal_id :iid, scope: :project, init: ->(s) { s&.project&.issues&.maximum(:iid) }
@@ -179,6 +180,10 @@ class Issue < ApplicationRecord
 
   def moved?
     !moved_to_id.nil?
+  end
+
+  def duplicated?
+    !duplicated_to_id.nil?
   end
 
   def can_move?(user, to_project = nil)
