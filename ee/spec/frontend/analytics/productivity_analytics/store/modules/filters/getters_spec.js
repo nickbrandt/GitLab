98ab3a2a85a78cf41e68e99dda72d0cc3f33ffe7
Martin Wortschack <mwortschack@gitlab.com>
2019-09-16 20:59:45 +0000

    Rename author_username to author_id
    - Productivity analytics API requires author_id,
    but the filter bar sends author_username

--- ee/spec/frontend/analytics/productivity_analytics/store/modules/filters/getters_spec.js
+++ ee/spec/frontend/analytics/productivity_analytics/store/modules/filters/getters_spec.js
@@ -18,7 +18,7 @@ describe('Productivity analytics filter getters', () => {
 
       const mockGetters = { mergedOnAfterDate: '2019-07-16T00:00:00.00Z' };
       const expected = {
-        author_username: 'root',
+        author_id: 'root',
         group_id: 'gitlab-org',
         label_name: ['labelxyz'],
         merged_at_after: '2019-07-16T00:00:00.00Z',
