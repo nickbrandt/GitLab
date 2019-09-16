96881f7fc153f5ff648d8723b88a3551338a847c
jakeburden <jburden@gitlab.com>
2019-09-16 16:21:57 -0400

    add type=module attribute to script tags

--- app/helpers/defer_script_tag_helper.rb
+++ app/helpers/defer_script_tag_helper.rb
@@ -2,7 +2,7 @@
 
 module DeferScriptTagHelper
   # Override the default ActionView `javascript_include_tag` helper to support page specific deferred loading
-  # def javascript_include_tag(*sources)
-  #   super(*sources, defer: true, extname: false)
-  # end
+  def javascript_include_tag(*sources)
+    super(*sources, defer: true)
+  end
 end
