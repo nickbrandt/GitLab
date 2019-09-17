ebfc3637df44ea95060d34a17688f4cfa35a153e
Sebastián Arcila Valenzuela <sarcila@gitlab.com>
2019-09-17 09:18:33 +0000

    Add basic protected branches log audits events
    These changes add EE custom behavior for the different protected
    branches (create, destroy and udpate).
    
    It also adds a possible fix for
    spec/services/git/branch_push_service_spec.rb that was using
    expect_any_instance_of that was failing with the new EE modules

---
+++ ee/spec/services/ee/protected_branches/destroy_service_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe ProtectedBranches::DestroyService do
+  let(:protected_branch) { create(:protected_branch) }
+  let(:branch_name) { protected_branch.name }
+  let(:project) { protected_branch.project }
+  let(:user) { project.owner }
+
+  describe '#execute' do
+    subject(:service) { described_class.new(project, user) }
+
+    it 'adds a security audit event entry' do
+      expect { service.execute(protected_branch) }.to change(::SecurityEvent, :count).by(1)
+    end
+
+    context 'when destroy fails' do
+      before do
+        expect(protected_branch).to receive(:destroy).and_return(false)
+      end
+
+      it "doesn't add a security audit event entry" do
+        expect { service.execute(protected_branch) }.not_to change(::SecurityEvent, :count)
+      end
+    end
+  end
+end
