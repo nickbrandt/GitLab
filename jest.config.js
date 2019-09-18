570921c3fca39b00710102727367596f24caee88
Winnie Hellmann <winnie@gitlab.com>
2019-09-18 13:52:10 +0000

    Show error if Jest is used with Karma files

--- jest.config.js
+++ jest.config.js
@@ -63,3 +63,15 @@ module.exports = {
     IS_EE,
   },
 };
+
+const karmaTestFile = process.argv.find(arg => arg.includes('spec/javascripts/'));
+if (karmaTestFile) {
+  console.error(`
+Files in spec/javascripts/ and ee/spec/javascripts need to be run with Karma.
+Please use the following command instead:
+
+yarn karma -f ${karmaTestFile}
+
+`);
+  process.exit(1);
+}
