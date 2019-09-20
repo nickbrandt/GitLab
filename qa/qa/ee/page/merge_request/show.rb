e6673393e0a7aa303814fc215d1b480c084d1bf6
Aleksandr Soborov <asoborov@gitlab.com>
2019-09-20 10:53:04 +0000

    Added E2E test for all Secure reports in MRs
    Updated page object and data selectors

--- qa/qa/ee/page/merge_request/show.rb
+++ qa/qa/ee/page/merge_request/show.rb
@@ -39,6 +39,10 @@ module QA
 
               view 'ee/app/assets/javascripts/vue_shared/security_reports/grouped_security_reports_app.vue' do
                 element :vulnerability_report_grouped
+                element :sast_scan_report
+                element :dependency_scan_report
+                element :container_scan_report
+                element :dast_scan_report
               end
 
               view 'app/assets/javascripts/reports/components/report_section.vue' do
@@ -129,11 +133,28 @@ module QA
               finished_loading?
               has_element?(:vulnerability_report_grouped, wait: 1)
             end
+            find_element(:vulnerability_report_grouped).has_no_content?("is loading")
           end
 
-          def has_detected_vulnerability_count_of?(expected)
+          def has_total_vulnerability_count_of?(expected)
             # Match text cut off in order to find both "1 vulnerability" and "X vulnerabilities"
-            find_element(:vulnerability_report_grouped).has_content?("detected #{expected} vulnerabilit")
+            find_element(:vulnerability_report_grouped).has_content?("Security scanning detected #{expected} vulnerabilit")
+          end
+
+          def has_sast_vulnerability_count_of?(expected)
+            find_element(:sast_scan_report).has_content?("SAST detected #{expected} vulnerabilit")
+          end
+
+          def has_dependency_vulnerability_count_of?(expected)
+            find_element(:dependency_scan_report).has_content?("Dependency scanning detected #{expected} vulnerabilit")
+          end
+
+          def has_container_vulnerability_count_of?(expected)
+            find_element(:container_scan_report).has_content?("Container scanning detected #{expected} vulnerabilit")
+          end
+
+          def has_dast_vulnerability_count_of?(expected)
+            find_element(:dast_scan_report).has_content?("DAST detected #{expected} vulnerabilit")
           end
 
           def num_approvals_required
