5ab7e57c63a5265b7f594e0f689d800a2cad8ee3
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2019-09-24 22:46:16 +0000

    Merge branch '32696-geo-lfs-not-being-synced' into 'master'
    Resolve "Geo: LFS not being synced"
    
    Closes #32696
    
    See merge request gitlab-org/gitlab!17633

--- ee/spec/models/geo/fdw/lfs_object_spec.rb
+++ ee/spec/models/geo/fdw/lfs_object_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe Geo::Fdw::LfsObject, :geo, type: :model do
     it { is_expected.to have_many(:lfs_objects_projects).class_name('Geo::Fdw::LfsObjectsProject') }
     it { is_expected.to have_many(:projects).class_name('Geo::Fdw::Project').through(:lfs_objects_projects) }
   end
+
+  describe '.missing_file_registry', :geo_fdw do
+    subject { described_class.missing_file_registry }
+
+    it 'returns lfs objects that doesnt have a corresponding file registry entry' do
+      lfs_objects = create_list(:lfs_object, 2)
+
+      # simulate existing registry entries with the same +id+, but different +file_type+
+      lfs_objects.each do |lfs|
+        create(:geo_file_registry, file_id: lfs.id)
+      end
+
+      expect(subject).to match_ids(lfs_objects)
+    end
+  end
 end
