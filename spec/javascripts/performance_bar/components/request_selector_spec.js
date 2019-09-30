tigefa4u/gitlabhq

Show warnings in performance bar
Right now, the performance bar is unopinionated: it will tell you how
many SQL queries a page ran, but not if that's good or bad. It also
hides away AJAX requests behind a dropdown, which means people are less
likely to see those requests.

This adds a way of highlighting warnings generated by the backend in
both places. In a request itself, a metric will have a warning item if
the backend thinks it's too slow. And in the request selector, we
highlight requests that have warnings associated.

spec/javascripts/performance_bar/components/request_selector_spec.js
@@ -4,14 +4,25 @@ import mountComponent from 'spec/helpers/vue_mount_component_helper';
 
 describe('request selector', () => {
   const requests = [
-    { id: '123', url: 'https://gitlab.com/' },
+    {
+      id: '123',
+      url: 'https://gitlab.com/',
+      hasWarnings: false,
+    },
     {
       id: '456',
       url: 'https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/1',
+      hasWarnings: false,
     },
     {
       id: '789',
       url: 'https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/1.json?serializer=widget',
+      hasWarnings: false,
+    },
+    {
+      id: 'abc',
+      url: 'https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/1/discussions.json',
+      hasWarnings: true,
     },
   ];
 
@@ -42,5 +53,16 @@ describe('request selector', () => {
 
   it('ignores trailing slashes', () => {
     expect(optionText(requests[0].id)).toEqual('gitlab.com');
+  });
+
+  it('has a warning icon if any requests have warnings', () => {
+    expect(vm.$el.querySelector('span > gl-emoji').dataset.name).toEqual('warning');
+  });
+
+  it('adds a warning icon to requests with warnings', () => {
+    const option = vm.$el.querySelector('[value="abc"]');
+
+    expect(option.querySelector('gl-emoji').dataset.name).toEqual('warning');
+    expect(option.innerText).toContain('discussions.json');
   });
 });

nickbrandt/GitLab

spec/javascripts/performance_bar/components/request_selector_spec.js
@@ -4,14 +4,25 @@ import mountComponent from 'spec/helpers/vue_mount_component_helper';
 
 describe('request selector', () => {
   const requests = [
-    { id: '123', url: 'https://gitlab.com/' },
+    {
+      id: '123',
+      url: 'https://gitlab.com/',
+      hasWarnings: false,
+    },
     {
       id: '456',
       url: 'https://gitlab.com/gitlab-org/gitlab-foss/merge_requests/1',
+      hasWarnings: false,
     },
     {
       id: '789',
       url: 'https://gitlab.com/gitlab-org/gitlab-foss/merge_requests/1.json?serializer=widget',
+      hasWarnings: false,
+    },
+    {
+      id: 'abc',
+      url: 'https://gitlab.com/gitlab-org/gitlab-foss/merge_requests/1/discussions.json',
+      hasWarnings: true,
     },
   ];
 
@@ -42,5 +53,16 @@ describe('request selector', () => {
 
   it('ignores trailing slashes', () => {
     expect(optionText(requests[0].id)).toEqual('gitlab.com');
+  });
+
+  it('has a warning icon if any requests have warnings', () => {
+    expect(vm.$el.querySelector('span > gl-emoji').dataset.name).toEqual('warning');
+  });
+
+  it('adds a warning icon to requests with warnings', () => {
+    const option = vm.$el.querySelector('[value="abc"]');
+
+    expect(option.querySelector('gl-emoji').dataset.name).toEqual('warning');
+    expect(option.innerText).toContain('discussions.json');
   });
 });