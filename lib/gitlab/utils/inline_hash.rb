5335b855ea655ae0ee64c0bd2e0838d4bf4d9192
allison.browne <abrowne@gitlab.com>
2019-09-30 14:45:31 -0400

    Add examples and specs for edge case of InlineHash
    Unested symbolic or numeric keys should not be converted to strings.
    These changes document that and adds specs reflecting that. Also
    makes a minor change to simplify and speed perf in the InlineHash
    implementation.

--- lib/gitlab/utils/inline_hash.rb
+++ lib/gitlab/utils/inline_hash.rb
@@ -11,13 +11,15 @@ module Gitlab
       #
       #  {
       #    'root_param' => 'Root',
+      #    12 => 'number',
+      #    symbol: 'symbol',
       #    nested_param: {
       #      key: 'Value'
       #    },
       #    'very' => {
       #      'deep' => {
       #        'nested' => {
-      #          'param' => 'Deep nested value'
+      #          12 => 'Deep nested value'
       #        }
       #      }
       #    }
@@ -28,15 +30,17 @@ module Gitlab
       #
       #  {
       #    'root_param' => 'Root',
+      #     12 => 'number',
+      #     symbol: symbol,
       #    'nested_param.key' => 'Value',
-      #    'very.deep.nested.param' => 'Deep nested value'
+      #    'very.deep.nested.12' => 'Deep nested value'
       #  }
       #
       def merge_keys(hash, prefix: nil, connector: '.')
         result = {}
-        base_prefix = prefix ? "#{prefix}#{connector}" : ''
         pairs =
           if prefix
+            base_prefix = "#{prefix}#{connector}"
             hash.map { |key, value| ["#{base_prefix}#{key}", value] }
           else
             hash.to_a
