a16ac0afe6d714d6b034d2cd057d131b2e4ea970
jakeburden <jburden@gitlab.com>
2019-10-04 18:02:36 -0400

    Change test to not have Save button

--- spec/features/projects/pages_spec.rb
+++ spec/features/projects/pages_spec.rb
@@ -357,7 +357,7 @@ shared_examples 'pages settings editing' do
 
         expect(page).not_to have_field(:project_pages_https_only)
         expect(page).not_to have_content('Force HTTPS (requires valid certificates)')
-        expect(page).to have_button('Save')
+        expect(page).not_to have_button('Save')
       end
     end
   end
