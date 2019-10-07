b1d19581392af1cd2dd54354c586fcbe43269287
Sean Arnold <sarnold@gitlab.com>
2019-10-07 22:02:24 +0000

    Add join table between Issue and PrometheusAlertEvent
    Prometheus specific alert handling

--- ee/spec/models/issue_spec.rb
+++ ee/spec/models/issue_spec.rb
@@ -45,6 +45,8 @@ describe Issue do
   describe 'relations' do
     it { is_expected.to have_many(:designs) }
     it { is_expected.to have_many(:design_versions) }
+    it { is_expected.to have_and_belong_to_many(:prometheus_alert_events) }
+    it { is_expected.to have_many(:prometheus_alerts) }
 
     describe 'versions.most_recent' do
       it 'returns the most recent version' do
