5f02c9949e668deb1d72f650bca76947b918b207
allison.browne <abrowne@gitlab.com>
2019-10-07 18:02:06 -0400

    Factory syntax fix

--- spec/factories/zoom_meetings.rb
+++ spec/factories/zoom_meetings.rb
@@ -4,15 +4,15 @@ FactoryBot.define do
   factory :zoom_meeting do
     project { issue.project }
     issue
-    url 'https://zoom.us/j/123456789'
-    issue_status :added
+    url { 'https://zoom.us/j/123456789' }
+    issue_status { :added }
 
     trait :added_to_issue do
-      issue_status :added
+      issue_status { :added }
     end
 
     trait :removed_from_issue do
-      issue_status :removed
+      issue_status { :removed }
     end
   end
 end
