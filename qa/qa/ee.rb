aca4050b855958342bc15a975ea17f30bc709d12
Mark Lapierre <mlapierre@gitlab.com>
2019-10-10 10:44:25 +1100

    Update E2E test of assigning code owners
    A recent change removed the option to require approval from codeowners
    from project settings and added it to the protected branch creation
    screen.
    
    See https://gitlab.com/gitlab-org/gitlab/merge_requests/15862
    
    This updates the E2E test of the affected feature:
    
    * converts the old qa class selectors to data attributes.
    * removes the EE project settings module because it only contained the
      merge request approval setting, which only include the code owners
      setting and was removed with the feature change
    * fixes a bug in `Resource::Repository::Commit` that caused new files
      to not be committed
    * updates the test to conform with our current style

--- qa/qa/ee.rb
+++ qa/qa/ee.rb
@@ -113,7 +113,6 @@ module QA
         module Settings
           autoload :ProtectedBranches, 'qa/ee/page/project/settings/protected_branches'
           autoload :MirroringRepositories, 'qa/ee/page/project/settings/mirroring_repositories'
-          autoload :Main, 'qa/ee/page/project/settings/main'
           autoload :MergeRequest, 'qa/ee/page/project/settings/merge_request'
           autoload :Repository, 'qa/ee/page/project/settings/repository'
           autoload :PushRules, 'qa/ee/page/project/settings/push_rules'
