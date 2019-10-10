037e5d3992e096c451fc004a760cac17192417d9
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2019-10-10 15:00:38 -0300

    Fix undefined method log_geo_deleted_event for MergeRequestDiff

---
+++ ee/app/models/ee/merge_request_diff.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module EE
+  module MergeRequestDiff
+    def log_geo_deleted_event
+      # Keep empty for now. Should be addressed in future
+      # by https://gitlab.com/gitlab-org/gitlab/issues/33817
+    end
+  end
+end
