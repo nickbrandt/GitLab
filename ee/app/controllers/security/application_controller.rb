3d1681e795d89d1c0843a3d3a10e2e112e4b48cf
Avielle Wolfe <awolfe@gitlab.com>
2019-10-11 14:12:50 +0000

    Add policy for instance security dashboard
    So long as the right license is there, any logged in user should
    be able to view the dashboard.
    
    https://gitlab.com/gitlab-org/gitlab/issues/33831

---
+++ ee/app/controllers/security/application_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Security
+  class ApplicationController < ::ApplicationController
+    before_action :authorize_read_security_dashboard!
+    before_action do
+      push_frontend_feature_flag(:security_dashboard)
+    end
+
+    private
+
+    def authorize_read_security_dashboard!
+      render_404 unless Feature.enabled?(:security_dashboard) &&
+        can?(current_user, :read_security_dashboard)
+    end
+  end
+end
---
+++ ee/spec/controllers/security/dashboard_controller_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Security::DashboardController do
+  describe 'GET #show' do
+    it_behaves_like Security::ApplicationController do
+      let(:security_application_controller_child_action) do
+        get :show
+      end
+    end
+  end
+end
