0ba0476dbf484da5edcd5380cf366554c73ce44b
Vitali Tatarintev <vtatarintev@gitlab.com>
2019-10-15 11:32:35 +0200

    Removes generic_alert_endpoint feature flag
    Releases Generic alerts integration that accepts alerts
    from any source via a generic webhook receiver.

--- ee/spec/controllers/projects/alerting/notifications_controller_spec.rb
+++ ee/spec/controllers/projects/alerting/notifications_controller_spec.rb
@@ -22,84 +22,66 @@ describe Projects::Alerting::NotificationsController do
       post :create, params: project_params, body: body.to_json, as: :json
     end
 
-    context 'when feature flag is on' do
-      before do
-        stub_feature_flags(generic_alert_endpoint: true)
+    context 'when notification service succeeds' do
+      let(:payload) do
+        {
+          title: 'Alert title',
+          hosts: 'https://gitlab.com'
+        }
       end
 
-      context 'when notification service succeeds' do
-        let(:payload) do
-          {
-            title: 'Alert title',
-            hosts: 'https://gitlab.com'
-          }
-        end
+      let(:permitted_params) { ActionController::Parameters.new(payload).permit! }
 
-        let(:permitted_params) { ActionController::Parameters.new(payload).permit! }
+      it 'responds with ok' do
+        make_request
 
-        it 'responds with ok' do
-          make_request
-
-          expect(response).to have_gitlab_http_status(:ok)
-        end
-
-        it 'does not pass excluded parameters to the notify service' do
-          make_request(payload)
-
-          expect(Projects::Alerting::NotifyService)
-            .to have_received(:new)
-            .with(project, nil, permitted_params)
-        end
+        expect(response).to have_gitlab_http_status(:ok)
       end
 
-      context 'when notification service fails' do
-        let(:service_response) { ServiceResponse.error(message: 'Unauthorized', http_status: 401) }
+      it 'does not pass excluded parameters to the notify service' do
+        make_request(payload)
 
-        it 'responds with the service response' do
-          make_request
-
-          expect(response).to have_gitlab_http_status(:unauthorized)
-        end
-      end
-
-      context 'bearer token' do
-        context 'when set' do
-          it 'extracts bearer token' do
-            request.headers['HTTP_AUTHORIZATION'] = 'Bearer some token'
-
-            expect(notify_service).to receive(:execute).with('some token')
-
-            make_request
-          end
-
-          it 'pass nil if cannot extract a non-bearer token' do
-            request.headers['HTTP_AUTHORIZATION'] = 'some token'
-
-            expect(notify_service).to receive(:execute).with(nil)
-
-            make_request
-          end
-        end
-
-        context 'when missing' do
-          it 'passes nil' do
-            expect(notify_service).to receive(:execute).with(nil)
-
-            make_request
-          end
-        end
+        expect(Projects::Alerting::NotifyService)
+          .to have_received(:new)
+          .with(project, nil, permitted_params)
       end
     end
 
-    context 'when feature flag is off' do
-      before do
-        stub_feature_flags(generic_alert_endpoint: false)
-      end
+    context 'when notification service fails' do
+      let(:service_response) { ServiceResponse.error(message: 'Unauthorized', http_status: 401) }
 
-      it 'responds with not_found' do
+      it 'responds with the service response' do
         make_request
 
-        expect(response).to have_gitlab_http_status(:not_found)
+        expect(response).to have_gitlab_http_status(:unauthorized)
+      end
+    end
+
+    context 'bearer token' do
+      context 'when set' do
+        it 'extracts bearer token' do
+          request.headers['HTTP_AUTHORIZATION'] = 'Bearer some token'
+
+          expect(notify_service).to receive(:execute).with('some token')
+
+          make_request
+        end
+
+        it 'pass nil if cannot extract a non-bearer token' do
+          request.headers['HTTP_AUTHORIZATION'] = 'some token'
+
+          expect(notify_service).to receive(:execute).with(nil)
+
+          make_request
+        end
+      end
+
+      context 'when missing' do
+        it 'passes nil' do
+          expect(notify_service).to receive(:execute).with(nil)
+
+          make_request
+        end
       end
     end
   end
