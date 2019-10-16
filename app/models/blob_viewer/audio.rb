2ecc5ae289d0b076866591e722c7d5d229cbea1c
Jesse Hall <jessehall3@gmail.com>
2019-10-16 14:59:29 +0000

    Feature #19822, users can preview audio in a repository.

--- app/models/blob_viewer/audio.rb
+++ app/models/blob_viewer/audio.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 module BlobViewer
-  class Video < Base
+  class Audio < Base
     include Rich
     include ClientSide
 
-    self.partial_name = 'video'
-    self.extensions = UploaderHelper::SAFE_VIDEO_EXT
+    self.partial_name = 'audio'
+    self.extensions = UploaderHelper::SAFE_AUDIO_EXT
     self.binary = true
   end
 end
