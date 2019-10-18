7b12fed6e690cd8abcc8c317de3fa427ee524a07
Vladimir Shushlin <v.shushlin@gmail.com>
2019-10-18 14:34:24 +0800

    Schedule fix access control migration only for .com
    Remove rake task and add post_migration

--- lib/gitlab/background_migration/fix_gitlab_com_pages_access_level.rb
+++ lib/gitlab/background_migration/fix_gitlab_com_pages_access_level.rb
@@ -4,26 +4,93 @@ module Gitlab
   module BackgroundMigration
     # Fixes https://gitlab.com/gitlab-org/gitlab/issues/32961
     class FixGitlabComPagesAccessLevel
-      include Gitlab::Database::MigrationHelpers
+      # Copy routable here to avoid relying on application logic
+      module Routable
+        def build_full_path
+          if parent && path
+            parent.build_full_path + '/' + path
+          else
+            path
+          end
+        end
+      end
 
-      MIGRATION = 'FixGitlabComPagesAccessLevelBatch'
-      BATCH_SIZE = 10_000
-      BATCH_TIME = 2.minutes
+      # Namespace
+      class Namespace < ActiveRecord::Base
+        self.table_name = 'namespaces'
+        self.inheritance_column = :_type_disabled
+
+        include Routable
+
+        belongs_to :parent, class_name: "Namespace"
+      end
 
       # Project
       class Project < ActiveRecord::Base
-        include EachBatch
-
         self.table_name = 'projects'
         self.inheritance_column = :_type_disabled
+
+        include Routable
+
+        belongs_to :namespace
+        alias_method :parent, :namespace
+        alias_attribute :parent_id, :namespace_id
+
+        has_one :project_feature, inverse_of: :project
+
+        PRIVATE = 0
+        INTERNAL = 10
+        PUBLIC = 20
+
+        delegate :public_pages?, to: :project_feature
+
+        def pages_deployed?
+          Dir.exist?(public_pages_path)
+        end
+
+        def public_pages_path
+          File.join(pages_path, 'public')
+        end
+
+        def pages_path
+          File.join(Settings.pages.path, build_full_path)
+        end
+
+        def public?
+          visibility_level == PUBLIC
+        end
       end
 
-      def perform
-        queue_background_migration_jobs_by_range_at_intervals(
-          Project,
-          MIGRATION,
-          BATCH_TIME,
-          batch_size: BATCH_SIZE)
+      # ProjectFeature
+      class ProjectFeature < ActiveRecord::Base
+        self.table_name = 'project_features'
+
+        belongs_to :project
+
+        DISABLED = 0
+        PRIVATE  = 10
+        ENABLED  = 20
+        PUBLIC   = 30
+      end
+
+      def perform(start_id, stop_id)
+        Project.where(id: start_id..stop_id).find_each do |project|
+          next unless project.pages_deployed?
+
+          config_path = File.join(project.pages_path, 'config.json')
+          ac_is_enabled_in_config = JSON.parse(File.read(config_path))["access_control"]
+
+          next if ac_is_enabled_in_config # we already made site private and don't want to surprise the user
+
+          next if project.project_feature.pages_access_level == ProjectFeature::DISABLED
+
+          new_access_level = project.public? ? ProjectFeature::ENABLED : ProjectFeature::PUBLIC
+
+          project.project_feature.update_column(:pages_access_level, new_access_level)
+
+        rescue => e
+          Gitlab::Sentry.track_exception(e, extra: { project_id: project.id })
+        end
       end
     end
   end
---
+++ spec/migrations/schedule_fix_gitlab_com_pages_access_level_spec.rb
@@ -0,0 +1,179 @@
+require 'spec_helper'
+require Rails.root.join('db', 'post_migrate', '20191017045817_schedule_fix_gitlab_com_pages_access_level.rb')
+
+describe ScheduleFixGitlabComPagesAccessLevel, :migration, :sidekiq, schema: 2019_10_16_072826 do
+  using RSpec::Parameterized::TableSyntax
+
+  let(:migration_name) { 'FixGitlabComPagesAccessLevel' }
+
+  ProjectClass = ::Gitlab::BackgroundMigration::FixGitlabComPagesAccessLevel::Project
+  FeatureClass = ::Gitlab::BackgroundMigration::FixGitlabComPagesAccessLevel::ProjectFeature
+
+  class UpdatePagesConfigurationService
+    attr_reader :project
+
+    def initialize(project)
+      @project = project
+    end
+
+    def execute
+      update_file(pages_config_file, pages_config_json)
+    end
+
+    def update_file(file, data)
+      temp_file = "#{file}.#{SecureRandom.hex(16)}"
+      File.open(temp_file, 'w') do |f|
+        f.write(data)
+      end
+      FileUtils.move(temp_file, file, force: true)
+    ensure
+      # In case if the updating fails
+      FileUtils.remove(temp_file, force: true)
+    end
+
+    def pages_config_json
+      pages_config.to_json
+    end
+
+    def pages_config
+      {
+        domains: [],
+        https_only: project.pages_https_only?,
+        id: project.id,
+        access_control: !project.public_pages?
+      }
+    end
+
+    def pages_path
+      @pages_path ||= project.pages_path
+    end
+
+    def pages_config_file
+      File.join(pages_path, 'config.json')
+    end
+  end
+
+  let(:namespaces_table) { table(:namespaces) }
+
+  let(:subgroup) do
+    root_group = namespaces_table.create(path: "group", name: "group")
+    namespaces_table.create!(path: "subgroup", name: "group", parent_id: root_group.id)
+  end
+
+  before do
+    allow(::Gitlab).to receive(:com?).and_return true
+  end
+
+  describe 'scheduling migration' do
+    let!(:first_project) { create_project(ProjectClass::PRIVATE, FeatureClass::PRIVATE, false, false, 'first' ) }
+    let!(:last_project) { create_project(ProjectClass::PRIVATE, FeatureClass::PRIVATE, false, false, 'second' ) }
+
+    subject do
+      Sidekiq::Testing.fake! do
+        migrate!
+      end
+    end
+
+    it 'schedules background migrations' do
+      Timecop.freeze do
+        subject
+
+        expect(migration_name).to be_scheduled_delayed_migration(2.minutes, first_project.id, last_project.id)
+        expect(BackgroundMigrationWorker.jobs.size).to eq(1)
+      end
+    end
+
+    context 'not on gitlab.com' do
+      before do
+        allow(::Gitlab).to receive(:com?).and_return false
+      end
+
+      it 'does not schedule background migrations' do
+        Timecop.freeze do
+          subject
+
+          expect(BackgroundMigrationWorker.jobs.size).to eq(0)
+        end
+      end
+    end
+  end
+
+  where(:visibility_level, :pages_access_level,
+        :pages_deployed, :ac_is_enabled_in_config,
+        :result_pages_access_level) do
+    # Does not change anything if pages are not deployed
+    ProjectClass::PRIVATE  | FeatureClass::DISABLED | false | false | FeatureClass::DISABLED
+    ProjectClass::PRIVATE  | FeatureClass::PRIVATE  | false | false | FeatureClass::PRIVATE
+    ProjectClass::PRIVATE  | FeatureClass::ENABLED  | false | false | FeatureClass::ENABLED
+    ProjectClass::PRIVATE  | FeatureClass::PUBLIC   | false | false | FeatureClass::PUBLIC
+    ProjectClass::INTERNAL | FeatureClass::DISABLED | false | false | FeatureClass::DISABLED
+    ProjectClass::INTERNAL | FeatureClass::PRIVATE  | false | false | FeatureClass::PRIVATE
+    ProjectClass::INTERNAL | FeatureClass::ENABLED  | false | false | FeatureClass::ENABLED
+    ProjectClass::INTERNAL | FeatureClass::PUBLIC   | false | false | FeatureClass::PUBLIC
+    ProjectClass::PUBLIC   | FeatureClass::DISABLED | false | false | FeatureClass::DISABLED
+    ProjectClass::PUBLIC   | FeatureClass::PRIVATE  | false | false | FeatureClass::PRIVATE
+    ProjectClass::PUBLIC   | FeatureClass::ENABLED  | false | false | FeatureClass::ENABLED
+    ProjectClass::PUBLIC   | FeatureClass::PUBLIC   | false | false | FeatureClass::PUBLIC
+
+    # Does not change anything if pages are already private in config.json
+    # many of these cases are invalid and will not occur in production
+    ProjectClass::PRIVATE  | FeatureClass::DISABLED | true | true | FeatureClass::DISABLED
+    ProjectClass::PRIVATE  | FeatureClass::PRIVATE  | true | true | FeatureClass::PRIVATE
+    ProjectClass::PRIVATE  | FeatureClass::ENABLED  | true | true | FeatureClass::ENABLED
+    ProjectClass::PRIVATE  | FeatureClass::PUBLIC   | true | true | FeatureClass::PUBLIC
+    ProjectClass::INTERNAL | FeatureClass::DISABLED | true | true | FeatureClass::DISABLED
+    ProjectClass::INTERNAL | FeatureClass::PRIVATE  | true | true | FeatureClass::PRIVATE
+    ProjectClass::INTERNAL | FeatureClass::ENABLED  | true | true | FeatureClass::ENABLED
+    ProjectClass::INTERNAL | FeatureClass::PUBLIC   | true | true | FeatureClass::PUBLIC
+    ProjectClass::PUBLIC   | FeatureClass::DISABLED | true | true | FeatureClass::DISABLED
+    ProjectClass::PUBLIC   | FeatureClass::PRIVATE  | true | true | FeatureClass::PRIVATE
+    ProjectClass::PUBLIC   | FeatureClass::ENABLED  | true | true | FeatureClass::ENABLED
+    ProjectClass::PUBLIC   | FeatureClass::PUBLIC   | true | true | FeatureClass::PUBLIC
+
+    # when pages are deployed and ac is disabled in config
+    ProjectClass::PRIVATE  | FeatureClass::DISABLED | true | false | FeatureClass::DISABLED
+    ProjectClass::PRIVATE  | FeatureClass::PRIVATE  | true | false | FeatureClass::PUBLIC   # need to update
+    ProjectClass::PRIVATE  | FeatureClass::ENABLED  | true | false | FeatureClass::PUBLIC   # invalid state, need to update
+    ProjectClass::PRIVATE  | FeatureClass::PUBLIC   | true | false | FeatureClass::PUBLIC
+    ProjectClass::INTERNAL | FeatureClass::DISABLED | true | false | FeatureClass::DISABLED
+    ProjectClass::INTERNAL | FeatureClass::PRIVATE  | true | false | FeatureClass::PUBLIC   # need to update
+    ProjectClass::INTERNAL | FeatureClass::ENABLED  | true | false | FeatureClass::PUBLIC   # invalid state, need to update
+    ProjectClass::INTERNAL | FeatureClass::PUBLIC   | true | false | FeatureClass::PUBLIC
+    ProjectClass::PUBLIC   | FeatureClass::DISABLED | true | false | FeatureClass::DISABLED
+    ProjectClass::PUBLIC   | FeatureClass::PRIVATE  | true | false | FeatureClass::ENABLED  # need to update
+    ProjectClass::PUBLIC   | FeatureClass::ENABLED  | true | false | FeatureClass::ENABLED
+    ProjectClass::PUBLIC   | FeatureClass::PUBLIC   | true | false | FeatureClass::ENABLED  # invalid state, need to update
+  end
+
+  with_them do
+    it 'fixes settings' do
+      perform_enqueued_jobs do
+        project = create_project(visibility_level, pages_access_level, pages_deployed, ac_is_enabled_in_config)
+
+        expect(project.reload.project_feature.pages_access_level).to eq(pages_access_level)
+
+        migrate!
+
+        expect(project.reload.project_feature.pages_access_level).to eq(result_pages_access_level)
+      end
+    end
+  end
+
+  def create_project(visibility_level, pages_access_level, pages_deployed, ac_is_enabled_in_config, path = 'project')
+    project = ProjectClass.create!(path: path, visibility_level: visibility_level,
+                                     namespace_id: subgroup.id)
+
+    if pages_deployed
+      FileUtils.mkdir_p(project.public_pages_path)
+
+      # write config.json
+      allow(project).to receive(:public_pages?).and_return(!ac_is_enabled_in_config)
+      UpdatePagesConfigurationService.new(project).execute
+    end
+
+    project.update!(visibility_level: visibility_level)
+    FeatureClass.create!(project: project, pages_access_level: pages_access_level)
+
+    project
+  end
+end
