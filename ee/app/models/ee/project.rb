a9e24eab6e84ec1b63441f85845cbbcc6d7ec485
Małgorzata Ksionek <mksionek@gitlab.com>
2019-10-18 13:22:43 +0200

    Remove unneeded differences

--- ee/app/models/ee/project.rb
+++ ee/app/models/ee/project.rb
@@ -641,8 +641,7 @@ module EE
     end
 
     def alerts_service_available?
-      ::Feature.enabled?(:generic_alert_endpoint, self) &&
-        feature_available?(:incident_management)
+      feature_available?(:incident_management)
     end
 
     def package_already_taken?(package_name)
