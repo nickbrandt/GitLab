50fdd5656e10a78ea08d29d0e7d24faa06705967
Małgorzata Ksionek <mksionek@gitlab.com>
2019-10-18 09:17:01 +0200

    Add db changes
    Add new columns to projects and application settings

---
+++ db/migrate/20191003161032_add_mark_for_deletion_indexes_to_projects.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# See http://doc.gitlab.com/ce/development/migration_style_guide.html
+# for more information on how to write migrations for GitLab.
+
+class AddMarkForDeletionIndexesToProjects < ActiveRecord::Migration[5.2]
+  include Gitlab::Database::MigrationHelpers
+
+  # Set this constant to true if this migration requires downtime.
+  DOWNTIME = false
+
+  # To disable transactions uncomment the following line and remove these
+  # comments:
+  disable_ddl_transaction!
+
+  def up
+    add_concurrent_foreign_key :projects, :users, column: :marked_for_deletion_by_id, on_delete: :nullify
+  end
+
+  def down
+    remove_foreign_key_if_exists :projects, column: :marked_for_deletion_by_id
+  end
+end
