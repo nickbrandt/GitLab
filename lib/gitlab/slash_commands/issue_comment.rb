7d465e21f735924de21df40c63b5b6692b375010
Vitali Tatarintev <vtatarintev@gitlab.com>
2019-10-22 14:33:50 +0200

    Add "issue comment" Slack command
    Introduces a Slack slash command to add a comment to an issue
    
    https://gitlab.com/gitlab-org/gitlab/issues/30822

---
+++ lib/gitlab/slash_commands/issue_comment.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module SlashCommands
+    class IssueComment < IssueCommand
+      def self.match(text)
+        /\Aissue\s+comment\s+#{Issue.reference_prefix}?(?<iid>\d+)\n*(?<note_body>(.|\n)*)/.match(text)
+      end
+
+      def self.help_message
+        'issue comment <id> *`⇧ Shift`*+*`↵ Enter`* <comment>'
+      end
+
+      def execute(match)
+        note_body = match[:note_body].to_s.strip
+        issue = find_by_iid(match[:iid])
+
+        return not_found unless issue
+
+        note = create_note(issue: issue, note: note_body)
+
+        if note.persisted?
+          presenter(note).present
+        else
+          presenter(note).display_errors
+        end
+      end
+
+      private
+
+      def not_found
+        Gitlab::SlashCommands::Presenters::Access.new.not_found
+      end
+
+      def create_note(issue:, note:)
+        note_params = { noteable: issue, note: note }
+
+        Notes::CreateService.new(project, current_user, note_params).execute
+      end
+
+      def presenter(note)
+        Gitlab::SlashCommands::Presenters::IssueComment.new(note)
+      end
+    end
+  end
+end
