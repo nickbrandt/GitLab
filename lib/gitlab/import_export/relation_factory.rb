nickbrandt/GitLab

commit e404c31152cb6ce7acc82969ba103505912946ce

Deprecate issues and merge requests state column
This is the second patch of the plan to migrate issues and
merge requests state column from string to integer type.
Deprecates usage  of old state:string column in ruby code.

lib/gitlab/import_export/relation_factory.rb
@@ -292,7 +292,11 @@ module Gitlab
 
             existing_object
           else
-            relation_class.new(parsed_relation_hash)
+            object = relation_class.new
+
+            # Use #assign_attributes here to call object custom setters
+            object.assign_attributes(parsed_relation_hash)
+            object
           end
         end
       end

spec/lib/gitlab/import_export/project_tree_restorer_spec.rb
@@ -434,6 +434,11 @@ describe Gitlab::ImportExport::ProjectTreeRestorer do
                       labels: 0,
                       milestones: 0,
                       first_issue_labels: 1
+
+      it 'restores issue states' do
+        expect(project.issues.with_state(:closed).count).to eq(1)
+        expect(project.issues.with_state(:opened).count).to eq(1)
+      end
     end
 
     context 'with existing group models' do

app/finders/issuable_finder.rb
@@ -161,7 +161,7 @@ class IssuableFinder
     labels_count = label_names.any? ? label_names.count : 1
     labels_count = 1 if use_cte_for_search?
 
-    finder.execute.reorder(nil).group(:state).count.each do |key, value|
+    finder.execute.reorder(nil).group(:state_id).count.each do |key, value|
       counts[count_key(key)] += value / labels_count
     end
 
@@ -385,7 +385,8 @@ class IssuableFinder
   end
 
   def count_key(value)
-    Array(value).last.to_sym
+    value = Array(value).last
+    klass.available_states.key(value)
   end
 
   # Negates all params found in `negatable_params`
@@ -444,7 +445,6 @@ class IssuableFinder
     items
   end
 
-  # rubocop: disable CodeReuse/ActiveRecord
   def by_state(items)
     case params[:state].to_s
     when 'closed'
@@ -454,12 +454,11 @@ class IssuableFinder
     when 'opened'
       items.opened
     when 'locked'
-      items.where(state: 'locked')
+      items.with_state(:locked)
     else
       items
     end
   end
-  # rubocop: enable CodeReuse/ActiveRecord
 
   def by_group(items)
     # Selection by group is already covered by `by_project` and `projects`

app/models/concerns/issuable.rb
@@ -32,6 +32,13 @@ module Issuable
   DESCRIPTION_LENGTH_MAX = 1.megabyte
   DESCRIPTION_HTML_LENGTH_MAX = 5.megabytes
 
+  STATE_ID_MAP = {
+    opened: 1,
+    closed: 2,
+    merged: 3,
+    locked: 4
+  }.with_indifferent_access.freeze
+
   # This object is used to gather issuable meta data for displaying
   # upvotes, downvotes, notes and closing merge requests count for issues and merge requests
   # lists avoiding n+1 queries and improving performance.
@@ -173,13 +180,17 @@ module Issuable
       fuzzy_search(query, [:title])
     end
 
-    # Available state values persisted in state_id column using state machine
+    def available_states
+      @available_states ||= STATE_ID_MAP.slice(*available_state_names)
+    end
+
+    # Available state names used to persist state_id column using state machine
     #
     # Override this on subclasses if different states are needed
     #
-    # Check MergeRequest.available_states for example
-    def available_states
-      @available_states ||= { opened: 1, closed: 2 }.with_indifferent_access
+    # Check MergeRequest.available_states_names for example
+    def available_state_names
+      [:opened, :closed]
     end
 
     # Searches for records with a matching title or description.
@@ -296,6 +307,14 @@ module Issuable
     def parent_class
       ::Project
     end
+  end
+
+  def state
+    self.class.available_states.key(state_id)
+  end
+
+  def state=(value)
+    self.state_id = self.class.available_states[value]
   end
 
   def resource_parent

app/models/concerns/issuable_states.rb
@@ -4,22 +4,20 @@ module IssuableStates
   extend ActiveSupport::Concern
 
   # The state:string column is being migrated to state_id:integer column
-  # This is a temporary hook to populate state_id column with new values
-  # and should be removed after the state column is removed.
-  # Check https://gitlab.com/gitlab-org/gitlab-foss/issues/51789 for more information
+  # This is a temporary hook to keep state column in sync until it is removed.
+  # Check https: https://gitlab.com/gitlab-org/gitlab/issues/33814 for more information
+  # The state column can be safely removed after 2019-10-27
   included do
-    before_save :set_state_id
+    before_save :sync_issuable_deprecated_state
   end
 
-  def set_state_id
-    return if state.nil? || state.empty?
+  def sync_issuable_deprecated_state
+    return if self.is_a?(Epic)
+    return unless respond_to?(:state)
+    return if state_id.nil?
 
-    # Needed to prevent breaking some migration specs that
-    # rollback database to a point where state_id does not exist.
-    # We can use this guard clause for now since this file will
-    # be removed in the next release.
-    return unless self.has_attribute?(:state_id)
+    deprecated_state = self.class.available_states.key(state_id)
 
-    self.state_id = self.class.available_states[state]
+    self.write_attribute(:state, deprecated_state)
   end
 end

app/models/concerns/milestoneish.rb
@@ -6,7 +6,9 @@ module Milestoneish
   end
 
   def closed_issues_count(user)
-    count_issues_by_state(user)['closed'].to_i
+    closed_state_id = Issue.available_states[:closed]
+
+    count_issues_by_state(user)[closed_state_id].to_i
   end
 
   def complete?(user)
@@ -117,7 +119,7 @@ module Milestoneish
 
   def count_issues_by_state(user)
     memoize_per_user(user, :count_issues_by_state) do
-      issues_visible_to_user(user).reorder(nil).group(:state).count
+      issues_visible_to_user(user).reorder(nil).group(:state_id).count
     end
   end
 

app/models/issue.rb
@@ -71,7 +71,7 @@ class Issue < ApplicationRecord
   attr_spammable :title, spam_title: true
   attr_spammable :description, spam_description: true
 
-  state_machine :state, initial: :opened do
+  state_machine :state_id, initial: :opened do
     event :close do
       transition [:opened] => :closed
     end
@@ -80,8 +80,8 @@ class Issue < ApplicationRecord
       transition closed: :opened
     end
 
-    state :opened
-    state :closed
+    state :opened, value: Issue.available_states[:opened]
+    state :closed, value: Issue.available_states[:closed]
 
     before_transition any => :closed do |issue|
       issue.closed_at = issue.system_note_timestamp
@@ -91,6 +91,13 @@ class Issue < ApplicationRecord
       issue.closed_at = nil
       issue.closed_by = nil
     end
+  end
+
+  # Alias to state machine .with_state_id method
+  # This needs to be defined after the state machine block to avoid errors
+  class << self
+    alias_method :with_state, :with_state_id
+    alias_method :with_states, :with_state_ids
   end
 
   def self.relative_positioning_query_base(issue)

app/models/merge_request.rb
@@ -85,7 +85,13 @@ class MergeRequest < ApplicationRecord
   # when creating new merge request
   attr_accessor :can_be_created, :compare_commits, :diff_options, :compare
 
-  state_machine :state, initial: :opened do
+  # Keep states definition to be evaluated before the state_machine block to avoid spec failures.
+  # If this gets evaluated after, the `merged` and `locked` states which are overrided can be nil.
+  def self.available_state_names
+    super + [:merged, :locked]
+  end
+
+  state_machine :state_id, initial: :opened do
     event :close do
       transition [:opened] => :closed
     end
@@ -116,10 +122,17 @@ class MergeRequest < ApplicationRecord
       end
     end
 
-    state :opened
-    state :closed
-    state :merged
-    state :locked
+    state :opened, value: MergeRequest.available_states[:opened]
+    state :closed, value: MergeRequest.available_states[:closed]
+    state :merged, value: MergeRequest.available_states[:merged]
+    state :locked, value: MergeRequest.available_states[:locked]
+  end
+
+  # Alias to state machine .with_state_id method
+  # This needs to be defined after the state machine block to avoid errors
+  class << self
+    alias_method :with_state, :with_state_id
+    alias_method :with_states, :with_state_ids
   end
 
   state_machine :merge_status, initial: :unchecked do
@@ -209,10 +222,6 @@ class MergeRequest < ApplicationRecord
 
   def self.reference_prefix
     '!'
-  end
-
-  def self.available_states
-    @available_states ||= super.merge(merged: 3, locked: 4)
   end
 
   # Returns the top 100 target branches

commit 8aebc174415b6b4724743b81b78490954214a4ad

Fix project imports not working with serialized data
The change in e404c31152cb6ce7acc82969ba103505912946ce broke models that
relied on single-type inheritance:

```
CommitStatus.new(type: 'Ci::Build').class
=> Ci::Build

CommitStatus.new.tap{|c| c.assign_attributes(type: 'Ci::Build')}.class
=> CommitStatus
```

As a result, any serialized options would not be properly saved to the
database, leading to errors expecting a `Hash` vs. a `String`, for
example.

This affected all pipelines that used shared runners because the build
would error out due to this serialization error.

Closes https://gitlab.com/gitlab-org/gitlab/issues/34860

lib/gitlab/import_export/relation_factory.rb
@@ -292,9 +292,11 @@ module Gitlab
 
             existing_object
           else
-            object = relation_class.new
-
-            # Use #assign_attributes here to call object custom setters
+            # Because of single-type inheritance, we need to be careful to use the `type` field
+            # See https://gitlab.com/gitlab-org/gitlab/issues/34860#note_235321497
+            inheritance_column = relation_class.try(:inheritance_column)
+            inheritance_attributes = parsed_relation_hash.slice(inheritance_column)
+            object = relation_class.new(inheritance_attributes)
             object.assign_attributes(parsed_relation_hash)
             object
           end

spec/lib/gitlab/import_export/project_tree_restorer_spec.rb
@@ -283,6 +283,10 @@ describe Gitlab::ImportExport::ProjectTreeRestorer do
         it 'correctly restores association between a pipeline and a job' do
           expect(CommitStatus.all).to all(have_attributes(pipeline_id: a_value > 0))
         end
+
+        it 'restores a Hash for CommitStatus options' do
+          expect(CommitStatus.all.map(&:options).compact).to all(be_a(Hash))
+        end
       end
     end
   end