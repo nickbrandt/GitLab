deb3e2e44f4bed40c4e444d2abe4a7a93526d6ee
Alex Buijs <abuijs@gitlab.com>
2019-10-25 17:32:45 +0200

    Trigger tracking event without user interaction

--- app/assets/javascripts/pages/sessions/new/index.js
+++ app/assets/javascripts/pages/sessions/new/index.js
@@ -20,12 +20,14 @@ document.addEventListener('DOMContentLoaded', () => {
   // Save the URL fragment from the current window location. This will be present if the user was
   // redirected to sign-in after attempting to access a protected URL that included a fragment.
   preserveUrlFragment(window.location.hash);
-
-  if (gon.tracking_data) {
-    const tab = document.querySelector(".new-session-tabs a[href='#register-pane']");
-    const { category, action, ...data } = gon.tracking_data;
-    tab.addEventListener('click', () => {
-      Tracking.event(category, action, data);
-    });
-  }
 });
+
+if (gon.tracking_data) {
+  const { category, action, ...data } = gon.tracking_data;
+
+  $('#signin-container a[data-toggle="tab"]').on('shown.bs.tab', e => {
+    if (e.target.dataset.qaSelector === 'register_tab') {
+      Tracking.event(category, action, data);
+    }
+  });
+}
