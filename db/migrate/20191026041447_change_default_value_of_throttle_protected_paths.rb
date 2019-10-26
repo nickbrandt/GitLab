ea66022b88322d596e746648331256920e0ae548
Stan Hu <stanhu@gmail.com>
2019-10-25 22:26:02 -0700

    Force the throttle_protected_paths_enabled toggle off
    Because we already set the value to true in the previous migration, this
    feature was switched on inadvertently in GitLab 12.4. This migration
    toggles it back off to ensure we don't inadvertently block legitimate
    users. The admin will have to re-enable it in the application settings.

--- db/migrate/20191026041447_change_default_value_of_throttle_protected_paths.rb
+++ db/migrate/20191026041447_change_default_value_of_throttle_protected_paths.rb
@@ -3,10 +3,22 @@
 class ChangeDefaultValueOfThrottleProtectedPaths < ActiveRecord::Migration[5.2]
   include Gitlab::Database::MigrationHelpers
 
-  # Set this constant to true if this migration requires downtime.
   DOWNTIME = false
 
-  def change
-    change_column_default :application_settings, :throttle_protected_paths_enabled, from: true, to: false
+  def up
+    change_column_default :application_settings, :throttle_protected_paths_enabled, false
+
+    # Because we already set the value to true in the previous
+    # migration, this feature was switched on inadvertently in GitLab
+    # 12.4. This migration toggles it back off to ensure we don't
+    # inadvertently block legitimate users. The admin will have to
+    # re-enable it in the application settings.
+    execute "UPDATE application_settings SET throttle_protected_paths_enabled = #{false_value}"
+  end
+
+  def down
+    change_column_default :application_settings, :throttle_protected_paths_enabled, true
+
+    execute "UPDATE application_settings SET throttle_protected_paths_enabled = #{true_value}"
   end
 end
