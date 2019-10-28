d5c5e00c7ab5c4b982a79cf597474ed1c8976e45
Miguel Rincon <mrincon@gitlab.com>
2019-10-28 14:30:33 +0800

    Readded empty bundle file

--- ee/app/assets/javascripts/pages/projects/environments/logs/index.js
+++ ee/app/assets/javascripts/pages/projects/environments/logs/index.js
@@ -1,7 +1,8 @@
+import logsBundle from 'ee/logs/logs_bundle';
 import KubernetesLogs from '../../../../kubernetes_logs';
 
 if (gon.features.environmentLogsUseVueUi) {
-  document.addEventListener('DOMContentLoaded', () => {});
+  document.addEventListener('DOMContentLoaded', logsBundle);
 } else {
   document.addEventListener('DOMContentLoaded', () => {
     const kubernetesLogContainer = document.querySelector('.js-kubernetes-logs');
