43d2d32dfb3fecfc5ac1c72592ba3cc1b9d0dc10
Natalia Tepluhina <ntepluhina@gitlab.com>
2019-10-28 08:30:31 +0000

    Created a selector for a given design
    - added selection by id and ids

--- ee/app/finders/design_management/designs_finder.rb
+++ ee/app/finders/design_management/designs_finder.rb
@@ -2,8 +2,11 @@
 
 module DesignManagement
   class DesignsFinder
-    attr_reader :issue, :current_user, :params
+    include Gitlab::Allowable
 
+    # Params:
+    # ids: integer[]
+    # visible_at_version: ?version
     def initialize(issue, current_user, params = {})
       @issue = issue
       @current_user = current_user
@@ -11,18 +14,33 @@ module DesignManagement
     end
 
     def execute
-      unless Ability.allowed?(current_user, :read_design, issue)
-        return ::DesignManagement::Design.none
-      end
+      items = init_collection
 
-      by_visible_at_version(issue.designs)
+      items = by_visible_at_version(items)
+      items = by_ids(items)
+
+      items
     end
 
     private
 
+    attr_reader :issue, :current_user, :params
+
+    def init_collection
+      return ::DesignManagement::Design.none unless can?(current_user, :read_design, issue)
+
+      issue.designs
+    end
+
     # Returns all designs that existed at a particular design version
     def by_visible_at_version(items)
       items.visible_at_version(params[:visible_at_version])
+    end
+
+    def by_ids(items)
+      return items unless params[:ids].present?
+
+      items.id_in(params[:ids])
     end
   end
 end
