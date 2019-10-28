7b16422c21f374507a81ef474525758beb0c1e3f
Stan Hu <stanhu@gmail.com>
2019-10-28 14:54:33 -0700

    Only override default if Omnibus protected paths is not enabled

--- db/migrate/20191026041447_change_default_value_of_throttle_protected_paths.rb
+++ db/migrate/20191026041447_change_default_value_of_throttle_protected_paths.rb
@@ -13,12 +13,26 @@ class ChangeDefaultValueOfThrottleProtectedPaths < ActiveRecord::Migration[5.2]
     # 12.4. This migration toggles it back off to ensure we don't
     # inadvertently block legitimate users. The admin will have to
     # re-enable it in the application settings.
-    execute "UPDATE application_settings SET throttle_protected_paths_enabled = #{false_value}"
+    unless omnibus_protected_paths_present?
+      execute "UPDATE application_settings SET throttle_protected_paths_enabled = #{false_value}"
+    end
   end
 
   def down
     change_column_default :application_settings, :throttle_protected_paths_enabled, true
 
     execute "UPDATE application_settings SET throttle_protected_paths_enabled = #{true_value}"
+  end
+
+  private
+
+  def omnibus_protected_paths_present?
+    Rack::Attack.throttles.key?('protected paths')
+  rescue e
+    say "Error while checking if Omnibus protected paths were already enabled: #{e.message}"
+    say 'Continuing. Protected paths will remain enabled.'
+
+    # Return true so we don't take a risk
+    true
   end
 end
