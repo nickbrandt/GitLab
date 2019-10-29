22c01303d44a7ae988ff808c791ab5d3d402a2b7
Adam Hegyi <ahegyi@gitlab.com>
2019-10-29 06:00:28 +0000

    Expose CA median and records via API
    This MR exposes to the frontend the median and the related records
    calculated for a given stage.

---
+++ ee/lib/gitlab/analytics/cycle_analytics/request_params.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module Analytics
+    module CycleAnalytics
+      class RequestParams
+        include ActiveModel::Model
+        include ActiveModel::Validations
+        include ActiveModel::Attributes
+
+        attribute :created_after, :date
+        attribute :created_before, :date
+
+        validates :created_after, presence: true
+        validates :created_before, presence: true
+
+        validate :validate_created_before
+
+        private
+
+        def validate_created_before
+          return if created_after.nil? || created_before.nil?
+
+          errors.add(:created_before, :invalid) if created_after > created_before
+        end
+      end
+    end
+  end
+end
