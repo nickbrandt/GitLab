52bdd4c3d2086477f278e1fe836e870dd27ef67e
leiminghuan <leiminghuan@126.com>
2019-10-30 07:02:53 +0800

    remove unusable codes

--- db/migrate/20191013100213_add_squash_commit_sha_to_merge_requests.rb
+++ db/migrate/20191013100213_add_squash_commit_sha_to_merge_requests.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 class AddSquashCommitShaToMergeRequests < ActiveRecord::Migration[5.2]
-  include Gitlab::Database::MigrationHelpers
 
   DOWNTIME = false
-
-  # disable_ddl_transaction!
 
   def change
     add_column :merge_requests, :squash_commit_sha, :varchar
