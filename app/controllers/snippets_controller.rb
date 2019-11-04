791903bdb2cc2b717810fb7b82cee368d822cf20
Francisco Javier López <fjlopez@gitlab.com>
2019-11-04 19:02:29 +0100

    Simplifying MR

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -5,8 +5,6 @@ class SnippetsController < ApplicationController
   include ToggleAwardEmoji
   include SpammableActions
   include SnippetsActions
-  include SnippetsHelper
-  # include SnippetsUrl
   include RendersBlob
   include PreviewMarkdown
   include PaginatedCollection
@@ -19,9 +17,6 @@ class SnippetsController < ApplicationController
 
   # Allow read snippet
   before_action :authorize_read_snippet!, only: [:show, :raw]
-
-  # Ensure we're displaying the correct url, specifically for secret snippets
-  # before_action :ensure_complete_url, only: [:show, :raw]
 
   # Allow modify snippet
   before_action :authorize_update_snippet!, only: [:edit, :update]
@@ -124,7 +119,7 @@ class SnippetsController < ApplicationController
   alias_method :spammable, :snippet
 
   def spammable_path
-    reliable_snippet_path(@snippet)
+    snippet_path(@snippet)
   end
 
   def authorize_read_snippet!
