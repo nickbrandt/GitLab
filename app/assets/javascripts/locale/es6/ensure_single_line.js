2a4a5923cd71bdafce9699b4a16071255044b9f7
jakeburden <jburden@gitlab.com>
2019-11-05 10:49:00 -0500

    Set babel to utilize `useBuiltIns: "usage”`
    Removes explict calls to core-js polyfills.
    Lets babel find polyfills when needed for our target browsers.

---
+++ app/assets/javascripts/locale/es6/ensure_single_line.js
@@ -0,0 +1,5 @@
+// co-locating ensure_single_line to satisfy static analysis while allowing Node.js to import the function with CommonJS
+
+import ensureSingleLine from '../ensure_single_line';
+
+export default ensureSingleLine;
