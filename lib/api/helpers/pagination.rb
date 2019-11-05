88d33022db741598aee667d2fc1237a560ac98ad
Andreas Brandl <abrandl@gitlab.com>
2019-11-05 13:39:34 +0100

    Keyset pagination (id based)
    This is the squashed together change from
    https://gitlab.com/gitlab-org/gitlab/merge_requests/19623
    that had been reverted previously.
    
    Based on the refactoring in
    https://gitlab.com/gitlab-org/gitlab/merge_requests/21130

--- lib/api/helpers/pagination.rb
+++ lib/api/helpers/pagination.rb
@@ -4,13 +4,27 @@ module API
   module Helpers
     module Pagination
       def paginate(relation)
-        ::Gitlab::Pagination::OffsetPagination.new(self).paginate(relation)
+        return Gitlab::Pagination::OffsetPagination.new(self).paginate(relation) unless keyset_pagination_enabled?
+
+        request_context = Gitlab::Pagination::Keyset::RequestContext.new(self)
+
+        unless Gitlab::Pagination::Keyset.available?(request_context, relation)
+          return error!('Keyset pagination is not yet available for this type of request', 501)
+        end
+
+        Gitlab::Pagination::Keyset.paginate(request_context, relation)
       end
 
       # This applies pagination and executes the query
       # It always returns an array instead of an ActiveRecord relation
       def paginate_and_retrieve!(relation)
         paginate(relation).to_a
+      end
+
+      private
+
+      def keyset_pagination_enabled?
+        params[:pagination] == 'keyset' && Feature.enabled?(:api_keyset_pagination, default_enabled: true)
       end
     end
   end
