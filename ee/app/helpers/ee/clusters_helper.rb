dbf333335282a8297d20ed18b0b9df4f78fd897a
Olena Horal-Koretska <ohoralkoretska@gitlab.com>
2019-11-07 13:59:16 +0200

    Update link to docs on cluster page

--- ee/app/helpers/ee/clusters_helper.rb
+++ ee/app/helpers/ee/clusters_helper.rb
@@ -15,17 +15,17 @@ module EE
 
     def cluster_health_data(cluster)
       {
-        'clusters-path': clusterable.index_path,
-        'metrics-endpoint': clusterable.metrics_cluster_path(cluster, format: :json),
-        'dashboard-endpoint': clusterable.metrics_dashboard_path(cluster),
-        'documentation-path': help_page_path('administration/monitoring/prometheus/index.md'),
-        'empty-getting-started-svg-path': image_path('illustrations/monitoring/getting_started.svg'),
-        'empty-loading-svg-path': image_path('illustrations/monitoring/loading.svg'),
-        'empty-no-data-svg-path': image_path('illustrations/monitoring/no_data.svg'),
-        'empty-unable-to-connect-svg-path': image_path('illustrations/monitoring/unable_to_connect.svg'),
-        'settings-path': '',
-        'project-path': '',
-        'tags-path': ''
+        'clusters-path' => clusterable.index_path,
+        'metrics-endpoint' => clusterable.metrics_cluster_path(cluster, format: :json),
+        'dashboard-endpoint' => clusterable.metrics_dashboard_path(cluster),
+        'documentation-path' => help_page_path('user/project/clusters/index', anchor: 'monitoring-your-kubernetes-cluster-ultimate'),
+        'empty-getting-started-svg-path' => image_path('illustrations/monitoring/getting_started.svg'),
+        'empty-loading-svg-path' => image_path('illustrations/monitoring/loading.svg'),
+        'empty-no-data-svg-path' => image_path('illustrations/monitoring/no_data.svg'),
+        'empty-unable-to-connect-svg-path' => image_path('illustrations/monitoring/unable_to_connect.svg'),
+        'settings-path' => '',
+        'project-path' => '',
+        'tags-path' => ''
       }
     end
   end
