d370e11f7a163b71575c28a3e65c7b0f325b9885
Heinrich Lee Yu <heinrich@gitlab.com>
2019-11-12 04:28:23 +0800

    Fix unsupported routes in Rails 6
    `nil` format is not supported and throws an exception
    
    Regex anchors in constraints are also not supported anymore. These
    shouldn't be needed anyway

--- ee/config/routes/project.rb
+++ ee/config/routes/project.rb
@@ -25,7 +25,7 @@ constraints(::Constraints::ProjectUrlConstrainer.new) do
 
         resources :jobs, only: [], constraints: { id: /\d+/ } do
           member do
-            get '/proxy.ws/authorize', to: 'jobs#proxy_websocket_authorize', constraints: { format: nil }
+            get '/proxy.ws/authorize', to: 'jobs#proxy_websocket_authorize', format: false
             get :proxy
           end
         end
