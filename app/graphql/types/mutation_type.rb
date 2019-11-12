01b2974e3e1c51f3ef399f277b508a8dfff05fba
Mario de la Ossa <mariodelaossa@gmail.com>
2019-11-11 18:54:05 -0600

    GraphQL: Add MRSetSubscription mutation
    Adds a mutation for the current user to subscribe/unsubscribe from a
    Merge Request.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -12,6 +12,7 @@ module Types
     mount_mutation Mutations::MergeRequests::SetLabels
     mount_mutation Mutations::MergeRequests::SetLocked
     mount_mutation Mutations::MergeRequests::SetMilestone
+    mount_mutation Mutations::MergeRequests::SetSubscription
     mount_mutation Mutations::MergeRequests::SetWip, calls_gitaly: true
     mount_mutation Mutations::MergeRequests::SetAssignees
     mount_mutation Mutations::Notes::Create::Note, calls_gitaly: true
