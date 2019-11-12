17911b6256b813e058fffd6963b0bf6033bcdedb
Mahendra Bagul <mahendra@infracloud.com>
2019-11-12 18:34:28 +0530

    Changes to create role and binding for crossplane database

--- app/services/clusters/kubernetes/create_or_update_service_account_service.rb
+++ app/services/clusters/kubernetes/create_or_update_service_account_service.rb
@@ -49,6 +49,8 @@ module Clusters
 
         create_or_update_knative_serving_role
         create_or_update_knative_serving_role_binding
+        create_or_update_crossplane_serving_role
+        create_or_update_crossplane_serving_role_binding
       end
 
       private
@@ -76,6 +78,14 @@ module Clusters
 
       def create_or_update_knative_serving_role_binding
         kubeclient.update_role_binding(knative_serving_role_binding_resource)
+      end
+
+      def create_or_update_crossplane_serving_role
+        kubeclient.update_role(crossplane_serving_role_resource)
+      end
+
+      def create_or_update_crossplane_serving_role_binding
+        kubeclient.update_role_binding(crossplane_serving_role_binding_resource)
       end
 
       def service_account_resource
@@ -132,6 +142,28 @@ module Clusters
           role_kind: :Role,
           namespace: service_account_namespace,
           service_account_name: service_account_name
+        ).generate
+      end
+
+      def crossplane_serving_role_resource
+        Gitlab::Kubernetes::Role.new(
+            name: Clusters::Kubernetes::GITLAB_CROSSPLANE_DATABASE_ROLE_NAME,
+            namespace: service_account_namespace,
+            rules: [{
+                        apiGroups: %w(database.crossplane.io),
+                        resources: %w(postgresqlinstances),
+                        verbs: %w(get list create watch)
+                    }]
+        ).generate
+      end
+
+      def crossplane_serving_role_binding_resource
+        Gitlab::Kubernetes::RoleBinding.new(
+            name: Clusters::Kubernetes::GITLAB_CROSSPLANE_DATABASE_ROLE_BINDING_NAME,
+            role_name: Clusters::Kubernetes::GITLAB_CROSSPLANE_DATABASE_ROLE_NAME,
+            role_kind: :Role,
+            namespace: service_account_namespace,
+            service_account_name: service_account_name
         ).generate
       end
     end
