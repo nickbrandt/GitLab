3a9f64cee6538aaf8a0b12c2dbb1863496be0ebf
Adam Hegyi <ahegyi@gitlab.com>
2019-11-12 13:10:58 +0100

    Move default Cycle Analytics stage info to method
    This change moves the nested hash that contains I18n text for the
    default cycle analytics stages to an instance method for readability
    reasons.
    
    This commit adds the missing legend attribute. Note: these strings are
    coming from the old `_stage` classes (`issue_stage`, `code_stage`,
    etc.)

--- ee/app/presenters/analytics/cycle_analytics/stage_presenter.rb
+++ ee/app/presenters/analytics/cycle_analytics/stage_presenter.rb
@@ -3,37 +3,6 @@
 module Analytics
   module CycleAnalytics
     class StagePresenter < Gitlab::View::Presenter::Delegated
-      DEFAULT_STAGE_ATTRIBUTES = {
-        issue: {
-          title: -> { s_('CycleAnalyticsStage|Issue') },
-          description: -> { _('Time before an issue gets scheduled') }
-        }.freeze,
-        plan: {
-          title: -> { s_('CycleAnalyticsStage|Plan') },
-          description: -> { _('Time before an issue starts implementation') }
-        }.freeze,
-        code: {
-          title: -> { s_('CycleAnalyticsStage|Code') },
-          description: -> { _('Time until first merge request') }
-        }.freeze,
-        test: {
-          title: -> { s_('CycleAnalyticsStage|Test') },
-          description: -> { _('Total test time for all commits/merges') }
-        }.freeze,
-        review: {
-          title: -> { s_('CycleAnalyticsStage|Review') },
-          description: -> { _('Time between merge request creation and merge/close') }
-        }.freeze,
-        staging: {
-          title: -> { s_('CycleAnalyticsStage|Staging') },
-          description: -> { _('From merge request merge until deploy to production') }
-        }.freeze,
-        production: {
-          title: -> { s_('CycleAnalyticsStage|Total') },
-          description: -> { _('From issue creation until deploy to production') }
-        }.freeze
-      }.freeze
-
       def title
         extract_default_stage_attribute(:title) || name
       end
@@ -43,13 +12,53 @@ module Analytics
       end
 
       def legend
-        ''
+        extract_default_stage_attribute(:legend) || title
       end
 
       private
 
       def extract_default_stage_attribute(attribute)
-        DEFAULT_STAGE_ATTRIBUTES.dig(name.to_sym, attribute.to_sym)&.call
+        default_stage_attributes.dig(name.to_sym, attribute.to_sym)
+      end
+
+      def default_stage_attributes
+        @default_stage_attributes ||= {
+          issue: {
+            title: s_('CycleAnalyticsStage|Issue'),
+            description: _('Time before an issue gets scheduled'),
+            legend: _("Related Issues")
+          },
+          plan: {
+            title: s_('CycleAnalyticsStage|Plan'),
+            description: _('Time before an issue starts implementation'),
+            legend: _("Related Issues")
+          },
+          code: {
+            title: s_('CycleAnalyticsStage|Code'),
+            description: _('Time until first merge request'),
+            legend: _("Related Merge Requests")
+          },
+          test: {
+            title: s_('CycleAnalyticsStage|Test'),
+            description: _('Total test time for all commits/merges'),
+            legend: _("Related Jobs")
+          },
+          review: {
+            title: s_('CycleAnalyticsStage|Review'),
+            description: _('Time between merge request creation and merge/close'),
+            legend: _("Related Merged Requests")
+          },
+          staging: {
+            title: s_('CycleAnalyticsStage|Staging'),
+            description: _('From merge request merge until deploy to production'),
+            legend: _("Related Deployed Jobs")
+          },
+          production: {
+            title: -> { s_('CycleAnalyticsStage|Total') },
+            description: -> { _('From issue creation until deploy to production') },
+            legend: _("Related Issues")
+          }
+        }
       end
     end
   end
