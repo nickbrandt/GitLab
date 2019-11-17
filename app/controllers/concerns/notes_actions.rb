9dc2e7f28db3ac660a0c1ee1301c96795c8c9765
raju249 <rajendrakadam249@gmail.com>
2019-11-17 21:27:02 +0530

    Add check for empty keys

--- app/controllers/concerns/notes_actions.rb
+++ app/controllers/concerns/notes_actions.rb
@@ -63,7 +63,7 @@ module NotesActions
           json.merge!(note_json(@note))
         end
 
-        if @note.errors && @note.errors.keys != [:commands_only]
+        if @note.errors && @note.errors.keys != [] && @note.errors.keys != [:commands_only]
           render json: json, status: 422
         else
           render json: json
