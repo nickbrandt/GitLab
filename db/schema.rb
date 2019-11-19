85b3db34e359b8dc175146ea275662e186bf8341
Mikolaj Wawrzyniak <mwawrzyniak@gitlab.com>
2019-11-19 11:58:12 +0100

    Fix db/schema CI indentation issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_11_15_121407) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
