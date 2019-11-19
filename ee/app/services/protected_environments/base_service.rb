95279fdc2bde1be7f869b0caeb871693935a55af
Krasimir Angelov <krasio@codingspree.net>
2019-11-19 15:02:22 +1300

    Filter out deploy access level for users having lower than Developer access
    Also do not allow deploy access levels that have more than one of access
    level / group_id / user_id specified (such params are not expected).
    
    Related to https://gitlab.com/gitlab-org/gitlab/issues/13686.

--- ee/app/services/protected_environments/base_service.rb
+++ ee/app/services/protected_environments/base_service.rb
@@ -8,23 +8,48 @@ module ProtectedEnvironments
     def sanitized_params
       params.dup.tap do |sanitized_params|
         sanitized_params[:deploy_access_levels_attributes] =
-          filter_valid_groups(sanitized_params[:deploy_access_levels_attributes])
+          filter_valid_deploy_access_level_attributes(sanitized_params[:deploy_access_levels_attributes])
       end
     end
 
     private
 
-    def filter_valid_groups(attributes)
+    def filter_valid_deploy_access_level_attributes(attributes)
       return unless attributes
 
-      attributes.select do |attribute|
-        attribute[:group_id].nil? || invited_group_ids.include?(attribute[:group_id])
+      attributes.select { |attribute| valid_attribute?(attribute) }
+    end
+
+    def valid_attribute?(attribute)
+      keys = attribute.slice(:access_level, :group_id, :user_id).keys
+      return false unless keys.count == 1
+
+      if attribute[:group_id].present?
+        invited_group_ids.include?(attribute[:group_id])
+      elsif attribute[:user_id].present?
+        qualified_user_ids.include?(attribute[:user_id])
+      else
+        true
       end
     end
 
     def invited_group_ids
       strong_memoize(:invited_group_ids) do
         project.invited_groups.pluck_primary_key.to_set
+      end
+    end
+
+    def qualified_user_ids
+      strong_memoize(:qualified_user_ids) do
+        user_ids = params[:deploy_access_levels_attributes].each.with_object([]) do |attribute, user_ids|
+          user_ids << attribute[:user_id] if attribute[:user_id].present?
+          user_ids
+        end
+
+        project.project_authorizations
+          .visible_to_user_and_access_level(user_ids, Gitlab::Access::DEVELOPER)
+          .pluck_user_ids
+          .to_set
       end
     end
   end
