e7882c20f1bfc12079ced68c947c3516bffc8787
Małgorzata Ksionek <mksionek@gitlab.com>
2019-11-21 17:02:08 +0100

    First part of refactor

--- db/post_migrate/20191118132107_update_existing_subgroup_to_match_visibility_level_of_parent.rb
+++ db/post_migrate/20191118132107_update_existing_subgroup_to_match_visibility_level_of_parent.rb
@@ -21,30 +21,32 @@ class UpdateExistingSubgroupToMatchVisibilityLevelOfParent < ActiveRecord::Migra
 
   def update_group_visibility_level
     group_ids = execute <<~SQL
-      WITH RECURSIVE base_and_descendants AS
-        (SELECT namespaces.id,
-          namespaces.visibility_level, namespaces.parent_id, 0 AS parent_level
+      SELECT array_agg(id) as ids, min from (WITH RECURSIVE base_and_descendants AS (
+        (SELECT visibility_level AS strictest_parent_level,
+                namespaces.*
           FROM namespaces
-          WHERE parent_id IS NOT NULL
-          And type IN ('Group')
-          UNION
-          SELECT base_and_descendants.id,
-          base_and_descendants.visibility_level, base_and_descendants.parent_id, namespaces.visibility_level AS parent_level
-          FROM namespaces, base_and_descendants
-          WHERE namespaces.type IN ('Group')
-          AND namespaces.id = base_and_descendants.parent_id)
-        SELECT id
+          WHERE namespaces.type IN ('Group'))
+        UNION
+        (SELECT LEAST(namespaces.visibility_level, base_and_descendants.strictest_parent_level) AS strictest_parent_level,
+                namespaces.*
+        FROM namespaces,
+              base_and_descendants
+        WHERE namespaces.type IN ('Group')
+          AND namespaces.parent_id = base_and_descendants.id))
+        SELECT id, MIN(strictest_parent_level) as min
         FROM base_and_descendants
-        WHERE visibility_level > parent_level
-      SQL
+        WHERE visibility_level > strictest_parent_level
+        GROUP BY id) as t1
+      GROUP BY min
+    SQL
     group_ids = group_ids.column_values(0)
 
     return if group_ids.empty?
 
-    execute("UPDATE namespaces
-      SET visibility_level = parent.visibility_level
-      FROM namespaces AS parent
-      WHERE namespaces.parent_id = parent.id
-      AND namespaces.id IN (#{group_ids.join(',')})")
-  end
+  #   execute("UPDATE namespaces
+  #     SET visibility_level = parent.visibility_level
+  #     FROM namespaces AS parent
+  #     WHERE namespaces.parent_id = parent.id
+  #     AND namespaces.id IN (#{group_ids.join(',')})")
+  # end
 end
