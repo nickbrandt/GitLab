b8a63b7ca7de5061cc3f76a320df203c0a056581
Dylan Griffith <dyl.griffith@gmail.com>
2019-11-26 16:30:39 +1100

    Hide elasticsearch namespaces and projects when too many
    As part of our rollout for
    https://gitlab.com/gitlab-org/gitlab/issues/33681 we will end up having
    thousands of namespaces in the rollout and this admin UI would break so
    it's safest to hide it.

--- ee/spec/views/admin/application_settings/_elasticsearch_form.html.haml_spec.rb
+++ ee/spec/views/admin/application_settings/_elasticsearch_form.html.haml_spec.rb
@@ -31,4 +31,56 @@ describe 'admin/application_settings/_elasticsearch_form' do
       expect(page.find_field('Enter new AWS Secret Access Key').value).to be_blank
     end
   end
+
+  context 'when there are elasticsearch indexed namespaces' do
+    let(:application_setting) { build(:application_setting, elasticsearch_limit_indexing: true) }
+
+    before do
+      create(:elasticsearch_indexed_namespace)
+      create(:elasticsearch_indexed_namespace)
+      create(:elasticsearch_indexed_namespace)
+    end
+
+    it 'shows the input' do
+      render
+      expect(rendered).to have_field('application_setting[elasticsearch_namespace_ids]')
+    end
+
+    context 'when there are too many elasticsearch indexed namespaces' do
+      before do
+        create_list :elasticsearch_indexed_namespace, 60
+      end
+
+      it 'hides the input' do
+        render
+        expect(rendered).not_to have_field('application_setting[elasticsearch_namespace_ids]')
+      end
+    end
+  end
+
+  context 'when there are elasticsearch indexed projects' do
+    let(:application_setting) { build(:application_setting, elasticsearch_limit_indexing: true) }
+
+    before do
+      create(:elasticsearch_indexed_project)
+      create(:elasticsearch_indexed_project)
+      create(:elasticsearch_indexed_project)
+    end
+
+    it 'shows the input' do
+      render
+      expect(rendered).to have_field('application_setting[elasticsearch_project_ids]')
+    end
+
+    context 'when there are too many elasticsearch indexed projects' do
+      before do
+        create_list :elasticsearch_indexed_project, 60
+      end
+
+      it 'hides the input' do
+        render
+        expect(rendered).not_to have_field('application_setting[elasticsearch_project_ids]')
+      end
+    end
+  end
 end
