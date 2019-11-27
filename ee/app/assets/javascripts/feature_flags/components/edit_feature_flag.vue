9102f7013864123dacf8a1439b5b2cf44552e207
Andrew Fontaine <afontaine@gitlab.com>
2019-11-27 11:25:01 -0500

    Show Feature Flag Toggle for Editing Feature Flags
    This is a global toggle that will enable or disable the entire flag,
    regardless of later details.

--- ee/app/assets/javascripts/feature_flags/components/edit_feature_flag.vue
+++ ee/app/assets/javascripts/feature_flags/components/edit_feature_flag.vue
@@ -1,5 +1,5 @@
 <script>
-import { GlLoadingIcon } from '@gitlab/ui';
+import { GlLoadingIcon, GlToggle } from '@gitlab/ui';
 import { createNamespacedHelpers } from 'vuex';
 import { sprintf, s__ } from '~/locale';
 import glFeatureFlagMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
@@ -12,6 +12,7 @@ export default {
   store,
   components: {
     GlLoadingIcon,
+    GlToggle,
     FeatureFlagForm,
   },
   mixins: [glFeatureFlagMixin()],
@@ -30,7 +31,16 @@ export default {
     },
   },
   computed: {
-    ...mapState(['error', 'name', 'description', 'scopes', 'isLoading', 'hasError', 'iid']),
+    ...mapState([
+      'error',
+      'name',
+      'description',
+      'scopes',
+      'isLoading',
+      'hasError',
+      'iid',
+      'active',
+    ]),
     title() {
       return this.hasFeatureFlagsIID
         ? `^${this.iid} ${this.name}`
@@ -39,13 +49,22 @@ export default {
     hasFeatureFlagsIID() {
       return this.glFeatures.featureFlagIID && this.iid;
     },
+    hasFeatureFlagToggle() {
+      return this.glFeatures.featureFlagToggle;
+    },
   },
   created() {
     this.setPath(this.path);
     return this.setEndpoint(this.endpoint).then(() => this.fetchFeatureFlag());
   },
   methods: {
-    ...mapActions(['updateFeatureFlag', 'setEndpoint', 'setPath', 'fetchFeatureFlag']),
+    ...mapActions([
+      'updateFeatureFlag',
+      'setEndpoint',
+      'setPath',
+      'fetchFeatureFlag',
+      'toggleActive',
+    ]),
   },
 };
 </script>
@@ -54,7 +73,15 @@ export default {
     <gl-loading-icon v-if="isLoading" />
 
     <template v-else-if="!isLoading && !hasError">
-      <h3 class="page-title">{{ title }}</h3>
+      <div class="d-flex align-items-center mb-3 mt-3">
+        <gl-toggle
+          v-if="hasFeatureFlagToggle"
+          :value="active"
+          class="m-0 mr-3"
+          @change="toggleActive"
+        />
+        <h3 class="page-title m-0">{{ title }}</h3>
+      </div>
 
       <div v-if="error.length" class="alert alert-danger">
         <p v-for="(message, index) in error" :key="index" class="mb-0">{{ message }}</p>
@@ -67,6 +94,7 @@ export default {
         :cancel-path="path"
         :submit-text="__('Save changes')"
         :environments-endpoint="environmentsEndpoint"
+        :active="active || !hasFeatureFlagToggle"
         @handleSubmit="data => updateFeatureFlag(data)"
       />
     </template>
