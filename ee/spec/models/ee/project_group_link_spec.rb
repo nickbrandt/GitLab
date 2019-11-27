8dffe61934d388a6365da5e6276b8998ddd2f995
Patrick Bajao <ebajao@gitlab.com>
2019-11-27 14:41:17 +0800

    Remove protected tag access when group is removed
    When a group is removed from the project, it should no longer
    be included in any protected tags.

--- ee/spec/models/ee/project_group_link_spec.rb
+++ ee/spec/models/ee/project_group_link_spec.rb
@@ -13,16 +13,46 @@ describe ProjectGroupLink do
       project.add_developer(user)
     end
 
-    it 'removes related protected environment deploy access levels' do
-      params = attributes_for(:protected_environment,
-                              deploy_access_levels_attributes: [{ group_id: group.id }, { user_id: user.id }])
+    shared_examples_for 'deleted related access levels' do |access_level_class|
+      it "removes related #{access_level_class}" do
+        expect { project_group_link.destroy! }.to change(access_level_class, :count).by(-1)
+        expect(access_levels.find_by_group_id(group)).to be_nil
+        expect(access_levels.find_by_user_id(user)).to be_persisted
+      end
+    end
 
-      protected_environment = ProtectedEnvironments::CreateService.new(project, user, params).execute
+    context 'protected tags' do
+      let!(:protected_tag) do
+        ProtectedTags::CreateService.new(
+          project,
+          project.owner,
+          attributes_for(
+            :protected_tag,
+            create_access_levels_attributes: [{ group_id: group.id }, { user_id: user.id }]
+          )
+        ).execute
+      end
 
-      expect { project_group_link.destroy! }.to change(ProtectedEnvironment::DeployAccessLevel, :count).by(-1)
+      let(:access_levels) { protected_tag.create_access_levels }
 
-      expect(protected_environment.deploy_access_levels.find_by_group_id(group)).to be_nil
-      expect(protected_environment.deploy_access_levels.find_by_user_id(user)).to be_persisted
+      it_behaves_like 'deleted related access levels', ProtectedTag::CreateAccessLevel
+    end
+
+    context 'protected environments' do
+      let!(:protected_environment) do
+        ProtectedEnvironments::CreateService.new(
+          project,
+          project.owner,
+          attributes_for(
+            :protected_environment,
+            deploy_access_levels_attributes: [{ group_id: group.id }, { user_id: user.id }]
+          )
+        ).execute
+      end
+
+      let(:access_levels) { protected_environment.deploy_access_levels }
+
+      it_behaves_like 'deleted related access levels', ProtectedEnvironment::DeployAccessLevel
     end
   end
 end
