cf2e06aa5bf9d9a8438a883d3fbc753192137c18
Denys Mishunov <dmishunov@gitlab.com>
2019-11-29 17:38:42 +0100

    Introduced the snippets_vue feature flag
    Moved pushing the FE feature flag
    
    Now, instead of doing ot from Snippets controller, we do it from
    gon_helper to make it shared between all types of snippets

--- spec/features/snippets_spec.rb
+++ spec/features/snippets_spec.rb
@@ -13,4 +13,34 @@ describe 'Snippets' do
 
     it_behaves_like 'paginated snippets'
   end
+
+  describe 'when feature flag is enabled' do
+    let(:user) { create(:user) }
+    let!(:snippets) { create(:personal_snippet, :public, author: user) }
+
+    before do
+      stub_feature_flags(snippets_vue: true)
+      visit snippet_path(1)
+    end
+
+    it 'should render Vue application' do
+      expect(page).to have_selector('#js-snippet-view')
+      expect(page).not_to have_selector('.personal-snippets')
+    end
+  end
+
+  describe 'when feature flag is disabled' do
+    let(:user) { create(:user) }
+    let!(:snippets) { create(:personal_snippet, :public, author: user) }
+
+    before do
+      stub_feature_flags(snippets_vue: false)
+      visit snippet_path(1)
+    end
+
+    it 'should render HAML application and not Vue' do
+      expect(page).not_to have_selector('#js-snippet-view')
+      expect(page).to have_selector('.personal-snippets')
+    end
+  end
 end
