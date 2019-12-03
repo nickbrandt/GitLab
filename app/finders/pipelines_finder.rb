c2d8cb7321d1e1dd5971b3577b9d9d87273f0987
Pavel Shutsin <pshutsin@gitlab.com>
2019-12-03 15:58:07 +0000

    Improve pipelines finder ordering spec

--- app/finders/pipelines_finder.rb
+++ app/finders/pipelines_finder.rb
@@ -129,7 +129,7 @@ class PipelinesFinder
   end
   # rubocop: enable CodeReuse/ActiveRecord
 
-   def by_updated_at(items)
+  def by_updated_at(items)
     items = items.updated_before(params[:updated_before]) if params[:updated_before].present?
     items = items.updated_after(params[:updated_after]) if params[:updated_after].present?
 
