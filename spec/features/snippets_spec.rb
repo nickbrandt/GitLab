6e032f669c41f062dc29e9bb53d4a37807c62a7c
Francisco Javier López <fjlopez@gitlab.com>
2019-12-03 09:16:08 +0100

    Fixed feature spec

--- spec/features/snippets_spec.rb
+++ spec/features/snippets_spec.rb
@@ -6,41 +6,38 @@ describe 'Snippets' do
   context 'when the project has snippets' do
     let(:project) { create(:project, :public) }
     let!(:snippets) { create_list(:project_snippet, 2, :public, author: project.owner, project: project) }
+
     before do
       allow(Snippet).to receive(:default_per_page).and_return(1)
-      visit snippets_path(username: project.owner.username)
+
+      visit project_snippets_path(project)
     end
 
     it_behaves_like 'paginated snippets'
   end
 
-  describe 'when feature flag is enabled' do
-    let(:user) { create(:user) }
-    let!(:snippets) { create(:personal_snippet, :public, author: user) }
+  describe 'rendering engine' do
+    let_it_be(:snippet) { create(:personal_snippet, :public) }
+    let(:snippets_vue_feature_flag_enabled) { true }
 
     before do
-      stub_feature_flags(snippets_vue: true)
-      visit snippet_path(1)
+      stub_feature_flags(snippets_vue: snippets_vue_feature_flag_enabled)
+
+      visit snippet_path(snippet)
     end
 
-    it 'should render Vue application' do
+    it 'renders Vue application' do
       expect(page).to have_selector('#js-snippet-view')
       expect(page).not_to have_selector('.personal-snippets')
     end
-  end
 
-  describe 'when feature flag is disabled' do
-    let(:user) { create(:user) }
-    let!(:snippets) { create(:personal_snippet, :public, author: user) }
+    context 'when feature flag is disabled' do
+      let(:snippets_vue_feature_flag_enabled) { false }
 
-    before do
-      stub_feature_flags(snippets_vue: false)
-      visit snippet_path(1)
-    end
-
-    it 'should render HAML application and not Vue' do
-      expect(page).not_to have_selector('#js-snippet-view')
-      expect(page).to have_selector('.personal-snippets')
+      it 'renders HAML application and not Vue' do
+        expect(page).not_to have_selector('#js-snippet-view')
+        expect(page).to have_selector('.personal-snippets')
+      end
     end
   end
 end
