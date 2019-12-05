3c7e15fc22942e03be0aa507367d07f227c81d19
Nick Kipling <nkipling@gitlab.com>
2019-12-05 11:55:49 +0000

    Added Conan recipe to package details
    Created utils for generating package information
    Created function to generate Conan recipe
    Added Conan metadata to package details
    Changed component to use utils function
    Added specs to cover utils
    Updated pot file

--- ee/app/assets/javascripts/packages/details/utils.js
+++ ee/app/assets/javascripts/packages/details/utils.js
@@ -1,6 +1,8 @@
-import { TrackingActions } from './constants';
+import { __ } from '~/locale';
+import { formatDate } from '~/lib/utils/datetime_utility';
+import { PackageType, TrackingActions } from './constants';
 
-const trackInstallationTabChange = {
+export const trackInstallationTabChange = {
   methods: {
     trackInstallationTabChange(tabIndex) {
       const action = tabIndex === 0 ? TrackingActions.INSTALLATION : TrackingActions.REGISTRY_SETUP;
@@ -9,4 +11,47 @@ const trackInstallationTabChange = {
   },
 };
 
-export default trackInstallationTabChange;
+export function generateConanRecipe(packageEntity = {}) {
+  const {
+    name = '',
+    version = '',
+    conan_metadatum: {
+      package_username: packageUsername = '',
+      package_channel: packageChannel = '',
+    } = {},
+  } = packageEntity;
+
+  return `${name}/${version}@${packageUsername}/${packageChannel}`;
+}
+
+export function generatePackageInfo(packageEntity = {}) {
+  const information = [];
+
+  if (packageEntity.package_type === PackageType.CONAN) {
+    information.push({
+      label: __('Recipe'),
+      value: generateConanRecipe(packageEntity),
+    });
+  } else {
+    information.push({
+      label: __('Name'),
+      value: packageEntity.name || '',
+    });
+  }
+
+  return [
+    ...information,
+    {
+      label: __('Version'),
+      value: packageEntity.version || '',
+    },
+    {
+      label: __('Created on'),
+      value: formatDate(packageEntity.created_at),
+    },
+    {
+      label: __('Updated at'),
+      value: formatDate(packageEntity.updated_at),
+    },
+  ];
+}
