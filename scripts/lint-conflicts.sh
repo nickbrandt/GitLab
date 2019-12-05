fbd3a4767d0529ac4a494ddf39a446722e5979e8
Thong Kuah <tkuah@gitlab.com>
2019-12-05 13:55:07 +1300

    Search for conflict headers everywhere
    We seem to have restricted to only no haml, nor js, nor rb because we
    can rely on tests. But since the new scripts is orders of magnitude
    faster, we can include all files by default.
    
    We need to exclude two files for now as these have conflict headers as
    part of their tests.

--- scripts/lint-conflicts.sh
+++ scripts/lint-conflicts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-output=`git grep -En '^<<<<<<< ' -- . ':(exclude)*.haml' ':(exclude)*.js' ':(exclude)*.rb'`
+output=`git grep -En '^<<<<<<< ' -- . ':(exclude)spec/lib/gitlab/conflict/file_spec.rb' ':(exclude)spec/lib/gitlab/git/conflict/parser_spec.rb'`
 echo $output
 test -z "$output"
