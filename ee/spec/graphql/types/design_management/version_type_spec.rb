da30fdb503f981eb4f4dcaa783ef012b2da4fdfc
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-12-05 20:07:36 +0000

    Connect designs-at-version to versions

--- ee/spec/graphql/types/design_management/version_type_spec.rb
+++ ee/spec/graphql/types/design_management/version_type_spec.rb
@@ -3,7 +3,11 @@
 require 'spec_helper'
 
 describe GitlabSchema.types['DesignVersion'] do
+  let(:expected_fields) do
+    %i[id sha designs design_at_version designs_at_version]
+  end
+
   it { expect(described_class).to require_graphql_authorizations(:read_design) }
 
-  it { expect(described_class).to have_graphql_fields(:id, :sha, :designs) }
+  it { expect(described_class).to have_graphql_fields(*expected_fields) }
 end
