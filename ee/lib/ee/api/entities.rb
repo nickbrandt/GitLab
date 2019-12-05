dd70b37104d2286f9b5a808b276f1351c186bfdf
mo khan <mo.khan@gmail.com>
2019-12-05 13:44:07 -0700

    Use Hash#key instead of Hash#invert

--- ee/lib/ee/api/entities.rb
+++ ee/lib/ee/api/entities.rb
@@ -862,8 +862,8 @@ module EE
       class ManagedLicense < Grape::Entity
         expose :id, :name
         expose :classification, as: :approval_status do |policy|
-          SoftwareLicensePolicy::APPROVAL_STATUS.invert
-            .fetch(policy.classification, policy.classification)
+          SoftwareLicensePolicy::APPROVAL_STATUS.key(policy.classification) ||
+            policy.classification
         end
       end
 
