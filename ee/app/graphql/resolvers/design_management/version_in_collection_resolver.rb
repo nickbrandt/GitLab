2101fef6e82cb8ed4e1eea8d49c88197ebc0e1c8
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-12-05 20:04:01 +0000

    Update version resolvers to handle more fields consistently
    This makes sure that shas are possible arguments.
    
    Three variants of version resolvers are distinguished:
    
    * when there is no relevant context (e.g. top level):
      - singular resolver able to retrieve versions by ID
    * when there is a relevant context (a source of versions)
      - plural resolver to find all versions up-to a given version
      - singular resolver to find a version by ID or SHA

---
+++ ee/app/graphql/resolvers/design_management/version_in_collection_resolver.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Resolvers
+  module DesignManagement
+    class VersionInCollectionResolver < BaseResolver
+      include Gitlab::Graphql::Authorize::AuthorizeResource
+
+      type Types::DesignManagement::VersionType, null: true
+
+      authorize :read_design
+
+      alias_method :design_or_collection, :object
+
+      argument :sha, GraphQL::STRING_TYPE,
+               required: false,
+               description: "The SHA256 of a specific version"
+      argument :id, GraphQL::ID_TYPE,
+               required: false,
+               description: 'The Global ID of the version'
+
+      def resolve(id: nil, sha: nil)
+        check_args(id, sha)
+
+        gid = GitlabSchema.parse_gid(id, expected_type: ::DesignManagement::Version) if id
+
+        ::DesignManagement::VersionsFinder
+          .new(design_or_collection, current_user, sha: sha, version_id: gid&.model_id)
+          .execute
+          .first
+      end
+
+      private
+
+      def check_args(id, sha)
+        return if id.present? || sha.present?
+
+        raise ::Gitlab::Graphql::Errors::ArgumentError, 'id or sha is required'
+      end
+    end
+  end
+end
