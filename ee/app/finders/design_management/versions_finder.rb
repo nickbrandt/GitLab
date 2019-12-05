ceb41f2e1bf2018033236d9704f983d5e24dec14
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-12-05 17:58:54 +0000

    Add allow version finder to search by ID
    This has the version finder use the `id_in` scope to query for versions
    by ID.

--- ee/app/finders/design_management/versions_finder.rb
+++ ee/app/finders/design_management/versions_finder.rb
@@ -11,6 +11,12 @@ module DesignManagement
     #
     # The object will have `#versions` called on it to set up the
     # initial scope of the versions.
+    #
+    # valid params:
+    #   - earlier_or_equal_to: Version
+    #   - sha: String
+    #   - version_id: Integer
+    #
     def initialize(design_or_collection, current_user, params = {})
       @design_or_collection = design_or_collection
       @current_user = current_user
@@ -25,6 +31,7 @@ module DesignManagement
       items = design_or_collection.versions
       items = by_earlier_or_equal_to(items)
       items = by_sha(items)
+      items = by_version_id(items)
       items.ordered
     end
 
@@ -34,6 +41,12 @@ module DesignManagement
       return items unless params[:earlier_or_equal_to]
 
       items.earlier_or_equal_to(params[:earlier_or_equal_to])
+    end
+
+    def by_version_id(items)
+      return items unless params[:version_id]
+
+      items.id_in(params[:version_id])
     end
 
     def by_sha(items)
