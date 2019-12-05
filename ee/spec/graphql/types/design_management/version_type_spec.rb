7d4cc076624ca75b191aa5ccc3095f43183e85b0
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-12-05 20:08:38 +0000

    Connect designs-at-versions to design-collections
    This mounts the new resolvers on the design collection type

--- ee/spec/graphql/types/design_management/version_type_spec.rb
+++ ee/spec/graphql/types/design_management/version_type_spec.rb
@@ -3,11 +3,11 @@
 require 'spec_helper'
 
 describe GitlabSchema.types['DesignVersion'] do
-  let(:expected_fields) do
-    %i[id sha designs design_at_version designs_at_version]
-  end
-
   it { expect(described_class).to require_graphql_authorizations(:read_design) }
 
-  it { expect(described_class).to have_graphql_fields(*expected_fields) }
+  it 'has the expected fields' do
+    expected_fields = %i[id sha designs design_at_version designs_at_version]
+
+    expect(described_class).to have_graphql_fields(*expected_fields)
+  end
 end
