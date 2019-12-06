0cc8e93a2fbec88ccd03b2a915d77b0e990067b3
Cédric Tabin <cedric.tabin@gmail.com>
2019-12-06 23:52:36 +0100

    Adds 'tags' keyword in 'default'

--- ee/spec/lib/ee/gitlab/ci/config/entry/bridge_spec.rb
+++ ee/spec/lib/ee/gitlab/ci/config/entry/bridge_spec.rb
@@ -13,7 +13,8 @@ describe EE::Gitlab::Ci::Config::Entry::Bridge do
     # that we know that we don't want to inherit
     # as they do not have sense in context of Bridge
     let(:ignored_inheritable_columns) do
-      %i[before_script after_script image services cache interruptible timeout retry]
+      %i[before_script after_script image services cache interruptible timeout
+         retry tags]
     end
   end
 
