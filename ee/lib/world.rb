74d3cddef926627f019837667052559e2fe72a3c
Alex Buijs <abuijs@gitlab.com>
2019-12-09 15:19:58 +0100

    Add API for states by country
    Add an API route for fetching states by country

--- ee/lib/world.rb
+++ ee/lib/world.rb
@@ -10,6 +10,18 @@ module World
     strong_memoize(:countries_for_select) { all_countries.sort_by(&:name).map { |c| [c.name, c.alpha2] } }
   end
 
+  def states_for_country(country_code)
+    strong_memoize("states_for_country_#{country_code}") do
+      country = ISO3166::Country.find_country_by_alpha2(country_code)
+      next unless country
+
+      country.states
+        &.reject { |_, state| state.name.nil? }
+        &.sort_by { |_, state| state.name }
+        &.map { |code, state| [state.name, code] }.to_h
+    end
+  end
+
   def all_countries
     strong_memoize(:all_countries) { ISO3166::Country.all.reject {|item| DENYLIST.include?(item.name) } }
   end
