eb5bd9fd855d8fbbfa433f234a7787a04dc7b3af
Mark Chao <mchao@gitlab.com>
2019-12-11 17:46:34 +0800

    Update redactiable results class list
    Blob search returns ES::Model::Response::Response.
    To redact that, the old condition check for blobs
    is made more accurate by checking collection elements
    being blobs or not.

--- ee/app/services/ee/search_service.rb
+++ ee/app/services/ee/search_service.rb
@@ -2,10 +2,14 @@
 
 module EE
   module SearchService
-    # Both of these classes conform to the necessary pagination interface and
-    # both of these are returned in various places from search results. There
+    # All of these classes conform to the necessary pagination interface and
+    # all of these are returned in various places from search results. There
     # doesn't seem to be a common ancestor to check.
-    REDACTABLE_RESULTS = [Kaminari::PaginatableArray, Elasticsearch::Model::Response::Records].freeze
+    REDACTABLE_RESULTS = [
+      Kaminari::PaginatableArray,
+      Elasticsearch::Model::Response::Records,
+      Elasticsearch::Model::Response::Response
+    ].freeze
 
     # This is a proper method instead of a `delegate` in order to
     # avoid adding unnecessary methods to Search::SnippetService
