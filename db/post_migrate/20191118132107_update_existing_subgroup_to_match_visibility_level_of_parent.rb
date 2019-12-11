7c3bff38f0705d1267a484fffc6e255f60e32f73
Małgorzata Ksionek <mksionek@gitlab.com>
2019-12-11 13:15:51 +0100

    Refactor main method

--- db/post_migrate/20191118132107_update_existing_subgroup_to_match_visibility_level_of_parent.rb
+++ db/post_migrate/20191118132107_update_existing_subgroup_to_match_visibility_level_of_parent.rb
@@ -3,47 +3,53 @@
 class UpdateExistingSubgroupToMatchVisibilityLevelOfParent < ActiveRecord::Migration[5.2]
   include Gitlab::Database::MigrationHelpers
 
-  class Namespace < ActiveRecord::Base
-    self.table_name = 'namespaces'
-  end
-
   DOWNTIME = false
 
   disable_ddl_transaction!
 
+  VISIBILITY_LEVELS = {
+    private: 0,
+    internal: 10,
+  }
+
   def up
-    group_ids = execute <<~SQL
-      SELECT ARRAY_TO_STRING(ARRAY_AGG(id), ',') as ids, min as level FROM (WITH RECURSIVE base_and_descendants AS (
-        (SELECT visibility_level AS strictest_parent_level,
-                namespaces.*
-          FROM namespaces
-          WHERE namespaces.type IN ('Group'))
+    update_namespaces_with_level(VISIBILITY_LEVELS[:private])
+    update_namespaces_with_level(VISIBILITY_LEVELS[:internal])
+  end
+
+
+  def update_namespaces_with_level(visibility_level)
+    namespace_ids_with_level(visibility_level).rows.each_slice(500) do |ids|
+      execute <<~SQL
+        UPDATE namespaces
+        SET visibility_level = #{visibility_level}
+        WHERE namespaces.id IN (#{ids.join(',')})
+      SQL
+    end
+  end
+
+  def namespace_ids_with_level(visibility_level)
+    exec_query <<~SQL
+      SELECT id
+      FROM (
+        WITH RECURSIVE base_and_descendants AS (
+          (SELECT visibility_level AS strictest_parent_level,
+                  namespaces.*
+           FROM namespaces
+           WHERE namespaces.type IN ('Group'))
         UNION
-        (SELECT LEAST(namespaces.visibility_level, base_and_descendants.strictest_parent_level) AS strictest_parent_level,
-                namespaces.*
-        FROM namespaces,
-              base_and_descendants
-        WHERE namespaces.type IN ('Group')
-          AND namespaces.parent_id = base_and_descendants.id))
-        SELECT id, MIN(strictest_parent_level) as min
+          (SELECT LEAST(namespaces.visibility_level, base_and_descendants.strictest_parent_level) AS strictest_parent_level,
+                  namespaces.*
+           FROM namespaces,
+                base_and_descendants
+           WHERE namespaces.type IN ('Group')
+             AND namespaces.parent_id = base_and_descendants.id))
+        SELECT id, MIN(strictest_parent_level) AS strictest_parent_level
         FROM base_and_descendants
         WHERE visibility_level > strictest_parent_level
-        GROUP BY id) as t1
-      GROUP BY level
+        GROUP BY id) as namespaces_levels
+      WHERE strictest_parent_level = #{visibility_level}
     SQL
-
-    group_ids.each do |row|
-      ids = row.fetch('ids', nil)
-      level = row.fetch('level')
-      next unless ids
-
-      execute("UPDATE namespaces
-                SET visibility_level = #{level}
-                WHERE namespaces.id IN (#{ids})")
-
-      logger = Gitlab::BackgroundMigration::Logger.build
-      logger.info(message: "groups were updated to metch visibility level of a parent", groups_ids: ids, parent_level: level)
-    end
   end
 
   def down
