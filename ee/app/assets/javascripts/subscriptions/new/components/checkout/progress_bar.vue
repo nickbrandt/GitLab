7fbe091057f245dbd04cde531a4b63fb427cb411
Alex Buijs <abuijs@gitlab.com>
2019-12-11 12:07:03 +0100

    Add vue components for checkout and progress bar
    This is part of the paid signup flow MR

---
+++ ee/app/assets/javascripts/subscriptions/new/components/checkout/progress_bar.vue
@@ -0,0 +1,35 @@
+<script>
+import { s__ } from '~/locale';
+
+export default {
+  props: {
+    step: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    classObject(stepNumber) {
+      return {
+        phase: true,
+        bold: true,
+        center: true,
+        finished: this.step > stepNumber,
+        current: this.step === stepNumber,
+      };
+    },
+  },
+  i18n: {
+    yourProfile: s__('Checkout|1. Your profile'),
+    checkout: s__('Checkout|2. Checkout'),
+    yourGroup: s__('Checkout|3. Your GitLab group'),
+  },
+};
+</script>
+<template>
+  <div class="bar d-flex">
+    <div v-for="(value, name, index) in $options.i18n" :key="name" :class="classObject(index + 1)">
+      {{ value }}
+    </div>
+  </div>
+</template>
---
+++ ee/spec/frontend/subscriptions/new/components/checkout/progress_bar_spec.js
@@ -0,0 +1,46 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import component from 'ee/subscriptions/new/components/checkout/progress_bar.vue';
+
+describe('Progress Bar', () => {
+  const localVue = createLocalVue();
+  let wrapper;
+
+  const factory = propsData => {
+    wrapper = shallowMount(localVue.extend(component), {
+      propsData,
+      localVue,
+      sync: false,
+    });
+  };
+
+  const firstStep = () => wrapper.find('.bar div:nth-child(1)');
+  const secondStep = () => wrapper.find('.bar div:nth-child(2)');
+
+  beforeEach(() => {
+    factory({ step: 2 });
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe('current', () => {
+    it('step 1 is not current', () => {
+      expect(firstStep().classes()).not.toContain('current');
+    });
+
+    it('step 2 is current', () => {
+      expect(secondStep().classes()).toContain('current');
+    });
+  });
+
+  describe('finished', () => {
+    it('step 1 is finished', () => {
+      expect(firstStep().classes()).toContain('finished');
+    });
+
+    it('step 2 is not finished', () => {
+      expect(secondStep().classes()).not.toContain('finished');
+    });
+  });
+});
