7d63417721ea8c474b060a3ca5eb253da912c1ab
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2019-12-11 08:10:10 +0000

    Update spec descriptions with new routing
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- ee/spec/features/security/project/public_access_spec.rb
+++ ee/spec/features/security/project/public_access_spec.rb
@@ -56,7 +56,7 @@ describe '[EE] Public Project Access' do
     it { is_expected.to be_allowed_for(:auditor) }
   end
 
-  describe "GET /:project_path/settings/members" do
+  describe "GET /:project_path/-/settings/members" do
     subject { project_settings_members_path(project) }
 
     it { is_expected.to be_allowed_for(:auditor) }
@@ -117,20 +117,20 @@ describe '[EE] Public Project Access' do
     end
   end
 
-  describe "GET /:project_path/environments" do
+  describe "GET /:project_path/-/environments" do
     subject { project_environments_path(project) }
 
     it { is_expected.to be_allowed_for(:auditor) }
   end
 
-  describe "GET /:project_path/environments/:id" do
+  describe "GET /:project_path/-/environments/:id" do
     let(:environment) { create(:environment, project: project) }
     subject { project_environment_path(project, environment) }
 
     it { is_expected.to be_allowed_for(:auditor) }
   end
 
-  describe "GET /:project_path/environments/new" do
+  describe "GET /:project_path/-/environments/new" do
     subject { new_project_environment_path(project) }
 
     it { is_expected.to be_denied_for(:auditor) }
@@ -208,7 +208,7 @@ describe '[EE] Public Project Access' do
     it { is_expected.to be_allowed_for(:auditor) }
   end
 
-  describe "GET /:project_path/settings/integrations" do
+  describe "GET /:project_path/-/settings/integrations" do
     subject { project_settings_integrations_path(project) }
 
     it { is_expected.to be_denied_for(:auditor) }
