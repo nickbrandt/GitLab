34f47ffd1318b0817277f848aa3f308dd60e4028
Walmyr Lima <walmyr@gitlab.com>
2019-12-13 16:26:35 +0100

    Simplify the issues weight end-to-end test
    By leaving the project fabrication to the milestone fabrication
    since it creates it by default if none is provided.

--- qa/qa/specs/features/ee/browser_ui/2_plan/issues_weight/issue_weight_visualization_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/2_plan/issues_weight/issue_weight_visualization_spec.rb
@@ -7,15 +7,8 @@ module QA
         Flow::Login.sign_in
       end
 
-      let(:project) do
-        QA::Resource::Project.fabricate_via_api! do |project|
-          project.name = 'the-lord-of-the-rings'
-        end
-      end
-
       let(:milestone) do
         QA::EE::Resource::ProjectMilestone.fabricate_via_api! do |m|
-          m.project = project
           m.title = 'the-fellowship-of-the-ring'
         end
       end
@@ -25,7 +18,7 @@ module QA
       let(:issue) do
         Resource::Issue.fabricate_via_api! do |issue|
           issue.milestone = milestone
-          issue.project = project
+          issue.project = milestone.project
           issue.title = 'keep-the-ring-safe'
           issue.weight = weight
         end
