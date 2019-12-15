7b79b4bf9d81376e0c609a5f98f8e7db4644ed83
Mark Lapierre <mlapierre@gitlab.com>
2019-12-16 09:44:10 +1100

    Remove passing test from quarantine

--- qa/qa/specs/features/ee/browser_ui/3_create/repository/pull_mirroring_over_ssh_with_key_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/3_create/repository/pull_mirroring_over_ssh_with_key_spec.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 module QA
-  # Failure issue: https://gitlab.com/gitlab-org/gitlab/issues/35152
-  context 'Create', :quarantine do
+  context 'Create' do
     describe 'Pull mirror a repository over SSH with a private key' do
       let(:source) do
         Resource::Repository::ProjectPush.fabricate! do |project_push|
