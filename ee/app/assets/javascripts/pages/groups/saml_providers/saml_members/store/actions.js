076430bc70db787777e714ce5c8617879efdd4af
Illya Klymov <xanf@xanf.me>
2019-12-16 19:52:17 +0200

    List identities on group SAML SSO page
    Introduces base table, listing members of group
    which use SAML SSO on settings page

---
+++ ee/app/assets/javascripts/pages/groups/saml_providers/saml_members/store/actions.js
@@ -0,0 +1,29 @@
+import { __ } from '~/locale';
+import Api from '~/api';
+import createFlash from '~/flash';
+import { normalizeHeaders, parseIntPagination } from '~/lib/utils/common_utils';
+import * as types from './mutation_types';
+
+export function fetchPage({ commit, state }, newPage) {
+  return Api.groupMembers(state.groupId, {
+    with_saml_identity: 'true',
+    page: newPage || state.pageInfo.page,
+    per_page: state.pageInfo.perPage,
+  })
+    .then(response => {
+      const { headers, data } = response;
+      const pageInfo = parseIntPagination(normalizeHeaders(headers));
+      commit(types.RECEIVE_SAML_MEMBERS_SUCCESS, {
+        members: data.map(({ group_saml_identity: identity, ...item }) => ({
+          ...item,
+          identity: identity ? identity.extern_uid : null,
+        })),
+        pageInfo,
+      });
+    })
+    .catch(() => {
+      createFlash(__('An error occurred while loading group members.'));
+    });
+}
+// prevent babel-plugin-rewire from generating an invalid default during karma tests
+export default () => {};
