adeaeb493d09db7e02bd7f51026cef145202f96e
Winnie Hellmann <winnie@gitlab.com>
2019-12-18 11:46:13 +0000

    Disable ESLint rule no-jquery/no-fade inline
    Ignore all existing offenses of no-jquery/no-fade.

--- ee/app/assets/javascripts/weight_select.js
+++ ee/app/assets/javascripts/weight_select.js
@@ -11,6 +11,7 @@ function WeightSelect(els, options = {}) {
     const $selectbox = $dropdown.closest('.selectbox');
     const $block = $selectbox.closest('.block');
     const $value = $block.find('.value');
+    // eslint-disable-next-line no-jquery/no-fade
     $block.find('.block-loading').fadeOut();
     const fieldName = options.fieldName || $dropdown.data('fieldName');
     const inputField = $dropdown.closest('.selectbox').find(`input[name='${fieldName}']`);
