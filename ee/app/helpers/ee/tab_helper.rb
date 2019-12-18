6f7245068729a9383d955d6a66b268bd18ce0ae9
Brandon Labuschagne <blabuschagne@gitlab.com>
2019-12-18 16:28:10 +0000

    Merge instance statistics and cycle analytics
    This MR also adds a navbar item for analytics which
    takes a user to the merged analytics space.

--- ee/app/helpers/ee/tab_helper.rb
+++ ee/app/helpers/ee/tab_helper.rb
@@ -5,7 +5,7 @@ module EE
     extend ::Gitlab::Utils::Override
 
     def analytics_controllers
-      ['analytics/productivity_analytics', 'analytics/cycle_analytics']
+      ['analytics/productivity_analytics', 'analytics/cycle_analytics', 'instance_statistics/dev_ops_score', 'instance_statistics/cohorts']
     end
   end
 end
--- ee/spec/views/layouts/nav/sidebar/_analytics.html.haml_spec.rb
+++ ee/spec/views/layouts/nav/sidebar/_analytics.html.haml_spec.rb
@@ -16,7 +16,7 @@ describe 'layouts/nav/sidebar/_analytics' do
 
         expect(rendered).to have_content('Analytics')
         expect(rendered).to include(analytics_root_path)
-        expect(rendered).to match(/<use xlink:href=".+?icons-.+?#log">/)
+        expect(rendered).to match(/<use xlink:href=".+?icons-.+?#chart">/)
       end
 
       it 'has `Productivity Analytics` link' do
@@ -38,6 +38,35 @@ describe 'layouts/nav/sidebar/_analytics' do
         expect(rendered).to include(analytics_cycle_analytics_path)
         expect(rendered).to match(/<use xlink:href=".+?icons-.+?#repeat">/)
       end
+
+      context 'and user has access to instance statistics features' do
+        it 'has `DevOps Score` link' do
+          render
+
+          expect(rendered).to have_content('DevOps Score')
+          expect(rendered).to include(instance_statistics_dev_ops_score_index_path)
+          expect(rendered).to match(/<use xlink:href=".+?icons-.+?#comment">/)
+        end
+
+        it 'has `Cohorts` link' do
+          render
+
+          expect(rendered).to have_content('Cohorts')
+          expect(rendered).to include(instance_statistics_cohorts_path)
+          expect(rendered).to match(/<use xlink:href=".+?icons-.+?#users">/)
+        end
+      end
+
+      context 'and user does not have access to instance statistics features' do
+        it 'no instance statistics links are rendered' do
+          allow(view).to receive(:dashboard_nav_link?).and_return(false)
+
+          render
+
+          expect(rendered).not_to have_content('DevOps Score')
+          expect(rendered).not_to have_content('Cohorts')
+        end
+      end
     end
 
     context 'when feature flags are disabled' do
@@ -46,6 +75,35 @@ describe 'layouts/nav/sidebar/_analytics' do
 
         expect(rendered).not_to have_content('Productivity Analytics')
         expect(rendered).not_to have_content('Cycle Analytics')
+      end
+
+      context 'and user has access to instance statistics features' do
+        it 'has `DevOps Score` link' do
+          render
+
+          expect(rendered).to have_content('DevOps Score')
+          expect(rendered).to include(instance_statistics_dev_ops_score_index_path)
+          expect(rendered).to match(/<use xlink:href=".+?icons-.+?#comment">/)
+        end
+
+        it 'has `Cohorts` link' do
+          render
+
+          expect(rendered).to have_content('Cohorts')
+          expect(rendered).to include(instance_statistics_cohorts_path)
+          expect(rendered).to match(/<use xlink:href=".+?icons-.+?#users">/)
+        end
+      end
+
+      context 'and user does not have access to instance statistics features' do
+        it 'no instance statistics links are rendered' do
+          allow(view).to receive(:dashboard_nav_link?).and_return(false)
+
+          render
+
+          expect(rendered).not_to have_content('DevOps Score')
+          expect(rendered).not_to have_content('Cohorts')
+        end
       end
     end
   end
