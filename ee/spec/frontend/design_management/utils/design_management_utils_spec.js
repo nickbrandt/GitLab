9ad18925fe91162c19ce1643cd3aa22bae3db25f
Tom Quirk <tquirk@gitlab.com>
2019-12-18 08:01:04 +0000

    Refactored design page to use Apollo utils

--- ee/spec/frontend/design_management/utils/design_management_utils_spec.js
+++ ee/spec/frontend/design_management/utils/design_management_utils_spec.js
@@ -1,7 +1,9 @@
+import underscore from 'underscore';
 import {
   extractCurrentDiscussion,
   extractDiscussions,
   findVersionId,
+  designUploadOptimisticResponse,
 } from 'ee/design_management/utils/design_management_utils';
 
 describe('extractCurrentDiscussion', () => {
@@ -66,5 +68,38 @@ describe('version parser', () => {
     const testInvalidVersionString = `gid://gitlab/DesignManagement::Version`;
 
     expect(findVersionId(testInvalidVersionString)).toBeUndefined();
+  });
+});
+
+describe('optimistic responses', () => {
+  it('correctly generated for design upload', () => {
+    jest.spyOn(underscore, 'uniqueId').mockImplementation(() => 1);
+    const expectedResponse = {
+      __typename: 'Mutation',
+      designManagementUpload: {
+        __typename: 'DesignManagementUploadPayload',
+        designs: [
+          {
+            __typename: 'Design',
+            id: -1,
+            image: '',
+            filename: 'test',
+            fullPath: '',
+            notesCount: 0,
+            event: 'NONE',
+            diffRefs: { __typename: 'DiffRefs', baseSha: '', startSha: '', headSha: '' },
+            discussions: { __typename: 'DesignDiscussion', edges: [] },
+            versions: {
+              __typename: 'DesignVersionConnection',
+              edges: {
+                __typename: 'DesignVersionEdge',
+                node: { __typename: 'DesignVersion', id: -1, sha: -1 },
+              },
+            },
+          },
+        ],
+      },
+    };
+    expect(designUploadOptimisticResponse([{ name: 'test' }])).toEqual(expectedResponse);
   });
 });
