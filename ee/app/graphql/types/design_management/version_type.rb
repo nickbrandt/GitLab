0148cb09cc5b02940ceae77d4ca2e9ad6bd0fefc
charlieablett <cablett@gitlab.com>
2019-12-19 19:23:24 +1300

    Backfill GraphQL descriptions
    - For design collection GraphQL types
    - Make quotes consistent (single quotes unless interpolation
    required)

--- ee/app/graphql/types/design_management/version_type.rb
+++ ee/app/graphql/types/design_management/version_type.rb
@@ -9,12 +9,12 @@ module Types
 
       authorize :read_design
 
-      field :id, GraphQL::ID_TYPE, null: false # rubocop:disable Graphql/Descriptions
-      field :sha, GraphQL::ID_TYPE, null: false # rubocop:disable Graphql/Descriptions
-      field :designs,
-            Types::DesignManagement::DesignType.connection_type,
-            null: false,
-            description: "All designs that were changed in this version"
+      field :id, GraphQL::ID_TYPE, null: false,
+            description: 'ID of the design version'
+      field :sha, GraphQL::ID_TYPE, null: false,
+            description: 'SHA of the design version'
+      field :designs, Types::DesignManagement::DesignType.connection_type, null: false,
+            description: 'All designs that were changed in the version'
     end
   end
 end
