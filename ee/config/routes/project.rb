8fb9ea95f9d72ddabd50a0535211861fe077ef92
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2019-12-19 11:46:40 +0200

    Copy merge request routes to - scope
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- ee/config/routes/project.rb
+++ ee/config/routes/project.rb
@@ -137,35 +137,15 @@ constraints(::Constraints::ProjectUrlConstrainer.new) do
       get '/service_desk' => 'service_desk#show', as: :service_desk
       put '/service_desk' => 'service_desk#update', as: :service_desk_refresh
 
-      resources :merge_requests, only: [], constraints: { id: /\d+/ } do
-        member do
-          get '/descriptions/:version_id/diff', action: :description_diff, as: :description_diff
-          get :metrics_reports
-          get :license_management_reports
-          get :container_scanning_reports
-          get :dependency_scanning_reports
-          get :sast_reports
-          get :dast_reports
+      # Unscoped route. It will be replaced with redirect to /-/merge_requests/
+      # Issue https://gitlab.com/gitlab-org/gitlab/issues/118849
+      draw :merge_requests_ee
 
-          get :approvals
-          post :approvals, action: :approve
-          delete :approvals, action: :unapprove
-
-          post :rebase
-        end
-
-        resources :approvers, only: :destroy
-        delete 'approvers', to: 'approvers#destroy_via_user_id', as: :approver_via_user_id
-        resources :approver_groups, only: :destroy
-
-        scope module: :merge_requests do
-          resources :drafts, only: [:index, :update, :create, :destroy] do
-            collection do
-              post :publish
-              delete :discard
-            end
-          end
-        end
+      # To ensure an old unscoped routing is used for the UI we need to
+      # add prefix 'as' to the scope routing and place it below original MR routing.
+      # Issue https://gitlab.com/gitlab-org/gitlab/issues/118849
+      scope '-', as: 'scoped' do
+        draw :merge_requests_ee
       end
 
       post '/restore' => '/projects#restore', as: :restore
