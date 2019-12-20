263ab502fa5e9e85f6acb309077aec074d4cef43
Alper Akgun <aakgun@gitlab.com>
2019-12-20 23:08:15 +0300

    Refactor can_admin_namespace ability allowed

--- ee/lib/ee/api/entities.rb
+++ ee/lib/ee/api/entities.rb
@@ -148,15 +148,17 @@ module EE
         extend ActiveSupport::Concern
 
         prepended do
+          can_admin_namespace = ->(namespace, opts) { ::Ability.allowed?(opts[:current_user], :admin_namespace, namespace) }
+
           expose :shared_runners_minutes_limit, if: ->(_, options) { options[:current_user]&.admin? }
           expose :extra_shared_runners_minutes_limit, if: ->(_, options) { options[:current_user]&.admin? }
           expose :billable_members_count do |namespace, options|
             namespace.billable_members_count(options[:requested_hosted_plan])
           end
-          expose :plan, if: ->(namespace, opts) { ::Ability.allowed?(opts[:current_user], :admin_namespace, namespace) } do |namespace, _|
+          expose :plan, if: can_admin_namespace do |namespace, _|
             namespace.actual_plan_name
           end
-          expose :trial_ends_on, if: ->(namespace, opts) { ::Ability.allowed?(opts[:current_user], :admin_namespace, namespace) } do |namespace, _|
+          expose :trial_ends_on, if: can_admin_namespace do |namespace, _|
             namespace.trial_ends_on
           end
         end
