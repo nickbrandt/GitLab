36eafbed1ceadc27791969d6df3b4ab85d225490
Balasankar "Balu" C <balasankarc@autistici.org>
2019-12-22 19:06:18 +0530

    Add option to configure branches to send emails on push
    Signed-off-by: Balasankar "Balu" C <balasankarc@autistici.org>

--- spec/models/project_services/emails_on_push_service_spec.rb
+++ spec/models/project_services/emails_on_push_service_spec.rb
@@ -25,19 +25,113 @@ describe EmailsOnPushService do
     let(:push_data) { { object_kind: 'push' } }
     let(:project)   { create(:project, :repository) }
     let(:service)   { create(:emails_on_push_service, project: project) }
+    let(:recipients) { 'test@gitlab.com' }
 
-    it 'does not send emails when disabled' do
-      expect(project).to receive(:emails_disabled?).and_return(true)
-      expect(EmailsOnPushWorker).not_to receive(:perform_async)
-
-      service.execute(push_data)
+    before do
+      subject.recipients = recipients
     end
 
-    it 'does send emails when enabled' do
-      expect(project).to receive(:emails_disabled?).and_return(false)
-      expect(EmailsOnPushWorker).to receive(:perform_async)
+    shared_examples 'sending email' do |branches_to_be_notified|
+      before do
+        subject.branches_to_be_notified = branches_to_be_notified
+      end
 
-      service.execute(push_data)
+      it 'sends email' do
+        expect(EmailsOnPushWorker).not_to receive(:perform_async)
+
+        service.execute(push_data)
+      end
+    end
+
+    shared_examples 'not sending email' do |branches_to_be_notified|
+      before do
+        subject.branches_to_be_notified = branches_to_be_notified
+      end
+
+      it 'does not send email' do
+        expect(EmailsOnPushWorker).not_to receive(:perform_async)
+
+        service.execute(push_data)
+      end
+    end
+
+    context 'when emails are disabled on the project' do
+      it 'does not send emails' do
+        expect(project).to receive(:emails_disabled?).and_return(true)
+        expect(EmailsOnPushWorker).not_to receive(:perform_async)
+
+        service.execute(push_data)
+      end
+    end
+
+    context 'when emails are enabled on the project' do
+      before do
+        expect(project).to receive(:emails_disabled?).and_return(true)
+      end
+
+      context 'pushing to the default branch' do
+        let(:push_data) { { object_kind: 'push', object_attributes: { ref: project.default_branch } } }
+
+        context 'when configured to send email on pushes to any branch' do
+          it_behaves_like 'sending email', branches_to_be_notified: "all"
+        end
+
+        context 'when configured to send email on pushes to default branch' do
+          it_behaves_like 'sending email', branches_to_be_notified: "default"
+        end
+
+        context 'when configured to send email on pushes to protected branches only' do
+          it_behaves_like 'not sending email', branches_to_be_notified: "protected"
+        end
+
+        context 'when configured to send email on pushes to default and protected branches only' do
+          it_behaves_like 'sending email', branches_to_be_notified: "default_and_protected"
+        end
+      end
+
+      context 'pushing to a protected branch' do
+        before do
+          create(:protected_branch, project: project, name: 'a-protected-branch')
+        end
+
+        let(:push_data) { { object_kind: 'push', object_attributes: { ref: 'a-protected-branch' } } }
+
+        context 'when configured to send email on pushes to any branch' do
+          it_behaves_like 'sending email', branches_to_be_notified: "all"
+        end
+
+        context 'when configured to send email on pushes to default branch' do
+          it_behaves_like 'not sending email', branches_to_be_notified: "default"
+        end
+
+        context 'when configured to send email on pushes to protected branches only' do
+          it_behaves_like 'sending email', branches_to_be_notified: "protected"
+        end
+
+        context 'when configured to send email on pushes to default and protected branches only' do
+          it_behaves_like 'sending email', branches_to_be_notified: "default_and_protected"
+        end
+      end
+
+      context 'pushing to a random branch' do
+        let(:push_data) { { object_kind: 'push', object_attributes: { ref: 'a-random-branch' } } }
+
+        context 'when configured to send email on pushes to any branch' do
+          it_behaves_like 'sending email', branches_to_be_notified: "all"
+        end
+
+        context 'when configured to send email on pushes to default branch' do
+          it_behaves_like 'not sending email', branches_to_be_notified: "default"
+        end
+
+        context 'when configured to send email on pushes to protected branches only' do
+          it_behaves_like 'not sending email', branches_to_be_notified: "protected"
+        end
+
+        context 'when configured to send email on pushes to default and protected branches only' do
+          it_behaves_like 'not sending email', branches_to_be_notified: "default_and_protected"
+        end
+      end
     end
   end
 end
