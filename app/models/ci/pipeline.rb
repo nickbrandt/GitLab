mehulagg/gitlab_test

Ci Resouce Group status transition
This commit supports Ci Resource Group status transition

app/models/ci/pipeline.rb
@@ -95,8 +95,12 @@ module Ci
 
     state_machine :status, initial: :created do
       event :enqueue do
-        transition [:created, :preparing, :skipped, :scheduled] => :pending
+        transition [:created, :waiting_for_resource, :preparing, :skipped, :scheduled] => :pending
         transition [:success, :failed, :canceled] => :running
+      end
+
+      event :require_resource do
+        transition any - [:waiting_for_resource] => :waiting_for_resource
       end
 
       event :prepare do
@@ -135,7 +139,7 @@ module Ci
       # Do not add any operations to this state_machine
       # Create a separate worker for each new operation
 
-      before_transition [:created, :preparing, :pending] => :running do |pipeline|
+      before_transition [:created, :waiting_for_resource, :preparing, :pending] => :running do |pipeline|
         pipeline.started_at = Time.now
       end
 
@@ -158,7 +162,7 @@ module Ci
         end
       end
 
-      after_transition [:created, :preparing, :pending] => :running do |pipeline|
+      after_transition [:created, :waiting_for_resource, :preparing, :pending] => :running do |pipeline|
         pipeline.run_after_commit { PipelineMetricsWorker.perform_async(pipeline.id) }
       end
 
@@ -166,7 +170,7 @@ module Ci
         pipeline.run_after_commit { PipelineMetricsWorker.perform_async(pipeline.id) }
       end
 
-      after_transition [:created, :preparing, :pending, :running] => :success do |pipeline|
+      after_transition [:created, :waiting_for_resource, :preparing, :pending, :running] => :success do |pipeline|
         pipeline.run_after_commit { PipelineSuccessWorker.perform_async(pipeline.id) }
       end
 
@@ -317,7 +321,7 @@ module Ci
     end
 
     def self.bridgeable_statuses
-      ::Ci::Pipeline::AVAILABLE_STATUSES - %w[created preparing pending]
+      ::Ci::Pipeline::AVAILABLE_STATUSES - %w[created waiting_for_resource preparing pending]
     end
 
     def stages_count
@@ -576,6 +580,7 @@ module Ci
         new_status = latest_builds_status.to_s
         case new_status
         when 'created' then nil
+        when 'waiting_for_resource' then require_resource
         when 'preparing' then prepare
         when 'pending' then enqueue
         when 'running' then run

app/models/ci/stage.rb
@@ -39,8 +39,12 @@ module Ci
 
     state_machine :status, initial: :created do
       event :enqueue do
-        transition [:created, :preparing] => :pending
+        transition [:created, :waiting_for_resource, :preparing] => :pending
         transition [:success, :failed, :canceled, :skipped] => :running
+      end
+
+      event :require_resource do
+        transition any - [:waiting_for_resource] => :waiting_for_resource
       end
 
       event :prepare do
@@ -81,6 +85,7 @@ module Ci
         new_status = latest_stage_status.to_s
         case new_status
         when 'created' then nil
+        when 'waiting_for_resource' then require_resource
         when 'preparing' then prepare
         when 'pending' then enqueue
         when 'running' then run

spec/features/projects/pipelines/pipeline_spec.rb
@@ -606,6 +606,117 @@ describe 'Pipeline', :js do
     end
   end
 
+  context 'when build requires resource', :sidekiq_inline do
+    let_it_be(:project) { create(:project, :repository) }
+    let(:pipeline) { create(:ci_pipeline, project: project) }
+    let(:resource_group) { create(:ci_resource_group, project: project) }
+
+    let!(:test_job) do
+      create(:ci_build, :pending, stage: 'test', name: 'test',
+        stage_idx: 1, pipeline: pipeline, project: project)
+    end
+
+    let!(:deploy_job) do
+      create(:ci_build, :created, stage: 'deploy', name: 'deploy',
+        stage_idx: 2, pipeline: pipeline, project: project, resource_group: resource_group)
+    end
+
+    describe 'GET /:project/pipelines/:id' do
+      subject { visit project_pipeline_path(project, pipeline) }
+
+      it 'shows deploy job as created' do
+        subject
+
+        within('.pipeline-header-container') do
+          expect(page).to have_content('pending')
+        end
+
+        within('.pipeline-graph') do
+          within '.stage-column:nth-child(1)' do
+            expect(page).to have_content('test')
+            expect(page).to have_css('.ci-status-icon-pending')
+          end
+
+          within '.stage-column:nth-child(2)' do
+            expect(page).to have_content('deploy')
+            expect(page).to have_css('.ci-status-icon-created')
+          end
+        end
+      end
+
+      context 'when test job succeeded' do
+        before do
+          test_job.success!
+        end
+
+        it 'shows deploy job as pending' do
+          subject
+
+          within('.pipeline-header-container') do
+            expect(page).to have_content('running')
+          end
+
+          within('.pipeline-graph') do
+            within '.stage-column:nth-child(1)' do
+              expect(page).to have_content('test')
+              expect(page).to have_css('.ci-status-icon-success')
+            end
+
+            within '.stage-column:nth-child(2)' do
+              expect(page).to have_content('deploy')
+              expect(page).to have_css('.ci-status-icon-pending')
+            end
+          end
+        end
+      end
+
+      context 'when test job succeeded but there are no available resources' do
+        let(:another_job) { create(:ci_build, :running, project: project, resource_group: resource_group) }
+
+        before do
+          resource_group.retain_resource_for(another_job)
+          test_job.success!
+        end
+
+        it 'shows deploy job as waiting for resource' do
+          subject
+
+          within('.pipeline-header-container') do
+            expect(page).to have_content('waiting')
+          end
+
+          within('.pipeline-graph') do
+            within '.stage-column:nth-child(2)' do
+              expect(page).to have_content('deploy')
+              expect(page).to have_css('.ci-status-icon-waiting-for-resource')
+            end
+          end
+        end
+
+        context 'when resource is released from another job' do
+          before do
+            another_job.success!
+          end
+
+          it 'shows deploy job as pending' do
+            subject
+
+            within('.pipeline-header-container') do
+              expect(page).to have_content('running')
+            end
+
+            within('.pipeline-graph') do
+              within '.stage-column:nth-child(2)' do
+                expect(page).to have_content('deploy')
+                expect(page).to have_css('.ci-status-icon-pending')
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
   describe 'GET /:project/pipelines/:id/builds' do
     include_context 'pipeline builds'
 

spec/models/ci/pipeline_spec.rb
@@ -1748,7 +1748,7 @@ describe Ci::Pipeline, :mailer do
     subject { described_class.bridgeable_statuses }
 
     it { is_expected.to be_an(Array) }
-    it { is_expected.not_to include('created', 'preparing', 'pending') }
+    it { is_expected.not_to include('created', 'waiting_for_resource', 'preparing', 'pending') }
   end
 
   describe '#status', :sidekiq_might_not_need_inline do
@@ -1757,6 +1757,17 @@ describe Ci::Pipeline, :mailer do
     end
 
     subject { pipeline.reload.status }
+
+    context 'on waiting for resource' do
+      before do
+        allow(build).to receive(:requires_resource?) { true }
+        allow(Ci::ResourceGroups::RetainResourceForBuildWorker).to receive(:perform_async)
+
+        build.enqueue
+      end
+
+      it { is_expected.to eq('waiting_for_resource') }
+    end
 
     context 'on prepare' do
       before do

nickbrandt/GitLab

app/models/ci/pipeline.rb
@@ -95,8 +95,12 @@ module Ci
 
     state_machine :status, initial: :created do
       event :enqueue do
-        transition [:created, :preparing, :skipped, :scheduled] => :pending
+        transition [:created, :waiting_for_resource, :preparing, :skipped, :scheduled] => :pending
         transition [:success, :failed, :canceled] => :running
+      end
+
+      event :request_resource do
+        transition any - [:waiting_for_resource] => :waiting_for_resource
       end
 
       event :prepare do
@@ -135,7 +139,7 @@ module Ci
       # Do not add any operations to this state_machine
       # Create a separate worker for each new operation
 
-      before_transition [:created, :preparing, :pending] => :running do |pipeline|
+      before_transition [:created, :waiting_for_resource, :preparing, :pending] => :running do |pipeline|
         pipeline.started_at = Time.now
       end
 
@@ -158,7 +162,7 @@ module Ci
         end
       end
 
-      after_transition [:created, :preparing, :pending] => :running do |pipeline|
+      after_transition [:created, :waiting_for_resource, :preparing, :pending] => :running do |pipeline|
         pipeline.run_after_commit { PipelineMetricsWorker.perform_async(pipeline.id) }
       end
 
@@ -166,7 +170,7 @@ module Ci
         pipeline.run_after_commit { PipelineMetricsWorker.perform_async(pipeline.id) }
       end
 
-      after_transition [:created, :preparing, :pending, :running] => :success do |pipeline|
+      after_transition [:created, :waiting_for_resource, :preparing, :pending, :running] => :success do |pipeline|
         pipeline.run_after_commit { PipelineSuccessWorker.perform_async(pipeline.id) }
       end
 
@@ -317,7 +321,7 @@ module Ci
     end
 
     def self.bridgeable_statuses
-      ::Ci::Pipeline::AVAILABLE_STATUSES - %w[created preparing pending]
+      ::Ci::Pipeline::AVAILABLE_STATUSES - %w[created waiting_for_resource preparing pending]
     end
 
     def stages_count
@@ -576,6 +580,7 @@ module Ci
         new_status = latest_builds_status.to_s
         case new_status
         when 'created' then nil
+        when 'waiting_for_resource' then request_resource
         when 'preparing' then prepare
         when 'pending' then enqueue
         when 'running' then run

app/models/ci/stage.rb
@@ -39,8 +39,12 @@ module Ci
 
     state_machine :status, initial: :created do
       event :enqueue do
-        transition [:created, :preparing] => :pending
+        transition [:created, :waiting_for_resource, :preparing] => :pending
         transition [:success, :failed, :canceled, :skipped] => :running
+      end
+
+      event :request_resource do
+        transition any - [:waiting_for_resource] => :waiting_for_resource
       end
 
       event :prepare do
@@ -81,6 +85,7 @@ module Ci
         new_status = latest_stage_status.to_s
         case new_status
         when 'created' then nil
+        when 'waiting_for_resource' then request_resource
         when 'preparing' then prepare
         when 'pending' then enqueue
         when 'running' then run

spec/features/projects/pipelines/pipeline_spec.rb
@@ -606,6 +606,117 @@ describe 'Pipeline', :js do
     end
   end
 
+  context 'when build requires resource', :sidekiq_inline do
+    let_it_be(:project) { create(:project, :repository) }
+    let(:pipeline) { create(:ci_pipeline, project: project) }
+    let(:resource_group) { create(:ci_resource_group, project: project) }
+
+    let!(:test_job) do
+      create(:ci_build, :pending, stage: 'test', name: 'test',
+        stage_idx: 1, pipeline: pipeline, project: project)
+    end
+
+    let!(:deploy_job) do
+      create(:ci_build, :created, stage: 'deploy', name: 'deploy',
+        stage_idx: 2, pipeline: pipeline, project: project, resource_group: resource_group)
+    end
+
+    describe 'GET /:project/pipelines/:id' do
+      subject { visit project_pipeline_path(project, pipeline) }
+
+      it 'shows deploy job as created' do
+        subject
+
+        within('.pipeline-header-container') do
+          expect(page).to have_content('pending')
+        end
+
+        within('.pipeline-graph') do
+          within '.stage-column:nth-child(1)' do
+            expect(page).to have_content('test')
+            expect(page).to have_css('.ci-status-icon-pending')
+          end
+
+          within '.stage-column:nth-child(2)' do
+            expect(page).to have_content('deploy')
+            expect(page).to have_css('.ci-status-icon-created')
+          end
+        end
+      end
+
+      context 'when test job succeeded' do
+        before do
+          test_job.success!
+        end
+
+        it 'shows deploy job as pending' do
+          subject
+
+          within('.pipeline-header-container') do
+            expect(page).to have_content('running')
+          end
+
+          within('.pipeline-graph') do
+            within '.stage-column:nth-child(1)' do
+              expect(page).to have_content('test')
+              expect(page).to have_css('.ci-status-icon-success')
+            end
+
+            within '.stage-column:nth-child(2)' do
+              expect(page).to have_content('deploy')
+              expect(page).to have_css('.ci-status-icon-pending')
+            end
+          end
+        end
+      end
+
+      context 'when test job succeeded but there are no available resources' do
+        let(:another_job) { create(:ci_build, :running, project: project, resource_group: resource_group) }
+
+        before do
+          resource_group.assign_resource_to(another_job)
+          test_job.success!
+        end
+
+        it 'shows deploy job as waiting for resource' do
+          subject
+
+          within('.pipeline-header-container') do
+            expect(page).to have_content('waiting')
+          end
+
+          within('.pipeline-graph') do
+            within '.stage-column:nth-child(2)' do
+              expect(page).to have_content('deploy')
+              expect(page).to have_css('.ci-status-icon-waiting-for-resource')
+            end
+          end
+        end
+
+        context 'when resource is released from another job' do
+          before do
+            another_job.success!
+          end
+
+          it 'shows deploy job as pending' do
+            subject
+
+            within('.pipeline-header-container') do
+              expect(page).to have_content('running')
+            end
+
+            within('.pipeline-graph') do
+              within '.stage-column:nth-child(2)' do
+                expect(page).to have_content('deploy')
+                expect(page).to have_css('.ci-status-icon-pending')
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
   describe 'GET /:project/pipelines/:id/builds' do
     include_context 'pipeline builds'
 

spec/models/ci/pipeline_spec.rb
@@ -1749,7 +1749,7 @@ describe Ci::Pipeline, :mailer do
     subject { described_class.bridgeable_statuses }
 
     it { is_expected.to be_an(Array) }
-    it { is_expected.not_to include('created', 'preparing', 'pending') }
+    it { is_expected.not_to include('created', 'waiting_for_resource', 'preparing', 'pending') }
   end
 
   describe '#status', :sidekiq_might_not_need_inline do
@@ -1758,6 +1758,17 @@ describe Ci::Pipeline, :mailer do
     end
 
     subject { pipeline.reload.status }
+
+    context 'on waiting for resource' do
+      before do
+        allow(build).to receive(:requires_resource?) { true }
+        allow(Ci::ResourceGroups::AssignResourceFromResourceGroupWorker).to receive(:perform_async)
+
+        build.enqueue
+      end
+
+      it { is_expected.to eq('waiting_for_resource') }
+    end
 
     context 'on prepare' do
       before do