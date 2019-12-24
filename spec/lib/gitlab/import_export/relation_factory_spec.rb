97a82635cba2210437eb399efe4ae43f18974d59
nmilojevic1 <nmilojevic@gitlab.com>
2019-12-24 14:19:56 +0100

    Apply suggestion from MR comments
    Fix identation
    Fix changlog description

--- spec/lib/gitlab/import_export/relation_factory_spec.rb
+++ spec/lib/gitlab/import_export/relation_factory_spec.rb
@@ -102,19 +102,19 @@ describe Gitlab::ImportExport::RelationFactory do
 
     let(:exported_member) do
       {
-          "id" => 111,
-          "access_level" => 30,
-          "source_id" => 1,
-          "source_type" => "Project",
-          "user_id" => 3,
-          "notification_level" => 3,
-          "created_at" => "2016-11-18T09:29:42.634Z",
-          "updated_at" => "2016-11-18T09:29:42.634Z",
-          "user" => {
-              "id" => user.id,
-              "email" => user.email,
-              "username" => user.username
-          }
+        "id" => 111,
+        "access_level" => 30,
+        "source_id" => 1,
+        "source_type" => "Project",
+        "user_id" => 3,
+        "notification_level" => 3,
+        "created_at" => "2016-11-18T09:29:42.634Z",
+        "updated_at" => "2016-11-18T09:29:42.634Z",
+        "user" => {
+          "id" => user.id,
+          "email" => user.email,
+          "username" => user.username
+        }
       }
     end
 
@@ -127,24 +127,24 @@ describe Gitlab::ImportExport::RelationFactory do
 
     let(:relation_hash) do
       {
-          'id' => 27,
-          'target_branch' => "feature",
-          'source_branch' => "feature_conflict",
-          'source_project_id' => project.id,
-          'target_project_id' => project.id,
-          'author_id' => user.id,
-          'assignee_id' => user.id,
-          'updated_by_id' => user.id,
-          'title' => "MR1",
-          'created_at' => "2016-06-14T15:02:36.568Z",
-          'updated_at' => "2016-06-14T15:02:56.815Z",
-          'state' => "opened",
-          'merge_status' => "unchecked",
-          'description' => "Description",
-          'position' => 0,
-          'source_branch_sha' => "ABCD",
-          'target_branch_sha' => "DCBA",
-          'merge_when_pipeline_succeeds' => true
+        'id' => 27,
+        'target_branch' => "feature",
+        'source_branch' => "feature_conflict",
+        'source_project_id' => project.id,
+        'target_project_id' => project.id,
+        'author_id' => user.id,
+        'assignee_id' => user.id,
+        'updated_by_id' => user.id,
+        'title' => "MR1",
+        'created_at' => "2016-06-14T15:02:36.568Z",
+        'updated_at' => "2016-06-14T15:02:56.815Z",
+        'state' => "opened",
+        'merge_status' => "unchecked",
+        'description' => "Description",
+        'position' => 0,
+        'source_branch_sha' => "ABCD",
+        'target_branch_sha' => "DCBA",
+        'merge_when_pipeline_succeeds' => true
       }
     end
 
@@ -169,16 +169,16 @@ describe Gitlab::ImportExport::RelationFactory do
     let(:relation_sym) { :labels }
     let(:relation_hash) do
       {
-          "id": 3,
-          "title": "test3",
-          "color": "#428bca",
-          "group_id": project.group.id,
-          "created_at": "2016-07-22T08:55:44.161Z",
-          "updated_at": "2016-07-22T08:55:44.161Z",
-          "template": false,
-          "description": "",
-          "project_id": project.id,
-          "type": "GroupLabel"
+        "id": 3,
+        "title": "test3",
+        "color": "#428bca",
+        "group_id": project.group.id,
+        "created_at": "2016-07-22T08:55:44.161Z",
+        "updated_at": "2016-07-22T08:55:44.161Z",
+        "template": false,
+        "description": "",
+        "project_id": project.id,
+        "type": "GroupLabel"
       }
     end
 
