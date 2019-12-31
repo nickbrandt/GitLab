e97ce95782722e275f9ccc9fa8b8b6bc0f2b80c3
Alexandru Croitor <acroitor@gitlab.com>
2019-12-31 14:30:50 +0200

    Add a newline between table creation and add_index for readability

--- db/migrate/20191205212923_support_multiple_milestones_for_issues.rb
+++ db/migrate/20191205212923_support_multiple_milestones_for_issues.rb
@@ -8,6 +8,7 @@ class SupportMultipleMilestonesForIssues < ActiveRecord::Migration[5.2]
       t.references :issue, foreign_key: { on_delete: :cascade }, index: { unique: true }, null: false
       t.references :milestone, foreign_key: { on_delete: :cascade }, index: true, null: false
     end
+
     add_index :issue_milestones, [:issue_id, :milestone_id], unique: true
   end
 end
--- db/migrate/20191205212924_support_multiple_milestones_for_merge_requests.rb
+++ db/migrate/20191205212924_support_multiple_milestones_for_merge_requests.rb
@@ -8,6 +8,7 @@ class SupportMultipleMilestonesForMergeRequests < ActiveRecord::Migration[5.2]
       t.references :merge_request, foreign_key: { on_delete: :cascade }, index: { unique: true }, null: false
       t.references :milestone, foreign_key: { on_delete: :cascade }, index: true, null: false
     end
+
     add_index :merge_request_milestones, [:merge_request_id, :milestone_id], name: 'index_mrs_milestones_on_mr_id_and_milestone_id', unique: true
   end
 end
