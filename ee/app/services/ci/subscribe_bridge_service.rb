mehulagg/gitlab_test

Trigger child pipeline via YAML syntax
Define `trigger:include` to spawn a child
pipeline inside the same project.

ee/app/services/ci/subscribe_bridge_service.rb
@@ -29,7 +29,9 @@ module Ci
 
     def upstream_project
       strong_memoize(:upstream_project) do
-        ::Project.find_by_full_path(@bridge.target_project_path)
+        # TODO: shouldn't this be @bridge.upstream_project?
+        # otherwise it could potentially default to the downstream project
+        @bridge.target_project
       end
     end
 

nickbrandt/GitLab

ee/app/services/ci/subscribe_bridge_service.rb
@@ -5,7 +5,7 @@ module Ci
     include ::Gitlab::Utils::StrongMemoize
 
     def execute(bridge)
-      return unless bridge.upstream_project
+      return unless bridge.upstream_project_path
 
       @bridge = bridge
 
@@ -29,7 +29,7 @@ module Ci
 
     def upstream_project
       strong_memoize(:upstream_project) do
-        ::Project.find_by_full_path(@bridge.target_project_path)
+        @bridge.upstream_project
       end
     end
 