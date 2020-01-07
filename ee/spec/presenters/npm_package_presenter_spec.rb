olearycrew/gitlab-core

Add NPM dist tags support
Add support for the `npm dist-tag` operations:
https://docs.npmjs.com/cli/dist-tag
Add relevant models and services
Update API::NpmPackages to support these new requests

ee/spec/presenters/npm_package_presenter_spec.rb
@@ -3,18 +3,43 @@
 require 'spec_helper'
 
 describe NpmPackagePresenter do
-  set(:project) { create(:project) }
-  set(:package) { create(:npm_package, version: '1.0.4', project: project) }
-  set(:latest_package) { create(:npm_package, version: '1.0.11', project: project) }
-  let(:presenter) { described_class.new(project, package.name, project.packages.all) }
-
-  describe '#dist_tags' do
-    it { expect(presenter.dist_tags).to be_a(Hash) }
-    it { expect(presenter.dist_tags[:latest]).to eq(latest_package.version) }
-  end
+  let(:project) { create(:project) }
+  let(:package_name) { "@#{project.root_namespace.path}/test" }
+  let!(:package1) { create(:npm_package, version: '1.0.4', project: project, name: package_name) }
+  let!(:package2) { create(:npm_package, version: '1.0.6', project: project, name: package_name) }
+  let!(:latest_package) { create(:npm_package, version: '1.0.11', project: project, name: package_name) }
+  let(:packages) { project.packages.npm.with_name(package_name).last_of_each_version }
+  let(:presenter) { described_class.new(project, package_name, packages) }
 
   describe '#versions' do
-    it { expect(presenter.versions).to be_a(Hash) }
-    it { expect(presenter.versions[package.version]).to match_schema('public_api/v4/packages/npm_package_version', dir: 'ee') }
+    subject { presenter.versions }
+
+    it { is_expected.to be_a(Hash) }
+    it { expect(subject[package1.version]).to match_schema('public_api/v4/packages/npm_package_version', dir: 'ee') }
+    it { expect(subject[package2.version]).to match_schema('public_api/v4/packages/npm_package_version', dir: 'ee') }
+  end
+
+  describe '#dist_tags' do
+    subject { presenter.dist_tags }
+
+    context 'for packages without tags' do
+      it { is_expected.to be_a(Hash) }
+      it { expect(subject["latest"]).to eq(latest_package.version) }
+    end
+
+    context 'for packages with tags' do
+      let!(:package_tag1) { create(:package_tag, package: package1, name: 'release_a') }
+      let!(:package_tag2) { create(:package_tag, package: package1, name: 'test_release') }
+      let!(:package_tag3) { create(:package_tag, package: package2, name: 'release_b') }
+      let!(:package_tag4) { create(:package_tag, package: latest_package, name: 'release_c') }
+      let!(:package_tag5) { create(:package_tag, package: latest_package, name: 'latest') }
+
+      it { is_expected.to be_a(Hash) }
+      it { expect(subject[package_tag1.name]).to eq(package1.version) }
+      it { expect(subject[package_tag2.name]).to eq(package1.version) }
+      it { expect(subject[package_tag3.name]).to eq(package2.version) }
+      it { expect(subject[package_tag4.name]).to eq(latest_package.version) }
+      it { expect(subject[package_tag5.name]).to eq(latest_package.version) }
+    end
   end
 end

ee/spec/requests/api/npm_packages_spec.rb
@@ -160,6 +160,17 @@ describe API::NpmPackages do
 
             expect(response).to have_gitlab_http_status(400)
           end
+
+          context 'with empty versions' do
+            let(:params) { upload_params(package_name).merge!(versions: {}) }
+
+            it 'throws a 400 error' do
+              expect { upload_package_with_token(package_name, params) }
+              .not_to change { project.packages.count }
+
+              expect(response).to have_gitlab_http_status(400)
+            end
+          end
         end
 
         context 'invalid package name' do
@@ -183,6 +194,7 @@ describe API::NpmPackages do
           expect { upload_package_with_token(package_name, params) }
             .to change { project.packages.count }.by(1)
             .and change { Packages::PackageFile.count }.by(1)
+            .and change { Packages::PackageTag.count }.by(1)
 
           expect(response).to have_gitlab_http_status(200)
         end
@@ -217,11 +229,149 @@ describe API::NpmPackages do
     end
   end
 
+  describe 'GET /api/v4/packages/npm/-/package/*package_name/dist-tags' do
+    let(:package) { create(:npm_package, project: project) }
+    let!(:package_tag1) { create(:package_tag, package: package) }
+    let!(:package_tag2) { create(:package_tag, package: package) }
+    let(:package_name) { package.name }
+    let(:user) { create(:user) }
+    let(:url) { "/packages/npm/-/package/#{package_name}/dist-tags" }
+
+    subject { get api(url) }
+
+    context 'with packages feature enabled' do
+      before do
+        stub_licensed_features(packages: true)
+      end
+
+      context 'with public group' do
+        context 'with authenticated user' do
+          subject { get api(url, user) }
+
+          it_behaves_like 'returns package tags', :maintainer
+          it_behaves_like 'returns package tags', :developer
+          it_behaves_like 'returns package tags', :reporter
+          it_behaves_like 'returns package tags', :guest
+        end
+
+        context 'with unauthenticated user' do
+          it_behaves_like 'returns package tags', :no_type
+        end
+      end
+
+      context 'with private group' do
+        let(:project) { create(:project, :private) }
+
+        context 'with authenticated user' do
+          subject { get api(url, user) }
+
+          it_behaves_like 'returns package tags', :maintainer
+          it_behaves_like 'returns package tags', :developer
+          it_behaves_like 'returns package tags', :reporter
+          it_behaves_like 'rejects package tags access', :guest, :forbidden
+        end
+
+        context 'with unauthenticated user' do
+          it_behaves_like 'rejects package tags access', :no_type, :forbidden
+        end
+      end
+    end
+
+    context 'with packages feature disabled' do
+      before do
+        stub_licensed_features(packages: false)
+      end
+
+      it_behaves_like 'rejects package tags access', :no_type, :forbidden
+    end
+  end
+
+  describe 'PUT /api/v4/packages/npm/-/package/*package_name/dist-tags/:tag' do
+    let(:package) { create(:npm_package, project: project) }
+    let(:package_name) { package.name }
+    let(:user) { create(:user) }
+    let(:tag_name) { 'test' }
+    let(:version) { package.version }
+    let(:url) { "/packages/npm/-/package/#{package_name}/dist-tags/#{tag_name}" }
+
+    subject { put api(url), env: { 'api.request.body': version } }
+
+    context 'with packages feature enabled' do
+      before do
+        stub_licensed_features(packages: true)
+      end
+
+      context 'with public group' do
+        context 'with authenticated user' do
+          subject { put api(url, user), env: { 'api.request.body': version } }
+
+          it_behaves_like 'create package tag', :maintainer
+          it_behaves_like 'create package tag', :developer
+          it_behaves_like 'rejects package tags access', :reporter, :forbidden
+          it_behaves_like 'rejects package tags access', :guest, :forbidden
+        end
+
+        context 'with unauthenticated user' do
+          it_behaves_like 'rejects package tags access', :no_type, :unauthorized
+        end
+      end
+    end
+
+    context 'with packages feature disabled' do
+      before do
+        stub_licensed_features(packages: false)
+      end
+
+      it_behaves_like 'rejects package tags access', :no_type, :unauthorized
+    end
+  end
+
+  describe 'DELETE /api/v4/packages/npm/-/package/*package_name/dist-tags/:tag' do
+    let(:package) { create(:npm_package, project: project) }
+    let(:package_tag) { create(:package_tag, package: package) }
+    let(:user) { create(:user) }
+    let(:package_name) { package.name }
+    let(:tag_name) { package_tag.name }
+    let(:url) { "/packages/npm/-/package/#{package_name}/dist-tags/#{tag_name}" }
+
+    subject { delete api(url) }
+
+    context 'with packages feature enabled' do
+      before do
+        stub_licensed_features(packages: true)
+      end
+
+      context 'with public group' do
+        context 'with authenticated user' do
+          subject { delete api(url, user) }
+
+          it_behaves_like 'delete package tag', :maintainer
+          it_behaves_like 'rejects package tags access', :developer, :forbidden
+          it_behaves_like 'rejects package tags access', :reporter, :forbidden
+          it_behaves_like 'rejects package tags access', :guest, :forbidden
+        end
+
+        context 'with unauthenticated user' do
+          it_behaves_like 'rejects package tags access', :no_type, :unauthorized
+        end
+      end
+    end
+
+    context 'with packages feature disabled' do
+      before do
+        stub_licensed_features(packages: false)
+      end
+
+      it_behaves_like 'rejects package tags access', :no_type, :unauthorized
+    end
+  end
+
   def expect_a_valid_package_response
     expect(response).to have_gitlab_http_status(200)
     expect(response.content_type.to_s).to eq('application/json')
     expect(response).to match_response_schema('public_api/v4/packages/npm_package', dir: 'ee')
     expect(json_response['name']).to eq(package.name)
     expect(json_response['versions'][package.version]).to match_schema('public_api/v4/packages/npm_package_version', dir: 'ee')
+    expect(json_response['dist-tags']).to match_schema('public_api/v4/packages/npm_package_tags', dir: 'ee')
   end
 end

nickbrandt/GitLab

Co-Authored-By: Sara Ahbabou <sahbabou@gitlab.com>

ee/spec/presenters/npm_package_presenter_spec.rb
@@ -39,11 +39,27 @@ describe NpmPackagePresenter do
     end
   end
 
-  describe '#dist-tags' do
+  describe '#dist_tags' do
     subject { presenter.dist_tags }
 
-    it { is_expected.to be_a(Hash) }
-    it { expect(subject.size).to eq(1) }
-    it { expect(subject[:latest]).to eq(latest_package.version) }
+    context 'for packages without tags' do
+      it { is_expected.to be_a(Hash) }
+      it { expect(subject["latest"]).to eq(latest_package.version) }
+    end
+
+    context 'for packages with tags' do
+      let!(:package_tag1) { create(:packages_tag, package: package1, name: 'release_a') }
+      let!(:package_tag2) { create(:packages_tag, package: package1, name: 'test_release') }
+      let!(:package_tag3) { create(:packages_tag, package: package2, name: 'release_b') }
+      let!(:package_tag4) { create(:packages_tag, package: latest_package, name: 'release_c') }
+      let!(:package_tag5) { create(:packages_tag, package: latest_package, name: 'latest') }
+
+      it { is_expected.to be_a(Hash) }
+      it { expect(subject[package_tag1.name]).to eq(package1.version) }
+      it { expect(subject[package_tag2.name]).to eq(package1.version) }
+      it { expect(subject[package_tag3.name]).to eq(package2.version) }
+      it { expect(subject[package_tag4.name]).to eq(latest_package.version) }
+      it { expect(subject[package_tag5.name]).to eq(latest_package.version) }
+    end
   end
 end

ee/spec/requests/api/npm_packages_spec.rb
@@ -200,6 +200,17 @@ describe API::NpmPackages do
 
             expect(response).to have_gitlab_http_status(400)
           end
+
+          context 'with empty versions' do
+            let(:params) { upload_params(package_name).merge!(versions: {}) }
+
+            it 'throws a 400 error' do
+              expect { upload_package_with_token(package_name, params) }
+              .not_to change { project.packages.count }
+
+              expect(response).to have_gitlab_http_status(400)
+            end
+          end
         end
 
         context 'invalid package name' do
@@ -223,6 +234,7 @@ describe API::NpmPackages do
           expect { upload_package_with_token(package_name, params) }
             .to change { project.packages.count }.by(1)
             .and change { Packages::PackageFile.count }.by(1)
+            .and change { Packages::Tag.count }.by(1)
 
           expect(response).to have_gitlab_http_status(200)
         end
@@ -320,6 +332,177 @@ describe API::NpmPackages do
     end
   end
 
+  describe 'GET /api/v4/packages/npm/-/package/*package_name/dist-tags' do
+    let(:package) { create(:npm_package, project: project) }
+    let!(:package_tag1) { create(:packages_tag, package: package) }
+    let!(:package_tag2) { create(:packages_tag, package: package) }
+    let(:package_name) { package.name }
+    let(:user) { create(:user) }
+    let(:url) { "/packages/npm/-/package/#{package_name}/dist-tags" }
+
+    subject { get api(url) }
+
+    context 'with packages feature enabled' do
+      before do
+        stub_licensed_features(packages: true)
+      end
+
+      context 'with public project' do
+        context 'with authenticated user' do
+          subject { get api(url, user) }
+
+          it_behaves_like 'returns package tags', :maintainer
+          it_behaves_like 'returns package tags', :developer
+          it_behaves_like 'returns package tags', :reporter
+          it_behaves_like 'returns package tags', :guest
+        end
+
+        context 'with unauthenticated user' do
+          it_behaves_like 'returns package tags', :no_type
+        end
+      end
+
+      context 'with private project' do
+        let(:project) { create(:project, :private) }
+
+        context 'with authenticated user' do
+          subject { get api(url, user) }
+
+          it_behaves_like 'returns package tags', :maintainer
+          it_behaves_like 'returns package tags', :developer
+          it_behaves_like 'returns package tags', :reporter
+          it_behaves_like 'rejects package tags access', :guest, :forbidden
+        end
+
+        context 'with unauthenticated user' do
+          it_behaves_like 'rejects package tags access', :no_type, :forbidden
+        end
+      end
+    end
+
+    context 'with packages feature disabled' do
+      before do
+        stub_licensed_features(packages: false)
+      end
+
+      it_behaves_like 'rejects package tags access', :no_type, :forbidden
+    end
+  end
+
+  describe 'PUT /api/v4/packages/npm/-/package/*package_name/dist-tags/:tag' do
+    let(:package) { create(:npm_package, project: project) }
+    let(:package_name) { package.name }
+    let(:user) { create(:user) }
+    let(:tag_name) { 'test' }
+    let(:version) { package.version }
+    let(:url) { "/packages/npm/-/package/#{package_name}/dist-tags/#{tag_name}" }
+
+    subject { put api(url), env: { 'api.request.body': version } }
+
+    context 'with packages feature enabled' do
+      before do
+        stub_licensed_features(packages: true)
+      end
+
+      context 'with public project' do
+        context 'with authenticated user' do
+          subject { put api(url, user), env: { 'api.request.body': version } }
+
+          it_behaves_like 'create package tag', :maintainer
+          it_behaves_like 'create package tag', :developer
+          it_behaves_like 'rejects package tags access', :reporter, :forbidden
+          it_behaves_like 'rejects package tags access', :guest, :forbidden
+        end
+
+        context 'with unauthenticated user' do
+          it_behaves_like 'rejects package tags access', :no_type, :unauthorized
+        end
+      end
+
+      context 'with private project' do
+        let(:project) { create(:project, :private) }
+
+        context 'with authenticated user' do
+          subject { put api(url, user), env: { 'api.request.body': version } }
+
+          it_behaves_like 'create package tag', :maintainer
+          it_behaves_like 'create package tag', :developer
+          it_behaves_like 'rejects package tags access', :reporter, :forbidden
+          it_behaves_like 'rejects package tags access', :guest, :forbidden
+        end
+
+        context 'with unauthenticated user' do
+          it_behaves_like 'rejects package tags access', :no_type, :unauthorized
+        end
+      end
+    end
+
+    context 'with packages feature disabled' do
+      before do
+        stub_licensed_features(packages: false)
+      end
+
+      it_behaves_like 'rejects package tags access', :no_type, :unauthorized
+    end
+  end
+
+  describe 'DELETE /api/v4/packages/npm/-/package/*package_name/dist-tags/:tag' do
+    let(:package) { create(:npm_package, project: project) }
+    let(:package_tag) { create(:packages_tag, package: package) }
+    let(:user) { create(:user) }
+    let(:package_name) { package.name }
+    let(:tag_name) { package_tag.name }
+    let(:url) { "/packages/npm/-/package/#{package_name}/dist-tags/#{tag_name}" }
+
+    subject { delete api(url) }
+
+    context 'with packages feature enabled' do
+      before do
+        stub_licensed_features(packages: true)
+      end
+
+      context 'with public project' do
+        context 'with authenticated user' do
+          subject { delete api(url, user) }
+
+          it_behaves_like 'delete package tag', :maintainer
+          it_behaves_like 'rejects package tags access', :developer, :forbidden
+          it_behaves_like 'rejects package tags access', :reporter, :forbidden
+          it_behaves_like 'rejects package tags access', :guest, :forbidden
+        end
+
+        context 'with unauthenticated user' do
+          it_behaves_like 'rejects package tags access', :no_type, :unauthorized
+        end
+      end
+
+      context 'with private project' do
+        let(:project) { create(:project, :private) }
+
+        context 'with authenticated user' do
+          subject { delete api(url, user) }
+
+          it_behaves_like 'delete package tag', :maintainer
+          it_behaves_like 'rejects package tags access', :developer, :forbidden
+          it_behaves_like 'rejects package tags access', :reporter, :forbidden
+          it_behaves_like 'rejects package tags access', :guest, :forbidden
+        end
+
+        context 'with unauthenticated user' do
+          it_behaves_like 'rejects package tags access', :no_type, :unauthorized
+        end
+      end
+    end
+
+    context 'with packages feature disabled' do
+      before do
+        stub_licensed_features(packages: false)
+      end
+
+      it_behaves_like 'rejects package tags access', :no_type, :unauthorized
+    end
+  end
+
   def expect_a_valid_package_response
     expect(response).to have_gitlab_http_status(200)
     expect(response.content_type.to_s).to eq('application/json')
@@ -329,5 +512,6 @@ describe API::NpmPackages do
     NpmPackagePresenter::NPM_VALID_DEPENDENCY_TYPES.each do |dependency_type|
       expect(json_response.dig('versions', package.version, dependency_type.to_s)).to be_any
     end
+    expect(json_response['dist-tags']).to match_schema('public_api/v4/packages/npm_package_tags', dir: 'ee')
   end
 end