48eaed2c3f4c08dc0799a69f16fa4b73fd7be893
Roger Meier <r.meier@siemens.com>
2020-01-07 14:25:52 +0000

    feat: fingerprint_sha256 for SSH keys background migration
    Closes gitlab-org/gitlab#39455

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_170221) do
+ActiveRecord::Schema.define(version: 2020_01_06_071113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
