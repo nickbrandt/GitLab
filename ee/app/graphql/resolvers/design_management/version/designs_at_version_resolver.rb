4c739cb7a5762d95cffd5653fd84a89d9620fba5
Alex Kalderimis <alex.kalderimis@gmail.com>
2020-01-09 20:04:31 +0000

    Split up resolver into custom singular version

--- ee/app/graphql/resolvers/design_management/version/designs_at_version_resolver.rb
+++ ee/app/graphql/resolvers/design_management/version/designs_at_version_resolver.rb
@@ -11,19 +11,6 @@ module Resolvers
 
         authorize :read_design
 
-        # For use in single resolver context
-        argument :id, GraphQL::ID_TYPE,
-                 required: false,
-                 as: :design_at_version_id,
-                 description: 'The ID of the DesignAtVersion'
-        argument :design_id, GraphQL::ID_TYPE,
-                 required: false,
-                 description: 'The ID of a specific design'
-        argument :filename, GraphQL::STRING_TYPE,
-                 required: false,
-                 description: 'The filename of a specific design'
-
-        # For use in default (collection) context
         argument :ids,
                  [GraphQL::ID_TYPE],
                  required: false,
@@ -33,63 +20,15 @@ module Resolvers
                  required: false,
                  description: 'Filters designs by their filename'
 
-        def resolve(ids: nil, filenames: nil, design_id: nil, filename: nil, design_at_version_id: nil)
-          validate_arguments(ids, filenames, design_id, filename, design_at_version_id)
+        def self.single
+          ::Resolvers::DesignManagement::Version::DesignAtVersionResolver
+        end
 
-          return specific_design_at_version(design_at_version_id) if design_at_version_id
-
-          design_ids = array_argument(design_id, ids)
-          filenames = array_argument(filename, filenames)
-
-          find(design_ids, filenames).execute.map { |d| make(d) }
+        def resolve(ids: nil, filenames: nil)
+          find(ids, filenames).execute.map { |d| make(d) }
         end
 
         private
-
-        def validate_arguments(ids, filenames, design_id, filename, design_at_version_id)
-          if single?
-            expect_one(filename: filename, id: design_at_version_id, design_id: design_id)
-            forbid(ids: ids, filenames: filenames)
-          else
-            forbid(filename: filename, id: design_at_version_id, design_id: design_id)
-          end
-        end
-
-        # Take a nullable scalar object and a nullable array and return an
-        # array of their values that is nil if both arguments are nil
-        def array_argument(one, many)
-          return if one.nil? && many.nil?
-
-          Array.wrap(one).concat(Array.wrap(many))
-        end
-
-        def expect_one(args)
-          passed = defined(args)
-
-          return if passed.size == 1
-
-          raise Gitlab::Graphql::Errors::ArgumentError, "Exactly one of #{args.keys.join(', ')} expected, got #{passed}"
-        end
-
-        def forbid(args)
-          passed = defined(args)
-
-          return if passed.empty?
-
-          raise Gitlab::Graphql::Errors::ArgumentError, "Unexpected arguments: #{passed}"
-        end
-
-        def defined(args)
-          args.each.select { |(_, v)| v.present? }.map(&:first)
-        end
-
-        def specific_design_at_version(id)
-          return [] unless id.present? && Ability.allowed?(current_user, :read_design, issue)
-
-          [find_dav_by_id(id)].select do |dav|
-            dav.design.issue_id == issue.id && dav.version.id == version.id && dav.design.visible_in?(version)
-          end
-        end
 
         def find(ids, filenames)
           ids = ids&.map { |id| parse_design_id(id).model_id }
@@ -98,14 +37,6 @@ module Resolvers
                                                 ids: ids,
                                                 filenames: filenames,
                                                 visible_at_version: version)
-        end
-
-        def current_user
-          context[:current_user]
-        end
-
-        def find_dav_by_id(id)
-          GitlabSchema.object_from_id(id, expected_type: ::DesignManagement::DesignAtVersion)
         end
 
         def parse_design_id(id)
