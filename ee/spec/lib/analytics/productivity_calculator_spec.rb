abbc2ddaf97ccfcdd9cdd8ef03f552afa077c5ae
Pavel Shutsin <pshutsin@gitlab.com>
2020-01-14 12:27:05 +0300

    Add first comment metric calculation
    Metric is used at Code Review Analytics page

--- ee/spec/lib/analytics/productivity_calculator_spec.rb
+++ ee/spec/lib/analytics/productivity_calculator_spec.rb
@@ -5,20 +5,45 @@ require 'spec_helper'
 describe Analytics::ProductivityCalculator do
   subject { described_class.new(merge_request) }
 
-  let(:merge_request) { create(:merge_request_with_diff_notes, :merged, :with_diffs, created_at: 31.days.ago) }
+  let_it_be(:merge_request) { create(:merge_request, :merged, :with_diffs, created_at: 31.days.ago) }
+  let_it_be(:merge_request_note) do
+    create(:diff_note_on_merge_request, noteable: merge_request, project: merge_request.source_project, author: create(:user))
+  end
+  let_it_be(:merge_request_author_note) do
+    create(:diff_note_on_merge_request,
+           noteable: merge_request,
+           project: merge_request.source_project,
+           author: merge_request.author,
+           created_at: 11.months.ago
+          )
+  end
+  let_it_be(:merge_request_bot_note) do
+    create(:diff_note_on_merge_request,
+           noteable: merge_request,
+           project: merge_request.source_project,
+           author: create(:user, :bot),
+           created_at: 12.months.ago
+          )
+  end
 
   describe '#productivity_data' do
     it 'calculates productivity data' do
       expected_data = {
-        first_comment_at: merge_request.notes.order(created_at: :asc).first.created_at,
-        first_commit_at: merge_request.first_commit.authored_date,
-        last_commit_at: merge_request.merge_request_diff.last_commit.committed_date,
+        first_comment_at: be_like_time(merge_request_note.created_at),
+        first_commit_at: be_like_time(merge_request.first_commit.authored_date),
+        last_commit_at: be_like_time(merge_request.merge_request_diff.last_commit.committed_date),
         commits_count: merge_request.commits_count,
         diff_size: merge_request.merge_request_diff.lines_count,
         modified_paths_size: merge_request.modified_paths.size
       }
 
-      expect(subject.productivity_data).to eq(expected_data)
+      expect(subject.productivity_data).to match(expected_data)
+    end
+  end
+
+  describe '#first_comment_at' do
+    it 'returns first non-author comment' do
+      expect(subject.first_comment_at).to be_like_time(merge_request_note.created_at)
     end
   end
 end
