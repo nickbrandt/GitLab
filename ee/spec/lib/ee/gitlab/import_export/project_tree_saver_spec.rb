052ef2a4d5627354408962c5e90bb95ee3698f47
Imre Farkas <ifarkas@gitlab.com>
2020-01-14 11:02:37 +0000

    Add Epics to Project import/export
    As Epics are Group level objects,
    ImportExport::GroupProjectObjectBuilder tries to find existing one on
    the Group level, and falls back to creating one if not found.

--- ee/spec/lib/ee/gitlab/import_export/project_tree_saver_spec.rb
+++ ee/spec/lib/ee/gitlab/import_export/project_tree_saver_spec.rb
@@ -4,12 +4,18 @@ require 'spec_helper'
 
 describe Gitlab::ImportExport::ProjectTreeSaver do
   describe 'saves the project tree into a json object' do
-    set(:user) { create(:user) }
-    set(:project) { create(:project) }
-    set(:issue) { create(:issue, project: project) }
-    set(:design) { create(:design, :with_file, versions_count: 2, issue: issue) }
-    set(:note) { create(:diff_note_on_design, noteable: design, project: project, author: user) }
-    set(:note2) { create(:note, noteable: issue, project: project, author: user) }
+    let_it_be(:user) { create(:user) }
+    let_it_be(:group) { create(:group) }
+    let_it_be(:project) { create(:project, group: group) }
+    let_it_be(:issue) { create(:issue, project: project) }
+
+    let_it_be(:design) { create(:design, :with_file, versions_count: 2, issue: issue) }
+    let_it_be(:note) { create(:diff_note_on_design, noteable: design, project: project, author: user) }
+    let_it_be(:note2) { create(:note, noteable: issue, project: project, author: user) }
+
+    let_it_be(:epic) { create(:epic, group: group) }
+    let_it_be(:epic_issue) { create(:epic_issue, issue: issue, epic: epic) }
+
     let(:shared) { project.import_export_shared }
     let(:export_path) { "#{Dir.tmpdir}/project_tree_saver_spec_ee" }
     let(:project_tree_saver) { described_class.new(project: project, current_user: user, shared: shared) }
@@ -50,6 +56,16 @@ describe Gitlab::ImportExport::ProjectTreeSaver do
         actions.each do |action|
           expect(action['design']).to be_present
         end
+      end
+    end
+
+    context 'epics' do
+      it 'has issue epic' do
+        expect(saved_project_json['issues'].first['epic']).not_to be_empty
+      end
+
+      it 'has issue epic id' do
+        expect(saved_project_json['issues'].first['epic']['id']).to eql(epic.id)
       end
     end
   end
