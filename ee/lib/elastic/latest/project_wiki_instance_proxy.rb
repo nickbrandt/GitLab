2b0f6dce7048431659ac1556c5c441db28d1f27e
Mark Chao <mchao@gitlab.com>
2020-01-14 12:10:40 +0800

    ES: Wrap wiki search results as FoundWikiPage
    This is similar to FoundBlob, but allows different policy checks.
    Delegation is used instead of inheritance because it allows reuse of
    SearchResults.parse_search_result.

--- ee/lib/elastic/latest/project_wiki_instance_proxy.rb
+++ ee/lib/elastic/latest/project_wiki_instance_proxy.rb
@@ -8,6 +8,13 @@ module Elastic
       delegate :project, to: :target
       delegate :id, to: :project, prefix: true
 
+      # @return [Kaminari::PaginatableArray]
+      def elastic_search_as_wiki_page(query, page: 1, per: 20, options: {})
+        options = repository_specific_options(options)
+
+        self.class.elastic_search_as_wiki_page(query, page: page, per: per, options: options)
+      end
+
       private
 
       def repository_id
---
+++ ee/spec/lib/elastic/latest/project_wiki_instance_proxy_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Elastic::Latest::ProjectWikiInstanceProxy do
+  let_it_be(:project) { create(:project, :wiki_repo) }
+
+  subject { described_class.new(project.wiki) }
+
+  describe '#elastic_search_as_wiki_page' do
+    let(:params) do
+      {
+        page: 2,
+        per: 30,
+        options: { foo: :bar }
+      }
+    end
+
+    it 'provides repository_id if not provided' do
+      expected_params = params.deep_dup
+      expected_params[:options][:repository_id] = "wiki_#{project.id}"
+
+      expect(subject.class).to receive(:elastic_search_as_wiki_page).with('foo', expected_params)
+
+      subject.elastic_search_as_wiki_page('foo', params)
+    end
+
+    it 'uses provided repository_id' do
+      params[:options][:repository_id] = "wiki_63"
+
+      expect(subject.class).to receive(:elastic_search_as_wiki_page).with('foo', params)
+
+      subject.elastic_search_as_wiki_page('foo', params)
+    end
+  end
+end
