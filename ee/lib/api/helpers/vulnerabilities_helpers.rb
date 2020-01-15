d3cf6188511f5b102589ac5ab52b142f426ffcd2
Victor Zagorodny <3337413-vzagorodny@users.noreply.gitlab.com>
2020-01-15 05:30:45 +0000

    Add service to delete VulnerabilityIssueLinks
    Add service class w/ tests. Improve
    ServiceResponse class and
    vulnerabilities_issue_link factory for this.

--- ee/lib/api/helpers/vulnerabilities_helpers.rb
+++ ee/lib/api/helpers/vulnerabilities_helpers.rb
@@ -5,8 +5,12 @@ module API
     module VulnerabilitiesHelpers
       def find_and_authorize_vulnerability!(action)
         find_vulnerability!.tap do |vulnerability|
-          authorize! action, vulnerability.project
+          authorize_vulnerability!(vulnerability, action)
         end
+      end
+
+      def authorize_vulnerability!(vulnerability, action)
+        authorize! action, vulnerability.project
       end
     end
   end
