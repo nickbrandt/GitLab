9fee239f5e9d788152f374cf4ff4c3726f968845
Tom Quirk <tquirk@gitlab.com>
2020-01-16 09:55:50 +0000

    Add design_scaler component
    This component is intended to be the
    UI entry point for scaling/zooming a design.
    
    It emits a @scale event, and holds the increment
    and constraint logic for updating scale

--- ee/app/assets/javascripts/design_management/components/design_presentation.vue
+++ ee/app/assets/javascripts/design_management/components/design_presentation.vue
@@ -1,4 +1,5 @@
 <script>
+import _ from 'underscore';
 import DesignImage from './image.vue';
 import DesignOverlay from './design_overlay.vue';
 
@@ -27,12 +28,24 @@ export default {
       required: false,
       default: false,
     },
+    scale: {
+      type: Number,
+      required: false,
+      default: 1,
+    },
   },
   data() {
     return {
       overlayDimensions: null,
       overlayPosition: null,
       currentAnnotationCoordinates: null,
+      zoomFocalPoint: {
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+      },
+      initialLoad: true,
     };
   },
   computed: {
@@ -43,6 +56,22 @@ export default {
       return (this.isAnnotating && this.currentAnnotationCoordinates) || null;
     },
   },
+  beforeDestroy() {
+    const { presentationViewport } = this.$refs;
+    if (!presentationViewport) return;
+
+    presentationViewport.removeEventListener('scroll', this.scrollThrottled, false);
+  },
+  mounted() {
+    const { presentationViewport } = this.$refs;
+    if (!presentationViewport) return;
+
+    this.scrollThrottled = _.throttle(() => {
+      this.shiftZoomFocalPoint();
+    }, 400);
+
+    presentationViewport.addEventListener('scroll', this.scrollThrottled, false);
+  },
   methods: {
     setOverlayDimensions(overlayDimensions) {
       this.overlayDimensions = overlayDimensions;
@@ -52,8 +81,8 @@ export default {
         this.overlayPosition = {};
       }
 
-      const { presentationViewport } = this.$refs;
-      if (!presentationViewport) return;
+      const { presentationContainer } = this.$refs;
+      if (!presentationContainer) return;
 
       // default to center
       this.overlayPosition = {
@@ -62,16 +91,88 @@ export default {
       };
 
       // if the overlay overflows, then don't center
-      if (this.overlayDimensions.width > presentationViewport.offsetWidth) {
+      if (this.overlayDimensions.width > presentationContainer.offsetWidth) {
         this.overlayPosition.left = '0';
       }
-      if (this.overlayDimensions.height > presentationViewport.offsetHeight) {
+      if (this.overlayDimensions.height > presentationContainer.offsetHeight) {
         this.overlayPosition.top = '0';
       }
+    },
+    /**
+     * Return a point that represents the center of an
+     * overflowing child element w.r.t it's parent
+     */
+    getViewportCenter() {
+      const { presentationViewport } = this.$refs;
+      if (!presentationViewport) return {};
+
+      // get height of scroll bars (i.e. the max values for scrollTop, scrollLeft)
+      const scrollBarWidth = presentationViewport.scrollWidth - presentationViewport.offsetWidth;
+      const scrollBarHeight = presentationViewport.scrollHeight - presentationViewport.offsetHeight;
+
+      // determine how many child pixels have been scrolled
+      const xScrollRatio =
+        presentationViewport.scrollLeft > 0 ? presentationViewport.scrollLeft / scrollBarWidth : 0;
+      const yScrollRatio =
+        presentationViewport.scrollTop > 0 ? presentationViewport.scrollTop / scrollBarHeight : 0;
+      const xScrollOffset =
+        (presentationViewport.scrollWidth - presentationViewport.offsetWidth - 0) * xScrollRatio;
+      const yScrollOffset =
+        (presentationViewport.scrollHeight - presentationViewport.offsetHeight - 0) * yScrollRatio;
+
+      const viewportCenterX = presentationViewport.offsetWidth / 2;
+      const viewportCenterY = presentationViewport.offsetHeight / 2;
+      const focalPointX = viewportCenterX + xScrollOffset;
+      const focalPointY = viewportCenterY + yScrollOffset;
+
+      return {
+        x: focalPointX,
+        y: focalPointY,
+      };
+    },
+    /**
+     * Scroll the viewport such that the focal point is positioned centrally
+     */
+    scrollToFocalPoint() {
+      const { presentationViewport } = this.$refs;
+      if (!presentationViewport) return;
+
+      const scrollX = this.zoomFocalPoint.x - presentationViewport.offsetWidth / 2;
+      const scrollY = this.zoomFocalPoint.y - presentationViewport.offsetHeight / 2;
+
+      presentationViewport.scrollTo(scrollX, scrollY);
+    },
+    scaleZoomFocalPoint() {
+      const { x, y, width, height } = this.zoomFocalPoint;
+      const widthRatio = this.overlayDimensions.width / width;
+      const heightRatio = this.overlayDimensions.height / height;
+
+      this.zoomFocalPoint = {
+        x: Math.round(x * widthRatio * 100) / 100,
+        y: Math.round(y * heightRatio * 100) / 100,
+        ...this.overlayDimensions,
+      };
+    },
+    shiftZoomFocalPoint() {
+      this.zoomFocalPoint = {
+        ...this.getViewportCenter(),
+        ...this.overlayDimensions,
+      };
     },
     onImageResize(imageDimensions) {
       this.setOverlayDimensions(imageDimensions);
       this.setOverlayPosition();
+
+      this.$nextTick(() => {
+        if (this.initialLoad) {
+          // set focal point on initial load
+          this.shiftZoomFocalPoint();
+          this.initialLoad = false;
+        } else {
+          this.scaleZoomFocalPoint();
+          this.scrollToFocalPoint();
+        }
+      });
     },
     openCommentForm(position) {
       const { x, y } = position;
@@ -89,9 +190,18 @@ export default {
 </script>
 
 <template>
-  <div ref="presentationViewport" class="h-100 w-100 p-3 overflow-auto">
-    <div class="h-100 w-100 d-flex align-items-center position-relative">
-      <design-image v-if="image" :image="image" :name="imageName" @resize="onImageResize" />
+  <div ref="presentationViewport" class="h-100 w-100 p-3 overflow-auto position-relative">
+    <div
+      ref="presentationContainer"
+      class="h-100 w-100 d-flex align-items-center position-relative"
+    >
+      <design-image
+        v-if="image"
+        :image="image"
+        :name="imageName"
+        :scale="scale"
+        @resize="onImageResize"
+      />
       <design-overlay
         v-if="overlayDimensions && overlayPosition"
         :dimensions="overlayDimensions"
--- ee/spec/frontend/design_management/components/design_presentation_spec.js
+++ ee/spec/frontend/design_management/components/design_presentation_spec.js
@@ -75,47 +75,64 @@ describe('Design management design presentation component', () => {
     });
   });
 
-  it('currentCommentForm is null when isAnnotating is false', () => {
-    createComponent({
-      image: 'test.jpg',
-      imageName: 'test',
+  describe('currentCommentForm', () => {
+    it('currentCommentForm is null when isAnnotating is false', () => {
+      createComponent(
+        {
+          image: 'test.jpg',
+          imageName: 'test',
+        },
+        mockOverlayData,
+      );
+
+      return wrapper.vm.$nextTick().then(() => {
+        expect(wrapper.vm.currentCommentForm).toBeNull();
+        expect(wrapper.element).toMatchSnapshot();
+      });
     });
 
-    expect(wrapper.vm.currentCommentForm).toBeNull();
-  });
+    it('currentCommentForm is null when isAnnotating is true but annotation coordinates are falsey', () => {
+      createComponent(
+        {
+          image: 'test.jpg',
+          imageName: 'test',
+          isAnnotating: true,
+        },
+        mockOverlayData,
+      );
 
-  it('currentCommentForm is null when isAnnotating is true but annotation coordinates are falsey', () => {
-    createComponent({
-      image: 'test.jpg',
-      imageName: 'test',
-      isAnnotating: true,
+      return wrapper.vm.$nextTick().then(() => {
+        expect(wrapper.vm.currentCommentForm).toBeNull();
+        expect(wrapper.element).toMatchSnapshot();
+      });
     });
 
-    expect(wrapper.vm.currentCommentForm).toBeNull();
-  });
-
-  it('currentCommentForm is equal to current annotation coordinates when isAnnotating is true', () => {
-    createComponent(
-      {
-        image: 'test.jpg',
-        imageName: 'test',
-        isAnnotating: true,
-      },
-      {
-        currentAnnotationCoordinates: {
+    it('currentCommentForm is equal to current annotation coordinates when isAnnotating is true', () => {
+      createComponent(
+        {
+          image: 'test.jpg',
+          imageName: 'test',
+          isAnnotating: true,
+        },
+        {
+          ...mockOverlayData,
+          currentAnnotationCoordinates: {
+            x: 1,
+            y: 1,
+            width: 100,
+            height: 100,
+          },
+        },
+      );
+      return wrapper.vm.$nextTick().then(() => {
+        expect(wrapper.vm.currentCommentForm).toEqual({
           x: 1,
           y: 1,
           width: 100,
           height: 100,
-        },
-      },
-    );
-
-    expect(wrapper.vm.currentCommentForm).toEqual({
-      x: 1,
-      y: 1,
-      width: 100,
-      height: 100,
+        });
+        expect(wrapper.element).toMatchSnapshot();
+      });
     });
   });
 
@@ -135,8 +152,10 @@ describe('Design management design presentation component', () => {
     });
 
     it('sets overlay position correctly when overlay is smaller than viewport', () => {
-      jest.spyOn(wrapper.vm.$refs.presentationViewport, 'offsetWidth', 'get').mockReturnValue(200);
-      jest.spyOn(wrapper.vm.$refs.presentationViewport, 'offsetHeight', 'get').mockReturnValue(200);
+      jest.spyOn(wrapper.vm.$refs.presentationContainer, 'offsetWidth', 'get').mockReturnValue(200);
+      jest
+        .spyOn(wrapper.vm.$refs.presentationContainer, 'offsetHeight', 'get')
+        .mockReturnValue(200);
 
       wrapper.vm.setOverlayPosition();
       expect(wrapper.vm.overlayPosition).toEqual({
@@ -146,8 +165,10 @@ describe('Design management design presentation component', () => {
     });
 
     it('sets overlay position correctly when overlay width is larger than viewports', () => {
-      jest.spyOn(wrapper.vm.$refs.presentationViewport, 'offsetWidth', 'get').mockReturnValue(50);
-      jest.spyOn(wrapper.vm.$refs.presentationViewport, 'offsetHeight', 'get').mockReturnValue(200);
+      jest.spyOn(wrapper.vm.$refs.presentationContainer, 'offsetWidth', 'get').mockReturnValue(50);
+      jest
+        .spyOn(wrapper.vm.$refs.presentationContainer, 'offsetHeight', 'get')
+        .mockReturnValue(200);
 
       wrapper.vm.setOverlayPosition();
       expect(wrapper.vm.overlayPosition).toEqual({
@@ -157,13 +178,171 @@ describe('Design management design presentation component', () => {
     });
 
     it('sets overlay position correctly when overlay height is larger than viewports', () => {
-      jest.spyOn(wrapper.vm.$refs.presentationViewport, 'offsetWidth', 'get').mockReturnValue(200);
-      jest.spyOn(wrapper.vm.$refs.presentationViewport, 'offsetHeight', 'get').mockReturnValue(50);
+      jest.spyOn(wrapper.vm.$refs.presentationContainer, 'offsetWidth', 'get').mockReturnValue(200);
+      jest.spyOn(wrapper.vm.$refs.presentationContainer, 'offsetHeight', 'get').mockReturnValue(50);
 
       wrapper.vm.setOverlayPosition();
       expect(wrapper.vm.overlayPosition).toEqual({
         left: `calc(50% - ${mockOverlayData.overlayDimensions.width / 2}px)`,
         top: '0',
+      });
+    });
+  });
+
+  describe('getViewportCenter', () => {
+    /**
+     * Spy on $refs.presentationViewport with given values
+     * @param {Object} viewportDimensions {width, height}
+     * @param {Object} childDimensions {width, height}
+     * @param {Float} scrollTopPerc 0 < x < 1
+     * @param {Float} scrollLeftPerc  0 < x < 1
+     */
+    const spyOnPresentationViewport = (
+      viewportDimensions,
+      childDimensions,
+      scrollTopPerc,
+      scrollLeftPerc,
+    ) => {
+      jest
+        .spyOn(wrapper.vm.$refs.presentationViewport, 'scrollWidth', 'get')
+        .mockReturnValue(childDimensions.width);
+      jest
+        .spyOn(wrapper.vm.$refs.presentationViewport, 'scrollHeight', 'get')
+        .mockReturnValue(childDimensions.height);
+      jest
+        .spyOn(wrapper.vm.$refs.presentationViewport, 'offsetWidth', 'get')
+        .mockReturnValue(viewportDimensions.width);
+      jest
+        .spyOn(wrapper.vm.$refs.presentationViewport, 'offsetHeight', 'get')
+        .mockReturnValue(viewportDimensions.height);
+      jest
+        .spyOn(wrapper.vm.$refs.presentationViewport, 'scrollLeft', 'get')
+        .mockReturnValue((childDimensions.width - viewportDimensions.width) * scrollLeftPerc);
+      jest
+        .spyOn(wrapper.vm.$refs.presentationViewport, 'scrollTop', 'get')
+        .mockReturnValue((childDimensions.height - viewportDimensions.height) * scrollTopPerc);
+    };
+
+    beforeEach(() => {
+      createComponent(
+        {
+          image: 'test.jpg',
+          imageName: 'test',
+        },
+        mockOverlayData,
+      );
+    });
+
+    it('calculate center correctly with no scroll', () => {
+      spyOnPresentationViewport({ width: 10, height: 10 }, { width: 20, height: 20 }, 0, 0);
+
+      expect(wrapper.vm.getViewportCenter()).toEqual({
+        x: 5,
+        y: 5,
+      });
+    });
+
+    it('calculate center correctly with some scroll', () => {
+      spyOnPresentationViewport({ width: 10, height: 10 }, { width: 20, height: 20 }, 0.5, 0.5);
+
+      expect(wrapper.vm.getViewportCenter()).toEqual({
+        x: 10,
+        y: 10,
+      });
+    });
+
+    it('Returns default case if no overflow (scrollWidth==offsetWidth, etc.)', () => {
+      spyOnPresentationViewport({ width: 20, height: 20 }, { width: 20, height: 20 }, 0.5, 0.5);
+
+      expect(wrapper.vm.getViewportCenter()).toEqual({
+        x: 10,
+        y: 10,
+      });
+    });
+  });
+
+  describe('scaleZoomFocalPoint', () => {
+    it('scaleZoomFocalPoint scales focal point correctly when zooming in', () => {
+      createComponent(
+        {
+          image: 'test.jpg',
+          imageName: 'test',
+        },
+        {
+          ...mockOverlayData,
+          zoomFocalPoint: {
+            x: 5,
+            y: 5,
+            width: 50,
+            height: 50,
+          },
+        },
+      );
+
+      wrapper.vm.scaleZoomFocalPoint();
+      expect(wrapper.vm.zoomFocalPoint).toEqual({
+        x: 10,
+        y: 10,
+        width: 100,
+        height: 100,
+      });
+    });
+
+    it('scaleZoomFocalPoint scales focal point correctly when zooming out', () => {
+      createComponent(
+        {
+          image: 'test.jpg',
+          imageName: 'test',
+        },
+        {
+          ...mockOverlayData,
+          zoomFocalPoint: {
+            x: 10,
+            y: 10,
+            width: 200,
+            height: 200,
+          },
+        },
+      );
+
+      wrapper.vm.scaleZoomFocalPoint();
+      expect(wrapper.vm.zoomFocalPoint).toEqual({
+        x: 5,
+        y: 5,
+        width: 100,
+        height: 100,
+      });
+    });
+  });
+
+  describe('onImageResize', () => {
+    it('sets zoom focal point on initial load', () => {
+      createComponent(
+        {
+          image: 'test.jpg',
+          imageName: 'test',
+        },
+        mockOverlayData,
+      );
+
+      wrapper.setMethods({
+        shiftZoomFocalPoint: jest.fn(),
+        scaleZoomFocalPoint: jest.fn(),
+        scrollToFocalPoint: jest.fn(),
+      });
+
+      wrapper.vm.onImageResize({ width: 10, height: 10 });
+      return wrapper.vm.$nextTick().then(() => {
+        expect(wrapper.vm.shiftZoomFocalPoint).toHaveBeenCalled();
+        expect(wrapper.vm.initialLoad).toBe(false);
+      });
+    });
+
+    it('calls scaleZoomFocalPoint and scrollToFocalPoint after initial load', () => {
+      wrapper.vm.onImageResize({ width: 10, height: 10 });
+      return wrapper.vm.$nextTick().then(() => {
+        expect(wrapper.vm.scaleZoomFocalPoint).toHaveBeenCalled();
+        expect(wrapper.vm.scrollToFocalPoint).toHaveBeenCalled();
       });
     });
   });
