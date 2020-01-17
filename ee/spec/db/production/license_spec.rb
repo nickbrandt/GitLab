bd18aa43e61b2ecf641e3d660c55a02a3fc6f91c
David Fernandez <dfernandez@gitlab.com>
2020-01-17 16:35:14 +0100

    Add seed for the license file
    Use a common rake task between the development and production seed

--- ee/spec/db/production/license_spec.rb
+++ ee/spec/db/production/license_spec.rb
@@ -4,52 +4,9 @@ require 'spec_helper'
 describe 'Automated License Installation' do
   subject { load Rails.root.join('ee', 'db', 'fixtures', 'production', '010_license.rb') }
 
-  it 'works when no license to be installed' do
-    expect { subject }.not_to raise_error
-  end
+  it 'executes the gitlab:license:load task' do
+    expect(Rake::Task).to receive(:[]).with('gitlab:license:load').and_return(OpenStruct.new(invoke: true))
 
-  context 'when GITLAB_LICENSE_FILE env variable is set' do
-    let(:license_path) { 'arbitrary_file_name' }
-
-    before do
-      stub_env('GITLAB_LICENSE_FILE', license_path)
-    end
-
-    it 'fails when the file does not exist' do
-      license_file = double('Pathname', exist?: false)
-      allow(Pathname).to receive(:new).and_call_original
-      expect(Pathname).to receive(:new).with(license_path).and_return(license_file)
-      expect { subject }.to raise_error(RuntimeError, "License File Missing")
-    end
-
-    context 'when the file does exist' do
-      before do
-        license_file = double('Pathname', exist?: true, read: license_file_contents)
-        allow(Pathname).to receive(:new).and_call_original
-        expect(Pathname).to receive(:new).with(license_path).and_return(license_file)
-      end
-
-      context 'and contains a valid license' do
-        let(:license_file_contents) { 'valid contents' }
-
-        it 'succeeds in adding the license' do
-          license = double('License', save: true)
-          expect(License).to receive(:new).with(data: license_file_contents).and_return(license)
-
-          expect { subject }.not_to raise_error
-        end
-      end
-
-      context 'but does not contain a valid license' do
-        let(:license_file_contents) { 'invalid contents' }
-
-        it 'fails to add the license' do
-          license = double('License', save: false)
-          expect(License).to receive(:new).with(data: license_file_contents).and_return(license)
-
-          expect { subject }.to raise_error(RuntimeError, "License Invalid")
-        end
-      end
-    end
+    subject
   end
 end
