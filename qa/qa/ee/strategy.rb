b5ff89295ce88d5368bf3dd71967039182978dc2
Rémy Coutable <remy@rymai.me>
2020-01-17 18:00:05 +0000

    Use 'sign_out_if_signed_in' in 'qa/qa/ee/resource/license.rb'
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- qa/qa/ee/strategy.rb
+++ qa/qa/ee/strategy.rb
@@ -10,15 +10,13 @@ module QA
       end
 
       def perform_before_hooks
-        # The login page could take some time to load the first time it is visited.
-        # We visit the login page and wait for it to properly load only once before the tests.
-        QA::Support::Retrier.retry_on_exception do
-          QA::Runtime::Browser.visit(:gitlab, QA::Page::Main::Login)
-        end
-
         return unless ENV['EE_LICENSE']
 
-        EE::Resource::License.fabricate!(ENV['EE_LICENSE'])
+        QA::Support::Retrier.retry_on_exception do
+          QA::Runtime::Browser.visit(:gitlab, QA::Page::Main::Login) do
+            EE::Resource::License.fabricate!(ENV['EE_LICENSE'])
+          end
+        end
       end
     end
   end
