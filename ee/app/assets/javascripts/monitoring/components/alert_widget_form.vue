mehulagg/gitlab_test

Add QA test for prometheus alerts
The configuration of alerts depends on a lot of moving parts and has
been prone to issue. This test sets up a cluster, creates a project,
connects the cluster to the project, sets up Auto Devops, deploys the
application, then performs basic CRUD operations on alerts from the
metrics dashboard. Alerts are configurated by updating Prometheus, so
a future test could build upon the assertions by ensuring that the alert
can indeed be fired.

ee/app/assets/javascripts/monitoring/components/alert_widget_form.vue
@@ -226,11 +226,12 @@ export default {
         <gl-dropdown
           id="alert-query-dropdown"
           :text="queryDropdownLabel"
-          toggle-class="dropdown-menu-toggle"
+          toggle-class="dropdown-menu-toggle qa-alert-query-dropdown"
         >
           <gl-dropdown-item
             v-for="query in relevantQueries"
             :key="query.metricId"
+            data-qa-selector="alert_query_option"
             @click="selectQuery(query.metricId)"
           >
             {{ query.label }}
@@ -269,6 +270,7 @@ export default {
           v-model.number="threshold"
           :disabled="formDisabled"
           type="number"
+          data-qa-selector="alert_threshold_input"
         />
       </gl-form-group>
     </div>

nickbrandt/GitLab

ee/app/assets/javascripts/monitoring/components/alert_widget_form.vue
@@ -237,11 +237,12 @@ export default {
         <gl-dropdown
           id="alert-query-dropdown"
           :text="queryDropdownLabel"
-          toggle-class="dropdown-menu-toggle"
+          toggle-class="dropdown-menu-toggle qa-alert-query-dropdown"
         >
           <gl-dropdown-item
             v-for="query in relevantQueries"
             :key="query.metricId"
+            data-qa-selector="alert_query_option"
             @click="selectQuery(query.metricId)"
           >
             {{ query.label }}
@@ -280,6 +281,7 @@ export default {
           v-model.number="threshold"
           :disabled="formDisabled"
           type="number"
+          data-qa-selector="alert_threshold_field"
         />
       </gl-form-group>
     </div>