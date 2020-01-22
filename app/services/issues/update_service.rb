3a9e4754914c42e612f4cdf430a8dd2393e4c674
Heinrich Lee Yu <heinrich@gitlab.com>
2020-01-22 13:52:21 +0800

    Trigger subscription when issue is updated

--- app/services/issues/update_service.rb
+++ app/services/issues/update_service.rb
@@ -21,6 +21,10 @@ module Issues
       spam_check(issue, current_user) unless skip_spam_check
     end
 
+    def after_update(issue)
+      GitlabSchema.subscriptions.trigger('issueUpdated', { id: issue.id }, issue)
+    end
+
     def handle_changes(issue, options)
       old_associations = options.fetch(:old_associations, {})
       old_labels = old_associations.fetch(:labels, [])
