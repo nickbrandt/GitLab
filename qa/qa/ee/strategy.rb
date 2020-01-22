245e5dedbbf83deec67ae3ba1c1d82e1d5126373
Rémy Coutable <remy@rymai.me>
2020-01-22 17:43:42 +0100

    Various improvements to the QA framework
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- qa/qa/ee/strategy.rb
+++ qa/qa/ee/strategy.rb
@@ -18,7 +18,11 @@ module QA
 
         return unless ENV['EE_LICENSE']
 
-        EE::Resource::License.fabricate!(ENV['EE_LICENSE'])
+        QA::Support::Retrier.retry_on_exception do
+          QA::Runtime::Browser.visit(:gitlab, QA::Page::Main::Login) do
+            EE::Resource::License.fabricate!(ENV['EE_LICENSE'])
+          end
+        end
       end
     end
   end
