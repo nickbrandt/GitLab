ebabf56e64f53ac57f5b60c23f346b17903dacb3
Alex Kalderimis <alex.kalderimis@gmail.com>
2020-01-22 21:16:15 +0000

    Add integration test for design tab link filtering
    This verifies that the new information from `object_link_text_extras` is
    used correctly

--- ee/spec/lib/banzai/filter/issue_reference_filter_spec.rb
+++ ee/spec/lib/banzai/filter/issue_reference_filter_spec.rb
@@ -6,24 +6,38 @@ describe Banzai::Filter::IssueReferenceFilter do
   include FilterSpecHelper
   include DesignManagementTestHelpers
 
+  let_it_be(:issue) { create(:issue) }
+  let_it_be(:project) { issue.project }
+  let_it_be(:current_user) { project.owner }
+  let_it_be(:designs_tab_url) { url_for_designs(issue) }
+
+  context 'when processing a link to the designs tab' do
+    before do
+      enable_design_management
+    end
+
+    let(:input_text) { "See #{designs_tab_url}" }
+
+    subject(:link) { reference_filter(input_text).css('a').first }
+
+    it 'includes the word "designs" after the reference in the text content', :aggregate_failures do
+      expect(link.attr('title')).to eq(issue.title)
+      expect(link.attr('href')).to eq(designs_tab_url)
+      expect(link.text).to eq("#{issue.to_reference} (designs)")
+    end
+  end
+
   describe '#object_link_text_extras' do
     before do
       enable_design_management(enabled)
     end
 
     let(:enabled) { true }
-    let_it_be(:issue) { create(:issue) }
 
-    let(:project) { issue.project }
     let(:matches) { ::Issue.link_reference_pattern.match(input_text) }
-    let(:current_user) { project.owner }
     let(:extras) { subject.object_link_text_extras(issue, matches) }
 
-    subject do
-      render_context = Banzai::RenderContext.new(project, current_user)
-      context = { project: project, current_user: current_user, render_context: render_context }
-      described_class.new(input_text, context)
-    end
+    subject { filter_instance }
 
     context 'the link does not go to the designs tab' do
       let(:input_text) { Gitlab::Routing.url_helpers.project_issue_url(issue.project, issue) }
@@ -34,7 +48,7 @@ describe Banzai::Filter::IssueReferenceFilter do
     end
 
     context 'the link goes to the designs tab' do
-      let(:input_text) { url_for_designs(issue) }
+      let(:input_text) { designs_tab_url }
 
       it 'includes designs' do
         expect(extras).to include('designs')
