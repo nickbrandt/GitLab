329ee6ea86c452cdcfe47f891c2167d8c7cf4d5d
Jiaan Louw <3468028-jiaan@users.noreply.gitlab.com>
2020-01-24 12:10:54 +0000

    Rebase of BE branch
    This includes all the work from BE branch:
    https://gitlab.com/gitlab-org/gitlab/merge_requests/20844
    
    Add new scope to gather data properly
    
    Add basic spec
    
    Fix variable name
    
    Refactor compliance controller
    
    Add specs and try to order things as they should be ordered
    
    Add empty state
    
    Add menu link
    
    Add specs to the empty state
    
    Refactor controller method into separate finder
    
    Add specs
    
    Move compliance dashboard feature to ultumate
    
    Add changelog entry
    
    Add specs and regenerate string file
    
    Fix lint problem
    
    Add cr remarks
    
    Add code review remarks
    
    Rename controller
    
    Fix presentable approvers number
    
    Change query in finder
    
    WIP on changes
    
    Refactor finder to use more efficient sql query
    
    Add first new files for dashboard MVC
    
    Remove debugger
    
    Add first draft of a view
    
    Add info to view
    
    Refactor preloading collections
    
    Add new scope to gather data properly
    
    Add basic spec
    
    Fix variable name
    
    Refactor compliance controller
    
    Add specs and try to order things as they should be ordered
    
    Add empty state
    
    Add menu link
    
    Add specs to the empty state
    
    Refactor controller method into separate finder
    
    Add specs
    
    Move compliance dashboard feature to ultumate
    
    Add changelog entry
    
    Add specs and regenerate string file
    
    Fix lint problem
    
    Add cr remarks
    
    Add code review remarks
    
    Rename controller
    
    Add first new files for dashboard MVC
    
    Remove debugger
    
    Add first draft of a view
    
    Add info to view
    
    Add new scope to gather data properly
    
    Add basic spec
    
    Fix variable name
    
    Refactor compliance controller
    
    Add specs and try to order things as they should be ordered
    
    Add empty state
    
    Add specs to the empty state
    
    Refactor controller method into separate finder
    
    Add specs
    
    Move compliance dashboard feature to ultumate
    
    Add changelog entry
    
    Fix lint problem
    
    Add cr remarks
    
    Add code review remarks
    
    Rename controller
    
    Change query in finder
    
    WIP on changes
    
    Refactor finder to use more efficient sql query
    
    Add first new files for dashboard MVC
    
    Add code review remarks
    
    Rename controller

--- ee/spec/views/groups/security/compliance_dashboards/show.html.haml_spec.rb
+++ ee/spec/views/groups/security/compliance_dashboards/show.html.haml_spec.rb
@@ -3,34 +3,95 @@
 require 'spec_helper'
 
 describe 'groups/security/compliance_dashboards/show.html.haml' do
-  set(:user) { create(:user) }
+  let(:user) { create(:user) }
   let(:group) { create(:group) }
+  let(:project) { create(:project, namespace: group) }
 
   before do
     group.add_owner(user)
-
-    assign(:group, group)
-    allow(view).to receive(:current_user) { user }
+    stub_licensed_features(group_level_compliance_dashboard: true)
   end
 
-  it 'shows empty state if there are no merge requests' do
-    render
+  context 'when there are no merge requests' do
+    it 'renders empty state' do
+      render
 
-    expect(rendered).to have_css("div.empty-state")
+      expect(rendered).to have_css('div', class: 'empty-state merge-requests')
+      expect(rendered).not_to have_css('div', class: 'compliance-dashboard')
+    end
   end
 
   context 'when there are merge requests' do
-    let(:mr) { create(:merge_request, :merged) }
+    let(:merge_request) { create(:merge_request, source_project: project, state: :merged) }
 
     before do
-      mr.metrics.update!(merged_at: 1.day.ago)
-      assign(:merge_requests, Kaminari.paginate_array([mr]).page(1))
+      merge_request.metrics.update!(merged_at: 10.minutes.ago)
+      assign(:merge_requests, Kaminari.paginate_array([merge_request]).page(0))
     end
 
-    it 'shows merge requests' do
+    it 'renders merge requests' do
       render
 
-      expect(rendered).to have_css(".merge-request-title.title")
+      expect(rendered).to have_link(merge_request.title)
+      expect(rendered).not_to have_css('div', class: 'empty-state merge-requests')
+    end
+
+    it 'renders merge requests with time merged tooltip' do
+      render
+
+      expect(rendered).to have_css('time', class: 'js-timeago')
+    end
+
+    context 'with no approvers' do
+      it 'renders the message "No approvers"' do
+        render
+
+        expect(rendered).to have_css("li span", text: 'No approvers')
+      end
+    end
+
+    context 'with a single approvers' do
+      let(:approver_1) { create(:user) }
+      let!(:approval_rule) { create :approval_merge_request_rule, merge_request: merge_request, users: [approver_1] }
+      let!(:approval) { create :approval, merge_request: merge_request, user: approver_1 }
+
+      before do
+        project.add_developer(approver_1)
+      end
+
+      it 'renders a single approver avatar link' do
+        render
+
+        expect(rendered).to have_css('a', class: 'author-link', count: 1)
+        expect(rendered).to have_link(approver_1.name)
+      end
+    end
+
+    context 'with more than two approvers' do
+      let(:approver_1) { create(:user) }
+      let(:approver_2) { create(:user) }
+      let(:approver_3) { create(:user) }
+      let!(:approval_1) { create :approval, merge_request: merge_request, user: approver_1 }
+      let!(:approval_2) { create :approval, merge_request: merge_request, user: approver_2 }
+      let!(:approval_3) { create :approval, merge_request: merge_request, user: approver_3 }
+
+      before do
+        project.add_developer(approver_1)
+        project.add_developer(approver_2)
+        project.add_developer(approver_3)
+      end
+
+      it 'renders the two latest approvers\'s avatar links' do
+        render
+
+        expect(rendered).to have_css('a', class: 'author-link', count: 2)
+      end
+
+      it 'renders a tooltip for additional approvers' do
+        render
+
+        expect(rendered).to have_css('span', class: 'avatar-counter', text: '+ 1')
+      end
     end
   end
 end
