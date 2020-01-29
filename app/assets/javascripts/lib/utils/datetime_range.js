11c5e561392a733ae435a338305ee305774370a2
Miguel Rincon <mrincon@gitlab.com>
2020-01-29 11:48:38 +0800

    Added fixes from reviews
    - Improve validations of time ranges
    - Use deconstructing for named parameters in functions
    - Improve time range examples in jsdoc
    - Switch to placing the functions in `const`

--- app/assets/javascripts/lib/utils/datetime_range.js
+++ app/assets/javascripts/lib/utils/datetime_range.js
@@ -1,118 +1,138 @@
+import dateformat from 'dateformat';
 import { secondsToMilliseconds } from './datetime_utility';
 
 const MINIMUM_DATE = new Date(0);
+
+const DEFAULT_DIRECTION = 'before';
 
 const durationToMillis = duration => {
   if (Object.entries(duration).length === 1 && Number.isFinite(duration.seconds)) {
     return secondsToMilliseconds(duration.seconds);
   }
   // eslint-disable-next-line @gitlab/i18n/no-non-i18n-strings
-  throw new Error('The only duration allowed is Number of `seconds`.');
+  throw new Error('Invalid duration: only `seconds` is supported');
 };
 
 const dateMinusDuration = (date, duration) => new Date(date.getTime() - durationToMillis(duration));
 
 const datePlusDuration = (date, duration) => new Date(date.getTime() + durationToMillis(duration));
 
-function handleRangeDirection({ direction = 'before', anchor, before, after }) {
+const isValidDuration = duration => Boolean(duration && Number.isFinite(duration.seconds));
+
+const isValidDateString = dateString => {
+  if (typeof dateString !== 'string' || !dateString.trim()) {
+    return false;
+  }
+
+  try {
+    // dateformat throws error that can be caught.
+    // This is better than using `new Date()`
+    dateformat(dateString, 'isoUtcDateTime');
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const handleRangeDirection = ({ direction = DEFAULT_DIRECTION, anchorDate, minDate, maxDate }) => {
   let startDate;
   let endDate;
 
-  if (direction === 'before') {
-    startDate = before;
-    endDate = anchor;
+  if (direction === DEFAULT_DIRECTION) {
+    startDate = minDate;
+    endDate = anchorDate;
   } else {
-    startDate = anchor;
-    endDate = after;
+    startDate = anchorDate;
+    endDate = maxDate;
   }
 
   return {
     startDate,
     endDate,
   };
-}
+};
 
 /**
  * Converts a fixed range to a fixed range
- * @param {Object} fixedRange - A range with fixed start and end (e.g. From January 1st 2020 to January 31st 2020)
+ * @param {Object} fixedRange - A range with fixed start and
+ * end (e.g. "midnight January 1st 2020 to midday January31st 2020")
  */
-function convertFixedToFixed(fixedRange) {
-  //
-  return {
-    startTime: new Date(fixedRange.startTime).toISOString(),
-    endTime: new Date(fixedRange.endTime).toISOString(),
-  };
-}
+const convertFixedToFixed = ({ start, end }) => ({
+  start,
+  end,
+});
 
 /**
  * Converts an anchored range to a fixed range
- * @param {Object} anchoredRange - A duration of time relative to an fixed point in time (2 minutes before January 1st, 1 day after)
+ * @param {Object} anchoredRange - A duration of time
+ * relative to a fixed point in time (e.g., "the 30 minutes
+ * before midnight January 1st 2020", or "the 2 days
+ * after midday on the 11th of May 2019")
  */
-function convertAnchoredToFixed(anchoredRange) {
-  const anchor = new Date(anchoredRange.anchor);
+const convertAnchoredToFixed = ({ anchor, duration, direction }) => {
+  const anchorDate = new Date(anchor);
 
   const { startDate, endDate } = handleRangeDirection({
-    before: dateMinusDuration(anchor, anchoredRange.duration),
-    after: datePlusDuration(anchor, anchoredRange.duration),
-    direction: anchoredRange.direction,
-    anchor,
+    minDate: dateMinusDuration(anchorDate, duration),
+    maxDate: datePlusDuration(anchorDate, duration),
+    direction,
+    anchorDate,
   });
 
   return {
-    startTime: startDate.toISOString(),
-    endTime: endDate.toISOString(),
+    start: startDate.toISOString(),
+    end: endDate.toISOString(),
   };
-}
+};
 
 /**
  * Converts a rolling change to a fixed range
- * @param {Object} rollingRange - a time range relative to now (Last 2 minutes, Next 2 days)
+ *
+ * @param {Object} rollingRange - A time range relative to
+ * now (e.g., "last 2 minutes", or "next 2 days")
  */
-function convertRollingToFixed(rollingRange) {
+const convertRollingToFixed = ({ duration, direction }) => {
+  // Use Date.now internally for easier mocking in tests
   const now = new Date(Date.now());
 
   return convertAnchoredToFixed({
-    duration: rollingRange.duration,
-    direction: rollingRange.direction,
+    duration,
+    direction,
     anchor: now.toISOString(),
   });
-}
+};
 
 /**
  * Converts an open range to a fixed range
- * @param {Object} openRange - a time range relative to an anchor (Before 1st of January, After 1st of January)
+ *
+ * @param {Object} openRange - A time range relative
+ * to an anchor (e.g., "before midnight on the 1st of
+ * January 2020", or "after midday on the 11th of May 2019")
  */
-function convertOpenToFixed(openRange) {
+const convertOpenToFixed = ({ anchor, direction }) => {
+  // Use Date.now internally for easier mocking in tests
   const now = new Date(Date.now());
-  const anchor = new Date(openRange.anchor);
 
   const { startDate, endDate } = handleRangeDirection({
-    before: MINIMUM_DATE,
-    after: now,
-    direction: openRange.direction,
-    anchor,
+    minDate: MINIMUM_DATE,
+    maxDate: now,
+    direction,
+    anchorDate: new Date(anchor),
   });
 
   return {
-    startTime: startDate.toISOString(),
-    endTime: endDate.toISOString(),
+    start: startDate.toISOString(),
+    end: endDate.toISOString(),
   };
-}
+};
 
-function handleInvalidRange(range) {
-  const hasStart = range.startTime;
-  const hasEnd = range.endTime;
-
-  /* eslint-disable @gitlab/i18n/no-non-i18n-strings */
-  const messages = {
-    [true]: 'The input range does not have the right format.',
-    [!hasStart && hasEnd]: 'The input fixed range does not have a start time.',
-    [hasStart && !hasEnd]: 'The input fixed range does not have an end time.',
-  };
-  /* eslint-enable @gitlab/i18n/no-non-i18n-strings */
-
-  throw new Error(messages.true);
-}
+/**
+ * Handles invalid date ranges
+ */
+const handleInvalidRange = () => {
+  // eslint-disable-next-line @gitlab/i18n/no-non-i18n-strings
+  throw new Error('The input range does not have the right format.');
+};
 
 const handlers = {
   invalid: handleInvalidRange,
@@ -122,38 +142,39 @@ const handlers = {
   open: convertOpenToFixed,
 };
 
+/**
+ * Validates and returns the type of range
+ *
+ * @param {Object} Date time range
+ * @returns {String} `key` value for one of the handlers
+ */
 export function getRangeType(range) {
-  const hasStart = range.startTime;
-  const hasEnd = range.endTime;
-  const hasAnchor = range.anchor;
-  const hasDuration = range.duration;
+  const { start, end, anchor, duration } = range;
 
-  const types = {
-    fixed: hasStart && hasEnd,
-    anchored: hasAnchor && hasDuration,
-    rolling: hasDuration && !hasAnchor,
-    open: hasAnchor && !hasDuration,
-  };
-
-  return (Object.entries(types).find(([, truthy]) => truthy) || ['invalid'])[0];
+  if ((start || end) && !anchor && !duration) {
+    return isValidDateString(start) && isValidDateString(end) ? 'fixed' : 'invalid';
+  }
+  if (anchor && duration) {
+    return isValidDateString(anchor) && isValidDuration(duration) ? 'anchored' : 'invalid';
+  }
+  if (duration && !anchor) {
+    return isValidDuration(duration) ? 'rolling' : 'invalid';
+  }
+  if (anchor && !duration) {
+    return isValidDateString(anchor) ? 'open' : 'invalid';
+  }
+  return 'invalid';
 }
 
 /**
  * convertToFixedRange Transforms a `range of time` into a `fixed range of time`.
  *
- * A a range of time can be understood as an arbitrary period
- * of time that can represent points in time relative to the
- * present moment:
+ * The following types of a `ranges of time` can be represented:
  *
- * The range of time can take different shapes according to
- * the point of time and type of time range it represents.
- *
- * The following types of ranges can be represented:
- *
- * Fixed Range: A range with fixed start and end (e.g. From January 1st 2020 to January 31st 2020)
- * Anchored Range: A duration of time relative to an fixed point in time (2 minutes before January 1st, 1 day after)
- * Rolling Range: A time range relative to now (Last 2 minutes, Next 2 days)
- * Open Range: A time range relative to an anchor (Before 1st of January, After 1st of January)
+ * Fixed Range: A range with fixed start and end (e.g. "midnight January 1st 2020 to midday January 31st 2020")
+ * Anchored Range: A duration of time relative to a fixed point in time (e.g., "the 30 minutes before midnight January 1st 2020", or "the 2 days after midday on the 11th of May 2019")
+ * Rolling Range: A time range relative to now (e.g., "last 2 minutes", or "next 2 days")
+ * Open Range: A time range relative to an anchor (e.g., "before midnight on the 1st of January 2020", or "after midday on the 11th of May 2019")
  *
  * @param {Object} dateTimeRange - A Time Range representation
  * It contains the data needed to create a fixed time range plus
@@ -166,7 +187,7 @@ export function getRangeType(range) {
  *   seconds: number;
  * }
  *
- * type Direction = 'before' | 'after'; // Direction of time
+ * type Direction = 'before' | 'after'; // Direction of time relative to an anchor
  *
  * type FixedRange = {
  *   start: ISO8601;
@@ -177,27 +198,26 @@ export function getRangeType(range) {
  * type AnchoredRange = {
  *   anchor: ISO8601;
  *   duration: Duration;
- *   direction: Direction;
+ *   direction: Direction; // defaults to 'before'
  *   label: string;
  * }
  *
  * type RollingRange = {
  *   duration: Duration;
- *   direction: Direction;
+ *   direction: Direction; // defaults to 'before'
  *   label: string;
  * }
  *
  * type OpenRange = {
  *   anchor: ISO8601;
- *   direction: Direction;
+ *   direction: Direction; // defaults to 'before'
  *   label: string;
  * }
  *
  * type DateTimeRange = FixedRange | AnchoredRange | RollingRange | OpenRange;
  *
  *
- * @returns An object with a fixed startTime and endTime that
- * corresponds to the input time.
+ * @returns {FixedRange} An object with a start and end in ISO8601 format.
  */
 export const convertToFixedRange = dateTimeRange =>
   handlers[getRangeType(dateTimeRange)](dateTimeRange);
--- spec/frontend/lib/utils/datetime_range_spec.js
+++ spec/frontend/lib/utils/datetime_range_spec.js
@@ -1,20 +1,32 @@
 import _ from 'lodash';
-import * as datetimeRange from '~/lib/utils/datetime_range';
+import { getRangeType, convertToFixedRange } from '~/lib/utils/datetime_range';
 
-const MOCK_NOW = Date.UTC(2020, 0, 23, 20); // 2020-01-23T20:00:00.000Z
-const rangeTypes = {
-  fixed: [{ startTime: 'exists', endTime: 'exists' }],
-  anchored: [{ anchor: 'exists', duration: 'exists' }],
-  rolling: [{ duration: 'exists' }],
-  open: [{ anchor: 'exists' }],
-  invalid: [{ startTime: 'exists' }, { endTime: 'exists' }, {}, { junk: 'exists' }],
-};
+const MOCK_NOW = Date.UTC(2020, 0, 23, 20);
+
+const MOCK_NOW_ISO_STRING = new Date(MOCK_NOW).toISOString();
 
 describe('Date time range utils', () => {
   describe('getRangeType', () => {
-    const { getRangeType } = datetimeRange;
+    it('infers correctly the range type from the input object', () => {
+      const rangeTypes = {
+        fixed: [{ start: MOCK_NOW_ISO_STRING, end: MOCK_NOW_ISO_STRING }],
+        anchored: [{ anchor: MOCK_NOW_ISO_STRING, duration: { seconds: 0 } }],
+        rolling: [{ duration: { seconds: 0 } }],
+        open: [{ anchor: MOCK_NOW_ISO_STRING }],
+        invalid: [
+          {},
+          { start: MOCK_NOW_ISO_STRING },
+          { end: MOCK_NOW_ISO_STRING },
+          { start: 'NOT_A_DATE', end: 'NOT_A_DATE' },
+          { duration: { seconds: 'NOT_A_NUMBER' } },
+          { duration: { seconds: Infinity } },
+          { duration: { minutes: 20 } },
+          { anchor: MOCK_NOW_ISO_STRING, duration: { seconds: 'NOT_A_NUMBER' } },
+          { anchor: MOCK_NOW_ISO_STRING, duration: { seconds: Infinity } },
+          { junk: 'exists' },
+        ],
+      };
 
-    it('it correctly infers the range type from the input object', () => {
       Object.entries(rangeTypes).forEach(([type, examples]) => {
         examples.forEach(example => expect(getRangeType(example)).toEqual(type));
       });
@@ -22,10 +34,8 @@ describe('Date time range utils', () => {
   });
 
   describe('convertToFixedRange', () => {
-    const { convertToFixedRange } = datetimeRange;
-
     beforeEach(() => {
-      Date.now = jest.spyOn(Date, 'now').mockImplementation(() => MOCK_NOW);
+      jest.spyOn(Date, 'now').mockImplementation(() => MOCK_NOW);
     });
 
     afterEach(() => {
@@ -34,37 +44,37 @@ describe('Date time range utils', () => {
 
     describe('When a fixed range is input', () => {
       const defaultFixedRange = {
-        startTime: '2020-01-01T00:00:00.000Z',
-        endTime: '2020-01-31T23:59:00.000Z',
+        start: '2020-01-01T00:00:00.000Z',
+        end: '2020-01-31T23:59:00.000Z',
         label: 'January 2020',
       };
 
       const mockFixedRange = params => ({ ...defaultFixedRange, ...params });
 
-      it('it converts a fixed range to an equal fixed range', () => {
+      it('converts a fixed range to an equal fixed range', () => {
         const aFixedRange = mockFixedRange();
 
         expect(convertToFixedRange(aFixedRange)).toEqual({
-          startTime: defaultFixedRange.startTime,
-          endTime: defaultFixedRange.endTime,
+          start: defaultFixedRange.start,
+          end: defaultFixedRange.end,
         });
       });
 
-      it('it throws an error when fixed range does not contain an end time', () => {
-        const aFixedRangeMissingEnd = _.omit(mockFixedRange(), 'endTime');
+      it('throws an error when fixed range does not contain an end time', () => {
+        const aFixedRangeMissingEnd = _.omit(mockFixedRange(), 'end');
 
         expect(() => convertToFixedRange(aFixedRangeMissingEnd)).toThrow();
       });
 
-      it('it throws an error when fixed range does not contain a start time', () => {
-        const aFixedRangeMissingStart = _.omit(mockFixedRange(), 'startTime');
+      it('throws an error when fixed range does not contain a start time', () => {
+        const aFixedRangeMissingStart = _.omit(mockFixedRange(), 'start');
 
         expect(() => convertToFixedRange(aFixedRangeMissingStart)).toThrow();
       });
 
-      it('it throws an error when the dates cannot be parsed', () => {
-        const wrongStart = mockFixedRange({ startTime: 'I_CANNOT_BE_PARSED' });
-        const wrongEnd = mockFixedRange({ endTime: 'I_CANNOT_BE_PARSED' });
+      it('throws an error when the dates cannot be parsed', () => {
+        const wrongStart = mockFixedRange({ start: 'I_CANNOT_BE_PARSED' });
+        const wrongEnd = mockFixedRange({ end: 'I_CANNOT_BE_PARSED' });
 
         expect(() => convertToFixedRange(wrongStart)).toThrow();
         expect(() => convertToFixedRange(wrongEnd)).toThrow();
@@ -82,42 +92,41 @@ describe('Date time range utils', () => {
       };
       const mockAnchoredRange = params => ({ ...defaultAnchoredRange, ...params });
 
-      it('it converts to a fixed range', () => {
+      it('converts to a fixed range', () => {
         const anAnchoredRange = mockAnchoredRange();
 
         expect(convertToFixedRange(anAnchoredRange)).toEqual({
-          startTime: '2020-01-01T00:00:00.000Z',
-          endTime: '2020-01-01T00:02:00.000Z',
+          start: '2020-01-01T00:00:00.000Z',
+          end: '2020-01-01T00:02:00.000Z',
         });
       });
 
-      it('it converts to a fixed range with a `before` direction', () => {
+      it('converts to a fixed range with a `before` direction', () => {
         const anAnchoredRange = mockAnchoredRange({ direction: 'before' });
 
         expect(convertToFixedRange(anAnchoredRange)).toEqual({
-          startTime: '2019-12-31T23:58:00.000Z',
-          endTime: '2020-01-01T00:00:00.000Z',
+          start: '2019-12-31T23:58:00.000Z',
+          end: '2020-01-01T00:00:00.000Z',
         });
       });
 
-      it('it converts to a fixed range without an explicit direction, defaulting to `before`', () => {
+      it('converts to a fixed range without an explicit direction, defaulting to `before`', () => {
         const anAnchoredRange = _.omit(mockAnchoredRange(), 'direction');
 
         expect(convertToFixedRange(anAnchoredRange)).toEqual({
-          startTime: '2019-12-31T23:58:00.000Z',
-          endTime: '2020-01-01T00:00:00.000Z',
+          start: '2019-12-31T23:58:00.000Z',
+          end: '2020-01-01T00:00:00.000Z',
         });
       });
 
-      it('it throws an error when the anchor cannot be parsed', () => {
+      it('throws an error when the anchor cannot be parsed', () => {
         const wrongAnchor = mockAnchoredRange({ anchor: 'I_CANNOT_BE_PARSED' });
-
         expect(() => convertToFixedRange(wrongAnchor)).toThrow();
       });
     });
 
     describe('when a rolling range is input', () => {
-      it('it converts to a fixed range', () => {
+      it('converts to a fixed range', () => {
         const aRollingRange = {
           direction: 'after',
           duration: {
@@ -127,12 +136,12 @@ describe('Date time range utils', () => {
         };
 
         expect(convertToFixedRange(aRollingRange)).toEqual({
-          startTime: '2020-01-23T20:00:00.000Z',
-          endTime: '2020-01-23T20:02:00.000Z',
+          start: '2020-01-23T20:00:00.000Z',
+          end: '2020-01-23T20:02:00.000Z',
         });
       });
 
-      it('it converts to a fixed range with an implicit `before` direction', () => {
+      it('converts to a fixed range with an implicit `before` direction', () => {
         const aRollingRangeWithNoDirection = {
           duration: {
             seconds: 60 * 2,
@@ -141,24 +150,24 @@ describe('Date time range utils', () => {
         };
 
         expect(convertToFixedRange(aRollingRangeWithNoDirection)).toEqual({
-          startTime: '2020-01-23T19:58:00.000Z',
-          endTime: '2020-01-23T20:00:00.000Z',
+          start: '2020-01-23T19:58:00.000Z',
+          end: '2020-01-23T20:00:00.000Z',
         });
       });
 
-      it('it throws an error when the duration is not the right format', () => {
+      it('throws an error when the duration is not in the right format', () => {
         const wrongDuration = {
           direction: 'before',
           duration: {
-            minutes: 2,
+            minutes: 20,
           },
-          label: 'Last 2 minutes',
+          label: 'Last 20 minutes',
         };
 
         expect(() => convertToFixedRange(wrongDuration)).toThrow();
       });
 
-      it('it throws an error when the duration is not ', () => {
+      it('throws an error when the anchor is not valid', () => {
         const wrongAnchor = {
           anchor: 'CAN_T_PARSE_THIS',
           direction: 'after',
@@ -170,7 +179,7 @@ describe('Date time range utils', () => {
     });
 
     describe('when an open range is input', () => {
-      it('it converts to a fixed range with an `after` direction', () => {
+      it('converts to a fixed range with an `after` direction', () => {
         const soFar2020 = {
           anchor: '2020-01-01T00:00:00.000Z',
           direction: 'after',
@@ -178,12 +187,12 @@ describe('Date time range utils', () => {
         };
 
         expect(convertToFixedRange(soFar2020)).toEqual({
-          startTime: '2020-01-01T00:00:00.000Z',
-          endTime: '2020-01-23T20:00:00.000Z',
+          start: '2020-01-01T00:00:00.000Z',
+          end: '2020-01-23T20:00:00.000Z',
         });
       });
 
-      it('it converts to a fixed range with the explicit `before` direction', () => {
+      it('converts to a fixed range with the explicit `before` direction', () => {
         const before2020 = {
           anchor: '2020-01-01T00:00:00.000Z',
           direction: 'before',
@@ -191,24 +200,24 @@ describe('Date time range utils', () => {
         };
 
         expect(convertToFixedRange(before2020)).toEqual({
-          startTime: '1970-01-01T00:00:00.000Z',
-          endTime: '2020-01-01T00:00:00.000Z',
+          start: '1970-01-01T00:00:00.000Z',
+          end: '2020-01-01T00:00:00.000Z',
         });
       });
 
-      it('it converts to a fixed range with the implicit `before` direction', () => {
+      it('converts to a fixed range with the implicit `before` direction', () => {
         const alsoBefore2020 = {
           anchor: '2020-01-01T00:00:00.000Z',
           label: 'Before 2020',
         };
 
         expect(convertToFixedRange(alsoBefore2020)).toEqual({
-          startTime: '1970-01-01T00:00:00.000Z',
-          endTime: '2020-01-01T00:00:00.000Z',
+          start: '1970-01-01T00:00:00.000Z',
+          end: '2020-01-01T00:00:00.000Z',
         });
       });
 
-      it('it throws an error when the anchor cannot be parsed', () => {
+      it('throws an error when the anchor cannot be parsed', () => {
         const wrongAnchor = {
           anchor: 'CAN_T_PARSE_THIS',
           direction: 'after',
