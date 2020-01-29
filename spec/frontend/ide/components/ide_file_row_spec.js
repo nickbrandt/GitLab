0a0231975888a9435ccc961e99e1d9d851faa398
Samantha Ming <sming@gitlab.com>
2020-01-28 19:01:29 -0800

    Add specs for components
    - Fill in slot for ide file row & remove unused prop from file_row
    - Add spec test to ide_file_row
    - Remove un-needed test from file_row

--- spec/frontend/ide/components/ide_file_row_spec.js
+++ spec/frontend/ide/components/ide_file_row_spec.js
@@ -1,13 +1,29 @@
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, mount } from '@vue/test-utils';
+import Vuex from 'vuex';
 import IdeFileRow from '~/ide/components/ide_file_row.vue';
 import FileRow from '~/vue_shared/components/file_row.vue';
+import FileRowExtra from '~/ide/components/file_row_extra.vue';
+import { createStore } from '~/ide/stores';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const defaultComponentProps = (type = 'tree') => ({
+  level: 4,
+  file: {
+    type,
+    name: 'js',
+  },
+});
 
 describe('Ide File Row component', () => {
   let wrapper;
 
   const createComponent = (props = {}) => {
-    wrapper = shallowMount(IdeFileRow, {
+    wrapper = mount(IdeFileRow, {
       propsData: { ...props },
+      store: createStore(),
+      localVue,
     });
   };
 
@@ -16,10 +32,58 @@ describe('Ide File Row component', () => {
   });
 
   it('renders file row component', () => {
-    createComponent({
-      level: 4,
-      file: {},
-    });
+    createComponent(defaultComponentProps());
     expect(wrapper.find(FileRow).exists()).toEqual(true);
+  });
+
+  it('hides dropdown when mouseleave', () => {
+    createComponent(defaultComponentProps());
+    wrapper.setMethods({
+      toggleDropdown: jest.fn(),
+    });
+
+    wrapper.trigger('mouseleave');
+
+    return wrapper.vm.$nextTick().then(() => {
+      expect(wrapper.vm.toggleDropdown).toHaveBeenCalled();
+      expect(wrapper.vm.toggleDropdown).toHaveBeenCalledWith(false);
+    });
+  });
+
+  describe('FileRowExtra component', () => {
+    it('hides dropdown on click', () => {
+      createComponent(defaultComponentProps());
+      wrapper.setMethods({
+        toggleDropdown: jest.fn(),
+      });
+
+      wrapper.find(FileRowExtra).vm.$emit('click');
+
+      return wrapper.vm.$nextTick().then(() => {
+        expect(wrapper.vm.toggleDropdown).toHaveBeenCalled();
+        expect(wrapper.vm.toggleDropdown).toHaveBeenCalledWith(false);
+      });
+    });
+
+    it('call dropdown on toggle', () => {
+      createComponent(defaultComponentProps());
+      wrapper.setMethods({
+        toggleDropdown: jest.fn(),
+      });
+      wrapper.find(FileRowExtra).vm.$emit('toggle');
+
+      return wrapper.vm.$nextTick().then(() => {
+        expect(wrapper.vm.toggleDropdown).toHaveBeenCalled();
+      });
+    });
+
+    it.each`
+      type       | value    | desc
+      ${'tree'}  | ${true}  | ${'is shown if file type is tree'}
+      ${'hello'} | ${false} | ${'is hidden if file is not tree'}
+    `('$desc', ({ type, value }) => {
+      createComponent(defaultComponentProps(type));
+      expect(wrapper.find(FileRowExtra).exists()).toEqual(value);
+    });
   });
 });
