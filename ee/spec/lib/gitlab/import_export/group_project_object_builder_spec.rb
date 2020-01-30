f6a568673583ca4e7f118889836357e11e4b24e2
Imre Farkas <ifarkas@gitlab.com>
2020-01-30 17:30:23 +0200

    Search group-level objects among all ancestors during import
    Otherwise objects are created during import that already exists in one
    of the ancestors (not direct parent).

---
+++ ee/spec/lib/gitlab/import_export/group_project_object_builder_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Gitlab::ImportExport::GroupProjectObjectBuilder do
+  let!(:group) { create(:group, :private) }
+  let!(:subgroup) { create(:group, :private, parent: group) }
+  let!(:project) do
+    create(:project, :repository,
+           :builds_disabled,
+           :issues_disabled,
+           name: 'project',
+           path: 'project',
+           group: subgroup)
+  end
+
+  context 'epics' do
+    it 'finds the existing epic' do
+      epic = create(:epic, title: 'epic', group: project.group)
+
+      expect(described_class.build(Epic,
+                                   'iid' => 1,
+                                   'title' => 'epic',
+                                   'group' => project.group,
+                                   'author_id' => project.creator.id)).to eq(epic)
+    end
+
+    it 'finds the existing epic in root ancestor' do
+      epic = create(:epic, title: 'epic', group: group)
+
+      expect(described_class.build(Epic,
+                                   'iid' => 1,
+                                   'title' => 'epic',
+                                   'group' => project.group,
+                                   'author_id' => project.creator.id)).to eq(epic)
+    end
+
+    it 'creates a new epic' do
+      epic = described_class.build(Epic,
+                                   'iid' => 1,
+                                   'title' => 'epic',
+                                   'group' => project.group,
+                                   'author_id' => project.creator.id)
+
+      expect(epic.persisted?).to be true
+    end
+  end
+end
