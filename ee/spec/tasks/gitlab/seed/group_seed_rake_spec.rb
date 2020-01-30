385a6fbbe0d33c93965a37a0118ac59772fcc433
George Koltsov <gkoltsov@gitlab.com>
2020-01-30 10:08:48 +0000

    Revert of revert "Add Group Seeder rake task"
    This reverts merge request https://gitlab.com/gitlab-org/gitlab/-/merge_requests/23852

---
+++ ee/spec/tasks/gitlab/seed/group_seed_rake_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rake_helper'
+
+describe 'gitlab:seed:group_seed rake task', :sidekiq do
+  let(:username) { 'group_seed' }
+  let!(:user) { create(:user, username: username) }
+  let(:task_params) { [2, username] }
+
+  before do
+    Rake.application.rake_require('tasks/gitlab/seed/group_seed')
+  end
+
+  subject { run_rake_task('gitlab:seed:group_seed', task_params) }
+
+  it 'performs group seed successfully' do
+    expect { subject }.not_to raise_error
+
+    group = user.groups.first
+
+    expect(group.epics.count).to be 2
+  end
+end
