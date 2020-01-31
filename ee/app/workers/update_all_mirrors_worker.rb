01b394f01de4607249f8dcd74bacf6c026ff2c64
Bob Van Landuyt <bob@gitlab.com>
2020-01-31 07:51:31 +0000

    Allow scheduling jobs in batch with contexts
    This adds helper methods for the `bulk_perform_in` and
    `bulk_perform_async` methods that allow specifying a context per job.
    
    Scheduling jobs with contexts that need to be different based on the
    arguments passed can be done by passing in an array of objects, and
    defining how to build arguments and contexts for those objects.
    
    For example:
          ProjectImportScheduleWorker.bulk_perform_async_with_contexts(
            projects,
            arguments_proc: -> (project) { project.id },
            context_proc: -> (project) { { project: project } }
          )
    
    This will schedule `ProjectImportScheduleWorker` jobs called with the
    project.id. And will create contexts using the project. The developers
    need to make sure that the required resources for building a context
    are loaded: The route and the namespace of a project.
    
    The batch context will be stored on the worker class when scheduling
    the job, so the sidekiq middleware can fetch the right context based
    on those arguments and apply them to the job when scheduling it.
    
    This also implements this method for the `UpdateAllMirrorsWorker`
    which schedules `ProjectImportScheduleWorker` in batch. When adding
    this, the `ProjectImportScheduleWorker`-jobs will have a context.

--- ee/app/workers/update_all_mirrors_worker.rb
+++ ee/app/workers/update_all_mirrors_worker.rb
@@ -47,11 +47,12 @@ class UpdateAllMirrorsWorker
       projects = pull_mirrors_batch(freeze_at: now, batch_size: batch_size, offset_at: last).to_a
       break if projects.empty?
 
-      project_ids = projects.lazy.select(&:mirror?).take(capacity).map(&:id).force
-      capacity -= project_ids.length
+      projects_to_schedule = projects.lazy.select(&:mirror?).take(capacity).force
+      capacity -= projects_to_schedule.size
 
-      ProjectImportScheduleWorker.bulk_perform_async(project_ids.map { |id| [id] })
-      scheduled += project_ids.length
+      schedule_projects_in_batch(projects_to_schedule)
+
+      scheduled += projects_to_schedule.length
 
       # If fewer than `batch_size` projects were returned, we don't need to query again
       break if projects.length < batch_size
@@ -95,6 +96,7 @@ class UpdateAllMirrorsWorker
       .mirrors_to_sync(freeze_at)
       .reorder('import_state.next_execution_timestamp')
       .limit(batch_size)
+      .with_route
       .with_namespace # Used by `project.mirror?`
 
     relation = relation.where('import_state.next_execution_timestamp > ?', offset_at) if offset_at
@@ -102,4 +104,12 @@ class UpdateAllMirrorsWorker
     relation
   end
   # rubocop: enable CodeReuse/ActiveRecord
+
+  def schedule_projects_in_batch(projects)
+    ProjectImportScheduleWorker.bulk_perform_async_with_contexts(
+      projects,
+      arguments_proc: -> (project) { project.id },
+      context_proc: -> (project) { { project: project } }
+    )
+  end
 end
