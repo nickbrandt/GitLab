c7f7a8e13105801ae169a77eeb77f2f974e441e2
Nick Kipling <nkipling@gitlab.com>
2020-01-31 12:13:06 +0000

    Added install instructions for Nuget
    Created Nuget installation component
    Updated main Vue app
    Passed in required props
    Created Nuget path helper
    Added tests

--- ee/app/helpers/ee/packages_helper.rb
+++ ee/app/helpers/ee/packages_helper.rb
@@ -18,6 +18,11 @@ module EE
       ::Gitlab::Utils.append_path(::Gitlab.config.gitlab.url, "api/#{::API::API.version}/packages/conan")
     end
 
+    def nuget_package_registry_url(project_id)
+      project_registry = expose_path(api_v4_projects_packages_nuget_index_path(id: project_id, format: ".json"))
+      ::Gitlab::Utils.append_path(::Gitlab.config.gitlab.url, project_registry)
+    end
+
     def package_registry_project_url(project_id, registry_type = :maven)
       project_api_path = expose_path(api_v4_projects_path(id: project_id))
       package_registry_project_path = "#{project_api_path}/packages/#{registry_type}"
