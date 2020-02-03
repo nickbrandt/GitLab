5a45f0edb793ab822248631d1eec5d2dc2c6e965
Mark Florian <553096-markrian@users.noreply.gitlab.com>
2020-02-03 14:08:04 +0000

    Remove unused utility/support code
    This removes various utility/support code that was needed during the
    existence of the `parsed_license_report` feature flag.
    
    Part of [Remove parsed_license_report flag from front end code][1].
    
    [1]: https://gitlab.com/gitlab-org/gitlab/issues/37271

--- ee/app/assets/javascripts/vue_shared/license_management/store/utils.js
+++ ee/app/assets/javascripts/vue_shared/license_management/store/utils.js
@@ -1,9 +1,7 @@
 import { LICENSE_APPROVAL_STATUS } from 'ee/vue_shared/license_management/constants';
-import ReportMapper from 'ee/vue_shared/license_management/report_mapper';
 import { n__, sprintf } from '~/locale';
 import { STATUS_FAILED, STATUS_NEUTRAL, STATUS_SUCCESS } from '~/reports/constants';
 
-const toLowerCase = name => name.toLowerCase();
 /**
  *
  * Converts the snake case in license objects to camel case
@@ -20,28 +18,6 @@ export const normalizeLicense = license => {
   };
 };
 
-/**
- *
- * Comparator function for sorting licenses by name
- *
- * @param a {Object} License Object a
- * @param b {Object} License Object b
- * @returns {number}
- *
- * @example
- *
- * arrayOfLicenses.sort(byLicenseNameComparator)
- *
- */
-export const byLicenseNameComparator = (a, b) => {
-  const x = toLowerCase(a.name || '');
-  const y = toLowerCase(b.name || '');
-  if (x === y) {
-    return 0;
-  }
-  return x > y ? 1 : -1;
-};
-
 export const getIssueStatusFromLicenseStatus = approvalStatus => {
   if (approvalStatus === LICENSE_APPROVAL_STATUS.APPROVED) {
     return STATUS_SUCCESS;
@@ -49,81 +25,6 @@ export const getIssueStatusFromLicenseStatus = approvalStatus => {
     return STATUS_FAILED;
   }
   return STATUS_NEUTRAL;
-};
-
-const caseInsensitiveMatch = (name, otherName) => toLowerCase(name) === toLowerCase(otherName);
-const getLicenseStatusByName = (managedLicenses = [], licenseName) =>
-  managedLicenses.find(license => caseInsensitiveMatch(license.name, licenseName)) || {};
-
-const getDependenciesByLicenseName = (dependencies = [], licenseName) =>
-  dependencies.filter(dependencyItem => {
-    const licenses = dependencyItem.licenses || [dependencyItem.license];
-    return licenses.find(license => caseInsensitiveMatch(license.name, licenseName));
-  });
-
-/**
- *
- * Prepares a license report of the format:
- *
- * [
- *  {
- *   name: 'MIT',
- *   count: 1,
- *   url: 'https://spdx.org/MIT',
- *   packages: [{name: 'vue'}],
- *   approvalStatus: 'approved',
- *   id: 4,
- *  }
- * ]
- *
- * @param headMetrics {Object}
- *   License scanning report on head. Contains all found licenses and dependencies.
- * @param baseMetrics {Object}
- *   License scanning report on base. Contains all found licenses and dependencies.
- * @param managedLicenses {Array} List of licenses currently managed. (Approval Status)
- * @returns {Array}
- */
-export const parseLicenseReportMetrics = (headMetrics, baseMetrics, managedLicenses) => {
-  if (!headMetrics && !baseMetrics) {
-    return [];
-  }
-  const reportMapper = new ReportMapper();
-  const headReport = reportMapper.mapFrom(headMetrics);
-  const baseReport = reportMapper.mapFrom(baseMetrics);
-
-  const headLicenses = headReport.licenses || [];
-  const headDependencies = headReport.dependencies || [];
-  const baseLicenses = baseReport.licenses || [];
-  const managedLicenseList = managedLicenses || [];
-
-  if (!headLicenses.length && !headDependencies.length) return [];
-
-  const knownLicenses = baseLicenses.map(license => toLowerCase(license.name));
-  const identityMap = license => knownLicenses.includes(toLowerCase(license.name));
-  const mapper = license => {
-    const { name, count } = license;
-    const { id, approvalStatus } = getLicenseStatusByName(managedLicenseList, name);
-    const dependencies = getDependenciesByLicenseName(headDependencies, name);
-    const url =
-      license.url ||
-      (dependencies && dependencies[0] && dependencies[0].license && dependencies[0].license.url) ||
-      '';
-
-    return {
-      name,
-      count,
-      url,
-      packages: dependencies.map(dependencyItem => dependencyItem.dependency),
-      status: getIssueStatusFromLicenseStatus(approvalStatus),
-      approvalStatus,
-      id,
-    };
-  };
-
-  return headLicenses
-    .filter(license => !identityMap(license))
-    .map(mapper)
-    .sort(byLicenseNameComparator);
 };
 
 export const getPackagesString = (packages, truncate, maxPackages) => {
