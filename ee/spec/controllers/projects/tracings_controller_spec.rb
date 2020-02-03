fdabcf350603cd011bf8494ee8016de7a55f0427
Rémy Coutable <remy@rymai.me>
2020-02-03 17:40:19 +0100

    Fix a flaky 'Projects::TracingsController' test
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- ee/spec/controllers/projects/tracings_controller_spec.rb
+++ ee/spec/controllers/projects/tracings_controller_spec.rb
@@ -9,30 +9,36 @@ describe Projects::TracingsController do
     shared_examples 'user with read access' do |visibility_level|
       let(:project) { create(:project, visibility_level) }
 
-      before do
-        project.add_maintainer(user)
-      end
+      %w[developer maintainer].each do |role|
+        context "with a #{visibility_level} project and #{role} role" do
+          before do
+            project.add_role(user, role)
+          end
 
-      it 'renders OK' do
-        get :show, params: { namespace_id: project.namespace, project_id: project }
+          it 'renders OK' do
+            get :show, params: { namespace_id: project.namespace, project_id: project }
 
-        expect(response).to have_gitlab_http_status(:ok)
-        expect(response).to render_template(:show)
+            expect(response).to have_gitlab_http_status(:ok)
+            expect(response).to render_template(:show)
+          end
+        end
       end
     end
 
     shared_examples 'user without read access' do |visibility_level|
       let(:project) { create(:project, visibility_level) }
 
-      %w[guest reporter developer].each do |role|
-        before do
-          project.public_send("add_#{role}", user)
-        end
+      %w[guest reporter].each do |role|
+        context "with a #{visibility_level} project and #{role} role" do
+          before do
+            project.add_role(user, role)
+          end
 
-        it 'returns 404' do
-          get :show, params: { namespace_id: project.namespace, project_id: project }
+          it 'returns 404' do
+            get :show, params: { namespace_id: project.namespace, project_id: project }
 
-          expect(response).to have_gitlab_http_status(:not_found)
+            expect(response).to have_gitlab_http_status(:not_found)
+          end
         end
       end
     end
