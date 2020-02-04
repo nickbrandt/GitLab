7da009c3c0d70281c7b68e9da637e167ab03ca8c
Thomas Randolph <trandolph@gitlab.com>
2020-02-04 00:03:02 -0700

    Make sure the endpoints are always requesting the right view style
    Also: pass the current state in when computing the tree data

--- app/assets/javascripts/diffs/store/actions.js
+++ app/assets/javascripts/diffs/store/actions.js
@@ -111,19 +111,28 @@ export const fetchDiffFilesBatch = ({ commit, state }) => {
   commit(types.SET_BATCH_LOADING, true);
   commit(types.SET_RETRIEVING_BATCHES, true);
 
-  const getBatch = page =>
-    axios
+  const getBatch = page => {
+    if (page) {
+      urlParams.page = page;
+    }
+
+    return axios
       .get(state.endpointBatch, {
-        params: { ...urlParams, page },
+        params: urlParams,
       })
       .then(({ data: { pagination, diff_files } }) => {
         commit(types.SET_DIFF_DATA_BATCH, { diff_files });
         commit(types.SET_BATCH_LOADING, false);
-        if (!pagination.next_page) commit(types.SET_RETRIEVING_BATCHES, false);
+
+        if (!pagination.next_page) {
+          commit(types.SET_RETRIEVING_BATCHES, false);
+        }
+
         return pagination.next_page;
       })
       .then(nextPage => nextPage && getBatch(nextPage))
       .catch(() => commit(types.SET_RETRIEVING_BATCHES, false));
+  };
 
   return getBatch()
     .then(handleLocationHash)
@@ -132,6 +141,11 @@ export const fetchDiffFilesBatch = ({ commit, state }) => {
 
 export const fetchDiffFilesMeta = ({ commit, state }) => {
   const worker = new TreeWorker();
+  const urlParams = {};
+
+  if (state.useSingleDiffStyle) {
+    urlParams.view = state.diffViewType;
+  }
 
   commit(types.SET_LOADING, true);
 
@@ -142,16 +156,17 @@ export const fetchDiffFilesMeta = ({ commit, state }) => {
   });
 
   return axios
-    .get(state.endpointMetadata)
+    .get(mergeUrlParams(urlParams, state.endpointMetadata))
     .then(({ data }) => {
       const strippedData = { ...data };
+
       delete strippedData.diff_files;
       commit(types.SET_LOADING, false);
       commit(types.SET_MERGE_REQUEST_DIFFS, data.merge_request_diffs || []);
       commit(types.SET_DIFF_DATA, strippedData);
 
-      prepareDiffData({ diff: data });
-      worker.postMessage(data.diff_files);
+      worker.postMessage(prepareDiffData({ diff: data, priorFiles: state.diffFiles }));
+
       return data;
     })
     .catch(() => worker.terminate());
