f68ed6d5925b8c0587b4b6a29c5b0d954435f949
Alexandru Croitor <acroitor@gitlab.com>
2020-02-04 15:20:17 +0200

    Filter out notes with noteable_id pointing to inexistent records
    Notes table uses polymorphism to relate notes to different models:
    issues, merge requests, epics, designs, snippets, etc. Because of
    polymorphism we cannot have a DB level FK, which inevitably leads to
    some data inconsistency at DB level, which should be accounted for
    in code and migrations.

--- ee/spec/lib/gitlab/background_migration/user_mentions/create_resource_user_mention_spec.rb
+++ ee/spec/lib/gitlab/background_migration/user_mentions/create_resource_user_mention_spec.rb
@@ -9,9 +9,8 @@ describe Gitlab::BackgroundMigration::UserMentions::CreateResourceUserMention do
 
   let(:users) { table(:users) }
   let(:namespaces) { table(:namespaces) }
-  let(:epics) { table(:epics) }
+  let(:projects) { table(:projects) }
   let(:notes) { table(:notes) }
-  let(:epic_user_mentions) { table(:epic_user_mentions) }
 
   let(:author) { users.create!(email: 'author@example.com', notification_email: 'author@example.com', name: 'author', username: 'author', projects_limit: 10, state: 'active') }
   let(:member) { users.create!(email: 'member@example.com', notification_email: 'member@example.com', name: 'member', username: 'member', projects_limit: 10, state: 'active') }
@@ -20,14 +19,15 @@ describe Gitlab::BackgroundMigration::UserMentions::CreateResourceUserMention do
   let(:skipped) { users.create!(email: 'skipped@example.com', notification_email: 'skipped@example.com', name: 'skipped', username: 'skipped', projects_limit: 10, state: 'active') }
 
   let(:mentioned_users) { [author, member, admin, john_doe, skipped] }
-  let(:user_mentions) { mentioned_users.map { |u| "@#{u.username}" }.join(' ') }
+  let(:mentioned_users_refs) { mentioned_users.map { |u| "@#{u.username}" }.join(' ') }
 
   let(:group) { namespaces.create!(name: 'test1', path: 'test1', runners_token: 'my-token1', project_creation_level: 1, visibility_level: 20, type: 'Group') }
   let(:inaccessible_group) { namespaces.create!(name: 'test2', path: 'test2', runners_token: 'my-token2', project_creation_level: 1, visibility_level: 0, type: 'Group') }
+  let(:project) { projects.create!(id: 1, name: 'gitlab1', path: 'gitlab1', namespace_id: group.id, visibility_level: 0) }
 
   let(:mentioned_groups) { [group, inaccessible_group] }
   let(:group_mentions) { [group, inaccessible_group].map { |gr| "@#{gr.path}" }.join(' ') }
-  let(:description_mentions) { "description with mentions #{user_mentions} and #{group_mentions}" }
+  let(:description_mentions) { "description with mentions #{mentioned_users_refs} and #{group_mentions}" }
 
   before do
     # build personal namespaces and routes for users
@@ -41,62 +41,36 @@ describe Gitlab::BackgroundMigration::UserMentions::CreateResourceUserMention do
     end
   end
 
-  context 'mentions in epic description' do
-    let(:epic) do
-      epics.create!(iid: 1, group_id: group.id, author_id: author.id, title: "epic title @#{author.username}",
-                    title_html: "epic title  @#{author.username}", description: description_mentions)
-    end
+  describe 'epic mentions' do
+    let(:epics) { table(:epics) }
+    let(:epic_user_mentions) { table(:epic_user_mentions) }
 
-    it 'has correct no_quote_columns' do
-      expect(Gitlab::BackgroundMigration::UserMentions::Models::Epic.no_quote_columns).to match([:note_id, :epic_id])
-    end
-
-    it 'migrates mentions' do
-      join = MigrateEpicMentionsToDb::JOIN
-      conditions = MigrateEpicMentionsToDb::QUERY_CONDITIONS
-
-      expect do
-        subject.perform('Epic', join, conditions, false, epic.id, epic.id)
-      end.to change { epic_user_mentions.count }.by(1)
-
-      epic_user_mention = epic_user_mentions.last
-      expect(epic_user_mention.mentioned_users_ids.sort).to eq(mentioned_users.pluck(:id).sort)
-      expect(epic_user_mention.mentioned_groups_ids.sort).to eq([group.id])
-      expect(epic_user_mention.mentioned_groups_ids.sort).not_to include(inaccessible_group.id)
-    end
-
-    context 'mentions in epic notes' do
-      let(:epic_note) { notes.create!(noteable_id: epic.id, noteable_type: 'Epic', author_id: author.id, note: description_mentions) }
-      let(:epic_note2) { notes.create!(noteable_id: epic.id, noteable_type: 'Epic', author_id: author.id, note: 'sample note') }
-      let(:system_epic_note) { notes.create!(noteable_id: epic.id, noteable_type: 'Epic', author_id: author.id, note: description_mentions, system: true) }
-
-      before do
-        epic_note.becomes(Note).save!
-        epic_note2.becomes(Note).save!
-        system_epic_note.becomes(Note).save!
+    context 'mentions in epic description' do
+      let!(:epic) do
+        epics.create!(iid: 1, group_id: group.id, author_id: author.id, title: "epic title @#{author.username}",
+                      title_html: "epic title  @#{author.username}", description: description_mentions)
+      end
+      let!(:epic_without_mentions) do
+        epics.create!(iid: 2, group_id: group.id, author_id: author.id, title: "epic title}",
+                      title_html: "epic title", description: 'simple description')
       end
 
-      it 'migrates mentions from note' do
-        join = MigrateEpicNotesMentionsToDb::JOIN
-        conditions = MigrateEpicNotesMentionsToDb::QUERY_CONDITIONS
+      let(:user_mentions) { epic_user_mentions }
+      let(:resource) { epic }
 
-        expect do
-          subject.perform('Epic', join, conditions, true, Note.minimum(:id), Note.maximum(:id))
-        end.to change { epic_user_mentions.where(note_id: epic_note.id).count }.by(1)
+      it_behaves_like 'resource mentions migration', MigrateEpicMentionsToDb, Epic
 
-        # check that the epic_user_mention for regular note is created
-        epic_user_mention = epic_user_mentions.first
-        expect(epic_user_mention.becomes(EpicUserMention).note.system).to be false
-        expect(epic_user_mention.mentioned_users_ids.sort).to eq(users.pluck(:id).sort)
-        expect(epic_user_mention.mentioned_groups_ids.sort).to eq([group.id])
-        expect(epic_user_mention.mentioned_groups_ids.sort).not_to include(inaccessible_group.id)
+      it 'has correct no_quote_columns' do
+        expect(Gitlab::BackgroundMigration::UserMentions::Models::Epic.no_quote_columns).to match([:note_id, :epic_id])
+      end
 
-        # check that the epic_user_mention for system note is created
-        epic_user_mention = epic_user_mentions.second
-        expect(epic_user_mention.becomes(EpicUserMention).note.system).to be true
-        expect(epic_user_mention.mentioned_users_ids.sort).to eq(users.pluck(:id).sort)
-        expect(epic_user_mention.mentioned_groups_ids.sort).to eq([group.id])
-        expect(epic_user_mention.mentioned_groups_ids.sort).not_to include(inaccessible_group.id)
+      context 'mentions in epic notes' do
+        let(:note1) { notes.create!(noteable_id: epic.id, noteable_type: 'Epic', author_id: author.id, note: description_mentions) }
+        let(:note2) { notes.create!(noteable_id: epic.id, noteable_type: 'Epic', author_id: author.id, note: 'sample note') }
+        let(:note3) { notes.create!(noteable_id: epic.id, noteable_type: 'Epic', author_id: author.id, note: description_mentions, system: true) }
+        let!(:note4) { notes.create!(noteable_id: epics.maximum(:id) + 10, noteable_type: 'Epic', author_id: author.id, note: description_mentions, project_id: project.id) }
+
+        it_behaves_like 'resource notes mentions migration', MigrateEpicNotesMentionsToDb, Epic
       end
     end
   end
--- ee/spec/migrations/migrate_epic_mentions_to_db_spec.rb
+++ ee/spec/migrations/migrate_epic_mentions_to_db_spec.rb
@@ -10,28 +10,13 @@ describe MigrateEpicMentionsToDb, :migration do
 
   let(:user) { users.create!(name: 'root', email: 'root@example.com', username: 'root', projects_limit: 0) }
   let(:group) { namespaces.create!(name: 'group1', path: 'group1', owner_id: user.id, type: 'Group') }
-  let!(:epic1) { epics.create!(iid: 1, title: "title1", title_html: 'title1', description: 'epic description with @root mention', group_id: group.id, author_id: user.id) }
-  let!(:epic2) { epics.create!(iid: 2, title: "title2", title_html: 'title2', description: 'epic description with @root mention', group_id: group.id, author_id: user.id) }
-  let!(:epic3) { epics.create!(iid: 3, title: "title3", title_html: 'title3', description: 'epic description with @root mention', group_id: group.id, author_id: user.id) }
+  let!(:resource1) { epics.create!(iid: 1, title: "title1", title_html: 'title1', description: 'epic description with @root mention', group_id: group.id, author_id: user.id) }
+  let!(:resource2) { epics.create!(iid: 2, title: "title2", title_html: 'title2', description: 'epic description with @root mention', group_id: group.id, author_id: user.id) }
+  let!(:resource3) { epics.create!(iid: 3, title: "title3", title_html: 'title3', description: 'epic description with @root mention', group_id: group.id, author_id: user.id) }
 
   before do
     stub_const("#{described_class.name}::BATCH_SIZE", 1)
   end
 
-  it 'schedules epic mentions migrations' do
-    Sidekiq::Testing.fake! do
-      Timecop.freeze do
-        migrate!
-
-        migration = described_class::MIGRATION
-        join = described_class::JOIN
-        conditions = described_class::QUERY_CONDITIONS
-
-        expect(migration).to be_scheduled_delayed_migration(2.minutes, 'Epic', join, conditions, false, epic1.id, epic1.id)
-        expect(migration).to be_scheduled_delayed_migration(4.minutes, 'Epic', join, conditions, false, epic2.id, epic2.id)
-        expect(migration).to be_scheduled_delayed_migration(6.minutes, 'Epic', join, conditions, false, epic3.id, epic3.id)
-        expect(BackgroundMigrationWorker.jobs.size).to eq 3
-      end
-    end
-  end
+  it_behaves_like 'schedules resource mentions migration', Epic, false
 end
