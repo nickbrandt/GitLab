5175b5ef9b2d758156740061ac8fabe4dac83fff
jakeburden <jburden@gitlab.com>
2020-02-04 07:54:02 -0500

    Remove ensure_single_line co-location

--- app/assets/javascripts/locale/es6/ensure_single_line.js
+++
@@ -1,5 +0,0 @@
-// co-locating ensure_single_line to satisfy static analysis while allowing Node.js to import the function with CommonJS
-
-import ensureSingleLine from '../ensure_single_line';
-
-export default ensureSingleLine;
