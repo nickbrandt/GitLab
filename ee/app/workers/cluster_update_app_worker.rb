5d9b8b233a3e4ee5f4e54b708d03e375cb445259
Reuben Pereira <rpereira@gitlab.com>
2020-02-04 20:55:11 +0000

    Add exclusive guard to ClusterUpdateAppWorker
    Due to https://gitlab.com/gitlab-org/gitlab/issues/196243, the
    ClusterUpdateAppWorker should never be run multiple times concurrently
    for the same application.

--- ee/app/workers/cluster_update_app_worker.rb
+++ ee/app/workers/cluster_update_app_worker.rb
@@ -6,11 +6,24 @@ class ClusterUpdateAppWorker
   include ApplicationWorker
   include ClusterQueue
   include ClusterApplications
+  include ExclusiveLeaseGuard
 
   sidekiq_options retry: 3, dead: false
 
-  # rubocop: disable CodeReuse/ActiveRecord
+  LEASE_TIMEOUT = 10.minutes.to_i
+
   def perform(app_name, app_id, project_id, scheduled_time)
+    @app_id = app_id
+
+    try_obtain_lease do
+      execute(app_name, app_id, project_id, scheduled_time)
+    end
+  end
+
+  private
+
+  # rubocop: disable CodeReuse/ActiveRecord
+  def execute(app_name, app_id, project_id, scheduled_time)
     project = Project.find_by(id: project_id)
     return unless project
 
@@ -22,4 +35,12 @@ class ClusterUpdateAppWorker
     end
   end
   # rubocop: enable CodeReuse/ActiveRecord
+
+  def lease_key
+    @lease_key ||= "#{self.class.name.underscore}-#{@app_id}"
+  end
+
+  def lease_timeout
+    LEASE_TIMEOUT
+  end
 end
