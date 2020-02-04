adf82f8503e41344b288ba17a561fafe1bda0157
Tetiana Chupryna <tchupryna@gitlab.com>
2020-02-04 20:07:27 +0200

    Add tests for `expose_security_dashboard?`

--- ee/spec/presenters/ci/pipeline_presenter_spec.rb
+++ ee/spec/presenters/ci/pipeline_presenter_spec.rb
@@ -4,13 +4,11 @@ require 'spec_helper'
 
 describe Ci::PipelinePresenter do
   let_it_be(:project, reload: true) { create(:project) }
-  let_it_be(:pipeline, reload: true) { create(:ci_pipeline, project: project) }
+  let_it_be(:pipeline, reload: true) { create(:ee_ci_pipeline, project: project) }
 
-  subject(:presenter) do
-    described_class.new(pipeline)
-  end
+  let(:presenter) { described_class.new(pipeline) }
 
-  describe '.failure_reason' do
+  describe '#failure_reason' do
     context 'when pipeline has failure reason' do
       it 'represents a failure reason sentence' do
         pipeline.failure_reason = :activity_limit_exceeded
@@ -24,6 +22,28 @@ describe Ci::PipelinePresenter do
       it 'returns nil' do
         expect(presenter.failure_reason).to be_nil
       end
+    end
+  end
+
+  describe '#expose_security_dashboard?' do
+    subject { pipeline.expose_security_dashboard? }
+
+    context 'when there is an artifact of a right type' do
+      let!(:build) { create(:ee_ci_build, :dependency_scanning, pipeline: pipeline) }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when there is an artifact of a wrong type' do
+      let!(:build) { create(:ee_ci_build, :license_scanning, pipeline: pipeline) }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when there is no found artifact' do
+      let!(:build) { create(:ee_ci_build, pipeline: pipeline) }
+
+      it { is_expected.to be_falsey }
     end
   end
 end
