a27940e992d21e45297dfd8509d8193ee7dd21e8
Andreas Brandl <abrandl@gitlab.com>
2020-02-05 13:34:33 +0100

    Add spec for scheduling migration

--- db/post_migrate/20200127111840_fix_projects_without_project_feature.rb
+++ db/post_migrate/20200127111840_fix_projects_without_project_feature.rb
@@ -4,6 +4,7 @@ class FixProjectsWithoutProjectFeature < ActiveRecord::Migration[5.2]
   include Gitlab::Database::MigrationHelpers
 
   BATCH_SIZE = 50_000
+  MIGRATION = 'FixProjectsWithoutProjectFeature'
 
   disable_ddl_transaction!
 
@@ -12,7 +13,7 @@ class FixProjectsWithoutProjectFeature < ActiveRecord::Migration[5.2]
   end
 
   def up
-    queue_background_migration_jobs_by_range_at_intervals(Project, 'FixProjectsWithoutProjectFeature', 2.minutes, batch_size: BATCH_SIZE)
+    queue_background_migration_jobs_by_range_at_intervals(Project, MIGRATION, 2.minutes, batch_size: BATCH_SIZE)
   end
 
   def down
