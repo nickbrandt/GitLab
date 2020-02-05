fc7613e1050b861ca375d4e791f6f50be64801ad
Steve Abrams <sabrams@gitlab.com>
2020-02-05 14:13:04 -0700

    Add validation to check package_type for metadatum
    Validation to ensure a metadatum record is not created
    on a package type that it does not belong to.

--- ee/spec/factories/packages.rb
+++ ee/spec/factories/packages.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     project
     name { 'my/company/app/my-app' }
     sequence(:version) { |n| "1.#{n}-SNAPSHOT" }
-    package_type { 'maven' }
+    package_type { :maven }
 
     factory :maven_package do
       maven_metadatum
@@ -23,7 +23,7 @@ FactoryBot.define do
     factory :npm_package do
       sequence(:name) { |n| "@#{project.root_namespace.path}/package-#{n}"}
       version { '1.0.0' }
-      package_type { 'npm' }
+      package_type { :npm }
 
       after :create do |package|
         create :package_file, :npm, package: package
@@ -57,7 +57,7 @@ FactoryBot.define do
 
       sequence(:name) { |n| "package-#{n}" }
       version { '1.0.0' }
-      package_type { 'conan' }
+      package_type { :conan }
 
       after :create do |package|
         create :conan_package_file, :conan_recipe_file, package: package
@@ -180,7 +180,7 @@ FactoryBot.define do
   end
 
   factory :maven_metadatum, class: 'Packages::MavenMetadatum' do
-    package
+    association :package, package_type: :maven
     path { 'my/company/app/my-app/1.0-SNAPSHOT' }
     app_group { 'my.company.app' }
     app_name { 'my-app' }
@@ -188,7 +188,7 @@ FactoryBot.define do
   end
 
   factory :conan_metadatum, class: 'Packages::ConanMetadatum' do
-    package
+    association :package, package_type: :conan
     package_username { 'username' }
     package_channel { 'stable' }
   end
