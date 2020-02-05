nickbrandt/GitLab

commit f755a5fffd1834fae2bc20a33c544157b7852f24

Allow instance admins to link all projects to DVCS
Use `Project.public_or_visible_to_user` instead of
`current_user.authorized_projects`

ee/lib/api/v3/github.rb
@@ -105,17 +105,19 @@ module API
         params do
           use :pagination
         end
-        # rubocop: disable CodeReuse/ActiveRecord
+
         get ':namespace/repos', requirements: NAMESPACE_ENDPOINT_REQUIREMENTS do
           namespace = Namespace.find_by_full_path(params[:namespace])
           not_found!('Namespace') unless namespace
 
-          projects = current_user.authorized_projects.where(namespace_id: namespace.self_and_descendants).to_a
+          projects = Project.public_or_visible_to_user(current_user)
+                            .in_namespace(namespace.self_and_descendants)
+                            .to_a
           projects.select! { |project| licensed_project?(project) }
           projects = ::Kaminari.paginate_array(projects)
+
           present paginate(projects), with: ::API::Github::Entities::Repository
         end
-        # rubocop: enable CodeReuse/ActiveRecord
       end
 
       # Jira dev panel integration weirdly requests for "/-/jira/pulls" instead

ee/spec/requests/api/v3/github_spec.rb
@@ -242,6 +242,18 @@ describe API::V3::Github do
       expect(json_response.size).to eq(projects.size)
     end
 
+    context 'when instance admin' do
+      let(:project) { create(:project, group: group) }
+
+      before do
+        stub_licensed_features(jira_dev_panel_integration: true)
+      end
+
+      it 'returns an array of projects belonging to group with github format' do
+        expect_project_under_namespace([project], group, create(:user, :admin))
+      end
+    end
+
     context 'group namespace' do
       let(:project) { create(:project, group: group) }
 

commit 3877f9e165d0e66c5b2f26806370dec5298fa638

Fix JIRA DVCS retrieving repositories
Retrieving repositories from personal namespace on GitLab.com results in
an error. This is a regression introduced in f755a5ff.

The intent was to return all projects for instance admins but we don't
need to include public projects for regular users.

ee/lib/api/v3/github.rb
@@ -117,10 +117,10 @@ module API
           namespace = Namespace.find_by_full_path(params[:namespace])
           not_found!('Namespace') unless namespace && licensed?(namespace)
 
-          projects = Project.public_or_visible_to_user(current_user)
-                            .in_namespace(namespace.self_and_descendants)
-                            .eager_load_namespace_and_owner
-                            .with_route
+          projects = current_user.can_read_all_resources? ? Project.all : current_user.authorized_projects
+          projects = projects.in_namespace(namespace.self_and_descendants)
+                             .eager_load_namespace_and_owner
+                             .with_route
 
           present paginate(projects),
                   with: ::API::Github::Entities::Repository,