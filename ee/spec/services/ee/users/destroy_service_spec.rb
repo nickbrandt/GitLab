9f019158fb2df1b1027446660d9b1e99de1afa4b
Tan Le <tle@gitlab.com>
2020-02-06 11:32:44 +1100

    Move specs to correct folder

--- ee/spec/services/ee/users/destroy_service_spec.rb
+++ ee/spec/services/ee/users/destroy_service_spec.rb
@@ -16,4 +16,70 @@ describe Users::DestroyService do
       end.to change { project.reload.mirror_user }.from(mirror_user).to(new_mirror_user)
     end
   end
+
+  describe 'audit events' do
+    let(:current_user) { create(:admin) }
+    let(:user) { create(:user) }
+
+    subject(:service) { described_class.new(current_user) }
+
+    before do
+      stub_licensed_features(admin_audit_log: true)
+    end
+
+    context 'soft delete' do
+      let(:hard_delete) { false }
+
+      context 'when user destroy operation succeeds' do
+        it 'logs audit events for ghost user migration and destroy operation' do
+          service.execute(user, hard_delete: hard_delete)
+
+          expect(AuditEvent.last(3)).to contain_exactly(
+            have_attributes(details: hash_including(change: 'email address')),
+            have_attributes(details: hash_including(change: 'username')),
+            have_attributes(details: hash_including(remove: 'user'))
+          )
+        end
+      end
+
+      context 'when user destroy operation fails' do
+        before do
+          allow(user).to receive(:destroy).and_return(user)
+        end
+
+        it 'logs audit events for ghost user migration operation' do
+          service.execute(user, hard_delete: hard_delete)
+
+          expect(AuditEvent.last(2)).to contain_exactly(
+            have_attributes(details: hash_including(change: 'email address')),
+            have_attributes(details: hash_including(change: 'username'))
+          )
+        end
+      end
+    end
+
+    context 'hard delete' do
+      let(:hard_delete) { true }
+
+      context 'when user destroy operation succeeds' do
+        it 'logs audit events for destroy operation' do
+          service.execute(user, hard_delete: hard_delete)
+
+          expect(AuditEvent.last)
+            .to have_attributes(details: hash_including(remove: 'user'))
+        end
+      end
+
+      context 'when user destroy operation fails' do
+        before do
+          allow(user).to receive(:destroy).and_return(user)
+        end
+
+        it 'does not log any audit event' do
+          expect { service.execute(user, hard_delete: hard_delete) }
+            .not_to change { AuditEvent.count }
+        end
+      end
+    end
+  end
 end
