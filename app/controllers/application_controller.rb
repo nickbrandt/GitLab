1375b2cdb91b121d9ddc498649f2352fe22e8f43
nhirokinet <nhirokinet@nhiroki.net>
2020-02-06 14:45:34 +0000

    Move Referrer-Policy from HTML to header
    commit cbf7b3f7d4089026c0d3669272e6d672c1d1a757
    Author: nhirokinet <nhirokinet@nhiroki.net>
    Date:   Thu Jan 16 13:57:15 2020 +0000
    
        add origin-when-cross-origin Referrer-Policy by default from Rails
        app
    
    commit 74150647c4b652d2594efcb4a4d8c819dbf4e7ef
    Author: nhirokinet <nhirokinet@nhiroki.net>
    Date:   Wed Dec 11 11:54:06 2019 +0000
    
        Correct default value
    
    commit 922be99f8bf41dbcbf15ba5292e6665a2ab20665
    Author: nhirokinet <nhirokinet@nhiroki.net>
    Date:   Sun Dec 8 23:45:25 2019 +0900
    
        add default Referrer-policy for sample nginx configuration
    
    commit 8b95d9dbcd3af163efea6567e307b0d0d7fb9ee5
    Author: nhirokinet <nhirokinet@nhiroki.net>
    Date:   Sun Nov 24 11:41:50 2019 +0000
    
        remove duplicated and hard-coded referrer-policy
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,6 +52,7 @@ class ApplicationController < ActionController::Base
   around_action :set_current_admin
 
   after_action :set_page_title_header, if: :json_request?
+  after_action :set_referrer_policy_header
   after_action :limit_session_time, if: -> { !current_user }
 
   protect_from_forgery with: :exception, prepend: true
@@ -558,6 +559,10 @@ class ApplicationController < ActionController::Base
     store_location_for :user, request.fullpath
 
     redirect_to users_sign_up_welcome_path
+  end
+
+  def set_referrer_policy_header
+    response.headers['Referrer-Policy'] = 'origin-when-cross-origin'
   end
 end
 
