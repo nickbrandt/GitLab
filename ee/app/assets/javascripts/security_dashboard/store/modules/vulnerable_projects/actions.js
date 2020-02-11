66ee0bf0ffa3f69f1c57b6b67ac8735df1854c76
Dave Pisek <dpisek@gitlab.com>
2020-02-11 12:01:50 +1100

    Remove reference to outdated issue in comments
    This commit removes a reference to an outdated issue from comments
    within vuex-action files.
    
    It pointed to an issue that aimed to make the default export obsolete,
    but the issue has been closed and the export will be needed until
    all tests have been migrated to karma.
    
    See comment on the issue:
    https://gitlab.com/gitlab-org/gitlab/issues/24511#note_273343173

--- ee/app/assets/javascripts/security_dashboard/store/modules/vulnerable_projects/actions.js
+++ ee/app/assets/javascripts/security_dashboard/store/modules/vulnerable_projects/actions.js
@@ -40,5 +40,4 @@ export const receiveProjectsError = ({ commit }) => {
 };
 
 // prevent babel-plugin-rewire from generating an invalid default during karma tests
-// This is no longer needed after gitlab-foss#52179 is merged
 export default () => {};
