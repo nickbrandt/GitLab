cd515461809dc788069c0e06eac435e4bf0a7871
Alex Buijs <abuijs@gitlab.com>
2020-02-11 15:20:36 +0100

    Apply license to selected group
    When a group is selected when a subscription
    is bought, apply the license to it.

--- ee/app/helpers/subscriptions_helper.rb
+++ ee/app/helpers/subscriptions_helper.rb
@@ -8,7 +8,8 @@ module SubscriptionsHelper
       setup_for_company: (current_user.setup_for_company == true).to_s,
       full_name: current_user.name,
       plan_data: plan_data.to_json,
-      plan_id: params[:plan_id]
+      plan_id: params[:plan_id],
+      new_user: new_user?.to_s
     }
   end
 
@@ -20,6 +21,12 @@ module SubscriptionsHelper
   end
 
   private
+
+  def new_user?
+    return false unless request.referer.present?
+
+    URI.parse(request.referer).path.in?([users_sign_up_welcome_path, users_sign_up_update_registration_path])
+  end
 
   def plan_data
     FetchSubscriptionPlansService.new(plan: :free).execute
