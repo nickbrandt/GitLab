086542e2049f749da50773e1063aa7fbd9868fa6
Alex Buijs <abuijs@gitlab.com>
2020-02-11 12:59:03 +0100

    Fix URL params breaking flow recognition

--- ee/app/helpers/ee/registrations_helper.rb
+++ ee/app/helpers/ee/registrations_helper.rb
@@ -3,7 +3,9 @@
 module EE
   module RegistrationsHelper
     def in_paid_signup_flow?
-      experiment_enabled?(:paid_signup_flow) && session['user_return_to'] == new_subscriptions_path
+      experiment_enabled?(:paid_signup_flow) &&
+        (redirect_to = session['user_return_to']) &&
+        URI.parse(redirect_to).path == new_subscriptions_path
     end
   end
 end
