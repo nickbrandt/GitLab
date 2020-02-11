223f8fb57fb756c9b666d1c0d8f6f53643a601d9
Matthias Kaeppler <mkaeppler@gitlab.com>
2020-02-11 16:17:17 +0100

    Wire up foreign-key IDs between assoc and owner
    Since we cannot rely on AR to do this for us anymore,
    we need to do it manually.

--- app/models/concerns/bulk_insertable_associations.rb
+++ app/models/concerns/bulk_insertable_associations.rb
@@ -7,8 +7,12 @@ module BulkInsertableAssociations
   MissingAssociationError = Class.new(StandardError)
 
   class_methods do
+    def supports_bulk_insert?(association)
+      BulkInsertableAssociations.association_class_for(self, association) < BulkInsertSafe
+    end
+
     def bulk_insert_on_save(association, items)
-      unless BulkInsertableAssociations.supports_bulk_insert?(self, association)
+      unless supports_bulk_insert?(association)
         raise "#{association} does not support bulk inserts"
       end
 
@@ -20,31 +24,32 @@ module BulkInsertableAssociations
 
   included do
     delegate :bulk_insert_on_save, to: self
-    after_save { BulkInsertableAssociations.flush_pending_bulk_inserts(self.class) }
-  end
-
-  def supports_bulk_insert?(model_class, association)
-    association_class_for(model_class, association) < BulkInsertSafe
+    after_save { BulkInsertableAssociations.flush_pending_bulk_inserts(self) }
   end
 
   def bulk_insert_context_for(model_class)
     bulk_insert_context[model_class] ||= {}
   end
 
-  def flush_pending_bulk_inserts(model_class)
+  def flush_pending_bulk_inserts(model_instance)
+    model_class = model_instance.class
     pending_association_items = bulk_insert_context_for(model_class)
     return unless pending_association_items&.any?
 
     pending_association_items.each do |association, items|
-      items.each { |item| item.delete('id') unless item['id'] }
+      items.each do |item|
+        # remove any `id` fields that are nil since these won't insert cleanly
+        item.delete('id') unless item['id']
+        # wire up foreign key ID
+        owner_id_attribute = model_class.reflections[association].foreign_key
+        item[owner_id_attribute] = model_instance.id
+      end
       association_class = association_class_for(model_class, association)
-      association_class.insert_all(items, returning: ['id'])
+      association_class.insert_all(items)
     end
   ensure
     clear_bulk_insert_context_for(model_class)
   end
-
-  private
 
   def association_class_for(model_class, association)
     reflection = model_class.reflect_on_association(association)
@@ -54,6 +59,8 @@ module BulkInsertableAssociations
 
     reflection.klass
   end
+
+  private
 
   def bulk_insert_context
     Thread.current['_bulk_insert_context'] ||= {}
