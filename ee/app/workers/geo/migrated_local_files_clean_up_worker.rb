33e387614827e776ccc519867a48339f92c6fb85
Valery Sizov <valery@gitlab.com>
2020-02-13 21:55:46 +0000

    Fix: [Geo]Replicating objects in object storage schedules removal
    and re-syncs object again
    
    We just skip MigratedLocalFilesCleanUpWorker when sync_object_storage
    for node is enabled

--- ee/app/workers/geo/migrated_local_files_clean_up_worker.rb
+++ ee/app/workers/geo/migrated_local_files_clean_up_worker.rb
@@ -7,10 +7,10 @@ module Geo
     MAX_CAPACITY = 1000
 
     def perform
+      # No need to run when objects stored in Object Storage should be synced too
+      return if sync_object_storage_enabled?
       # No need to run when nothing is configured to be in Object Storage
-      return unless attachments_object_store_enabled? ||
-          lfs_objects_object_store_enabled? ||
-          job_artifacts_object_store_enabled?
+      return unless object_store_enabled?
 
       super
     end
@@ -91,6 +91,16 @@ module Geo
 
     def job_artifacts_object_store_enabled?
       JobArtifactUploader.object_store_enabled?
+    end
+
+    def object_store_enabled?
+      attachments_object_store_enabled? ||
+        lfs_objects_object_store_enabled? ||
+        job_artifacts_object_store_enabled?
+    end
+
+    def sync_object_storage_enabled?
+      current_node.sync_object_storage
     end
 
     def attachments_finder
