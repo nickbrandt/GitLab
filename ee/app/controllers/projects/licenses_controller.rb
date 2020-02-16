168c0e5d0f97ecae7a1b0580a9490e06c799bf68
Stan Hu <stanhu@gmail.com>
2020-02-16 15:22:17 +0000

    Revert "Merge branch '14061-license-app-data' into 'master'"
    This reverts merge request !24831

--- ee/app/controllers/projects/licenses_controller.rb
+++ ee/app/controllers/projects/licenses_controller.rb
@@ -11,7 +11,6 @@ module Projects
     def index
       respond_to do |format|
         format.html do
-          @licenses_app_data = licenses_app_data
           render status: :ok
         end
         format.json do
@@ -83,14 +82,6 @@ module Projects
 
     def truthy?(value)
       value.in?(%w[true 1])
-    end
-
-    def licenses_app_data
-      {
-        endpoint: project_licenses_path(project, detected: true, format: :json),
-        documentation_path: help_page_path('user/application_security/license_compliance/index'),
-        empty_state_svg_path: helpers.image_path('illustrations/Dependency-list-empty-state.svg')
-      }
     end
   end
 end
