9664682c2b8bd2526a70ffd35272262a9fda2bf7
rpereira2 <rpereira@gitlab.com>
2020-02-17 22:59:35 +0530

    Set encoding of logs to UTF-8
    * Before the logs can be returned in the API response, the log strings
    should be converted to our default encoding, which is UTF-8.
    
    https://gitlab.com/gitlab-org/gitlab/issues/34965

--- ee/app/services/pod_logs/kubernetes_service.rb
+++ ee/app/services/pod_logs/kubernetes_service.rb
@@ -11,6 +11,9 @@ module PodLogs
           :check_pod_name,
           :check_container_name,
           :pod_logs,
+          :force_logs_encoding_to_utf8,
+          :encode_logs_to_utf8,
+          :split_logs,
           :filter_return_keys
 
     self.reactive_cache_worker_finder = ->(id, _cache_key, params) { new(Environment.find(id), params: params) }
@@ -18,7 +21,7 @@ module PodLogs
     private
 
     def pod_logs(result)
-      logs = environment.deployment_platform.kubeclient.get_pod_log(
+      result[:logs] = environment.deployment_platform.kubeclient.get_pod_log(
         result[:pod_name],
         environment.deployment_namespace,
         container: result[:container_name],
@@ -26,7 +29,48 @@ module PodLogs
         timestamps: true
       ).body
 
-      result[:logs] = logs.strip.lines(chomp: true).map do |line|
+      success(result)
+    rescue Kubeclient::ResourceNotFoundError
+      error(_('Pod not found'))
+    rescue Kubeclient::HttpError => e
+      ::Gitlab::ErrorTracking.track_exception(e)
+
+      log_error("#{e.class} Kubernetes API returned status code: #{e.error_code}, '#{e.message}'")
+
+      error(_('Kubernetes API returned status code: %{error_code}') % {
+        error_code: e.error_code
+      })
+    end
+
+    def force_logs_encoding_to_utf8(result)
+      return success(result) unless Feature.enabled?(:pod_logs_encoding_fix, environment.project)
+      return success(result) if result[:logs].encoding == Encoding::UTF_8
+
+      logs = force_utf8(result[:logs])
+
+      if logs.valid_encoding?
+        result[:logs] = logs
+      end
+
+      success(result)
+    end
+
+    def encode_logs_to_utf8(result)
+      return success(result) unless Feature.enabled?(:pod_logs_encoding_fix, environment.project)
+      return success(result) if result[:logs].encoding == Encoding::UTF_8
+
+      result[:logs] = result[:logs].encode(Encoding::UTF_8, invalid: :replace, undef: :replace)
+
+      success(result)
+    rescue EncodingError => exception
+      ::Gitlab::ErrorTracking.track_exception(exception)
+      log_error("#{exception.class} #{exception.message}")
+
+      error(_('Kubernetes logs could not be converted into UTF-8. Check Gitlab logs for errors.'))
+    end
+
+    def split_logs(result)
+      result[:logs] = result[:logs].strip.lines(chomp: true).map do |line|
         # message contains a RFC3339Nano timestamp, then a space, then the log line.
         # resolution of the nanoseconds can vary, so we split on the first space
         values = line.split(' ', 2)
@@ -37,14 +81,12 @@ module PodLogs
       end
 
       success(result)
-    rescue Kubeclient::ResourceNotFoundError
-      error(_('Pod not found'))
-    rescue Kubeclient::HttpError => e
-      ::Gitlab::ErrorTracking.track_exception(e)
+    end
 
-      error(_('Kubernetes API returned status code: %{error_code}') % {
-        error_code: e.error_code
-      })
+    def force_utf8(logs)
+      Gitlab::Utils.force_utf8(logs)
+    rescue EncodingError
+      logs
     end
   end
 end
--- ee/spec/services/pod_logs/kubernetes_service_spec.rb
+++ ee/spec/services/pod_logs/kubernetes_service_spec.rb
@@ -10,6 +10,12 @@ describe ::PodLogs::KubernetesService do
   let(:pod_name) { 'pod-1' }
   let(:container_name) { 'container-1' }
   let(:params) { {} }
+
+  let(:raw_logs) do
+    "2019-12-13T14:04:22.123456Z Log 1\n2019-12-13T14:04:23.123456Z Log 2\n" \
+      "2019-12-13T14:04:24.123456Z Log 3"
+  end
+
   let(:expected_logs) do
     [
       { message: "Log 1", timestamp: "2019-12-13T14:04:22.123456Z" },
@@ -27,6 +33,8 @@ describe ::PodLogs::KubernetesService do
         container_name: container_name
       }
     end
+
+    let(:expected_logs) { raw_logs }
     let(:service) { create(:cluster_platform_kubernetes, :configured) }
 
     before do
@@ -65,6 +73,165 @@ describe ::PodLogs::KubernetesService do
 
       expect(result[:status]).to eq(:error)
       expect(result[:message]).to eq('Kubernetes API returned status code: 500')
+    end
+  end
+
+  describe '#force_logs_encoding_to_utf8' do
+    let(:service) { create(:cluster_platform_kubernetes, :configured) }
+    let(:expected_logs) { '2019-12-13T14:04:22.123456Z ✔ Started logging errors to Sentry' }
+    let(:raw_logs) { expected_logs.dup.force_encoding(Encoding::ASCII_8BIT) }
+
+    let(:result_arg) do
+      {
+        pod_name: pod_name,
+        container_name: container_name,
+        logs: raw_logs
+      }
+    end
+
+    before do
+      stub_kubeclient_discover(service.api_url)
+
+      logs_url = service.api_url +
+        "/api/v1/namespaces/#{environment.deployment_namespace}/pods/#{pod_name}" \
+        "/log?container=#{container_name}&tailLines=#{PodLogs::KubernetesService::LOGS_LIMIT}&" \
+        "timestamps=true"
+
+      response = {
+        body: raw_logs
+      }
+
+      WebMock.stub_request(:get, logs_url).to_return(response)
+    end
+
+    it 'converts logs to utf-8' do
+      result = subject.send(:force_logs_encoding_to_utf8, result_arg)
+
+      expect(result[:status]).to eq(:success)
+      expect(result[:logs]).to eq(expected_logs)
+    end
+
+    it 'ignores errors' do
+      allow(Gitlab::Utils).to receive(:force_utf8)
+        .and_raise(Encoding::UndefinedConversionError, 'error')
+
+      result = subject.send(:force_logs_encoding_to_utf8, result_arg)
+
+      expect(result[:status]).to eq(:success)
+      expect(result[:logs]).to eq(raw_logs)
+    end
+
+    it 'does not execute if feature flag is disabled' do
+      stub_feature_flags(pod_logs_encoding_fix: false)
+
+      result = subject.send(:force_logs_encoding_to_utf8, result_arg)
+
+      expect(result[:status]).to eq(:success)
+      expect(result[:logs]).to eq(raw_logs)
+      expect(result[:logs].encoding).to eq(Encoding::ASCII_8BIT)
+    end
+
+    context 'when logs are already in utf-8' do
+      let(:raw_logs) { expected_logs }
+
+      it 'does not execute' do
+        result = subject.send(:force_logs_encoding_to_utf8, result_arg)
+
+        expect(result[:status]).to eq(:success)
+        expect(result[:logs]).to eq(expected_logs)
+      end
+    end
+  end
+
+  describe '#encode_logs_to_utf8' do
+    let(:service) { create(:cluster_platform_kubernetes, :configured) }
+    let(:expected_logs) { '2019-12-13T14:04:22.123456Z ✔ Started logging errors to Sentry' }
+    let(:raw_logs) { expected_logs.dup.force_encoding(Encoding::ASCII_8BIT) }
+
+    let(:result_arg) do
+      {
+        pod_name: pod_name,
+        container_name: container_name,
+        logs: raw_logs
+      }
+    end
+
+    before do
+      stub_kubeclient_discover(service.api_url)
+
+      logs_url = service.api_url +
+        "/api/v1/namespaces/#{environment.deployment_namespace}/pods/#{pod_name}" \
+        "/log?container=#{container_name}&tailLines=#{PodLogs::KubernetesService::LOGS_LIMIT}&" \
+        "timestamps=true"
+
+      response = {
+        body: raw_logs
+      }
+
+      WebMock.stub_request(:get, logs_url).to_return(response)
+    end
+
+    it 'encodes logs to utf-8' do
+      result = subject.send(:encode_logs_to_utf8, result_arg)
+
+      expect(result[:status]).to eq(:success)
+      expect(result[:logs].encoding).to eq(Encoding::UTF_8)
+    end
+
+    it 'does not execute if feature flag is disabled' do
+      stub_feature_flags(pod_logs_encoding_fix: false)
+
+      result = subject.send(:encode_logs_to_utf8, result_arg)
+
+      expect(result[:status]).to eq(:success)
+      expect(result[:logs]).to eq(raw_logs)
+      expect(result[:logs].encoding).to eq(Encoding::ASCII_8BIT)
+    end
+
+    it 'returns error if exception is raised' do
+      allow(result_arg[:logs]).to receive(:encode).and_raise(EncodingError, 'error')
+
+      result = subject.send(:encode_logs_to_utf8, result_arg)
+
+      expect(result[:status]).to eq(:error)
+      expect(result[:message]).to eq('Kubernetes logs could not be converted into UTF-8. Check Gitlab logs for errors.')
+    end
+
+    context 'when logs are already in utf-8' do
+      let(:raw_logs) { expected_logs }
+
+      it 'does not execute' do
+        result = subject.send(:encode_logs_to_utf8, result_arg)
+
+        expect(result[:status]).to eq(:success)
+        expect(result[:logs]).to eq(expected_logs)
+      end
+    end
+  end
+
+  describe '#split_logs' do
+    let(:result_arg) do
+      {
+        pod_name: pod_name,
+        container_name: container_name,
+        logs: raw_logs
+      }
+    end
+
+    let(:service) { create(:cluster_platform_kubernetes, :configured) }
+
+    before do
+      create(:cluster, :provided_by_gcp, environment_scope: '*', projects: [environment.project])
+      create(:deployment, :success, environment: environment)
+
+      stub_kubeclient_logs(pod_name, environment.deployment_namespace, container: container_name)
+    end
+
+    it 'returns the logs' do
+      result = subject.send(:split_logs, result_arg)
+
+      expect(result[:status]).to eq(:success)
+      expect(result[:logs]).to eq(expected_logs)
     end
   end
 end
