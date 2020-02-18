31d08625d06f24a1615457db63da37097636615d
Fernando <farias@gitlab.com>
2020-02-18 00:33:19 -0500

    Revert "Revert "Merge branch 'license-compliance-policy-tab' into 'master'""
    This reverts commit bf3a07c97488bfc8a391fb2da01e5ae681cf871f.

--- ee/app/assets/javascripts/vue_shared/license_management/components/license_management_row.vue
+++ ee/app/assets/javascripts/vue_shared/license_management/components/license_management_row.vue
@@ -1,88 +1,55 @@
 <script>
-import { mapActions } from 'vuex';
-import { GlDropdown, GlDropdownItem } from '@gitlab/ui';
-import { getIssueStatusFromLicenseStatus } from 'ee/vue_shared/license_management/store/utils';
-import { s__ } from '~/locale';
-import Icon from '~/vue_shared/components/icon.vue';
+import {
+  getIssueStatusFromLicenseStatus,
+  getStatusTranslationsFromLicenseStatus,
+} from 'ee/vue_shared/license_management/store/utils';
 import IssueStatusIcon from '~/reports/components/issue_status_icon.vue';
-
-import { LICENSE_APPROVAL_STATUS } from '../constants';
-
-const visibleClass = 'visible';
-const invisibleClass = 'invisible';
 
 export default {
   name: 'LicenseManagementRow',
   components: {
-    GlDropdown,
-    GlDropdownItem,
-    Icon,
     IssueStatusIcon,
   },
   props: {
     license: {
       type: Object,
-      required: true,
-      validator: license =>
-        Boolean(license.name) &&
-        Object.values(LICENSE_APPROVAL_STATUS).includes(license.approvalStatus),
+      required: false,
+      default: null,
     },
   },
-  LICENSE_APPROVAL_STATUS,
-  [LICENSE_APPROVAL_STATUS.APPROVED]: s__('LicenseCompliance|Approved'),
-  [LICENSE_APPROVAL_STATUS.BLACKLISTED]: s__('LicenseCompliance|Blacklisted'),
   computed: {
-    approveIconClass() {
-      return this.license.approvalStatus === LICENSE_APPROVAL_STATUS.APPROVED
-        ? visibleClass
-        : invisibleClass;
-    },
-    blacklistIconClass() {
-      return this.license.approvalStatus === LICENSE_APPROVAL_STATUS.BLACKLISTED
-        ? visibleClass
-        : invisibleClass;
-    },
-    status() {
+    iconStatus() {
       return getIssueStatusFromLicenseStatus(this.license.approvalStatus);
     },
-    dropdownText() {
-      return this.$options[this.license.approvalStatus];
+    textStatus() {
+      return getStatusTranslationsFromLicenseStatus(this.license.approvalStatus);
     },
-  },
-  methods: {
-    ...mapActions(['setLicenseInModal', 'approveLicense', 'blacklistLicense']),
   },
 };
 </script>
+
 <template>
-  <div data-qa-selector="license_compliance_row">
-    <issue-status-icon :status="status" class="float-left append-right-default" />
-    <span class="js-license-name" data-qa-selector="license_name_content">{{ license.name }}</span>
-    <div class="float-right">
-      <div class="d-flex">
-        <gl-dropdown
-          :text="dropdownText"
-          toggle-class="d-flex justify-content-between align-items-center"
-          right
+  <div class="gl-responsive-table-row flex-md-column align-items-md-stretch p-0">
+    <div class="d-md-flex align-items-center js-license-row">
+      <!-- Name-->
+      <div class="table-section section-30 section-wrap pr-md-3">
+        <div class="table-mobile-header" role="rowheader">
+          {{ s__('Licenses|Name') }}
+        </div>
+        <div class="table-mobile-content name">
+          {{ license.name }}
+        </div>
+      </div>
+
+      <!-- Policy -->
+      <div class="table-section section-70 section-wrap pr-md-3">
+        <div class="table-mobile-header" role="rowheader">{{ s__('Licenses|Policy') }}</div>
+        <div
+          class="table-mobile-content text-capitalize d-flex align-items-center justify-content-end justify-content-md-start status"
         >
-          <gl-dropdown-item @click="approveLicense(license)">
-            <icon :class="approveIconClass" name="mobile-issue-close" />
-            {{ $options[$options.LICENSE_APPROVAL_STATUS.APPROVED] }}
-          </gl-dropdown-item>
-          <gl-dropdown-item @click="blacklistLicense(license)">
-            <icon :class="blacklistIconClass" name="mobile-issue-close" />
-            {{ $options[$options.LICENSE_APPROVAL_STATUS.BLACKLISTED] }}
-          </gl-dropdown-item>
-        </gl-dropdown>
-        <button
-          class="btn btn-blank js-remove-button"
-          type="button"
-          data-toggle="modal"
-          data-target="#modal-license-delete-confirmation"
-          @click="setLicenseInModal(license)"
-        >
-          <icon name="remove" />
-        </button>
+          <issue-status-icon :status="iconStatus" />
+          {{ textStatus }}
+        </div>
       </div>
     </div>
   </div>
