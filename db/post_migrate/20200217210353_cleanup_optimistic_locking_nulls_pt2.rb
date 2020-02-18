be414324af252509fcdbe90902cbc6bd04a49a27
Mario de la Ossa <mariodelaossa@gmail.com>
2020-02-17 22:00:29 -0600

    Set all NULL `lock_version` values to 0 PART 2
    Rails silently casts NULL `lock_version` values to 0 while doing
    optimistic locking, which causes false stale object exceptions. We had a
    monkey patch that would change it to check for [NULL, 0] but want to
    avoid monkey-patching if possible, which means we need to clean up our
    database values.
    
    This Commit is for CI objects (CI Stages, CI Builds, CI Pipelines)

--- db/post_migrate/20200217210353_cleanup_optimistic_locking_nulls_pt2.rb
+++ db/post_migrate/20200217210353_cleanup_optimistic_locking_nulls_pt2.rb
@@ -3,14 +3,14 @@
 # See http://doc.gitlab.com/ce/development/migration_style_guide.html
 # for more information on how to write migrations for GitLab.
 
-class CleanupOptimisticLockingNulls < ActiveRecord::Migration[5.2]
+class CleanupOptimisticLockingNullsPt2 < ActiveRecord::Migration[5.2]
   include Gitlab::Database::MigrationHelpers
 
   DOWNTIME = false
 
   disable_ddl_transaction!
 
-  TABLES = %w(epics merge_requests issues).freeze
+  TABLES = %w(ci_stages ci_builds ci_pipelines).freeze
   BATCH_SIZE = 10_000
 
   def declare_class(table)
@@ -23,15 +23,18 @@ class CleanupOptimisticLockingNulls < ActiveRecord::Migration[5.2]
   end
 
   def up
+    last_table_final_delay = 0
+
     TABLES.each do |table|
       add_concurrent_index table.to_sym, :lock_version, where: "lock_version IS NULL"
 
-      queue_background_migration_jobs_by_range_at_intervals(
+      last_table_final_delay = queue_background_migration_jobs_by_range_at_intervals(
         declare_class(table).where(lock_version: nil),
         'CleanupOptimisticLockingNulls',
         2.minutes,
         batch_size: BATCH_SIZE,
-        other_job_arguments: [table]
+        other_job_arguments: [table],
+        initial_delay: last_table_final_delay
       )
     end
   end
