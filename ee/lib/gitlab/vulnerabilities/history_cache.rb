0f813f6934ae03c30ef4ad1953ab8754aab775f1
Avielle Wolfe <awolfe@gitlab.com>
2020-02-19 10:43:25 +0000

    Separate pipelines query from vuln findings query
    * Refactor VulnerabilityFindingsFinder to take pipeline IDs and use
        them in the query for vulnerability findings
    * Move query for pipelines to the controller actions and the caching
        classes
    * Removes Vulnerable. We are no longer using the methods included in it
    
    https://gitlab.com/gitlab-org/gitlab/issues/202183

--- ee/lib/gitlab/vulnerabilities/history_cache.rb
+++ ee/lib/gitlab/vulnerabilities/history_cache.rb
@@ -13,8 +13,8 @@ module Gitlab
       def fetch(range, force: false)
         Rails.cache.fetch(cache_key, force: force, expires_in: 1.day) do
           findings = ::Security::VulnerabilityFindingsFinder
-            .new(vulnerable, params: { project_id: [project_id] })
-            .execute(:all)
+            .new(pipeline_ids, params: { project_id: [project_id] })
+            .execute
             .count_by_day_and_severity(range)
           ::Vulnerabilities::HistorySerializer.new.represent(findings)
         end
@@ -26,6 +26,10 @@ module Gitlab
         # TODO: rename 'vulnerabilities' to 'findings' in the cache key, but carefully
         # https://gitlab.com/gitlab-org/gitlab/issues/32963
         ['projects', project_id, 'vulnerabilities']
+      end
+
+      def pipeline_ids
+        vulnerable.all_pipelines.with_vulnerabilities.success
       end
     end
   end
