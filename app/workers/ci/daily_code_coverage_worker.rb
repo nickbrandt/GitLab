1574963c6afd66308c2310c30a4cd60f39b14b9d
Erick Bajao <fbajao@gitlab.com>
2020-02-20 18:53:04 +0800

    Mark daily code coverage worker as idempotent

--- app/workers/ci/daily_code_coverage_worker.rb
+++ app/workers/ci/daily_code_coverage_worker.rb
@@ -5,6 +5,8 @@ module Ci
     include ApplicationWorker
     include PipelineBackgroundQueue
 
+    idempotent!
+
     def perform(pipeline_id)
       Ci::Pipeline.find_by_id(pipeline_id).try do |pipeline|
         Ci::DailyCodeCoverageService.new.execute(pipeline)
