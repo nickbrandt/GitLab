d08ee7a72d839f9529083387fb3036ac88d08dd8
Dheeraj Joshi <djoshi@gitlab.com>
2020-02-20 17:51:07 +0530

    Do not escape the approval rule name

--- app/assets/javascripts/lib/utils/grammar.js
+++ app/assets/javascripts/lib/utils/grammar.js
@@ -25,13 +25,13 @@ export const toNounSeriesText = items => {
     return sprintf(s__('nounSeries|%{firstItem} and %{lastItem}'), {
       firstItem: items[0],
       lastItem: items[1],
-    });
+    }, false);
   }
 
   return items.reduce((item, nextItem, idx) =>
     idx === items.length - 1
-      ? sprintf(s__('nounSeries|%{item}, and %{lastItem}'), { item, lastItem: nextItem })
-      : sprintf(s__('nounSeries|%{item}, %{nextItem}'), { item, nextItem }),
+      ? sprintf(s__('nounSeries|%{item}, and %{lastItem}'), { item, lastItem: nextItem }, false)
+      : sprintf(s__('nounSeries|%{item}, %{nextItem}'), { item, nextItem }, false),
   );
 };
 
