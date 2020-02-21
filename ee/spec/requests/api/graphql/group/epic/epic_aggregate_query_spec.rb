c0749f85923da9538a1bd52ce70aa1ac027e5522
charlieablett <cablett@gitlab.com>
2020-02-21 23:26:21 +1300

    Add feature flag
    Without the feature flag, the DescendantCountService
    functionality is used instead

--- ee/spec/requests/api/graphql/group/epic/epic_aggregate_query_spec.rb
+++ ee/spec/requests/api/graphql/group/epic/epic_aggregate_query_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe 'Epic aggregates (count and weight)' do
+describe 'Query epic aggregates (count and weight)' do
   include GraphqlHelpers
 
   let_it_be(:current_user) { create(:user) }
@@ -25,18 +25,18 @@ describe 'Epic aggregates (count and weight)' do
 
   let(:epic_aggregates_query) do
     <<~QUERY
-      nodes {
-        descendantWeightSum {
-          openedIssues
-          closedIssues
-        }
-        descendantCounts {
-          openedEpics
-          closedEpics
-          openedIssues
-          closedIssues
-        }
+    nodes {
+      descendantWeightSum {
+        openedIssues
+        closedIssues
       }
+      descendantCounts {
+        openedEpics
+        closedEpics
+        openedIssues
+        closedIssues
+      }
+    }
     QUERY
   end
 
@@ -54,88 +54,36 @@ describe 'Epic aggregates (count and weight)' do
 
   it_behaves_like 'a working graphql query'
 
-  context 'with feature flag enabled' do
-    before do
-      stub_feature_flags(unfiltered_epic_aggregates: true)
-    end
+  it 'returns the epic counts' do
+    epic_count_result = {
+      "openedEpics" => 1,
+      "closedEpics" => 2
+    }
 
-    it 'returns the epic counts' do
-      epic_count_result = {
-          "openedEpics" => 1,
-          "closedEpics" => 2
-      }
-
-      is_expected.to include(
-        a_hash_including('descendantCounts' => a_hash_including(epic_count_result))
-      )
-    end
-
-    it 'returns the issue counts' do
-      issue_count_result = {
-          "openedIssues" => 1,
-          "closedIssues" => 1
-      }
-
-      is_expected.to include(
-        a_hash_including('descendantCounts' => a_hash_including(issue_count_result))
-      )
-    end
-
-    it 'returns the weights' do
-      descendant_weight_result = {
-          "openedIssues" => 5,
-          "closedIssues" => 7
-      }
-
-      is_expected.to include(
-        a_hash_including('descendantWeightSum' => a_hash_including(descendant_weight_result))
-      )
-    end
+    is_expected.to include(
+      a_hash_including('descendantCounts' => a_hash_including(epic_count_result))
+    )
   end
 
-  context 'with feature flag disabled' do
-    before do
-      stub_feature_flags(unfiltered_epic_aggregates: false)
-    end
+  it 'returns the issue counts' do
+    issue_count_result = {
+      "openedIssues" => 1,
+      "closedIssues" => 1
+    }
 
-    context 'when requesting counts' do
-      let(:epic_aggregates_query) do
-        <<~QUERY
-          nodes {
-            descendantCounts {
-              openedEpics
-              closedEpics
-              openedIssues
-              closedIssues
-            }
-          }
-        QUERY
-      end
+    is_expected.to include(
+      a_hash_including('descendantCounts' => a_hash_including(issue_count_result))
+    )
+  end
 
-      it 'uses the DescendantCountService' do
-        expect(Epics::DescendantCountService).to receive(:new)
+  it 'returns the weights' do
+    descendant_weight_result = {
+      "openedIssues" => 5,
+      "closedIssues" => 7
+    }
 
-        post_graphql(query, current_user: current_user)
-      end
-    end
-
-    context 'when requesting weights' do
-      let(:epic_aggregates_query) do
-        <<~QUERY
-          nodes {
-            descendantWeightSum {
-              openedIssues
-              closedIssues
-            }
-          }
-        QUERY
-      end
-
-      it 'returns an error' do
-        post_graphql(query, current_user: current_user)
-
-        expect_graphql_errors_to_include /Field 'descendantWeightSum' doesn't exist on type 'Epic/
-      end
-    end
+    is_expected.to include(
+      a_hash_including('descendantWeightSum' => a_hash_including(descendant_weight_result))
+    )
   end
 end
