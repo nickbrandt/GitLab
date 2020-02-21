2a087ad3053c2716e99589cfcf1a089a761bba97
Lee Tickett <lee@tickett.net>
2020-02-21 15:47:46 +0000

    Drop forked_project_links table

--- db/schema.rb
+++ db/schema.rb
@@ -1654,14 +1654,6 @@ ActiveRecord::Schema.define(version: 2020_02_20_180944) do
     t.index ["root_project_id"], name: "index_fork_networks_on_root_project_id", unique: true
   end
 
-  create_table "forked_project_links", id: :serial, force: :cascade do |t|
-    t.integer "forked_to_project_id", null: false
-    t.integer "forked_from_project_id", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["forked_to_project_id"], name: "index_forked_project_links_on_forked_to_project_id", unique: true
-  end
-
   create_table "geo_cache_invalidation_events", force: :cascade do |t|
     t.string "key", null: false
   end
@@ -4747,7 +4739,6 @@ ActiveRecord::Schema.define(version: 2020_02_20_180944) do
   add_foreign_key "fork_network_members", "projects", column: "forked_from_project_id", name: "fk_b01280dae4", on_delete: :nullify
   add_foreign_key "fork_network_members", "projects", on_delete: :cascade
   add_foreign_key "fork_networks", "projects", column: "root_project_id", name: "fk_e7b436b2b5", on_delete: :nullify
-  add_foreign_key "forked_project_links", "projects", column: "forked_to_project_id", name: "fk_434510edb0", on_delete: :cascade
   add_foreign_key "geo_container_repository_updated_events", "container_repositories", name: "fk_212c89c706", on_delete: :cascade
   add_foreign_key "geo_event_log", "geo_cache_invalidation_events", column: "cache_invalidation_event_id", name: "fk_42c3b54bed", on_delete: :cascade
   add_foreign_key "geo_event_log", "geo_container_repository_updated_events", column: "container_repository_updated_event_id", name: "fk_6ada82d42a", on_delete: :cascade
