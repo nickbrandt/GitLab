df3d2069174eb82b7a864cc4ad48a8db7763a8b7
Takuya Noguchi <takninnovationresearch@gmail.com>
2020-02-21 11:17:24 +0900

    Address Gitaly n+1 call from DataBuilder::Push
    Signed-off-by: Takuya Noguchi <takninnovationresearch@gmail.com>

--- lib/gitlab/data_builder/push.rb
+++ lib/gitlab/data_builder/push.rb
@@ -100,10 +100,8 @@ module Gitlab
         # For performance purposes maximum 20 latest commits
         # will be passed as post receive hook data.
         # n+1: https://gitlab.com/gitlab-org/gitlab-foss/issues/38259
-        commit_attrs = Gitlab::GitalyClient.allow_n_plus_1_calls do
-          commits_limited.map do |commit|
-            commit.hook_attrs(with_changed_files: with_changed_files)
-          end
+        commit_attrs = commits_limited.map do |commit|
+          commit.hook_attrs(with_changed_files: with_changed_files)
         end
 
         type = Gitlab::Git.tag_ref?(ref) ? 'tag_push' : 'push'
