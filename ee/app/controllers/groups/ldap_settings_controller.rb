b4aa9619a10ada92264c280f08caf1dcdf594fbd
Sebastian Arcila Valenzuela <sarcila@gitlab.com>
2020-02-21 17:18:03 +0100

    Implement update

--- ee/app/controllers/groups/ldap_settings_controller.rb
+++ ee/app/controllers/groups/ldap_settings_controller.rb
@@ -1,6 +1,33 @@
 # frozen_string_literal: true
 
-class Groups::LdapSettingsController < ApplicationController
+class Groups::LdapSettingsController < Groups::ApplicationController
+  before_action :group
+  before_action :require_ldap_enabled
+  before_action :authorize_admin_group!
+  before_action :authorize_manage_ldap_settings!
+
   def update
+    if @group.update(ldap_settings_params)
+      redirect_back_or_default(default: { action: 'index' }, options: { notice: _('LDAP settings updated') })
+    else
+      redirect_back_or_default(default: { action: 'index' }, options: { alert: _('Could not update the LDAP settings') })
+    end
+  end
+
+  private
+
+  def authorize_manage_ldap_settings!
+    # todo
+    # render_404 unless can?(current_user, :admin_ldap_settings, group)
+  end
+
+  def require_ldap_enabled
+    render_404 unless Gitlab.config.ldap.enabled
+  end
+
+  def ldap_settings_params
+    attrs = %i[lock_membership_to_ldap]
+
+    params.require(:group).permit(attrs)
   end
 end
