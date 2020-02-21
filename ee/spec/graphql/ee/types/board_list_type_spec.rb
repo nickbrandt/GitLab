778e5ff34789c4d04e55d7c05a529eef7b1f979d
Brett Walker <bwalker@gitlab.com>
2020-02-21 14:48:32 -0600

    Allow graphql querying of board lists
    from project or group boards

---
+++ ee/spec/graphql/ee/types/board_list_type_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe GitlabSchema.types['BoardList'] do
+  it 'has specific fields' do
+    expected_fields = %w[milestone max_issue_count max_issue_weight assignee]
+
+    expect(described_class).to include_graphql_fields(*expected_fields)
+  end
+end
