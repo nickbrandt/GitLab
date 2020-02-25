72f7dd83ea651bb7541e8d00325c7071876c6957
Simon Knox <simon@gitlab.com>
2020-02-26 10:32:07 +1100

    Show assignee and milestone lists in boards modal
    Separate section and heading for each type

--- app/assets/javascripts/boards/components/modal/lists_dropdown.vue
+++ app/assets/javascripts/boards/components/modal/lists_dropdown.vue
@@ -1,12 +1,17 @@
 <script>
-import { GlLink } from '@gitlab/ui';
+import { GlDropdown, GlDropdownItem, GlDropdownHeader, GlDropdownDivider } from '@gitlab/ui';
 import Icon from '~/vue_shared/components/icon.vue';
+import UserAvatarImage from '~/vue_shared/components/user_avatar/user_avatar_image.vue';
 import ModalStore from '../../stores/modal_store';
 import boardsStore from '../../stores/boards_store';
 
 export default {
   components: {
-    GlLink,
+    GlDropdown,
+    GlDropdownItem,
+    GlDropdownHeader,
+    GlDropdownDivider,
+    UserAvatarImage,
     Icon,
   },
   data() {
@@ -19,6 +24,15 @@ export default {
     selected() {
       return this.modal.selectedList || this.state.lists[1];
     },
+    labelLists() {
+      return this.state.lists.filter(l => l.type === 'label');
+    },
+    assigneeLists() {
+      return this.state.lists.filter(l => l.type === 'assignee');
+    },
+    milestoneLists() {
+      return this.state.lists.filter(l => l.type === 'milestone');
+    },
   },
   destroyed() {
     this.modal.selectedList = null;
@@ -27,24 +41,71 @@ export default {
 </script>
 <template>
   <div class="dropdown inline">
-    <button class="dropdown-menu-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
-      <span :style="{ backgroundColor: selected.label.color }" class="dropdown-label-box"> </span>
-      {{ selected.title }} <icon name="chevron-down" />
-    </button>
-    <div class="dropdown-menu dropdown-menu-selectable dropdown-menu-drop-up">
-      <ul>
-        <li v-for="(list, i) in state.lists" v-if="list.type == 'label'" :key="i">
-          <gl-link
-            :class="{ 'is-active': list.id == selected.id }"
-            href="#"
-            role="button"
-            @click.prevent="modal.selectedList = list"
-          >
-            <span :style="{ backgroundColor: list.label.color }" class="dropdown-label-box"> </span>
-            {{ list.title }}
-          </gl-link>
-        </li>
-      </ul>
-    </div>
+    <gl-dropdown>
+      <template slot="button-content">
+        <span class="str-truncated-100 mr-2">
+          <template v-if="selected.type === 'label'">
+            <span :style="{ backgroundColor: selected.label.color }" class="dropdown-label-box"> </span>
+            {{ selected.title }}
+          </template>
+          <template v-if="selected.type === 'assignee'">
+            <user-avatar-image 
+              :img-src="selected.avatar_url"
+              :img-alt="selected.name"
+              :img-size="24"
+            />
+            {{ selected.title }}
+          </template>
+          <template v-if="selected.type === 'milestone'">
+            {{ selected.title }}
+          </template>
+        </span>
+        <icon name="chevron-down" class="ml-auto" />
+      </template>
+      <section>
+        <gl-dropdown-header>{{ __('Label') }}</gl-dropdown-header>
+        <ul>
+          <li v-for="(list, i) in labelLists" :key="i">
+            <gl-dropdown-item
+              :class="{ 'is-active': list.id == selected.id }"
+              @click.prevent="modal.selectedList = list"
+            >
+              <span :style="{ backgroundColor: list.label.color }" class="dropdown-label-box"> </span>
+              {{ list.title }}
+            </gl-dropdown-item>
+          </li>
+        </ul>
+      </section>
+      <gl-dropdown-divider />
+      <section>
+        <gl-dropdown-header>{{ __('Assignee') }}</gl-dropdown-header>
+        <ul>
+          <li v-for="(assigneeList, i) in assigneeLists" :key="i">
+            <gl-dropdown-item
+              :class="{ 'is-active': assigneeList.id == selected.id }"
+              @click.prevent="modal.selectedList = assigneeList"
+            >
+              <user-avatar-image :src="assigneeList.avatar" />
+              {{ assigneeList.title }}
+            </gl-dropdown-item>
+          </li>
+        </ul>
+      </section>
+      <gl-dropdown-divider />
+      <section>
+        <gl-dropdown-header>{{ __('Milestone') }}</gl-dropdown-header>
+        <ul>
+          <li v-for="(list, i) in milestoneLists" :key="i">
+            <gl-dropdown-item
+              :class="{ 'is-active': list.id == selected.id }"
+              @click.prevent="modal.selectedList = list"
+            >
+              
+              {{ list.title }}
+            </gl-dropdown-item>
+          </li>
+        </ul>
+      </section>
+    </gl-dropdown>
   </div>
 </template>
