1ad139e951b9f37985366b28f176beb5bf5e4ec1
Doug Stull <dstull@gitlab.com>
2020-02-26 08:36:46 -0500

    Improve tests for ci config path permission checks
    - needed to qualify the spec

--- app/serializers/merge_request_widget_entity.rb
+++ app/serializers/merge_request_widget_entity.rb
@@ -95,8 +95,7 @@ class MergeRequestWidgetEntity < Grape::Entity
       merge_request.all_pipelines.none? &&
       merge_request.commits_count.positive? &&
       can?(current_user, :read_build, merge_request.source_project) &&
-      can?(current_user, :create_pipeline, merge_request.source_project) &&
-      can?(current_user, :push_code, merge_request.source_project)
+      can?(current_user, :create_pipeline, merge_request.source_project)
   end
 end
 
