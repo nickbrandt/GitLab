678846d103e35f897df8b21f51dbdd24d0d94d1e
Mike Kozono <mkozono@gmail.com>
2020-02-25 16:53:18 -0800

    Test Blob Replicators

---
+++ spec/support/shared_examples/models/concerns/blob_replicator_strategy_shared_examples.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+# Include these shared examples in specs of Replicators that include
+# BlobReplicatorStrategy.
+#
+# A let variable called model_record should be defined in the spec. It should be
+# a valid, unpersisted instance of the model class.
+#
+RSpec.shared_examples 'a blob replicator' do
+  include EE::GeoHelpers
+
+  let_it_be(:primary) { create(:geo_node, :primary) }
+  let_it_be(:secondary) { create(:geo_node) }
+
+  subject(:replicator) { model_record.replicator }
+
+  before do
+    stub_current_geo_node(primary)
+  end
+
+  describe '#publish_created_event' do
+    it 'creates a Geo::Event' do
+      expect do
+        replicator.publish_created_event
+      end.to change { ::Geo::Event.count }.by(1)
+
+      expect(::Geo::Event.last.attributes).to include(
+        "replicable_name" => replicator.replicable_name, "event_name" => "created", "payload" => { "model_record_id" => replicator.model_record.id })
+    end
+  end
+
+  describe '#consume_created_event' do
+    it 'invokes Geo::BlobDownloadService' do
+      service = double(:service)
+
+      expect(service).to receive(:execute)
+      expect(::Geo::BlobDownloadService).to receive(:new).with(replicator: replicator).and_return(service)
+
+      replicator.consume_created_event
+    end
+  end
+
+  describe '#carrierwave_uploader' do
+    it 'is implemented' do
+      expect do
+        replicator.carrierwave_uploader
+      end.not_to raise_error
+    end
+  end
+
+  describe '#model' do
+    let(:invoke_model) { replicator.send(:model) }
+
+    it 'is implemented' do
+      expect do
+        invoke_model
+      end.not_to raise_error
+    end
+
+    it 'is a Class' do
+      expect(invoke_model).to be_a(Class)
+    end
+  end
+end
