7c3f7412e4ad143e824bcc8d65248e482b35f60b
Thong Kuah <tkuah@gitlab.com>
2020-02-27 14:52:30 +1300

    Wrap test inside describe block
    Also fix path construction to use path helper

--- spec/requests/groups/milestones_controller_spec.rb
+++ spec/requests/groups/milestones_controller_spec.rb
@@ -12,23 +12,25 @@ describe Groups::MilestonesController do
     end
     let!(:private_milestone) { create(:milestone, project: public_project_with_private_issues_and_mrs, title: 'project milestone') }
 
-    it 'avoids N+1 database queries' do
-      public_project = create(:project, :public, :merge_requests_enabled, :issues_enabled, group: public_group)
-      create(:milestone, project: public_project)
+    describe 'GET #index' do
+      it 'avoids N+1 database queries' do
+        public_project = create(:project, :public, :merge_requests_enabled, :issues_enabled, group: public_group)
+        create(:milestone, project: public_project)
 
-      control_count = ActiveRecord::QueryRecorder.new(skip_cached: false) { get "/groups/#{public_group.to_param}/-/milestones.json" }.count
+        control_count = ActiveRecord::QueryRecorder.new(skip_cached: false) { get group_milestones_path(public_group, format: :json) }.count
 
-      projects = create_list(:project, 2, :public, :merge_requests_enabled, :issues_enabled, group: public_group)
-      projects.each do |project|
-        create(:milestone, project: project)
+        projects = create_list(:project, 2, :public, :merge_requests_enabled, :issues_enabled, group: public_group)
+        projects.each do |project|
+          create(:milestone, project: project)
+        end
+
+        expect { get group_milestones_path(public_group, format: :json) }.not_to exceed_all_query_limit(control_count)
+        expect(response).to have_gitlab_http_status(:ok)
+        milestones = json_response
+
+        expect(milestones.count).to eq(3)
+        expect(milestones.map {|x| x['title']}).not_to include(private_milestone.title)
       end
-
-      expect { get "/groups/#{public_group.to_param}/-/milestones.json" }.not_to exceed_all_query_limit(control_count)
-      expect(response).to have_gitlab_http_status(:ok)
-      milestones = json_response
-
-      expect(milestones.count).to eq(3)
-      expect(milestones.map {|x| x['title']}).not_to include(private_milestone.title)
     end
   end
 end
