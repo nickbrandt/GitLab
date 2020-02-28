5d1cc96cf27c0e95b5acbae4f9910647f2ad5625
Małgorzata Ksionek <mksionek@gitlab.com>
2020-02-28 14:08:03 +0100

    Add checking forked project members membership in group managed account
    Add translation method
    
    Add different behaviour for forked project
    
    Add changelog entry
    
    Fix ambigous name
    
    Check for namespace kind
    
    Add cr remarks
    
    Fix badly merged files
    
    Add cr remarks
    
    Add cr remarks
    
    Fix badly merged files

--- ee/lib/gitlab/auth/group_saml/gma_membership_enforcer.rb
+++ ee/lib/gitlab/auth/group_saml/gma_membership_enforcer.rb
@@ -9,15 +9,27 @@ module Gitlab
         end
 
         def can_add_user?(user)
-          return true unless root_group&.enforced_group_managed_accounts?
+          can_add_user_to_main_project = check_group_membership(user, project)
+          can_add_user_to_source_project = project.forked? ? check_group_membership(user, project.forked_from_project) : true
 
-          root_group == user.managing_group
+          can_add_user_to_main_project && can_add_user_to_source_project
         end
 
         private
 
-        def root_group
-          @root_group ||= @project.root_ancestor
+        attr_reader :project
+
+        def check_group_membership(user, given_project)
+          root_ancestor = project_root_ancestor(given_project)
+
+          return true unless root_ancestor.kind == 'group'
+          return true unless root_ancestor.enforced_group_managed_accounts?
+
+          root_ancestor == user.managing_group
+        end
+
+        def project_root_ancestor(given_project)
+          given_project.root_ancestor
         end
       end
     end
