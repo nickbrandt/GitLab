mehulagg/gitlab_test

Return the full path to an upload in api response
To fetch an upload, a GET request needs to be made to
/namespace_path/project_path/:upload_path. This change includes
this url in the data returned after making an upload.

spec/requests/api/projects_spec.rb
@@ -1250,6 +1250,8 @@ describe API::Projects do
       expect(json_response['alt']).to eq("dk")
       expect(json_response['url']).to start_with("/uploads/")
       expect(json_response['url']).to end_with("/dk.png")
+
+      expect(json_response['full_url']).to start_with("/#{project.namespace.path}/#{project.path}/uploads")
     end
   end
 

nickbrandt/GitLab

spec/requests/api/projects_spec.rb
@@ -1250,6 +1250,8 @@ describe API::Projects do
       expect(json_response['alt']).to eq("dk")
       expect(json_response['url']).to start_with("/uploads/")
       expect(json_response['url']).to end_with("/dk.png")
+
+      expect(json_response['full_path']).to start_with("/#{project.namespace.path}/#{project.path}/uploads")
     end
   end
 