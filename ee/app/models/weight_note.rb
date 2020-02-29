d7ac948c2fa8bd36552331790192b8a2cc4ced66
Heinrich Lee Yu <heinrich@gitlab.com>
2020-02-29 18:22:40 +0800

    Fix incorrect class definitions
    Zeitwerk requires that class definitions match the file location

--- ee/app/models/weight_note.rb
+++ ee/app/models/weight_note.rb
@@ -1,51 +1,49 @@
 # frozen_string_literal: true
 
-module EE
-  class WeightNote < ::Note
-    attr_accessor :resource_parent, :event
+class WeightNote < ::Note
+  attr_accessor :resource_parent, :event
 
-    def self.from_event(event, resource: nil, resource_parent: nil)
-      resource ||= event.issue
+  def self.from_event(event, resource: nil, resource_parent: nil)
+    resource ||= event.issue
 
-      attrs = {
-        system: true,
-        author: event.user,
-        created_at: event.created_at,
-        noteable: resource,
-        event: event,
-        discussion_id: event.discussion_id,
-        system_note_metadata: ::SystemNoteMetadata.new(action: 'weight'),
-        resource_parent: resource_parent
-      }
+    attrs = {
+      system: true,
+      author: event.user,
+      created_at: event.created_at,
+      noteable: resource,
+      event: event,
+      discussion_id: event.discussion_id,
+      system_note_metadata: ::SystemNoteMetadata.new(action: 'weight'),
+      resource_parent: resource_parent
+    }
 
-      if resource_parent.is_a?(Project)
-        attrs[:project_id] = resource_parent.id
-      end
-
-      WeightNote.new(attrs)
+    if resource_parent.is_a?(Project)
+      attrs[:project_id] = resource_parent.id
     end
 
-    def note
-      @note ||= note_text
-    end
+    WeightNote.new(attrs)
+  end
 
-    def note_html
-      @note_html ||= "<p dir=\"auto\">#{note_text(html: true)}</p>"
-    end
+  def note
+    @note ||= note_text
+  end
 
-    def project
-      resource_parent if resource_parent.is_a?(Project)
-    end
+  def note_html
+    @note_html ||= "<p dir=\"auto\">#{note_text(html: true)}</p>"
+  end
 
-    def group
-      resource_parent if resource_parent.is_a?(Group)
-    end
+  def project
+    resource_parent if resource_parent.is_a?(Project)
+  end
 
-    private
+  def group
+    resource_parent if resource_parent.is_a?(Group)
+  end
 
-    def note_text(html: false)
-      weight_text = html ? "<strong>#{event.weight}</strong>" : event.weight
-      event.weight ? "changed weight to #{weight_text}" : 'removed the weight'
-    end
+  private
+
+  def note_text(html: false)
+    weight_text = html ? "<strong>#{event.weight}</strong>" : event.weight
+    event.weight ? "changed weight to #{weight_text}" : 'removed the weight'
   end
 end
