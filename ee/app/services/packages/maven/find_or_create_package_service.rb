ecee5447d7c1f7924a84a8cb513f629635f7eb5b
sara <admin@example.com>
2020-02-29 00:52:34 -0800

    Remove maven-metadata condition

--- ee/app/services/packages/maven/find_or_create_package_service.rb
+++ ee/app/services/packages/maven/find_or_create_package_service.rb
@@ -9,20 +9,7 @@ module Packages
           .new(params[:path], current_user, project: project).execute
 
         unless package
-          if params[:file_name] == MAVEN_METADATA_FILE
-            # Maven uploads several files during `mvn deploy` in next order:
-            #   - my-company/my-app/1.0-SNAPSHOT/my-app.jar
-            #   - my-company/my-app/1.0-SNAPSHOT/my-app.pom
-            #   - my-company/my-app/1.0-SNAPSHOT/maven-metadata.xml
-            #   - my-company/my-app/maven-metadata.xml
-            #
-            # The last xml file does not have VERSION in URL because it contains
-            # information about all versions.
-            package_name, version = params[:path], nil
-          else
-            package_name, _, version = params[:path].rpartition('/')
-          end
-
+          package_name, _, version = params[:path].rpartition('/')
           package_params = {
             name: package_name,
             path: params[:path],
--- ee/spec/services/packages/maven/find_or_create_package_service_spec.rb
+++ ee/spec/services/packages/maven/find_or_create_package_service_spec.rb
@@ -5,13 +5,13 @@ describe Packages::Maven::FindOrCreatePackageService do
   let_it_be(:project) { create(:project) }
   let_it_be(:user) { create(:user) }
   let_it_be(:app_name) { 'my-app' }
-  let_it_be(:version) { '1.0-SNAPSHOT' }
   let_it_be(:path) { "my/company/app/#{app_name}" }
+  let_it_be(:version) { '1.0.0' }
   let_it_be(:path_with_version) { "#{path}/#{version}" }
   let_it_be(:params) do
     {
+      name: app_name,
       path: path_with_version,
-      name: path,
       version: version
     }
   end
@@ -22,6 +22,28 @@ describe Packages::Maven::FindOrCreatePackageService do
     context 'without any existing package' do
       it 'creates a package' do
         expect { subject }.to change { Packages::Package.count }.by(1)
+      end
+      it 'assigns a numerical version to a release package' do
+        expect(subject.version).to eq('1.0.0')
+      end
+    end
+
+    context 'when publishing a Snapshot' do
+      let_it_be(:snapshot_path) { "my/company/app/my-app/1.0.1-SNAPSHOT" }
+      let_it_be(:params) do
+        {
+          path: snapshot_path,
+          name: "my-app",
+          version: "1.0.1-SNAPSHOT"
+        }
+      end
+
+      it 'Sets the version value with the correct -SNAPSHOT extension' do
+        package_name, _, snapshot_version = snapshot_path.rpartition('/')
+        snapshot_package = described_class.new(project, user, params).execute
+
+        expect(snapshot_package.version).to eq(snapshot_version)
+        expect(snapshot_package.name).to eq(package_name)
       end
     end
 
