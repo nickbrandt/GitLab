c0d2af18f4e3ee2087322519a2a001877311a6fd
Lukas Eipert <leipert@gitlab.com>
2020-02-29 16:46:24 +0100

    Add fetch polyfill
    Fetch has been broken for a while in Internet Explorer 11. This adds a
    tiny fetch polyfill (500 bytes) in order to mitigate the issue, until we
    are officially drop Internet Explorer 11 support later this year.
    
    - https://gitlab.com/gitlab-org/gitlab/issues/25700
    - https://gitlab.com/gitlab-org/gitlab/issues/43662

--- app/assets/javascripts/commons/polyfills.js
+++ app/assets/javascripts/commons/polyfills.js
@@ -1,4 +1,13 @@
 // Browser polyfills
+
+/**
+ * Polyfill: fetch
+ * @what https://fetch.spec.whatwg.org/
+ * @why Because Apollo GraphQL client relies on fetch
+ * @browsers Internet Explorer 11
+ * @see https://caniuse.com/#feat=fetch
+ */
+import 'unfetch/polyfill/index';
 import 'formdata-polyfill';
 import './polyfills/custom_event';
 import './polyfills/element';
