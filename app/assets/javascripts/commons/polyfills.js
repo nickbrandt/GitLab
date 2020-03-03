046e75fd6cd472440bde4df63a90be8f3aa241d4
Lukas Eipert <leipert@gitlab.com>
2020-03-03 10:52:46 +0100

    Add documentation for polyfills
    When looking into our polyfills, having to look up _why_ we polyfill was
    a bit tedious, so let's add documentation on the why!
    
    This will make it way easier to remove outdated polyfills once we
    deprecate a browser.

--- app/assets/javascripts/commons/polyfills.js
+++ app/assets/javascripts/commons/polyfills.js
@@ -8,7 +8,17 @@
  * @see https://caniuse.com/#feat=fetch
  */
 import 'unfetch/polyfill/index';
+
+/**
+ * Polyfill: FormData APIs
+ * @what delete(), get(), getAll(), has(), set(), entries(), keys(), values(),
+ *    and support for for...of
+ * @why Because Apollo GraphQL client relies on fetch
+ * @browsers Internet Explorer 11, Edge < 18
+ * @see https://caniuse.com/#feat=mdn-api_formdata and subfeatures
+ */
 import 'formdata-polyfill';
+
 import './polyfills/custom_event';
 import './polyfills/element';
 import './polyfills/event';
