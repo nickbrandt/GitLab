6ac2542bf69078ca8fe8712698eca22949be61ee
Changzheng Liu <cliu@gitlab.com>
2020-03-06 02:46:36 +0000

    Set the default SSL cert path when invoking indexer

--- ee/spec/lib/gitlab/elastic/indexer_spec.rb
+++ ee/spec/lib/gitlab/elastic/indexer_spec.rb
@@ -269,11 +269,14 @@ describe Gitlab::Elastic::Indexer do
     end
   end
 
-  context 'when SSL env vars are not set' do
+  context 'when SSL env vars are not set explicitly' do
+    let(:ruby_cert_file) { OpenSSL::X509::DEFAULT_CERT_FILE }
+    let(:ruby_cert_dir) { OpenSSL::X509::DEFAULT_CERT_DIR }
+
     subject { envvars }
 
-    it 'they will not be passed down to child process' do
-      is_expected.not_to include('SSL_CERT_FILE', 'SSL_CERT_DIR')
+    it 'they will be set to default values determined by Ruby' do
+      is_expected.to include('SSL_CERT_FILE' => ruby_cert_file, 'SSL_CERT_DIR' => ruby_cert_dir)
     end
   end
 
