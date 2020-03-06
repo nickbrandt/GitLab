f1aea54b51526d41275bf1e0d50c1afb051eb7ce
Jan Provaznik <jprovaznik@gitlab.com>
2020-03-06 17:00:51 +0100

    List requirement in GraphQL API
    Allows listing requirements through GraphQL API and use basic
    filtering and sorting parameters.

---
+++ ee/app/graphql/resolvers/requirements_resolver.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class RequirementsResolver < BaseResolver
+    argument :iid, GraphQL::ID_TYPE,
+             required: false,
+             description: 'IID of the requirement, e.g., "1"'
+
+    argument :iids, [GraphQL::ID_TYPE],
+             required: false,
+             description: 'List of IIDs of requirements, e.g., [1, 2]'
+
+    argument :sort, Types::SortEnum,
+             required: false,
+             description: 'List requirements by sort order'
+
+    argument :state, Types::RequirementStateEnum,
+             required: false,
+             description: 'Filter requirements by state'
+
+    type Types::RequirementType, null: true
+
+    def resolve(**args)
+      # The project could have been loaded in batch by `BatchLoader`.
+      # At this point we need the `id` of the project to query for issues, so
+      # make sure it's loaded and not `nil` before continuing.
+      project = object.respond_to?(:sync) ? object.sync : object
+      return Requirement.none if project.nil?
+      return Requirement.none unless Feature.enabled?(:requirements_management, project)
+
+      args[:project_id] = project.id
+      args[:iids] ||= [args[:iid]].compact
+
+      RequirementsFinder.new(context[:current_user], args).execute
+    end
+  end
+end
