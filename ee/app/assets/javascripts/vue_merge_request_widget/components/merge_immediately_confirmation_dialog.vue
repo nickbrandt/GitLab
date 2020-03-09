14dd2e4b5bd4d81d4fd111861b1b52514d99568e
Jose Vargas <jvargas@gitlab.com>
2020-03-09 11:16:58 -0600

    Replace underscore/lodash in mr_widget
    This replaces all of the usage of underscore
    to lodash for all of the features of the mr_widget

--- ee/app/assets/javascripts/vue_merge_request_widget/components/merge_immediately_confirmation_dialog.vue
+++ ee/app/assets/javascripts/vue_merge_request_widget/components/merge_immediately_confirmation_dialog.vue
@@ -1,6 +1,6 @@
 <script>
 import { GlModal, GlButton } from '@gitlab/ui';
-import _ from 'underscore';
+import { escape as esc } from 'lodash';
 import { __, sprintf } from '~/locale';
 
 export default {
@@ -22,7 +22,7 @@ export default {
           "Merging immediately isn't recommended as it may negatively impact the existing merge train. Read the %{docsLinkStart}documentation%{docsLinkEnd} for more information.",
         ),
         {
-          docsLinkStart: `<a href="${_.escape(
+          docsLinkStart: `<a href="${esc(
             this.docsUrl,
           )}" target="_blank" rel="noopener noreferrer">`,
           docsLinkEnd: '</a>',
