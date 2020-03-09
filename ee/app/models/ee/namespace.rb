mehulagg/gitlab_test

Add feature flag to disable CI/CD for GitHub
Feature flag name: free_period_for_pull_mirroring

To disable free CI/CD for GitHub for private projects without a
paid subscription we need to turn off the feature flag.

Also adds more checks and specs for licensed features

ee/app/models/ee/namespace.rb
@@ -199,7 +199,7 @@ module EE
     end
 
     def feature_available_in_plan?(feature)
-      return true if ::License::ANY_PLAN_FEATURES.include?(feature)
+      return true if ::License.trial_feature_available?(feature)
 
       available_features = strong_memoize(:features_available_in_plan) do
         Hash.new do |h, f|

ee/app/models/license.rb
@@ -282,6 +282,12 @@ class License < ApplicationRecord
     def trial_ends_on
       Gitlab::CurrentSettings.license_trial_ends_on
     end
+
+    def trial_feature_available?(feature)
+      return false unless ::Feature.enabled?(:free_period_for_pull_mirroring, default_enabled: true)
+
+      ANY_PLAN_FEATURES.include?(feature)
+    end
   end
 
   def data_filename

ee/spec/models/license_spec.rb
@@ -711,6 +711,32 @@ describe License do
     end
   end
 
+  describe '#trial_feature_available?' do
+    subject { described_class.trial_feature_available?(feature) }
+
+    shared_examples 'CI CD trial features' do |status|
+      before do
+        stub_feature_flags(free_period_for_pull_mirroring: status)
+      end
+
+      License::ANY_PLAN_FEATURES.each do |feature_name|
+        context "with #{feature_name}" do
+          let(:feature) { feature_name }
+
+          it { is_expected.to eq(status) }
+        end
+      end
+    end
+
+    context 'with free_period_for_pull_mirroring enabled' do
+      it_behaves_like 'CI CD trial features', true
+    end
+
+    context 'with free_period_for_pull_mirroring disabled' do
+      it_behaves_like 'CI CD trial features', false
+    end
+  end
+
   def set_restrictions(opts)
     gl_license.restrictions = {
       active_user_count: opts[:restricted_user_count],

nickbrandt/GitLab

ee/app/models/ee/namespace.rb
@@ -199,7 +199,7 @@ module EE
     end
 
     def feature_available_in_plan?(feature)
-      return true if ::License::ANY_PLAN_FEATURES.include?(feature)
+      return true if ::License.promo_feature_available?(feature)
 
       available_features = strong_memoize(:features_available_in_plan) do
         Hash.new do |h, f|

ee/app/models/license.rb
@@ -284,6 +284,12 @@ class License < ApplicationRecord
     def trial_ends_on
       Gitlab::CurrentSettings.license_trial_ends_on
     end
+
+    def promo_feature_available?(feature)
+      return false unless ::Feature.enabled?(:free_period_for_pull_mirroring, default_enabled: true)
+
+      ANY_PLAN_FEATURES.include?(feature)
+    end
   end
 
   def data_filename

ee/spec/models/license_spec.rb
@@ -711,6 +711,32 @@ describe License do
     end
   end
 
+  describe '#promo_feature_available?' do
+    subject { described_class.promo_feature_available?(feature) }
+
+    shared_examples 'CI CD trial features' do |status|
+      before do
+        stub_feature_flags(free_period_for_pull_mirroring: status)
+      end
+
+      License::ANY_PLAN_FEATURES.each do |feature_name|
+        context "with #{feature_name}" do
+          let(:feature) { feature_name }
+
+          it { is_expected.to eq(status) }
+        end
+      end
+    end
+
+    context 'with free_period_for_pull_mirroring enabled' do
+      it_behaves_like 'CI CD trial features', true
+    end
+
+    context 'with free_period_for_pull_mirroring disabled' do
+      it_behaves_like 'CI CD trial features', false
+    end
+  end
+
   def set_restrictions(opts)
     gl_license.restrictions = {
       active_user_count: opts[:restricted_user_count],