mehulagg/gitlab_test

Extract code from NotificationRecipientService
- move builders to a separated folder
- add basic specs for default builder

app/models/member.rb
@@ -369,14 +369,12 @@ class Member < ApplicationRecord
     @notification_setting ||= user&.notification_settings_for(source)
   end
 
-  # rubocop: disable CodeReuse/ServiceClass
   def notifiable?(type, opts = {})
     # always notify when there isn't a user yet
     return true if user.blank?
 
-    NotificationRecipientService.notifiable?(user, type, notifiable_options.merge(opts))
+    NotificationRecipients::Service.notifiable?(user, type, notifiable_options.merge(opts))
   end
-  # rubocop: enable CodeReuse/ServiceClass
 
   # Find the user's group member with a highest access level
   def highest_group_member

app/services/notification_service.rb
@@ -108,7 +108,7 @@ class NotificationService
   #  * users with custom level checked with "reassign issue"
   #
   def reassigned_issue(issue, current_user, previous_assignees = [])
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::Service.build_recipients(
       issue,
       current_user,
       action: "reassign",
@@ -161,7 +161,7 @@ class NotificationService
   def push_to_merge_request(merge_request, current_user, new_commits: [], existing_commits: [])
     new_commits = new_commits.map { |c| { short_id: c.short_id, title: c.title } }
     existing_commits = existing_commits.map { |c| { short_id: c.short_id, title: c.title } }
-    recipients = NotificationRecipientService.build_recipients(merge_request, current_user, action: "push_to")
+    recipients = NotificationRecipients::Service.build_recipients(merge_request, current_user, action: "push_to")
 
     recipients.each do |recipient|
       mailer.send(:push_to_merge_request_email, recipient.user.id, merge_request.id, current_user.id, recipient.reason, new_commits: new_commits, existing_commits: existing_commits).deliver_later
@@ -197,7 +197,7 @@ class NotificationService
   #  * users with custom level checked with "reassign merge request"
   #
   def reassigned_merge_request(merge_request, current_user, previous_assignees = [])
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::Service.build_recipients(
       merge_request,
       current_user,
       action: "reassign",
@@ -260,7 +260,7 @@ class NotificationService
   end
 
   def resolve_all_discussions(merge_request, current_user)
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::Service.build_recipients(
       merge_request,
       current_user,
       action: "resolve_all_discussions")
@@ -291,7 +291,7 @@ class NotificationService
   def send_new_note_notifications(note)
     notify_method = "note_#{note.noteable_ability_name}_email".to_sym
 
-    recipients = NotificationRecipientService.build_new_note_recipients(note)
+    recipients = NotificationRecipients::Service.build_new_note_recipients(note)
     recipients.each do |recipient|
       mailer.send(notify_method, recipient.user.id, note.id, recipient.reason).deliver_later
     end
@@ -299,7 +299,7 @@ class NotificationService
 
   # Notify users when a new release is created
   def send_new_release_notifications(release)
-    recipients = NotificationRecipientService.build_new_release_recipients(release)
+    recipients = NotificationRecipients::Service.build_new_release_recipients(release)
 
     recipients.each do |recipient|
       mailer.new_release_email(recipient.user.id, release, recipient.reason).deliver_later
@@ -413,7 +413,7 @@ class NotificationService
   end
 
   def issue_moved(issue, new_issue, current_user)
-    recipients = NotificationRecipientService.build_recipients(issue, current_user, action: 'moved')
+    recipients = NotificationRecipients::Service.build_recipients(issue, current_user, action: 'moved')
 
     recipients.map do |recipient|
       email = mailer.issue_moved_email(recipient.user, issue, new_issue, current_user, recipient.reason)
@@ -490,7 +490,7 @@ class NotificationService
   end
 
   def issue_due(issue)
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::Service.build_recipients(
       issue,
       issue.author,
       action: 'due',
@@ -526,7 +526,7 @@ class NotificationService
   protected
 
   def new_resource_email(target, method)
-    recipients = NotificationRecipientService.build_recipients(target, target.author, action: "new")
+    recipients = NotificationRecipients::Service.build_recipients(target, target.author, action: "new")
 
     recipients.each do |recipient|
       mailer.send(method, recipient.user.id, target.id, recipient.reason).deliver_later
@@ -534,7 +534,7 @@ class NotificationService
   end
 
   def new_mentions_in_resource_email(target, new_mentioned_users, current_user, method)
-    recipients = NotificationRecipientService.build_recipients(target, current_user, action: "new")
+    recipients = NotificationRecipients::Service.build_recipients(target, current_user, action: "new")
     recipients = recipients.select {|r| new_mentioned_users.include?(r.user) }
 
     recipients.each do |recipient|
@@ -545,7 +545,7 @@ class NotificationService
   def close_resource_email(target, current_user, method, skip_current_user: true, closed_via: nil)
     action = method == :merged_merge_request_email ? "merge" : "close"
 
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::Service.build_recipients(
       target,
       current_user,
       action: action,
@@ -573,7 +573,7 @@ class NotificationService
   end
 
   def removed_milestone_resource_email(target, current_user, method)
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::Service.build_recipients(
       target,
       current_user,
       action: 'removed_milestone'
@@ -585,7 +585,7 @@ class NotificationService
   end
 
   def changed_milestone_resource_email(target, milestone, current_user, method)
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::Service.build_recipients(
       target,
       current_user,
       action: 'changed_milestone'
@@ -597,7 +597,7 @@ class NotificationService
   end
 
   def reopen_resource_email(target, current_user, method, status)
-    recipients = NotificationRecipientService.build_recipients(target, current_user, action: "reopen")
+    recipients = NotificationRecipients::Service.build_recipients(target, current_user, action: "reopen")
 
     recipients.each do |recipient|
       mailer.send(method, recipient.user.id, target.id, status, current_user.id, recipient.reason).deliver_later
@@ -605,7 +605,7 @@ class NotificationService
   end
 
   def merge_request_unmergeable_email(merge_request)
-    recipients = NotificationRecipientService.build_merge_request_unmergeable_recipients(merge_request)
+    recipients = NotificationRecipients::Service.build_merge_request_unmergeable_recipients(merge_request)
 
     recipients.each do |recipient|
       mailer.merge_request_unmergeable_email(recipient.user.id, merge_request.id).deliver_later
@@ -619,15 +619,15 @@ class NotificationService
   private
 
   def project_maintainers_recipients(target, action:)
-    NotificationRecipientService.build_project_maintainers_recipients(target, action: action)
+    NotificationRecipients::Service.build_project_maintainers_recipients(target, action: action)
   end
 
   def notifiable?(*args)
-    NotificationRecipientService.notifiable?(*args)
+    NotificationRecipients::Service.notifiable?(*args)
   end
 
   def notifiable_users(*args)
-    NotificationRecipientService.notifiable_users(*args)
+    NotificationRecipients::Service.notifiable_users(*args)
   end
 
   def deliver_access_request_email(recipient, member)

ee/app/services/ee/notification_service.rb
@@ -88,7 +88,7 @@ module EE
     end
 
     def send_new_review_notification(review)
-      recipients = ::NotificationRecipientService.build_new_review_recipients(review)
+      recipients = ::NotificationRecipients::Service.build_new_review_recipients(review)
 
       recipients.each do |recipient|
         mailer.new_review_email(recipient.user.id, review.id).deliver_later
@@ -102,7 +102,7 @@ module EE
     end
 
     def approve_mr_email(merge_request, project, current_user)
-      recipients = ::NotificationRecipientService.build_recipients(merge_request, current_user, action: 'approve')
+      recipients = ::NotificationRecipients::Service.build_recipients(merge_request, current_user, action: 'approve')
 
       recipients.each do |recipient|
         mailer.approved_merge_request_email(recipient.user.id, merge_request.id, current_user.id).deliver_later
@@ -110,7 +110,7 @@ module EE
     end
 
     def unapprove_mr_email(merge_request, project, current_user)
-      recipients = ::NotificationRecipientService.build_recipients(merge_request, current_user, action: 'unapprove')
+      recipients = ::NotificationRecipients::Service.build_recipients(merge_request, current_user, action: 'unapprove')
 
       recipients.each do |recipient|
         mailer.unapproved_merge_request_email(recipient.user.id, merge_request.id, current_user.id).deliver_later
@@ -135,7 +135,7 @@ module EE
     def epic_status_change_email(target, current_user, status)
       action = status == 'reopened' ? 'reopen' : 'close'
 
-      recipients = ::NotificationRecipientService.build_recipients(
+      recipients = ::NotificationRecipients::Service.build_recipients(
         target,
         current_user,
         action: action

spec/services/notification_recipients/builder/default_spec.rb
@@ -1,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe NotificationRecipients::Builder::Default do
+  describe '#build!' do
+    let_it_be(:group)   { create(:group, :public) }
+    let_it_be(:project) { create(:project, :public, group: group).tap { |p| p.add_developer(project_watcher) } }
+    let_it_be(:issue)   { create(:issue, project: project) }
+
+    let_it_be(:current_user)    { create(:user) }
+    let_it_be(:other_user)      { create(:user) }
+    let_it_be(:participant)     { create(:user) }
+    let_it_be(:group_watcher)   { create(:user) }
+    let_it_be(:project_watcher) { create(:user) }
+
+    let_it_be(:notification_setting) { create(:notification_setting, source: project, user: project_watcher, level: 2) }
+    let_it_be(:notification_setting) { create(:notification_setting, source: group, user: group_watcher, level: 2) }
+
+    subject { described_class.new(issue, current_user, action: :new).tap { |s| s.build! } }
+
+    context 'participants and project watchers' do
+      before do
+        expect(issue).to receive(:participants).and_return([participant, current_user])
+      end
+
+      it 'adds all participants and watchers' do
+        expect(subject.recipients.map(&:user)).to include(participant, project_watcher, group_watcher)
+        expect(subject.recipients.map(&:user)).not_to include(other_user)
+      end
+    end
+
+    context 'subscribers' do
+      it 'adds all subscribers' do
+        subscriber = create(:user)
+        non_subscriber = create(:user)
+        create(:subscription, project: project, user: subscriber, subscribable: issue, subscribed: true)
+        create(:subscription, project: project, user: non_subscriber, subscribable: issue, subscribed: false)
+
+        expect(subject.recipients.map(&:user)).to include(subscriber)
+      end
+    end
+  end
+end

spec/services/notification_service_spec.rb
@@ -710,7 +710,7 @@ describe NotificationService, :mailer do
         user_3 = create(:user)
         recipient_1 = NotificationRecipient.new(user_1, :custom, custom_action: :new_release)
         recipient_2 = NotificationRecipient.new(user_2, :custom, custom_action: :new_release)
-        allow(NotificationRecipientService).to receive(:build_new_release_recipients).and_return([recipient_1, recipient_2])
+        allow(NotificationRecipients::Service).to receive(:build_new_release_recipients).and_return([recipient_1, recipient_2])
 
         release
 

nickbrandt/GitLab

app/models/member.rb
@@ -374,7 +374,7 @@ class Member < ApplicationRecord
     # always notify when there isn't a user yet
     return true if user.blank?
 
-    NotificationRecipientService.notifiable?(user, type, notifiable_options.merge(opts))
+    NotificationRecipients::BuildService.notifiable?(user, type, notifiable_options.merge(opts))
   end
   # rubocop: enable CodeReuse/ServiceClass
 

app/services/notification_service.rb
@@ -108,7 +108,7 @@ class NotificationService
   #  * users with custom level checked with "reassign issue"
   #
   def reassigned_issue(issue, current_user, previous_assignees = [])
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::BuildService.build_recipients(
       issue,
       current_user,
       action: "reassign",
@@ -161,7 +161,7 @@ class NotificationService
   def push_to_merge_request(merge_request, current_user, new_commits: [], existing_commits: [])
     new_commits = new_commits.map { |c| { short_id: c.short_id, title: c.title } }
     existing_commits = existing_commits.map { |c| { short_id: c.short_id, title: c.title } }
-    recipients = NotificationRecipientService.build_recipients(merge_request, current_user, action: "push_to")
+    recipients = NotificationRecipients::BuildService.build_recipients(merge_request, current_user, action: "push_to")
 
     recipients.each do |recipient|
       mailer.send(:push_to_merge_request_email, recipient.user.id, merge_request.id, current_user.id, recipient.reason, new_commits: new_commits, existing_commits: existing_commits).deliver_later
@@ -197,7 +197,7 @@ class NotificationService
   #  * users with custom level checked with "reassign merge request"
   #
   def reassigned_merge_request(merge_request, current_user, previous_assignees = [])
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::BuildService.build_recipients(
       merge_request,
       current_user,
       action: "reassign",
@@ -260,7 +260,7 @@ class NotificationService
   end
 
   def resolve_all_discussions(merge_request, current_user)
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::BuildService.build_recipients(
       merge_request,
       current_user,
       action: "resolve_all_discussions")
@@ -291,7 +291,7 @@ class NotificationService
   def send_new_note_notifications(note)
     notify_method = "note_#{note.noteable_ability_name}_email".to_sym
 
-    recipients = NotificationRecipientService.build_new_note_recipients(note)
+    recipients = NotificationRecipients::BuildService.build_new_note_recipients(note)
     recipients.each do |recipient|
       mailer.send(notify_method, recipient.user.id, note.id, recipient.reason).deliver_later
     end
@@ -299,7 +299,7 @@ class NotificationService
 
   # Notify users when a new release is created
   def send_new_release_notifications(release)
-    recipients = NotificationRecipientService.build_new_release_recipients(release)
+    recipients = NotificationRecipients::BuildService.build_new_release_recipients(release)
 
     recipients.each do |recipient|
       mailer.new_release_email(recipient.user.id, release, recipient.reason).deliver_later
@@ -413,7 +413,7 @@ class NotificationService
   end
 
   def issue_moved(issue, new_issue, current_user)
-    recipients = NotificationRecipientService.build_recipients(issue, current_user, action: 'moved')
+    recipients = NotificationRecipients::BuildService.build_recipients(issue, current_user, action: 'moved')
 
     recipients.map do |recipient|
       email = mailer.issue_moved_email(recipient.user, issue, new_issue, current_user, recipient.reason)
@@ -490,7 +490,7 @@ class NotificationService
   end
 
   def issue_due(issue)
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::BuildService.build_recipients(
       issue,
       issue.author,
       action: 'due',
@@ -526,7 +526,7 @@ class NotificationService
   protected
 
   def new_resource_email(target, method)
-    recipients = NotificationRecipientService.build_recipients(target, target.author, action: "new")
+    recipients = NotificationRecipients::BuildService.build_recipients(target, target.author, action: "new")
 
     recipients.each do |recipient|
       mailer.send(method, recipient.user.id, target.id, recipient.reason).deliver_later
@@ -534,7 +534,7 @@ class NotificationService
   end
 
   def new_mentions_in_resource_email(target, new_mentioned_users, current_user, method)
-    recipients = NotificationRecipientService.build_recipients(target, current_user, action: "new")
+    recipients = NotificationRecipients::BuildService.build_recipients(target, current_user, action: "new")
     recipients = recipients.select {|r| new_mentioned_users.include?(r.user) }
 
     recipients.each do |recipient|
@@ -545,7 +545,7 @@ class NotificationService
   def close_resource_email(target, current_user, method, skip_current_user: true, closed_via: nil)
     action = method == :merged_merge_request_email ? "merge" : "close"
 
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::BuildService.build_recipients(
       target,
       current_user,
       action: action,
@@ -573,7 +573,7 @@ class NotificationService
   end
 
   def removed_milestone_resource_email(target, current_user, method)
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::BuildService.build_recipients(
       target,
       current_user,
       action: 'removed_milestone'
@@ -585,7 +585,7 @@ class NotificationService
   end
 
   def changed_milestone_resource_email(target, milestone, current_user, method)
-    recipients = NotificationRecipientService.build_recipients(
+    recipients = NotificationRecipients::BuildService.build_recipients(
       target,
       current_user,
       action: 'changed_milestone'
@@ -597,7 +597,7 @@ class NotificationService
   end
 
   def reopen_resource_email(target, current_user, method, status)
-    recipients = NotificationRecipientService.build_recipients(target, current_user, action: "reopen")
+    recipients = NotificationRecipients::BuildService.build_recipients(target, current_user, action: "reopen")
 
     recipients.each do |recipient|
       mailer.send(method, recipient.user.id, target.id, status, current_user.id, recipient.reason).deliver_later
@@ -605,7 +605,7 @@ class NotificationService
   end
 
   def merge_request_unmergeable_email(merge_request)
-    recipients = NotificationRecipientService.build_merge_request_unmergeable_recipients(merge_request)
+    recipients = NotificationRecipients::BuildService.build_merge_request_unmergeable_recipients(merge_request)
 
     recipients.each do |recipient|
       mailer.merge_request_unmergeable_email(recipient.user.id, merge_request.id).deliver_later
@@ -619,15 +619,15 @@ class NotificationService
   private
 
   def project_maintainers_recipients(target, action:)
-    NotificationRecipientService.build_project_maintainers_recipients(target, action: action)
+    NotificationRecipients::BuildService.build_project_maintainers_recipients(target, action: action)
   end
 
   def notifiable?(*args)
-    NotificationRecipientService.notifiable?(*args)
+    NotificationRecipients::BuildService.notifiable?(*args)
   end
 
   def notifiable_users(*args)
-    NotificationRecipientService.notifiable_users(*args)
+    NotificationRecipients::BuildService.notifiable_users(*args)
   end
 
   def deliver_access_request_email(recipient, member)

ee/app/services/ee/notification_service.rb
@@ -88,7 +88,7 @@ module EE
     end
 
     def send_new_review_notification(review)
-      recipients = ::NotificationRecipientService.build_new_review_recipients(review)
+      recipients = ::NotificationRecipients::BuildService.build_new_review_recipients(review)
 
       recipients.each do |recipient|
         mailer.new_review_email(recipient.user.id, review.id).deliver_later
@@ -102,7 +102,7 @@ module EE
     end
 
     def approve_mr_email(merge_request, project, current_user)
-      recipients = ::NotificationRecipientService.build_recipients(merge_request, current_user, action: 'approve')
+      recipients = ::NotificationRecipients::BuildService.build_recipients(merge_request, current_user, action: 'approve')
 
       recipients.each do |recipient|
         mailer.approved_merge_request_email(recipient.user.id, merge_request.id, current_user.id).deliver_later
@@ -110,7 +110,7 @@ module EE
     end
 
     def unapprove_mr_email(merge_request, project, current_user)
-      recipients = ::NotificationRecipientService.build_recipients(merge_request, current_user, action: 'unapprove')
+      recipients = ::NotificationRecipients::BuildService.build_recipients(merge_request, current_user, action: 'unapprove')
 
       recipients.each do |recipient|
         mailer.unapproved_merge_request_email(recipient.user.id, merge_request.id, current_user.id).deliver_later
@@ -135,7 +135,7 @@ module EE
     def epic_status_change_email(target, current_user, status)
       action = status == 'reopened' ? 'reopen' : 'close'
 
-      recipients = ::NotificationRecipientService.build_recipients(
+      recipients = ::NotificationRecipients::BuildService.build_recipients(
         target,
         current_user,
         action: action

spec/services/notification_recipients/builder/default_spec.rb
@@ -1,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe NotificationRecipients::Builder::Default do
+  describe '#build!' do
+    let_it_be(:group)   { create(:group, :public) }
+    let_it_be(:project) { create(:project, :public, group: group).tap { |p| p.add_developer(project_watcher) } }
+    let_it_be(:issue)   { create(:issue, project: project) }
+
+    let_it_be(:current_user)    { create(:user) }
+    let_it_be(:other_user)      { create(:user) }
+    let_it_be(:participant)     { create(:user) }
+    let_it_be(:group_watcher)   { create(:user) }
+    let_it_be(:project_watcher) { create(:user) }
+
+    let_it_be(:notification_setting_project_w) { create(:notification_setting, source: project, user: project_watcher, level: 2) }
+    let_it_be(:notification_setting_group_w) { create(:notification_setting, source: group, user: group_watcher, level: 2) }
+
+    subject { described_class.new(issue, current_user, action: :new).tap { |s| s.build! } }
+
+    context 'participants and project watchers' do
+      before do
+        expect(issue).to receive(:participants).and_return([participant, current_user])
+      end
+
+      it 'adds all participants and watchers' do
+        expect(subject.recipients.map(&:user)).to include(participant, project_watcher, group_watcher)
+        expect(subject.recipients.map(&:user)).not_to include(other_user)
+      end
+    end
+
+    context 'subscribers' do
+      it 'adds all subscribers' do
+        subscriber = create(:user)
+        non_subscriber = create(:user)
+        create(:subscription, project: project, user: subscriber, subscribable: issue, subscribed: true)
+        create(:subscription, project: project, user: non_subscriber, subscribable: issue, subscribed: false)
+
+        expect(subject.recipients.map(&:user)).to include(subscriber)
+      end
+    end
+  end
+end

spec/services/notification_service_spec.rb
@@ -710,7 +710,7 @@ describe NotificationService, :mailer do
         user_3 = create(:user)
         recipient_1 = NotificationRecipient.new(user_1, :custom, custom_action: :new_release)
         recipient_2 = NotificationRecipient.new(user_2, :custom, custom_action: :new_release)
-        allow(NotificationRecipientService).to receive(:build_new_release_recipients).and_return([recipient_1, recipient_2])
+        allow(NotificationRecipients::BuildService).to receive(:build_new_release_recipients).and_return([recipient_1, recipient_2])
 
         release
 