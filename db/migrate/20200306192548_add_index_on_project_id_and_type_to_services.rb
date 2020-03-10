a3124c9201eb18aea399c1ece5b69c6ef8c696db
Doug Stull <dstull@gitlab.com>
2020-03-10 10:14:25 -0400

    Adjust indexes as per feedback
    - obsolete one due to being superseeded
    - redo one due to timing benefits with index

--- db/migrate/20200306192548_add_index_on_project_id_and_type_to_services.rb
+++ db/migrate/20200306192548_add_index_on_project_id_and_type_to_services.rb
@@ -4,14 +4,19 @@ class AddIndexOnProjectIdAndTypeToServices < ActiveRecord::Migration[6.0]
   include Gitlab::Database::MigrationHelpers
 
   DOWNTIME = false
+  INDEX_NAME = 'index_services_on_project_id'
 
   disable_ddl_transaction!
 
   def up
     add_concurrent_index :services, [:project_id, :type]
+
+    remove_concurrent_index_by_name :services, INDEX_NAME
   end
 
   def down
     remove_concurrent_index :services, [:project_id, :type]
+
+    add_concurrent_index :services, :project_id, name: INDEX_NAME
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -3471,9 +3471,8 @@ ActiveRecord::Schema.define(version: 2020_03_11_165635) do
     t.index ["created_at", "id"], name: "index_projects_api_vis20_created_at", where: "(visibility_level = 20)"
     t.index ["created_at", "id"], name: "index_projects_on_created_at_and_id"
     t.index ["creator_id", "created_at"], name: "index_projects_on_mirror_creator_id_created_at", where: "((mirror = true) AND (mirror_trigger_builds = true))"
-    t.index ["creator_id"], name: "index_projects_on_creator_id"
+    t.index ["creator_id", "created_at"], name: "index_projects_on_creator_id_and_created_at"
     t.index ["description"], name: "index_projects_on_description_trigram", opclass: :gin_trgm_ops, using: :gin
-    t.index ["id", "creator_id"], name: "index_projects_on_id_and_creator_id"
     t.index ["id", "repository_storage", "last_repository_updated_at"], name: "idx_projects_on_repository_storage_last_repository_updated_at"
     t.index ["id"], name: "index_on_id_partial_with_legacy_storage", where: "((storage_version < 2) OR (storage_version IS NULL))"
     t.index ["id"], name: "index_projects_on_id_partial_for_visibility", unique: true, where: "(visibility_level = ANY (ARRAY[10, 20]))"
