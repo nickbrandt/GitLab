9082e9103ab50768cead8c80636bc26f77e52257
Krasimir Angelov <kangelov@gitlab.com>
2020-03-12 22:31:25 +1300

    Fix lookup of namespace by pages host
    Ensure only top level namespaces are returned

--- spec/models/namespace_spec.rb
+++ spec/models/namespace_spec.rb
@@ -201,6 +201,19 @@ describe Namespace do
       expect(described_class.find_by_pages_host(host)).to eq(namespace)
     end
 
+    it 'finds correct namespace even if there is sub-namespace with same path' do
+      group = create(:group, path: 'pages')
+      subgroup = create(:group, :nested, path: 'pages')
+      host = "pages.#{Settings.pages.host.upcase}"
+
+      # This is to ensure the method does the right thing
+      # becasue there is no way to be 100% sure which exact
+      # group will be returned by the database
+      expect(described_class).to receive(:where).with(parent_id: nil).and_call_original
+
+      expect(described_class.find_by_pages_host(host)).to eq(group)
+    end
+
     it "returns no result if the provided host is not subdomain of the Pages host" do
       create(:namespace, name: 'namespace.io')
       host = "namespace.io"
