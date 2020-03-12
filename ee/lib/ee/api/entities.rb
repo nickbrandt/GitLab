0b9856d0047dd6f5c324c5d89175e8c151747c31
Ethan Urie <eurie@gitlab.com>
2020-03-12 10:33:07 +0000

    Expose `plan` and `trial` in the `/users` endpoint
    `plan` is hooked up to the models but trial is not yet.

--- ee/lib/ee/api/entities.rb
+++ ee/lib/ee/api/entities.rb
@@ -539,6 +539,20 @@ module EE
           end
         end
       end
+
+      module UserDetailsWithAdmin
+        extend ActiveSupport::Concern
+
+        prepended do
+          expose :plan do |user|
+            user.namespace.try(:gitlab_subscription)&.plan_name
+          end
+
+          expose :trial do |user|
+            user.namespace.try(:trial?)
+          end
+        end
+      end
     end
   end
 end
