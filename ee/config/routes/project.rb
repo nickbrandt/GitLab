479f03cffd72a96c92d3a16bcb65b150bee18980
Jan Provaznik <jprovaznik@gitlab.com>
2020-03-13 11:46:29 +0100

    Add requirements index action
    This action will be used by FE to setup Vue objects for
    Requirements UI.

--- ee/config/routes/project.rb
+++ ee/config/routes/project.rb
@@ -11,6 +11,7 @@ constraints(::Constraints::ProjectUrlConstrainer.new) do
       # Begin of the /-/ scope.
       # Use this scope for all new project routes.
       scope '-' do
+        resources :requirements, only: [:index]
         resources :packages, only: [:index, :show, :destroy], module: :packages
         resources :package_files, only: [], module: :packages do
           member do
