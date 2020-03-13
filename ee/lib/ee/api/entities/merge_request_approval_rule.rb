b02b3b14468d57495b2ef543f209ed0026a1dfdc
Rajendra Kadam <rajendrakadam249@gmail.com>
2020-03-13 19:28:30 +0530

    Add source rule class inside super class

--- ee/lib/ee/api/entities/merge_request_approval_rule.rb
+++ ee/lib/ee/api/entities/merge_request_approval_rule.rb
@@ -4,6 +4,10 @@ module EE
   module API
     module Entities
       class MergeRequestApprovalRule < ApprovalRule
+        class SourceRule < Grape::Entity
+          expose :approvals_required
+        end
+
         expose :source_rule, using: MergeRequestApprovalRule::SourceRule
       end
     end
