bb008694cb4228d4e136a9a7f721eb0ed0d7fa13
Craig Smith <5344211-craigmsmith@users.noreply.gitlab.com>
2020-03-13 19:06:10 +1100

    Add scan url to reports endpoint

--- ee/app/serializers/vulnerabilities/occurrence_reports_comparer_entity.rb
+++ ee/app/serializers/vulnerabilities/occurrence_reports_comparer_entity.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class Vulnerabilities::OccurrenceReportsComparerEntity < Grape::Entity
+  include RequestAwareEntity
+
   expose :base_report_created_at
   expose :base_report_out_of_date
   expose :head_report_created_at
   expose :added, using: Vulnerabilities::OccurrenceEntity
   expose :fixed, using: Vulnerabilities::OccurrenceEntity
   expose :existing, using: Vulnerabilities::OccurrenceEntity
-  expose :scans
+  expose :scans do |instance|
+    instance.security_scans.map do |scan|
+      { 
+        'url_count': scan.scanned_resources_count,
+        'scan_url': namespace_project_job_url(scan.build.project.namespace, scan.build.project, scan.build)
+      }
+    end
+  end
 end
