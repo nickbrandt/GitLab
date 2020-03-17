e605e6f02fe8beed1ae5407c8ce4446fe8bc3872
Heinrich Lee Yu <heinrich@gitlab.com>
2020-03-17 18:55:15 +0800

    Fix counting of pending AJAX requests
    @rails/ujs uses native XHR rather than jQuery AJAX. Thus we add event
    listeners to count pending Rails UJS requests. This uses the same
    methodology as our request counting for axios.

--- app/assets/javascripts/lib/utils/rails_ujs.js
+++ app/assets/javascripts/lib/utils/rails_ujs.js
@@ -5,6 +5,16 @@ export const initRails = () => {
   // eslint-disable-next-line no-underscore-dangle
   if (!window._rails_loaded) {
     Rails.start();
+
+    // Count XHR requests for tests. See spec/support/helpers/wait_for_requests.rb
+    window.pendingRailsUJSRequests = 0;
+    document.body.addEventListener('ajax:complete', () => {
+      window.pendingRailsUJSRequests -= 1;
+    });
+
+    document.body.addEventListener('ajax:beforeSend', () => {
+      window.pendingRailsUJSRequests += 1;
+    });
   }
 };
 
