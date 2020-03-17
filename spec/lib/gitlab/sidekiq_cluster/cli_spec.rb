610e611ce84296239c7add8f9d8a04a62c2e4b0f
Sean McGivern <sean@gitlab.com>
2020-03-17 14:00:44 +0000

    Fix spec for fast_spec_helper
    `worker_queues` has an argument that defaults to `Rails.root`, which
    isn't available with `fast_spec_helper`, so stub it instead.

--- spec/lib/gitlab/sidekiq_cluster/cli_spec.rb
+++ spec/lib/gitlab/sidekiq_cluster/cli_spec.rb
@@ -36,11 +36,13 @@ describe Gitlab::SidekiqCluster::CLI do
       end
 
       it 'allows the special * selector' do
+        worker_queues = %w(foo bar baz)
+
+        expect(Gitlab::SidekiqConfig::CliMethods)
+          .to receive(:worker_queues).and_return(worker_queues)
+
         expect(Gitlab::SidekiqCluster)
-          .to receive(:start).with(
-            [Gitlab::SidekiqConfig::CliMethods.worker_queues],
-            default_options
-          )
+          .to receive(:start).with([worker_queues], default_options)
 
         cli.run(%w(*))
       end
@@ -161,11 +163,13 @@ describe Gitlab::SidekiqCluster::CLI do
         end
 
         it 'allows the special * selector' do
+          worker_queues = %w(foo bar baz)
+
+          expect(Gitlab::SidekiqConfig::CliMethods)
+            .to receive(:worker_queues).and_return(worker_queues)
+
           expect(Gitlab::SidekiqCluster)
-            .to receive(:start).with(
-              [Gitlab::SidekiqConfig::CliMethods.worker_queues],
-              default_options
-            )
+            .to receive(:start).with([worker_queues], default_options)
 
           cli.run(%w(--experimental-queue-selector *))
         end
