70e9450ecaf97c0b6a934f71ef6d90511966377e
peterhegman <phegman@gitlab.com>
2020-03-18 15:30:57 -0700

    Stub `Gitlab.com?` to return `true`
    The employee badge is only a gitlab.com feature

--- spec/views/profiles/show.html.haml_spec.rb
+++ spec/views/profiles/show.html.haml_spec.rb
@@ -20,40 +20,46 @@ describe 'profiles/show' do
     end
   end
 
-  context 'when `:gitlab_employee_badge` feature flag is enabled' do
-    context 'and when user has an `@gitlab.com` email address' do
-      let(:user) { create(:user, email: 'test@gitlab.com') }
-
-      it 'displays the organization field as `readonly` with a `value` of `GitLab`' do
-        render
-
-        expect(rendered).to have_selector('#user_organization[readonly][value="GitLab"]')
-      end
-    end
-
-    context 'and when a user does not have an `@gitlab.com` email' do
-      let(:user) { create(:user, email: 'test@example.com') }
-
-      it 'displays an editable organization field' do
-        render
-
-        expect(rendered).to have_selector('#user_organization:not([readonly]):not([value="GitLab"])')
-      end
-    end
-  end
-
-  context 'when `:gitlab_employee_badge` feature flag is disabled' do
+  context 'gitlab.com organization field' do
     before do
-      stub_feature_flags(gitlab_employee_badge: false)
+      allow(Gitlab).to receive(:com?).and_return(true)
     end
 
-    context 'and when a user has an `@gitlab.com` email' do
-      let(:user) { create(:user, email: 'test@gitlab.com') }
+    context 'when `:gitlab_employee_badge` feature flag is enabled' do
+      context 'and when user has an `@gitlab.com` email address' do
+        let(:user) { create(:user, email: 'test@gitlab.com') }
 
-      it 'displays an editable organization field' do
-        render
+        it 'displays the organization field as `readonly` with a `value` of `GitLab`' do
+          render
 
-        expect(rendered).to have_selector('#user_organization:not([readonly]):not([value="GitLab"])')
+          expect(rendered).to have_selector('#user_organization[readonly][value="GitLab"]')
+        end
+      end
+
+      context 'and when a user does not have an `@gitlab.com` email' do
+        let(:user) { create(:user, email: 'test@example.com') }
+
+        it 'displays an editable organization field' do
+          render
+
+          expect(rendered).to have_selector('#user_organization:not([readonly]):not([value="GitLab"])')
+        end
+      end
+    end
+
+    context 'when `:gitlab_employee_badge` feature flag is disabled' do
+      before do
+        stub_feature_flags(gitlab_employee_badge: false)
+      end
+
+      context 'and when a user has an `@gitlab.com` email' do
+        let(:user) { create(:user, email: 'test@gitlab.com') }
+
+        it 'displays an editable organization field' do
+          render
+
+          expect(rendered).to have_selector('#user_organization:not([readonly]):not([value="GitLab"])')
+        end
       end
     end
   end
