d291afa417d5a612569aafc21ffd7e2a96261651
Alex Buijs <abuijs@gitlab.com>
2020-03-19 10:34:40 +0100

    Optimize query for usage ping
    By using batch counting

--- ee/lib/ee/gitlab/usage_data.rb
+++ ee/lib/ee/gitlab/usage_data.rb
@@ -153,7 +153,7 @@ module EE
                                          merge_requests_with_optional_codeowners: distinct_count(::ApprovalMergeRequestRule.code_owner_approval_optional, :merge_request_id),
                                          merge_requests_with_required_codeowners: distinct_count(::ApprovalMergeRequestRule.code_owner_approval_required, :merge_request_id),
                                          projects_mirrored_with_pipelines_enabled: count(::Project.mirrored_with_enabled_pipelines),
-                                         projects_reporting_ci_cd_back_to_github: count(::GithubService.without_defaults.active, batch: false),
+                                         projects_reporting_ci_cd_back_to_github: count(::GithubService.without_defaults.active),
                                          projects_with_packages: count(::Packages::Package.select('distinct project_id'), batch: false),
                                          projects_with_prometheus_alerts: count(PrometheusAlert.distinct_projects, batch: false),
                                          projects_with_tracing_enabled: count(ProjectTracingSetting, batch: false),
