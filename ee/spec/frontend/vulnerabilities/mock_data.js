d59eb9c9a9228de5f308a11860f3daf63f432cb1
Sam Beckham <sbeckham@gitlab.com>
2020-03-23 18:30:13 +0000

    Adds a remediation badge on vuln list
    Adds, translates, and tests a remediation badge on the vulnerability
    list component.

--- ee/spec/frontend/vulnerabilities/mock_data.js
+++ ee/spec/frontend/vulnerabilities/mock_data.js
@@ -1,15 +1,16 @@
-// eslint-disable-next-line import/prefer-default-export
-export const vulnerabilities = [
+export const generateVulnerabilities = () => [
   {
     id: 'id_0',
-    title: 'Vuln1',
+    title: 'Vulnerability 1',
     severity: 'critical',
     state: 'dismissed',
   },
   {
     id: 'id_1',
-    title: 'Vuln2',
+    title: 'Vulnerability 2',
     severity: 'high',
     state: 'opened',
   },
 ];
+
+export const vulnerabilities = generateVulnerabilities();
