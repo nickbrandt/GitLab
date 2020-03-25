6a15d17c155842ca133e2cb37fd76e6a42480d6d
allison.browne <abrowne@gitlab.com>
2020-03-25 15:02:25 -0400

    Add partial index on pkey to status_page_settings
    Adding a partial index will lower the cost of joining
    status_page_settings on project_id with the where clause
    `enabled = true`.

---
+++ db/migrate/20200325182619_add_partial_index_on_enabled_to_status_page_settings.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class AddPartialIndexOnEnabledToStatusPageSettings < ActiveRecord::Migration[6.0]
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  disable_ddl_transaction!
+
+  INDEX_NAME = 'status_page_settings_project_id_enabled_eq_true_partial'
+
+  def up
+    add_concurrent_index :status_page_settings, :project_id, name: INDEX_NAME, where: "enabled = true"
+  end
+
+  def down
+    remove_concurrent_index :status_page_settings, INDEX_NAME
+  end
+end
