5becd865c1aa9da504e5917858f597fcb4481726
Jarka Košanová <jarka@gitlab.com>
2020-03-25 14:42:54 +0100

    Create basic functions for OpenProjectService
    - associate with data fields
    - add specs

--- app/models/project_services/open_project_tracker_data.rb
+++ app/models/project_services/open_project_tracker_data.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class OpenProjectTrackerData < ApplicationRecord
+  include Services::DataFields
+
   # When the Open Project is fresh installed, the default closed status id is "13" based on current version: v8.
   DEFAULT_CLOSED_STATUS_ID = "13"
-
-  include Services::DataFields
 
   attr_encrypted :url, encryption_options
   attr_encrypted :api_url, encryption_options
