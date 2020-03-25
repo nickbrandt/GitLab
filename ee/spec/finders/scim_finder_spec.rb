mehulagg/gitlab_test

Add support for Okta as SCIM provider
Minor adjustments were necessary to support Okta SCIM payloads.
The SCIM spec gives enough leeway that providers can differ slightly
in the payload format. These changes allow us to be compatible with
Azure and Okta, but probably additional providers as well.

ee/spec/finders/scim_finder_spec.rb
@@ -38,8 +38,14 @@ describe ScimFinder do
           expect(finder.search(filter: "externalId eq #{identity.extern_uid}").first).to eq identity
         end
 
-        it 'allows lookup by userName' do
-          expect(finder.search(filter: "userName eq \"#{identity.user.username}\"").first).to eq identity
+        context 'allows lookup by userName' do
+          it 'finds user by username' do
+            expect(finder.search(filter: "userName eq \"#{identity.user.username}\"").first).to eq identity
+          end
+
+          it 'finds user by extern_uid' do
+            expect(finder.search(filter: "userName eq \"#{identity.extern_uid}\"").first).to eq identity
+          end
         end
       end
 

nickbrandt/GitLab

ee/spec/finders/scim_finder_spec.rb
@@ -56,8 +56,18 @@ describe ScimFinder do
             expect(finder.search(filter: "userName eq \"#{id.user.username}\"").first).to eq id
           end
 
-          it 'allows lookup by userName when userName is an email address' do
-            expect(finder.search(filter: "userName eq #{id.user.email}").first).to eq id
+          context 'allows lookup by userName' do
+            it 'finds user when userName is an email address' do
+              expect(finder.search(filter: "userName eq #{id.user.email}").first).to eq id
+            end
+
+            it 'finds user by username' do
+              expect(finder.search(filter: "userName eq \"#{id.user.username}\"").first).to eq id
+            end
+
+            it 'finds user by extern_uid' do
+              expect(finder.search(filter: "userName eq \"#{id.extern_uid}\"").first).to eq id
+            end
           end
         end
 