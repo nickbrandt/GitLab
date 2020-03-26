8710f533ae83f6279b0c4c675bb6e20bf60a416e
Martin Wortschack <mwortschack@gitlab.com>
2020-03-26 12:18:42 +0100

    Add new members count
    - Adds the number of new members
    to the group activity analytics
    panel.

--- ee/app/assets/javascripts/api.js
+++ ee/app/assets/javascripts/api.js
@@ -28,6 +28,7 @@ export default {
   codeReviewAnalyticsPath: '/api/:version/analytics/code_review',
   groupActivityIssuesPath: '/api/:version/analytics/group_activity/issues_count',
   groupActivityMergeRequestsPath: '/api/:version/analytics/group_activity/merge_requests_count',
+  groupActivityNewMembersPath: '/api/:version/analytics/group_activity/new_members_count',
   countriesPath: '/-/countries',
   countryStatesPath: '/-/country_states',
   paymentFormPath: '/-/subscriptions/payment_form',
@@ -232,6 +233,11 @@ export default {
 
   groupActivityIssuesCount(groupPath) {
     const url = Api.buildUrl(this.groupActivityIssuesPath);
+    return axios.get(url, { params: { group_path: groupPath } });
+  },
+
+  groupActivityNewMembersCount(groupPath) {
+    const url = Api.buildUrl(this.groupActivityNewMembersPath);
     return axios.get(url, { params: { group_path: groupPath } });
   },
 
