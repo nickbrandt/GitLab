4dbf0e69c30a5150abd2c4fd8803fe817f545c73
syasonik <syasonik@gitlab.com>
2020-03-26 14:49:26 -0500

    Add support for embedding all prometheus alerts
    Adds functionality to incidenment management issue creation
    to automatically include a metrics embed in issues created
    for alerts which come from manually configured prometheus
    instances.

--- ee/app/presenters/ee/projects/prometheus/alert_presenter.rb
+++ ee/app/presenters/ee/projects/prometheus/alert_presenter.rb
@@ -10,22 +10,63 @@ module EE
 
         override :metric_embed_for_alert
         def metric_embed_for_alert
+          url = embed_url_for_gitlab_alert || embed_url_for_self_managed_alert
+
+          "\n[](#{url})" if url
+        end
+
+        private
+
+        def embed_url_for_gitlab_alert
           return unless gitlab_alert
 
-          time = starts_at ? Time.rfc3339(starts_at) : Time.current
-          url = metrics_dashboard_project_prometheus_alert_url(
+          metrics_dashboard_project_prometheus_alert_url(
             project,
             gitlab_alert.prometheus_metric_id,
             environment_id: environment.id,
-            start: format_embed_timestamp(time - METRIC_TIME_WINDOW),
-            end: format_embed_timestamp(time + METRIC_TIME_WINDOW)
+            **alert_embed_window_params(embed_time)
           )
-
-          "\n[](#{url})"
         end
 
-        def format_embed_timestamp(timestamp)
-          timestamp.utc.strftime('%FT%TZ')
+        def embed_url_for_self_managed_alert
+          return unless environment && full_query && title
+
+          metrics_dashboard_project_environment_url(
+            project,
+            environment,
+            embed_json: dashboard_for_self_managed_alert.to_json,
+            **alert_embed_window_params(embed_time)
+          )
+        end
+
+        def embed_time
+          starts_at ? Time.rfc3339(starts_at) : Time.current
+        end
+
+        def alert_embed_window_params(time)
+          {
+            start: format_embed_timestamp(time - METRIC_TIME_WINDOW),
+            end: format_embed_timestamp(time + METRIC_TIME_WINDOW)
+          }
+        end
+
+        def format_embed_timestamp(time)
+          time.utc.strftime('%FT%TZ')
+        end
+
+        def dashboard_for_self_managed_alert
+          {
+            panel_groups: [{
+              panels: [{
+                type: 'line-graph',
+                title: title,
+                y_label: y_label,
+                metrics: [{
+                  query_range: full_query
+                }]
+              }]
+            }]
+          }
         end
       end
     end
