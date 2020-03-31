mehulagg/gitlab_test

Make issue sidebar links style more consistent
Made code changes as a result of review

ee/app/assets/javascripts/sidebar/components/weight/weight.vue
@@ -173,7 +173,7 @@ export default {
       <gl-loading-icon v-if="fetching || loading" :inline="true" class="js-weight-loading-icon" />
       <a
         v-if="editable"
-        class="float-right js-weight-edit-link"
+        class="float-right btn-link js-weight-edit-link"
         data-qa-selector="edit_weight_link"
         href="#"
         @click="onEditClick(!shouldShowEditField)"

nickbrandt/GitLab

ee/app/assets/javascripts/sidebar/components/weight/weight.vue
@@ -173,7 +173,7 @@ export default {
       <gl-loading-icon v-if="fetching || loading" :inline="true" class="js-weight-loading-icon" />
       <a
         v-if="editable"
-        class="float-right js-weight-edit-link"
+        class="float-right edit-link js-weight-edit-link"
         data-qa-selector="edit_weight_link"
         href="#"
         @click.prevent="onEditClick(!shouldShowEditField)"