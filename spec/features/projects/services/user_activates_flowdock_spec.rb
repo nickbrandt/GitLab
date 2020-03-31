2825932b3d34921133da9e8f9d5e7c1da0642caf
Justin Ho <hduong@gitlab.com>
2020-03-31 16:19:45 +0700

    Fix feature specs around project integrations
    - With https://gitlab.com/gitlab-org/gitlab/merge_requests/23009,
    new services are active by default. Thus, we do not
    need to manually activate them.
    - Specs now need to execute with the `:js` option to
    enable the active toggle
    - Extract common spec functionality to a shared
    context to DRY up code

--- spec/features/projects/services/user_activates_flowdock_spec.rb
+++ spec/features/projects/services/user_activates_flowdock_spec.rb
@@ -3,22 +3,18 @@
 require 'spec_helper'
 
 describe 'User activates Flowdock' do
-  let(:project) { create(:project) }
-  let(:user) { create(:user) }
+  include_context 'project service activation'
+  let(:project) { create(:project, :repository) }
 
   before do
-    project.add_maintainer(user)
-    sign_in(user)
-
-    visit(project_settings_integrations_path(project))
-
-    click_link('Flowdock')
+    stub_request(:post, /.*api.flowdock.com.*/)
   end
 
-  it 'activates service' do
-    check('Active')
+  it 'activates service', :js do
+    visit_project_integration('Flowdock')
     fill_in('Token', with: 'verySecret')
-    click_button('Save')
+
+    click_test_integration
 
     expect(page).to have_content('Flowdock activated.')
   end
