10afe1b09a27427fdf6556163aacb1d2f561bd42
Markus Koller <mkoller@gitlab.com>
2020-03-31 17:32:17 +0200

    Support more types in UrlBuilder
    - Add support for projects and groups.
    - Add support for building path-only URLs.
    - Reuse it in `HasRepository#web_url`.
    - Remove state and turn the class into a singleton.
    - Provide a default instance in presenters.
    - Rewrite specs with parameterized table syntax.

--- ee/lib/ee/gitlab/url_builder.rb
+++ ee/lib/ee/gitlab/url_builder.rb
@@ -3,45 +3,47 @@
 module EE
   module Gitlab
     module UrlBuilder
-      extend ::Gitlab::Utils::Override
+      extend ActiveSupport::Concern
 
-      override :url
-      def url
-        case object
-        when ::DesignManagement::Design
-          design_url
-        when Epic
-          group_epic_url(object.group, object)
-        when Vulnerability
-          project_security_vulnerability_url(object.project, object)
-        else
-          super
+      class_methods do
+        extend ::Gitlab::Utils::Override
+
+        override :build
+        def build(object, **options)
+          case object.itself
+          when ::DesignManagement::Design
+            design_url(object, **options)
+          when Epic
+            instance.group_epic_url(object.group, object, **options)
+          when Vulnerability
+            instance.project_security_vulnerability_url(object.project, object, **options)
+          else
+            super
+          end
         end
-      end
 
-      override :note_url
-      def note_url
-        noteable = object.noteable
+        override :note_url
+        def note_url(note, **options)
+          noteable = note.noteable
 
-        if object.for_epic?
-          group_epic_url(noteable.group, noteable, anchor: dom_id(object))
-        elsif object.for_vulnerability?
-          project_security_vulnerability_url(noteable.project, noteable, anchor: dom_id(object))
-        else
-          super
+          if note.for_epic?
+            instance.group_epic_url(noteable.group, noteable, anchor: dom_id(note), **options)
+          elsif note.for_vulnerability?
+            instance.project_security_vulnerability_url(noteable.project, noteable, anchor: dom_id(note), **options)
+          else
+            super
+          end
         end
-      end
 
-      private
+        def design_url(design, **options)
+          size, ref = options.values_at(:size, :ref)
+          options.except!(:size, :ref)
 
-      def design_url
-        size, ref = opts.values_at(:size, :ref)
-        design = object
-
-        if size
-          project_design_management_designs_resized_image_url(design.project, design, ref, size)
-        else
-          project_design_management_designs_raw_image_url(design.project, design, ref)
+          if size
+            instance.project_design_management_designs_resized_image_url(design.project, design, ref, size, **options)
+          else
+            instance.project_design_management_designs_raw_image_url(design.project, design, ref, **options)
+          end
         end
       end
     end
