3712268d424aac5668b4fbe6c0b8e94071b3ad29
Maxime Orefice <morefice@gitlab.com>
2020-04-02 21:22:49 +0000

    De-duplicate system hooks service EE specs

--- ee/spec/services/ee/system_hooks_service_spec.rb
+++ ee/spec/services/ee/system_hooks_service_spec.rb
@@ -3,25 +3,35 @@
 require 'spec_helper'
 
 describe EE::SystemHooksService do
+  let(:group_member) { create(:group_member) }
   let(:user) { create(:user) }
 
-  context 'event data' do
-    context 'for GitLab.com' do
-      before do
-        expect(Gitlab).to receive(:com?).and_return(true)
-      end
-
-      it { expect(event_data(user, :create)).to include(:event_name, :name, :created_at, :updated_at, :email, :user_id, :username, :email_opted_in, :email_opted_in_ip, :email_opted_in_source, :email_opted_in_at) }
-      it { expect(event_data(user, :destroy)).to include(:event_name, :name, :created_at, :updated_at, :email, :user_id, :username, :email_opted_in, :email_opted_in_ip, :email_opted_in_source, :email_opted_in_at) }
+  context 'when group member' do
+    context 'event data' do
+      it { expect(event_data(group_member, :create)).to include(:event_name, :created_at, :updated_at, :group_name, :group_path, :group_plan, :group_id, :user_name, :user_username, :user_email, :user_id, :group_access) }
+      it { expect(event_data(group_member, :destroy)).to include(:event_name, :created_at, :updated_at, :group_name, :group_path, :group_plan, :group_id, :user_name, :user_username, :user_email, :user_id, :group_access) }
     end
+  end
 
-    context 'for non-GitLab.com' do
-      before do
-        expect(Gitlab).to receive(:com?).and_return(false)
+  context 'when user' do
+    context 'event data' do
+      context 'for GitLab.com' do
+        before do
+          expect(Gitlab).to receive(:com?).and_return(true)
+        end
+
+        it { expect(event_data(user, :create)).to include(:event_name, :name, :created_at, :updated_at, :email, :user_id, :username, :email_opted_in, :email_opted_in_ip, :email_opted_in_source, :email_opted_in_at) }
+        it { expect(event_data(user, :destroy)).to include(:event_name, :name, :created_at, :updated_at, :email, :user_id, :username, :email_opted_in, :email_opted_in_ip, :email_opted_in_source, :email_opted_in_at) }
       end
 
-      it { expect(event_data(user, :create)).to include(:event_name, :name, :created_at, :updated_at, :email, :user_id, :username) }
-      it { expect(event_data(user, :destroy)).to include(:event_name, :name, :created_at, :updated_at, :email, :user_id, :username) }
+      context 'for non-GitLab.com' do
+        before do
+          expect(Gitlab).to receive(:com?).and_return(false)
+        end
+
+        it { expect(event_data(user, :create)).to include(:event_name, :name, :created_at, :updated_at, :email, :user_id, :username) }
+        it { expect(event_data(user, :destroy)).to include(:event_name, :name, :created_at, :updated_at, :email, :user_id, :username) }
+      end
     end
   end
 
