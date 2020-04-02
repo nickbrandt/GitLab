fae7cb5024cdb609e018724fdf9dd6db6507cd4b
Jan Provaznik <jprovaznik@gitlab.com>
2020-04-02 14:02:19 +0200

    Clarify epic create service
    CreateService created new epics with whitelisted params, but at the
    same time this service inherits from IssuableBaseService which
    updates Issuable with all `params` passed when initializing the service.
    
    To avoid impression that only whitelisted params are
    assigned, we process date `params` directly and let
    IssuableBaseService assign these params.
    
    Because parms filtering is done on upper layer and UpdateService doesn't
    do additional params whitelisting either, there is no reason to
    introduce params whitelisting for CreateService.

--- ee/app/services/epics/create_service.rb
+++ ee/app/services/epics/create_service.rb
@@ -3,15 +3,17 @@
 module Epics
   class CreateService < Epics::BaseService
     def execute
-      @epic = group.epics.new(whitelisted_epic_params)
-      @epic.move_to_start if @epic.parent
+      set_date_params
 
-      create(@epic)
+      epic = group.epics.new
+      create(epic)
     end
 
     private
 
     def before_create(epic)
+      epic.move_to_start if epic.parent
+
       # current_user (defined in BaseService) is not available within run_after_commit block
       user = current_user
       epic.run_after_commit do
@@ -19,18 +21,14 @@ module Epics
       end
     end
 
-    def whitelisted_epic_params
-      result = params.slice(:title, :description, :label_ids, :parent_id)
-
+    def set_date_params
       if params[:start_date_fixed] && params[:start_date_is_fixed]
-        result[:start_date] = params[:start_date_fixed]
+        params[:start_date] = params[:start_date_fixed]
       end
 
       if params[:due_date_fixed] && params[:due_date_is_fixed]
-        result[:end_date] = params[:due_date_fixed]
+        params[:end_date] = params[:due_date_fixed]
       end
-
-      result
     end
   end
 end
