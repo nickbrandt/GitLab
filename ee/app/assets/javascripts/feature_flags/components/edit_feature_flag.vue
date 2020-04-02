46c99e74eea31aa65a4d9eb57ac90576a44ac853
Andrew Fontaine <afontaine@gitlab.com>
2020-04-02 14:31:03 -0400

    Alert Users that Legacy Flags will be Read Only
    Add both an alert to the feature flag edit page and icon to the list to
    inform users that legacy feature flags will be going read-only soon, and
    that these flags should be re-created as a new version flag as soon as
    possible.

--- ee/app/assets/javascripts/feature_flags/components/edit_feature_flag.vue
+++ ee/app/assets/javascripts/feature_flags/components/edit_feature_flag.vue
@@ -1,8 +1,9 @@
 <script>
-import { GlLoadingIcon, GlToggle } from '@gitlab/ui';
+import { GlAlert, GlLoadingIcon, GlToggle } from '@gitlab/ui';
 import { createNamespacedHelpers } from 'vuex';
 import { sprintf, s__ } from '~/locale';
 import glFeatureFlagMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
+import { LEGACY_FLAG } from '../constants';
 import store from '../store/index';
 import FeatureFlagForm from './form.vue';
 
@@ -11,6 +12,7 @@ const { mapState, mapActions } = createNamespacedHelpers('edit');
 export default {
   store,
   components: {
+    GlAlert,
     GlLoadingIcon,
     GlToggle,
     FeatureFlagForm,
@@ -30,6 +32,11 @@ export default {
       required: true,
     },
   },
+  translations: {
+    legacyFlagAlert: s__(
+      'FeatureFlags|GitLab is moving to a new way of managing feature flags, and in 13.4, this feature flag will become read-only. Please create a new feature flag.',
+    ),
+  },
   computed: {
     ...mapState([
       'error',
@@ -47,6 +54,9 @@ export default {
       return this.iid
         ? `^${this.iid} ${this.name}`
         : sprintf(s__('Edit %{name}'), { name: this.name });
+    },
+    deprecated() {
+      return this.glFeatures.featureFlagsNewVersion && this.version === LEGACY_FLAG;
     },
   },
   created() {
@@ -69,6 +79,9 @@ export default {
     <gl-loading-icon v-if="isLoading" />
 
     <template v-else-if="!isLoading && !hasError">
+      <gl-alert v-if="deprecated" variant="warning" :dismissible="false" class="gl-my-5">
+        {{ $options.translations.legacyFlagAlert }}
+      </gl-alert>
       <div class="d-flex align-items-center mb-3 mt-3">
         <gl-toggle :value="active" class="m-0 mr-3" @change="toggleActive" />
         <h3 class="page-title m-0">{{ title }}</h3>
