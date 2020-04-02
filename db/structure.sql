31fbda6f3c751544f41fbdfcf45f265aa5366bc2
Matthias Kaeppler <mkaeppler@gitlab.com>
2020-04-02 15:22:07 +0200

    Migration: add correlation_id to import status

--- db/structure.sql
+++ db/structure.sql
@@ -4898,7 +4898,8 @@ CREATE TABLE public.project_mirror_data (
     jid character varying,
     last_error text,
     last_update_at timestamp with time zone,
-    last_successful_update_at timestamp with time zone
+    last_successful_update_at timestamp with time zone,
+    correlation_id_value character varying(128)
 );
 
 CREATE SEQUENCE public.project_mirror_data_id_seq
@@ -13003,6 +13004,7 @@ COPY "schema_migrations" (version) FROM STDIN;
 20200330132913
 20200331220930
 20200402123926
+20200402124802
 20200402135250
 20200402185044
 20200403184110
