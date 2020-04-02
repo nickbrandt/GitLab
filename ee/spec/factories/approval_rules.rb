8c9b173ce891fdfb22dab3385930ded34e54ed6c
Tetiana Chupryna <tchupryna@gitlab.com>
2020-04-02 17:12:59 +0300

    Apply new _scanning factories

--- ee/spec/factories/approval_rules.rb
+++ ee/spec/factories/approval_rules.rb
@@ -23,11 +23,6 @@ FactoryBot.define do
       approvals_required { rand(1..ApprovalProjectRule::APPROVALS_REQUIRED_MAX) }
     end
 
-    trait :license_management do
-      name { ApprovalRuleLike::DEFAULT_NAME_FOR_LICENSE_REPORT }
-      report_type { :license_management }
-    end
-
     trait :license_scanning do
       name { ApprovalRuleLike::DEFAULT_NAME_FOR_LICENSE_REPORT }
       report_type { :license_scanning }
@@ -57,7 +52,7 @@ FactoryBot.define do
       security_report
     end
 
-    trait :license_management do
+    trait :license_scanning do
       name { ApprovalRuleLike::DEFAULT_NAME_FOR_LICENSE_REPORT }
       rule_type { :report_approver }
     end
