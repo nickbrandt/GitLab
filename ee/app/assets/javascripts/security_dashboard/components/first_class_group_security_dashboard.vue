b567ab3c2cdd911123b340fb2e4048dadd7cbeb5
Savas Vedova <svedova@gitlab.com>
2020-04-03 13:07:41 +0300

    Load the GraphQL based vulnerabilities dashboard
    Create a new dashboard for groups which loads data from GQL

---
+++ ee/app/assets/javascripts/security_dashboard/components/first_class_group_security_dashboard.vue
@@ -0,0 +1,35 @@
+<script>
+import SecurityDashboardLayout from 'ee/security_dashboard/components/security_dashboard_layout.vue';
+import GroupSecurityVulnerabilities from './first_class_group_security_dashboard_vulnerabilities.vue';
+
+export default {
+  components: {
+    SecurityDashboardLayout,
+    GroupSecurityVulnerabilities,
+  },
+  props: {
+    dashboardDocumentation: {
+      type: String,
+      required: true,
+    },
+    emptyStateSvgPath: {
+      type: String,
+      required: true,
+    },
+    groupFullPath: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <security-dashboard-layout>
+    <group-security-vulnerabilities
+      :dashboard-documentation="dashboardDocumentation"
+      :empty-state-svg-path="emptyStateSvgPath"
+      :group-full-path="groupFullPath"
+    />
+  </security-dashboard-layout>
+</template>
---
+++ ee/app/assets/javascripts/security_dashboard/components/first_class_group_security_dashboard_vulnerabilities.vue
@@ -0,0 +1,128 @@
+<script>
+import { s__ } from '~/locale';
+import { GlAlert, GlNewButton, GlEmptyState, GlIntersectionObserver } from '@gitlab/ui';
+import VulnerabilityList from 'ee/vulnerabilities/components/vulnerability_list.vue';
+import vulnerabilitiesQuery from '../graphql/group_vulnerabilities.graphql';
+import { VULNERABILITIES_PER_PAGE } from 'ee/vulnerabilities/constants';
+
+export default {
+  components: {
+    GlAlert,
+    GlNewButton,
+    GlEmptyState,
+    GlIntersectionObserver,
+    VulnerabilityList,
+  },
+  props: {
+    dashboardDocumentation: {
+      type: String,
+      required: true,
+    },
+    emptyStateSvgPath: {
+      type: String,
+      required: true,
+    },
+    groupFullPath: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      pageInfo: {},
+      vulnerabilities: [],
+      errorLoadingVulnerabilities: false,
+    };
+  },
+  apollo: {
+    vulnerabilities: {
+      query: vulnerabilitiesQuery,
+      variables() {
+        return {
+          fullPath: this.groupFullPath,
+          first: VULNERABILITIES_PER_PAGE,
+        };
+      },
+      update: ({ group }) => group.vulnerabilities.nodes,
+      result({ data }) {
+        this.pageInfo = data.group.vulnerabilities.pageInfo;
+      },
+      error() {
+        this.errorLoadingVulnerabilities = true;
+      },
+    },
+  },
+  computed: {
+    isLoadingQuery() {
+      return this.$apollo.queries.vulnerabilities.loading;
+    },
+    isLoadingFirstResult() {
+      return this.isLoadingQuery && this.vulnerabilities.length === 0;
+    },
+  },
+  methods: {
+    onErrorDismiss() {
+      this.errorLoadingVulnerabilities = false;
+    },
+    fetchNextPage() {
+      if (this.pageInfo.hasNextPage) {
+        this.$apollo.queries.vulnerabilities.fetchMore({
+          variables: { after: this.pageInfo.endCursor },
+          updateQuery: (previousResult, { fetchMoreResult }) => {
+            fetchMoreResult.group.vulnerabilities.nodes.unshift(
+              ...previousResult.group.vulnerabilities.nodes,
+            );
+            return fetchMoreResult;
+          },
+        });
+      }
+    },
+  },
+  emptyStateDescription: s__(
+    `While it's rare to have no vulnerabilities for your group, it can happen. In any event, we ask that you double check your settings to make sure you've set up your dashboard correctly.`,
+  ),
+};
+</script>
+
+<template>
+  <div>
+    <gl-alert
+      v-if="errorLoadingVulnerabilities"
+      class="mb-4"
+      variant="danger"
+      @dismiss="onErrorDismiss"
+    >
+      {{
+        s__(
+          'Security Dashboard|Error fetching the vulnerability list. Please check your network connection and try again.',
+        )
+      }}
+    </gl-alert>
+    <vulnerability-list
+      v-else
+      :is-loading="isLoadingFirstResult"
+      :dashboard-documentation="dashboardDocumentation"
+      :empty-state-svg-path="emptyStateSvgPath"
+      :vulnerabilities="vulnerabilities"
+    >
+      <template #emptyState>
+        <gl-empty-state
+          :title="s__(`No vulnerabilities found for this group`)"
+          :svg-path="emptyStateSvgPath"
+          :description="$options.emptyStateDescription"
+          :primary-button-link="dashboardDocumentation"
+          :primary-button-text="s__('Security Reports|Learn more about setting up your dashboard')"
+        />
+      </template>
+    </vulnerability-list>
+    <gl-intersection-observer
+      v-if="pageInfo.hasNextPage"
+      class="text-center"
+      @appear="fetchNextPage"
+    >
+      <gl-new-button :loading="isLoadingQuery" :disabled="isLoadingQuery" @click="fetchNextPage">{{
+        __('Load more vulnerabilities')
+      }}</gl-new-button>
+    </gl-intersection-observer>
+  </div>
+</template>
