dfd519376fb0f52d76a0f5cee1c347d8937b69fd
charlie ablett <cablett@gitlab.com>
2020-04-03 05:27:58 +0000

    Merge branch '213265-fix-search-for-complex-queries' into 'master'
    Update index_options to fix advanced search queries
    
    Closes #213265
    
    See merge request gitlab-org/gitlab!28712
    
    (cherry picked from commit e7da90572e9697b3cab2e16f7ab02d4bde65b8f0)
    
    55f03a2a Fix advanced global search illegal_state_exception
    627815f1 Add spec for advanced queries
    4d578aa2 Add changelog for index_options to fix advanced search

--- ee/spec/support/shared_examples/services/search_service_shared_examples.rb
+++ ee/spec/support/shared_examples/services/search_service_shared_examples.rb
@@ -45,5 +45,53 @@ RSpec.shared_examples 'EE search service shared examples' do |normal_results, el
 
       is_expected.to be_a(normal_results)
     end
+
+    context 'advanced syntax queries for all scopes', :elastic, :sidekiq_inline do
+      queries = [
+        '"display bug"',
+        'bug -display',
+        'bug display | sound',
+        'bug | (display +sound)',
+        'bug find_by_*',
+        'argument \-last'
+      ]
+
+      scopes = if elasticsearch_results == ::Gitlab::Elastic::SnippetSearchResults
+                 %w[
+                   snippet_titles
+                   snippet_blobs
+                 ]
+               else
+                 %w[
+                   merge_requests
+                   notes
+                   commits
+                   blobs
+                   projects
+                   issues
+                   wiki_blobs
+                   milestones
+                 ]
+               end
+
+      queries.each do |query|
+        scopes.each do |scope|
+          context "with query #{query} and scope #{scope}" do
+            let(:params) { { search: query, scope: scope } }
+
+            it "allows advanced query" do
+              allow(Gitlab::CurrentSettings)
+                .to receive(:search_using_elasticsearch?)
+                .and_return(true)
+
+              ensure_elasticsearch_index!
+
+              results = subject
+              expect(results.objects(scope)).to be_kind_of(Enumerable)
+            end
+          end
+        end
+      end
+    end
   end
 end
