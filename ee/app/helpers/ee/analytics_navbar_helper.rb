acf7cdb378f3635a2424e33677a1f5aa9c4fc8d0
Gilang Gumilar <gilangmlr@gmail.com>
2020-04-05 23:09:17 +0700

    Revert priority ordering

--- ee/app/helpers/ee/analytics_navbar_helper.rb
+++ ee/app/helpers/ee/analytics_navbar_helper.rb
@@ -16,7 +16,7 @@ module EE
     override :group_analytics_navbar_links
     def group_analytics_navbar_links(group, current_user)
       super + [
-        group_cycle_analytics_navbar_link(group, current_user, -1),
+        group_cycle_analytics_navbar_link(group, current_user),
         contribution_analytics_navbar_link(group, current_user),
         group_insights_navbar_link(group, current_user),
         issues_analytics_navbar_link(group, current_user),
@@ -37,58 +37,55 @@ module EE
       )
     end
 
-    def group_cycle_analytics_navbar_link(group, current_user, priority = 0)
+    def group_cycle_analytics_navbar_link(group, current_user)
       return unless group_sidebar_link?(:cycle_analytics)
 
       navbar_sub_item(
         title: _('Value Stream'),
         path: 'groups/analytics/cycle_analytics#show',
-        link: group_analytics_cycle_analytics_path(group),
-        link_to_options: { data: { priority: priority } }
+        link: group_analytics_cycle_analytics_path(group)
       )
     end
 
-    def productivity_analytics_navbar_link(group, current_user, priority = 0)
+    def productivity_analytics_navbar_link(group, current_user)
       return unless group_sidebar_link?(:productivity_analytics)
 
       navbar_sub_item(
         title: _('Productivity'),
         path: 'groups/analytics/productivity_analytics#show',
-        link: group_analytics_productivity_analytics_path(group),
-        link_to_options: { data: { priority: priority } }
+        link: group_analytics_productivity_analytics_path(group)
       )
     end
 
-    def contribution_analytics_navbar_link(group, current_user, priority = 0)
+    def contribution_analytics_navbar_link(group, current_user)
       return unless group_sidebar_link?(:contribution_analytics)
 
       navbar_sub_item(
         title: _('Contribution'),
         path: 'groups/contribution_analytics#show',
         link: group_contribution_analytics_path(group),
-        link_to_options: { data: { placement: 'right', qa_selector: 'contribution_analytics_link', priority: priority } }
+        link_to_options: { data: { placement: 'right', qa_selector: 'contribution_analytics_link' } }
       )
     end
 
-    def group_insights_navbar_link(group, current_user, priority = 0)
+    def group_insights_navbar_link(group, current_user)
       return unless group_sidebar_link?(:group_insights)
 
       navbar_sub_item(
         title: _('Insights'),
         path: 'groups/insights#show',
         link:  group_insights_path(group),
-        link_to_options: { class: 'shortcuts-group-insights', data: { qa_selector: 'group_insights_link', priority: priority } }
+        link_to_options: { class: 'shortcuts-group-insights', data: { qa_selector: 'group_insights_link' } }
       )
     end
 
-    def issues_analytics_navbar_link(group, current_user, priority = 0)
+    def issues_analytics_navbar_link(group, current_user)
       return unless group_sidebar_link?(:analytics)
 
       navbar_sub_item(
         title: _('Issues'),
         path: 'issues_analytics#show',
-        link: group_issues_analytics_path(group),
-        link_to_options: { data: { priority: priority } }
+        link: group_issues_analytics_path(group)
       )
     end
 
