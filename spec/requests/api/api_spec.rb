d5ccca5be2c916d6e5ec2e9ad1a1c159706dd606
Małgorzata Ksionek <mksionek@gitlab.com>
2020-04-06 18:17:41 +0200

    Add read only scope for api
    Add changelog entry

--- spec/requests/api/api_spec.rb
+++ spec/requests/api/api_spec.rb
@@ -3,12 +3,13 @@
 require 'spec_helper'
 
 describe API::API do
-  let(:user) { create(:user, last_activity_on: Date.yesterday) }
+  include GroupAPIHelpers
 
   describe 'Record user last activity in after hook' do
     # It does not matter which endpoint is used because last_activity_on should
     # be updated on every request. `/groups` is used as an example
     # to represent any API endpoint
+    let(:user) { create(:user, last_activity_on: Date.yesterday) }
 
     it 'updates the users last_activity_on date' do
       expect { get api('/groups', user) }.to change { user.reload.last_activity_on }.to(Date.today)
@@ -19,6 +20,50 @@ describe API::API do
         stub_feature_flags(api_activity_logging: false)
 
         expect { get api('/groups', user) }.not_to change { user.reload.last_activity_on }
+      end
+    end
+  end
+
+  describe 'User with only read_api scope personal access token' do
+    # It does not matter which endpoint is used because this should
+    # in the same way for every request. `/groups` is used as an example
+    # to represent any API endpoint
+
+    context 'when personal access token has only read_api scope' do
+      let_it_be(:user) { create(:user) }
+      let_it_be(:group) { create(:group) }
+      let_it_be(:token) { create(:personal_access_token, user: user, scopes: [:read_api]) }
+
+      before do
+        group.add_owner(user)
+      end
+
+      it 'does authorize user for get request' do
+        get api('/groups', personal_access_token: token)
+
+        expect(response).to have_gitlab_http_status(:ok)
+      end
+
+      it 'does not authorize user for post request' do
+        group_attributes = attributes_for_group_api
+
+        post api("/groups", personal_access_token: token), params: group_attributes
+
+        expect(response).to have_gitlab_http_status(:forbidden)
+      end
+
+      it 'does not authorize user for put request' do
+        group_param = { name: 'Test' }
+
+        put api("/groups/#{group.id}", personal_access_token: token), params: group_param
+
+        expect(response).to have_gitlab_http_status(:forbidden)
+      end
+
+      it 'does not authorize user for delete request' do
+        delete api("/groups/#{group.id}", personal_access_token: token)
+
+        expect(response).to have_gitlab_http_status(:forbidden)
       end
     end
   end
