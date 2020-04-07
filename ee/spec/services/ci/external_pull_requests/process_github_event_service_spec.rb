d2c83de2a2561f89e8cdd263eab2b5f19e63c348
Fabio Pitino <fpitino@gitlab.com>
2020-04-07 13:38:01 +0100

    Feedback from review

--- ee/spec/services/ci/external_pull_requests/process_github_event_service_spec.rb
+++ ee/spec/services/ci/external_pull_requests/process_github_event_service_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe Ci::ProcessGithubPullRequestEventService do
+describe Ci::ExternalPullRequests::ProcessGithubEventService do
   let_it_be(:project) { create(:project, :repository) }
   let_it_be(:user) { create(:user) }
   let(:action) { 'opened' }
