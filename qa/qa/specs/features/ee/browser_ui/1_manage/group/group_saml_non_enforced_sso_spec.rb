f95ec96edbf579d55547de38d39a0d5425f34f7a
Aakriti Gupta <agupta@gitlab.com>
2020-04-07 12:38:44 +0000

    Add additional feature check for Group Activity
    This is for when the feature flag is not set.

--- qa/qa/specs/features/ee/browser_ui/1_manage/group/group_saml_non_enforced_sso_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/1_manage/group/group_saml_non_enforced_sso_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module QA
-  context 'Manage', :group_saml, :orchestrated, :requires_admin, quarantine: { issue: 'https://gitlab.com/gitlab-org/gitlab/-/issues/212378', type: :bug } do
+  context 'Manage', :group_saml, :orchestrated, :requires_admin do
     describe 'Group SAML SSO - Non enforced SSO' do
       include Support::Api
 
