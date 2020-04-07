1ec4418fded3362cce449b241dd720f8bd61c1a1
Samantha Ming <sming@gitlab.com>
2020-04-07 07:56:58 +0000

    Pass approval rules into MR create
    - Rules are loaded for the current target branch

--- ee/app/assets/javascripts/approvals/components/app.vue
+++ ee/app/assets/javascripts/approvals/components/app.vue
@@ -29,9 +29,15 @@ export default {
     removeModalId() {
       return `${this.settings.prefix}-approvals-remove-modal`;
     },
+    targetBranch() {
+      if (this.settings.prefix === 'mr-edit' && !this.settings.mrSettingsPath) {
+        return this.settings.mrCreateTargetBranch;
+      }
+      return null;
+    },
   },
-  created() {
-    this.fetchRules();
+  mounted() {
+    return this.fetchRules(this.targetBranch);
   },
   methods: {
     ...mapActions(['fetchRules']),
