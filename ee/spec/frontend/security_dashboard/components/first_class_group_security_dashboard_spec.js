8eb5f3e68ab58a194ed9434f475b049b625dd10f
Savas Vedova <svedova@gitlab.com>
2020-04-08 17:15:53 +0300

    Add vulnerable projects table
    Include the Project Severity Status table in the Dashboard

--- ee/spec/frontend/security_dashboard/components/first_class_group_security_dashboard_spec.js
+++ ee/spec/frontend/security_dashboard/components/first_class_group_security_dashboard_spec.js
@@ -1,6 +1,8 @@
 import { shallowMount } from '@vue/test-utils';
+import SecurityDashboardLayout from 'ee/security_dashboard/components/security_dashboard_layout.vue';
 import FirstClassGroupDashboard from 'ee/security_dashboard/components/first_class_group_security_dashboard.vue';
 import FirstClassGroupVulnerabilities from 'ee/security_dashboard/components/first_class_group_security_dashboard_vulnerabilities.vue';
+import VulnerabilitySeverity from 'ee/security_dashboard/components/vulnerability_severity.vue';
 
 describe('First Class Group Dashboard Component', () => {
   let wrapper;
@@ -8,8 +10,10 @@ describe('First Class Group Dashboard Component', () => {
   const dashboardDocumentation = 'dashboard-documentation';
   const emptyStateSvgPath = 'empty-state-path';
   const groupFullPath = 'group-full-path';
+  const vulnerableProjectsEndpoint = '/vulnerable/projects';
 
   const findGroupVulnerabilities = () => wrapper.find(FirstClassGroupVulnerabilities);
+  const findVulnerabilitySeverity = () => wrapper.find(VulnerabilitySeverity);
 
   const createWrapper = () => {
     return shallowMount(FirstClassGroupDashboard, {
@@ -17,6 +21,10 @@ describe('First Class Group Dashboard Component', () => {
         dashboardDocumentation,
         emptyStateSvgPath,
         groupFullPath,
+        vulnerableProjectsEndpoint,
+      },
+      stubs: {
+        SecurityDashboardLayout,
       },
     });
   };
@@ -35,5 +43,9 @@ describe('First Class Group Dashboard Component', () => {
       emptyStateSvgPath,
       groupFullPath,
     });
+  });
+
+  it('displays the vulnerability severity in an aside', () => {
+    expect(findVulnerabilitySeverity().exists()).toBe(true);
   });
 });
