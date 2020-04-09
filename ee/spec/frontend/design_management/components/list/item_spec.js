36f7b21227bf9d973e79ec762761eee3c7a76913
Tom Quirk <tquirk@gitlab.com>
2020-04-09 14:07:44 +0930

    Fix imageLoading bug when scrolling back to design
    imageLoading was being set incorrectly (every time the item appeared).
    This meant that even if the image was not loading, the loading
    spinner would appear if the image appeared in view again.
    
    This commit changes the logic so that we only set imageLoading if the
    image has never appeared in view.
    
    This commit also adds and reorganises some tests to ensure this path is
    covered.

--- ee/spec/frontend/design_management/components/list/item_spec.js
+++ ee/spec/frontend/design_management/components/list/item_spec.js
@@ -1,5 +1,5 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils';
-import { GlIcon, GlIntersectionObserver } from '@gitlab/ui';
+import { GlIcon, GlLoadingIcon, GlIntersectionObserver } from '@gitlab/ui';
 import VueRouter from 'vue-router';
 import Item from 'ee/design_management/components/list/item.vue';
 
@@ -60,36 +60,63 @@ describe('Design management list item component', () => {
 
   describe('when item appears in view', () => {
     let image;
+    let glIntersectionObserver;
 
     beforeEach(() => {
       createComponent();
       image = wrapper.find('img');
+      glIntersectionObserver = wrapper.find(GlIntersectionObserver);
 
       expect(image.attributes('src')).toBe('');
 
-      wrapper.find(GlIntersectionObserver).vm.$emit('appear');
+      glIntersectionObserver.vm.$emit('appear');
       return wrapper.vm.$nextTick();
     });
 
-    it('renders an image', () => {
-      expect(image.attributes('src')).toBe('http://via.placeholder.com/300');
-    });
-
-    it('renders media broken icon when image onerror triggered', () => {
-      image.trigger('error');
-      return wrapper.vm.$nextTick().then(() => {
-        expect(image.isVisible()).toBe(false);
-        expect(wrapper.find(GlIcon).element).toMatchSnapshot();
+    describe('before image is loaded', () => {
+      it('renders loading spinner', () => {
+        expect(wrapper.find(GlLoadingIcon)).toExist();
       });
     });
 
-    describe('when imageV432x230 and image provided', () => {
-      it('renders imageV432x230 image', () => {
-        const mockSrc = 'mock-imageV432x230-url';
-        wrapper.setProps({ imageV432x230: mockSrc });
+    describe('after image is loaded', () => {
+      beforeEach(() => {
+        image.trigger('load');
+        return wrapper.vm.$nextTick();
+      });
 
+      it('renders an image', () => {
+        expect(image.attributes('src')).toBe('http://via.placeholder.com/300');
+        expect(image.isVisible()).toBe(true);
+      });
+
+      it('renders media broken icon when image onerror triggered', () => {
+        image.trigger('error');
         return wrapper.vm.$nextTick().then(() => {
-          expect(image.attributes('src')).toBe(mockSrc);
+          expect(image.isVisible()).toBe(false);
+          expect(wrapper.find(GlIcon).element).toMatchSnapshot();
+        });
+      });
+
+      describe('when imageV432x230 and image provided', () => {
+        it('renders imageV432x230 image', () => {
+          const mockSrc = 'mock-imageV432x230-url';
+          wrapper.setProps({ imageV432x230: mockSrc });
+
+          return wrapper.vm.$nextTick().then(() => {
+            expect(image.attributes('src')).toBe(mockSrc);
+          });
+        });
+      });
+
+      describe('when image disappears from view and then reappears', () => {
+        beforeEach(() => {
+          glIntersectionObserver.vm.$emit('appear');
+          return wrapper.vm.$nextTick();
+        });
+
+        it('renders an image', () => {
+          expect(image.isVisible()).toBe(true);
         });
       });
     });
