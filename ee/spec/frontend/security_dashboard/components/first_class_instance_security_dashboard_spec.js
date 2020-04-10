a767966db7ed052b1108a0cb116c7697c9ee7337
Savas Vedova <svedova@gitlab.com>
2020-04-10 14:04:07 +0300

    Add the Project Security Status table
    - Add related tests

--- ee/spec/frontend/security_dashboard/components/first_class_instance_security_dashboard_spec.js
+++ ee/spec/frontend/security_dashboard/components/first_class_instance_security_dashboard_spec.js
@@ -1,20 +1,28 @@
 import { shallowMount } from '@vue/test-utils';
+import SecurityDashboardLayout from 'ee/security_dashboard/components/security_dashboard_layout.vue';
 import FirstClassInstanceDashboard from 'ee/security_dashboard/components/first_class_instance_security_dashboard.vue';
 import FirstClassInstanceVulnerabilities from 'ee/security_dashboard/components/first_class_instance_security_dashboard_vulnerabilities.vue';
+import VulnerabilitySeverity from 'ee/security_dashboard/components/vulnerability_severity.vue';
 
-describe('First Class Group Dashboard Component', () => {
+describe('First Class Instance Dashboard Component', () => {
   let wrapper;
 
   const dashboardDocumentation = 'dashboard-documentation';
   const emptyStateSvgPath = 'empty-state-path';
+  const vulnerableProjectsEndpoint = '/vulnerable/projects';
 
-  const findGroupVulnerabilities = () => wrapper.find(FirstClassInstanceVulnerabilities);
+  const findInstanceVulnerabilities = () => wrapper.find(FirstClassInstanceVulnerabilities);
+  const findVulnerabilitySeverity = () => wrapper.find(VulnerabilitySeverity);
 
   const createWrapper = () => {
     return shallowMount(FirstClassInstanceDashboard, {
       propsData: {
         dashboardDocumentation,
         emptyStateSvgPath,
+        vulnerableProjectsEndpoint,
+      },
+      stubs: {
+        SecurityDashboardLayout,
       },
     });
   };
@@ -28,9 +36,13 @@ describe('First Class Group Dashboard Component', () => {
   });
 
   it('should render correctly', () => {
-    expect(findGroupVulnerabilities().props()).toEqual({
+    expect(findInstanceVulnerabilities().props()).toEqual({
       dashboardDocumentation,
       emptyStateSvgPath,
     });
+  });
+
+  it('displays the vulnerability severity in an aside', () => {
+    expect(findVulnerabilitySeverity().exists()).toBe(true);
   });
 });
