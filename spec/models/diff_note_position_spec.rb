mehulagg/gitlab_test

Capture diff note position on mergeability check
When a merge request is checked whether it's mergeable
merge_ref_head is updated and we need to update diff
note positions within it

spec/models/diff_note_position_spec.rb
@@ -3,13 +3,33 @@
 require 'spec_helper'
 
 describe DiffNotePosition, type: :model do
-  it 'has a position attribute' do
-    diff_position = build(:diff_position)
-    line_code = 'bd4b7bfff3a247ccf6e3371c41ec018a55230bcc_534_521'
-    diff_note_position = build(:diff_note_position, line_code: line_code, position: diff_position)
+  describe '.create_or_update_by' do
+    context 'when a diff note' do
+      let(:note) { create(:diff_note_on_merge_request) }
+      let(:diff_position) { build(:diff_position) }
+      let(:line_code) { 'bd4b7bfff3a247ccf6e3371c41ec018a55230bcc_534_521' }
+      let(:diff_note_position) { note.diff_note_positions.first }
 
-    expect(diff_note_position.position).to eq(diff_position)
-    expect(diff_note_position.line_code).to eq(line_code)
-    expect(diff_note_position.position_type).to eq('text')
+      context 'does not have a diff note position' do
+        it 'creates a diff note position' do
+          note.diff_note_positions.create_or_update_by(:merge_ref_head, line_code: line_code, position: diff_position)
+
+          expect(diff_note_position.position).to eq(diff_position)
+          expect(diff_note_position.line_code).to eq(line_code)
+          expect(diff_note_position.position_type).to eq('text')
+        end
+      end
+
+      context 'has a diff note position' do
+        it 'updates the existing diff note position' do
+          create(:diff_note_position, note: note)
+          note.diff_note_positions.create_or_update_by(:merge_ref_head, line_code: line_code, position: diff_position)
+
+          expect(diff_note_position.position).to eq(diff_position)
+          expect(diff_note_position.line_code).to eq(line_code)
+          expect(note.diff_note_positions.size).to eq(1)
+        end
+      end
+    end
   end
 end

nickbrandt/GitLab

spec/models/diff_note_position_spec.rb
@@ -3,14 +3,35 @@
 require 'spec_helper'
 
 describe DiffNotePosition, type: :model do
-  it 'has a position attribute' do
-    diff_position = build(:diff_position)
-    line_code = 'bd4b7bfff3a247ccf6e3371c41ec018a55230bcc_534_521'
-    diff_note_position = build(:diff_note_position, line_code: line_code, position: diff_position)
+  describe '.create_or_update_by' do
+    context 'when a diff note' do
+      let(:note) { create(:diff_note_on_merge_request) }
+      let(:diff_position) { build(:diff_position) }
+      let(:line_code) { 'bd4b7bfff3a247ccf6e3371c41ec018a55230bcc_534_521' }
+      let(:diff_note_position) { note.diff_note_positions.first }
+      let(:params) { { diff_type: :head, line_code: line_code, position: diff_position } }
 
-    expect(diff_note_position.position).to eq(diff_position)
-    expect(diff_note_position.line_code).to eq(line_code)
-    expect(diff_note_position.diff_content_type).to eq('text')
+      context 'does not have a diff note position' do
+        it 'creates a diff note position' do
+          described_class.create_or_update_for(note, params)
+
+          expect(diff_note_position.position).to eq(diff_position)
+          expect(diff_note_position.line_code).to eq(line_code)
+          expect(diff_note_position.diff_content_type).to eq('text')
+        end
+      end
+
+      context 'has a diff note position' do
+        it 'updates the existing diff note position' do
+          create(:diff_note_position, note: note)
+          described_class.create_or_update_for(note, params)
+
+          expect(note.diff_note_positions.size).to eq(1)
+          expect(diff_note_position.position).to eq(diff_position)
+          expect(diff_note_position.line_code).to eq(line_code)
+        end
+      end
+    end
   end
 
   it 'unique by note_id and diff type' do