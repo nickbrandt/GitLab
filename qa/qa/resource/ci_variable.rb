mehulagg/gitlab_test

Update QA ci variables spec
Updated page object and relative
spec to ensure E2E is working
like it should.

qa/qa/resource/ci_variable.rb
@@ -19,9 +19,8 @@ module QA
 
         Page::Project::Settings::CICD.perform do |setting|
           setting.expand_ci_variables do |page|
+            page.open_modal
             page.fill_variable(key, value, masked)
-
-            page.save_variables
           end
         end
       end

nickbrandt/GitLab

qa/qa/resource/ci_variable.rb
@@ -19,9 +19,8 @@ module QA
 
         Page::Project::Settings::CICD.perform do |setting|
           setting.expand_ci_variables do |page|
+            page.click_add_variable
             page.fill_variable(key, value, masked)
-
-            page.save_variables
           end
         end
       end