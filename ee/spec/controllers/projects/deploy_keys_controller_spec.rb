0ecdf4fef91eddff503a3984f501ff321ab9f262
Etienne Baqué <ebaque@gitlab.com>
2020-04-14 15:26:04 -0400

    Moved deploy keys back to repository settings
    Reverted changes in controllers, views and rspecs.
    Reverted changes in documentation.
    Added changelog file.

--- ee/spec/controllers/projects/deploy_keys_controller_spec.rb
+++ ee/spec/controllers/projects/deploy_keys_controller_spec.rb
@@ -30,7 +30,7 @@ describe Projects::DeployKeysController do
     it 'records an audit event' do
       expect { post :create, params: params }.to change { AuditEvent.count }.by(1)
 
-      expect(response).to redirect_to(project_settings_ci_cd_path(project, anchor: 'js-deploy-keys-settings'))
+      expect(response).to redirect_to(project_settings_repository_path(project, anchor: 'js-deploy-keys-settings'))
     end
   end
 
