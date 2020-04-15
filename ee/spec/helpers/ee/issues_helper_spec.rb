444d09c227e4d45cc64dae8a0446d277d464c65d
Felipe Artur <felipefac@gmail.com>
2020-04-15 16:51:04 -0300

    Display warning for issues moved from service desk
    Backend part for displaying a warning when issues gets moved
    from service desk.

--- ee/spec/helpers/ee/issues_helper_spec.rb
+++ ee/spec/helpers/ee/issues_helper_spec.rb
@@ -2,7 +2,7 @@
 
 require "spec_helper"
 
-describe IssuesHelper do
+describe EE::IssuesHelper do
   let(:project) { create(:project) }
   let(:issue) { create :issue, project: project }
 
@@ -40,6 +40,28 @@ describe IssuesHelper do
           end
         end
       end
+    end
+  end
+
+  describe '#show_moved_service_desk_issue_warning?' do
+    let(:project1) { create(:project, service_desk_enabled: true) }
+    let(:project2) { create(:project, service_desk_enabled: true) }
+    let!(:old_issue) { create(:issue, author: User.support_bot, project: project1) }
+    let!(:new_issue) { create(:issue, author: User.support_bot, project: project2) }
+
+    before do
+      allow(::EE::Gitlab::ServiceDesk).to receive(:enabled?).and_return(true)
+      old_issue.update(moved_to: new_issue)
+    end
+
+    it 'is true when moved issue project has service desk disabled' do
+      project2.update!(service_desk_enabled: false)
+
+      expect(helper.show_moved_service_desk_issue_warning?(new_issue)).to be(true)
+    end
+
+    it 'is false when moved issue project has service desk enabled' do
+      expect(helper.show_moved_service_desk_issue_warning?(new_issue)).to be(false)
     end
   end
 end
