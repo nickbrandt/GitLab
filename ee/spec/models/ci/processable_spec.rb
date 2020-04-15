eab5215b4f0d305f4f4d389037e6690739547d09
drew cimino <dcimino@gitlab.com>
2020-04-15 12:52:26 -0400

    Refactor delegation tests in favor of "delegate_method" matcher

--- ee/spec/models/ci/processable_spec.rb
+++ ee/spec/models/ci/processable_spec.rb
@@ -3,40 +3,9 @@
 require 'spec_helper'
 
 describe Ci::Processable do
-  let_it_be(:project) { create(:project) }
-  let_it_be(:pipeline) { create(:ci_pipeline, project: project) }
+  describe 'delegations' do
+    subject { Ci::Processable.new }
 
-  let_it_be(:detached_merge_request_pipeline) do
-    create(:ci_pipeline, :detached_merge_request_pipeline, :with_job, project: project)
-  end
-
-  let_it_be(:legacy_detached_merge_request_pipeline) do
-    create(:ci_pipeline, :legacy_detached_merge_request_pipeline, :with_job, project: project)
-  end
-
-  let_it_be(:merged_result_pipeline) do
-    create(:ci_pipeline, :merged_result_pipeline, :with_job, project: project)
-  end
-
-  describe '#merge_train_pipeline?' do
-    subject { pipeline.processables.first.merge_train_pipeline? }
-
-    context 'in a detached merge request pipeline' do
-      let(:pipeline) { detached_merge_request_pipeline }
-
-      it { is_expected.to eq(pipeline.merge_train_pipeline?) }
-    end
-
-    context 'in a legacy detached merge_request_pipeline' do
-      let(:pipeline) { legacy_detached_merge_request_pipeline }
-
-      it { is_expected.to eq(pipeline.merge_train_pipeline?) }
-    end
-
-    context 'in a pipeline for merged results' do
-      let(:pipeline) { merged_result_pipeline }
-
-      it { is_expected.to eq(pipeline.merge_train_pipeline?) }
-    end
+    it { is_expected.to delegate_method(:merge_train_pipeline?).to(:pipeline) }
   end
 end
