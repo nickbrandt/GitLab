1396177dccb8e308aa4df6c3427865de819af3be
Blair Lunceford <blunceford@gitlab.com>
2020-04-16 12:56:12 +0000

    Add LDAP User Filter to group link API

--- ee/lib/ee/api/entities/ldap_group_link.rb
+++ ee/lib/ee/api/entities/ldap_group_link.rb
@@ -5,6 +5,7 @@ module EE
     module Entities
       class LdapGroupLink < Grape::Entity
         expose :cn, :group_access, :provider
+        expose :filter, if: ->(_, _) { License.feature_available?(:ldap_group_sync_filter) }
       end
     end
   end
