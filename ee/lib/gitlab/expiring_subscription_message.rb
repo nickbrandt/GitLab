75e1d74efbc8a1565708f7359c6d7449e4a9b354
Jay Swain <jswain@gitlab.com>
2020-04-16 14:41:04 -0700

    Renewal banner has auto-renew specific messaging
    The expring subscribable banner has messaging specific to auto-renew
    
    part of: https://gitlab.com/gitlab-org/growth/product/-/issues/143

--- ee/lib/gitlab/expiring_subscription_message.rb
+++ ee/lib/gitlab/expiring_subscription_message.rb
@@ -35,7 +35,11 @@ module Gitlab
 
     def expired_subject
       if subscribable.block_changes?
-        _('Your subscription has been downgraded')
+        if auto_renew?
+          _('Something went wrong with your automatic subscription renewal')
+        else
+          _('Your subscription has been downgraded')
+        end
       else
         _('Your subscription expired!')
       end
@@ -44,7 +48,11 @@ module Gitlab
     def expiring_subject
       remaining_days = pluralize(subscribable.remaining_days, 'day')
 
-      _('Your subscription will expire in %{remaining_days}') % { remaining_days: remaining_days }
+      if auto_renew?
+        _('Your subscription will automatically renew in %{remaining_days}') % { remaining_days: remaining_days }
+      else
+        _('Your subscription will expire in %{remaining_days}') % { remaining_days: remaining_days }
+      end
     end
 
     def expiration_blocking_message
@@ -56,24 +64,40 @@ module Gitlab
     end
 
     def expired_message
-      if subscribable.block_changes?
-        if namespace
-          _('You didn\'t renew your %{strong}%{plan_name}%{strong_close} subscription for %{strong}%{namespace_name}%{strong_close} so it was downgraded to the free plan.') % { plan_name: plan_name, strong: strong, strong_close: strong_close, namespace_name: namespace.name }
-        else
-          _('You didn\'t renew your %{strong}%{plan_name}%{strong_close} subscription so it was downgraded to the GitLab Core Plan.') % { plan_name: plan_name, strong: strong, strong_close: strong_close }
-        end
-      else
-        remaining_days = pluralize((subscribable.block_changes_at - Date.today).to_i, 'day')
+      return block_changes_message if subscribable.block_changes?
 
-        _('No worries, you can still use all the %{strong}%{plan_name}%{strong_close} features for now. You have %{remaining_days} to renew your subscription.') % { plan_name: plan_name, remaining_days: remaining_days, strong: strong, strong_close: strong_close }
+      remaining_days = pluralize((subscribable.block_changes_at - Date.today).to_i, 'day')
+
+      _('No worries, you can still use all the %{strong}%{plan_name}%{strong_close} features for now. You have %{remaining_days} to renew your subscription.') % { plan_name: plan_name, remaining_days: remaining_days, strong: strong, strong_close: strong_close }
+    end
+
+    def block_changes_message
+      return namespace_block_changes_message if namespace
+
+      _('You didn\'t renew your %{strong}%{plan_name}%{strong_close} subscription so it was downgraded to the GitLab Core Plan.') % { plan_name: plan_name, strong: strong, strong_close: strong_close }
+    end
+
+    def namespace_block_changes_message
+      if auto_renew?
+        support_link = '<a href="mailto:support@gitlab.com">support@gitlab.com</a>'.html_safe
+
+        _('We tried to automatically renew your %{strong}%{plan_name}%{strong_close} subscription for %{strong}%{namespace_name}%{strong_close} on %{expires_on} but something went wrong so your subscription was downgraded to the free plan. Don\'t worry, your data is safe. We suggest you check your payment method and get in touch with our support team (%{support_link}). They\'ll gladly help with your subscription renewal.') % { plan_name: plan_name, strong: strong, strong_close: strong_close, namespace_name: namespace.name, support_link: support_link, expires_on: subscribable.expires_at.strftime("%Y-%m-%d") }
+      else
+        _('You didn\'t renew your %{strong}%{plan_name}%{strong_close} subscription for %{strong}%{namespace_name}%{strong_close} so it was downgraded to the free plan.') % { plan_name: plan_name, strong: strong, strong_close: strong_close, namespace_name: namespace.name }
       end
     end
 
     def expiring_message
-      if namespace
-        _('Your %{strong}%{plan_name}%{strong_close} subscription for %{strong}%{namespace_name}%{strong_close} will expire on %{strong}%{expires_on}%{strong_close}. After that, you will not to be able to create issues or merge requests as well as many other features.') % { expires_on: subscribable.expires_at.strftime("%Y-%m-%d"), plan_name: plan_name, strong: strong, strong_close: strong_close, namespace_name: namespace.name }
+      return namespace_expiring_message if namespace
+
+      _('Your %{strong}%{plan_name}%{strong_close} subscription will expire on %{strong}%{expires_on}%{strong_close}. After that, you will not to be able to create issues or merge requests as well as many other features.') % { expires_on: subscribable.expires_at.strftime("%Y-%m-%d"), plan_name: plan_name, strong: strong, strong_close: strong_close }
+    end
+
+    def namespace_expiring_message
+      if auto_renew?
+        _('We will automatically renew your %{strong}%{plan_name}%{strong_close} subscription for %{strong}%{namespace_name}%{strong_close} on %{strong}%{expires_on}%{strong_close}. There\'s nothing that you need to do, we\'ll let you know when the renewal is complete. Need more seats, a higher plan or just want to review your payment method?') % { expires_on: subscribable.expires_at.strftime("%Y-%m-%d"), plan_name: plan_name, strong: strong, strong_close: strong_close, namespace_name: namespace.name }
       else
-        _('Your %{strong}%{plan_name}%{strong_close} subscription will expire on %{strong}%{expires_on}%{strong_close}. After that, you will not to be able to create issues or merge requests as well as many other features.') % { expires_on: subscribable.expires_at.strftime("%Y-%m-%d"), plan_name: plan_name, strong: strong, strong_close: strong_close }
+        _('Your %{strong}%{plan_name}%{strong_close} subscription for %{strong}%{namespace_name}%{strong_close} will expire on %{strong}%{expires_on}%{strong_close}. After that, you will not to be able to create issues or merge requests as well as many other features.') % { expires_on: subscribable.expires_at.strftime("%Y-%m-%d"), plan_name: plan_name, strong: strong, strong_close: strong_close, namespace_name: namespace.name }
       end
     end
 
@@ -101,6 +125,10 @@ module Gitlab
 
     def strong_close
       '</strong>'.html_safe
+    end
+
+    def auto_renew?
+      subscribable.try(:auto_renew?)
     end
   end
 end
