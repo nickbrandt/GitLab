e7ef874e5a76ccea5ccd924b8176163ad0b193e9
ap4y <aevstifeev@gitlab.com>
2020-04-21 11:05:24 +1200

    Add usage data counter for network policy packet stats
    This MR adds redis based usage data counter that will be used for
    collecting usage metrics for network policy packet stats

---
+++ ee/spec/lib/gitlab/usage_data_counters/network_policy_counter_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Gitlab::UsageDataCounters::NetworkPolicyCounter, :clean_gitlab_redis_shared_state do
+  describe '.add' do
+    it 'increases drops and forwards counters' do
+      described_class.add(10, 5)
+      expect(described_class.totals).to eq(network_policy_forwards: 10, network_policy_drops: 5)
+
+      described_class.add(2, 1)
+      expect(described_class.totals).to eq(network_policy_forwards: 12, network_policy_drops: 6)
+    end
+  end
+end
