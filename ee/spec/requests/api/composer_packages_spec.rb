07fd8655efb5d7bb00a8a85bfea10a7a289267c7
Giorgenes Gelatti <ggelatti@gitlab.com>
2020-04-20 11:01:36 +1000

    Handle composer branch/tag retrieval
    Validates and retrieve branch or tag for
    PHP Composer package publish

--- ee/spec/requests/api/composer_packages_spec.rb
+++ ee/spec/requests/api/composer_packages_spec.rb
@@ -7,7 +7,7 @@ describe API::ComposerPackages do
   let_it_be(:user) { create(:user) }
   let_it_be(:group, reload: true) { create(:group, :public) }
   let_it_be(:personal_access_token) { create(:personal_access_token, user: user) }
-  let_it_be(:project, reload: true) { create(:project, :public) }
+  let_it_be(:project, reload: true) { create(:project, :repository, path: 'my.project') }
 
   describe 'GET /api/v4/group/:id/-/packages/composer/packages' do
     let(:url) { "/group/#{group.id}/-/packages/composer/packages.json" }
@@ -175,54 +175,104 @@ describe API::ComposerPackages do
 
   describe 'POST /api/v4/projects/:id/packages/composer' do
     let(:url) { "/projects/#{project.id}/packages/composer" }
-    let(:params) { { branch: 'foobar' } }
+    let(:params) { {} }
 
-    subject { post api(url), headers: headers }
+    subject { post api(url), headers: headers, params: params }
 
-    context 'with packages features enabled' do
+    shared_examples 'composer package publish' do
+      context 'with packages features enabled' do
+        before do
+          stub_licensed_features(packages: true)
+        end
+
+        context 'with valid project' do
+          using RSpec::Parameterized::TableSyntax
+
+          where(:project_visibility_level, :user_role, :member, :user_token, :shared_examples_name, :expected_status) do
+            'PUBLIC'  | :developer  | true  | true  | 'process Composer api request' | :created
+            'PUBLIC'  | :guest      | true  | true  | 'process Composer api request' | :forbidden
+            'PUBLIC'  | :developer  | true  | false | 'process Composer api request' | :unauthorized
+            'PUBLIC'  | :guest      | true  | false | 'process Composer api request' | :unauthorized
+            'PUBLIC'  | :developer  | false | true  | 'process Composer api request' | :forbidden
+            'PUBLIC'  | :guest      | false | true  | 'process Composer api request' | :forbidden
+            'PUBLIC'  | :developer  | false | false | 'process Composer api request' | :unauthorized
+            'PUBLIC'  | :guest      | false | false | 'process Composer api request' | :unauthorized
+            'PUBLIC'  | :anonymous  | false | true  | 'process Composer api request' | :unauthorized
+            'PRIVATE' | :developer  | true  | true  | 'process Composer api request' | :created
+            'PRIVATE' | :guest      | true  | true  | 'process Composer api request' | :forbidden
+            'PRIVATE' | :developer  | true  | false | 'process Composer api request' | :unauthorized
+            'PRIVATE' | :guest      | true  | false | 'process Composer api request' | :unauthorized
+            'PRIVATE' | :developer  | false | true  | 'process Composer api request' | :not_found
+            'PRIVATE' | :guest      | false | true  | 'process Composer api request' | :not_found
+            'PRIVATE' | :developer  | false | false | 'process Composer api request' | :unauthorized
+            'PRIVATE' | :guest      | false | false | 'process Composer api request' | :unauthorized
+            'PRIVATE' | :anonymous  | false | true  | 'process Composer api request' | :unauthorized
+          end
+
+          with_them do
+            let(:token) { user_token ? personal_access_token.token : 'wrong' }
+            let(:headers) { user_role == :anonymous ? {} : build_basic_auth_header(user.username, token) }
+
+            before do
+              project.update!(visibility_level: Gitlab::VisibilityLevel.const_get(project_visibility_level, false))
+            end
+
+            it_behaves_like params[:shared_examples_name], params[:user_role], params[:expected_status], params[:member]
+          end
+        end
+
+        it_behaves_like 'rejects Composer access with unknown project id'
+      end
+    end
+
+    it_behaves_like 'rejects Composer packages access with packages features disabled'
+
+    context 'with no tag or branch params' do
+      let(:headers) { build_basic_auth_header(user.username, personal_access_token.token) }
+
       before do
         stub_licensed_features(packages: true)
       end
 
-      context 'with valid project' do
-        using RSpec::Parameterized::TableSyntax
-
-        where(:project_visibility_level, :user_role, :member, :user_token, :shared_examples_name, :expected_status) do
-          'PUBLIC'  | :developer  | true  | true  | 'process Composer api request' | :created
-          'PUBLIC'  | :guest      | true  | true  | 'process Composer api request' | :forbidden
-          'PUBLIC'  | :developer  | true  | false | 'process Composer api request' | :unauthorized
-          'PUBLIC'  | :guest      | true  | false | 'process Composer api request' | :unauthorized
-          'PUBLIC'  | :developer  | false | true  | 'process Composer api request' | :forbidden
-          'PUBLIC'  | :guest      | false | true  | 'process Composer api request' | :forbidden
-          'PUBLIC'  | :developer  | false | false | 'process Composer api request' | :unauthorized
-          'PUBLIC'  | :guest      | false | false | 'process Composer api request' | :unauthorized
-          'PUBLIC'  | :anonymous  | false | true  | 'process Composer api request' | :unauthorized
-          'PRIVATE' | :developer  | true  | true  | 'process Composer api request' | :created
-          'PRIVATE' | :guest      | true  | true  | 'process Composer api request' | :forbidden
-          'PRIVATE' | :developer  | true  | false | 'process Composer api request' | :unauthorized
-          'PRIVATE' | :guest      | true  | false | 'process Composer api request' | :unauthorized
-          'PRIVATE' | :developer  | false | true  | 'process Composer api request' | :not_found
-          'PRIVATE' | :guest      | false | true  | 'process Composer api request' | :not_found
-          'PRIVATE' | :developer  | false | false | 'process Composer api request' | :unauthorized
-          'PRIVATE' | :guest      | false | false | 'process Composer api request' | :unauthorized
-          'PRIVATE' | :anonymous  | false | true  | 'process Composer api request' | :unauthorized
-        end
-
-        with_them do
-          let(:token) { user_token ? personal_access_token.token : 'wrong' }
-          let(:headers) { user_role == :anonymous ? {} : build_basic_auth_header(user.username, token) }
-
-          before do
-            project.update!(visibility_level: Gitlab::VisibilityLevel.const_get(project_visibility_level, false))
-          end
-
-          it_behaves_like params[:shared_examples_name], params[:user_role], params[:expected_status], params[:member]
-        end
-      end
-
-      it_behaves_like 'rejects Composer access with unknown project id'
+      it_behaves_like 'process Composer api request', :developer, :bad_request
     end
 
-    it_behaves_like 'rejects Composer packages access with packages features disabled'
+    context 'with a tag' do
+      context 'with an existing branch' do
+        let(:params) { { tag: 'v1.0.0' } }
+
+        it_behaves_like 'composer package publish'
+      end
+
+      context 'with a non existing tag' do
+        let(:params) { { tag: 'non-existing-tag' } }
+        let(:headers) { build_basic_auth_header(user.username, personal_access_token.token) }
+
+        before do
+          stub_licensed_features(packages: true)
+        end
+
+        it_behaves_like 'process Composer api request', :developer, :not_found
+      end
+    end
+
+    context 'with a branch' do
+      context 'with an existing branch' do
+        let(:params) { { branch: 'feature' } }
+
+        it_behaves_like 'composer package publish'
+      end
+
+      context 'with a non existing branch' do
+        let(:params) { { branch: 'non-existing-branch' } }
+        let(:headers) { build_basic_auth_header(user.username, personal_access_token.token) }
+
+        before do
+          stub_licensed_features(packages: true)
+        end
+
+        it_behaves_like 'process Composer api request', :developer, :not_found
+      end
+    end
   end
 end
