dc3c560e4e50c3e2bb4adb7144145e2d15568d25
Erick Banks <ebanks@gitlab.com>
2020-04-21 21:08:18 +0000

    Use best practices in elasticsearch test files
    Use let instead of instance variables
    Move helper methods to Flow::Project module
    Change from before(:all) to just before
    Do same with after(:all)

--- qa/qa/specs/features/ee/api/enablement/elasticsearch/elasticsearch_api_spec.rb
+++ qa/qa/specs/features/ee/api/enablement/elasticsearch/elasticsearch_api_spec.rb
@@ -6,14 +6,21 @@ module QA
   context 'Enablement:Search' do
     include Support::Api
     describe 'When using elasticsearch API to search for a known blob', :orchestrated, :elasticsearch, :requires_admin, quarantine: { type: :new } do
-      before(:all) do
-        @api_client = Runtime::API::Client.new(:gitlab)
-        @project_file_content = "elasticsearch: #{SecureRandom.hex(8)}"
-        non_member_user = Resource::User.fabricate_or_use('non_member_user', 'non_member_user_password')
-        @non_member_api_client = Runtime::API::Client.new(user: non_member_user)
-        @elasticsearch_original_state_on = Runtime::Search.elasticsearch_on?(@api_client)
+      let(:project_file_content) { "elasticsearch: #{SecureRandom.hex(8)}" }
+      let(:non_member_user) { Resource::User.fabricate_or_use('non_member_user', 'non_member_user_password') }
+      let(:api_client) { Runtime::API::Client.new(:gitlab) }
+      let(:non_member_api_client) { Runtime::API::Client.new(user: non_member_user) }
 
-        unless @elasticsearch_original_state_on
+      let(:project) do
+        Resource::Project.fabricate_via_api! do |project|
+          project.name = "api-es-#{SecureRandom.hex(8)}"
+        end
+      end
+
+      let(:elasticsearch_original_state_on?) { Runtime::Search.elasticsearch_on?(api_client) }
+
+      before do
+        unless elasticsearch_original_state_on?
           QA::EE::Resource::Settings::Elasticsearch.fabricate_via_api!
           sleep(60)
           # wait for the change to propagate before inserting records or else
@@ -23,32 +30,36 @@ module QA
           # as per this issue https://gitlab.com/gitlab-org/quality/team-tasks/issues/395
         end
 
-        @project = Runtime::Project.create_project("api-es-#{SecureRandom.hex(8)}", @api_client)
-        Runtime::Project.push_file_to_project(@project, 'README.md', @project_file_content)
+        Resource::Repository::Commit.fabricate_via_api! do |commit|
+          commit.project = project
+          commit.add_files([
+            { file_path: 'README.md', content: project_file_content }
+          ])
+        end
       end
 
-      after(:all) do
-        if !@elasticsearch_original_state_on && !@api_client.nil?
-          Runtime::Search.disable_elasticsearch(@api_client)
+      after do
+        if !elasticsearch_original_state_on? && !api_client.nil?
+          Runtime::Search.disable_elasticsearch(api_client)
         end
       end
 
       it 'searches public project and finds a blob as an non-member user' do
-        successful_search(@non_member_api_client)
+        successful_search(non_member_api_client)
       end
 
       describe 'When searching a private repository' do
-        before(:all) do
-          Runtime::Project.set_project_visibility(@api_client, @project.id, 'private')
+        before do
+          project.set_visibility(:private)
         end
 
         it 'finds a blob as an authorized user' do
-          successful_search(@api_client)
+          successful_search(api_client)
         end
 
         it 'does not find a blob as an non-member user' do
           QA::Support::Retrier.retry_on_exception(max_attempts: 10, sleep_interval: 3) do
-            get Runtime::Search.create_search_request(@non_member_api_client, 'blobs', @project_file_content).url
+            get Runtime::Search.create_search_request(non_member_api_client, 'blobs', project_file_content).url
             expect_status(QA::Support::Api::HTTP_STATUS_OK)
             expect(json_body).to be_empty
           end
@@ -59,13 +70,13 @@ module QA
 
       def successful_search(api_client)
         QA::Support::Retrier.retry_on_exception(max_attempts: 10, sleep_interval: 3) do
-          get Runtime::Search.create_search_request(api_client, 'blobs', @project_file_content).url
+          get Runtime::Search.create_search_request(api_client, 'blobs', project_file_content).url
           expect_status(QA::Support::Api::HTTP_STATUS_OK)
 
           raise 'Empty search result returned' if json_body.empty?
 
-          expect(json_body[0][:data]).to match(@project_file_content)
-          expect(json_body[0][:project_id]).to equal(@project.id)
+          expect(json_body[0][:data]).to match(project_file_content)
+          expect(json_body[0][:project_id]).to equal(project.id)
         end
       end
     end
