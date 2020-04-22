195dcebf3a96c0d8d1ba96222a93f101c2585c02
can eldem <eldemcan@gmail.com>
2020-04-22 16:16:21 +0100

    Sort DS analysers according to scanner id

--- ee/app/services/security/merge_reports_service.rb
+++ ee/app/services/security/merge_reports_service.rb
@@ -2,6 +2,15 @@
 
 module Security
   class MergeReportsService
+    ANALYZER_ORDER = {
+      "bundler_audit" => 1,
+      "retire.js" =>  2,
+      "gemnasium" => 3,
+      "gemnasium-maven" => 3,
+      "gemnasium-python" => 3,
+      "unknown" => 999
+    }.freeze
+
     IdentifierKey = Struct.new(:location_sha, :identifier_type, :identifier_value) do
       def ==(other)
         location_sha == other.location_sha &&
@@ -18,6 +27,8 @@ module Security
 
     def initialize(*source_reports)
       @source_reports = source_reports
+      # temporary sort https://gitlab.com/gitlab-org/gitlab/-/issues/213839
+      sort_by_ds_analyzers!
       @target_report = ::Gitlab::Ci::Reports::Security::Report.new(
         @source_reports.first.type,
         @source_reports.first.commit_sha,
@@ -109,6 +120,20 @@ module Security
       sort_occurrences!
 
       @occurrences.each { |occurrence| @target_report.add_occurrence(occurrence) }
+    end
+
+    def sort_by_ds_analyzers!
+      return if @source_reports.any? { |x| x.type != :dependency_scanning }
+
+      @source_reports.sort! do |a, b|
+        a_scanner_id, b_scanner_id = a.scanners.values[0].external_id, b.scanners.values[0].external_id
+
+        # for custom analyzers
+        a_scanner_id = "unknown" if ANALYZER_ORDER[a_scanner_id].nil?
+        b_scanner_id = "unknown" if ANALYZER_ORDER[b_scanner_id].nil?
+
+        ANALYZER_ORDER[a_scanner_id] <=> ANALYZER_ORDER[b_scanner_id]
+      end
     end
   end
 end
