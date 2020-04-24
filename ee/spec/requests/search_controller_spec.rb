d4a9126c2484d9d384c08ee26f684c13a6a64782
Tan Le <tle@gitlab.com>
2020-04-24 15:10:01 +1000

    Preload project associations on web search results
    This is to mirror the preloads logic on Projects::Dashboard
    controller. The dashboard and search view both share the same project
    view partial (i.e. `_project.html.haml`) and require similar project
    data for presentation.
    
    The N+1 test was failing to pick up this N+1 issue as it uses the same
    associations for project test data and these queries are cached. This
    change ensures that we skip cached queries and count everything.

--- ee/spec/requests/search_controller_spec.rb
+++ ee/spec/requests/search_controller_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe SearchController, type: :request do
   let_it_be(:user) { create(:user) }
   let_it_be(:group) { create(:group) }
-  let(:project) { create(:project, :public, :repository, :wiki_repo, name: 'awesome project', group: group) }
+  let_it_be(:project) { create(:project, :public, :repository, :wiki_repo, name: 'awesome project', group: group) }
 
   before_all do
     login_as(user)
@@ -20,6 +20,7 @@ describe SearchController, type: :request do
       before do
         stub_ee_application_setting(elasticsearch_search: true, elasticsearch_indexing: true)
       end
+
       context 'for merge_request scope' do
         before do
           create(:merge_request, target_branch: 'feature_1', source_project: project)
@@ -28,6 +29,7 @@ describe SearchController, type: :request do
           create(:merge_request, target_branch: 'feature_4', source_project: project)
           ensure_elasticsearch_index!
         end
+
         it 'avoids N+1 queries' do
           control = ActiveRecord::QueryRecorder.new(skip_cached: false) { send_search_request(scope: 'merge_requests', search: '*') }
 
@@ -40,7 +42,30 @@ describe SearchController, type: :request do
 
           # some N+1 queries still exist
           expect { send_search_request(scope: 'merge_requests', search: '*') }
-              .not_to exceed_all_query_limit(control.count + 2)
+            .not_to exceed_all_query_limit(control)
+        end
+      end
+
+      context 'for project scope' do
+        before do
+          create(:project, :public)
+          ensure_elasticsearch_index!
+        end
+
+        it 'avoids N+1 queries' do
+          control = ActiveRecord::QueryRecorder.new(skip_cached: false) { send_search_request(scope: 'project', search: '*') }
+
+          create_list(:project, 3, :public)
+
+          ensure_elasticsearch_index!
+
+          # some N+1 queries still exist
+          # each project requires 3 extra queries
+          #   - one count for forks
+          #   - one count for open MRs
+          #   - one count for open Issues
+          expect { send_search_request(scope: 'project', search: '*') }
+            .not_to exceed_all_query_limit(control).with_threshold(9)
         end
       end
     end
