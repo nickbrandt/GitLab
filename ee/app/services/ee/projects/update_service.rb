c5dc353169979cf6d18d0c47040b098017058eb1
Nick Thomas <nick@gitlab.com>
2020-04-24 15:46:05 +0100

    Change the mirror user along with pull mirror settings
    Pull mirroring stores a "mirror_user", which is used to attribute
    changes performed by the mirroring option and, in some cases, for
    checking permissions. In general, the mirror user should be the user
    who set up the pull mirror. However, **changes** to mirror attributes
    do not reset the mirror_user at present.
    
    This property allows a mirror to be changed by one project maintainer,
    and for the changes to be wrongly attributed to another maintainer. In
    the worst case, this could lead to permissions bypasses or credential
    theft.
    
    To mitigate this, we remove the ability to specify who the mirror user
    will be, and ensure it is always set to the ID of the user who modifies
    the pull mirroring settings. The only exception is if you're an admin
    user making changes via the API - in this circumstance, we allow the
    mirror_user_id to be set arbitrarily. This preserves an API endpoint
    that may be widely used.

--- ee/app/services/ee/projects/update_service.rb
+++ ee/app/services/ee/projects/update_service.rb
@@ -6,19 +6,27 @@ module EE
       extend ::Gitlab::Utils::Override
       include CleanupApprovers
 
+      PULL_MIRROR_ATTRIBUTES = %i[
+        mirror
+        mirror_user_id
+        import_url
+        username_only_import_url
+        mirror_trigger_builds
+        only_mirror_protected_branches
+        mirror_overwrites_diverged_branches
+        pull_mirror_branch_prefix
+        import_data_attributes
+      ].freeze
+
       override :execute
       def execute
         should_remove_old_approvers = params.delete(:remove_old_approvers)
+        limit = params.delete(:repository_size_limit)
         wiki_was_enabled = project.wiki_enabled?
 
-        limit = params.delete(:repository_size_limit)
-
-        unless valid_mirror_user?
-          project.errors.add(:mirror_user_id, 'is invalid')
-          return project
-        end
-
+        mirror_user_setting
         compliance_framework_setting
+        return update_failed! if project.errors.any?
 
         result = super do
           # Repository size limit comes as MB from the view
@@ -41,13 +49,19 @@ module EE
 
       private
 
-      def valid_mirror_user?
-        return true unless params[:mirror_user_id].present?
+      # A user who changes any aspect of pull mirroring settings must be made
+      # into the mirror user, to prevent them from acquiring capabilities
+      # owned by the previous user, such as writing to a protected branch.
+      #
+      # Only admins can set the mirror user to be an arbitrary user.
+      def mirror_user_setting
+        return unless PULL_MIRROR_ATTRIBUTES.any? { |symbol| params.key?(symbol) }
 
-        mirror_user_id = params[:mirror_user_id].to_i
-
-        mirror_user_id == current_user.id ||
-          mirror_user_id == project.mirror_user&.id
+        if params[:mirror_user_id] && params[:mirror_user_id] != project.mirror_user_id
+          project.errors.add(:mirror_user_id, 'is invalid') unless current_user&.admin?
+        else
+          params[:mirror_user_id] = current_user.id
+        end
       end
 
       def compliance_framework_setting
--- ee/spec/controllers/projects_controller_spec.rb
+++ ee/spec/controllers/projects_controller_spec.rb
@@ -271,7 +271,6 @@ describe ProjectsController do
         {
           mirror: true,
           mirror_trigger_builds: true,
-          mirror_user_id: user.id,
           import_url: 'https://example.com'
         }
       end
@@ -296,6 +295,20 @@ describe ProjectsController do
           expect(project.mirror_trigger_builds).to eq(true)
           expect(project.mirror_user).to eq(user)
           expect(project.import_url).to eq('https://example.com')
+        end
+
+        it 'ignores mirror_user_id' do
+          other_user = create(:user)
+
+          put :update,
+            params: {
+              namespace_id: project.namespace,
+              id: project,
+              project: params.merge(mirror_user_id: other_user.id)
+            }
+          project.reload
+
+          expect(project.mirror_user).to eq(user)
         end
       end
 
