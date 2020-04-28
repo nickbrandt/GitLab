1d17fd30f52ef42e66d92d0796dcb2b2c3ea4485
Heinrich Lee Yu <heinrich@gitlab.com>
2020-04-29 05:18:08 +0800

    Improve method of retrieving session

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -5,23 +5,18 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_current_user
+      self.current_user = find_user_from_session_store
     end
 
     private
 
-    def find_current_user
-      raw_session = Gitlab::Redis::SharedState.with { |redis| redis.get("#{Gitlab::Redis::SharedState::SESSION_NAMESPACE}:#{session_id}") }
-      return unless raw_session
-
-      data = Marshal.load(raw_session) # rubocop:disable Security/MarshalLoad
-      user_id = data['warden.user.user.key']&.first
-
-      User.find_by_id(user_id) if user_id
+    def find_user_from_session_store
+      session = ActiveSession.sessions_from_ids([session_id]).first
+      Warden::SessionSerializer.new('rack.session' => session).fetch(:user)
     end
 
     def session_id
-      cookies[Gitlab::Application.config.session_options[:key]]
+      Rack::Session::SessionId.new(cookies[Gitlab::Application.config.session_options[:key]])
     end
   end
 end
