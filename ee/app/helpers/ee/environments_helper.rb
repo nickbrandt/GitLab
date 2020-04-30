f7b2740ca7b519cfd136fd751c69e3f5505b9a4e
Ryan Cobb <rcobb@gitlab.com>
2020-04-30 16:10:47 -0700

    Expect metric_data key

--- ee/app/helpers/ee/environments_helper.rb
+++ ee/app/helpers/ee/environments_helper.rb
@@ -32,10 +32,20 @@ module EE
 
     override :project_metrics_data
     def project_metrics_data(project)
-      ee_project_metrics_data = {}
-      ee_project_metrics_data['logs_path'] = project_logs_path(project) if can?(current_user, :read_pod_logs, project)
+      ee_metrics_data = {}
+      ee_metrics_data['logs_path'] = project_logs_path(project) if can?(current_user, :read_pod_logs, project)
 
-      super.merge(ee_project_metrics_data)
+      super.merge(ee_metrics_data)
+    end
+
+    override :project_and_environment_metrics_data
+    def project_and_environment_metrics_data(project, environment)
+      ee_metrics_data = {}
+
+      # overwrites project_metrics_data logs_path if environment is available
+      ee_metrics_data['logs_path'] = project_logs_path(project, environment_name: environment.name) if can?(current_user, :read_pod_logs, project)
+
+      super.merge(ee_metrics_data)
     end
   end
 end
