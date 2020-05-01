8b7ad815081caa3cf2f977e5015ec13c39c5a367
Luke Duncalfe <lduncalfe@eml.cc>
2020-05-01 16:04:17 +1200

    Move Design Management routes, controllers to FOSS
    This change is part of
    https://gitlab.com/gitlab-org/gitlab/-/issues/212566 to move all Design
    Management code to FOSS.
    
    This change moves the routes and controllers.

--- ee/config/routes/issues.rb
+++ ee/config/routes/issues.rb
@@ -4,7 +4,6 @@ resources :issues, only: [], constraints: { id: /\d+/ } do
   member do
     get '/descriptions/:version_id/diff', action: :description_diff, as: :description_diff
     delete '/descriptions/:version_id', action: :delete_description_version, as: :delete_description_version
-    get '/designs(/*vueroute)', to: 'issues#designs', as: :designs, format: false
   end
 
   collection do
