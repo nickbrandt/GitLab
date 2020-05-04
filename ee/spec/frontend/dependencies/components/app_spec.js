72849ad6ec01dad3c79f6767df8d79a28980906b
Mark Florian <mflorian@gitlab.com>
2020-05-04 20:08:53 +0100

    maybe that's it?

--- ee/spec/frontend/dependencies/components/app_spec.js
+++ ee/spec/frontend/dependencies/components/app_spec.js
@@ -185,9 +185,9 @@ describe('DependenciesApp component', () => {
         return wrapper.vm.$nextTick();
       });
 
-      it('shows both dependencies tables with the correct props', () => {
+      it('shows the dependencies table with the correct props', () => {
         expectHeader();
-        expectDependenciesTables();
+        expectDependenciesTable();
       });
 
       it('shows a link to the latest job', () => {
@@ -202,75 +202,8 @@ describe('DependenciesApp component', () => {
         expect(findHeaderHelpLink().attributes('href')).toBe(TEST_HOST);
       });
 
-      it('displays the tabs correctly', () => {
-        const expected = [
-          {
-            text: 'All',
-            total: '2',
-          },
-          {
-            text: 'Vulnerable',
-            total: '1',
-          },
-        ];
-
-        const tabs = findTabControls();
-        expected.forEach(({ text, total }, i) => {
-          const tab = tabs.at(i);
-          expect(tab.text()).toEqual(expect.stringContaining(text));
-          expect(
-            tab
-              .find(GlBadge)
-              .text()
-              .trim(),
-          ).toEqual(total);
-        });
-      });
-
       it('passes the correct namespace to dependencies actions component', () => {
         expectComponentWithProps(DependenciesActions, { namespace: allNamespace });
-      });
-
-      describe('given the user clicks on the vulnerable tab', () => {
-        beforeEach(() => {
-          findVulnerableTabControl().trigger('click');
-
-          return wrapper.vm.$nextTick();
-        });
-
-        it('changes the current list', () => {
-          expect(store.dispatch).toHaveBeenCalledWith('setCurrentList', vulnerableNamespace);
-        });
-      });
-
-      describe('given the current list is the vulnerable dependencies list', () => {
-        const namespace = vulnerableNamespace;
-        beforeEach(() => {
-          store.state.currentList = namespace;
-
-          return wrapper.vm.$nextTick();
-        });
-
-        it('passes the correct namespace to dependencies actions component', () => {
-          expectComponentWithProps(DependenciesActions, { namespace });
-        });
-      });
-
-      it('has enabled vulnerable tab', () => {
-        expect(findVulnerableTabComponent().classes('disabled')).toBe(false);
-      });
-
-      describe('given there are no vulnerable dependencies', () => {
-        beforeEach(() => {
-          store.state[vulnerableNamespace].dependencies = [];
-          store.state[vulnerableNamespace].pageInfo.total = 0;
-
-          return wrapper.vm.$nextTick();
-        });
-
-        it('disables the vulnerable tab', () => {
-          expect(findVulnerableTabComponent().classes('disabled')).toBe(true);
-        });
       });
 
       describe('given the user has public permissions', () => {
@@ -308,7 +241,7 @@ describe('DependenciesApp component', () => {
         });
       });
 
-      it('shows both dependencies tables with the correct props', expectDependenciesTables);
+      it('shows the dependencies table with the correct props', expectDependenciesTable);
 
       describe('when the job failure alert emits the dismiss event', () => {
         beforeEach(() => {
@@ -334,7 +267,7 @@ describe('DependenciesApp component', () => {
         expectComponentWithProps(DependencyListIncompleteAlert);
       });
 
-      it('shows both dependencies tables with the correct props', expectDependenciesTables);
+      it('shows the dependencies table with the correct props', expectDependenciesTable);
 
       describe('when the incomplete-list alert emits the dismiss event', () => {
         beforeEach(() => {
@@ -359,159 +292,6 @@ describe('DependenciesApp component', () => {
         expectComponentPropsToMatchSnapshot(GlEmptyState);
         expectNoHeader();
         expectNoDependenciesTables();
-      });
-    });
-  });
-
-  describe('given the dependencyListUi feature flag is enabled', () => {
-    describe('on creation', () => {
-      beforeEach(() => {
-        factory({ provide: { glFeatures: { dependencyListUi: true } } });
-      });
-
-      it('dispatches the correct initial actions', () => {
-        expect(store.dispatch.mock.calls).toEqual([
-          ['setDependenciesEndpoint', basicAppProps.endpoint],
-          ['fetchDependencies'],
-        ]);
-      });
-
-      it('shows only the loading icon', () => {
-        expectComponentWithProps(GlLoadingIcon);
-        expectNoHeader();
-        expectNoDependenciesTables();
-      });
-
-      describe('given the dependency list job has not yet run', () => {
-        beforeEach(() => {
-          setStateJobNotRun();
-
-          return wrapper.vm.$nextTick();
-        });
-
-        it('shows only the empty state', () => {
-          expectComponentWithProps(GlEmptyState, { svgPath: basicAppProps.emptyStateSvgPath });
-          expectComponentPropsToMatchSnapshot(GlEmptyState);
-          expectNoHeader();
-          expectNoDependenciesTables();
-        });
-      });
-
-      describe('given a list of dependencies and ok report', () => {
-        beforeEach(() => {
-          setStateLoaded();
-
-          return wrapper.vm.$nextTick();
-        });
-
-        it('shows the dependencies table with the correct props', () => {
-          expectHeader();
-          expectDependenciesTable();
-        });
-
-        it('shows a link to the latest job', () => {
-          expect(findHeaderJobLink().attributes('href')).toBe('/jobs/foo/321');
-        });
-
-        it('shows when the last job ran', () => {
-          expect(findHeader().text()).toContain('1 week ago');
-        });
-
-        it('shows a link to the dependencies documentation page', () => {
-          expect(findHeaderHelpLink().attributes('href')).toBe(TEST_HOST);
-        });
-
-        it('passes the correct namespace to dependencies actions component', () => {
-          expectComponentWithProps(DependenciesActions, { namespace: allNamespace });
-        });
-
-        describe('given the user has public permissions', () => {
-          beforeEach(() => {
-            store.state[allNamespace].reportInfo.generatedAt = '';
-            store.state[allNamespace].reportInfo.jobPath = '';
-
-            return wrapper.vm.$nextTick();
-          });
-
-          it('shows the header', () => {
-            expectHeader();
-          });
-
-          it('does not show when the last job ran', () => {
-            expect(findHeader().text()).not.toContain('1 week ago');
-          });
-
-          it('does not show a link to the latest job', () => {
-            expect(findHeaderJobLink().exists()).toBe(false);
-          });
-        });
-      });
-
-      describe('given the dependency list job failed', () => {
-        beforeEach(() => {
-          setStateJobFailed();
-
-          return wrapper.vm.$nextTick();
-        });
-
-        it('passes the correct props to the job failure alert', () => {
-          expectComponentWithProps(DependencyListJobFailedAlert, {
-            jobPath: '/jobs/foo/321',
-          });
-        });
-
-        it('shows the dependencies table with the correct props', expectDependenciesTable);
-
-        describe('when the job failure alert emits the dismiss event', () => {
-          beforeEach(() => {
-            const alertWrapper = findJobFailedAlert();
-            alertWrapper.vm.$emit('dismiss');
-            return wrapper.vm.$nextTick();
-          });
-
-          it('does not render the job failure alert', () => {
-            expect(findJobFailedAlert().exists()).toBe(false);
-          });
-        });
-      });
-
-      describe('given a dependency list which is known to be incomplete', () => {
-        beforeEach(() => {
-          setStateListIncomplete();
-
-          return wrapper.vm.$nextTick();
-        });
-
-        it('passes the correct props to the incomplete-list alert', () => {
-          expectComponentWithProps(DependencyListIncompleteAlert);
-        });
-
-        it('shows the dependencies table with the correct props', expectDependenciesTable);
-
-        describe('when the incomplete-list alert emits the dismiss event', () => {
-          beforeEach(() => {
-            const alertWrapper = findIncompleteListAlert();
-            alertWrapper.vm.$emit('dismiss');
-            return wrapper.vm.$nextTick();
-          });
-
-          it('does not render the incomplete-list alert', () => {
-            expect(findIncompleteListAlert().exists()).toBe(false);
-          });
-        });
-      });
-
-      describe('given there are no dependencies detected', () => {
-        beforeEach(() => {
-          setStateNoDependencies();
-        });
-
-        it('shows only the empty state', () => {
-          expectComponentWithProps(GlEmptyState, { svgPath: basicAppProps.emptyStateSvgPath });
-          expectComponentPropsToMatchSnapshot(GlEmptyState);
-          expectNoHeader();
-          expectNoDependenciesTables();
-        });
       });
     });
   });
