30886a18db327146853579ce583928d4bf45c071
Mehmet Emin INAC <minac@gitlab.com>
2020-05-05 06:41:03 +0200

    Check access to instance-level vulnerability export APIs
    We restrict access to these endpoints if the `security_dashboard`
    feature is not available.

--- ee/app/policies/instance_security_dashboard_policy.rb
+++ ee/app/policies/instance_security_dashboard_policy.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class InstanceSecurityDashboardPolicy < BasePolicy
-  rule { ~anonymous }.policy do
-    enable :read_instance_security_dashboard
-    enable :create_vulnerability_export
+  with_scope :global
+  condition(:security_dashboard_enabled) do
+    License.feature_available?(:security_dashboard)
   end
+
+  rule { ~anonymous }.enable :read_instance_security_dashboard
+  rule { security_dashboard_enabled & can?(:read_instance_security_dashboard) }.enable :create_vulnerability_export
 end
