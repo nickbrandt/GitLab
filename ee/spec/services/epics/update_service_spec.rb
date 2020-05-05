a8f3c575adc1d7d5df645d42ab981d2674cb9ad1
Heinrich Lee Yu <heinrich@gitlab.com>
2020-05-06 04:05:03 +0800

    Support quick actions when editing descriptions
    This applies to issues, MRs, and Epics

--- ee/spec/services/epics/update_service_spec.rb
+++ ee/spec/services/epics/update_service_spec.rb
@@ -312,5 +312,15 @@ RSpec.describe Epics::UpdateService do
       let(:issuable) { epic }
       let(:parent) { group }
     end
+
+    context 'with quick actions in the description' do
+      let(:label) { create(:group_label, group: group) }
+
+      it 'adds labels to the epic' do
+        update_epic(description: "/label ~#{label.name}")
+
+        expect(epic.label_ids).to contain_exactly(label.id)
+      end
+    end
   end
 end
