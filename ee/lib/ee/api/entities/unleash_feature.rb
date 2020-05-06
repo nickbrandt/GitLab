7e3604dd44c38e94c4c9474752eb4830c483d1c1
Jason Goodman <jgoodman@gitlab.com>
2020-05-06 14:31:21 +0000

    Add associations for feature flag strategy user lists
    Require user list for gitlabUserList strategy
    Forbid user list for other strategies

--- ee/lib/ee/api/entities/unleash_feature.rb
+++ ee/lib/ee/api/entities/unleash_feature.rb
@@ -7,7 +7,27 @@ module EE
         expose :name
         expose :description, unless: ->(feature) { feature.description.nil? }
         expose :active, as: :enabled
-        expose :strategies, using: UnleashStrategy
+        expose :strategies do |flag|
+          flag.strategies.map do |strategy|
+            if legacy_strategy?(strategy)
+              UnleashLegacyStrategy.represent(strategy)
+            elsif gitlab_user_list_strategy?(strategy)
+              UnleashGitlabUserListStrategy.represent(strategy)
+            else
+              UnleashStrategy.represent(strategy)
+            end
+          end
+        end
+
+        private
+
+        def legacy_strategy?(strategy)
+          !strategy.respond_to?(:name)
+        end
+
+        def gitlab_user_list_strategy?(strategy)
+          strategy.name == ::Operations::FeatureFlags::Strategy::STRATEGY_GITLABUSERLIST
+        end
       end
     end
   end
