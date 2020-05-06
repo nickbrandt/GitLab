460d7af3d2c7d22c3b027461e8ab445bf3b93d90
Alan (Maciej) Paruszewski <mparuszewski@gitlab.com>
2020-05-06 01:40:45 +0000

    Modify mutation to add Project to Instance Security Dashboard
    This change introduces small improvement to AddProject mutation to not
    accept multiple project ids, but only one and let GraphQL manage
    multiple mutations to add multiple projects to Security Dashboard.

---
+++ ee/app/graphql/mutations/security_dashboard/add_project.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module Mutations
+  module SecurityDashboard
+    class AddProject < BaseMutation
+      graphql_name 'AddProjectToSecurityDashboard'
+
+      authorize :read_vulnerability
+
+      field :project, Types::ProjectType,
+            null: true,
+            description: 'Project that was added to the Instance Security Dashboard'
+
+      argument :id, GraphQL::ID_TYPE,
+               required: true,
+               description: 'ID of the project to be added to Instance Security Dashboard'
+
+      def resolve(id:)
+        project = authorized_find!(id: id)
+        result = add_project(project)
+
+        {
+          project: result ? project : nil,
+          errors: result ? [] : ['The project already belongs to your dashboard or you don\'t have permission to perform this action']
+        }
+      end
+
+      private
+
+      def find_object(id:)
+        GitlabSchema.object_from_id(id)
+      end
+
+      def add_project(project)
+        Dashboard::Projects::CreateService
+          .new(current_user, current_user.security_dashboard_projects, feature: :security_dashboard)
+          .execute([project.id])
+          .then { |result| result.added_project_ids.include?(project.id) }
+      end
+    end
+  end
+end
