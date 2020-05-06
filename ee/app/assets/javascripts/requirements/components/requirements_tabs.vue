2946e918eb6acb2300b79a0fab009d336a6ba1c0
Kushal Pandya <kushalspandya@gmail.com>
2020-05-06 14:38:00 +0530

    Move tabs within Requirements Vue app
    - Moves Requirements tabs into Vue app.
    - Makes Requirements count more reliable by refetching it from backend
    on mutation.

---
+++ ee/app/assets/javascripts/requirements/components/requirements_tabs.vue
@@ -0,0 +1,93 @@
+<script>
+import { GlLink, GlBadge, GlButton } from '@gitlab/ui';
+
+import { FilterState } from '../constants';
+
+export default {
+  FilterState,
+  components: {
+    GlLink,
+    GlBadge,
+    GlButton,
+  },
+  props: {
+    filterBy: {
+      type: String,
+      required: true,
+    },
+    requirementsCount: {
+      type: Object,
+      required: true,
+    },
+    showCreateForm: {
+      type: Boolean,
+      required: true,
+    },
+    canCreateRequirement: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  computed: {
+    isOpenTab() {
+      return this.filterBy === FilterState.opened;
+    },
+    isArchivedTab() {
+      return this.filterBy === FilterState.archived;
+    },
+    isAllTab() {
+      return this.filterBy === FilterState.all;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="top-area">
+    <ul class="nav-links mobile-separator requirements-state-filters js-requirements-state-filters">
+      <li :class="{ active: isOpenTab }">
+        <gl-link
+          id="state-opened"
+          data-state="opened"
+          :title="__('Filter by requirements that are currently opened.')"
+          @click="$emit('clickTab', { filterBy: $options.FilterState.opened })"
+        >
+          {{ __('Open') }}
+          <gl-badge class="badge-pill">{{ requirementsCount.OPENED }}</gl-badge>
+        </gl-link>
+      </li>
+      <li :class="{ active: isArchivedTab }">
+        <gl-link
+          id="state-archived"
+          data-state="archived"
+          :title="__('Filter by requirements that are currently archived.')"
+          @click="$emit('clickTab', { filterBy: $options.FilterState.archived })"
+        >
+          {{ __('Archived') }}
+          <gl-badge class="badge-pill">{{ requirementsCount.ARCHIVED }}</gl-badge>
+        </gl-link>
+      </li>
+      <li :class="{ active: isAllTab }">
+        <gl-link
+          id="state-all"
+          data-state="all"
+          :title="__('Show all requirements.')"
+          @click="$emit('clickTab', { filterBy: $options.FilterState.all })"
+        >
+          {{ __('All') }}
+          <gl-badge class="badge-pill">{{ requirementsCount.ALL }}</gl-badge>
+        </gl-link>
+      </li>
+    </ul>
+    <div v-if="isOpenTab && canCreateRequirement" class="nav-controls">
+      <gl-button
+        category="primary"
+        variant="success"
+        class="js-new-requirement qa-new-requirement-button"
+        :disabled="showCreateForm"
+        @click="$emit('clickNewRequirement')"
+        >{{ __('New requirement') }}</gl-button
+      >
+    </div>
+  </div>
+</template>
---
+++ ee/spec/frontend/requirements/components/requirements_tabs_spec.js
@@ -0,0 +1,123 @@
+import { shallowMount } from '@vue/test-utils';
+
+import { GlLink, GlBadge, GlButton } from '@gitlab/ui';
+
+import RequirementsTabs from 'ee/requirements/components/requirements_tabs.vue';
+import { FilterState } from 'ee/requirements/constants';
+
+import { mockRequirementsCount } from '../mock_data';
+
+const createComponent = ({
+  filterBy = FilterState.opened,
+  requirementsCount = mockRequirementsCount,
+  showCreateForm = false,
+  canCreateRequirement = true,
+} = {}) =>
+  shallowMount(RequirementsTabs, {
+    propsData: {
+      filterBy,
+      requirementsCount,
+      showCreateForm,
+      canCreateRequirement,
+    },
+  });
+
+describe('RequirementsTabs', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = createComponent();
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe('template', () => {
+    it('renders "Open" tab', () => {
+      const tabEl = wrapper.findAll(GlLink).at(0);
+
+      expect(tabEl.attributes('id')).toBe('state-opened');
+      expect(tabEl.attributes('data-state')).toBe('opened');
+      expect(tabEl.attributes('title')).toBe('Filter by requirements that are currently opened.');
+      expect(tabEl.text()).toContain('Open');
+      expect(tabEl.find(GlBadge).text()).toBe(`${mockRequirementsCount.OPENED}`);
+    });
+
+    it('renders "Archived" tab', () => {
+      const tabEl = wrapper.findAll(GlLink).at(1);
+
+      expect(tabEl.attributes('id')).toBe('state-archived');
+      expect(tabEl.attributes('data-state')).toBe('archived');
+      expect(tabEl.attributes('title')).toBe('Filter by requirements that are currently archived.');
+      expect(tabEl.text()).toContain('Archived');
+      expect(tabEl.find(GlBadge).text()).toBe(`${mockRequirementsCount.ARCHIVED}`);
+    });
+
+    it('renders "All" tab', () => {
+      const tabEl = wrapper.findAll(GlLink).at(2);
+
+      expect(tabEl.attributes('id')).toBe('state-all');
+      expect(tabEl.attributes('data-state')).toBe('all');
+      expect(tabEl.attributes('title')).toBe('Show all requirements.');
+      expect(tabEl.text()).toContain('All');
+      expect(tabEl.find(GlBadge).text()).toBe(`${mockRequirementsCount.ALL}`);
+    });
+
+    it('renders class `active` on currently selected tab', () => {
+      const tabEl = wrapper.findAll('li').at(0);
+
+      expect(tabEl.classes()).toContain('active');
+    });
+
+    it('renders "New requirement" button when current tab is "Open" tab', () => {
+      wrapper.setProps({
+        filterBy: FilterState.opened,
+      });
+
+      return wrapper.vm.$nextTick(() => {
+        const buttonEl = wrapper.find(GlButton);
+
+        expect(buttonEl.exists()).toBe(true);
+        expect(buttonEl.text()).toBe('New requirement');
+      });
+    });
+
+    it('does not render "New requirement" button when current tab is not "Open" tab', () => {
+      wrapper.setProps({
+        filterBy: FilterState.closed,
+      });
+
+      return wrapper.vm.$nextTick(() => {
+        const buttonEl = wrapper.find(GlButton);
+
+        expect(buttonEl.exists()).toBe(false);
+      });
+    });
+
+    it('does not render "New requirement" button when `canCreateRequirement` prop is false', () => {
+      wrapper.setProps({
+        filterBy: FilterState.opened,
+        canCreateRequirement: false,
+      });
+
+      return wrapper.vm.$nextTick(() => {
+        const buttonEl = wrapper.find(GlButton);
+
+        expect(buttonEl.exists()).toBe(false);
+      });
+    });
+
+    it('disables "New requirement" button when `showCreateForm` is true', () => {
+      wrapper.setProps({
+        showCreateForm: true,
+      });
+
+      return wrapper.vm.$nextTick(() => {
+        const buttonEl = wrapper.find(GlButton);
+
+        expect(buttonEl.props('disabled')).toBe(true);
+      });
+    });
+  });
+});
