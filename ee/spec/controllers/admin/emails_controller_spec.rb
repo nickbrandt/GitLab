7105937eba5417e1aed2393913785573395b07a0
manojmj <mmj@gitlab.com>
2020-05-11 12:10:13 +0530

    Rate limit the 'Send emails from Admin Area' feature
    This change rate limits the ‘Send emails from Admin area’
    feature so that admins can trigger only 1 set of emails
    every 10 minutes.

--- ee/spec/controllers/admin/emails_controller_spec.rb
+++ ee/spec/controllers/admin/emails_controller_spec.rb
@@ -2,7 +2,9 @@
 
 require 'spec_helper'
 
-describe Admin::EmailsController do
+describe Admin::EmailsController, :clean_gitlab_redis_shared_state do
+  include ExclusiveLeaseHelpers
+
   let_it_be(:admin) { create(:admin) }
   let_it_be(:user) { create(:user) }
 
@@ -53,11 +55,15 @@ describe Admin::EmailsController do
   end
 
   describe 'POST #create' do
+    let(:recipients) { 'all' }
+    let(:email_subject) { 'subject' }
+    let(:body) { 'body' }
+
     subject do
       post :create, params: {
-        recipients: 'all',
-        subject: 'subject',
-        body: 'body'
+        recipients: recipients,
+        subject: email_subject,
+        body: body
       }
     end
 
@@ -71,18 +77,45 @@ describe Admin::EmailsController do
           stub_licensed_features(send_emails_from_admin_area: true)
         end
 
-        it 'trigger the background job to send emails' do
-          expect(AdminEmailsWorker).to receive(:perform_async).with('all', 'subject', 'body')
+        context 'when emails from admin area are not rate limited' do
+          it 'triggers the service to send emails' do
+            expect_next_instance_of(Admin::EmailService, recipients, email_subject, body) do |email_service|
+              expect(email_service).to receive(:execute)
+            end
 
-          subject
+            subject
+          end
+
+          it 'redirects to `admin_email_path` with success notice' do
+            subject
+
+            expect(response).to have_gitlab_http_status(:found)
+            expect(response).to redirect_to(admin_email_path)
+            expect(flash[:notice]).to eq('Email sent')
+          end
         end
 
-        it 'redirects to `admin_email_path`' do
-          subject
+        context 'when emails from admin area are rate limited' do
+          let(:lease_key) { Admin::EmailService::LEASE_KEY }
+          let(:timeout) { Admin::EmailService::DEFAULT_LEASE_TIMEOUT }
 
-          expect(response).to have_gitlab_http_status(:found)
-          expect(response).to redirect_to(admin_email_path)
-          expect(flash[:notice]).to eq('Email sent')
+          before do
+            stub_exclusive_lease(lease_key, timeout: timeout)
+          end
+
+          it 'does not trigger the service to send emails' do
+            expect(Admin::EmailService).not_to receive(:new)
+
+            subject
+          end
+
+          it 'redirects to `admin_email_path`' do
+            subject
+
+            expect(response).to have_gitlab_http_status(:found)
+            expect(response).to redirect_to(admin_email_path)
+            expect(flash[:alert]).to eq('Email could not be sent')
+          end
         end
       end
 
@@ -91,8 +124,8 @@ describe Admin::EmailsController do
           stub_licensed_features(send_emails_from_admin_area: false)
         end
 
-        it 'does not trigger the background job to send emails' do
-          expect(AdminEmailsWorker).not_to receive(:perform_async)
+        it 'does not trigger the service to send emails' do
+          expect(Admin::EmailService).not_to receive(:new)
 
           subject
         end
---
+++ ee/spec/features/admin/admin_emails_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Admin::Emails', :clean_gitlab_redis_shared_state do
+  include ExclusiveLeaseHelpers
+
+  before do
+    sign_in(create(:admin))
+  end
+
+  context 'when `send_emails_from_admin_area` feature is not licensed' do
+    before do
+      stub_licensed_features(send_emails_from_admin_area: false)
+    end
+
+    it 'returns 404' do
+      visit admin_email_path
+
+      expect(page.status_code).to eq(404)
+    end
+  end
+
+  context 'when `send_emails_from_admin_area` feature is licensed' do
+    let(:rate_limited_alert) do
+      'An email notification was recently sent from the admin panel. '\
+      'Please wait 10 minutes before attempting to send another message.'
+    end
+    let(:submit_button) { find('input[type="submit"]') }
+
+    before do
+      stub_licensed_features(send_emails_from_admin_area: true)
+    end
+
+    context 'when emails from admin area are not rate limited' do
+      it 'does not show the waiting period alert'\
+        'and the submit button is in enabled state' do
+        visit admin_email_path
+
+        expect(page).not_to have_content(rate_limited_alert)
+        expect(submit_button.disabled?).to eq(false)
+      end
+    end
+
+    context 'when emails from admin area are rate limited' do
+      let(:lease_key) { Admin::EmailService::LEASE_KEY }
+      let(:timeout) { Admin::EmailService::DEFAULT_LEASE_TIMEOUT }
+
+      before do
+        allow(Gitlab::ExclusiveLease).to receive(:new).and_call_original
+        stub_exclusive_lease(lease_key, timeout: timeout)
+      end
+
+      it 'shows the waiting period alert'\
+        'and the submit button is in disabled state' do
+        visit admin_email_path
+
+        expect(page).to have_content(rate_limited_alert)
+        expect(submit_button.disabled?).to eq(true)
+      end
+    end
+  end
+end
