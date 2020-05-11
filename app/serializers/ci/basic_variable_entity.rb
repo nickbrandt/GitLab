96df491e3fd13c44cf1472f59d10a6c8895b5037
Marius Bobin <mbobin@gitlab.com>
2020-05-11 14:49:28 +0300

    Unify variables serializers
    Extract common elements from variables serializers into a new
    base class.

--- app/serializers/ci/basic_variable_entity.rb
+++ app/serializers/ci/basic_variable_entity.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Ci
-  class InstanceVariableEntity < Grape::Entity
+  class BasicVariableEntity < Grape::Entity
     expose :id
     expose :key
     expose :value
--- app/serializers/ci/instance_variable_serializer.rb
+++ app/serializers/ci/instance_variable_serializer.rb
@@ -2,6 +2,6 @@
 
 module Ci
   class InstanceVariableSerializer < BaseSerializer
-    entity InstanceVariableEntity
+    entity BasicVariableEntity
   end
 end
