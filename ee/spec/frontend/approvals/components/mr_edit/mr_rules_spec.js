3ac176bbb2c40ca95b2601e052b5c0876dcdf116
Vitaly Slobodin <vslobodin@gitlab.com>
2020-05-11 21:54:38 +0300

    Fix MetricEmbed spec with a HTML fixture
    - Fix MutationObserver
    - Fix ES6 mock in RelatedIssuableInput spec
    - Fix GraphComponent spec with a HTML fixture
    - Remove redundant timers in branches_select_spec
    - Fix eslint warning about unnamed function
    - Remove unneeded regenerator-runtime
    - Refactor setup and teardown in mr_rules_spec
    - Add MutationObserver to DOM shims
    - Fix Jest mock for hoisted variable

--- ee/spec/frontend/approvals/components/mr_edit/mr_rules_spec.js
+++ ee/spec/frontend/approvals/components/mr_edit/mr_rules_spec.js
@@ -17,6 +17,7 @@ describe('EE Approvals MRRules', () => {
   let wrapper;
   let store;
   let approvalRules;
+  let OriginalMutationObserver;
 
   const factory = () => {
     if (approvalRules) {
@@ -50,16 +51,12 @@ describe('EE Approvals MRRules', () => {
     return onTargetBranchMutationHandler();
   };
 
-  let OriginalMutationObserver;
-  beforeAll(() => {
-    OriginalMutationObserver = global.MutationObserver;
-  });
-
-  afterAll(() => {
-    global.MutationObserver = OriginalMutationObserver;
-  });
-
   beforeEach(() => {
+    OriginalMutationObserver = global.MutationObserver;
+    global.MutationObserver = jest
+      .fn()
+      .mockImplementation(args => new OriginalMutationObserver(args));
+
     store = createStoreOptions(MREditModule());
     store.modules.approvals.state = {
       hasLoaded: true,
@@ -73,19 +70,14 @@ describe('EE Approvals MRRules', () => {
     wrapper = null;
     store = null;
     approvalRules = null;
+    global.MutationObserver = OriginalMutationObserver;
   });
 
   describe('when editing a MR', () => {
     const initialTargetBranch = 'master';
     let targetBranchInputElement;
-    let MutationObserverMock;
 
     beforeEach(() => {
-      MutationObserverMock = jest
-        .fn()
-        .mockImplementation(args => new OriginalMutationObserver(args));
-      global.MutationObserver = MutationObserverMock;
-
       targetBranchInputElement = document.createElement('input');
       targetBranchInputElement.id = 'merge_request_target_branch';
       targetBranchInputElement.value = initialTargetBranch;
@@ -103,7 +95,6 @@ describe('EE Approvals MRRules', () => {
 
     afterEach(() => {
       targetBranchInputElement.parentNode.removeChild(targetBranchInputElement);
-      MutationObserverMock.mockClear();
     });
 
     it('sets the target branch data to be the same value as the target branch dropdown', () => {
@@ -115,20 +106,20 @@ describe('EE Approvals MRRules', () => {
     it('updates the target branch data when the target branch dropdown is changed', () => {
       factory();
       const newValue = setTargetBranchInputValue();
-      callTargetBranchHandler(MutationObserverMock);
+      callTargetBranchHandler(global.MutationObserver);
       expect(wrapper.vm.targetBranch).toBe(newValue);
     });
 
     it('re-fetches rules when target branch has changed', () => {
       factory();
       setTargetBranchInputValue();
-      callTargetBranchHandler(MutationObserverMock);
+      callTargetBranchHandler(global.MutationObserver);
       expect(store.modules.approvals.actions.fetchRules).toHaveBeenCalled();
     });
 
     it('disconnects MutationObserver when component gets destroyed', () => {
       const mockDisconnect = jest.fn();
-      MutationObserverMock.mockImplementation(() => ({
+      global.MutationObserver.mockImplementation(() => ({
         disconnect: mockDisconnect,
         observe: jest.fn(),
       }));
