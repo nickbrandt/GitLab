969b7f01ffd73cb3f6368dce18171707e674927e
Alexander Randa <randa.alex@gmail.com>
2020-05-11 14:15:14 +0300

    Extract featurable concern
    https://gitlab.com/gitlab-org/gitlab/-/issues/208412
    
    Move access_level string conversion

--- ee/spec/elastic_integration/global_search_spec.rb
+++ ee/spec/elastic_integration/global_search_spec.rb
@@ -160,7 +160,7 @@ describe 'GlobalSearch', :elastic do
 
   # access_level can be :disabled, :enabled or :private
   def feature_settings(access_level)
-    Hash[features.collect { |k| ["#{k}_access_level", ProjectFeature.const_get(access_level.to_s.upcase, false)] }]
+    Hash[features.collect { |k| ["#{k}_access_level", Featurable.const_get(access_level.to_s.upcase, false)] }]
   end
 
   def expect_no_items_to_be_found(user)
