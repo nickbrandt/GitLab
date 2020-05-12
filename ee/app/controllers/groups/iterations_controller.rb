2e43520c57fe3368f04f480061b44d9ff408708a
Simon Knox <simon@gitlab.com>
2020-05-13 08:28:37 +1000

    Add HAML form and POST endpoint
    This seems way simpler than faking all this on the
    frontend

--- ee/app/controllers/groups/iterations_controller.rb
+++ ee/app/controllers/groups/iterations_controller.rb
@@ -3,15 +3,32 @@
 class Groups::IterationsController < Groups::ApplicationController
   before_action :check_iterations_available!
   before_action :authorize_show_iteration!, only: :show
-  before_action :authorize_create_iteration!, only: :new
+  before_action :authorize_create_iteration!, only: [:new, :create]
 
   def index; end
 
-  def show; end
+  def new
+    @iteration = Sprint.new
+  end
 
-  def new; end
+  def create
+    @iteration = Iterations::CreateService.new(group, current_user, iteration_params).execute
+
+    if @iteration.success?
+      redirect_to iteration_path
+    else
+      render "new"
+    end
+  end
+
+  def show
+  end
 
   private
+
+  def iteration_params
+    params.require(:sprint).permit(:title, :description, :start_date, :due_date)
+  end
 
   def check_iterations_available!
     return render_404 unless group.feature_available?(:iterations)
--- ee/config/routes/group.rb
+++ ee/config/routes/group.rb
@@ -109,7 +109,7 @@ constraints(::Constraints::GroupUrlConstrainer.new) do
       end
     end
 
-    resources :iterations, only: [:index, :new, :show], constraints: { id: /\d+/ }
+    resources :iterations, only: [:index, :new, :show, :create], constraints: { id: /\d+/ }
 
     resources :issues, only: [] do
       collection do
