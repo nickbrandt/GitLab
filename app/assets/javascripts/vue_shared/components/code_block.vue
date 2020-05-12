32ed4fc821b8d9960038ab13507ac0e0e2dae765
Dave Pisek <dpisek@gitlab.com>
2020-05-12 11:33:31 +1000

    WIP - add expand / collapse experiment

--- app/assets/javascripts/vue_shared/components/code_block.vue
+++ app/assets/javascripts/vue_shared/components/code_block.vue
@@ -6,9 +6,46 @@ export default {
       type: String,
       required: true,
     },
+    maxHeight: {
+      type: String,
+      required: false,
+      default: 'initial',
+    },
+  },
+  data() {
+    return {
+      isOverflowing: false,
+      isExpanded: false,
+    };
+  },
+  computed: {
+    style() {
+      return {
+        maxHeight: this.isExpanded ? 'initial' : this.maxHeight,
+        overflow: 'hidden',
+      };
+    },
+  },
+  mounted() {
+    // need nextTick, otherwise `clientHeight` and `scrollHeight` will both be `0`
+    this.$nextTick(() => {
+      const { codeBlock } = this.$refs;
+      const { clientHeight, scrollHeight } = codeBlock;
+      this.isOverflowing = scrollHeight > clientHeight;
+    });
+  },
+  methods: {
+    toggleExpanded() {
+      this.isExpanded = !this.isExpanded;
+    },
   },
 };
 </script>
 <template>
-  <pre class="code-block rounded"><code class="d-block">{{ code }}</code></pre>
+  <div>
+    <pre
+      class="code-block"
+    ><code ref="codeBlock" class="d-block" :style="style">{{ code }}</code></pre>
+    <button v-if="isOverflowing" @click="toggleExpanded">Show all</button>
+  </div>
 </template>
