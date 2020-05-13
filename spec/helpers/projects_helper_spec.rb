a874ac5a60b0df7e75fc16a1906bfd1b36a2193f
Gilang Gumilar <gilangmlr@gmail.com>
2020-05-13 18:53:40 +0000

    Remove project_list_show_issue_count feature flag

--- spec/helpers/projects_helper_spec.rb
+++ spec/helpers/projects_helper_spec.rb
@@ -760,11 +760,7 @@ describe ProjectsHelper do
   end
 
   describe '#show_issue_count?' do
-    context 'when the feature flag is enabled' do
-      before do
-        stub_feature_flags(project_list_show_issue_count: true)
-      end
-
+    context 'enabled flag' do
       it 'returns true if compact mode is disabled' do
         expect(helper.show_issue_count?).to be_truthy
       end
@@ -774,22 +770,7 @@ describe ProjectsHelper do
       end
     end
 
-    context 'when the feature flag is disabled' do
-      before do
-        stub_feature_flags(project_list_show_issue_count: false)
-      end
-
-      it 'always returns false' do
-        expect(helper.show_issue_count?(disabled: false)).to be_falsy
-        expect(helper.show_issue_count?(disabled: true)).to be_falsy
-      end
-    end
-
     context 'disabled flag' do
-      before do
-        stub_feature_flags(project_list_show_issue_count: true)
-      end
-
       it 'returns false if disabled flag is true' do
         expect(helper.show_issue_count?(disabled: true)).to be_falsey
       end
