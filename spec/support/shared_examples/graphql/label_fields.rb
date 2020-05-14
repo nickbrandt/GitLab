a71676ce7c70281f21bd18b650ddfeb59586b1ad
Alex Kalderimis <alex.kalderimis@gmail.com>
2020-05-14 17:15:15 -0400

    Add issue reference to pending example

--- spec/support/shared_examples/graphql/label_fields.rb
+++ spec/support/shared_examples/graphql/label_fields.rb
@@ -106,7 +106,7 @@ RSpec.shared_examples 'querying a GraphQL type with labels' do
     end
 
     it 'batches queries for labels by title' do
-      pending('Making type authorization fully lazy')
+      pending('See: https://gitlab.com/gitlab-org/gitlab/-/issues/217767')
 
       multi_selection = query_for(label_b, label_c)
       single_selection = query_for(label_d)
