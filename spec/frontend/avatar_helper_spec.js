776b502e0ab3d1290b1a8e411d44ceca5b129899
Alexander Turinske <aturinske@gitlab.com>
2020-05-15 13:25:14 -0600

    Add test to document behavior of avatar_helper
    - getIdenticonBackgroundClass behavior better documented
    - remove changelog`

--- spec/frontend/avatar_helper_spec.js
+++ spec/frontend/avatar_helper_spec.js
@@ -27,6 +27,10 @@ describe('avatar_helper', () => {
       expect(getIdenticonBackgroundClass('gid://gitlab/Project/1')).toEqual('bg2');
     });
 
+    it('returns identicon bg class from unparsable string', () => {
+      expect(getIdenticonBackgroundClass('gid://gitlab/')).toEqual('bg1');
+    });
+
     it(`wraps around if id is bigger than ${IDENTICON_BG_COUNT}`, () => {
       expect(getIdenticonBackgroundClass(IDENTICON_BG_COUNT + 4)).toEqual('bg5');
       expect(getIdenticonBackgroundClass(IDENTICON_BG_COUNT * 5 + 6)).toEqual('bg7');
