613361fde4b41cadc4008c494f83f76aa76e2fab
Brandon Labuschagne <blabuschagne@gitlab.com>
2020-05-18 11:25:28 +0200

    Add group to VSA path FF

--- ee/app/controllers/groups/analytics/cycle_analytics_controller.rb
+++ ee/app/controllers/groups/analytics/cycle_analytics_controller.rb
@@ -11,7 +11,7 @@ class Groups::Analytics::CycleAnalyticsController < Groups::Analytics::Applicati
   before_action do
     push_frontend_feature_flag(:cycle_analytics_scatterplot_enabled, default_enabled: true)
     push_frontend_feature_flag(:cycle_analytics_scatterplot_median_enabled, default_enabled: true)
-    push_frontend_feature_flag(:value_stream_analytics_path_navigation)
+    push_frontend_feature_flag(:value_stream_analytics_path_navigation, @group)
   end
 
   before_action :load_group, only: :show
