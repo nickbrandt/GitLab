c867b54bace46336eef22dc5a0a5ca6d199fc5f0
Zack Cuddy <zcuddy@gitlab.com>
2020-05-18 14:59:32 -0500

    Make Geo Node Url more Prominent
    Currently the Geo Node url is not a link.
    Yet it is a valid route.
    
    This changes it from a span to a gllink.

--- ee/app/assets/javascripts/geo_nodes/components/node_detail_sections/node_details_section_main.vue
+++ ee/app/assets/javascripts/geo_nodes/components/node_detail_sections/node_details_section_main.vue
@@ -1,4 +1,5 @@
 <script>
+import { GlLink, GlIcon } from '@gitlab/ui';
 import { __, sprintf } from '~/locale';
 
 import GeoNodeHealthStatus from '../geo_node_health_status.vue';
@@ -6,6 +7,8 @@ import GeoNodeActions from '../geo_node_actions.vue';
 
 export default {
   components: {
+    GlLink,
+    GlIcon,
     GeoNodeHealthStatus,
     GeoNodeActions,
   },
@@ -70,9 +73,14 @@ export default {
   <div class="row-fluid clearfix py-3 primary-section">
     <div class="col-md-12">
       <div class="d-flex geo-node-actions-container">
-        <div class="d-flex flex-column">
-          <span class="text-secondary-700 js-node-url-title">{{ s__('GeoNodes|Node URL') }}</span>
-          <span class="mt-1 font-weight-bold js-node-url-value">{{ node.url }}</span>
+        <div data-testid="nodeUrl" class="d-flex flex-column">
+          <span class="gl-text-gray-700">{{ s__('GeoNodes|Node URL') }}</span>
+          <gl-link
+            class="gl-display-flex gl-align-items-center gl-text-black-normal gl-font-weight-bold gl-text-decoration-underline gl-mt-1"
+            :href="node.url"
+            target="_blank"
+            >{{ node.url }} <gl-icon name="external-link" class="gl-ml-1"
+          /></gl-link>
         </div>
         <geo-node-actions
           class="flex-grow-1"
@@ -83,20 +91,15 @@ export default {
           :node-missing-oauth="nodeDetails.missingOAuthApplication"
         />
       </div>
-      <div class="d-flex flex-column mt-2">
-        <span class="text-secondary-700 js-node-version-title">{{
-          s__('GeoNodes|GitLab version')
-        }}</span>
-        <span
-          :class="{ 'text-danger-500': versionMismatch }"
-          class="mt-1 font-weight-bold js-node-version-value"
-        >
+      <div data-testid="nodeVersion" class="d-flex flex-column mt-2">
+        <span class="gl-text-gray-700">{{ s__('GeoNodes|GitLab version') }}</span>
+        <span :class="{ 'gl-text-red-500': versionMismatch }" class="gl-mt-1 gl-font-weight-bold">
           {{ nodeVersion }}
         </span>
       </div>
       <div v-if="selectiveSyncronization" class="d-flex flex-column mt-2">
         <span class="text-secondary-700">{{ s__('GeoNodes|Selective synchronization') }}</span>
-        <span data-testid="selectiveSync" class="mt-1 font-weight-bold">
+        <span data-testid="selectiveSync" class="gl-mt-1 gl-font-weight-bold">
           {{ selectiveSyncronization }}
         </span>
       </div>
