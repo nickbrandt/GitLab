e47a02e39f615bbc963250e9577263ccac6bd5a0
Alper Akgun <aakgun@gitlab.com>
2020-05-18 11:21:05 +0300

    Increment redis counters if usage_ping is disabled

--- spec/lib/gitlab/usage_data_counters/redis_counter_spec.rb
+++ spec/lib/gitlab/usage_data_counters/redis_counter_spec.rb
@@ -7,27 +7,9 @@ describe Gitlab::UsageDataCounters::RedisCounter, :clean_gitlab_redis_shared_sta
 
   subject { Class.new.extend(described_class) }
 
-  before do
-    stub_application_setting(usage_ping_enabled: setting_value)
-  end
-
-  context 'when usage_ping is disabled' do
-    let(:setting_value) { false }
-
-    it 'counter is not increased' do
-      expect do
-        subject.increment(redis_key)
-      end.not_to change { subject.total_count(redis_key) }
-    end
-  end
-
-  context 'when usage_ping is enabled' do
-    let(:setting_value) { true }
-
-    it 'counter is increased' do
-      expect do
-        subject.increment(redis_key)
-      end.to change { subject.total_count(redis_key) }.by(1)
-    end
+  it 'counter is increased' do
+    expect do
+      subject.increment(redis_key)
+    end.to change { subject.total_count(redis_key) }.by(1)
   end
 end
