d33188596c307d8d8654851423ea6131c211b15a
Igor Drozdov <idrozdov@gitlab.com>
2020-05-20 22:28:16 +0300

    Accept string or int type for LSIF ids
    Define Id type with custom marshalling which accepts either
    int or string, because some languages (for example, Javascript)
    define ids in LSIF as ints, but some languages (like Golang) use
    string type for ids
    
    It also changes int to uint32 for easier binary conversion

--- internal/lsif_transformer/parser/hovers_test.go
+++ internal/lsif_transformer/parser/hovers_test.go
@@ -9,7 +9,7 @@ import (
 func TestHoversRead(t *testing.T) {
 	h := setupHovers(t)
 
-	require.Equal(t, `[{"value":"hello"}]`, string(h.For("1")))
+	require.Equal(t, `[{"value":"hello"}]`, string(h.For(1)))
 
 	require.NoError(t, h.Close())
 }
@@ -19,8 +19,8 @@ func setupHovers(t *testing.T) *Hovers {
 	require.NoError(t, err)
 
 	require.NoError(t, h.Read("hoverResult", []byte(`{"id":"2","label":"hoverResult","result":{"contents": ["hello"]}}`)))
-	require.NoError(t, h.Read("textDocument/hover", []byte(`{"id":"4","label":"textDocument/hover","outV":"3","inV":"2"}`)))
-	require.NoError(t, h.Read("textDocument/references", []byte(`{"id":"3","label":"textDocument/references","outV":"3","inV":"1"}`)))
+	require.NoError(t, h.Read("textDocument/hover", []byte(`{"id":4,"label":"textDocument/hover","outV":"3","inV":2}`)))
+	require.NoError(t, h.Read("textDocument/references", []byte(`{"id":"3","label":"textDocument/references","outV":3,"inV":"1"}`)))
 
 	return h
 }
