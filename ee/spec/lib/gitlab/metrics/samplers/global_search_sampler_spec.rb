b3f2c8cfe1dd8af0ee6bf0a68408a9bdff454eb6
Sean McGivern <sean@gitlab.com>
2020-05-21 16:12:17 +0100

    Hard-code non-configurable sampler intervals
    While these samplers had intervals in 1_settings.rb, those weren't
    exposed anywhere else: in the `gitlab.yml.example` file, in
    `gitlab.rb.template` in Omnibus, or in our Helm charts.
    
    Based on that, it doesn't appear that we expected people to configure
    these intervals, so we should make the hard-coding explicit.

--- ee/spec/lib/gitlab/metrics/samplers/global_search_sampler_spec.rb
+++ ee/spec/lib/gitlab/metrics/samplers/global_search_sampler_spec.rb
@@ -3,7 +3,17 @@
 require 'spec_helper'
 
 describe Gitlab::Metrics::Samplers::GlobalSearchSampler do
-  subject { described_class.new(60.seconds) }
+  subject { described_class.new }
+
+  describe '#interval' do
+    it 'samples every sixty seconds by default' do
+      expect(subject.interval).to eq(60)
+    end
+
+    it 'samples at other intervals if requested' do
+      expect(described_class.new(11).interval).to eq(11)
+    end
+  end
 
   describe '#sample' do
     it 'invokes the Elastic::MetricsUpdateService' do
