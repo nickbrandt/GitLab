8da43dea90a2a1dc38ed71183c23af992a591be1
Vladimir Shushlin <v.shushlin@gmail.com>
2020-05-25 13:40:05 +0300

    Add spec for filtering issues from release evidence

--- spec/models/releases/evidence_spec.rb
+++ spec/models/releases/evidence_spec.rb
@@ -3,10 +3,23 @@
 require 'spec_helper'
 
 describe Releases::Evidence do
-  let(:release) { create(:release) }
-  let(:summary_json) { described_class.last.summary.to_json }
+  let_it_be(:project) { create(:project) }
+  let(:release) { create(:release, project: project) }
 
   describe 'associations' do
     it { is_expected.to belong_to(:release) }
+  end
+
+  it 'filters out issues from summary json' do
+    milestone = create(:milestone, project: project, due_date: nil)
+    issue = create(:issue, project: project, description: nil, state: 'closed')
+    milestone.issues << issue
+    release.milestones << milestone
+
+    ::Releases::CreateEvidenceService.new(release).execute
+    evidence = release.evidences.last
+
+    expect(evidence.read_attribute(:summary)["release"]["milestones"].first["issues"].first["title"]).to be_present
+    expect(evidence.summary["release"]["milestones"].first["issues"]).to be_nil
   end
 end
