0a7d358e93467c10420b6be817f7d9a17b2225c3
Jarka Košanová <jarka@gitlab.com>
2020-05-26 18:01:09 +0200

    Move base supports_weight to Issuable

--- ee/app/models/ee/merge_request.rb
+++ ee/app/models/ee/merge_request.rb
@@ -124,10 +124,6 @@ module EE
       project.feature_available?(:multiple_merge_request_assignees)
     end
 
-    def supports_weight?
-      false
-    end
-
     def visible_blocking_merge_requests(user)
       Ability.merge_requests_readable_by_user(blocking_merge_requests, user)
     end
