b2a86f7d889d54467fba085dd8ad447678f2dc0f
Thong Kuah <tkuah@gitlab.com>
2020-05-28 11:46:24 +1200

    Add experimental internal Kubernetes API
    - Return agent_id in response if present
    - Receive token via Authorization header
    
    Use standard way for Authorization which is the header. We currently not
    care about the
    Authorization type but may enforce this in future
    
    Adds /project_info endpoint. Only for public projects. We will sort out
    authorization for internal and private projects later
    
    Remove need to hack around InternalHelpers. InternalHelpers assumes an
    actor (we don't have it) and params (don't have that either). So get the
    required data directly instead.

---
+++ lib/api/internal/kubernetes.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+module API
+  # Kubernetes Internal API
+  module Internal
+    class Kubernetes < Grape::API::Instance
+      helpers do
+        def repo_type
+          Gitlab::GlRepository::PROJECT
+        end
+
+        def gl_repository(project)
+          repo_type.identifier_for_container(project)
+        end
+
+        def gl_repository_path(project)
+          repo_type.repository_for(project).full_path
+        end
+
+        def authorization_header
+          strong_memoize(:authorization_header) do
+            request.headers['Authorization']
+          end
+        end
+
+        def authorization_token
+          unless authorization_header.present?
+            unauthorized!
+          end
+
+          _token_type, authorization_token = authorization_header.split(' ', 2)
+
+          authorization_token
+        end
+
+        def check_feature_enabled
+          not_found! unless Feature.enabled?(:kubernetes_agent_internal_api)
+        end
+      end
+
+      namespace 'internal' do
+        namespace 'kubernetes' do
+          desc 'Gets agent info' do
+            detail 'Retrieves agent info for the given token'
+          end
+          get '/agent_info' do
+            check_feature_enabled
+
+            agent_token = Clusters::AgentToken.find_by_token(authorization_token)
+
+            if agent_token
+              agent = agent_token.agent
+              project = agent.project
+              @gl_project_string = "project-#{project.id}"
+
+              status 200
+              {
+                project_id: project.id,
+                agent_id: agent.id,
+                agent_name: agent.name,
+                storage_name: project.repository_storage,
+                relative_path: project.disk_path + '.git',
+                gl_repository: gl_repository(project),
+                gl_project_path: gl_repository_path(project)
+              }
+            else
+              status 403
+            end
+          end
+
+          desc 'Gets project info' do
+            detail 'Retrieves project info (if authorized)'
+          end
+          get '/project_info' do
+            check_feature_enabled
+
+            agent_token = Clusters::AgentToken.find_by_token(authorization_token)
+
+            if agent_token
+              project = find_project(params[:id])
+
+              # TODO sort out authorization for real
+              # https://gitlab.com/gitlab-org/gitlab/-/issues/220912
+              if !project || !project.public?
+                not_found!
+              end
+
+              @gl_project_string = "project-#{project.id}"
+
+              status 200
+              {
+                project_id: project.id,
+                storage_name: project.repository_storage,
+                relative_path: project.disk_path + '.git',
+                gl_repository: gl_repository(project),
+                gl_project_path: gl_repository_path(project)
+              }
+            else
+              status 403
+            end
+          end
+        end
+      end
+    end
+  end
+end
