mehulagg/gitlab_test

Add `AvoidFeatureGet` RuboCop rule
This removes the usage of `Feature.get`
and instead provides a helpers for almost
all possible usages.

ee/app/models/ee/user.rb
@@ -334,7 +334,7 @@ module EE
       filter = user_preference.feature_filter_type.presence || 0
 
       # We use a 2nd feature flag for control as enabled and percentage_of_time for chatops
-      flipper_feature = ::Feature.get((feature.to_s + '_control').to_sym)
+      flipper_feature = ::Feature.get((feature.to_s + '_control').to_sym) # rubocop:disable Cop/AvoidFeatureGet
       percentage ||= flipper_feature.gate_values[:percentage_of_time] || 0 if flipper_feature
       return false if percentage <= 0
 

lib/api/features.rb
@@ -61,7 +61,7 @@ module API
         mutually_exclusive :key, :project
       end
       post ':name' do
-        feature = Feature.get(params[:name])
+        feature = Feature.get(params[:name]) # rubocop:disable Cop/AvoidFeatureGet
         targets = gate_targets(params)
         value = gate_value(params)
         key = gate_key(params)
@@ -92,7 +92,7 @@ module API
 
       desc 'Remove the gate value for the given feature'
       delete ':name' do
-        Feature.get(params[:name]).remove
+        Feature.remove(params[:name])
 
         no_content!
       end

lib/gitlab/sourcegraph.rb
@@ -19,7 +19,7 @@ module Gitlab
       private
 
       def feature
-        Feature.get(:sourcegraph)
+        Feature.get(:sourcegraph) # rubocop:disable Cop/AvoidFeatureGet
       end
     end
   end

spec/support/helpers/stub_feature_flags.rb
@@ -47,7 +47,7 @@ module StubFeatureFlags
   def stub_feature_flags(features)
     features.each do |feature_name, actors|
       # Remove feature flag overwrite
-      feature = Feature.get(feature_name)
+      feature = Feature.get(feature_name) # rubocop:disable Cop/AvoidFeatureGet
       feature.remove
 
       Array(actors).each do |actor|

nickbrandt/GitLab

ee/app/models/ee/user.rb
@@ -337,7 +337,7 @@ module EE
       filter = user_preference.feature_filter_type.presence || 0
 
       # We use a 2nd feature flag for control as enabled and percentage_of_time for chatops
-      flipper_feature = ::Feature.get((feature.to_s + '_control').to_sym)
+      flipper_feature = ::Feature.get((feature.to_s + '_control').to_sym) # rubocop:disable Gitlab/AvoidFeatureGet
       percentage ||= flipper_feature.gate_values[:percentage_of_time] || 0 if flipper_feature
       return false if percentage <= 0
 

lib/api/features.rb
@@ -61,7 +61,7 @@ module API
         mutually_exclusive :key, :project
       end
       post ':name' do
-        feature = Feature.get(params[:name])
+        feature = Feature.get(params[:name]) # rubocop:disable Gitlab/AvoidFeatureGet
         targets = gate_targets(params)
         value = gate_value(params)
         key = gate_key(params)
@@ -92,7 +92,7 @@ module API
 
       desc 'Remove the gate value for the given feature'
       delete ':name' do
-        Feature.get(params[:name]).remove
+        Feature.remove(params[:name])
 
         no_content!
       end

lib/gitlab/sourcegraph.rb
@@ -19,7 +19,7 @@ module Gitlab
       private
 
       def feature
-        Feature.get(:sourcegraph)
+        Feature.get(:sourcegraph) # rubocop:disable Gitlab/AvoidFeatureGet
       end
     end
   end

spec/support/helpers/stub_feature_flags.rb
@@ -47,7 +47,7 @@ module StubFeatureFlags
   def stub_feature_flags(features)
     features.each do |feature_name, actors|
       # Remove feature flag overwrite
-      feature = Feature.get(feature_name)
+      feature = Feature.get(feature_name) # rubocop:disable Gitlab/AvoidFeatureGet
       feature.remove
 
       Array(actors).each do |actor|