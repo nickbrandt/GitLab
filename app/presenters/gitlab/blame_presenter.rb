bab0aadcc78050ff2acd21518d3d2eee7ea296be
Aleksei Lipniagov <alipniagov@gitlab.com>
2020-06-01 17:41:22 +0300

    Refactor presenter class

--- app/presenters/gitlab/blame_presenter.rb
+++ app/presenters/gitlab/blame_presenter.rb
@@ -24,27 +24,27 @@ module Gitlab
       @groups ||= blame.groups
     end
 
-    def author_avatar_for_commit(commit_id)
+    def author_avatar_for(commit_id)
       @author_avatars[commit_id]
     end
 
-    def age_map_class_for_commit(commit_id)
+    def age_map_class_for(commit_id)
       @age_map_classes[commit_id]
     end
 
-    def commit_link_for_commit(commit_id)
+    def commit_link_for(commit_id)
       @commit_links[commit_id]
     end
 
-    def commit_author_link_for_commit(commit_id)
+    def commit_author_link_for(commit_id)
       @commit_author_links[commit_id]
     end
 
-    def project_blame_link_for_commit(commit_id)
+    def project_blame_link_for(commit_id)
       @project_blame_links[commit_id]
     end
 
-    def time_ago_tooltip_for_commit(commit_id)
+    def time_ago_tooltip_for(commit_id)
       @time_ago_tooltips[commit_id]
     end
 
@@ -58,37 +58,36 @@ module Gitlab
       @project_blame_links = {}
       @time_ago_tooltips = {}
 
-      sprite_icon = sprite_icon('doc-versions', size: 16, css_class: 'doc-versions align-text-bottom')
-
       groups.each do |blame_group|
         commit = blame_group[:commit]
-        @author_avatars[commit.id] ||= author_avatar(commit, size: 36, has_tooltip: false)
 
+        @author_avatars[commit.id] ||= author_avatar(commit, size: 36, has_tooltip: false)
         @age_map_classes[commit.id] ||= age_map_class(commit.committed_date, project_duration)
         @commit_links[commit.id] ||= link_to commit.title, project_commit_path(project, commit.id), class: "cdark", title: commit.title
-
-
         @commit_author_links[commit.id] ||= commit_author_link(commit, avatar: false)
         @time_ago_tooltips[commit.id] ||= time_ago_with_tooltip(commit.committed_date)
-
-        previous_commit_id = commit.parent_id
-
-        @project_blame_links[commit.id] ||=
-          begin
-            if previous_commit_id
-              link_to project_blame_path(project, tree_join(previous_commit_id, path)),
-                      title: _('View blame prior to this change'),
-                      aria: { label: _('View blame prior to this change') },
-                      data: { toggle: 'tooltip', placement: 'right', container: 'body' } do
-                        sprite_icon
-                      end
-            end
-          end
+        @project_blame_links[commit.id] ||= project_blame_link(commit)
         end
-      end
+    end
 
-      def project_duration
-        @project_duration ||= age_map_duration(groups, project)
+    def project_blame_link(commit)
+      previous_commit_id = commit.parent_id
+      return unless previous_commit_id
+
+      link_to project_blame_path(project, tree_join(previous_commit_id, path)),
+        title: _('View blame prior to this change'),
+        aria: { label: _('View blame prior to this change') },
+        data: { toggle: 'tooltip', placement: 'right', container: 'body' } do
+          versions_sprite_icon
+        end
+    end
+
+    def project_duration
+      @project_duration ||= age_map_duration(groups, project)
+    end
+
+    def versions_sprite_icon
+      @versions_sprite_icon ||= sprite_icon('doc-versions', size: 16, css_class: 'doc-versions align-text-bottom')
     end
   end
 end
