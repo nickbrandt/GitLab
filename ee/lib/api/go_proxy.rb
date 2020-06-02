214eeb501ed2b9f4c06b2b20f3496cfc409d23fd
Tiger <twatson@gitlab.com>
2020-06-02 13:01:03 +1000

    Lookup user access token with basic auth only when required
    Overriding #access_token defeats the super method's
    `strong_memoize`. Access tokens via basic auth
    are only required for a small number of endpoints, so
    these endpoints can override #find_personal_access_token
    directly.

--- ee/lib/api/go_proxy.rb
+++ ee/lib/api/go_proxy.rb
@@ -64,6 +64,10 @@ module API
       rescue ArgumentError
         not_found!
       end
+
+      def find_personal_access_token
+        super || find_personal_access_token_from_http_basic_auth
+      end
     end
 
     params do
--- lib/api/helpers/terraform_helpers.rb
+++ lib/api/helpers/terraform_helpers.rb
@@ -7,6 +7,10 @@ module API
         ::Terraform::RemoteStateHandler.new(user_project, current_user, name: params[:name], lock_id: params[:ID])
       end
 
+      def find_personal_access_token
+        find_personal_access_token_from_http_basic_auth
+      end
+
       def find_user_from_job_token
         find_user_from_basic_auth_job
       end
