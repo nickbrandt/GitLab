c5ae43f7850c4ae899c6ba2b8c72d1052655c384
Simon Knox <simon@gitlab.com>
2020-06-03 00:28:43 +1000

    Add actual data and description field [ci-skip]
    Format dates, show status, title and description
    This appears to be all the info available right now

--- ee/app/assets/javascripts/iterations/components/iteration_report.vue
+++ ee/app/assets/javascripts/iterations/components/iteration_report.vue
@@ -1,5 +1,7 @@
 <script>
 import { GlBadge, GlButton, GlIcon } from '@gitlab/ui';
+import dateFormat from 'dateformat';
+import { __ } from '~/locale';
 import query from '../queries/group_iteration.query.graphql';
 
 export default {
@@ -17,11 +19,17 @@ export default {
           id: this.iterationId,
         };
       },
-      update(data) {
-        console.log(data);
-
+      update(data, errors) {
+        // console.log(errors);
         return data.group.iterations.nodes[0];
       },
+    },
+  },
+  filters: {
+    date: value => {
+      if (!value) return '';
+      const date = new Date(value);
+      return dateFormat(date, 'mmm d, yyyy', true);
     },
   },
   props: {
@@ -36,30 +44,46 @@ export default {
   },
   data() {
     return {
-      title: 'Iteration TODO',
-      startDate: '2020-05-01',
-      dueDate: '2020-05-01',
+      title: '',
+      startDate: '',
+      dueDate: '',
+      iteration: {},
     };
+  },
+  computed: {
+    status() {
+      switch (this.iteration.state) {
+        case 'closed':
+          return {
+            text: __('Closed'),
+            style: 'danger',
+          };
+        case 'expired':
+          return { text: __('Past due'), style: 'warning' };
+        case 'upcoming':
+          return { text: __('Upcoming'), style: 'secondary' };
+        default:
+          return { text: __('Open'), style: 'success' };
+      }
+    },
   },
 };
 </script>
 
 <template>
   <div>
-    <div class="gl-display-flex gl-justify-items-center gl-align-items-center gl-mt-5">
-      <gl-badge variant="success">
-        In Progress
+    <div
+      class="gl-display-flex gl-justify-items-center gl-align-items-center gl-py-3 gl-border-1 gl-border-b-solid gl-border-gray-200"
+    >
+      <gl-badge :variant="status.variant">
+        {{ status.text }}
       </gl-badge>
-      {{ startDate }}
-      -
-      {{ dueDate }}
-      <gl-button variant="link" class="gl-ml-auto">
+      <span class="gl-ml-4">{{ iteration.startDate | date }} – {{ iteration.dueDate | date }}</span>
+      <gl-button v-if="false" variant="link" class="gl-ml-auto gl-text-secondary">
         <gl-icon name="ellipsis_v" />
       </gl-button>
     </div>
-    <hr />
-    <div class="gl-display-flex">
-      <h3 class="page-title">{{ title }}</h3>
-    </div>
+    <h3 class="page-title">{{ iteration.title }}</h3>
+    <div v-html="iteration.description"></div>
   </div>
 </template>
