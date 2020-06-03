bafb3952ffb37ac7f818907cac599cc5bb8762f8
Nick Kipling <nkipling@gitlab.com>
2020-06-03 10:40:30 +0100

    Adds NuGet package icon to package details page
    Updated package-title component to display icon
    Created Vuex getter to get the package icon
    Updated tests

--- ee/app/assets/javascripts/packages/details/components/package_title.vue
+++ ee/app/assets/javascripts/packages/details/components/package_title.vue
@@ -1,6 +1,6 @@
 <script>
 import { mapState, mapGetters } from 'vuex';
-import { GlIcon, GlLink, GlSprintf, GlTooltipDirective } from '@gitlab/ui';
+import { GlAvatar, GlIcon, GlLink, GlSprintf, GlTooltipDirective } from '@gitlab/ui';
 import PackageTags from '../../shared/components/package_tags.vue';
 import { numberToHumanSize } from '~/lib/utils/number_utils';
 import timeagoMixin from '~/vue_shared/mixins/timeago';
@@ -8,6 +8,7 @@ import timeagoMixin from '~/vue_shared/mixins/timeago';
 export default {
   name: 'PackageTitle',
   components: {
+    GlAvatar,
     GlIcon,
     GlLink,
     GlSprintf,
@@ -19,7 +20,7 @@ export default {
   mixins: [timeagoMixin],
   computed: {
     ...mapState(['packageEntity', 'packageFiles']),
-    ...mapGetters(['packageTypeDisplay', 'packagePipeline']),
+    ...mapGetters(['packageTypeDisplay', 'packagePipeline', 'packageIcon']),
     hasTagsToDisplay() {
       return Boolean(this.packageEntity.tags && this.packageEntity.tags.length);
     },
@@ -31,30 +32,42 @@ export default {
 </script>
 
 <template>
-  <div class="flex-column">
-    <h1 class="gl-font-size-20-deprecated-no-really-do-not-use-me gl-mt-3 append-bottom-4">
-      {{ packageEntity.name }}
-    </h1>
+  <div class="gl-flex-direction-column">
+    <div class="gl-display-flex">
+      <gl-avatar
+        v-if="packageIcon"
+        :src="packageIcon"
+        shape="rect"
+        class="gl-align-self-center gl-mr-4"
+        data-testid="package-icon"
+      />
 
-    <div class="gl-display-flex gl-align-items-center text-secondary">
-      <gl-icon name="eye" class="gl-mr-3" />
-      <gl-sprintf message="v%{version} published %{timeAgo}">
-        <template #version>
-          {{ packageEntity.version }}
-        </template>
+      <div class="gl-display-flex gl-flex-direction-column">
+        <h1 class="gl-font-size-h1 gl-mt-3 gl-mb-2">
+          {{ packageEntity.name }}
+        </h1>
 
-        <template #timeAgo>
-          <span v-gl-tooltip :title="tooltipTitle(packageEntity.created_at)">
-            &nbsp;{{ timeFormatted(packageEntity.created_at) }}
-          </span>
-        </template>
-      </gl-sprintf>
+        <div class="gl-display-flex gl-align-items-center text-secondary">
+          <gl-icon name="eye" class="gl-mr-3" />
+          <gl-sprintf message="v%{version} published %{timeAgo}">
+            <template #version>
+              {{ packageEntity.version }}
+            </template>
+
+            <template #timeAgo>
+              <span v-gl-tooltip :title="tooltipTitle(packageEntity.created_at)">
+                &nbsp;{{ timeFormatted(packageEntity.created_at) }}
+              </span>
+            </template>
+          </gl-sprintf>
+        </div>
+      </div>
     </div>
 
-    <div class="gl-display-flex flex-wrap gl-align-items-center append-bottom-8">
-      <div v-if="packageTypeDisplay" class="gl-display-flex align-items-center gl-mr-5">
+    <div class="gl-display-flex gl-flex-wrap gl-align-items-center gl-mb-3">
+      <div v-if="packageTypeDisplay" class="gl-display-flex gl-align-items-center gl-mr-5">
         <gl-icon name="package" class="text-secondary gl-mr-3" />
-        <span ref="package-type" class="font-weight-bold">{{ packageTypeDisplay }}</span>
+        <span data-testid="package-type" class="gl-font-weight-bold">{{ packageTypeDisplay }}</span>
       </div>
 
       <div v-if="hasTagsToDisplay" class="gl-display-flex gl-align-items-center gl-mr-5">
@@ -64,9 +77,9 @@ export default {
       <div v-if="packagePipeline" class="gl-display-flex gl-align-items-center gl-mr-5">
         <gl-icon name="review-list" class="text-secondary gl-mr-3" />
         <gl-link
-          ref="pipeline-project"
+          data-testid="pipeline-project"
           :href="packagePipeline.project.web_url"
-          class="font-weight-bold text-truncate"
+          class="gl-font-weight-bold text-truncate"
         >
           {{ packagePipeline.project.name }}
         </gl-link>
@@ -74,13 +87,13 @@ export default {
 
       <div
         v-if="packagePipeline"
-        ref="package-ref"
+        data-testid="package-ref"
         class="gl-display-flex gl-align-items-center gl-mr-5"
       >
         <gl-icon name="branch" class="text-secondary gl-mr-3" />
         <span
           v-gl-tooltip
-          class="font-weight-bold text-truncate mw-xs"
+          class="gl-font-weight-bold text-truncate mw-xs"
           :title="packagePipeline.ref"
           >{{ packagePipeline.ref }}</span
         >
@@ -88,7 +101,7 @@ export default {
 
       <div class="gl-display-flex gl-align-items-center gl-mr-5">
         <gl-icon name="disk" class="text-secondary gl-mr-3" />
-        <span ref="package-size" class="font-weight-bold">{{ totalSize }}</span>
+        <span data-testid="package-size" class="gl-font-weight-bold">{{ totalSize }}</span>
       </div>
     </div>
   </div>
