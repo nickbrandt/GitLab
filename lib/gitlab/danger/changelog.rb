ef2e079900f169badd3ee8ce521eae834ec00729
Rémy Coutable <remy@rymai.me>
2020-06-03 17:59:37 +0200

    Transition to scoped feature/tooling labels instead of backstage
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- lib/gitlab/danger/changelog.rb
+++ lib/gitlab/danger/changelog.rb
@@ -3,7 +3,7 @@
 module Gitlab
   module Danger
     module Changelog
-      NO_CHANGELOG_LABELS = %w[backstage ci-build meta].freeze
+      NO_CHANGELOG_LABELS = %w[tooling::pipelines tooling::workflow ci-build meta].freeze
       NO_CHANGELOG_CATEGORIES = %i[docs none].freeze
 
       def needed?
