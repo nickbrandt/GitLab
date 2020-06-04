804222427ba9f4a176847a391335a5b38e216fe5
Mark Chao <mchao@gitlab.com>
2020-06-04 15:12:57 +0800

    Note feature level in spec
    [skip-ci]

--- ee/spec/lib/gitlab/gl_repository_spec.rb
+++ ee/spec/lib/gitlab/gl_repository_spec.rb
@@ -6,6 +6,7 @@ describe ::Gitlab::GlRepository do
   describe '.parse' do
     let_it_be(:group) { create(:group) }
 
+    # Group Wiki is a GitLab Starter feature
     it 'parses a group wiki gl_repository' do
       expect(described_class.parse("group-#{group.id}-wiki")).to eq([group, nil, Gitlab::GlRepository::WIKI])
     end
