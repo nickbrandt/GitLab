nickbrandt/GitLab

commit fe5533a7b102d0f989c00c911b5f20265a92e716

Add group wiki model
- Refactor existing `ProjectWiki` into `Wiki`, and add new child classes
  `ProjectWiki` and `GroupWiki`.
- Rename `project` to `container` in the wiki classes.
- Add `HasWiki` concern for use in `Project` and `Group`.
- Add `@groups` prefix in `Storage::Hashed`.
- Refactor existing specs into shared examples.

app/models/wiki.rb
@@ -1,0 +1,221 @@
+# frozen_string_literal: true
+
+class Wiki
+  extend ::Gitlab::Utils::Override
+  include HasRepository
+  include Gitlab::Utils::StrongMemoize
+
+  MARKUPS = { # rubocop:disable Style/MultilineIfModifier
+    'Markdown' => :markdown,
+    'RDoc'     => :rdoc,
+    'AsciiDoc' => :asciidoc,
+    'Org'      => :org
+  }.freeze unless defined?(MARKUPS)
+
+  CouldNotCreateWikiError = Class.new(StandardError)
+
+  HOMEPAGE = 'home'
+  SIDEBAR = '_sidebar'
+
+  TITLE_ORDER = 'title'
+  CREATED_AT_ORDER = 'created_at'
+  DIRECTION_DESC = 'desc'
+  DIRECTION_ASC = 'asc'
+
+  attr_reader :container, :user
+
+  # Returns a string describing what went wrong after
+  # an operation fails.
+  attr_reader :error_message
+
+  def self.for_container(container, user = nil)
+    "#{container.class.name}Wiki".constantize.new(container, user)
+  end
+
+  def initialize(container, user = nil)
+    @container = container
+    @user = user
+  end
+
+  def path
+    container.path + '.wiki'
+  end
+
+  # Returns the Gitlab::Git::Wiki object.
+  def wiki
+    strong_memoize(:wiki) do
+      repository.create_if_not_exists
+      raise CouldNotCreateWikiError unless repository_exists?
+
+      Gitlab::Git::Wiki.new(repository.raw)
+    end
+  rescue => err
+    Gitlab::ErrorTracking.track_exception(err, wiki: {
+      container_type: container.class.name,
+      container_id: container.id,
+      full_path: full_path,
+      disk_path: disk_path
+    })
+
+    raise CouldNotCreateWikiError
+  end
+
+  def has_home_page?
+    !!find_page(HOMEPAGE)
+  end
+
+  def empty?
+    list_pages(limit: 1).empty?
+  end
+
+  def exists?
+    !empty?
+  end
+
+  # Lists wiki pages of the repository.
+  #
+  # limit - max number of pages returned by the method.
+  # sort - criterion by which the pages are sorted.
+  # direction - order of the sorted pages.
+  # load_content - option, which specifies whether the content inside the page
+  #                will be loaded.
+  #
+  # Returns an Array of GitLab WikiPage instances or an
+  # empty Array if this Wiki has no pages.
+  def list_pages(limit: 0, sort: nil, direction: DIRECTION_ASC, load_content: false)
+    wiki.list_pages(
+      limit: limit,
+      sort: sort,
+      direction_desc: direction == DIRECTION_DESC,
+      load_content: load_content
+    ).map do |page|
+      WikiPage.new(self, page)
+    end
+  end
+
+  # Finds a page within the repository based on a tile
+  # or slug.
+  #
+  # title - The human readable or parameterized title of
+  #         the page.
+  #
+  # Returns an initialized WikiPage instance or nil
+  def find_page(title, version = nil)
+    page_title, page_dir = page_title_and_dir(title)
+
+    if page = wiki.page(title: page_title, version: version, dir: page_dir)
+      WikiPage.new(self, page)
+    end
+  end
+
+  def find_sidebar(version = nil)
+    find_page(SIDEBAR, version)
+  end
+
+  def find_file(name, version = nil)
+    wiki.file(name, version)
+  end
+
+  def create_page(title, content, format = :markdown, message = nil)
+    commit = commit_details(:created, message, title)
+
+    wiki.write_page(title, format.to_sym, content, commit)
+
+    update_container_activity
+  rescue Gitlab::Git::Wiki::DuplicatePageError => e
+    @error_message = "Duplicate page: #{e.message}"
+    false
+  end
+
+  def update_page(page, content:, title: nil, format: :markdown, message: nil)
+    commit = commit_details(:updated, message, page.title)
+
+    wiki.update_page(page.path, title || page.name, format.to_sym, content, commit)
+
+    update_container_activity
+  end
+
+  def delete_page(page, message = nil)
+    return unless page
+
+    wiki.delete_page(page.path, commit_details(:deleted, message, page.title))
+
+    update_container_activity
+  end
+
+  def page_title_and_dir(title)
+    return unless title
+
+    title_array = title.split("/")
+    title = title_array.pop
+    [title, title_array.join("/")]
+  end
+
+  def ensure_repository
+    raise CouldNotCreateWikiError unless wiki.repository_exists?
+  end
+
+  def hook_attrs
+    {
+      web_url: web_url,
+      git_ssh_url: ssh_url_to_repo,
+      git_http_url: http_url_to_repo,
+      path_with_namespace: full_path,
+      default_branch: default_branch
+    }
+  end
+
+  override :repository
+  def repository
+    @repository ||= Repository.new(full_path, container, shard: repository_storage, disk_path: disk_path, repo_type: Gitlab::GlRepository::WIKI)
+  end
+
+  def repository_storage
+    raise NotImplementedError
+  end
+
+  def hashed_storage?
+    raise NotImplementedError
+  end
+
+  override :full_path
+  def full_path
+    container.full_path + '.wiki'
+  end
+  alias_method :id, :full_path
+
+  # @deprecated use full_path when you need it for an URL route or disk_path when you want to point to the filesystem
+  alias_method :path_with_namespace, :full_path
+
+  override :default_branch
+  def default_branch
+    wiki.class.default_ref
+  end
+
+  def wiki_base_path
+    Gitlab.config.gitlab.relative_url_root + web_url(only_path: true).sub(%r{/#{Wiki::HOMEPAGE}\z}, '')
+  end
+
+  private
+
+  def commit_details(action, message = nil, title = nil)
+    commit_message = message.presence || default_message(action, title)
+    git_user = Gitlab::Git::User.from_gitlab(user)
+
+    Gitlab::Git::Wiki::CommitDetails.new(user.id,
+                                         git_user.username,
+                                         git_user.name,
+                                         git_user.email,
+                                         commit_message)
+  end
+
+  def default_message(action, title)
+    "#{user.username} #{action} page: #{title}"
+  end
+
+  def update_container_activity
+    container.after_wiki_activity
+  end
+end
+
+Wiki.prepend_if_ee('EE::Wiki')

spec/lib/banzai/pipeline/wiki_pipeline_spec.rb
@@ -59,8 +59,8 @@ describe Banzai::Pipeline::WikiPipeline do
     let(:project_wiki) { ProjectWiki.new(project, double(:user)) }
     let(:page) { build(:wiki_page, wiki: project_wiki, page: OpenStruct.new(url_path: 'nested/twice/start-page')) }
 
-    { "when GitLab is hosted at a root URL" => '/',
-      "when GitLab is hosted at a relative URL" => '/nested/relative/gitlab' }.each do |test_name, relative_url_root|
+    { 'when GitLab is hosted at a root URL' => '',
+      'when GitLab is hosted at a relative URL' => '/nested/relative/gitlab' }.each do |test_name, relative_url_root|
       context test_name do
         before do
           allow(Gitlab.config.gitlab).to receive(:relative_url_root).and_return(relative_url_root)

spec/support/shared_examples/models/wiki_shared_examples.rb
@@ -1,0 +1,400 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'wiki model' do
+  let_it_be(:user) { create(:user, :commit_email) }
+  let(:wiki_container) { raise NotImplementedError }
+  let(:wiki_container_without_repo) { raise NotImplementedError }
+  let(:wiki) { described_class.new(wiki_container, user) }
+  let(:commit) { subject.repository.head_commit }
+
+  subject { wiki }
+
+  it_behaves_like 'model with repository' do
+    let(:container) { wiki }
+    let(:stubbed_container) { described_class.new(wiki_container_without_repo, user) }
+    let(:expected_full_path) { "#{container.container.full_path}.wiki" }
+    let(:expected_web_url_path) { "#{container.container.web_url(only_path: true).sub(%r{^/}, '')}/-/wikis/home" }
+  end
+
+  describe '#repository' do
+    it 'returns a wiki repository' do
+      expect(subject.repository.repo_type).to be_wiki
+    end
+  end
+
+  describe '#full_path' do
+    it 'returns the container path with the .wiki extension' do
+      expect(subject.full_path).to eq(wiki_container.full_path + '.wiki')
+    end
+  end
+
+  describe '#wiki_base_path' do
+    it 'returns the wiki base path' do
+      expect(subject.wiki_base_path).to eq("#{wiki_container.web_url(only_path: true)}/-/wikis")
+    end
+  end
+
+  describe '#wiki' do
+    it 'contains a Gitlab::Git::Wiki instance' do
+      expect(subject.wiki).to be_a Gitlab::Git::Wiki
+    end
+
+    it 'creates a new wiki repo if one does not yet exist' do
+      expect(subject.create_page('index', 'test content')).to be_truthy
+    end
+
+    it 'creates a new wiki repo with a default commit message' do
+      expect(subject.create_page('index', 'test content', :markdown, '')).to be_truthy
+
+      page = subject.find_page('index')
+
+      expect(page.last_version.message).to eq("#{user.username} created page: index")
+    end
+
+    context 'when the repository cannot be created' do
+      let(:wiki_container) { wiki_container_without_repo }
+
+      before do
+        expect(subject.repository).to receive(:create_if_not_exists) { false }
+      end
+
+      it 'raises CouldNotCreateWikiError' do
+        expect { subject.wiki }.to raise_exception(Wiki::CouldNotCreateWikiError)
+      end
+    end
+  end
+
+  describe '#empty?' do
+    context 'when the wiki repository is empty' do
+      it 'returns true' do
+        expect(subject.empty?).to be(true)
+      end
+    end
+
+    context 'when the wiki has pages' do
+      before do
+        subject.create_page('index', 'This is an awesome new Gollum Wiki')
+        subject.create_page('another-page', 'This is another page')
+      end
+
+      describe '#empty?' do
+        it 'returns false' do
+          expect(subject.empty?).to be(false)
+        end
+
+        it 'only instantiates a Wiki page once' do
+          expect(WikiPage).to receive(:new).once.and_call_original
+
+          subject.empty?
+        end
+      end
+    end
+  end
+
+  describe '#list_pages' do
+    let(:wiki_pages) { subject.list_pages }
+
+    before do
+      create_page('index', 'This is an index')
+      create_page('index2', 'This is an index2')
+      create_page('an index3', 'This is an index3')
+    end
+
+    it 'returns an array of WikiPage instances' do
+      expect(wiki_pages.first).to be_a WikiPage
+    end
+
+    it 'does not load WikiPage content by default' do
+      wiki_pages.each do |page|
+        expect(page.content).to be_empty
+      end
+    end
+
+    it 'returns all pages by default' do
+      expect(wiki_pages.count).to eq(3)
+    end
+
+    context 'with limit option' do
+      it 'returns limited set of pages' do
+        expect(subject.list_pages(limit: 1).count).to eq(1)
+      end
+    end
+
+    context 'with sorting options' do
+      it 'returns pages sorted by title by default' do
+        pages = ['an index3', 'index', 'index2']
+
+        expect(subject.list_pages.map(&:title)).to eq(pages)
+        expect(subject.list_pages(direction: 'desc').map(&:title)).to eq(pages.reverse)
+      end
+
+      it 'returns pages sorted by created_at' do
+        pages = ['index', 'index2', 'an index3']
+
+        expect(subject.list_pages(sort: 'created_at').map(&:title)).to eq(pages)
+        expect(subject.list_pages(sort: 'created_at', direction: 'desc').map(&:title)).to eq(pages.reverse)
+      end
+    end
+
+    context 'with load_content option' do
+      let(:pages) { subject.list_pages(load_content: true) }
+
+      it 'loads WikiPage content' do
+        expect(pages.first.content).to eq('This is an index3')
+        expect(pages.second.content).to eq('This is an index')
+        expect(pages.third.content).to eq('This is an index2')
+      end
+    end
+  end
+
+  describe '#find_page' do
+    before do
+      create_page('index page', 'This is an awesome Gollum Wiki')
+    end
+
+    it 'returns the latest version of the page if it exists' do
+      page = subject.find_page('index page')
+
+      expect(page.title).to eq('index page')
+    end
+
+    it 'returns nil if the page does not exist' do
+      expect(subject.find_page('non-existent')).to eq(nil)
+    end
+
+    it 'can find a page by slug' do
+      page = subject.find_page('index-page')
+
+      expect(page.title).to eq('index page')
+    end
+
+    it 'returns a WikiPage instance' do
+      page = subject.find_page('index page')
+
+      expect(page).to be_a WikiPage
+    end
+
+    context 'pages with multibyte-character title' do
+      before do
+        create_page('autre pagé', "C'est un génial Gollum Wiki")
+      end
+
+      it 'can find a page by slug' do
+        page = subject.find_page('autre pagé')
+
+        expect(page.title).to eq('autre pagé')
+      end
+    end
+
+    context 'pages with invalidly-encoded content' do
+      before do
+        create_page('encoding is fun', "f\xFCr".b)
+      end
+
+      it 'can find the page' do
+        page = subject.find_page('encoding is fun')
+
+        expect(page.content).to eq('fr')
+      end
+    end
+  end
+
+  describe '#find_sidebar' do
+    before do
+      create_page(described_class::SIDEBAR, 'This is an awesome Sidebar')
+    end
+
+    it 'finds the page defined as _sidebar' do
+      page = subject.find_page('_sidebar')
+
+      expect(page.content).to eq('This is an awesome Sidebar')
+    end
+  end
+
+  describe '#find_file' do
+    let(:image) { File.open(Rails.root.join('spec', 'fixtures', 'big-image.png')) }
+
+    before do
+      subject.wiki # Make sure the wiki repo exists
+
+      subject.repository.create_file(user, 'image.png', image, branch_name: subject.default_branch, message: 'add image')
+    end
+
+    it 'returns the latest version of the file if it exists' do
+      file = subject.find_file('image.png')
+
+      expect(file.mime_type).to eq('image/png')
+    end
+
+    it 'returns nil if the page does not exist' do
+      expect(subject.find_file('non-existent')).to eq(nil)
+    end
+
+    it 'returns a Gitlab::Git::WikiFile instance' do
+      file = subject.find_file('image.png')
+
+      expect(file).to be_a Gitlab::Git::WikiFile
+    end
+
+    it 'returns the whole file' do
+      file = subject.find_file('image.png')
+      image.rewind
+
+      expect(file.raw_data.b).to eq(image.read.b)
+    end
+  end
+
+  describe '#create_page' do
+    it 'creates a new wiki page' do
+      expect(subject.create_page('test page', 'this is content')).not_to eq(false)
+      expect(subject.list_pages.count).to eq(1)
+    end
+
+    it 'returns false when a duplicate page exists' do
+      subject.create_page('test page', 'content')
+
+      expect(subject.create_page('test page', 'content')).to eq(false)
+    end
+
+    it 'stores an error message when a duplicate page exists' do
+      2.times { subject.create_page('test page', 'content') }
+
+      expect(subject.error_message).to match(/Duplicate page:/)
+    end
+
+    it 'sets the correct commit message' do
+      subject.create_page('test page', 'some content', :markdown, 'commit message')
+
+      expect(subject.list_pages.first.page.version.message).to eq('commit message')
+    end
+
+    it 'sets the correct commit email' do
+      subject.create_page('test page', 'content')
+
+      expect(user.commit_email).not_to eq(user.email)
+      expect(commit.author_email).to eq(user.commit_email)
+      expect(commit.committer_email).to eq(user.commit_email)
+    end
+
+    it 'updates container activity' do
+      expect(subject).to receive(:update_container_activity)
+
+      subject.create_page('Test Page', 'This is content')
+    end
+  end
+
+  describe '#update_page' do
+    before do
+      create_page('update-page', 'some content')
+      @gitlab_git_wiki_page = subject.wiki.page(title: 'update-page')
+      subject.update_page(
+        @gitlab_git_wiki_page,
+        content: 'some other content',
+        format: :markdown,
+        message: 'updated page'
+      )
+      @page = subject.list_pages(load_content: true).first.page
+    end
+
+    it 'updates the content of the page' do
+      expect(@page.raw_data).to eq('some other content')
+    end
+
+    it 'sets the correct commit message' do
+      expect(@page.version.message).to eq('updated page')
+    end
+
+    it 'sets the correct commit email' do
+      expect(user.commit_email).not_to eq(user.email)
+      expect(commit.author_email).to eq(user.commit_email)
+      expect(commit.committer_email).to eq(user.commit_email)
+    end
+
+    it 'updates container activity' do
+      expect(subject).to receive(:update_container_activity)
+
+      subject.update_page(
+        @gitlab_git_wiki_page,
+        content: 'Yet more content',
+        format: :markdown,
+        message: 'Updated page again'
+      )
+    end
+  end
+
+  describe '#delete_page' do
+    before do
+      create_page('index', 'some content')
+      @page = subject.wiki.page(title: 'index')
+    end
+
+    it 'deletes the page' do
+      subject.delete_page(@page)
+
+      expect(subject.list_pages.count).to eq(0)
+    end
+
+    it 'sets the correct commit email' do
+      subject.delete_page(@page)
+
+      expect(user.commit_email).not_to eq(user.email)
+      expect(commit.author_email).to eq(user.commit_email)
+      expect(commit.committer_email).to eq(user.commit_email)
+    end
+
+    it 'updates container activity' do
+      expect(subject).to receive(:update_container_activity)
+
+      subject.delete_page(@page)
+    end
+  end
+
+  describe '#ensure_repository' do
+    context 'if the repository exists' do
+      it 'does not create the repository' do
+        expect(subject.repository.exists?).to eq(true)
+        expect(subject.repository.raw).not_to receive(:create_repository)
+
+        subject.ensure_repository
+      end
+    end
+
+    context 'if the repository does not exist' do
+      let(:wiki_container) { wiki_container_without_repo }
+
+      it 'creates the repository' do
+        expect(subject.repository.exists?).to eq(false)
+
+        subject.ensure_repository
+
+        expect(subject.repository.exists?).to eq(true)
+      end
+    end
+  end
+
+  describe '#hook_attrs' do
+    it 'returns a hash with values' do
+      expect(subject.hook_attrs).to be_a Hash
+      expect(subject.hook_attrs.keys).to contain_exactly(:web_url, :git_ssh_url, :git_http_url, :path_with_namespace, :default_branch)
+    end
+  end
+
+  private
+
+  def create_temp_repo(path)
+    FileUtils.mkdir_p path
+    system(*%W(#{Gitlab.config.git.bin_path} init --quiet --bare -- #{path}))
+  end
+
+  def remove_temp_repo(path)
+    FileUtils.rm_rf path
+  end
+
+  def commit_details
+    Gitlab::Git::Wiki::CommitDetails.new(user.id, user.username, user.name, user.commit_email, 'test commit')
+  end
+
+  def create_page(name, content)
+    subject.wiki.write_page(name, :markdown, content, commit_details)
+  end
+end

ee/app/models/ee/wiki.rb
@@ -1,0 +1,13 @@
+# frozen_string_literal: true
+
+module EE
+  module Wiki
+    extend ActiveSupport::Concern
+
+    # No need to have a Kerberos Web url. Kerberos URL will be used only to
+    # clone
+    def kerberos_url_to_repo
+      [::Gitlab.config.build_gitlab_kerberos_url, '/', full_path, '.git'].join('')
+    end
+  end
+end

ee/spec/support/shared_examples/models/wiki_shared_examples.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
-require "spec_helper"
+RSpec.shared_examples_for 'EE wiki model' do
+  let_it_be(:user) { create(:user) }
+  let(:wiki) { described_class.for_container(wiki_container, user) }
 
-describe ProjectWiki do
-  let(:user) { create(:user, :commit_email) }
-  let(:project) { create(:project, :wiki_repo, namespace: user.namespace) }
-  let(:project_wiki) { described_class.new(project, user) }
+  subject { wiki }
 
-  subject { project_wiki }
-
-  describe "#kerberos_url_to_repo" do
+  describe '#kerberos_url_to_repo' do
     it 'returns valid kerberos url for this repo' do
       gitlab_kerberos_url = Gitlab.config.build_gitlab_kerberos_url
       repo_kerberos_url = "#{gitlab_kerberos_url}/#{subject.full_path}.git"

.rubocop_todo.yml
@@ -475,7 +475,6 @@ Style/MixinUsage:
 Style/MultilineIfModifier:
   Exclude:
     - 'app/helpers/snippets_helper.rb'
-    - 'app/models/project_wiki.rb'
     - 'app/services/ci/process_pipeline_service.rb'
     - 'lib/api/commit_statuses.rb'
 

app/models/concerns/has_repository.rb
@@ -9,7 +9,6 @@
 # needs any special behavior.
 module HasRepository
   extend ActiveSupport::Concern
-  include AfterCommitQueue
   include Referable
   include Gitlab::ShellAdapter
   include Gitlab::Utils::StrongMemoize

app/models/concerns/has_wiki.rb
@@ -1,0 +1,44 @@
+# frozen_string_literal: true
+
+module HasWiki
+  extend ActiveSupport::Concern
+
+  included do
+    validate :check_wiki_path_conflict
+  end
+
+  def create_wiki
+    wiki.wiki
+    true
+  rescue Wiki::CouldNotCreateWikiError
+    errors.add(:base, _('Failed to create wiki'))
+    false
+  end
+
+  def wiki
+    strong_memoize(:wiki) do
+      Wiki.for_container(self, self.owner)
+    end
+  end
+
+  def wiki_repository_exists?
+    wiki.repository_exists?
+  end
+
+  def after_wiki_activity
+    true
+  end
+
+  private
+
+  def check_wiki_path_conflict
+    return if path.blank?
+
+    path_to_check = path.ends_with?('.wiki') ? path.chomp('.wiki') : "#{path}.wiki"
+
+    if Project.in_namespace(parent_id).where(path: path_to_check).exists? ||
+        GroupsFinder.new(nil, parent: parent_id).execute.where(path: path_to_check).exists?
+      errors.add(:name, _('has already been taken'))
+    end
+  end
+end

commit 9a748ec1bbe15f7bcd61d450065b067c46eaf99f

Fix relative URL root in wiki_base_path
This was a bug introduced in fe5533a7b10.

Previously we were building the path manually and had to also include
`Gitlab.config.gitlab.relative_url_root`, but since we're using route
helpers now it's already included via `default_url_options`, which
resulted in a duplicated root in the URL.

app/models/wiki.rb
@@ -205,7 +205,7 @@ class Wiki
   end
 
   def wiki_base_path
-    Gitlab.config.gitlab.relative_url_root + web_url(only_path: true).sub(%r{/#{Wiki::HOMEPAGE}\z}, '')
+    web_url(only_path: true).sub(%r{/#{Wiki::HOMEPAGE}\z}, '')
   end
 
   private

spec/lib/banzai/pipeline/wiki_pipeline_spec.rb
@@ -63,7 +63,7 @@ describe Banzai::Pipeline::WikiPipeline do
       'when GitLab is hosted at a relative URL' => '/nested/relative/gitlab' }.each do |test_name, relative_url_root|
       context test_name do
         before do
-          allow(Gitlab.config.gitlab).to receive(:relative_url_root).and_return(relative_url_root)
+          allow(Rails.application.routes).to receive(:default_url_options).and_return(script_name: relative_url_root)
         end
 
         describe "linking to pages within the wiki" do

spec/support/shared_examples/models/wiki_shared_examples.rb
@@ -32,6 +32,13 @@ RSpec.shared_examples 'wiki model' do
     it 'returns the wiki base path' do
       expect(subject.wiki_base_path).to eq("#{wiki_container.web_url(only_path: true)}/-/wikis")
     end
+
+    it 'includes the relative URL root' do
+      allow(Rails.application.routes).to receive(:default_url_options).and_return(script_name: '/root')
+
+      expect(subject.wiki_base_path).to start_with('/root/')
+      expect(subject.wiki_base_path).not_to start_with('/root/root')
+    end
   end
 
   describe '#wiki' do