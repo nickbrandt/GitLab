448f7fae06a07c98a582a2150e8a3ba141d0d50b
Mario de la Ossa <mariodelaossa@gmail.com>
2020-06-09 14:34:58 -0600

    Do not set fallback mirror user
    - Notify owners and maintainers when mirror disabled. They should have
    some notification if the mirror was disabled due to the mirror user
    being deleted
    
    - Do not send mirror disabled email if user is deleted. It is possible
    to schedule a mail to a user while the user is being deleted in another
    worker. So we do a check that the user exists first.

--- ee/app/mailers/ee/emails/projects.rb
+++ ee/app/mailers/ee/emails/projects.rb
@@ -11,6 +11,17 @@ module EE
              subject: subject('Repository mirroring paused'))
       end
 
+      def mirror_was_disabled_email(project_id, user_id, deleted_user_name)
+        @project = ::Project.find(project_id)
+        user = ::User.find_by_id(user_id)
+        @deleted_user_name = deleted_user_name
+
+        return unless user
+
+        mail(to: user.notification_email_for(@project.group),
+             subject: subject('Repository mirroring disabled'))
+      end
+
       def project_mirror_user_changed_email(new_mirror_user_id, deleted_user_name, project_id)
         @project = ::Project.find(project_id)
         @deleted_user_name = deleted_user_name
