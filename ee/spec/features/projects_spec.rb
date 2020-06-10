87ff15e706aaeba34c056fee54bf96feb7a5bb77
Lee Tickett <lee@tickett.net>
2020-06-10 11:17:42 +0000

    Fix default path when creating project from group template

---
+++ ee/spec/features/projects_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Project' do
+  describe 'when creating from group template' do
+    let(:user) { create(:user) }
+    let(:group) { create(:group, name: 'parent-group') }
+    let(:template_subgroup) { create(:group, parent: group, name: 'template-subgroup') }
+    let(:other_subgroup) { create(:group, parent: group, name: 'other-subgroup') }
+    let(:template) { create(:project, namespace: template_subgroup) }
+
+    before do
+      stub_licensed_features(custom_project_templates: true)
+      group.add_owner(user)
+      group.update!(custom_project_templates_group_id: template_subgroup.id)
+      sign_in user
+    end
+
+    it "defaults to correct namespace", :js do
+      visit new_project_path
+      find('#create-from-template-tab').click
+      find('.custom-group-project-templates-tab').click
+      find("label[for=#{template.name}]").click
+
+      expect(find('.js-select-namespace')).to have_content group.name
+    end
+
+    it "uses supplied namespace", :js do
+      visit new_project_path(namespace_id: other_subgroup.id)
+      find('#create-from-template-tab').click
+      find('.custom-group-project-templates-tab').click
+      find("label[for=#{template.name}]").click
+
+      expect(find('.js-select-namespace')).to have_content other_subgroup.name
+    end
+  end
+end
