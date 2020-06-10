eb461157821e93e2736b67ca9a1c3fac3c2dddf7
Doug Stull <dstull@gitlab.com>
2020-06-10 08:46:02 +0000

    Add tracking for invite members experiment
    - to measure how successful adding invite
      members link is to assignee dropdown

--- ee/app/assets/javascripts/pages/projects/issues/show/index.js
+++ ee/app/assets/javascripts/pages/projects/issues/show/index.js
@@ -2,6 +2,7 @@ import initSidebarBundle from 'ee/sidebar/sidebar_bundle';
 import initRelatedIssues from 'ee/related_issues';
 import initShow from '~/pages/projects/issues/show';
 import UserCallout from '~/user_callout';
+import trackShowInviteMemberLink from 'ee/projects/track_invite_members';
 
 document.addEventListener('DOMContentLoaded', () => {
   initShow();
@@ -14,4 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
   new UserCallout({ className: 'js-epics-sidebar-callout' });
   // eslint-disable-next-line no-new
   new UserCallout({ className: 'js-weight-sidebar-callout' });
+
+  const assigneeDropdown = document.querySelector('.js-sidebar-assignee-dropdown');
+
+  if (assigneeDropdown) trackShowInviteMemberLink(assigneeDropdown);
 });
