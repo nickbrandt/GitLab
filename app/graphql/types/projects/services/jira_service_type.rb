9e5a576ae763f9f329b2da762b2498b8b91f7426
Alexandru Croitor <acroitor@gitlab.com>
2020-06-10 22:24:04 +0300

    Change projects to return all projects in a single request
    Replace the paginated `projects` field implementation with one
    that returns all Jira projects in one request. This is required
    in order to support Jira Server API that does not yet support
    getting the paginated list of projects.

--- app/graphql/types/projects/services/jira_service_type.rb
+++ app/graphql/types/projects/services/jira_service_type.rb
@@ -10,26 +10,13 @@ module Types
 
         authorize :admin_project
 
-        field :all_projects,
-              [Types::Projects::Services::JiraProjectType],
-              null: true,
-              connection: false,
-              description: 'List of all Jira projects fetched through Jira REST API. Latest Jira Server API version ([8.9.0](https://docs.atlassian.com/software/jira/docs/api/REST/8.9.0/)) compatible.'
-
         field :projects,
               Types::Projects::Services::JiraProjectType.connection_type,
               null: true,
               connection: false,
               extensions: [Gitlab::Graphql::Extensions::ExternallyPaginatedArrayExtension],
-              description: 'List of Jira projects fetched through Jira REST API',
+              description: 'List of all Jira projects fetched through Jira REST API',
               resolver: Resolvers::Projects::JiraProjectsResolver
-
-        def all_projects
-          raise Gitlab::Graphql::Errors::BaseError, _('Jira service not configured.') unless object&.active?
-          raise Gitlab::Graphql::Errors::BaseError, _('Unable to connect to the Jira instance. Please check your Jira integration configuration.') unless object.test(nil)[:success]
-
-          object.client.Project.all
-        end
       end
     end
   end
