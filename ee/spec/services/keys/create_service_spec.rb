77baf398de4a491f60031075cc8bff64ed9ab1f1
Rajendra Kadam <rajendrakadam249@gmail.com>
2020-06-11 19:08:06 +0530

    Add spec for checking Security event and fix specs

--- ee/spec/services/keys/create_service_spec.rb
+++ ee/spec/services/keys/create_service_spec.rb
@@ -3,14 +3,18 @@
 require 'spec_helper'
 
 RSpec.describe Keys::CreateService do
+  let(:admin_user) { create(:admin)}
   let(:user) { create(:user) }
-  let(:params) { attributes_for(:key) }
+  let(:params) { attributes_for(:key).merge(user: user) }
 
-  subject { described_class.new(user, params) }
+  subject { described_class.new(admin_user, params) }
 
   it 'creates' do
     stub_licensed_features(extended_audit_events: true)
 
     expect { subject.execute }.to change { SecurityEvent.count }.by(1)
+    event = SecurityEvent.last
+    expect(event.author_name).to eq(admin_user.name)
+    expect(event.entity_id).to eq(user.id)
   end
 end
