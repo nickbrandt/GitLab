feaad3cea03cbb53db82d4490e129e9b667a6100
Zack Cuddy <zcuddy@gitlab.com>
2020-06-11 10:19:26 -0500

    Geo Settings Form - UI
    This MR is an attempt at MVC.
    
    This MR adds the form elements to
    the new setting form.
    
    Following this MR we will add
    the API hook up and validations.

--- ee/app/assets/javascripts/geo_settings/components/app.vue
+++ ee/app/assets/javascripts/geo_settings/components/app.vue
@@ -1,11 +1,24 @@
 <script>
+import GeoSettingsForm from './geo_settings_form.vue';
+
 export default {
   name: 'GeoSettingsApp',
+  components: {
+    GeoSettingsForm,
+  },
 };
 </script>
 
 <template>
   <article data-testid="geoSettingsContainer">
     <h3 class="page-title">{{ __('Geo Settings') }}</h3>
+    <p>
+      {{
+        __(
+          'Set the timeout in seconds to send a secondary node status to the primary and IPs allowed for the secondary nodes.',
+        )
+      }}
+    </p>
+    <geo-settings-form />
   </article>
 </template>
---
+++ ee/app/assets/javascripts/geo_settings/components/geo_settings_form.vue
@@ -0,0 +1,62 @@
+<script>
+import { GlFormGroup, GlFormInput, GlButton } from '@gitlab/ui';
+import { visitUrl } from '~/lib/utils/url_utility';
+import { DEFAULT_TIMEOUT, DEFAULT_ALLOWED_IP } from '../constants';
+
+export default {
+  name: 'GeoSettingsForm',
+  components: {
+    GlFormGroup,
+    GlFormInput,
+    GlButton,
+  },
+  data() {
+    return {
+      timeout: DEFAULT_TIMEOUT,
+      allowedIp: DEFAULT_ALLOWED_IP,
+    };
+  },
+  methods: {
+    redirect() {
+      visitUrl('/admin/geo/nodes');
+    },
+  },
+};
+</script>
+
+<template>
+  <form>
+    <gl-form-group
+      :label="__('Connection timeout')"
+      label-for="settings-timeout-field"
+      :description="__('Time in seconds')"
+    >
+      <gl-form-input id="settings-timeout-field" v-model="timeout" class="col-sm-2" type="number" />
+    </gl-form-group>
+    <gl-form-group
+      :label="__('Allowed Geo IP')"
+      label-for="settings-allowed-ip-field"
+      :description="__('Comma-separated, e.g. \'1.1.1.1, 2.2.2.0/24\'')"
+    >
+      <gl-form-input
+        id="settings-allowed-ip-field"
+        v-model="allowedIp"
+        class="col-sm-6"
+        type="text"
+      />
+    </gl-form-group>
+    <section
+      class="gl-display-flex gl-align-items-center gl-p-5 gl-mt-6 gl-bg-gray-10 gl-border-t-solid gl-border-b-solid gl-border-t-1 gl-border-b-1 gl-border-gray-200"
+    >
+      <gl-button
+        data-testid="settingsSaveButton"
+        data-qa-selector="add_node_button"
+        variant="success"
+        >{{ __('Save changes') }}</gl-button
+      >
+      <gl-button data-testid="settingsCancelButton" class="gl-ml-auto" @click="redirect">{{
+        __('Cancel')
+      }}</gl-button>
+    </section>
+  </form>
+</template>
