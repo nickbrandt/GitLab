21dac779ae8da7c8fc614da98c2a893bc0777901
Florie Guibert <fguibert@gitlab.com>
2020-06-11 06:35:44 +0000

    Swimlanes column headers
    - Move board header out of board column for reusability

--- ee/app/assets/javascripts/boards/components/board_column.vue
+++ ee/app/assets/javascripts/boards/components/board_column.vue
@@ -1,56 +1,11 @@
 <script>
-import { mapState, mapActions } from 'vuex';
 import BoardColumnFoss from '~/boards/components/board_column.vue';
-import { __, sprintf, s__ } from '~/locale';
-import boardsStore from '~/boards/stores/boards_store';
-import { inactiveListId } from '~/boards/constants';
 import BoardPromotionState from 'ee/boards/components/board_promotion_state';
-import eventHub from '~/sidebar/event_hub';
 
 export default {
   components: {
     BoardPromotionState,
   },
   extends: BoardColumnFoss,
-  data() {
-    return {
-      weightFeatureAvailable: boardsStore.weightFeatureAvailable,
-    };
-  },
-  computed: {
-    ...mapState(['activeListId']),
-    issuesTooltip() {
-      const { issuesSize, maxIssueCount } = this.list;
-
-      if (maxIssueCount > 0) {
-        return sprintf(__('%{issuesSize} issues with a limit of %{maxIssueCount}'), {
-          issuesSize,
-          maxIssueCount,
-        });
-      }
-
-      // TODO: Remove this pattern.
-      return BoardColumnFoss.computed.issuesTooltip.call(this);
-    },
-    weightCountToolTip() {
-      const { totalWeight } = this.list;
-
-      if (this.weightFeatureAvailable) {
-        return sprintf(s__('%{totalWeight} total weight'), { totalWeight });
-      }
-
-      return null;
-    },
-  },
-  methods: {
-    ...mapActions(['setActiveListId']),
-    openSidebarSettings() {
-      if (this.activeListId === inactiveListId) {
-        eventHub.$emit('sidebar.closeAll');
-      }
-
-      this.setActiveListId(this.list.id);
-    },
-  },
 };
 </script>
---
+++ ee/app/assets/javascripts/boards/components/epics_swimlanes.vue
@@ -0,0 +1,66 @@
+<script>
+import BoardListHeader from 'ee_else_ce/boards/components/board_list_header.vue';
+
+export default {
+  components: {
+    BoardListHeader,
+  },
+  props: {
+    lists: {
+      type: Array,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+      required: true,
+    },
+    issueLinkBase: {
+      type: String,
+      required: true,
+    },
+    rootPath: {
+      type: String,
+      required: true,
+    },
+    boardId: {
+      type: String,
+      required: true,
+    },
+    canAdminList: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    groupId: {
+      type: Number,
+      required: false,
+      default: null,
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    class="board-epics-swimlanes gl-white-space-nowrap gl-py-5 gl-px-3"
+    data_qa_selector="board_epics_swimlanes"
+  >
+    <div
+      v-for="list in lists"
+      :key="list.id"
+      :class="{
+        'is-expandable': list.isExpandable,
+        'is-collapsed': !list.isExpanded,
+      }"
+      class="board gl-h-full gl-px-3 gl-vertical-align-top gl-white-space-normal"
+    >
+      <board-list-header
+        :can-admin-list="canAdminList"
+        :list="list"
+        :disabled="disabled"
+        :board-id="boardId"
+        :is-swimlanes-header="true"
+      />
+    </div>
+  </div>
+</template>
