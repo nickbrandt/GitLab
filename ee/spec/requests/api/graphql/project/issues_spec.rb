8610f33d81bd2d203fc2b16beb8502f58ace248d
Brett Walker <bwalker@gitlab.com>
2020-06-11 23:38:03 +0000

    Use shared pagination specs
    for board lists

--- ee/spec/requests/api/graphql/project/issues_spec.rb
+++ ee/spec/requests/api/graphql/project/issues_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'getting an issue list for a project' do
       graphql_query_for(
         'project',
         { 'fullPath' => sort_project.full_path },
-        "issues(#{params}) { #{page_info} edges { node { iid weight } } }"
+        query_graphql_field('issues', params, "#{page_info} edges { node { iid weight} }")
       )
     end
 
