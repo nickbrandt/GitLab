31db89401f08e2a49e42d50dd3a231f1b8ed072e
Patrick Bajao <ebajao@gitlab.com>
2020-06-16 16:45:19 +0800

    Clear hard failure when updating mirror via UI
    There was a regression wherein updating a mirror that hard failed
    wasn't updating even when the update is triggered via web UI.
    
    It was because the `StartPullMirroringService` was reseting the
    retry count before calling `ProjectImportState#force_import_jobi!`.
    As a result, the method will return and do nothing because it
    detects that the mirror is due to be updated and it's no longer
    considered as a hard failure.
    
    To fix it, we remove the need to reset the retry count before
    `#force_import_job!` is called. The retry count will be reset in
    `#force_import_job!` once it deemed that the mirror can be force
    updated.
    
    Now, we only reset the retry count when updating the next
    execution timestamp in the said service.

--- ee/app/services/start_pull_mirroring_service.rb
+++ ee/app/services/start_pull_mirroring_service.rb
@@ -6,15 +6,12 @@ class StartPullMirroringService < BaseService
   def execute
     import_state = project.import_state
 
-    if import_state.hard_failed?
-      return error('Mirroring for the project is on pause', 403) if params[:pause_on_hard_failure]
-
-      import_state.reset_retry_count
-    end
+    return error('Mirroring for the project is on pause', 403) if params[:pause_on_hard_failure] && import_state.hard_failed?
 
     if update_now?(import_state)
       import_state.force_import_job!
     else
+      import_state.reset_retry_count if import_state.hard_failed?
       import_state.update(next_execution_timestamp: INTERVAL.since(import_state.last_update_at))
     end
 
