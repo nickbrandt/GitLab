4167feefc8264cd9cfb524e9c1d4e4c6bd7c716d
Ezekiel Kigbo <ekigbo@gitlab.com>
2020-06-17 10:06:17 +1000

    Fetch data for the filter bar tokens
    Adds vuex actions to fetch and update
    data for the VSA filter bar tokens
    
    Adds additional action / mutation specs

--- ee/spec/frontend/analytics/cycle_analytics/store/modules/filters/actions_spec.js
+++ ee/spec/frontend/analytics/cycle_analytics/store/modules/filters/actions_spec.js
@@ -4,7 +4,8 @@ import testAction from 'helpers/vuex_action_helper';
 import * as actions from 'ee/analytics/cycle_analytics/store/modules/filters/actions';
 import * as types from 'ee/analytics/cycle_analytics/store/modules/filters/mutation_types';
 import initialState from 'ee/analytics/cycle_analytics/store/modules/filters/state';
-import { filterLabels } from '../../../mock_data';
+import createFlash from '~/flash';
+import { filterMilestones, filterUsers, filterLabels } from '../../../mock_data';
 
 const milestonesPath = 'fake_milestones_path';
 const labelsPath = 'fake_labels_path';
@@ -77,8 +78,185 @@ describe('Filters actions', () => {
           { payload: 'fake_milestones_path.json', type: types.SET_MILESTONES_PATH },
           { payload: 'fake_labels_path.json', type: types.SET_LABELS_PATH },
         ],
-        [],
+        [
+          { type: 'fetchLabels' },
+          { type: 'fetchMilestones' },
+          { type: 'fetchAuthors' },
+          { type: 'fetchAssignees' },
+        ],
       );
+    });
+  });
+
+  describe('fetchAuthors', () => {
+    describe('success', () => {
+      beforeEach(() => {
+        mock.onAny().replyOnce(200, filterUsers);
+      });
+
+      it('dispatches RECEIVE_AUTHORS_SUCCESS with received data', () => {
+        testAction(
+          actions.fetchAuthors,
+          null,
+          state,
+          [
+            { type: types.REQUEST_AUTHORS },
+            { type: types.RECEIVE_AUTHORS_SUCCESS, payload: filterUsers },
+          ],
+          [],
+        );
+      });
+    });
+
+    describe('error', () => {
+      beforeEach(() => {
+        mock.onAny().replyOnce(500);
+      });
+
+      it('dispatches RECEIVE_AUTHORS_ERROR', () => {
+        return testAction(
+          actions.fetchAuthors,
+          null,
+          state,
+          [
+            { type: types.REQUEST_AUTHORS },
+            {
+              type: types.RECEIVE_AUTHORS_ERROR,
+              payload: 500,
+            },
+          ],
+          [],
+        ).then(() => expect(createFlash).toHaveBeenCalled());
+      });
+    });
+  });
+
+  describe('fetchMilestones', () => {
+    describe('success', () => {
+      beforeEach(() => {
+        mock.onGet(milestonesPath).replyOnce(200, filterMilestones);
+      });
+
+      it('dispatches RECEIVE_MILESTONES_SUCCESS with received data', () => {
+        testAction(
+          actions.fetchMilestones,
+          null,
+          { ...state, milestonesPath },
+          [
+            { type: types.REQUEST_MILESTONES },
+            { type: types.RECEIVE_MILESTONES_SUCCESS, payload: filterMilestones },
+          ],
+          [],
+        );
+      });
+    });
+
+    describe('error', () => {
+      beforeEach(() => {
+        mock.onAny().replyOnce(500);
+      });
+
+      it('dispatches RECEIVE_MILESTONES_ERROR', () => {
+        return testAction(
+          actions.fetchMilestones,
+          null,
+          state,
+          [
+            { type: types.REQUEST_MILESTONES },
+            {
+              type: types.RECEIVE_MILESTONES_ERROR,
+              payload: 500,
+            },
+          ],
+          [],
+        ).then(() => expect(createFlash).toHaveBeenCalled());
+      });
+    });
+  });
+
+  describe('fetchAssignees', () => {
+    describe('success', () => {
+      beforeEach(() => {
+        mock.onAny().replyOnce(200, filterUsers);
+      });
+
+      it('dispatches RECEIVE_ASSIGNEES_SUCCESS with received data', () => {
+        testAction(
+          actions.fetchAssignees,
+          null,
+          { ...state, milestonesPath },
+          [
+            { type: types.REQUEST_ASSIGNEES },
+            { type: types.RECEIVE_ASSIGNEES_SUCCESS, payload: filterUsers },
+          ],
+          [],
+        );
+      });
+    });
+
+    describe('error', () => {
+      beforeEach(() => {
+        mock.onAny().replyOnce(500);
+      });
+
+      it('dispatches RECEIVE_ASSIGNEES_ERROR', () => {
+        return testAction(
+          actions.fetchAssignees,
+          null,
+          state,
+          [
+            { type: types.REQUEST_ASSIGNEES },
+            {
+              type: types.RECEIVE_ASSIGNEES_ERROR,
+              payload: 500,
+            },
+          ],
+          [],
+        ).then(() => expect(createFlash).toHaveBeenCalled());
+      });
+    });
+  });
+
+  describe('fetchLabels', () => {
+    describe('success', () => {
+      beforeEach(() => {
+        mock.onGet(labelsPath).replyOnce(200, filterLabels);
+      });
+
+      it('dispatches RECEIVE_LABELS_SUCCESS with received data', () => {
+        testAction(
+          actions.fetchLabels,
+          null,
+          { ...state, labelsPath },
+          [
+            { type: types.REQUEST_LABELS },
+            { type: types.RECEIVE_LABELS_SUCCESS, payload: filterLabels },
+          ],
+          [],
+        );
+      });
+    });
+
+    describe('error', () => {
+      beforeEach(() => {
+        mock.onAny().replyOnce(500);
+      });
+
+      it('dispatches RECEIVE_LABELS_ERROR', () => {
+        return testAction(
+          actions.fetchLabels,
+          null,
+          state,
+          [
+            { type: types.REQUEST_LABELS },
+            {
+              type: types.RECEIVE_LABELS_ERROR,
+              payload: 500,
+            },
+          ],
+          [],
+        ).then(() => expect(createFlash).toHaveBeenCalled());
+      });
     });
   });
 });
