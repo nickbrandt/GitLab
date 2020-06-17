5993f92aa5527681eeb02828d6843ec27d6ccfef
Alexander Turinske <aturinske@gitlab.com>
2020-06-17 16:26:24 -0600

    Sanitize vulnerability history comment
    - add sanitation to vulnerability history comments
    - add additional tests for XSS of alert and iframe
    - add changelog for comment sanitization
    - use comment.note_url instead of comment.note to render html
    - use .md class to normalize incoming CSS
    - Use v-html on note_url

--- ee/app/assets/javascripts/vulnerabilities/components/history_comment.vue
+++ ee/app/assets/javascripts/vulnerabilities/components/history_comment.vue
@@ -46,9 +46,6 @@ export default {
     noteIdUrl() {
       return joinPaths(this.notesUrl, this.comment.id);
     },
-    commentNote() {
-      return this.comment?.note;
-    },
     actionButtons() {
       return [
         {
@@ -139,8 +136,8 @@ export default {
 <template>
   <history-comment-editor
     v-if="isEditingComment"
-    class="discussion-reply-holder m-3"
-    :initial-comment="commentNote"
+    class="discussion-reply-holder"
+    :initial-comment="comment && comment.note"
     :is-saving="isSavingComment"
     @onSave="saveComment"
     @onCancel="cancelEditingComment"
@@ -158,7 +155,7 @@ export default {
     icon-class="timeline-icon m-0"
     class="m-3"
   >
-    <div v-html="comment.note"></div>
+    <div class="md" v-html="comment.note_html"></div>
 
     <template #right-content>
       <gl-button
