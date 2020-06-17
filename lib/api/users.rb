bc8a5ec7320f10e5b87f5e3ea45efcffbc2dd211
Rajendra Kadam <rajendrakadam249@gmail.com>
2020-06-17 19:38:41 +0530

    Use CreateService when an admin creates a new gpg key for a user

--- lib/api/users.rb
+++ lib/api/users.rb
@@ -328,9 +328,9 @@ module API
         user = User.find_by(id: params.delete(:id))
         not_found!('User') unless user
 
-        key = user.gpg_keys.new(declared_params(include_missing: false))
+        key = ::GpgKeys::CreateService.new(current_user, declared_params(include_missing: false).merge(user: user)).execute
 
-        if key.save
+        if key.persisted?
           present key, with: Entities::GpgKey
         else
           render_validation_error!(key)
