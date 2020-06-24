c2b88ee836bac5a4ec085ec6ad123b6f1c52e8e9
Toon Claes <toon@gitlab.com>
2020-06-24 10:50:40 +0000

    Remove separate list of EE Foreign Keys
    Because the complete schema is defined in the shared codebase, there
    is no need to have a separate list of ignored EE foreign keys.

--- ee/spec/db/schema_support.rb
+++ ee/spec/db/schema_support.rb
@@ -6,38 +6,10 @@ module EE
       extend ActiveSupport::Concern
 
       prepended do
-        EE_IGNORED_FK_COLUMNS = {
-          application_settings: %w[slack_app_id snowplow_app_id],
-          approvals: %w[user_id],
-          approver_groups: %w[target_id],
-          approvers: %w[target_id user_id],
-          boards: %w[milestone_id],
-          draft_notes: %w[discussion_id],
-          epics: %w[updated_by_id last_edited_by_id],
-          geo_event_log: %w[hashed_storage_attachments_event_id],
-          geo_job_artifact_deleted_events: %w[job_artifact_id],
-          geo_lfs_object_deleted_events: %w[lfs_object_id],
-          geo_node_statuses: %w[last_event_id cursor_last_event_id],
-          geo_nodes: %w[oauth_application_id],
-          geo_repository_deleted_events: %w[project_id],
-          geo_upload_deleted_events: %w[upload_id model_id],
-          ldap_group_links: %w[group_id],
-          projects: %w[mirror_user_id],
-          slack_integrations: %w[team_id user_id],
-          users: %w[email_opted_in_source_id],
-          vulnerability_identifiers: %w[external_id],
-          vulnerability_scanners: %w[external_id],
-          web_hooks: %w[group_id]
-        }.with_indifferent_access.freeze
-
         IGNORED_LIMIT_ENUMS = {
           'SoftwareLicensePolicy' => %w[classification],
           'User' => %w[group_view]
         }.freeze
-      end
-
-      def ignored_fk_columns(column)
-        super + EE_IGNORED_FK_COLUMNS.fetch(column, [])
       end
 
       def ignored_limit_enums(model)
