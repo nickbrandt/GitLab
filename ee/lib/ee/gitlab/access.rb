3e72d9ae52769f356e4ff1ba39ed6db74ec99dee
Mehmet Emin INAC <minac@gitlab.com>
2020-06-29 19:05:51 +0000

    Security fix project authorizations for security dashboard

--- ee/lib/ee/gitlab/access.rb
+++ ee/lib/ee/gitlab/access.rb
@@ -10,6 +10,12 @@ module EE
     module Access
       extend ActiveSupport::Concern
       ADMIN = 60
+
+      class_methods do
+        def vulnerability_access_levels
+          @vulnerability_access_levels ||= options_with_owner.except('Guest')
+        end
+      end
     end
   end
 end
