0885158a944d69cc94e5dadfa15661fa4a06a99a
Erick Banks <ebanks@gitlab.com>
2020-07-01 16:11:57 -0700

    Quarantine failing test

--- qa/qa/specs/features/ee/browser_ui/3_create/merge_request/approval_rules_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/3_create/merge_request/approval_rules_spec.rb
@@ -2,7 +2,7 @@
 
 module QA
   context 'Create' do
-    describe 'Approval rules' do
+    describe 'Approval rules', quarantine: { issue: 'https://gitlab.com/gitlab-org/gitlab/-/issues/225595', type: :investigating } do
       let(:approver1) { Resource::User.fabricate_or_use(Runtime::Env.gitlab_qa_username_1, Runtime::Env.gitlab_qa_password_1) }
       let(:approver2) { Resource::User.fabricate_or_use(Runtime::Env.gitlab_qa_username_2, Runtime::Env.gitlab_qa_password_2) }
       let(:project) do
