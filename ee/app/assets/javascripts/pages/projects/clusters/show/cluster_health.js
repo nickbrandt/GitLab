92f35b62abcde6ea14407f76ab389bff89c81a6c
Kushal Pandya <kushalspandya@gmail.com>
2020-07-01 08:52:40 +0000

    Revert "Merge branch 'jivanvl-move-cluster-health-frontend-core' into 'master'"
    This reverts merge request !35198

--- ee/app/assets/javascripts/pages/projects/clusters/show/cluster_health.js
+++ ee/app/assets/javascripts/pages/projects/clusters/show/cluster_health.js
@@ -1,10 +1,10 @@
-import monitoringApp from '~/monitoring/monitoring_app';
+import initCeBundle from '~/monitoring/monitoring_app';
 
 export default () => {
   const el = document.getElementById('prometheus-graphs');
 
   if (el && el.dataset) {
-    monitoringApp({
+    initCeBundle({
       ...el.dataset,
       showLegend: false,
       showHeader: false,
