257e881918366c743adb20ba3cc068caea45ed93
Andy Soiron <asoiron@gitlab.com>
2020-07-02 12:19:56 +0200

    Delete records in batches of 50

--- db/post_migrate/20200623142159_remove_gitlab_issue_tracker_service_records.rb
+++ db/post_migrate/20200623142159_remove_gitlab_issue_tracker_service_records.rb
@@ -3,10 +3,26 @@
 class RemoveGitlabIssueTrackerServiceRecords < ActiveRecord::Migration[6.0]
   DOWNTIME = false
 
+  disable_ddl_transaction!
+
+  class Service < ActiveRecord::Base
+    include EachBatch
+
+    self.table_name = 'services'
+  end
+
+  class GitlabIssueTrackerService < Service
+    def self.store_full_sti_class
+      false
+    end
+  end
+
   def up
-    execute <<~SQL.strip
-      DELETE FROM services WHERE type = 'GitlabIssueTrackerService';
-    SQL
+    while GitlabIssueTrackerService.any?
+      execute <<~SQL.strip
+        DELETE FROM services WHERE id IN (SELECT id FROM services WHERE type = 'GitlabIssueTrackerService' LIMIT 50)
+      SQL
+    end
   end
 
   def down
