de53647196f75c3250b2ba99efdfd3343902bebc
Terri Chu <tchu@gitlab.com>
2020-07-03 01:45:33 +0000

    Ensure newly paid groups are indexed in Elasticsearch
    Create a new ElasticsearchIndexedNamespace object
    when GitlabSubscription objects are created/updated
    to paid, hosted plans. Trials are not included but
    will be indexed if updated to a non-trial paid plan.

--- ee/spec/factories/gitlab_subscriptions.rb
+++ ee/spec/factories/gitlab_subscriptions.rb
@@ -28,5 +28,17 @@ FactoryBot.define do
     trait :gold do
       association :hosted_plan, factory: :gold_plan
     end
+
+    # for testing elasticsearch_indexed_namespace and elastic_namespace_rollout_worker which
+    # eventually will not be required once all paid groups are indexed
+    trait :without_index_namespace_callback do
+      after(:build) do |gitlab_subcription|
+        GitlabSubscription.skip_callback(:commit, :after, :index_namespace)
+      end
+
+      after(:create) do
+        GitlabSubscription.set_callback(:commit, :after, :index_namespace)
+      end
+    end
   end
 end
