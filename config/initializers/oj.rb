4e8fd2d9757b724a88c16c56c6e076df29b14816
Robert May <rmay@gitlab.com>
2020-07-07 14:02:09 +0100

    Ensure oj defaults to :rails mode

--- config/initializers/oj.rb
+++ config/initializers/oj.rb
@@ -1,7 +1,3 @@
 # Ensure Oj runs in json-gem compatibility mode by default
-#
-# Oj pollutes ActiveRecord by default without being told to,
-# so this setting is required in order to ensure it maintains
-# compatibility with the existing system.
 
 Oj.default_options = { mode: :rails }
