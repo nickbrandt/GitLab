ff85ebceb037c777c06e76ccc0aaf2786708620c
Brett Walker <bwalker@gitlab.com>
2020-07-08 18:13:58 -0500

    Convert jira issue dates to UTC on serialization

--- ee/spec/serializers/integrations/jira/issue_entity_spec.rb
+++ ee/spec/serializers/integrations/jira/issue_entity_spec.rb
@@ -25,9 +25,9 @@ RSpec.describe Integrations::Jira::IssueEntity do
     expect(subject).to include(
       project_id: project.id,
       title: 'summary',
-      created_at: '2020-06-25T15:39:30.000+0000',
-      updated_at: '2020-06-26T15:38:32.000+0000',
-      closed_at: '2020-06-27T13:23:51.000+0000',
+      created_at: '2020-06-25T15:39:30.000+0000'.to_datetime.utc,
+      updated_at: '2020-06-26T15:38:32.000+0000'.to_datetime.utc,
+      closed_at: '2020-06-27T13:23:51.000+0000'.to_datetime.utc,
       labels: [
         {
           name: 'backend',
