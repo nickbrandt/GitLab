67a0dd04aea7a0071ec94e77b78ff8050010fa9c
Nathan Friend <nathan@gitlab.com>
2020-07-08 14:42:21 -0400

    Update milestone_url to milestone_path
    This commit updates a usage of new_project_milestone_url to
    new_project_milestone_path.

--- app/helpers/releases_helper.rb
+++ app/helpers/releases_helper.rb
@@ -32,7 +32,7 @@ module ReleasesHelper
       update_release_api_docs_path: help_page_path('api/releases/index.md', anchor: 'update-a-release'),
       release_assets_docs_path: help_page(anchor: 'release-assets'),
       manage_milestones_path: project_milestones_path(@project),
-      new_milestone_path: new_project_milestone_url(@project)
+      new_milestone_path: new_project_milestone_path(@project)
     }
   end
 end
