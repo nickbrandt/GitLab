4e821852aa8f3b85c0630f040a6f71a309f908bf
Florie Guibert <fguibert@gitlab.com>
2020-07-08 10:35:42 +0000

    Swimlanes - Display issues
    - Add issues in lane for issues not assigned to an epic
    - Display issues under epics

--- ee/app/assets/javascripts/boards/components/epic_lane.vue
+++ ee/app/assets/javascripts/boards/components/epic_lane.vue
@@ -4,12 +4,14 @@ import { __, n__, sprintf } from '~/locale';
 import timeagoMixin from '~/vue_shared/mixins/timeago';
 import { formatDate } from '~/lib/utils/datetime_utility';
 import { statusType } from '../../epic/constants';
+import IssuesLaneList from './issues_lane_list.vue';
 
 export default {
   components: {
     GlIcon,
     GlLink,
     GlPopover,
+    IssuesLaneList,
   },
   directives: {
     GlTooltip: GlTooltipDirective,
@@ -18,6 +20,10 @@ export default {
   props: {
     epic: {
       type: Object,
+      required: true,
+    },
+    lists: {
+      type: Array,
       required: true,
     },
   },
@@ -54,41 +60,58 @@ export default {
       return formatDate(this.epic.createdAt);
     },
   },
+  methods: {
+    epicIssuesForList(listIssues) {
+      return this.epic.issues.filter(epicIssue =>
+        Boolean(listIssues.find(listIssue => String(listIssue.iid) === epicIssue.iid)),
+      );
+    },
+  },
 };
 </script>
 
 <template>
-  <div class="board-epic-lane gl-py-5 gl-px-3 gl-display-flex gl-align-items-center">
-    <gl-icon
-      class="gl-mr-2 gl-flex-shrink-0"
-      :class="stateIconClass"
-      :name="epicIcon"
-      :aria-label="stateText"
-    />
-    <span
-      ref="epicTitle"
-      class="gl-mr-3 gl-font-weight-bold gl-white-space-nowrap gl-text-overflow-ellipsis gl-overflow-hidden"
-    >
-      {{ epic.title }}
-    </span>
-    <gl-popover :target="() => $refs.epicTitle" triggers="hover" placement="top">
-      <template #title
-        >{{ epic.title }} &middot; {{ epic.reference }}</template
+  <div>
+    <div class="board-epic-lane gl-py-5 gl-px-3 gl-display-flex gl-align-items-center">
+      <gl-icon
+        class="gl-mr-2 gl-flex-shrink-0"
+        :class="stateIconClass"
+        :name="epicIcon"
+        :aria-label="stateText"
+      />
+      <span
+        ref="epicTitle"
+        class="gl-mr-3 gl-font-weight-bold gl-white-space-nowrap gl-text-overflow-ellipsis gl-overflow-hidden"
       >
-      <p class="gl-m-0">{{ epicTimeAgoString }}</p>
-      <p class="gl-mb-2">{{ epicDateString }}</p>
-      <gl-link :href="epic.webUrl" class="gl-font-sm">{{ __('Go to epic') }}</gl-link>
-    </gl-popover>
-    <span
-      v-gl-tooltip.hover
-      :title="issuesCountTooltipText"
-      class="gl-display-flex gl-align-items-center gl-text-gray-700"
-      tabindex="0"
-      :aria-label="issuesCountTooltipText"
-      data-testid="epic-lane-issue-count"
-    >
-      <gl-icon class="gl-mr-2 gl-flex-shrink-0" name="issues" aria-hidden="true" />
-      <span aria-hidden="true">{{ issuesCount }}</span>
-    </span>
+        {{ epic.title }}
+      </span>
+      <gl-popover :target="() => $refs.epicTitle" triggers="hover" placement="top">
+        <template #title
+          >{{ epic.title }} &middot; {{ epic.reference }}</template
+        >
+        <p class="gl-m-0">{{ epicTimeAgoString }}</p>
+        <p class="gl-mb-2">{{ epicDateString }}</p>
+        <gl-link :href="epic.webUrl" class="gl-font-sm">{{ __('Go to epic') }}</gl-link>
+      </gl-popover>
+      <span
+        v-gl-tooltip.hover
+        :title="issuesCountTooltipText"
+        class="gl-display-flex gl-align-items-center gl-text-gray-700"
+        tabindex="0"
+        :aria-label="issuesCountTooltipText"
+        data-testid="epic-lane-issue-count"
+      >
+        <gl-icon class="gl-mr-2 gl-flex-shrink-0" name="issues" aria-hidden="true" />
+        <span aria-hidden="true">{{ issuesCount }}</span>
+      </span>
+    </div>
+    <div class="gl-display-flex">
+      <issues-lane-list
+        v-for="list in lists"
+        :key="`${list.id}-issues`"
+        :list="list"
+        :issues="epicIssuesForList(list.issues)"
+      />
+    </div>
   </div>
 </template>
---
+++ ee/app/assets/javascripts/boards/components/issues_lane_list.vue
@@ -0,0 +1,39 @@
+<script>
+import BoardCard from '~/boards/components/board_card.vue';
+
+export default {
+  components: {
+    BoardCard,
+  },
+  props: {
+    list: {
+      type: Object,
+      required: true,
+    },
+    issues: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    class="board gl-px-3 gl-vertical-align-top gl-white-space-normal gl-display-flex gl-flex-shrink-0 is-expandable"
+    :class="{ 'is-collapsed': !list.isExpanded }"
+  >
+    <div class="board-inner gl-p-2 gl-rounded-base gl-relative gl-w-full">
+      <ul v-if="list.isExpanded" class="gl-p-0 gl-m-0">
+        <board-card
+          v-for="(issue, index) in issues"
+          ref="issue"
+          :key="issue.id"
+          :index="index"
+          :list="list"
+          :issue="issue"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
