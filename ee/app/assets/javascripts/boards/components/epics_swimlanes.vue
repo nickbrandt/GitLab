cccf4a69d539e0cd83d8a447472196687f6e51fd
Florie Guibert <fguibert@gitlab.com>
2020-07-09 07:42:40 +0000

    Swimlanes - Add issue form unassigned issues lane
    - Add issue form on click on add issue in list header
    - Scrolls to Unassigned issues lane to display form

--- ee/app/assets/javascripts/boards/components/epics_swimlanes.vue
+++ ee/app/assets/javascripts/boards/components/epics_swimlanes.vue
@@ -34,6 +34,11 @@ export default {
       required: false,
       default: false,
     },
+    groupId: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
   },
   computed: {
     ...mapState(['epics']),
@@ -49,47 +54,48 @@ export default {
 
 <template>
   <div
-    class="board-swimlanes gl-white-space-nowrap gl-py-5 gl-px-3"
+    class="board-swimlanes gl-white-space-nowrap gl-pb-5 gl-px-3"
     data_qa_selector="board_epics_swimlanes"
   >
-    <div
-      v-for="list in lists"
-      :key="list.id"
-      :class="{
-        'is-expandable': list.isExpandable,
-        'is-collapsed': !list.isExpanded,
-      }"
-      class="board gl-px-3 gl-vertical-align-top gl-white-space-normal"
-    >
-      <board-list-header
-        :can-admin-list="canAdminList"
-        :list="list"
-        :disabled="disabled"
-        :board-id="boardId"
-        :is-swimlanes-header="true"
-      />
+    <div class="board-swimlanes-headers gl-sticky gl-pt-5 gl-bg-white gl-top-0 gl-z-index-3">
+      <div
+        v-for="list in lists"
+        :key="list.id"
+        :class="{
+          'is-collapsed': !list.isExpanded,
+        }"
+        class="board gl-px-3 gl-vertical-align-top gl-white-space-normal"
+      >
+        <board-list-header
+          :can-admin-list="canAdminList"
+          :list="list"
+          :disabled="disabled"
+          :board-id="boardId"
+          :is-swimlanes-header="true"
+        />
+      </div>
     </div>
     <div class="board-epics-swimlanes">
       <epic-lane v-for="epic in epics" :key="epic.id" :epic="epic" :lists="lists" />
-      <div
-        class="board-lane-unassigned-issues gl-py-5 gl-px-3 gl-display-flex gl-align-items-center"
-      >
-        <span
-          class="gl-mr-3 gl-font-weight-bold gl-white-space-nowrap gl-text-overflow-ellipsis gl-overflow-hidden"
-        >
-          {{ __('Issues with no epic assigned') }}
-        </span>
-        <span
-          v-gl-tooltip.hover
-          :title="issuesCountTooltipText"
-          class="gl-display-flex gl-align-items-center gl-text-gray-700"
-          tabindex="0"
-          :aria-label="issuesCountTooltipText"
-          data-testid="issues-lane-issue-count"
-        >
-          <gl-icon class="gl-mr-2 gl-flex-shrink-0" name="issues" aria-hidden="true" />
-          <span aria-hidden="true">{{ issuesCount }}</span>
-        </span>
+      <div class="board-lane-unassigned-issues gl-sticky gl-display-inline-block gl-left-0">
+        <div class="gl-left-0 gl-py-5 gl-px-3 gl-display-flex gl-align-items-center">
+          <span
+            class="gl-mr-3 gl-font-weight-bold gl-white-space-nowrap gl-text-overflow-ellipsis gl-overflow-hidden"
+          >
+            {{ __('Issues with no epic assigned') }}
+          </span>
+          <span
+            v-gl-tooltip.hover
+            :title="issuesCountTooltipText"
+            class="gl-display-flex gl-align-items-center gl-text-gray-700"
+            tabindex="0"
+            :aria-label="issuesCountTooltipText"
+            data-testid="issues-lane-issue-count"
+          >
+            <gl-icon class="gl-mr-2 gl-flex-shrink-0" name="issues" aria-hidden="true" />
+            <span aria-hidden="true">{{ issuesCount }}</span>
+          </span>
+        </div>
       </div>
       <div class="gl-display-flex">
         <issues-lane-list
@@ -97,6 +103,8 @@ export default {
           :key="`${list.id}-issues`"
           :list="list"
           :issues="list.issues"
+          :group-id="groupId"
+          :is-unassigned-issues-lane="true"
         />
       </div>
     </div>
--- ee/app/assets/javascripts/boards/components/issues_lane_list.vue
+++ ee/app/assets/javascripts/boards/components/issues_lane_list.vue
@@ -1,9 +1,12 @@
 <script>
+import eventHub from '~/boards/eventhub';
 import BoardCard from '~/boards/components/board_card.vue';
+import BoardNewIssue from '~/boards/components/board_new_issue.vue';
 
 export default {
   components: {
     BoardCard,
+    BoardNewIssue,
   },
   props: {
     list: {
@@ -14,16 +17,50 @@ export default {
       type: Array,
       required: true,
     },
+    groupId: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    isUnassignedIssuesLane: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      showIssueForm: false,
+    };
+  },
+  created() {
+    eventHub.$on(`toggle-issue-form-${this.list.id}`, this.toggleForm);
+  },
+  beforeDestroy() {
+    eventHub.$off(`toggle-issue-form-${this.list.id}`, this.toggleForm);
+  },
+  methods: {
+    toggleForm() {
+      this.showIssueForm = !this.showIssueForm;
+      if (this.showIssueForm && this.isUnassignedIssuesLane) {
+        this.$el.scrollIntoView(false);
+      }
+    },
   },
 };
 </script>
 
 <template>
   <div
-    class="board gl-px-3 gl-vertical-align-top gl-white-space-normal gl-display-flex gl-flex-shrink-0 is-expandable"
+    class="board gl-px-3 gl-vertical-align-top gl-white-space-normal gl-display-flex gl-flex-shrink-0"
     :class="{ 'is-collapsed': !list.isExpanded }"
   >
     <div class="board-inner gl-p-2 gl-rounded-base gl-relative gl-w-full">
+      <board-new-issue
+        v-if="list.type !== 'closed' && showIssueForm && isUnassignedIssuesLane"
+        :group-id="groupId"
+        :list="list"
+      />
       <ul v-if="list.isExpanded" class="gl-p-0 gl-m-0">
         <board-card
           v-for="(issue, index) in issues"
