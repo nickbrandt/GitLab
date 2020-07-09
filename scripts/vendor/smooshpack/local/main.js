a5fa999386b138f28f3dc144ea9dfce567393546
Paul Slaughter <pslaughter@gitlab.com>
2020-07-09 03:10:42 -0500

    wip

--- scripts/vendor/smooshpack/local/main.js
+++ scripts/vendor/smooshpack/local/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   // Register a service worker hosted at the root of the
   // site using the default scope.
-  navigator.serviceWorker.register('/local/sw.js').then(
+  navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(
     function(registration) {
       console.log('Service worker registration succeeded:', registration);
     },
@@ -9,6 +9,12 @@ if ('serviceWorker' in navigator) {
       console.log('Service worker registration failed:', error);
     },
   );
+
+  navigator.serviceWorker.ready.then(start);
 } else {
   console.log('Service workers are not supported.');
+}
+
+function start() {
+  window.location = 'main.html';
 }
--- scripts/vendor/smooshpack/local/sw.js
+++ scripts/vendor/smooshpack/local/sw.js
@@ -1,3 +1,38 @@
+const FALLBACK =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="180" stroke-linejoin="round">' +
+  '  <path stroke="#DDD" stroke-width="25" d="M99,18 15,162H183z"/>' +
+  '  <path stroke-width="17" fill="#FFF" d="M99,18 15,162H183z" stroke="#eee"/>' +
+  '  <path d="M91,70a9,9 0 0,1 18,0l-5,50a4,4 0 0,1-8,0z" fill="#aaa"/>' +
+  '  <circle cy="138" r="9" cx="100" fill="#aaa"/>' +
+  '</svg>';
+
 self.addEventListener('install', event => {
-  console.log('temp');
+  console.log('[sw] Installing service worker...');
+
+  event.waitUntil(install());
 });
+
+self.addEventListener('activate', event => {
+  // Start intercepting immediately...
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  console.log('[sw] handling fetch for ', event.request);
+
+  if (/\.png$/.test(event.request.url)) {
+    console.log('[sw] handling image!!');
+    const response = new Response(FALLBACK, {
+      headers: {
+        'Content-Type': 'image/svg+xml',
+      },
+    });
+    event.respondWith(response);
+  }
+});
+
+function install() {
+  return Promise.resolve().then(() => {
+    return self.skipWaiting();
+  });
+}
