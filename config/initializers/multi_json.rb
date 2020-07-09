37f23ec216ed7aa19b77ee073cf292ed834b6ab9
Robert May <rmay@gitlab.com>
2020-07-09 11:57:46 +0100

    Ensure frozen_string_literal is set

--- config/initializers/multi_json.rb
+++ config/initializers/multi_json.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 # Explicitly set the JSON adapter used by MultiJson
-# Currently we want this to default to the existing
-# json gem
+# Currently we want this to default to the existing json gem
 MultiJson.use(:json_gem)
--- config/initializers/oj.rb
+++ config/initializers/oj.rb
@@ -1,3 +1,4 @@
-# Ensure Oj runs in json-gem compatibility mode by default
+# frozen_string_literal: true
 
+# Ensure Oj runs in json-gem compatibility mode by default
 Oj.default_options = { mode: :rails }
