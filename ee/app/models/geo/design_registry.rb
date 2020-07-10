5799e2d9dd0491b6aee5b32fef64ebc04c2dfb0c
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2020-07-10 12:12:02 -0300

    Add method to find registries differences for designs
    This method is used to find untracked designs that
    we need to sync and unused registries that we can
    remove.

--- ee/app/models/geo/design_registry.rb
+++ ee/app/models/geo/design_registry.rb
@@ -39,6 +39,16 @@ class Geo::DesignRegistry < Geo::BaseRegistry
     end
   end
 
+  def self.find_registry_differences(range)
+    source_ids = Gitlab::Geo.current_node.designs.id_in(range).pluck_primary_key
+    tracked_ids = self.pluck_model_ids_in_range(range)
+
+    untracked_ids = source_ids - tracked_ids
+    unused_tracked_ids = tracked_ids - source_ids
+
+    [untracked_ids, unused_tracked_ids]
+  end
+
   # Search for a list of projects associated with registries,
   # based on the query given in `query`.
   #
