6f31b77a038e4b3383bf6f1700ca802a7fcc7f34
Tan Le <tle@gitlab.com>
2020-07-13 17:31:40 +1000

    Drop feature flag on compliance MR settings scope
    Remove code and test related to scoping MR approval settings to
    compliance-labeled projects feature. This is ready for GA.

--- ee/app/helpers/admin/merge_request_approval_settings_helper.rb
+++ ee/app/helpers/admin/merge_request_approval_settings_helper.rb
@@ -3,8 +3,7 @@
 module Admin
   module MergeRequestApprovalSettingsHelper
     def show_compliance_merge_request_approval_settings?
-      Feature.enabled?(:admin_compliance_merge_request_approval_settings) &&
-        License.feature_available?(:admin_merge_request_approvers_rules)
+      License.feature_available?(:admin_merge_request_approvers_rules)
     end
   end
 end
--- ee/app/models/ee/project.rb
+++ ee/app/models/ee/project.rb
@@ -635,7 +635,6 @@ module EE
 
     def disable_overriding_approvers_per_merge_request
       return super unless License.feature_available?(:admin_merge_request_approvers_rules)
-      return (::Gitlab::CurrentSettings.disable_overriding_approvers_per_merge_request? || super) unless project_compliance_mr_approval_settings?
       return super unless has_regulated_settings?
 
       ::Gitlab::CurrentSettings.disable_overriding_approvers_per_merge_request?
@@ -644,8 +643,6 @@ module EE
 
     def merge_requests_author_approval
       return super unless License.feature_available?(:admin_merge_request_approvers_rules)
-      return false if !project_compliance_mr_approval_settings? && ::Gitlab::CurrentSettings.prevent_merge_requests_author_approval?
-      return super if !project_compliance_mr_approval_settings? && !::Gitlab::CurrentSettings.prevent_merge_requests_author_approval?
       return super unless has_regulated_settings?
 
       !::Gitlab::CurrentSettings.prevent_merge_requests_author_approval?
@@ -654,16 +651,11 @@ module EE
 
     def merge_requests_disable_committers_approval
       return super unless License.feature_available?(:admin_merge_request_approvers_rules)
-      return (::Gitlab::CurrentSettings.prevent_merge_requests_committers_approval? || super) unless project_compliance_mr_approval_settings?
       return super unless has_regulated_settings?
 
       ::Gitlab::CurrentSettings.prevent_merge_requests_committers_approval?
     end
     alias_method :merge_requests_disable_committers_approval?, :merge_requests_disable_committers_approval
-
-    def project_compliance_mr_approval_settings?
-      ::Feature.enabled?(:project_compliance_merge_request_approval_settings, self)
-    end
 
     def license_compliance(pipeline = latest_pipeline_with_reports(::Ci::JobArtifact.license_scanning_reports))
       SCA::LicenseCompliance.new(self, pipeline)
