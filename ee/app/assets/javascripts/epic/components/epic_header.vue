07290d358d0ebead73f4b4c21609762d4483fa20
Axel García <agarcia@gitlab.com>
2020-07-13 21:39:17 -0400

    Add test for the new epic button on epic header
    Also some refactoring on the test and selectors

--- ee/app/assets/javascripts/epic/components/epic_header.vue
+++ ee/app/assets/javascripts/epic/components/epic_header.vue
@@ -45,12 +45,7 @@ export default {
       return this.isEpicOpen ? __('Open') : __('Closed');
     },
     actionButtonClass() {
-      // False positive css classes
-      // https://gitlab.com/gitlab-org/frontend/eslint-plugin-i18n/issues/24
-      // eslint-disable-next-line @gitlab/require-i18n-strings
-      return `js-btn-epic-action qa-close-reopen-epic-button ${
-        this.isEpicOpen ? 'btn-close' : 'btn-open'
-      }`;
+      return `qa-close-reopen-epic-button ${this.isEpicOpen ? 'btn-close' : 'btn-open'}`;
     },
     actionButtonText() {
       return this.isEpicOpen ? __('Close epic') : __('Reopen epic');
@@ -88,12 +83,17 @@ export default {
       <div
         :class="{ 'status-box-open': isEpicOpen, 'status-box-issue-closed': !isEpicOpen }"
         class="issuable-status-box status-box"
+        data-testid="status-box"
       >
         <gl-icon :name="statusIcon" class="d-block d-sm-none" />
         <span class="d-none d-sm-block">{{ statusText }}</span>
       </div>
-      <div class="issuable-meta">
-        <div v-if="confidential" class="issuable-warning-icon inline">
+      <div class="issuable-meta" data-testid="author-details">
+        <div
+          v-if="confidential"
+          class="issuable-warning-icon inline"
+          data-testid="confidential-icon"
+        >
           <gl-icon name="eye-slash" class="icon" />
         </div>
         {{ __('Opened') }}
@@ -117,14 +117,16 @@ export default {
     </div>
     <gl-button
       :aria-label="__('Toggle sidebar')"
-      class="float-right gl-display-block d-sm-none gl-align-self-center gutter-toggle issuable-gutter-toggle js-sidebar-toggle"
       type="button"
+      class="float-right gl-display-block d-sm-none gl-align-self-center gutter-toggle issuable-gutter-toggle"
+      data-testid="sidebar-toggle"
       @click="toggleSidebar({ sidebarCollapsed })"
     >
       <i class="fa fa-angle-double-left"></i>
     </gl-button>
     <div
-      class="detail-page-header-actions gl-display-flex gl-flex-wrap gl-align-items-center gl-w-full gl-w-sm-auto js-issuable-actions"
+      class="detail-page-header-actions gl-display-flex gl-flex-wrap gl-align-items-center gl-w-full gl-sm-w-auto!"
+      data-testid="action-buttons"
     >
       <gl-button
         v-if="canUpdate"
@@ -132,7 +134,8 @@ export default {
         :class="actionButtonClass"
         category="secondary"
         variant="warning"
-        class="gl-mt-3 gl-mt-sm-0 gl-w-full gl-w-sm-auto"
+        class="gl-mt-3 gl-sm-mt-0! gl-w-full gl-sm-w-auto!"
+        data-testid="toggle-status-button"
         @click="toggleEpicStatus(isEpicOpen)"
       >
         {{ actionButtonText }}
@@ -140,10 +143,10 @@ export default {
       <gl-button
         v-if="userCanCreate"
         :href="newEpicWebUrl"
-        data-testid="new-epic-button"
-        class="gl-mt-3 gl-mt-sm-0 gl-ml-sm-3 gl-w-full gl-w-sm-auto"
         category="secondary"
         variant="success"
+        class="gl-mt-3 gl-sm-mt-0! gl-sm-ml-3 gl-w-full gl-sm-w-auto!"
+        data-testid="new-epic-button"
       >
         {{ __('New epic') }}
       </gl-button>
