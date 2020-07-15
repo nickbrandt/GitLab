48f58f4bf75bcc152b7579079ae3d10ce2b0dd67
Christie Lenneville <clenneville@gitlab.com>
2020-07-15 20:17:03 +0000

    Updated deprecated buttons

--- ee/app/assets/javascripts/boards/components/board_settings_sidebar.vue
+++ ee/app/assets/javascripts/boards/components/board_settings_sidebar.vue
@@ -2,7 +2,7 @@
 import {
   GlDrawer,
   GlLabel,
-  GlDeprecatedButton,
+  GlButton,
   GlFormInput,
   GlAvatarLink,
   GlAvatarLabeled,
@@ -35,7 +35,7 @@ export default {
   components: {
     GlDrawer,
     GlLabel,
-    GlDeprecatedButton,
+    GlButton,
     GlFormInput,
     GlAvatarLink,
     GlAvatarLabeled,
@@ -210,11 +210,11 @@ export default {
       <div class="d-flex justify-content-between flex-column">
         <div class="d-flex justify-content-between align-items-center mb-2">
           <label class="m-0">{{ $options.wipLimitText }}</label>
-          <gl-deprecated-button
-            class="js-edit-button h-100 border-0 gl-line-height-14-deprecated-no-really-do-not-use-me text-dark"
+          <gl-button
+            class="js-edit-button h-100 border-0 text-dark"
             variant="link"
             @click="showInput"
-            >{{ $options.editLinkText }}</gl-deprecated-button
+            >{{ $options.editLinkText }}</gl-button
           >
         </div>
         <gl-form-input
@@ -235,11 +235,11 @@ export default {
           <p class="js-wip-limit bold m-0 text-secondary">{{ activeListWipLimit }}</p>
           <template v-if="wipLimitIsSet">
             <span class="m-1">-</span>
-            <gl-deprecated-button
-              class="js-remove-limit h-100 border-0 gl-line-height-14-deprecated-no-really-do-not-use-me text-secondary"
+            <gl-button
+              class="js-remove-limit h-100 border-0 text-secondary"
               variant="link"
               @click="clearWipLimit"
-              >{{ $options.removeLimitText }}</gl-deprecated-button
+              >{{ $options.removeLimitText }}</gl-button
             >
           </template>
         </div>
