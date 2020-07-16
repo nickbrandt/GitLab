910a689ab6ed12956e1494fcaf16109f2ca623f8
NataliaTepluhina <tarya.se@gmail.com>
2020-07-16 17:46:40 +0300

    Fixed add design qa spec

--- qa/qa/specs/features/browser_ui/3_create/design_management/add_design_add_annotation.rb
+++ qa/qa/specs/features/browser_ui/3_create/design_management/add_design_add_annotation.rb
@@ -16,14 +16,11 @@ module QA
         issue.visit!
 
         Page::Project::Issue::Show.perform do |show|
-          show.click_designs_tab
           show.add_design(design)
           show.click_design(design_filename)
           show.add_annotation(annotation)
 
           expect(show).to have_annotation(annotation)
-
-          show.click_discussion_tab
         end
       end
     end
