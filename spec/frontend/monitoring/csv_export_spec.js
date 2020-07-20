657c15f6d50670ad77c30931e1b559c53b719371
Miguel Rincon <mrincon@gitlab.com>
2020-07-21 07:31:33 +0800

    Fix broken specs and update changelog
    Fix the specs that failed due to generate data being updated.
    
    Added a new change log to match the current merge request.

--- spec/frontend/monitoring/csv_export_spec.js
+++ spec/frontend/monitoring/csv_export_spec.js
@@ -20,6 +20,17 @@ describe('monitoring export_csv', () => {
       expect(graphDataToCsv(data)).toEqual('');
     });
 
+    it('should return a csv with 1 metric', () => {
+      const data = timeSeriesGraphData({}, { metricCount: 1 });
+
+      expectCsvToMatchLines(graphDataToCsv(data), [
+        `timestamp,"Y Axis > Metric 1"`,
+        '2015-07-01T20:10:50.000Z,1',
+        '2015-07-01T20:12:50.000Z,2',
+        '2015-07-01T20:14:50.000Z,3',
+      ]);
+    });
+
     it('should return a csv with multiple metrics and one with no data', () => {
       const data = timeSeriesGraphData({}, { metricCount: 2 });
 
@@ -28,9 +39,9 @@ describe('monitoring export_csv', () => {
 
       expectCsvToMatchLines(graphDataToCsv(data), [
         `timestamp,"Y Axis > Metric 2"`,
-        '2015-07-01T20:10:51.781Z,1',
-        '2015-07-01T20:11:06.781Z,2',
-        '2015-07-01T20:11:21.781Z,3',
+        '2015-07-01T20:10:50.000Z,1',
+        '2015-07-01T20:12:50.000Z,2',
+        '2015-07-01T20:14:50.000Z,3',
       ]);
     });
 
@@ -45,21 +56,10 @@ describe('monitoring export_csv', () => {
 
       expectCsvToMatchLines(graphDataToCsv(data), [
         `timestamp,"Y Axis > Metric 1","Y Axis > Metric 2","Y Axis > Metric 3"`,
-        '2015-07-01T20:10:51.781Z,1,1,',
-        '2015-07-01T20:11:06.781Z,2,2,',
-        '2015-07-01T20:11:21.781Z,3,3,',
+        '2015-07-01T20:10:50.000Z,1,1,',
+        '2015-07-01T20:12:50.000Z,2,2,',
+        '2015-07-01T20:14:50.000Z,3,3,',
         '2016-01-01T00:00:00.000Z,,,9',
-      ]);
-    });
-
-    it('should return a csv with 1 metric', () => {
-      const data = timeSeriesGraphData({}, { metricCount: 1 });
-
-      expectCsvToMatchLines(graphDataToCsv(data), [
-        `timestamp,"Y Axis > Metric 1"`,
-        '2015-07-01T20:10:51.781Z,1',
-        '2015-07-01T20:11:06.781Z,2',
-        '2015-07-01T20:11:21.781Z,3',
       ]);
     });
 
@@ -70,9 +70,9 @@ describe('monitoring export_csv', () => {
 
       expectCsvToMatchLines(graphDataToCsv(data), [
         `timestamp,"Y Axis > My ""quoted"" metric"`,
-        '2015-07-01T20:10:51.781Z,1',
-        '2015-07-01T20:11:06.781Z,2',
-        '2015-07-01T20:11:21.781Z,3',
+        '2015-07-01T20:10:50.000Z,1',
+        '2015-07-01T20:12:50.000Z,2',
+        '2015-07-01T20:14:50.000Z,3',
       ]);
     });
 
@@ -81,9 +81,9 @@ describe('monitoring export_csv', () => {
 
       expectCsvToMatchLines(graphDataToCsv(data), [
         `timestamp,"Y Axis > Metric 1","Y Axis > Metric 2","Y Axis > Metric 3"`,
-        '2015-07-01T20:10:51.781Z,1,1,1',
-        '2015-07-01T20:11:06.781Z,2,2,2',
-        '2015-07-01T20:11:21.781Z,3,3,3',
+        '2015-07-01T20:10:50.000Z,1,1,1',
+        '2015-07-01T20:12:50.000Z,2,2,2',
+        '2015-07-01T20:14:50.000Z,3,3,3',
       ]);
     });
 
@@ -92,9 +92,9 @@ describe('monitoring export_csv', () => {
 
       expectCsvToMatchLines(graphDataToCsv(data), [
         `timestamp,"Y Axis > Metric 1","Y Axis > Metric 1"`,
-        '2015-07-01T20:10:51.781Z,1,4',
-        '2015-07-01T20:11:06.781Z,2,5',
-        '2015-07-01T20:11:21.781Z,3,6',
+        '2015-07-01T20:10:50.000Z,1,4',
+        '2015-07-01T20:12:50.000Z,2,5',
+        '2015-07-01T20:14:50.000Z,3,6',
       ]);
     });
 
@@ -103,9 +103,9 @@ describe('monitoring export_csv', () => {
 
       expectCsvToMatchLines(graphDataToCsv(data), [
         `timestamp,"Y Axis > __name__: up, job: prometheus, instance: localhost:9090","Y Axis > __name__: up, job: node, instance: localhost:9091"`,
-        '2015-07-01T20:10:51.781Z,1,4',
-        '2015-07-01T20:11:06.781Z,2,5',
-        '2015-07-01T20:11:21.781Z,3,6',
+        '2015-07-01T20:10:50.000Z,1,4',
+        '2015-07-01T20:12:50.000Z,2,5',
+        '2015-07-01T20:14:50.000Z,3,6',
       ]);
     });
 
@@ -117,9 +117,9 @@ describe('monitoring export_csv', () => {
 
       expectCsvToMatchLines(graphDataToCsv(data), [
         `timestamp,"Y Axis > __name__: up, job: prometheus, instance: localhost:9090","Y Axis > __name__: up, job: node, instance: localhost:9091","Y Axis > __name__: up, job: prometheus, instance: localhost:9090","Y Axis > __name__: up, job: node, instance: localhost:9091","Y Axis > __name__: up, job: prometheus, instance: localhost:9090","Y Axis > __name__: up, job: node, instance: localhost:9091"`,
-        '2015-07-01T20:10:51.781Z,1,4,1,4,1,4',
-        '2015-07-01T20:11:06.781Z,2,5,2,5,2,5',
-        '2015-07-01T20:11:21.781Z,3,6,3,6,3,6',
+        '2015-07-01T20:10:50.000Z,1,4,1,4,1,4',
+        '2015-07-01T20:12:50.000Z,2,5,2,5,2,5',
+        '2015-07-01T20:14:50.000Z,3,6,3,6,3,6',
       ]);
     });
   });
