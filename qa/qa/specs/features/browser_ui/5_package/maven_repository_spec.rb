e29fa9836ad743955dd6618cd96a974e81923aac
Giorgenes Gelatti <ggelatti@gitlab.com>
2020-07-20 20:31:14 +0000

    Move package qa feature specs to core

--- qa/qa/specs/features/browser_ui/5_package/maven_repository_spec.rb
+++ qa/qa/specs/features/browser_ui/5_package/maven_repository_spec.rb
@@ -80,19 +80,20 @@ module QA
 
         project.visit!
         Page::Project::Menu.perform(&:click_packages_link)
-        EE::Page::Project::Packages::Index.perform do |index|
+
+        Page::Project::Packages::Index.perform do |index|
           expect(index).to have_package(package_name)
 
           index.click_package(package_name)
         end
 
-        EE::Page::Project::Packages::Show.perform do |show|
+        Page::Project::Packages::Show.perform do |show|
           expect(show).to have_package_info(package_name, "1.0")
 
           show.click_delete
         end
 
-        EE::Page::Project::Packages::Index.perform do |index|
+        Page::Project::Packages::Index.perform do |index|
           expect(index).to have_content("Package was removed")
           expect(index).to have_no_package(package_name)
         end
