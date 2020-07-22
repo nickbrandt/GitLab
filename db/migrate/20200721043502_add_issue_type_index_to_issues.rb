ec5e8ba349a9c6e91654248dde6299f60e556546
Sean Arnold <sarnold@gitlab.com>
2020-07-22 18:48:58 +1200

    Remove comment from migration

--- db/migrate/20200721043502_add_issue_type_index_to_issues.rb
+++ db/migrate/20200721043502_add_issue_type_index_to_issues.rb
@@ -3,7 +3,6 @@
 class AddIssueTypeIndexToIssues < ActiveRecord::Migration[6.0]
   include Gitlab::Database::MigrationHelpers
 
-  # Set this constant to true if this migration requires downtime.
   DOWNTIME = false
   disable_ddl_transaction!
 
