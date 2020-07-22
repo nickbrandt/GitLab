e64e8a84ad2ca3c084f45fcc9117170e9b687cd3
Desiree Chevalier <dchevalier@gitlab.com>
2020-07-22 13:37:28 -0400

    Adds qa selectors for issuable list vue
    Adds and updates qa selectors for new vue issuable list

--- app/assets/javascripts/issuables_list/components/issuable.vue
+++ app/assets/javascripts/issuables_list/components/issuable.vue
@@ -234,6 +234,8 @@ export default {
     :data-id="issuable.id"
     :data-labels="labelIdsString"
     :data-url="issuable.web_url"
+    data-qa-selector="issue_container"
+    :data-qa-issue-title="issuable.title"
   >
     <div class="gl-display-flex">
       <!-- Bulk edit checkbox -->
@@ -262,7 +264,12 @@ export default {
               :title="$options.confidentialTooltipText"
               :aria-label="$options.confidentialTooltipText"
             />
-            <gl-link :href="issuable.web_url" :target="linkTarget" data-testid="issuable-title">
+            <gl-link
+              :href="issuable.web_url"
+              :target="linkTarget"
+              data-testid="issuable-title"
+              data-qa-selector="issue_link"
+            >
               {{ issuable.title }}
               <gl-icon
                 v-if="isJiraIssue"
@@ -354,6 +361,7 @@ export default {
             :title="__('Weight')"
             class="gl-display-none d-sm-inline-block"
             data-testid="weight"
+            data-qa-selector="issuable_weight_content"
           >
             <gl-icon name="weight" class="align-text-bottom" />
             {{ issuable.weight }}
