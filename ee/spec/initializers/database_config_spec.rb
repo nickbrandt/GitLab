ac9b5d2a24bbb8f949051a5460e3b75fc3c8b7e2
Jacob Vosmaer <jacob@gitlab.com>
2020-07-22 19:40:49 +0000

    Simplify DB connection pool size configuration (optional)

--- ee/spec/initializers/database_config_spec.rb
+++ ee/spec/initializers/database_config_spec.rb
@@ -39,6 +39,28 @@ RSpec.describe 'Database config initializer for GitLab EE' do
     end
   end
 
+  context "with new pool size logic" do
+    let(:max_threads) { 8 }
+
+    before do
+      stub_env('DB_USE_NEW_POOL_SIZE_LOGIC', '1')
+      allow(Gitlab::Runtime).to receive(:max_threads).and_return(max_threads)
+      allow(ActiveRecord::Base).to receive(:establish_connection)
+
+      expect(Geo::TrackingBase).to receive(:establish_connection)
+    end
+
+    context "and the runtime is Sidekiq" do
+      before do
+        allow(Gitlab::Runtime).to receive(:sidekiq?).and_return(true)
+      end
+
+      it "sets Geo DB connection pool size to the max number of worker threads" do
+        expect { subject }.to change { Rails.configuration.geo_database['pool'] }.from(1).to(18)
+      end
+    end
+  end
+
   def stub_geo_database_config(pool_size:)
     config = {
       'adapter' => 'postgresql',
