8a8784f17adef35672f769e8ad11ea1778e5a863
Dylan Griffith <dyl.griffith@gmail.com>
2020-07-23 14:01:02 +1000

    Fix Elasticsearch sorting incorrectly from DB
    Before this our search results were getting mixed up within a page. The
    [`Records#records`](
    https://github.com/elastic/elasticsearch-rails/blob/v6.1.0/elasticsearch-model/lib/elasticsearch/model/adapters/active_record.rb#L21
    )
    method is meant to sort again the query results based on the original
    Elasticsearch ordering after loading from the DB, but this was not
    working correctly in our code due to us monkey patching the
    elasticsearch-model Gem and changing the value of
    [`_id`](
    https://gitlab.com/gitlab-org/gitlab/blob/8214da073b9692ffb75095390bc2fa5d862b5c05/ee/lib/gem_extensions/elasticsearch/model/response/records.rb#L9
    )
    for Elasticsearch records.
    
    As such we needed to dig ourselves further into the monkey patch hole.
    This MR replaces a single line inside `Records#records` method, which,
    unfortunately, required us to replace the entire lengthy method.
    
    https://gitlab.com/gitlab-org/gitlab/-/issues/229982

---
+++ ee/spec/lib/gem_extensions/elasticsearch/model/adapter/active_record/records_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Elasticsearch::Model::Adapter::ActiveRecord::Records, :elastic do
+  describe '#records' do
+    let(:user) { create(:user) }
+    let(:search_options) { { options: { current_user: user, project_ids: :any } } }
+
+    before do
+      stub_ee_application_setting(elasticsearch_indexing: true)
+
+      @middle_relevant = create(
+        :issue,
+        title: 'Sorting could improve', # Some keywords in title
+        description: 'I think you could make it better'
+      )
+      @least_relevant = create(
+        :issue,
+        title: 'I love GitLab', # No keywords in title
+        description: 'There is so much to love! For example, you could not possibly make sorting any better'
+      )
+
+      @most_relevant = create(
+        :issue,
+        title: 'Make sorting better', # All keywords in title
+        description: 'This issue is here to make the sorting better'
+      )
+
+      ensure_elasticsearch_index!
+    end
+
+    it 'returns results in the same sorted order as they come back from Elasticsearch' do
+      expect(Issue.elastic_search('make sorting better', search_options).records.to_a).to eq([
+        @most_relevant,
+        @middle_relevant,
+        @least_relevant
+      ])
+    end
+  end
+end
