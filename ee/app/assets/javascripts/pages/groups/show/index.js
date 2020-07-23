dd01dce7d4f4b6614d2313272e711f7c2bc3cd64
Alexander Turinske <aturinske@gitlab.com>
2020-07-23 14:06:02 -0600

    Delete deprecated group security dashboard code
    - switch out final reference to deprecated code
    - remove unused code
    - delete tests

--- ee/app/assets/javascripts/pages/groups/show/index.js
+++ ee/app/assets/javascripts/pages/groups/show/index.js
@@ -1,4 +1,3 @@
-import initGroupSecurityDashboard from 'ee/security_dashboard/group_init';
 import leaveByUrl from '~/namespaces/leave_by_url';
 import initGroupDetails from '~/pages/groups/shared/group_details';
 import initGroupAnalytics from 'ee/analytics/group_analytics/group_analytics_bundle';
@@ -7,12 +6,7 @@ import initVueAlerts from '~/vue_alerts';
 document.addEventListener('DOMContentLoaded', () => {
   leaveByUrl('group');
 
-  if (document.querySelector('#js-group-security-dashboard')) {
-    initGroupSecurityDashboard();
-  } else {
-    initGroupDetails();
-  }
-
+  initGroupDetails();
   initGroupAnalytics();
   initVueAlerts();
 });
