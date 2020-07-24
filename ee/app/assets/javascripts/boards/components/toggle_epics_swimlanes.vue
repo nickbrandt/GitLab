9cea7c68086922748e9433e2e62b24e3cfecc0a4
jeldergl <jelder@gitlab.com>
2020-07-24 09:37:21 -0500

    Updating hex and remapping variables
    Make GitLab SCSS match GitLab UI SCSS and remap variables to adjust
    UI for the updated hex values.

--- ee/app/assets/javascripts/boards/components/toggle_epics_swimlanes.vue
+++ ee/app/assets/javascripts/boards/components/toggle_epics_swimlanes.vue
@@ -45,7 +45,7 @@ export default {
     <gl-dropdown
       right
       :text="dropdownLabel"
-      toggle-class="gl-ml-2 gl-border-none gl-inset-border-1-gray-400! border-radius-default"
+      toggle-class="gl-ml-2 gl-border-none gl-inset-border-1-gray-200! border-radius-default"
     >
       <gl-dropdown-item
         :is-check-item="true"
