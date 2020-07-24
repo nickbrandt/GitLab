5ca49bfb8aede6444cac99035a0c532df522445e
Eulyeon Ko <eko@gitlab.com>
2020-07-24 07:02:04 +0000

    Make issuable_header_warnings shared component
    Also added a js class in haml as a render target
    
    Apply suggested patch
    
    Update relevant specs
    
    Add js-lock-issue-data and update mock noteable data
    Test issuable_header_warnings for both mr and issue
    
    Move to vue_shared
    
    Update issuable header warning component
    
    Also update the spec
    
    Apply the suggestions from the second review round
    
    Re-organize the spec
    Remove not needed mock data

--- ee/app/assets/javascripts/pages/projects/merge_requests/show/index.js
+++ ee/app/assets/javascripts/pages/projects/merge_requests/show/index.js
@@ -1,6 +1,8 @@
 import initSidebarBundle from 'ee/sidebar/sidebar_bundle';
 import { initReviewBar } from '~/batch_comments';
 import initMrNotes from '~/mr_notes';
+import store from '~/mr_notes/stores';
+import initIssuableHeaderWarning from '~/vue_shared/components/issuable/init_issuable_header_warning';
 import initShow from '~/pages/projects/merge_requests/init_merge_request_show';
 import trackShowInviteMemberLink from 'ee/projects/track_invite_members';
 
@@ -11,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   initMrNotes();
   initReviewBar();
+  initIssuableHeaderWarning(store);
 
   const assigneeDropdown = document.querySelector('.js-sidebar-assignee-dropdown');
 
