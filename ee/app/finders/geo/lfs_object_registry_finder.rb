8cf121e8eb718f91d7c12906d7434be033b7f2a5
Mike Kozono <mkozono@gmail.com>
2020-07-24 18:56:49 -0700

    Push scopes out of find_registry_differences
    And defer refactoring GeoNode#lfs_objects for a follow up.

--- ee/app/finders/geo/lfs_object_registry_finder.rb
+++ ee/app/finders/geo/lfs_object_registry_finder.rb
@@ -3,7 +3,9 @@
 module Geo
   class LfsObjectRegistryFinder < FileRegistryFinder
     def replicables
-      current_node(fdw: false).lfs_objects
+      lfs_objects = current_node(fdw: false).lfs_objects
+
+      local_storage_only? ? lfs_objects.with_files_stored_locally : lfs_objects
     end
 
     def syncable
