mehulagg/gitlab_test

Add GraphQL API to temporarily increase storage
This adds the mutation to temporarily increase the storage, by
setting temporary_storage_increase_ends_on which later will be
used to change the limit during the period of the time.

ee/app/graphql/mutations/namespaces/base.rb
@@ -1,0 +1,22 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Namespaces
+    class Base < ::Mutations::BaseMutation
+      argument :iid, GraphQL::ID_TYPE,
+               required: true,
+               description: "The global id of the namespace to mutate"
+
+      field :namespace,
+            Types::NamespaceType,
+            null: true,
+            description: 'The namespace after mutation'
+
+      private
+
+      def find_object(iid:)
+        GitlabSchema.object_from_id(iid)
+      end
+    end
+  end
+end

ee/app/graphql/mutations/namespaces/increase_storage_temporarily.rb
@@ -1,0 +1,19 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Namespaces
+    class IncreaseStorageTemporarily < Base
+      graphql_name "NamespaceIncreaseStorageTemporarily"
+
+      authorize :admin_namespace
+
+      def resolve(args)
+        namespace = authorized_find!(iid: args[:iid])
+
+        namespace.enable_temporary_storage_increase!
+
+        { namespace: namespace, errors: namespace.errors.full_messages }
+      end
+    end
+  end
+end

ee/spec/graphql/mutations/namespaces/increase_storage_temporarily_spec.rb
@@ -1,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Mutations::Namespaces::IncreaseStorageTemporarily do
+  let_it_be(:user) { create(:user) }
+  let_it_be(:namespace) { user.namespace }
+
+  subject(:mutation) { described_class.new(object: nil, context: { current_user: user }, field: nil) }
+
+  describe '#resolve' do
+    subject do
+      mutation.resolve(iid: namespace.to_global_id.to_s)
+    end
+
+    before do
+      allow_next_instance_of(EE::Namespace::RootStorageSize, namespace) do |root_storage|
+        allow(root_storage).to receive(:usage_ratio).and_return(0.5)
+      end
+    end
+
+    context 'when user is not the admin of the namespace' do
+      let(:user) { create(:user) }
+
+      it 'raises a not accessible error' do
+        expect { subject }.to raise_error(Gitlab::Graphql::Errors::ResourceNotAvailable)
+      end
+    end
+
+    context 'when the user can admin the namespace' do
+      it 'sets temporary_storage_increase_ends_on' do
+        expect(namespace.temporary_storage_increase_ends_on).to be_nil
+
+        subject
+
+        expect(subject[:namespace]).to be_present
+        expect(subject[:errors]).to be_empty
+        expect(namespace.reload.temporary_storage_increase_ends_on).to be_present
+      end
+    end
+  end
+end

nickbrandt/GitLab

ee/app/graphql/mutations/namespaces/base.rb
@@ -1,0 +1,22 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Namespaces
+    class Base < ::Mutations::BaseMutation
+      argument :id, GraphQL::ID_TYPE,
+               required: true,
+               description: "The global id of the namespace to mutate"
+
+      field :namespace,
+            Types::NamespaceType,
+            null: true,
+            description: 'The namespace after mutation'
+
+      private
+
+      def find_object(id:)
+        GitlabSchema.object_from_id(id)
+      end
+    end
+  end
+end

ee/app/graphql/mutations/namespaces/increase_storage_temporarily.rb
@@ -1,0 +1,19 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Namespaces
+    class IncreaseStorageTemporarily < Base
+      graphql_name "NamespaceIncreaseStorageTemporarily"
+
+      authorize :admin_namespace
+
+      def resolve(args)
+        namespace = authorized_find!(id: args[:id])
+
+        namespace.enable_temporary_storage_increase!
+
+        { namespace: namespace, errors: namespace.errors.full_messages }
+      end
+    end
+  end
+end

ee/spec/graphql/mutations/namespaces/increase_storage_temporarily_spec.rb
@@ -1,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Mutations::Namespaces::IncreaseStorageTemporarily do
+  let_it_be(:user) { create(:user) }
+  let_it_be(:namespace) { user.namespace }
+
+  subject(:mutation) { described_class.new(object: nil, context: { current_user: user }, field: nil) }
+
+  describe '#resolve' do
+    subject do
+      mutation.resolve(id: namespace.to_global_id.to_s)
+    end
+
+    before do
+      allow_next_instance_of(EE::Namespace::RootStorageSize, namespace) do |root_storage|
+        allow(root_storage).to receive(:usage_ratio).and_return(0.5)
+      end
+    end
+
+    context 'when user is not the admin of the namespace' do
+      let(:user) { create(:user) }
+
+      it 'raises a not accessible error' do
+        expect { subject }.to raise_error(Gitlab::Graphql::Errors::ResourceNotAvailable)
+      end
+    end
+
+    context 'when the user can admin the namespace' do
+      it 'sets temporary_storage_increase_ends_on' do
+        expect(namespace.temporary_storage_increase_ends_on).to be_nil
+
+        subject
+
+        expect(subject[:namespace]).to be_present
+        expect(subject[:errors]).to be_empty
+        expect(namespace.reload.temporary_storage_increase_ends_on).to be_present
+      end
+    end
+  end
+end