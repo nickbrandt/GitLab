b4a740883c172cca77942f8847edc1931d53e05b
Jarka Košanová <jarka@gitlab.com>
2020-07-27 07:35:45 +0200

    Don't expose epic for users without permissions
    - epic is not required field anymore

--- ee/app/serializers/ee/issue_sidebar_extras_entity.rb
+++ ee/app/serializers/ee/issue_sidebar_extras_entity.rb
@@ -5,13 +5,17 @@ module EE
     extend ActiveSupport::Concern
 
     prepended do
-      expose :epic do
+      expose :epic, if: -> (issuable, _) { cen_read_epic?(issuable) } do
         expose :epic, merge: true, using: EpicBaseEntity
         expose :epic_issue_id do |issuable|
           issuable.epic_issue&.id
         end
       end
       expose :weight
+
+      def cen_read_epic?(issuable)
+        can?(request.current_user, :read_epic, issuable.epic)
+      end
     end
   end
 end
