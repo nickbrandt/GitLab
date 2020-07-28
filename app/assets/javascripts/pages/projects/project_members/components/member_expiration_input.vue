54281b38b7106e8fe446e844e6a68ff958d5d10c
Doug Stull <dstull@gitlab.com>
2020-07-28 15:18:40 -0400

    Change member expiration date picker
    - match pajamas design.

---
+++ app/assets/javascripts/pages/projects/project_members/components/member_expiration_input.vue
@@ -0,0 +1,38 @@
+<script>
+import { GlDatepicker } from '@gitlab/ui';
+
+export default {
+  components: {
+    GlDatepicker,
+  },
+  props: {
+    elId: {
+      type: String,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isEditingCustom: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <gl-datepicker>
+    <input
+      :id="elId"
+      :disabled="disabled"
+      class="gl-datepicker-input form-control"
+      name="date"
+      type="text"
+      :aria-label="__('Expiration date')"
+      :placeholder="__('Expiration date')"
+    />
+  </gl-datepicker>
+</template>
--- app/assets/javascripts/pages/projects/project_members/index.js
+++ app/assets/javascripts/pages/projects/project_members/index.js
@@ -4,6 +4,7 @@ import memberExpirationDate from '~/member_expiration_date';
 import UsersSelect from '~/users_select';
 import groupsSelect from '~/groups_select';
 import RemoveMemberModal from '~/vue_shared/components/remove_member_modal.vue';
+import MemberExpirationInput from './components/member_expiration_input.vue';
 
 function mountRemoveMemberModal() {
   const el = document.querySelector('.js-remove-member-modal');
@@ -19,11 +20,26 @@ function mountRemoveMemberModal() {
   });
 }
 
+function mountDatePicker() {
+  const memberInputs = document.querySelectorAll('.js-member-expiration-date-input');
+  memberInputs.forEach(el => {
+    return new Vue({
+      el,
+      render(createComponent) {
+        return createComponent(MemberExpirationInput, {
+          props: { ...el.dataset },
+        });
+      },
+    });
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   groupsSelect();
   memberExpirationDate();
   memberExpirationDate('.js-access-expiration-date-groups');
   mountRemoveMemberModal();
+  mountDatePicker();
 
   new Members(); // eslint-disable-line no-new
   new UsersSelect(); // eslint-disable-line no-new
