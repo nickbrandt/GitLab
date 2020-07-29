0eb7287c9173165b4014659fb30f3b5ad988eb88
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2020-07-29 14:12:25 -0300

    Remove unused FDW models

--- ee/app/models/geo/fdw/project_feature.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Geo
-  module Fdw
-    class ProjectFeature < ::Geo::BaseFdw
-      self.table_name = Gitlab::Geo::Fdw.foreign_table_name('project_features')
-    end
-  end
-end
