e2d74fcf63a7d8ad9a3d90d5fe9e16d22af9e7f2
Vitali Tatarintev <vtatarintev@gitlab.com>
2020-07-29 16:49:04 +0200

    Move Ci::PipelineEnums to Enums::Ci::Pipeline
    Move enums related modules to a separate directory

---
+++ ee/app/models/concerns/ee/enums/ci/pipeline.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module EE
+  module Enums
+    module Ci
+      module Pipeline
+        extend ActiveSupport::Concern
+
+        class_methods do
+          extend ::Gitlab::Utils::Override
+
+          override :failure_reasons
+          def failure_reasons
+            super.merge(
+              activity_limit_exceeded: 20,
+              size_limit_exceeded: 21,
+              job_activity_limit_exceeded: 22
+            )
+          end
+        end
+      end
+    end
+  end
+end
