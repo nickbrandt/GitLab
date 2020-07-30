99af0f44d27bc96bd90908807f56172ac18b1914
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2020-07-30 18:36:44 -0300

    Remove FDW related code for container repositories
    Since GitLab 13.2 we don't rely on FDW queries
    to sync these repositories.

--- ee/spec/models/geo/fdw/project_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-RSpec.describe Geo::Fdw::Project, :geo_fdw, type: :model do
-  context 'relationships' do
-    it { is_expected.to have_many(:container_repositories).class_name('Geo::Fdw::ContainerRepository') }
-  end
-end
