e76f826242c5ad3c5f4fe620b9a210099ef789b5
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2020-07-30 18:30:47 -0300

    Remove unused method from container repository registry finder

--- ee/app/finders/geo/container_repository_registry_finder.rb
+++ ee/app/finders/geo/container_repository_registry_finder.rb
@@ -63,33 +63,5 @@ module Geo
         .limit(batch_size)
     end
     # rubocop:enable CodeReuse/ActiveRecord
-
-    # Find limited amount of non replicated container repositories.
-    #
-    # You can pass a list with `except_ids:` so you can exclude items you
-    # already scheduled but haven't finished and aren't persisted to the database yet
-    #
-    # @param [Integer] batch_size used to limit the results returned
-    # @param [Array<Integer>] except_ids ids that will be ignored from the query
-    # rubocop:disable CodeReuse/ActiveRecord
-    def find_unsynced(batch_size:, except_ids: [])
-      current_node_fdw
-        .container_repositories
-        .missing_container_repository_registry
-        .id_not_in(except_ids)
-        .limit(batch_size)
-    end
-    # rubocop:enable CodeReuse/ActiveRecord
-
-    # rubocop:disable CodeReuse/ActiveRecord
-    def find_retryable_failed_ids(batch_size:, except_ids: [])
-      Geo::ContainerRepositoryRegistry
-        .failed
-        .retry_due
-        .model_id_not_in(except_ids)
-        .limit(batch_size)
-        .pluck_container_repository_key
-    end
-    # rubocop:enable CodeReuse/ActiveRecord
   end
 end
