24c81b28c7533e9b7e78c846c49b9c1aa0804704
Scott Stern <sstern@gitlab.com>
2020-08-01 00:26:27 +0000

    Fix board settings sidebar FOSS specs
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/38363

--- ee/spec/frontend/boards/components/board_settings_sidebar_spec.js
+++ ee/spec/frontend/boards/components/board_settings_sidebar_spec.js
@@ -5,6 +5,7 @@ import Vuex from 'vuex';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import BoardSettingsSidebar from '~/boards/components/board_settings_sidebar.vue';
 import BoardSettingsWipLimit from 'ee_component/boards/components/board_settings_wip_limit.vue';
+import BoardSettingsListTypes from 'ee_component/boards/components/board_settings_list_types.vue';
 import boardsStore from '~/boards/stores/boards_store';
 import { inactiveId } from '~/boards/constants';
 
@@ -33,6 +34,7 @@ describe('ee/BoardSettingsSidebar', () => {
       localVue,
       stubs: {
         'board-settings-sidebar-wip-limit': BoardSettingsWipLimit,
+        'board-settings-list-types': BoardSettingsListTypes,
       },
     });
   };
@@ -58,5 +60,21 @@ describe('ee/BoardSettingsSidebar', () => {
     createComponent({ activeId: listId });
 
     expect(wrapper.find(BoardSettingsWipLimit).exists()).toBe(true);
+  });
+
+  it('confirms we render BoardSettingsListTypes', () => {
+    boardsStore.addList({
+      id: 1,
+      milestone: {
+        webUrl: 'https://gitlab.com/h5bp/html5-boilerplate/-/milestones/1',
+        title: 'Backlog',
+      },
+      max_issue_count: 1,
+      list_type: 'milestone',
+    });
+
+    createComponent({ activeId: listId });
+
+    expect(wrapper.find(BoardSettingsListTypes).exists()).toBe(true);
   });
 });
