23969ec0e461b1baaf2db9d5107db2c2ef5206c5
Mark Florian <mflorian@gitlab.com>
2020-08-03 07:48:28 +0000

    Add FormInput component
    This adds a form input component, to be used in the [SAST Configuration
    UI][epic].
    
    This component is a fairly thin wrapper around the `GlFormGroup` and
    `GlFormInput` components, to conform to the [design]. It will be
    consumed by a [parent container component][container], itself driven via
    a schema to generate a form.
    
    Addresses https://gitlab.com/gitlab-org/gitlab/-/issues/225224.
    
    [epic]: https://gitlab.com/groups/gitlab-org/-/epics/3659
    [container]: https://gitlab.com/gitlab-org/gitlab/-/issues/231370
    [design]: https://gitlab.com/gitlab-org/gitlab/-/issues/228830

---
+++ ee/app/assets/javascripts/security_configuration/sast/components/constants.js
@@ -0,0 +1,17 @@
+import { s__ } from '~/locale';
+
+export const SMALL = 'small';
+export const MEDIUM = 'medium';
+export const LARGE = 'large';
+
+// The backend will supply sizes matching the keys of this map; the values
+// correspond to values acceptable to the underlying components' size props.
+export const SCHEMA_TO_PROP_SIZE_MAP = {
+  [SMALL]: 'xs',
+  [MEDIUM]: 'md',
+  [LARGE]: 'xl',
+};
+
+export const CUSTOM_VALUE_MESSAGE = s__(
+  "SecurityConfiguration|Using custom settings. You won't receive automatic updates on this variable. %{anchorStart}Restore to default%{anchorEnd}",
+);
---
+++ ee/app/assets/javascripts/security_configuration/sast/components/form_input.vue
@@ -0,0 +1,81 @@
+<script>
+import { GlFormGroup, GlFormInput, GlFormText, GlLink, GlSprintf } from '@gitlab/ui';
+import { CUSTOM_VALUE_MESSAGE, SCHEMA_TO_PROP_SIZE_MAP, LARGE } from './constants';
+
+export default {
+  components: {
+    GlFormGroup,
+    GlFormInput,
+    GlFormText,
+    GlLink,
+    GlSprintf,
+  },
+  model: {
+    prop: 'value',
+    event: 'input',
+  },
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    defaultValue: {
+      type: String,
+      required: true,
+    },
+    field: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: false,
+      default: LARGE,
+      validator: size => Object.keys(SCHEMA_TO_PROP_SIZE_MAP).includes(size),
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    isCustomValue() {
+      return this.value !== this.defaultValue;
+    },
+    inputSize() {
+      return SCHEMA_TO_PROP_SIZE_MAP[this.size];
+    },
+  },
+  methods: {
+    resetToDefaultValue() {
+      this.$emit('input', this.defaultValue);
+    },
+  },
+  i18n: {
+    CUSTOM_VALUE_MESSAGE,
+  },
+};
+</script>
+
+<template>
+  <gl-form-group :label-for="field">
+    <template #label>
+      {{ label }}
+      <gl-form-text class="gl-mt-3">{{ description }}</gl-form-text>
+    </template>
+
+    <gl-form-input :id="field" :size="inputSize" :value="value" @input="$emit('input', $event)" />
+
+    <template v-if="isCustomValue" #description>
+      <gl-sprintf :message="$options.i18n.CUSTOM_VALUE_MESSAGE">
+        <template #anchor="{ content }">
+          <gl-link @click="resetToDefaultValue" v-text="content" />
+        </template>
+      </gl-sprintf>
+    </template>
+  </gl-form-group>
+</template>
