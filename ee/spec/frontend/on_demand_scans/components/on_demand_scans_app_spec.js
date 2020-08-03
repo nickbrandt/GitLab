2660c558205fae14fba7e655c62c26d49e62173e
Paul Gascou-Vaillancourt <pgascouvaillancourt@gitlab.com>
2020-08-03 21:23:00 +0000

    Create DAST site profiles feature flag
    Created the security_on_demand_scans_site_profiles_feature_flag feature
    flag for using exisint DAST site profiles when running On-demand Scans.

--- ee/spec/frontend/on_demand_scans/components/on_demand_scans_app_spec.js
+++ ee/spec/frontend/on_demand_scans/components/on_demand_scans_app_spec.js
@@ -1,7 +1,9 @@
+import { merge } from 'lodash';
 import { shallowMount } from '@vue/test-utils';
 import { TEST_HOST } from 'helpers/test_constants';
 import OnDemandScansApp from 'ee/on_demand_scans/components/on_demand_scans_app.vue';
 import OnDemandScansForm from 'ee/on_demand_scans/components/on_demand_scans_form.vue';
+import OnDemandScansFormOld from 'ee/on_demand_scans/components/on_demand_scans_form_old.vue';
 import OnDemandScansEmptyState from 'ee/on_demand_scans/components/on_demand_scans_empty_state.vue';
 
 const helpPagePath = `${TEST_HOST}/application_security/dast/index#on-demand-scans`;
@@ -12,7 +14,6 @@ const emptyStateSvgPath = `${TEST_HOST}/assets/illustrations/alert-management-em
 describe('OnDemandScansApp', () => {
   let wrapper;
 
-  const findOnDemandScansForm = () => wrapper.find(OnDemandScansForm);
   const findOnDemandScansEmptyState = () => wrapper.find(OnDemandScansEmptyState);
 
   const expectEmptyState = () => {
@@ -20,26 +21,23 @@ describe('OnDemandScansApp', () => {
     expect(wrapper.contains(OnDemandScansEmptyState)).toBe(true);
   };
 
-  const expectForm = () => {
-    expect(wrapper.contains(OnDemandScansForm)).toBe(true);
-    expect(wrapper.contains(OnDemandScansEmptyState)).toBe(false);
+  const createComponent = options => {
+    wrapper = shallowMount(
+      OnDemandScansApp,
+      merge(
+        {},
+        {
+          propsData: {
+            helpPagePath,
+            projectPath,
+            defaultBranch,
+            emptyStateSvgPath,
+          },
+        },
+        options,
+      ),
+    );
   };
-
-  const createComponent = (props = {}) => {
-    wrapper = shallowMount(OnDemandScansApp, {
-      propsData: {
-        helpPagePath,
-        projectPath,
-        defaultBranch,
-        emptyStateSvgPath,
-        ...props,
-      },
-    });
-  };
-
-  beforeEach(() => {
-    createComponent();
-  });
 
   afterEach(() => {
     wrapper.destroy();
@@ -47,6 +45,10 @@ describe('OnDemandScansApp', () => {
   });
 
   describe('empty state', () => {
+    beforeEach(() => {
+      createComponent();
+    });
+
     it('renders an empty state by default', () => {
       expectEmptyState();
     });
@@ -59,29 +61,55 @@ describe('OnDemandScansApp', () => {
     });
   });
 
-  describe('form', () => {
-    beforeEach(async () => {
-      findOnDemandScansEmptyState().vm.$emit('createNewScan');
-      await wrapper.vm.$nextTick();
-    });
+  describe.each`
+    description   | securityOnDemandScansSiteProfilesFeatureFlag | expectedComponent       | unexpectedComponent
+    ${'enabled'}  | ${true}                                      | ${OnDemandScansForm}    | ${OnDemandScansFormOld}
+    ${'disabled'} | ${false}                                     | ${OnDemandScansFormOld} | ${OnDemandScansForm}
+  `(
+    'with :security_on_demand_scans_site_profiles_feature_flag $description',
+    ({ securityOnDemandScansSiteProfilesFeatureFlag, expectedComponent, unexpectedComponent }) => {
+      const findOnDemandScansForm = () => wrapper.find(expectedComponent);
+      const expectForm = () => {
+        expect(wrapper.contains(expectedComponent)).toBe(true);
+        expect(wrapper.contains(unexpectedComponent)).toBe(false);
+        expect(wrapper.contains(OnDemandScansEmptyState)).toBe(false);
+      };
 
-    it('renders the form when clicking on the primary button', () => {
-      expectForm();
-    });
-
-    it('passes correct props to GlEmptyState', () => {
-      expect(findOnDemandScansForm().props()).toMatchObject({
-        defaultBranch,
-        helpPagePath,
-        projectPath,
+      beforeEach(() => {
+        createComponent({
+          provide: {
+            glFeatures: {
+              securityOnDemandScansSiteProfilesFeatureFlag,
+            },
+          },
+        });
       });
-    });
 
-    it('shows the empty state on cancel', async () => {
-      findOnDemandScansForm().vm.$emit('cancel');
-      await wrapper.vm.$nextTick();
+      describe('form', () => {
+        beforeEach(async () => {
+          findOnDemandScansEmptyState().vm.$emit('createNewScan');
+          await wrapper.vm.$nextTick();
+        });
 
-      expectEmptyState();
-    });
-  });
+        it('renders the form when clicking on the primary button', () => {
+          expectForm();
+        });
+
+        it('passes correct props to GlEmptyState', () => {
+          expect(findOnDemandScansForm().props()).toMatchObject({
+            defaultBranch,
+            helpPagePath,
+            projectPath,
+          });
+        });
+
+        it('shows the empty state on cancel', async () => {
+          findOnDemandScansForm().vm.$emit('cancel');
+          await wrapper.vm.$nextTick();
+
+          expectEmptyState();
+        });
+      });
+    },
+  );
 });
