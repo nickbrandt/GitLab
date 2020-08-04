0dca9c44a1b8a3da72bedb1c7744dfc602a1a878
Adam Hegyi <ahegyi@gitlab.com>
2020-08-04 15:07:56 +0200

    Filter MRs by merged_at in GraphQL
    - Move the filter from ProductivityAnalytics
    - Expose `merged_after` and `merged_before` arguments in GraphQL

--- ee/app/finders/productivity_analytics_finder.rb
+++ ee/app/finders/productivity_analytics_finder.rb
@@ -1,24 +1,17 @@
 # frozen_string_literal: true
 
 class ProductivityAnalyticsFinder < MergeRequestsFinder
+  extend ::Gitlab::Utils::Override
+
   def self.array_params
     super.merge(days_to_merge: [])
   end
 
-  def self.scalar_params
-    @scalar_params ||= super + [:merged_before, :merged_after]
-  end
-
   def filter_items(_items)
-    items = by_days_to_merge(super)
-    by_merged_at(items)
+    by_days_to_merge(super)
   end
 
   private
-
-  def metrics_table
-    MergeRequest::Metrics.arel_table.alias(MergeRequest::Metrics.table_name)
-  end
 
   # rubocop: disable CodeReuse/ActiveRecord
   def by_days_to_merge(items)
@@ -32,27 +25,9 @@ class ProductivityAnalyticsFinder < MergeRequestsFinder
     "date_part('day',merge_request_metrics.merged_at - merge_requests.created_at)"
   end
 
-  # rubocop: disable CodeReuse/ActiveRecord
-  def by_merged_at(items)
-    return items unless params[:merged_after] || params[:merged_before]
-
-    items = items.joins(:metrics)
-    items = items.where(metrics_table[:merged_at].gteq(merged_at_between[:from])) if merged_at_between[:from]
-    items = items.where(metrics_table[:merged_at].lteq(merged_at_between[:to])) if merged_at_between[:to]
-
-    items
-  end
-  # rubocop: enable CodeReuse/ActiveRecord
-
-  def merged_at_between
-    @merged_at_between ||= begin
-      boundaries = { from: params[:merged_after], to: params[:merged_before] }
-
-      if ProductivityAnalytics.start_date && ProductivityAnalytics.start_date > boundaries[:from]
-        boundaries[:from] = ProductivityAnalytics.start_date
-      end
-
-      boundaries
-    end
+  # originated from from MergedAtFilter
+  override :merged_after
+  def merged_after
+    @merged_after ||= [super, ProductivityAnalytics.start_date].compact.max
   end
 end
