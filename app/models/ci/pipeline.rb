201dd19f6de266b3ae5663bc026cb763fc4f77ae
Maxime Orefice <morefice@gitlab.com>
2020-08-05 16:13:19 -0400

    Add pipeline create artifact service
    This new service will be used to persist generated
    coverage report once a pipeline has been completed.

--- app/models/ci/pipeline.rb
+++ app/models/ci/pipeline.rb
@@ -229,6 +229,12 @@ module Ci
       end
 
       after_transition any => ::Ci::Pipeline.completed_statuses do |pipeline|
+        pipeline.run_after_commit do
+          ::Ci::Pipelines::CreateArtifactWorker.perform_async(pipeline.id)
+        end
+      end
+
+      after_transition any => ::Ci::Pipeline.completed_statuses do |pipeline|
         next unless pipeline.bridge_triggered?
         next unless pipeline.bridge_waiting?
 
@@ -854,6 +860,10 @@ module Ci
 
     def has_reports?(reports_scope)
       complete? && latest_report_builds(reports_scope).exists?
+    end
+
+    def has_coverage_reports?
+      self.has_reports?(Ci::JobArtifact.coverage_reports)
     end
 
     def test_report_summary
