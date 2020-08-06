92a319e26762e747878c093e4d08c116e56094e7
Manoj M J <mmj@gitlab.com>
2020-08-06 12:13:23 +0000

    Allow setting a default role for Group SSO
    This change allows setting a default role
    for Group SSO

--- ee/spec/lib/ee/gitlab/scim/reprovision_service_spec.rb
+++ ee/spec/lib/ee/gitlab/scim/reprovision_service_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe ::EE::Gitlab::Scim::ReprovisionService do
     let_it_be(:identity) { create(:scim_identity, active: false) }
     let_it_be(:group) { identity.group }
     let_it_be(:user) { identity.user }
+    let_it_be(:saml_provider) do
+      create(:saml_provider, group: group, default_membership_role: Gitlab::Access::DEVELOPER)
+    end
 
     let(:service) { described_class.new(identity) }
 
@@ -22,12 +25,12 @@ RSpec.describe ::EE::Gitlab::Scim::ReprovisionService do
       expect(group.members.pluck(:user_id)).to include(user.id)
     end
 
-    it 'creates the member with guest access level' do
+    it 'creates the member with the access level as specified in saml_provider' do
       service.execute
 
       access_level = group.group_member(user).access_level
 
-      expect(access_level).to eq(Gitlab::Access::GUEST)
+      expect(access_level).to eq(Gitlab::Access::DEVELOPER)
     end
 
     it 'does not change group membership when the user is already a member' do
