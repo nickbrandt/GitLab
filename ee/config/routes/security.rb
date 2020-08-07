380b977c101a80fc0a42e4eeb06e5b7506cfcb8e
Savas Vedova <svedova@gitlab.com>
2020-08-07 20:55:02 +0300

    Prepare the code for settings page
    - Create a new route for the settings page which
      is still not accessible by the user
    - Update translations

--- ee/config/routes/security.rb
+++ ee/config/routes/security.rb
@@ -2,6 +2,7 @@
 
 namespace :security do
   root to: 'dashboard#show'
+  get 'dasboard/settings', to: 'dashboard#settings', as: :settings_dashboard
 
   resources :projects, only: [:index, :create, :destroy]
   resources :vulnerable_projects, only: [:index]
