bb5f7d52b7eaac200c7be18cc8cf4d4d4f83cf99
Jarka Košanová <jarka@gitlab.com>
2020-08-10 16:58:19 +0200

    Add new fields to the board list graphql endpoint
    - add total_weight and issues_count
    - update documentation
    - update specs

--- ee/spec/graphql/ee/types/board_list_type_spec.rb
+++ ee/spec/graphql/ee/types/board_list_type_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe GitlabSchema.types['BoardList'] do
   it 'has specific fields' do
-    expected_fields = %w[milestone max_issue_count max_issue_weight assignee]
+    expected_fields = %w[milestone max_issue_count max_issue_weight assignee total_weight]
 
     expect(described_class).to include_graphql_fields(*expected_fields)
   end
