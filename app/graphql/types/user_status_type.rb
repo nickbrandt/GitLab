77ce6d03ea263d8165ebcba564ec22dd72eabf4e
lauraMon <lmontemayor@gitlab.com>
2020-08-12 17:49:34 -0500

    Updates GraphQL schema and docs
    * Also fixes some strange linting
    * Adds some fields to User spec
    * Disabled RuboCop authorization type for UserStatus

--- app/graphql/types/user_status_type.rb
+++ app/graphql/types/user_status_type.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module Types
+  # rubocop: disable Graphql/AuthorizeTypes
   class UserStatusType < BaseObject
     graphql_name 'UserStatus'
-    expose_permissions Types::PermissionTypes::User
 
     field :message_html, GraphQL::STRING_TYPE, null: true,
       description: 'HTML of the user status message'
