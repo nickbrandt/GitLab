9d48764b84df9d7fd7cf059762a5e3778bb12871
Zack Cuddy <zcuddy@gitlab.com>
2020-08-12 04:27:58 +0000

    Geo - Generic Node Status Bars
    This change removes the need for a hard coded
    list of replicable types in the Geo Frontend
    in regards to Node Status Bars.
    
    Instead the frontend recieves the list and formats the
    data from the API as it needs to match the existing
    patterns.
    
    This change also required some small backend
    tweaks.

--- ee/spec/frontend/geo_nodes/components/node_detail_sections/node_details_section_verification_spec.js
+++ ee/spec/frontend/geo_nodes/components/node_detail_sections/node_details_section_verification_spec.js
@@ -3,6 +3,7 @@ import { GlPopover, GlSprintf } from '@gitlab/ui';
 
 import NodeDetailsSectionVerificationComponent from 'ee/geo_nodes/components/node_detail_sections/node_details_section_verification.vue';
 import SectionRevealButton from 'ee/geo_nodes/components/node_detail_sections/section_reveal_button.vue';
+import GeoNodeDetailItem from 'ee/geo_nodes/components/geo_node_detail_item.vue';
 
 import { mockNodeDetails } from '../../mock_data';
 
@@ -32,58 +33,11 @@ describe('NodeDetailsSectionVerification', () => {
   });
 
   const findGlPopover = () => wrapper.find(GlPopover);
+  const findDetailItems = () => wrapper.findAll(GeoNodeDetailItem);
 
   describe('data', () => {
     it('returns default data props', () => {
       expect(wrapper.vm.showSectionItems).toBe(false);
-      expect(Array.isArray(wrapper.vm.primaryNodeDetailItems)).toBe(true);
-      expect(Array.isArray(wrapper.vm.secondaryNodeDetailItems)).toBe(true);
-      expect(wrapper.vm.primaryNodeDetailItems.length).toBeGreaterThan(0);
-      expect(wrapper.vm.secondaryNodeDetailItems.length).toBeGreaterThan(0);
-    });
-  });
-
-  describe('methods', () => {
-    describe('getPrimaryNodeDetailItems', () => {
-      const primaryItems = [
-        {
-          title: 'Repository checksum progress',
-          valueProp: 'repositoriesChecksummed',
-        },
-        {
-          title: 'Wiki checksum progress',
-          valueProp: 'wikisChecksummed',
-        },
-      ];
-
-      it('returns array containing items to show under primary node', () => {
-        const actualPrimaryItems = wrapper.vm.getPrimaryNodeDetailItems();
-        primaryItems.forEach((item, index) => {
-          expect(actualPrimaryItems[index].itemTitle).toBe(item.title);
-          expect(actualPrimaryItems[index].itemValue).toBe(mockNodeDetails[item.valueProp]);
-        });
-      });
-    });
-
-    describe('getSecondaryNodeDetailItems', () => {
-      const secondaryItems = [
-        {
-          title: 'Repository verification progress',
-          valueProp: 'verifiedRepositories',
-        },
-        {
-          title: 'Wiki verification progress',
-          valueProp: 'verifiedWikis',
-        },
-      ];
-
-      it('returns array containing items to show under secondary node', () => {
-        const actualSecondaryItems = wrapper.vm.getSecondaryNodeDetailItems();
-        secondaryItems.forEach((item, index) => {
-          expect(actualSecondaryItems[index].itemTitle).toBe(item.title);
-          expect(actualSecondaryItems[index].itemValue).toBe(mockNodeDetails[item.valueProp]);
-        });
-      });
     });
   });
 
@@ -106,6 +60,50 @@ describe('NodeDetailsSectionVerification', () => {
 
         it('returns text about secondary nodes', () => {
           expect(wrapper.vm.nodeText).toBe('primary node');
+        });
+      });
+    });
+  });
+
+  describe('methods', () => {
+    describe.each`
+      primaryNode | dataKey           | nodeDetailItem
+      ${true}     | ${'checksum'}     | ${{ itemValue: { checksumSuccessCount: 20, checksumFailureCount: 10, verificationSuccessCount: 30, verificationFailureCount: 15 } }}
+      ${false}    | ${'verification'} | ${{ itemValue: { totalCount: 100, checksumSuccessCount: 20, checksumFailureCount: 10, verificationSuccessCount: 30, verificationFailureCount: 15 } }}
+    `(`itemValue`, ({ primaryNode, dataKey, nodeDetailItem }) => {
+      describe(`when node is ${primaryNode ? 'primary' : 'secondary'}`, () => {
+        let itemValue = {};
+
+        beforeEach(() => {
+          createComponent({ nodeTypePrimary: primaryNode });
+          itemValue = wrapper.vm.itemValue(nodeDetailItem);
+        });
+
+        it(`gets successCount correctly`, () => {
+          expect(itemValue.successCount).toBe(nodeDetailItem.itemValue[`${dataKey}SuccessCount`]);
+        });
+
+        it(`gets failureCount correctly`, () => {
+          expect(itemValue.failureCount).toBe(nodeDetailItem.itemValue[`${dataKey}FailureCount`]);
+        });
+      });
+    });
+
+    describe.each`
+      primaryNode | itemTitle | titlePostfix
+      ${true}     | ${'test'} | ${'checksum progress'}
+      ${false}    | ${'test'} | ${'verification progress'}
+    `(`itemTitle`, ({ primaryNode, itemTitle, titlePostfix }) => {
+      describe(`when node is ${primaryNode ? 'primary' : 'secondary'}`, () => {
+        let title = '';
+
+        beforeEach(() => {
+          createComponent({ nodeTypePrimary: primaryNode });
+          title = wrapper.vm.itemTitle({ itemTitle });
+        });
+
+        it(`creates full title correctly`, () => {
+          expect(title).toBe(`${itemTitle} ${titlePostfix}`);
         });
       });
     });
@@ -141,6 +139,30 @@ describe('NodeDetailsSectionVerification', () => {
             .find(GlSprintf)
             .attributes('message'),
         ).toContain('Replicated data is verified');
+      });
+    });
+
+    describe('GeoNodeDetailItems', () => {
+      describe('on Primary node', () => {
+        beforeEach(() => {
+          createComponent({ nodeTypePrimary: true });
+          wrapper.vm.showSectionItems = true;
+        });
+
+        it('renders the checksum data', () => {
+          expect(findDetailItems()).toHaveLength(mockNodeDetails.checksumStatuses.length);
+        });
+      });
+
+      describe('on Secondary node', () => {
+        beforeEach(() => {
+          createComponent({ nodeTypePrimary: false });
+          wrapper.vm.showSectionItems = true;
+        });
+
+        it('renders the verification data', () => {
+          expect(findDetailItems()).toHaveLength(mockNodeDetails.verificationStatuses.length);
+        });
       });
     });
   });
