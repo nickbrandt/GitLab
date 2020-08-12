158a8c8068c8c9615ed4f00770def836bdb057db
lauraMon <lmontemayor@gitlab.com>
2020-08-12 17:49:34 -0500

    Updates GraphQL schema and docs
    * Also fixes some strange linting
    * Adds some fields to User spec
    * Disabled RuboCop authorization type for UserStatus

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -18,7 +18,7 @@ module Types
           description: 'Human-readable name of the user'
     field :state, Types::UserStateEnum, null: false,
           description: 'State of the user'
-    field :email, GraphQL::STRING_TYPE, null:true,
+    field :email, GraphQL::STRING_TYPE, null: true,
           description: 'User email'
     field :avatar_url, GraphQL::STRING_TYPE, null: true,
           description: "URL of the user's avatar"
@@ -32,7 +32,7 @@ module Types
     field :group_memberships, Types::GroupMemberType.connection_type, null: true,
           description: 'Group memberships of the user',
           method: :group_members
-    field :status, Types::UserStatusType, null:true,
+    field :status, Types::UserStatusType, null: true,
            description: 'User status'
     field :project_memberships, Types::ProjectMemberType.connection_type, null: true,
           description: 'Project memberships of the user',
