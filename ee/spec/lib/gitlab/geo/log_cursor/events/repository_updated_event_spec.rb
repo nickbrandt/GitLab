b9d7e7db522a15d2b4b6859896203f6f5de486a6
Mike Kozono <mkozono@gmail.com>
2020-08-13 10:14:49 -0700

    Geo: Apply selective sync to project/wiki events
    For example, if a project is outside selective sync, it should not be
    synced, nor should it create a registry record (since that will impact
    counts and also lead to syncing).

--- ee/spec/lib/gitlab/geo/log_cursor/events/repository_updated_event_spec.rb
+++ ee/spec/lib/gitlab/geo/log_cursor/events/repository_updated_event_spec.rb
@@ -28,6 +28,18 @@ RSpec.describe Gitlab::Geo::LogCursor::Events::RepositoryUpdatedEvent, :clean_gi
       expect { subject.process }.to change(Geo::ProjectRegistry, :count).by(1)
     end
 
+    context 'when outside selective sync' do
+      before do
+        selective_sync_secondary = create(:geo_node, selective_sync_type: 'shards', selective_sync_shards: ['non-existent'])
+
+        stub_current_geo_node(selective_sync_secondary)
+      end
+
+      it 'does not create a new project registry' do
+        expect { subject.process }.not_to change(Geo::ProjectRegistry, :count)
+      end
+    end
+
     context 'when we have an event source' do
       before do
         repository_updated_event.update!(source: event_source)
