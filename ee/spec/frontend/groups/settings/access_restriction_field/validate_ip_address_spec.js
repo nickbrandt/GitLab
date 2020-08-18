a65ff14ab1eb24421539c86675607ba1f95cc5f3
peterhegman <phegman@gitlab.com>
2020-08-17 22:41:37 -0700

    Switch to mocking `ipaddr.isValid`

--- ee/spec/frontend/groups/settings/access_restriction_field/validate_ip_address_spec.js
+++ ee/spec/frontend/groups/settings/access_restriction_field/validate_ip_address_spec.js
@@ -1,20 +1,26 @@
-import * as validateIpAddress from 'ee/validators/ip_address';
+import ipaddr from 'ipaddr.js';
 import validateRestrictedIpAddress from 'ee/groups/settings/access_restriction_field/validate_ip_address';
 
 describe('validateRestrictedIpAddress', () => {
-  describe('when `validateIpAddress` returns false', () => {
+  describe('when IP address is not valid', () => {
     it('returns an error message', () => {
-      validateIpAddress.default = jest.fn(() => false);
+      ipaddr.isValid = jest.fn(() => false);
 
-      expect(validateRestrictedIpAddress('foo bar')).toBe(`foo bar is an invalid IP address range`);
+      const result = validateRestrictedIpAddress('foo bar');
+
+      expect(ipaddr.isValid).toHaveBeenCalledWith('foo bar');
+      expect(result).toBe(`foo bar is an invalid IP address range`);
     });
   });
 
   describe('when IP address is valid', () => {
     it('returns an empty string', () => {
-      validateIpAddress.default = jest.fn(() => true);
+      ipaddr.isValid = jest.fn(() => true);
 
-      expect(validateRestrictedIpAddress('192.168.0.0/24')).toBe('');
+      const result = validateRestrictedIpAddress('192.168.0.0');
+
+      expect(ipaddr.isValid).toHaveBeenCalledWith('192.168.0.0');
+      expect(result).toBe('');
     });
   });
 });
