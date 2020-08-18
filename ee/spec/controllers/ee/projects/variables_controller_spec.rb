3d2cccfcfee3074d63b6ba76bf63bac009a10ba0
Matija Čupić <matteeyah@gmail.com>
2020-08-18 14:57:15 +0000

    Audit variable changes in projects

---
+++ ee/spec/controllers/ee/projects/variables_controller_spec.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Projects::VariablesController do
+  let(:project) { create(:project) }
+  let(:user) { create(:user) }
+
+  before do
+    sign_in(user)
+    project.add_maintainer(user)
+  end
+
+  describe 'PATCH #update' do
+    subject(:patch_update) do
+      patch :update,
+        params: {
+        namespace_id: project.namespace.to_param,
+        project_id: project,
+        variables_attributes: [variable_attributes]
+      },
+      format: :json
+    end
+
+    before do
+      stub_licensed_features(extended_audit_events: true)
+    end
+
+    context 'when creating variable' do
+      let(:variable_attributes) do
+        { key: 'new_key',
+          secret_value: 'dummy_value',
+          protected: 'false',
+          environment_scope: '*' }
+      end
+
+      it 'logs audit event' do
+        expect { patch_update }.to change(AuditEvent, :count).from(0).to(1)
+      end
+
+      it 'logs variable creation' do
+        patch_update
+
+        audit_event = AuditEvent.last.present
+
+        expect(audit_event.action).to eq('Added ci variable')
+        expect(audit_event.target).to eq('new_key')
+      end
+    end
+
+    context 'when updating variable protection' do
+      let(:variable) { create(:ci_variable, project: project, protected: false) }
+
+      let(:variable_attributes) do
+        { id: variable.id,
+          protected: 'true' }
+      end
+
+      it 'logs audit event' do
+        expect { patch_update }.to change(AuditEvent, :count).from(0).to(1)
+      end
+
+      it 'logs variable protection update' do
+        patch_update
+
+        audit_event = AuditEvent.last.present
+
+        expect(audit_event.action).to eq('Changed variable protection from false to true')
+        expect(audit_event.target).to eq(variable.key)
+      end
+    end
+
+    context 'when destroying variable' do
+      let(:variable) { create(:ci_variable, project: project) }
+
+      let(:variable_attributes) do
+        { key: variable.key,
+          _destroy: 'true' }
+      end
+
+      it 'logs audit event' do
+        expect { patch_update }.to change(AuditEvent, :count).from(0).to(1)
+      end
+
+      it 'logs variable destruction' do
+        patch_update
+
+        audit_event = AuditEvent.last.present
+
+        expect(audit_event.action).to eq('Removed ci variable')
+        expect(audit_event.target).to eq(variable.key)
+      end
+    end
+  end
+end
---
+++ ee/spec/requests/api/variables_spec.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe API::Variables do
+  let(:user) { create(:user) }
+  let(:project) { create(:project) }
+
+  before do
+    project.add_maintainer(user)
+
+    stub_licensed_features(extended_audit_events: true)
+  end
+
+  describe 'POST /projects/:id/variables' do
+    subject(:post_create) do
+      post api("/projects/#{project.id}/variables", user), params: { key: 'new_variable', value: 'secret_value', protected: true }
+    end
+
+    it 'logs audit event' do
+      expect { post_create }.to change(AuditEvent, :count).from(0).to(1)
+    end
+
+    it 'logs variable creation' do
+      post_create
+
+      audit_event = AuditEvent.last.present
+
+      expect(audit_event.action).to eq('Added ci variable')
+      expect(audit_event.target).to eq('new_variable')
+    end
+  end
+
+  describe 'PUT /projects/:id/variables/:key' do
+    let(:variable) { create(:ci_variable, project: project, protected: false) }
+
+    subject(:put_update) do
+      put api("/projects/#{project.id}/variables/#{variable.key}", user), params: { protected: true }
+    end
+
+    it 'logs audit event' do
+      expect { put_update }.to change(AuditEvent, :count).from(0).to(1)
+    end
+
+    it 'logs variable protection update' do
+      put_update
+
+      audit_event = AuditEvent.last.present
+
+      expect(audit_event.action).to eq('Changed variable protection from false to true')
+      expect(audit_event.target).to eq(variable.key)
+    end
+  end
+
+  describe 'DELETE /projects/:id/variables/:key' do
+    let(:variable) { create(:ci_variable, project: project, protected: false) }
+
+    subject(:delete_destroy) do
+      delete api("/projects/#{project.id}/variables/#{variable.key}", user)
+    end
+
+    it 'logs audit event' do
+      expect { delete_destroy }.to change(AuditEvent, :count).from(0).to(1)
+    end
+
+    it 'logs variable destruction' do
+      delete_destroy
+
+      audit_event = AuditEvent.last.present
+
+      expect(audit_event.action).to eq('Removed ci variable')
+      expect(audit_event.target).to eq(variable.key)
+    end
+  end
+end
