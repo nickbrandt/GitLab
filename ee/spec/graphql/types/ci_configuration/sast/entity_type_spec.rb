293270956a066fb2869697a48e16521a793f77db
Saikat Sarkar <ssarkar@gitlab.com>
2020-08-19 09:28:06 -0600

    Add size field to SAST config graphql query

--- ee/spec/graphql/types/ci_configuration/sast/entity_type_spec.rb
+++ ee/spec/graphql/types/ci_configuration/sast/entity_type_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe GitlabSchema.types['SastCiConfigurationEntity'] do
-  let(:fields) { %i[field label description type options default_value value] }
+  let(:fields) { %i[field label description type options default_value value size] }
 
   it { expect(described_class.graphql_name).to eq('SastCiConfigurationEntity') }
 
