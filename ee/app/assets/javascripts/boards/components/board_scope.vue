ccc82bfa3665355e74b97675b7b39b5e289d942f
clenneville <clenneville@gitlab.com>
2020-08-19 12:43:52 -0500

    Series of several component fixes

--- ee/app/assets/javascripts/boards/components/board_scope.vue
+++ ee/app/assets/javascripts/boards/components/board_scope.vue
@@ -113,6 +113,7 @@ export default {
         :can-edit="canAdminBoard"
         :enable-scoped-labels="enableScopedLabels"
         ability-name="issue"
+        show-create
         @onLabelClick="handleLabelClick"
       >
         {{ __('Any label') }}
