029d6152cab4d7414c1a83a852ef422fb1517a4c
Alex Kalderimis <alex.kalderimis@gmail.com>
2020-08-21 10:21:57 -0400

    Add worker queue definition
    This adds the required worker queue definition for sidekiq, with its
    configuration and generated defintion.

--- app/workers/issue_rebalancing_worker.rb
+++ app/workers/issue_rebalancing_worker.rb
@@ -3,11 +3,15 @@
 class IssueRebalancingWorker # rubocop:disable Scalability/IdempotentWorker
   include ApplicationWorker
 
+  idempotent!
+  urgency :low
+  feature_category :issue_tracking
+
   def perform(issue_id)
     issue = Issue.find(issue_id)
 
     rebalance(issue)
-  rescue ActiveRecord::RecordNotFound => e
+  rescue ActiveRecord::RecordNotFound, IssueRebalancingService::TooManyIssues, RelativePositioning::NoSpaceLeft => e
     Sidekiq.logger.warn(e)
   end
 
