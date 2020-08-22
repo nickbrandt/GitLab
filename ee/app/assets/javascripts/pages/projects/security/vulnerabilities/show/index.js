5caab361d5d1b7b686e795435914b61ad6a69365
Kev <kevslashnull@gmail.com>
2020-08-22 18:02:56 +0200

    Remove now unused vulnerability component parts

--- ee/app/assets/javascripts/pages/projects/security/vulnerabilities/show/index.js
+++ ee/app/assets/javascripts/pages/projects/security/vulnerabilities/show/index.js
@@ -1,106 +1,5 @@
 import Vue from 'vue';
 import MainApp from 'ee/vulnerabilities/components/main.vue';
-import HeaderApp from 'ee/vulnerabilities/components/header.vue';
-import DetailsApp from 'ee/vulnerabilities/components/details.vue';
-import FooterApp from 'ee/vulnerabilities/components/footer.vue';
-import { VULNERABILITY_STATE_OBJECTS } from 'ee/vulnerabilities/constants';
-import { convertObjectPropsToCamelCase } from '~/lib/utils/common_utils';
-
-function createHeaderApp() {
-  const el = document.getElementById('js-vulnerability-header');
-  const vulnerability = JSON.parse(el.dataset.vulnerability);
-
-  return new Vue({
-    el,
-
-    render: h =>
-      h(HeaderApp, {
-        props: {
-          initialVulnerability: vulnerability,
-        },
-      }),
-  });
-}
-
-function createDetailsApp() {
-  const el = document.getElementById('js-vulnerability-details');
-  const vulnerability = JSON.parse(el.dataset.vulnerability);
-
-  return new Vue({
-    el,
-    render: h => h(DetailsApp, { props: { vulnerability } }),
-  });
-}
-
-function createFooterApp() {
-  const el = document.getElementById('js-vulnerability-footer');
-
-  if (!el) {
-    return false;
-  }
-
-  const {
-    vulnerabilityFeedbackHelpPath,
-    hasMr,
-    discussionsUrl,
-    createIssueUrl,
-    state,
-    issueFeedback,
-    mergeRequestFeedback,
-    notesUrl,
-    project,
-    projectFingerprint,
-    remediations,
-    reportType,
-    solution,
-    id,
-    canModifyRelatedIssues,
-    relatedIssuesHelpPath,
-  } = convertObjectPropsToCamelCase(JSON.parse(el.dataset.vulnerability));
-
-  const remediation = remediations?.length ? remediations[0] : null;
-  const hasDownload = Boolean(
-    state !== VULNERABILITY_STATE_OBJECTS.resolved.state && remediation?.diff?.length && !hasMr,
-  );
-  const hasRemediation = Boolean(remediation);
-
-  const props = {
-    vulnerabilityId: id,
-    discussionsUrl,
-    notesUrl,
-    solutionInfo: {
-      solution,
-      remediation,
-      hasDownload,
-      hasMr,
-      hasRemediation,
-      vulnerabilityFeedbackHelpPath,
-      isStandaloneVulnerability: true,
-    },
-    issueFeedback,
-    mergeRequestFeedback,
-    canModifyRelatedIssues,
-    project: {
-      url: project.full_path,
-      value: project.full_name,
-    },
-    relatedIssuesHelpPath,
-  };
-
-  return new Vue({
-    el,
-    provide: {
-      reportType,
-      createIssueUrl,
-      projectFingerprint,
-      vulnerabilityId: id,
-    },
-    render: h =>
-      h(FooterApp, {
-        props,
-      }),
-  });
-}
 
 function createMainApp() {
   const el = document.getElementById('js-vulnerability-main');
@@ -118,9 +17,7 @@ function createMainApp() {
 
     render: h =>
       h(MainApp, {
-        props: {
-          vulnerability,
-        },
+        props: { vulnerability },
       }),
   });
 }
