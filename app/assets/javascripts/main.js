dd8ee2cc1907dafad04bb0b3814af3562cd6f039
Doug Stull <dstull@gitlab.com>
2020-08-24 14:53:52 -0400

    Defer snowplow loading until rest of dom is loaded
    - the work being done in snowplow initialization
      was slowing down dom load times and deferring
      it here should speed it up a bit.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -37,7 +37,7 @@ import GlFieldErrors from './gl_field_errors';
 import initUserPopovers from './user_popovers';
 import initBroadcastNotifications from './broadcast_notification';
 import initPersistentUserCallouts from './persistent_user_callouts';
-import { initUserTracking } from './tracking';
+import { initUserTracking, initDefaultTrackers } from './tracking';
 import { __ } from './locale';
 
 import 'ee_else_ce/main_ee';
@@ -111,6 +111,7 @@ function deferredInitialisation() {
   initBroadcastNotifications();
   initFrequentItemDropdowns();
   initPersistentUserCallouts();
+  initDefaultTrackers();
 
   if (document.querySelector('.search')) initSearchAutocomplete();
 
