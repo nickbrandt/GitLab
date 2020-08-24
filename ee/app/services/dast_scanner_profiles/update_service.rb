55c7d24493059b6b78b971112c968d9a6384a9e7
Craig Smith <5344211-craigmsmith@users.noreply.gitlab.com>
2020-08-24 10:42:23 +1000

    Add endpoint to update dast scanner profile
    To allow dast users to edit their scanner profiles
    this commit implements the update DastScannerProfiles
    mutation

---
+++ ee/app/services/dast_scanner_profiles/update_service.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module DastScannerProfiles
+  class UpdateService < BaseService
+    include Gitlab::Allowable
+
+    def execute(id:, profile_name:, target_timeout:, spider_timeout:)
+      return unauthorized unless can_update_scanner_profile?
+
+      dast_scanner_profile = find_dast_scanner_profile(id)
+      return ServiceResponse.error(message: "Scanner profile not found for given parameters") unless dast_scanner_profile
+
+      if dast_scanner_profile.update(name: profile_name, target_timeout: target_timeout, spider_timeout: spider_timeout)
+        ServiceResponse.success(payload: dast_scanner_profile)
+      else
+        ServiceResponse.error(message: dast_scanner_profile.errors.full_messages)
+      end
+    end
+
+    private
+
+    def unauthorized
+      ::ServiceResponse.error(message: _('You are not authorized to update this scanner profile'), http_status: 403)
+    end
+
+    def can_update_scanner_profile?
+      can?(current_user, :create_on_demand_dast_scan, project)
+    end
+
+    def find_dast_scanner_profile(id)
+      project.dast_scanner_profiles.id_in(id).first
+    end
+  end
+end
