a16ab76f200d01a89e0d6a4a800640fc2fff5828
Alex Kalderimis <akalderimis@gitlab.com>
2020-08-25 15:10:25 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- app/workers/issue_rebalancing_worker.rb
+++ app/workers/issue_rebalancing_worker.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class IssueRebalancingWorker # rubocop:disable Scalability/IdempotentWorker
+class IssueRebalancingWorker
   include ApplicationWorker
 
   idempotent!
