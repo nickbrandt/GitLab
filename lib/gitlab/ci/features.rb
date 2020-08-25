0ffca5d96dd17b056f0596f219abd20fc8762063
Marius Bobin <mbobin@gitlab.com>
2020-08-25 16:36:46 +0300

    Remove feature flag helper method

--- lib/gitlab/ci/features.rb
+++ lib/gitlab/ci/features.rb
@@ -76,10 +76,6 @@ module Gitlab
         ::Feature.enabled?(:reset_ci_minutes_for_all_namespaces, default_enabled: false)
       end
 
-      def self.expand_names_for_cross_pipeline_artifacts?(project)
-        ::Feature.enabled?(:ci_expand_names_for_cross_pipeline_artifacts, project)
-      end
-
       def self.project_transactionless_destroy?(project)
         Feature.enabled?(:project_transactionless_destroy, project, default_enabled: false)
       end
