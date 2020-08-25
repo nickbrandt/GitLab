dd40f3429c303d4e9408b9a399f6e7c4570a33ca
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2020-08-25 12:54:28 -0400

    Use existing scanner profiles in on-demand scans
    Added the ability to select existing DAST scanner profiles when running
    an on-demand DAST scan. This currently requires the
    securityOnDemandScansScannerProfiles feature flag to be enabled.

--- ee/app/controllers/projects/on_demand_scans_controller.rb
+++ ee/app/controllers/projects/on_demand_scans_controller.rb
@@ -4,6 +4,7 @@ module Projects
   class OnDemandScansController < Projects::ApplicationController
     before_action do
       authorize_read_on_demand_scans!
+      push_frontend_feature_flag(:security_on_demand_scans_scanner_profiles)
     end
 
     def index
