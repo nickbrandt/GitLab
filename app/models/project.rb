785d505fbc4205c40a46f3ce44a83f15d5aebb75
Fabio Pitino <fpitino@gitlab.com>
2020-08-26 13:12:11 +0100

    Define dangling pipelines to be excluded from Project#ci_pipelines
    Project#ci_pipelines implicitly excluded some types of pipeline
    that do not contribute to the CI ref status. With this change
    we are making explicit why.
    
    We also change the way `ci_pipelines` are filtered from based to
    `config_source` to `source`. Dangling pipelines are characterized
    by what event triggered them (source) and not where the CI config
    came from (config_source).

--- app/models/project.rb
+++ app/models/project.rb
@@ -279,10 +279,9 @@ class Project < ApplicationRecord
   # The relation :all_pipelines is intended to be used when we want to get the
   # whole list of pipelines associated to the project
   has_many :all_pipelines, class_name: 'Ci::Pipeline', inverse_of: :project
-  # The relation :ci_pipelines is intended to be used when we want to get only
-  # those pipeline which are directly related to CI. There are
-  # other pipelines, like webide ones, that we won't retrieve
-  # if we use this relation.
+  # The relation :ci_pipelines includes all those that directly contribute to the
+  # latest status of a ref. This does not include dangling pipelines such as those
+  # from webide, child pipelines, etc.
   has_many :ci_pipelines,
           -> { ci_sources },
           class_name: 'Ci::Pipeline',
