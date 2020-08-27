c2ac9042b2bacf9de69fb084b1cb558077c5a0b2
Dave Pisek <dpisek@gitlab.com>
2020-08-27 16:01:51 +1000

    Fix failing rspec test

--- ee/app/controllers/projects/dast_profiles_controller.rb
+++ ee/app/controllers/projects/dast_profiles_controller.rb
@@ -2,9 +2,9 @@
 
 module Projects
   class DastProfilesController < Projects::ApplicationController
+    before_action :authorize_read_on_demand_scans!
     before_action do
       push_frontend_feature_flag(:security_on_demand_scans_scanner_profiles, project, default_enabled: false)
-      :authorize_read_on_demand_scans!
     end
 
     def index
