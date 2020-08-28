2dafb6f9c49e240abe6d373713d74d755c9a6c98
Mehmet Emin INAC <minac@gitlab.com>
2020-08-28 19:05:22 +0300

    Create foreign key constraints in seperate migrations
    To comply with the style guides at Gitlab.

--- db/migrate/20200825084819_create_security_findings_table.rb
+++ db/migrate/20200825084819_create_security_findings_table.rb
@@ -8,9 +8,9 @@ class CreateSecurityFindingsTable < ActiveRecord::Migration[6.0]
   disable_ddl_transaction!
 
   def up
-    create_table :security_findings, id: :bigserial do |t|
-      t.references :scan, null: false, foreign_key: { to_table: :security_scans, on_delete: :cascade }
-      t.references :scanner, null: false, foreign_key: { to_table: :vulnerability_scanners, on_delete: :cascade }
+    create_table :security_findings, if_not_exists: true do |t|
+      t.references :scan, null: false
+      t.references :scanner, null: false
       t.integer :severity, limit: 2, index: true, null: false
       t.integer :confidence, limit: 2, index: true, null: false
       t.text :project_fingerprint, index: true, null: false
