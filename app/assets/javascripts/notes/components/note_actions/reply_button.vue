5eba893eeedc838daebd6218bd9c5b7280e1cdf0
matejlatin <mlatin@gitlab.com>
2020-08-31 13:12:08 +0200

    Replaced the deprecated button

--- app/assets/javascripts/notes/components/note_actions/reply_button.vue
+++ app/assets/javascripts/notes/components/note_actions/reply_button.vue
@@ -1,11 +1,11 @@
 <script>
-import { GlTooltipDirective, GlDeprecatedButton, GlIcon } from '@gitlab/ui';
+import { GlTooltipDirective, GlButton, GlIcon } from '@gitlab/ui';
 
 export default {
   name: 'ReplyButton',
   components: {
     GlIcon,
-    GlDeprecatedButton,
+    GlButton,
   },
   directives: {
     GlTooltip: GlTooltipDirective,
@@ -15,17 +15,17 @@ export default {
 
 <template>
   <div class="note-actions-item">
-    <gl-deprecated-button
+    <gl-button
       ref="button"
       v-gl-tooltip
       class="note-action-button"
       data-track-event="click_button"
       data-track-label="reply_comment_button"
-      variant="transparent"
+      category="tertiary"
       :title="__('Reply to comment')"
       @click="$emit('startReplying')"
     >
       <gl-icon name="comment" class="link-highlight" />
-    </gl-deprecated-button>
+    </gl-button>
   </div>
 </template>
