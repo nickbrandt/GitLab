2bb36a0fd750dd8fe814ec49ecf79d070773dbff
Brandon Labuschagne <blabuschagne@gitlab.com>
2020-08-31 15:42:45 +0000

    MR Analytics - Fix throughput chart date range calculation

--- ee/spec/frontend/analytics/merge_request_analytics/utils_spec.js
+++ ee/spec/frontend/analytics/merge_request_analytics/utils_spec.js
@@ -2,10 +2,13 @@ import * as utils from 'ee/analytics/merge_request_analytics/utils';
 import { expectedMonthData } from './mock_data';
 
 describe('computeMonthRangeData', () => {
-  it('returns the data as expected', () => {
-    const startDate = new Date('2020-05-17T00:00:00.000Z');
-    const endDate = new Date('2020-07-17T00:00:00.000Z');
+  const start = new Date('2020-05-17T00:00:00.000Z');
 
+  it.each`
+    startDate | endDate
+    ${start}  | ${new Date('2020-07-17T00:00:00.000Z')}
+    ${start}  | ${new Date('2020-07-31T00:00:00.000Z')}
+  `('returns the data as expected', ({ startDate, endDate }) => {
     const monthData = utils.computeMonthRangeData(startDate, endDate);
 
     expect(monthData).toStrictEqual(expectedMonthData);
