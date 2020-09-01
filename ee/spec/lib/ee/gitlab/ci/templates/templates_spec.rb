ea79ef49b1a3abbafaa4aab03ea958c46d9940e3
Fabio Pitino <fpitino@gitlab.com>
2020-09-01 16:34:11 +0000

    Refactor YamlProcessor to return a result object
    Simplifies UX of YamlProcessor to be better used for CI Lint.
    Returns an object that can present the evaluated configuration,
    any possible errors and warnings.

--- ee/spec/lib/ee/gitlab/ci/templates/templates_spec.rb
+++ ee/spec/lib/ee/gitlab/ci/templates/templates_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe "CI YML Templates" do
   using RSpec::Parameterized::TableSyntax
 
-  subject { Gitlab::Ci::YamlProcessor.new(content) }
+  subject { Gitlab::Ci::YamlProcessor.new(content).execute }
 
   where(:template_name) do
     Gitlab::Template::GitlabCiYmlTemplate.all.map(&:full_name)
@@ -24,7 +24,7 @@ RSpec.describe "CI YML Templates" do
     end
 
     it 'is valid' do
-      expect { subject }.not_to raise_error
+      expect(subject).to be_valid
     end
 
     it 'require default stages to be included' do
