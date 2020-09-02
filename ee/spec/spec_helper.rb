26eaa109f7a93b570b7248540b0b8a83b2243d02
Rajendra Kadam <rajendrakadam249@gmail.com>
2020-09-02 13:46:15 +0000

    Fix Lint/NonDeterministicRequireOrder cop

--- ee/spec/spec_helper.rb
+++ ee/spec/spec_helper.rb
@@ -2,10 +2,10 @@
 
 require Rails.root.join("spec/support/helpers/stub_requests.rb")
 
-Dir[Rails.root.join("ee/spec/support/helpers/*.rb")].each { |f| require f }
-Dir[Rails.root.join("ee/spec/support/shared_contexts/*.rb")].each { |f| require f }
-Dir[Rails.root.join("ee/spec/support/shared_examples/*.rb")].each { |f| require f }
-Dir[Rails.root.join("ee/spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("ee/spec/support/helpers/*.rb")].sort.each { |f| require f }
+Dir[Rails.root.join("ee/spec/support/shared_contexts/*.rb")].sort.each { |f| require f }
+Dir[Rails.root.join("ee/spec/support/shared_examples/*.rb")].sort.each { |f| require f }
+Dir[Rails.root.join("ee/spec/support/**/*.rb")].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.include EE::LicenseHelpers
