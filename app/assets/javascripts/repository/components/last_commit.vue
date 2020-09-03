f813b56e1406109cdc5c1b9688b499b818725d11
Takuya Noguchi <takninnovationresearch@gmail.com>
2020-09-03 23:26:20 +0000

    Remove v-html from repository/.../last_commit.vue
    Signed-off-by: Takuya Noguchi <takninnovationresearch@gmail.com>

--- app/assets/javascripts/repository/components/last_commit.vue
+++ app/assets/javascripts/repository/components/last_commit.vue
@@ -1,6 +1,12 @@
 <script>
-/* eslint-disable vue/no-v-html */
-import { GlTooltipDirective, GlLink, GlButton, GlButtonGroup, GlLoadingIcon } from '@gitlab/ui';
+import {
+  GlTooltipDirective,
+  GlLink,
+  GlButton,
+  GlButtonGroup,
+  GlLoadingIcon,
+  GlSafeHtmlDirective as SafeHtml,
+} from '@gitlab/ui';
 import defaultAvatarUrl from 'images/no_avatar.png';
 import pathLastCommitQuery from 'shared_queries/repository/path_last_commit.query.graphql';
 import { sprintf, s__ } from '~/locale';
@@ -24,6 +30,7 @@ export default {
   },
   directives: {
     GlTooltip: GlTooltipDirective,
+    SafeHtml,
   },
   mixins: [getRefMixin],
   apollo: {
@@ -118,10 +125,10 @@ export default {
       <div class="commit-detail flex-list">
         <div class="commit-content qa-commit-content">
           <gl-link
+            v-safe-html="commit.titleHtml"
             :href="commit.webPath"
             :class="{ 'font-italic': !commit.message }"
             class="commit-row-message item-title"
-            v-html="commit.titleHtml"
           />
           <gl-button
             v-if="commit.descriptionHtml"
@@ -147,13 +154,13 @@ export default {
           </div>
           <pre
             v-if="commit.descriptionHtml"
+            v-safe-html="commit.descriptionHtml"
             :class="{ 'd-block': showDescription }"
             class="commit-row-description gl-mb-3"
-            v-html="commit.descriptionHtml"
           ></pre>
         </div>
         <div class="commit-actions flex-row">
-          <div v-if="commit.signatureHtml" v-html="commit.signatureHtml"></div>
+          <div v-if="commit.signatureHtml" v-safe-html="commit.signatureHtml"></div>
           <div v-if="commit.pipeline" class="ci-status-link">
             <gl-link
               v-gl-tooltip.left
