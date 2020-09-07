f09e4d5ba221fb54de273c107099f6cda2f60776
Jan Provaznik <jprovaznik@gitlab.com>
2020-09-07 17:50:53 +0000

    Revert "Merge branch 'confdential_epics_removal' into 'master'"
    This reverts merge request !41551

--- ee/app/controllers/groups/roadmap_controller.rb
+++ ee/app/controllers/groups/roadmap_controller.rb
@@ -11,6 +11,7 @@ module Groups
     before_action :persist_roadmap_layout, only: [:show]
     before_action do
       push_frontend_feature_flag(:roadmap_buffered_rendering, @group)
+      push_frontend_feature_flag(:confidential_epics, @group, default_enabled: true)
       push_frontend_feature_flag(:async_filtering, @group)
     end
 
