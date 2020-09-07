62bd16547fc00219125855c483aeddefb1427db8
Vasilii Iakliushin <viakliushin@gitlab.com>
2020-09-07 14:18:55 +0000

    Remove product suffixes from the codebase
    * Remove starter-only
    * Remove core-only
    * Remove premium-only
    * Remove ultimate-only
    * Remove ultimate
    * Remove premium
    * Remove starter
    * Remove core

--- ee/app/helpers/ee/projects_helper.rb
+++ ee/app/helpers/ee/projects_helper.rb
@@ -102,7 +102,7 @@ module EE
       'rules_path': expose_path(api_v4_projects_approval_settings_rules_path(id: project.id)),
       'allow_multi_rule': project.multiple_approval_rules_available?.to_s,
       'eligible_approvers_docs_path': help_page_path('user/project/merge_requests/merge_request_approvals', anchor: 'eligible-approvers'),
-      'security_approvals_help_page_path': help_page_path('user/application_security/index.md', anchor: 'security-approvals-in-merge-requests-ultimate'),
+      'security_approvals_help_page_path': help_page_path('user/application_security/index.md', anchor: 'security-approvals-in-merge-requests'),
       'security_configuration_path': project_security_configuration_path(project),
       'vulnerability_check_help_page_path': help_page_path('user/application_security/index', anchor: 'enabling-security-approvals-within-a-project'),
       'license_check_help_page_path': help_page_path('user/application_security/index', anchor: 'enabling-license-approvals-within-a-project') } }
