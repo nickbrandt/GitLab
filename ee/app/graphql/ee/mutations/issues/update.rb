5764dcb7d6983c31042fc555a8d4e3c0470570af
Jarka Košanová <jarka@gitlab.com>
2020-09-09 11:49:58 +0200

    Fix parsing epic_id id type param

--- ee/app/graphql/ee/mutations/issues/update.rb
+++ ee/app/graphql/ee/mutations/issues/update.rb
@@ -16,6 +16,12 @@ module EE
                    required: false,
                    description: 'The ID of the parent epic. NULL when removing the association'
         end
+
+        def resolve(project_path:, iid:, **args)
+          args[:epic_id] = ::GitlabSchema.parse_gid(args[:epic_id], expected_type: ::Epic).model_id if args[:epic_id]
+
+          super
+        end
       end
     end
   end
