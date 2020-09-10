f324d8a186048bce1e2669fdf54da3938762265f
Alex Kalderimis <alex.kalderimis@gmail.com>
2020-09-10 10:23:13 +0100

    Add exhaustive tests for find_next_gap_{before,after}

--- app/models/concerns/relative_positioning.rb
+++ app/models/concerns/relative_positioning.rb
@@ -33,6 +33,10 @@ module RelativePositioning
       @start_pos, @end_pos = start_pos, end_pos
     end
 
+    def ==(other)
+      other.is_a?(self.class) && other.start_pos == start_pos && other.end_pos == end_pos
+    end
+
     def delta
       ((start_pos - end_pos) / 2.0).abs.ceil.clamp(0, RelativePositioning::IDEAL_DISTANCE)
     end
