efd3ac56b5e35d94ec37f3f72a9af58f946dad6d
Toon Claes <toon@gitlab.com>
2020-09-14 15:56:03 +0200

    Stub callback in factory instead of skipping it
    Instead of skipping a callback, stub it on the instance, this avoids
    messing up the factory for any future use.
    
    See https://gitlab.com/gitlab-org/gitlab/-/issues/247865

--- ee/spec/models/geo/merge_request_diff_registry_spec.rb
+++ ee/spec/models/geo/merge_request_diff_registry_spec.rb
@@ -3,7 +3,9 @@
 require 'spec_helper'
 
 RSpec.describe Geo::MergeRequestDiffRegistry, :geo, type: :model do
-  let_it_be(:registry) { create(:geo_merge_request_diff_registry) }
+  let_it_be(:merge_request) { create(:merge_request) }
+  let_it_be(:merge_request_diff) { create(:merge_request_diff, merge_request: merge_request) }
+  let_it_be(:registry) { create(:geo_merge_request_diff_registry, merge_request_diff: merge_request_diff) }
 
   specify 'factory is valid' do
     expect(registry).to be_valid
