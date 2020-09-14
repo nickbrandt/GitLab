3864bf4ef61b882fa2ee5f758b1ce14cad2b8f79
Toon Claes <toon@gitlab.com>
2020-09-14 20:52:54 +0200

    Geo: Implement GraphQL endpoint for MR diffs
    Implement the GraphQL endpoint as documented at:
    https://docs.gitlab.com/ee/development/geo/framework.html#graphql-api
    
    Closes https://gitlab.com/gitlab-org/gitlab/-/issues/215138

--- ee/app/graphql/types/geo/geo_node_type.rb
+++ ee/app/graphql/types/geo/geo_node_type.rb
@@ -22,6 +22,11 @@ module Types
       field :selective_sync_shards, type: [GraphQL::STRING_TYPE], null: true, description: 'The repository storages whose projects should be synced, if `selective_sync_type` == `shards`'
       field :selective_sync_namespaces, ::Types::NamespaceType.connection_type, null: true, method: :namespaces, description: 'The namespaces that should be synced, if `selective_sync_type` == `namespaces`'
       field :minimum_reverification_interval, GraphQL::INT_TYPE, null: true, description: 'The interval (in days) in which the repository verification is valid. Once expired, it will be reverified'
+      field :merge_request_diff_registries, ::Types::Geo::MergeRequestDiffRegistryType.connection_type,
+            null: true,
+            resolver: ::Resolvers::Geo::MergeRequestDiffRegistriesResolver,
+            description: 'Find merge request diff registries on this Geo node',
+            feature_flag: :geo_merge_request_diff_replication
       field :package_file_registries, ::Types::Geo::PackageFileRegistryType.connection_type,
             null: true,
             resolver: ::Resolvers::Geo::PackageFileRegistriesResolver,
