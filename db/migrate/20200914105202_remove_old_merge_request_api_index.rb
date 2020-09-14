57c47fc0441d64f49cd6bc8b4f56581649783006
Robert May <rmay@gitlab.com>
2020-09-14 13:40:51 +0100

    Add index names to migrations

--- db/migrate/20200914105202_remove_old_merge_request_api_index.rb
+++ db/migrate/20200914105202_remove_old_merge_request_api_index.rb
@@ -8,10 +8,10 @@ class RemoveOldMergeRequestApiIndex < ActiveRecord::Migration[6.0]
   disable_ddl_transaction!
 
   def up
-    remove_concurrent_index :merge_requests, [:target_project_id, :created_at]
+    remove_concurrent_index :merge_requests, [:target_project_id, :created_at], name: :index_merge_requests_target_project_id_created_at
   end
 
   def down
-    add_concurrent_index :merge_requests, [:target_project_id, :created_at]
+    add_concurrent_index :merge_requests, [:target_project_id, :created_at], name: :index_merge_requests_target_project_id_created_at
   end
 end
