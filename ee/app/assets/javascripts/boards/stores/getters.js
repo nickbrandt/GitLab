d059366e325b9954ada5fa1c20846f6ae58b1451
Florie Guibert <fguibert@gitlab.com>
2020-09-14 07:31:36 +0000

    Boards refactor - Fetch issues with GraphQL
    Fetch issues per list with VueX action

--- ee/app/assets/javascripts/boards/stores/getters.js
+++ ee/app/assets/javascripts/boards/stores/getters.js
@@ -3,11 +3,6 @@ import gettersCE from '~/boards/stores/getters';
 export default {
   ...gettersCE,
 
-  getIssues: (state, getters) => listId => {
-    const listIssueIds = state.issuesByListId[listId] || [];
-
-    return listIssueIds.map(id => getters.getIssueById(id));
-  },
   getIssuesByEpic: (state, getters) => (listId, epicId) => {
     return getters.getIssues(listId).filter(issue => issue.epic && issue.epic.id === epicId);
   },
