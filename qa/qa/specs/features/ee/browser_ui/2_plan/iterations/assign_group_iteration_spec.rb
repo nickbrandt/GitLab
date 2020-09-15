61d27f921cf029fcd10b6ae668a8a3a2457222f1
Desiree Chevalier <dchevalier@gitlab.com>
2020-09-15 13:03:09 -0400

    Add GraphQL ability for qa resources
    Adds ability to create qa resources with graphql.
    Updates iteration resource.

--- qa/qa/specs/features/ee/browser_ui/2_plan/iterations/assign_group_iteration_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/2_plan/iterations/assign_group_iteration_spec.rb
@@ -3,9 +3,11 @@
 module QA
   RSpec.describe 'Plan' do
     describe 'Assign Iterations' do
+      let!(:iteration) { EE::Resource::GroupIteration.fabricate_via_api! }
+
       let(:project) do
         Resource::Project.fabricate_via_api! do |project|
-          project.group = @iteration.group
+          project.group = iteration.group
           project.name = "project-to-test-iterations-#{SecureRandom.hex(8)}"
         end
       end
@@ -22,14 +24,12 @@ module QA
       end
 
       it 'assigns a group iteration to an existing issue', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/958' do
-        @iteration = EE::Resource::GroupIteration.fabricate_via_browser_ui!
-
         issue.visit!
 
         Page::Project::Issue::Show.perform do |issue|
-          issue.assign_iteration(@iteration)
+          issue.assign_iteration(iteration)
 
-          expect(issue).to have_iteration(@iteration.title)
+          expect(issue).to have_iteration(iteration.title)
         end
       end
     end
