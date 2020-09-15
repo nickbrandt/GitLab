d2c573c3e46040f0235f5baa46dfbc941fc575f6
Scott Stern <sstern@gitlab.com>
2020-09-15 08:46:33 +0000

    Move issuable title to component

--- ee/app/assets/javascripts/boards/components/board_content_sidebar.vue
+++ ee/app/assets/javascripts/boards/components/board_content_sidebar.vue
@@ -4,21 +4,20 @@ import { GlDrawer } from '@gitlab/ui';
 import { ISSUABLE } from '~/boards/constants';
 import { contentTop } from '~/lib/utils/common_utils';
 import IssuableAssignees from '~/sidebar/components/assignees/issuable_assignees.vue';
+import IssuableTitle from '~/boards/components/issuable_title.vue';
 
 export default {
   headerHeight: `${contentTop()}px`,
   components: {
     IssuableAssignees,
     GlDrawer,
+    IssuableTitle,
   },
   computed: {
     ...mapGetters(['isSidebarOpen', 'getActiveIssue']),
     ...mapState(['sidebarType']),
     showSidebar() {
       return this.sidebarType === ISSUABLE;
-    },
-    issueTitle() {
-      return this.getActiveIssue.title;
     },
   },
   methods: {
@@ -35,10 +34,7 @@ export default {
     @close="unsetActiveId"
   >
     <template #header>
-      <div data-testid="issue-title">
-        <p class="gl-font-weight-bold">{{ issueTitle }}</p>
-        <p class="gl-mb-0">{{ getActiveIssue.referencePath }}</p>
-      </div>
+      <issuable-title :ref-path="getActiveIssue.referencePath" :title="getActiveIssue.title" />
     </template>
 
     <template>
