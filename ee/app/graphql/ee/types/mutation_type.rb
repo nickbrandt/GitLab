e61fe76ce1a874dfe748b5803332527564181fd1
Alan (Maciej) Paruszewski <mparuszewski@gitlab.com>
2020-09-18 15:05:35 +0000

    Add mutation to Confirm Vulnerability in GraphQL
    This change adds new mutation that allows user to confirm vulnerability
    using GraphQL API.

--- ee/app/graphql/ee/types/mutation_type.rb
+++ ee/app/graphql/ee/types/mutation_type.rb
@@ -24,6 +24,7 @@ module EE
         mount_mutation ::Mutations::RequirementsManagement::UpdateRequirement
         mount_mutation ::Mutations::Vulnerabilities::Dismiss
         mount_mutation ::Mutations::Vulnerabilities::Resolve
+        mount_mutation ::Mutations::Vulnerabilities::Confirm
         mount_mutation ::Mutations::Boards::Update
         mount_mutation ::Mutations::Boards::Lists::UpdateLimitMetrics
         mount_mutation ::Mutations::InstanceSecurityDashboard::AddProject
