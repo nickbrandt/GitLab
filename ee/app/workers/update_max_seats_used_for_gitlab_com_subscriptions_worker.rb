760808d9ee89ce2cacae565ab8a613a0bdf06b59
Alex Pooley <apooley@gitlab.com>
2020-09-21 12:00:58 +0800

    wip

--- ee/app/workers/update_max_seats_used_for_gitlab_com_subscriptions_worker.rb
+++ ee/app/workers/update_max_seats_used_for_gitlab_com_subscriptions_worker.rb
@@ -23,19 +23,15 @@ class UpdateMaxSeatsUsedForGitlabComSubscriptionsWorker # rubocop:disable Scalab
 
         subscription.refresh_seat_attributes!
 
-        tuples << [subscription.id, subscription.max_seats_used, subscription.seats_in_use, subscription.seats_owed]
+        tuples << {
+          id: subscription.id,
+          max_seats_used: subscription.max_seats_used,
+          seats_in_use: subscription.seats_in_use,
+          seats_owed: subscription.seats_owed
+        }
       end
 
-      if tuples.present?
-        GitlabSubscription.connection.execute <<-EOF
-          UPDATE gitlab_subscriptions AS s
-          SET max_seats_used = v.max_seats_used,
-              seats_in_use = v.seats_in_use,
-              seats_owed = v.seats_owed
-          FROM (VALUES #{tuples.map { |tuple| "(#{tuple.join(', ')})" }.join(', ')}) AS v(id, max_seats_used, seats_in_use, seats_owed)
-          WHERE s.id = v.id
-        EOF
-      end
+      GitlabSubscription.upsert_all(tuples, returning: false, unique_by: :id) if tuples.present?
     end
   end
 
