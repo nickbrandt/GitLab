45b1b6a1f553ca6c0f65318d99645ee59337308e
Arturo Herrero <arturo.herrero@gmail.com>
2020-09-21 19:48:21 +0100

    Remove unnecessary retrieving of IDs

--- app/services/bulk_update_integration_service.rb
+++ app/services/bulk_update_integration_service.rb
@@ -8,16 +8,11 @@ class BulkUpdateIntegrationService
 
   # rubocop: disable CodeReuse/ActiveRecord
   def execute
-    # Retrieving the IDs instantiates the ActiveRecord relation (batch)
-    # into concrete models, otherwise update_all will clear the relation.
-    # https://stackoverflow.com/q/34811646/462015
-    batch_ids = batch.pluck(:id)
-
     Service.transaction do
       batch.update_all(service_hash)
 
       if integration.data_fields_present?
-        integration.data_fields.class.where(service_id: batch_ids).update_all(data_fields_hash)
+        integration.data_fields.class.where(service_id: batch.select(:id)).update_all(data_fields_hash)
       end
     end
   end
