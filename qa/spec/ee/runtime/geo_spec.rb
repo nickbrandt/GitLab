5b9efb2a606cee545583c49c5c02cb61a8e03ca0
ddavison <ddavison@gitlab.com>
2020-09-23 15:05:47 -0400

    Update outer describe block for internal QA tests 1/2

--- qa/spec/ee/runtime/geo_spec.rb
+++ qa/spec/ee/runtime/geo_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-describe ::QA::EE::Runtime::Geo do
+RSpec.describe ::QA::EE::Runtime::Geo do
   describe '.max_db_replication_time' do
     subject { described_class.max_db_replication_time }
 
