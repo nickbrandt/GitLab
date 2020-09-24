d90de5823c2ec439a7f66ebe777f4b6110182790
Alan (Maciej) Paruszewski <mparuszewski@gitlab.com>
2020-09-24 10:46:45 +0000

    Add filtering vulnerabilities visible by user in Vulnerabilities Finder
    This change extends VulnerabilitiesFinder to accept current_user
    parameter that is used in FinderMethods to filter only vulnerabilities
    that are accessible by the user.

---
+++ ee/app/finders/autocomplete/vulnerabilities_autocomplete_finder.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Autocomplete
+  class VulnerabilitiesAutocompleteFinder
+    attr_reader :current_user, :vulnerable, :params
+
+    # current_user - the User object of the user that wants to view the list of Vulnerabilities
+    #
+    # vulnerable - any object that has a #vulnerabilities method that returns a collection of vulnerabilitie
+    # params - a Hash containing additional parameters to set
+    #
+    # The supported parameters are those supported by
+    # `Security::VulnerabilitiesFinder`.
+    def initialize(current_user, vulnerable, params = {})
+      @current_user = current_user
+      @vulnerable = vulnerable
+      @params = params
+    end
+
+    DEFAULT_AUTOCOMPLETE_LIMIT = 5
+
+    def execute
+      return [] unless vulnerable.feature_available?(:security_dashboard)
+
+      ::Security::VulnerabilitiesFinder # rubocop: disable CodeReuse/Finder
+        .new(vulnerable, params)
+        .execute
+        .autocomplete_search(params[:search].to_s)
+        .with_limit(DEFAULT_AUTOCOMPLETE_LIMIT)
+        .order_id_desc
+        .visible_to_user_and_access_level(current_user, ::Gitlab::Access::DEVELOPER)
+    end
+  end
+end
