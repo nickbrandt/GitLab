a578ffb7ac0a9ab843a34d1462ce41546abf5000
Sean Carroll <scarroll@gitlab.com>
2020-09-24 12:51:45 +0200

    Associate project releases with group milestones
    Closes https://gitlab.com/gitlab-org/gitlab/-/issues/235387
    
    See merge request https://gitlab.com/gitlab-org/gitlab/-/merge_requests/42455

--- ee/app/models/ee/release.rb
+++ ee/app/models/ee/release.rb
@@ -8,6 +8,9 @@ module EE
       include UsageStatistics
 
       belongs_to :milestone, optional: true
+
+      scope :project_releases, -> { where.not(project_id: nil) }
+      scope :milestone_releases, -> { where(project_id: nil) }
     end
   end
 end
--- ee/spec/models/milestone_spec.rb
+++ ee/spec/models/milestone_spec.rb
@@ -6,4 +6,30 @@ RSpec.describe Milestone do
   describe "Associations" do
     it { is_expected.to have_many(:boards) }
   end
+
+  context 'group milestone releases' do
+    let(:user) { create(:user) }
+    let(:issue) { create(:issue, project: project) }
+    let(:project) { create(:project, :public) }
+    let(:milestone) { build(:milestone, project: project) }
+
+    before do
+      allow(subject).to receive(:set_iid).and_return(false)
+    end
+
+    context 'when it is tied to a release for another project' do
+      it 'creates a validation error' do
+        other_project = create(:project)
+        milestone.releases << build(:release, project: other_project)
+        expect(milestone).not_to be_valid
+      end
+    end
+
+    context 'when it is tied to a release for the same project' do
+      it 'is valid' do
+        milestone.releases << build(:release, project: project)
+        expect(milestone).to be_valid
+      end
+    end
+  end
 end
