8caba214af6926e2a12e4f6231f4cd2bf5d476e5
Aishwarya Subramanian <asubramanian@gitlab.com>
2020-09-29 15:10:08 -0500

    Removed usages of gma_member_cleanup flag
    The gma_member_cleanup flag was never enabled on
    production and staging ever since it was introduced
    This MR removes the flag as well as the code related
    to removal of group members on GMA conversion.

--- ee/app/services/group_saml/saml_provider/base_service.rb
+++ ee/app/services/group_saml/saml_provider/base_service.rb
@@ -23,7 +23,6 @@ module GroupSaml
 
           if updated && saml_provider.enforced_group_managed_accounts? && !group_managed_accounts_was_enforced
             require_linked_saml_to_enable_group_managed!
-            cleanup_members! if ::Feature.enabled?(:gma_member_cleanup)
           end
         end
       end
@@ -34,12 +33,6 @@ module GroupSaml
         return if saml_provider.identities.for_user(current_user).exists?
 
         add_error!(_("Group Owner must have signed in with SAML before enabling Group Managed Accounts"))
-      end
-
-      def cleanup_members!
-        return if GroupManagedAccounts::CleanUpMembersService.new(current_user, group).execute
-
-        add_error!(_("Can't remove group members without group managed account"))
       end
 
       def add_error!(message)
