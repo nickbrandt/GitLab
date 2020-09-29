mehulagg/gitlab_test

Adds package event tracking
Adds package event model
and extend package event
tracking to support
existing tracking calls.

app/controllers/groups/registry/repositories_controller.rb
@@ -2,6 +2,8 @@
 module Groups
   module Registry
     class RepositoriesController < Groups::ApplicationController
+      include PackagesHelper
+
       before_action :verify_container_registry_enabled!
       before_action :authorize_read_container_image!
 
@@ -13,7 +15,7 @@ module Groups
                                                  .execute
                                                  .with_api_entity_associations
 
-            track_event(:list_repositories)
+            package_event(:list_repositories, :container)
 
             serializer = ContainerRepositoriesSerializer
               .new(current_user: current_user)

lib/api/composer_packages.rb
@@ -123,7 +123,7 @@ module API
             bad_request!
           end
 
-          package_event('push_package')
+          package_event('push_package', :composer)
 
           ::Packages::Composer::CreatePackageService
             .new(authorized_user_project, current_user, declared_params)

lib/api/conan_package_endpoints.rb
@@ -246,7 +246,7 @@ module API
         delete do
           authorize!(:destroy_package, project)
 
-          package_event('delete_package', category: 'API::ConanPackages')
+          package_event('delete_package', :conan, category: 'API::ConanPackages')
 
           package.destroy
         end

lib/api/group_container_repositories.rb
@@ -4,6 +4,8 @@ module API
   class GroupContainerRepositories < Grape::API::Instance
     include PaginationParams
 
+    helpers ::API::Helpers::PackagesHelpers
+
     before { authorize_read_group_container_images! }
 
     REPOSITORY_ENDPOINT_REQUIREMENTS = API::NAMESPACE_OR_PROJECT_REQUIREMENTS.merge(
@@ -27,7 +29,7 @@ module API
           user: current_user, subject: user_group
         ).execute
 
-        track_event('list_repositories')
+        package_event('list_repositories', :container)
 
         present paginate(repositories), with: Entities::ContainerRegistry::Repository, tags: params[:tags], tags_count: params[:tags_count]
       end

lib/api/npm_packages.rb
@@ -141,7 +141,7 @@ module API
         package_file = ::Packages::PackageFileFinder
           .new(package, params[:file_name]).execute!
 
-        package_event('pull_package')
+        package_event('pull_package', package)
 
         present_carrierwave_file!(package_file.file)
       end
@@ -157,7 +157,7 @@ module API
       put ':id/packages/npm/:package_name', requirements: NPM_ENDPOINT_REQUIREMENTS do
         authorize_create_package!(user_project)
 
-        package_event('push_package')
+        package_event('push_package', :npm)
 
         created_package = ::Packages::Npm::CreatePackageService
           .new(user_project, current_user, params.merge(build: current_authenticated_job)).execute

lib/api/project_container_repositories.rb
@@ -3,6 +3,7 @@
 module API
   class ProjectContainerRepositories < Grape::API::Instance
     include PaginationParams
+    helpers ::API::Helpers::PackagesHelpers
 
     REPOSITORY_ENDPOINT_REQUIREMENTS = API::NAMESPACE_OR_PROJECT_REQUIREMENTS.merge(
       tag_name: API::NO_SLASH_URL_PART_REGEX)
@@ -28,7 +29,7 @@ module API
           user: current_user, subject: user_project
         ).execute
 
-        track_event( 'list_repositories')
+        package_event('list_repositories', :container)
 
         present paginate(repositories), with: Entities::ContainerRegistry::Repository, tags: params[:tags], tags_count: params[:tags_count]
       end
@@ -43,7 +44,7 @@ module API
         authorize_admin_container_image!
 
         DeleteContainerRepositoryWorker.perform_async(current_user.id, repository.id) # rubocop:disable CodeReuse/Worker
-        track_event('delete_repository')
+        package_event('delete_repository', :container)
 
         status :accepted
       end
@@ -60,7 +61,7 @@ module API
         authorize_read_container_image!
 
         tags = Kaminari.paginate_array(repository.tags)
-        track_event('list_tags')
+        package_event('list_tags', :container)
 
         present paginate(tags), with: Entities::ContainerRegistry::Tag
       end
@@ -89,7 +90,7 @@ module API
           declared_params.except(:repository_id).merge(container_expiration_policy: false))
         # rubocop:enable CodeReuse/Worker
 
-        track_event('delete_tag_bulk')
+        package_event('delete_tag_bulk', :container)
 
         status :accepted
       end
@@ -125,7 +126,7 @@ module API
           .execute(repository)
 
         if result[:status] == :success
-          track_event('delete_tag')
+          package_event('delete_tag', :container)
 
           status :ok
         else

lib/api/pypi_packages.rb
@@ -72,7 +72,7 @@ module API
           package = packages_finder(project).by_file_name_and_sha256(filename, params[:sha256])
           package_file = ::Packages::PackageFileFinder.new(package, filename, with_file_name_like: false).execute
 
-          package_event('pull_package')
+          package_event('pull_package', :pypi)
 
           present_carrierwave_file!(package_file.file, supports_direct_download: true)
         end
@@ -91,7 +91,7 @@ module API
         get 'simple/*package_name', format: :txt do
           authorize_read_package!(authorized_user_project)
 
-          package_event('list_package')
+          package_event('list_package', :pypi)
 
           packages = find_package_versions
           presenter = ::Packages::Pypi::PackagePresenter.new(packages, authorized_user_project)
@@ -122,7 +122,7 @@ module API
           authorize_upload!(authorized_user_project)
           bad_request!('File is too large') if authorized_user_project.actual_limits.exceeded?(:pypi_max_file_size, params[:content].size)
 
-          package_event('push_package')
+          package_event('push_package', :pypi)
 
           ::Packages::Pypi::CreatePackageService
             .new(authorized_user_project, current_user, declared_params)

nickbrandt/GitLab

app/controllers/groups/registry/repositories_controller.rb
@@ -2,6 +2,8 @@
 module Groups
   module Registry
     class RepositoriesController < Groups::ApplicationController
+      include PackagesHelper
+
       before_action :verify_container_registry_enabled!
       before_action :authorize_read_container_image!
 
@@ -13,7 +15,7 @@ module Groups
                                                  .execute
                                                  .with_api_entity_associations
 
-            track_event(:list_repositories)
+            track_package_event(:list_repositories, :container)
 
             serializer = ContainerRepositoriesSerializer
               .new(current_user: current_user)

lib/api/composer_packages.rb
@@ -123,7 +123,7 @@ module API
             bad_request!
           end
 
-          package_event('push_package')
+          track_package_event('push_package', :composer)
 
           ::Packages::Composer::CreatePackageService
             .new(authorized_user_project, current_user, declared_params)

lib/api/conan_package_endpoints.rb
@@ -246,7 +246,7 @@ module API
         delete do
           authorize!(:destroy_package, project)
 
-          package_event('delete_package', category: 'API::ConanPackages')
+          track_package_event('delete_package', :conan, category: 'API::ConanPackages')
 
           package.destroy
         end

lib/api/group_container_repositories.rb
@@ -4,6 +4,8 @@ module API
   class GroupContainerRepositories < Grape::API::Instance
     include PaginationParams
 
+    helpers ::API::Helpers::PackagesHelpers
+
     before { authorize_read_group_container_images! }
 
     REPOSITORY_ENDPOINT_REQUIREMENTS = API::NAMESPACE_OR_PROJECT_REQUIREMENTS.merge(
@@ -27,7 +29,7 @@ module API
           user: current_user, subject: user_group
         ).execute
 
-        track_event('list_repositories')
+        track_package_event('list_repositories', :container)
 
         present paginate(repositories), with: Entities::ContainerRegistry::Repository, tags: params[:tags], tags_count: params[:tags_count]
       end

lib/api/npm_packages.rb
@@ -141,7 +141,7 @@ module API
         package_file = ::Packages::PackageFileFinder
           .new(package, params[:file_name]).execute!
 
-        package_event('pull_package')
+        track_package_event('pull_package', package)
 
         present_carrierwave_file!(package_file.file)
       end
@@ -157,7 +157,7 @@ module API
       put ':id/packages/npm/:package_name', requirements: NPM_ENDPOINT_REQUIREMENTS do
         authorize_create_package!(user_project)
 
-        package_event('push_package')
+        track_package_event('push_package', :npm)
 
         created_package = ::Packages::Npm::CreatePackageService
           .new(user_project, current_user, params.merge(build: current_authenticated_job)).execute

lib/api/project_container_repositories.rb
@@ -3,6 +3,7 @@
 module API
   class ProjectContainerRepositories < Grape::API::Instance
     include PaginationParams
+    helpers ::API::Helpers::PackagesHelpers
 
     REPOSITORY_ENDPOINT_REQUIREMENTS = API::NAMESPACE_OR_PROJECT_REQUIREMENTS.merge(
       tag_name: API::NO_SLASH_URL_PART_REGEX)
@@ -28,7 +29,7 @@ module API
           user: current_user, subject: user_project
         ).execute
 
-        track_event( 'list_repositories')
+        track_package_event('list_repositories', :container)
 
         present paginate(repositories), with: Entities::ContainerRegistry::Repository, tags: params[:tags], tags_count: params[:tags_count]
       end
@@ -43,7 +44,7 @@ module API
         authorize_admin_container_image!
 
         DeleteContainerRepositoryWorker.perform_async(current_user.id, repository.id) # rubocop:disable CodeReuse/Worker
-        track_event('delete_repository')
+        track_package_event('delete_repository', :container)
 
         status :accepted
       end
@@ -60,7 +61,7 @@ module API
         authorize_read_container_image!
 
         tags = Kaminari.paginate_array(repository.tags)
-        track_event('list_tags')
+        track_package_event('list_tags', :container)
 
         present paginate(tags), with: Entities::ContainerRegistry::Tag
       end
@@ -89,7 +90,7 @@ module API
           declared_params.except(:repository_id).merge(container_expiration_policy: false))
         # rubocop:enable CodeReuse/Worker
 
-        track_event('delete_tag_bulk')
+        track_package_event('delete_tag_bulk', :container)
 
         status :accepted
       end
@@ -125,7 +126,7 @@ module API
           .execute(repository)
 
         if result[:status] == :success
-          track_event('delete_tag')
+          track_package_event('delete_tag', :container)
 
           status :ok
         else

lib/api/pypi_packages.rb
@@ -72,7 +72,7 @@ module API
           package = packages_finder(project).by_file_name_and_sha256(filename, params[:sha256])
           package_file = ::Packages::PackageFileFinder.new(package, filename, with_file_name_like: false).execute
 
-          package_event('pull_package')
+          track_package_event('pull_package', :pypi)
 
           present_carrierwave_file!(package_file.file, supports_direct_download: true)
         end
@@ -91,7 +91,7 @@ module API
         get 'simple/*package_name', format: :txt do
           authorize_read_package!(authorized_user_project)
 
-          package_event('list_package')
+          track_package_event('list_package', :pypi)
 
           packages = find_package_versions
           presenter = ::Packages::Pypi::PackagePresenter.new(packages, authorized_user_project)
@@ -122,7 +122,7 @@ module API
           authorize_upload!(authorized_user_project)
           bad_request!('File is too large') if authorized_user_project.actual_limits.exceeded?(:pypi_max_file_size, params[:content].size)
 
-          package_event('push_package')
+          track_package_event('push_package', :pypi)
 
           ::Packages::Pypi::CreatePackageService
             .new(authorized_user_project, current_user, declared_params)