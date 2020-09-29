465eccc01d32dbc1dc9f3bf443cfc19a862e9654
Alex Pooley <apooley@gitlab.com>
2020-09-30 07:50:05 +0800

    Limit invitations to groups and projects
    Free plan on .com will limit invites to 20 per day.

---
+++ ee/spec/services/ee/members/create_service_spec.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Members::CreateService do
+  let_it_be(:user) { create(:user) }
+  let_it_be(:root_ancestor) { create(:group) }
+  let_it_be(:project, reload: true) { create(:project, group: root_ancestor) }
+  let_it_be(:subgroup) { create(:group, parent: root_ancestor) }
+  let_it_be(:subgroup_project) { create(:project, group: subgroup) }
+  let_it_be(:project_users) { create_list(:user, 2) }
+
+  let(:params) { { user_ids: project_users.map(&:id).join(','), access_level: Gitlab::Access::GUEST } }
+
+  subject { described_class.new(user, params).execute(project) }
+
+  before_all do
+    project.add_maintainer(user)
+
+    create(:project_member, :invited, project: subgroup_project, created_at: 2.days.ago)
+    create(:project_member, :invited, project: subgroup_project)
+    create(:group_member, :invited, group: subgroup, created_at: 2.days.ago)
+    create(:group_member, :invited, group: subgroup)
+  end
+
+  context 'with group plan' do
+    let(:plan_limits) { create(:plan_limits, daily_invites: daily_invites) }
+    let(:plan) { create(:plan, limits: plan_limits) }
+    let!(:subscription) do
+      create(
+        :gitlab_subscription,
+        namespace: root_ancestor,
+        hosted_plan: plan
+      )
+    end
+
+    shared_examples 'quota limit exceeded' do |limit|
+      it { expect(subject).to include(status: :error, message: "Invite limit of #{limit} per day exceeded") }
+      it { expect { subject }.not_to change { Member.count } }
+    end
+
+    context 'already exceeded invite quota limit' do
+      let(:daily_invites) { 2 }
+
+      it_behaves_like 'quota limit exceeded', 2
+    end
+
+    context 'will exceed invite quota limit' do
+      let(:daily_invites) { 3 }
+
+      it_behaves_like 'quota limit exceeded', 3
+    end
+
+    context 'within invite quota limit' do
+      let(:daily_invites) { 5 }
+
+      it { expect(subject).to eq({ status: :success }) }
+      it do
+        subject
+        expect(project.users).to include(*project_users)
+      end
+    end
+
+    context 'infinite invite quota limit' do
+      let(:daily_invites) { 0 }
+
+      it { expect(subject).to eq({ status: :success }) }
+      it do
+        subject
+        expect(project.users).to include(*project_users)
+      end
+    end
+  end
+
+  context 'without a plan' do
+    let(:plan) { nil }
+
+    it { expect(subject).to eq({ status: :success }) }
+    it do
+      subject
+      expect(project.users).to include(*project_users)
+    end
+  end
+end
