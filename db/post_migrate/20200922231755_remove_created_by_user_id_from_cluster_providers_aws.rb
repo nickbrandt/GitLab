138d6f7cadd6f36a1e6517d19627110af7b2e566
Toon Claes <toon@gitlab.com>
2020-09-30 12:00:48 +0200

    Include index name when reverting migration

--- db/post_migrate/20200922231755_remove_created_by_user_id_from_cluster_providers_aws.rb
+++ db/post_migrate/20200922231755_remove_created_by_user_id_from_cluster_providers_aws.rb
@@ -4,6 +4,7 @@ class RemoveCreatedByUserIdFromClusterProvidersAws < ActiveRecord::Migration[6.0
   include Gitlab::Database::MigrationHelpers
 
   DOWNTIME = false
+  INDEX_NAME = 'index_cluster_providers_aws_on_created_by_user_id'
 
   disable_ddl_transaction!
 
@@ -18,7 +19,7 @@ class RemoveCreatedByUserIdFromClusterProvidersAws < ActiveRecord::Migration[6.0
       add_column :cluster_providers_aws, :created_by_user_id, :integer
     end
 
-    add_concurrent_index :cluster_providers_aws, :created_by_user_id
+    add_concurrent_index :cluster_providers_aws, :created_by_user_id, name: INDEX_NAME
 
     add_concurrent_foreign_key :cluster_providers_aws, :users, column: :created_by_user_id, on_delete: :nullify
   end
