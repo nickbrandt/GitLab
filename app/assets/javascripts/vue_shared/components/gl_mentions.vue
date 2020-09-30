2b7190224864ad111ce1a8c98e2f813e3232453b
Heinrich Lee Yu <heinrich@gitlab.com>
2020-09-30 12:07:27 +0800

    Revert "Include assigned users in /reassign autocomplete"
    This reverts commit 6e1cca984804c198a61ecdf5b1965cc62deac9aa.

--- app/assets/javascripts/vue_shared/components/gl_mentions.vue
+++ app/assets/javascripts/vue_shared/components/gl_mentions.vue
@@ -65,13 +65,16 @@ const autoCompleteMap = {
           SidebarMediator.singleton?.store?.assignees?.map(assignee => assignee.username) || [];
       }
 
-      if (doesCurrentLineStartWith('/assign', fullText, selectionStart)) {
+      if (
+        doesCurrentLineStartWith('/assign', fullText, selectionStart) ||
+        doesCurrentLineStartWith('/reassign', fullText, selectionStart)
+      ) {
         return this.members.filter(
           member => member.type === 'User' && !this.assignees.includes(member.username),
         );
-      } else if (doesCurrentLineStartWith('/reassign', fullText, selectionStart)) {
-        return this.members.filter(member => member.type === 'User');
-      } else if (doesCurrentLineStartWith('/unassign', fullText, selectionStart)) {
+      }
+
+      if (doesCurrentLineStartWith('/unassign', fullText, selectionStart)) {
         return this.members.filter(member => this.assignees.includes(member.username));
       }
 
