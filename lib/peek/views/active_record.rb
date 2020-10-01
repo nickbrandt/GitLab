7ed0806a8432c15ce13f713f45cc479091e9f52a
nmilojevic1 <nmilojevic@gitlab.com>
2020-10-01 16:17:47 +0200

    Add cached queries to the performance bar

--- lib/peek/views/active_record.rb
+++ lib/peek/views/active_record.rb
@@ -5,6 +5,7 @@ module Peek
     class ActiveRecord < DetailedView
       DEFAULT_THRESHOLDS = {
         calls: 100,
+        cached_calls: 100,
         duration: 3000,
         individual_call: 1000
       }.freeze
@@ -12,10 +13,15 @@ module Peek
       THRESHOLDS = {
         production: {
           calls: 100,
+          cached_calls: 100,
           duration: 15000,
           individual_call: 5000
         }
       }.freeze
+
+      def results
+        super.merge(calls: detailed_calls)
+      end
 
       def self.thresholds
         @thresholds ||= THRESHOLDS.fetch(Rails.env.to_sym, DEFAULT_THRESHOLDS)
@@ -23,18 +29,37 @@ module Peek
 
       private
 
+      def detailed_calls
+        cached_calls? ? "#{calls} | #{cached_calls}" : calls
+      end
+
+      def calls
+        super - cached_calls
+      end
+
+      def cached_calls
+        detail_store.count { |item| item[:cached] == 'cached' }
+      end
+
+      def cached_calls?
+        detail_store.any? { |item| item[:cached] == 'cached' }
+      end
+
+      def warnings
+        (super + warning_for(cached_calls, self.class.thresholds[:cached_calls], label: "#{key} cached calls")).compact
+      end
+
       def setup_subscribers
         super
 
         subscribe('sql.active_record') do |_, start, finish, _, data|
           if Gitlab::PerformanceBar.enabled_for_request?
-            unless data[:cached]
-              detail_store << {
-                duration: finish - start,
-                sql: data[:sql].strip,
-                backtrace: Gitlab::BacktraceCleaner.clean_backtrace(caller)
-              }
-            end
+            detail_store << {
+              duration: finish - start,
+              sql: data[:sql].strip,
+              backtrace: Gitlab::BacktraceCleaner.clean_backtrace(caller),
+              cached: data[:cached] ? 'cached' : ''
+            }
           end
         end
       end
