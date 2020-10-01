2833223bbbdb7d60f4a1a7530f418d8c9cae18be
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2020-10-01 16:22:58 -0300

    Move Git access class to replicator
    Each repository type has its own Git access class
    with its specific error messages.

--- ee/app/replicators/geo/snippet_repository_replicator.rb
+++ ee/app/replicators/geo/snippet_repository_replicator.rb
@@ -8,23 +8,16 @@ module Geo
       ::SnippetRepository
     end
 
+    def self.git_access_class
+      ::Gitlab::GitAccessSnippet
+    end
+
     def self.replication_enabled_by_default?
       false
     end
 
     def needs_checksum?
       false
-    end
-
-    def repository
-      model_record.repository
-    end
-
-    # Once https://gitlab.com/gitlab-org/gitlab/-/issues/213021 is fixed
-    # this method can be removed
-    def remote_url
-      url = Gitlab::Geo.primary_node.repository_url(repository)
-      url.sub('@snippets', 'snippets')
     end
 
     # Once https://gitlab.com/gitlab-org/gitlab/-/issues/213021 is fixed
@@ -35,6 +28,17 @@ module Geo
       ).authorization
 
       { "http.#{remote_url}.extraHeader" => "Authorization: #{authorization}" }
+    end
+
+    # Once https://gitlab.com/gitlab-org/gitlab/-/issues/213021 is fixed
+    # this method can be removed
+    def remote_url
+      url = Gitlab::Geo.primary_node.repository_url(repository)
+      url.sub('@snippets', 'snippets')
+    end
+
+    def repository
+      model_record.repository
     end
   end
 end
