ae91647682b9ded5e24249d6d3ab6dd9c4d2ac44
Sean McGivern <sean@gitlab.com>
2020-10-02 13:33:01 +0100

    Add feature categories to controllers beginning with T

--- ee/app/controllers/trials_controller.rb
+++ ee/app/controllers/trials_controller.rb
@@ -10,6 +10,8 @@ class TrialsController < ApplicationController
   before_action :find_or_create_namespace, only: :apply
   before_action :record_user_for_group_only_trials_experiment, only: :select
 
+  feature_category :purchase
+
   def new
   end
 
--- spec/controllers/every_controller_spec.rb
+++ spec/controllers/every_controller_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe "Every controller" do
         next unless controller.to_s.start_with?('B', 'C', 'D', 'E', 'F',
                                                 'H', 'I', 'J', 'K', 'L',
                                                 'M', 'N', 'O', 'Q', 'R',
-                                                'S',
+                                                'S', 'T',
                                                 'Projects::MergeRequestsController')
 
         "#{controller}##{action}"
