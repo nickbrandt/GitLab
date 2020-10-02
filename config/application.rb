f7e873c302013f2a278057caf998ff4670c3874a
Paul Slaughter <pslaughter@gitlab.com>
2020-10-02 09:56:54 -0500

    Move merge_conflicts to page specific styles
    Also moves color variables to css variables for
    dark mode support

--- config/application.rb
+++ config/application.rb
@@ -191,6 +191,7 @@ module Gitlab
     config.assets.precompile << "page_bundles/ide.css"
     config.assets.precompile << "page_bundles/issues.css"
     config.assets.precompile << "page_bundles/jira_connect.css"
+    config.assets.precompile << "page_bundles/merge_conflicts.css"
     config.assets.precompile << "page_bundles/milestone.css"
     config.assets.precompile << "page_bundles/todos.css"
     config.assets.precompile << "page_bundles/xterm.css"
