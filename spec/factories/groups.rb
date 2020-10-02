6d3752fe6fbc581c7cf511ba083277c3bcae1ba0
Małgorzata Ksionek <mksionek@gitlab.com>
2020-10-02 22:44:30 +0200

    Add namespace settings to groups factory

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -7,7 +7,6 @@ FactoryBot.define do
     type { 'Group' }
     owner { nil }
     project_creation_level { ::Gitlab::Access::MAINTAINER_PROJECT_ACCESS }
-    association :namespace_settings, factory: :namespace_settings
 
     after(:create) do |group|
       if group.owner
@@ -24,7 +23,7 @@ FactoryBot.define do
     end
 
     trait :internal do
-      visibility_level { Gitlab::VisibilityLevel::INTERNAL }
+      visibility_level {Gitlab::VisibilityLevel::INTERNAL }
     end
 
     trait :private do
