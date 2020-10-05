6a8ce2a1f454b88b100d6683310a6554cd4b490a
Kerri Miller <kerrizor@kerrizor.com>
2020-10-05 11:26:37 +0000

    Add default_branch_name column to namespace_settings table

--- db/structure.sql
+++ db/structure.sql
@@ -13562,7 +13562,9 @@ CREATE TABLE namespace_settings (
     updated_at timestamp with time zone NOT NULL,
     namespace_id integer NOT NULL,
     prevent_forking_outside_group boolean DEFAULT false NOT NULL,
-    allow_mfa_for_subgroups boolean DEFAULT true NOT NULL
+    allow_mfa_for_subgroups boolean DEFAULT true NOT NULL,
+    default_branch_name text,
+    CONSTRAINT check_0ba93c78c7 CHECK ((char_length(default_branch_name) <= 255))
 );
 
 CREATE TABLE namespace_statistics (
