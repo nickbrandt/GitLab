ab189d63a4dbb000f00e23b6b27c5549bd03867a
Mehmet Emin INAC <minac@gitlab.com>
2020-10-05 07:35:45 +0000

    Change the Security dashboard path

--- ee/config/routes/security.rb
+++ ee/config/routes/security.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 namespace :security do
-  root to: 'dashboard#show'
-  get 'dashboard/settings', to: 'dashboard#settings', as: :settings_dashboard
+  root to: redirect('-/security/dashboard')
 
+  resource :dashboard, only: [:show], controller: :dashboard do
+    get :settings
+  end
   resources :projects, only: [:index, :create, :destroy]
   resources :vulnerabilities, only: [:index]
 end
