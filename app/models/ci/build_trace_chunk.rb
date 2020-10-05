88e268687e238a0ed5a2a7e3338c4993dc6e0f2c
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2020-10-05 15:25:33 +0200

    Retry a build trace chunk migration in case of an exception
    This commit adds a retry logic to build chunks migration. In case of not
    being able to migrate a chunk, we want to retry this operation only
    once.

--- app/models/ci/build_trace_chunk.rb
+++ app/models/ci/build_trace_chunk.rb
@@ -126,12 +126,18 @@ module Ci
       Ci::BuildTraceChunkFlushWorker.perform_async(id)
     end
 
-    def persisted?
-      !redis?
-    end
-
-    def live?
-      redis?
+    ##
+    # It is possible that we run into two concurrent migrations. It might
+    # happen that a chunk gets migrated after being loaded by another worker
+    # but before the worker acquires a lock to perform the migration.
+    #
+    # We want to reset a chunk in that case and retry migration. If it fails
+    # again, we want to re-raise the exception.
+    #
+    def flush!
+      persist_data!
+    rescue FailedToPersistDataError
+      self.reset.persist_data!
     end
 
     ##
@@ -142,6 +148,14 @@ module Ci
     def final?
       build.pending_state.present? &&
         build.trace_chunks.maximum(:chunk_index).to_i == chunk_index
+    end
+
+    def persisted?
+      !redis?
+    end
+
+    def live?
+      redis?
     end
 
     def <=>(other)
