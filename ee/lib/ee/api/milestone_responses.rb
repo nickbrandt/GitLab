cb3bf327769469ecf05973a12966d51b3e4ce793
Heinrich Lee Yu <heinrich@gitlab.com>
2020-10-06 17:34:32 +0800

    Remove unused burnup events API endpoint
    This is still behind a feature flag and we switched to using a GraphQL
    endpoint that returns the data in a different format

--- ee/lib/ee/api/milestone_responses.rb
+++ ee/lib/ee/api/milestone_responses.rb
@@ -17,16 +17,6 @@ module EE
               render_api_error!("Milestone does not support burndown chart", 405)
             end
           end
-
-          def milestone_burnup_events_for(parent)
-            milestone = parent.milestones.find(params[:milestone_id])
-
-            if milestone.supports_milestone_charts?
-              present BurnupChartService.new(milestone: milestone, user: current_user).execute
-            else
-              render_api_error!("Milestone does not support burnup chart", 405)
-            end
-          end
         end
       end
     end
