568f91618c5c58f4cefb16ca77022af9d9ecc856
drew cimino <dcimino@gitlab.com>
2020-10-06 11:34:17 -0400

    Separate repository analytics and group coverage licensing

--- ee/app/controllers/groups/analytics/repository_analytics_controller.rb
+++ ee/app/controllers/groups/analytics/repository_analytics_controller.rb
@@ -4,7 +4,6 @@ class Groups::Analytics::RepositoryAnalyticsController < Groups::Analytics::Appl
   layout 'group'
 
   before_action :load_group
-  before_action :check_feature_flag
   before_action -> { check_feature_availability!(:group_repository_analytics) }
   before_action -> { authorize_view_by_action!(:read_group_repository_analytics) }
   before_action -> { push_frontend_feature_flag(:group_coverage_data_report, @group, default_enabled: false) }
@@ -20,9 +19,5 @@ class Groups::Analytics::RepositoryAnalyticsController < Groups::Analytics::Appl
       label: 'group_id',
       value: @group.id
     }
-  end
-
-  def check_feature_flag
-    render_404 unless @group.feature_available?(:group_coverage_reports)
   end
 end
