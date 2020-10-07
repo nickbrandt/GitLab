bb79463cfc2a40b8a3944ccbd039c8df0b88a08f
Jean du Plessis <jduplessis@gitlab.com>
2020-10-07 08:49:49 +0200

    Fix prettier formatting error

--- app/assets/javascripts/pages/projects/shared/permissions/components/project_feature_setting.vue
+++ app/assets/javascripts/pages/projects/shared/permissions/components/project_feature_setting.vue
@@ -70,7 +70,10 @@ export default {
 </script>
 
 <template>
-  <div :data-for="name" class="project-feature-controls gl-display-flex gl-align-items-center gl-my-3 gl-mx-0">
+  <div
+    :data-for="name"
+    class="project-feature-controls gl-display-flex gl-align-items-center gl-my-3 gl-mx-0"
+  >
     <input v-if="name" :name="name" :value="value" type="hidden" />
     <project-feature-toggle
       class="gl-flex-grow-0 gl-mr-3"
