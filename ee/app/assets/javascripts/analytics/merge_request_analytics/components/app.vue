a9610ba317593676ce63b49e08becaddae09e094
Brandon Labuschagne <blabuschagne@gitlab.com>
2020-10-07 12:43:46 +0000

    Add MR Analytics datepicker
    Add a date range picker to the MR Analytics page.
    Change the date range util to include start and end dates.

--- ee/app/assets/javascripts/analytics/merge_request_analytics/components/app.vue
+++ ee/app/assets/javascripts/analytics/merge_request_analytics/components/app.vue
@@ -1,5 +1,6 @@
 <script>
 import { getDateInPast } from '~/lib/utils/datetime_utility';
+import DateRange from '../../shared/components/daterange.vue';
 import { DEFAULT_NUMBER_OF_DAYS } from '../constants';
 import FilterBar from './filter_bar.vue';
 import ThroughputChart from './throughput_chart.vue';
@@ -8,6 +9,7 @@ import ThroughputTable from './throughput_table.vue';
 export default {
   name: 'MergeRequestAnalyticsApp',
   components: {
+    DateRange,
     FilterBar,
     ThroughputChart,
     ThroughputTable,
@@ -18,12 +20,28 @@ export default {
       endDate: new Date(),
     };
   },
+  methods: {
+    setDateRange({ startDate, endDate }) {
+      this.startDate = startDate;
+      this.endDate = endDate;
+    },
+  },
 };
 </script>
 <template>
   <div class="merge-request-analytics-wrapper">
     <h3 data-testid="pageTitle" class="gl-mb-5">{{ __('Merge Request Analytics') }}</h3>
-    <filter-bar />
+    <div
+      class="gl-display-flex gl-flex-direction-column gl-lg-flex-direction-row gl-justify-content-space-between gl-bg-gray-10 gl-border-t-solid gl-border-t-1 gl-border-t-gray-100 gl-border-b-solid gl-border-b-1 gl-border-b-gray-100 gl-py-3"
+    >
+      <filter-bar class="gl-flex-grow-1 gl-lg-ml-3" />
+      <date-range
+        :start-date="startDate"
+        :end-date="endDate"
+        class="gl-lg-mx-3"
+        @change="setDateRange"
+      />
+    </div>
     <throughput-chart :start-date="startDate" :end-date="endDate" />
     <throughput-table :start-date="startDate" :end-date="endDate" class="gl-mt-6" />
   </div>
