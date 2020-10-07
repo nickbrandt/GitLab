99e12104faf11c88647a2c1651738aa50321099b
Sean Arnold <sarnold@gitlab.com>
2020-10-07 17:35:03 +1300

    Add exceeded scope to Incident SLA

--- ee/app/models/issuable_sla.rb
+++ ee/app/models/issuable_sla.rb
@@ -3,4 +3,6 @@
 class IssuableSla < ApplicationRecord
   belongs_to :issue, optional: false
   validates :due_at, presence: true
+
+  scope :exceeded_for_issues, -> { joins(:issue).merge(Issue.opened).where('due_at < ?', Time.current) }
 end
--- ee/spec/models/issuable_sla_spec.rb
+++ ee/spec/models/issuable_sla_spec.rb
@@ -10,4 +10,31 @@ RSpec.describe IssuableSla do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:due_at) }
   end
+
+  describe 'scopes' do
+    describe '.exceeded_for_issues' do
+      subject { described_class.exceeded_for_issues }
+
+      let_it_be(:project) { create(:project) }
+      let!(:issuable_sla) { create(:issuable_sla, issue: issue, due_at: due_at) }
+      let(:due_at) { Time.current - 1.hour }
+      let(:issue) { create(:issue, project: project) }
+
+      context 'issue closed' do
+        let(:issue) { create(:issue, :closed, project: project) }
+
+        it { is_expected.to be_empty }
+      end
+
+      context 'issue opened' do
+        context 'due_at has not passed' do
+          let(:due_at) { Time.current + 1.hour }
+
+          it { is_expected.to be_empty }
+        end
+
+        it { is_expected.to contain_exactly(issuable_sla) }
+      end
+    end
+  end
 end
