a6bfb2510966fb8b51297032e5ef455fb9bd761f
Axel García <agarcia@gitlab.com>
2020-10-08 15:43:03 -0300

    Add labels selector to swimlanes sidebar
    It uses the labels-select component to fetch
    labels and handle the selection logic.

--- ee/app/assets/javascripts/boards/components/board_content_sidebar.vue
+++ ee/app/assets/javascripts/boards/components/board_content_sidebar.vue
@@ -8,6 +8,7 @@ import IssuableTitle from '~/boards/components/issuable_title.vue';
 import glFeatureFlagsMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
 import BoardSidebarEpicSelect from './sidebar/board_sidebar_epic_select.vue';
 import BoardSidebarWeightInput from './sidebar/board_sidebar_weight_input.vue';
+import BoardSidebarLabelsSelect from '~/boards/components/sidebar/board_sidebar_labels_select.vue';
 
 export default {
   headerHeight: `${contentTop()}px`,
@@ -17,6 +18,7 @@ export default {
     IssuableTitle,
     BoardSidebarEpicSelect,
     BoardSidebarWeightInput,
+    BoardSidebarLabelsSelect,
   },
   mixins: [glFeatureFlagsMixin()],
   computed: {
@@ -47,6 +49,7 @@ export default {
       <issuable-assignees :users="getActiveIssue.assignees" />
       <board-sidebar-epic-select />
       <board-sidebar-weight-input v-if="glFeatures.issueWeights" />
+      <board-sidebar-labels-select />
     </template>
   </gl-drawer>
 </template>
