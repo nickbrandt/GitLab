2b3b9dc2e972da95e2bdac646991975dc254e6f8
Vasilii Iakliushin <viakliushin@gitlab.com>
2020-10-10 16:04:24 +0200

    Add counters for SSE commits and merge requests

--- lib/gitlab/usage_data_counters/static_site_editor_counter.rb
+++ lib/gitlab/usage_data_counters/static_site_editor_counter.rb
@@ -3,12 +3,20 @@
 module Gitlab
   module UsageDataCounters
     class StaticSiteEditorCounter < BaseCounter
-      KNOWN_EVENTS = %w[views].freeze
+      KNOWN_EVENTS = %w[views commits merge_requests].freeze
       PREFIX = 'static_site_editor'
 
       class << self
         def increment_views_count
           count(:views)
+        end
+
+        def increment_commits_count
+          count(:commits)
+        end
+
+        def increment_merge_requests_count
+          count(:merge_requests)
         end
       end
     end
