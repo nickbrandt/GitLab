6c8679088dbb99e883fb73b1e03fc65ee543c880
Peter Hegman <phegman@gitlab.com>
2020-10-12 07:07:21 +0000

    Setup scaffolding for LDAP override
    In group members Vue app. Part of a larger initiative to convert the
    group members page from HAML to Vue

---
+++ ee/spec/frontend/groups/members/utils_spec.js
@@ -0,0 +1,27 @@
+import { membersJsonString, membersParsed } from 'jest/groups/members/mock_data';
+import { parseDataAttributes } from 'ee/groups/members/utils';
+
+describe('group member utils', () => {
+  describe('parseDataAttributes', () => {
+    let el;
+
+    beforeEach(() => {
+      el = document.createElement('div');
+      el.setAttribute('data-members', membersJsonString);
+      el.setAttribute('data-group-id', '234');
+      el.setAttribute('data-ldap-override-path', '/groups/ldap-group/-/group_members/:id/override');
+    });
+
+    afterEach(() => {
+      el = null;
+    });
+
+    it('correctly parses the data attributes', () => {
+      expect(parseDataAttributes(el)).toEqual({
+        members: membersParsed,
+        sourceId: 234,
+        ldapOverridePath: '/groups/ldap-group/-/group_members/:id/override',
+      });
+    });
+  });
+});
