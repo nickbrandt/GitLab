2d77fed65845ca17a926f9bec84b7860716130b8
Sean Arnold <sarnold@gitlab.com>
2020-10-12 19:14:58 +1300

    Use path to create URL

--- ee/app/services/incident_management/create_incident_sla_exceeded_label_service.rb
+++ ee/app/services/incident_management/create_incident_sla_exceeded_label_service.rb
@@ -2,11 +2,15 @@
 
 module IncidentManagement
   class CreateIncidentSlaExceededLabelService < BaseService
+    def self.doc_url
+      Rails.application.routes.url_helpers.help_page_url('operations/incident_management/incidents', anchor: 'service-level-agreement-countdown-timer')
+    end
+
     LABEL_PROPERTIES = {
       title: 'missed::SLA',
       color: '#D9534F',
       description: <<~DESCRIPTION.chomp
-        Incidents that have missed the targeted SLA (Service Level Agreement). https://docs.gitlab.com/ee/operations/incident_management/incidents.html#service-level-agreement-countdown-timer
+        Incidents that have missed the targeted SLA (Service Level Agreement). #{doc_url}
       DESCRIPTION
     }.freeze
 
