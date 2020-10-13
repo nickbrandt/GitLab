75293bff003c7b82b5788f8ee701812941e65fc5
Tristan Read <tread@gitlab.com>
2020-10-13 05:50:24 +0000

    Add sla columns to project incident settings table
    - Update schema
    - Update helper specs

--- ee/spec/helpers/ee/operations_helper_spec.rb
+++ ee/spec/helpers/ee/operations_helper_spec.rb
@@ -3,21 +3,21 @@
 require 'spec_helper'
 
 RSpec.describe OperationsHelper, :routing do
-  let_it_be(:project) { create(:project, :private) }
+  let_it_be_with_refind(:project) { create(:project, :private) }
+  let_it_be(:user) { create(:user) }
 
   before do
     helper.instance_variable_set(:@project, project)
+    allow(helper).to receive(:current_user) { user }
   end
 
   describe '#status_page_settings_data' do
-    let_it_be(:user) { create(:user) }
     let_it_be(:status_page_setting) { project.build_status_page_setting }
 
     subject { helper.status_page_settings_data }
 
     before do
       allow(helper).to receive(:status_page_setting) { status_page_setting }
-      allow(helper).to receive(:current_user) { user }
       allow(helper)
         .to receive(:can?).with(user, :admin_operations, project) { true }
     end
@@ -113,6 +113,48 @@ RSpec.describe OperationsHelper, :routing do
         let(:opsgenie_available) { false }
 
         it { is_expected.not_to include(opsgenie_keys) }
+      end
+    end
+  end
+
+  describe '#operations_settings_data' do
+    let_it_be(:operations_settings) do
+      create(
+        :project_incident_management_setting,
+        project: project,
+        issue_template_key: 'template-key',
+        pagerduty_active: true,
+        auto_close_incident: false
+      )
+    end
+
+    subject { helper.operations_settings_data }
+
+    it 'returns the correct set of data' do
+      is_expected.to eq(
+        operations_settings_endpoint: project_settings_operations_path(project),
+        templates: '[]',
+        create_issue: 'false',
+        issue_template_key: 'template-key',
+        send_email: 'false',
+        auto_close_incident: 'false',
+        pagerduty_active: 'true',
+        pagerduty_token: operations_settings.pagerduty_token,
+        pagerduty_webhook_url: project_incidents_integrations_pagerduty_url(project, token: operations_settings.pagerduty_token),
+        pagerduty_reset_key_path: reset_pagerduty_token_project_settings_operations_path(project),
+        sla_feature_available: 'false',
+        sla_active: operations_settings.sla_timer.to_s,
+        sla_minutes: operations_settings.sla_timer_minutes
+      )
+    end
+
+    context 'incident_sla feature enabled' do
+      before do
+        stub_licensed_features(incident_sla: true)
+      end
+
+      it 'returns the feature as enabled' do
+        expect(subject[:sla_feature_available]).to eq('true')
       end
     end
   end
