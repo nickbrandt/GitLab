78845b8ff08b82c2383bfdf48039db00f4fef99a
Marius Bobin <mbobin@gitlab.com>
2020-10-13 18:56:53 +0300

    Support variables in rules:changes
    Add variable expansion to the rules:changes entries

--- lib/expand_variables.rb
+++ lib/expand_variables.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
 
 module ExpandVariables
+  VARIABLES_REGEXP = /\$([a-zA-Z_][a-zA-Z0-9_]*)|\${\g<1>}|%\g<1>%/.freeze
+
   class << self
     def expand(value, variables)
       variables_hash = nil
 
-      value.gsub(/\$([a-zA-Z_][a-zA-Z0-9_]*)|\${\g<1>}|%\g<1>%/) do
+      value.gsub(VARIABLES_REGEXP) do
         variables_hash ||= transform_variables(variables)
         variables_hash[Regexp.last_match(1) || Regexp.last_match(2)]
+      end
+    end
+
+    def expand_existing(value, variables)
+      variables_hash = nil
+
+      value.gsub(VARIABLES_REGEXP) do
+        variables_hash ||= transform_variables(variables)
+        variables_hash.fetch(
+          Regexp.last_match(1) || Regexp.last_match(2),
+          Regexp.last_match(0)
+        )
       end
     end
 
