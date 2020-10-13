mehulagg/gitlab_test

Refactor ExportCsvWorker to IssuableExportCsvWorker
Amends ExportCsvWorker to allow it to use
any issuable type. Specifically MergeRequests for
exports.

app/mailers/emails/merge_requests.rb
@@ -99,6 +99,20 @@ module Emails
       mail_answer_thread(@merge_request, merge_request_thread_options(mwps_set_by_user_id, recipient_id, reason))
     end
 
+    def merge_requests_csv_email(user, project, csv_data, export_status)
+      @project = project
+      @merge_requests_count = export_status.fetch(:rows_expected)
+      @written_count = export_status.fetch(:rows_written)
+      @truncated = export_status.fetch(:truncated)
+
+      filename = "#{project.full_path.parameterize}_merge_requests_#{Date.today.iso8601}.csv"
+      attachments[filename] = { content: csv_data, mime_type: 'text/csv' }
+      mail(to: user.notification_email_for(@project.group), subject: subject("Exported merge requests")) do |format|
+        format.html { render layout: 'mailer' }
+        format.text { render layout: 'mailer' }
+      end
+    end
+
     private
 
     def setup_merge_request_mail(merge_request_id, recipient_id, present: false)

nickbrandt/GitLab

app/mailers/emails/merge_requests.rb
@@ -110,6 +110,20 @@ module Emails
       mail_answer_thread(@merge_request, merge_request_thread_options(mwps_set_by_user_id, recipient_id, reason))
     end
 
+    def merge_requests_csv_email(user, project, csv_data, export_status)
+      @project = project
+      @count = export_status.fetch(:rows_expected)
+      @written_count = export_status.fetch(:rows_written)
+      @truncated = export_status.fetch(:truncated)
+
+      filename = "#{project.full_path.parameterize}_merge_requests_#{Date.current.iso8601}.csv"
+      attachments[filename] = { content: csv_data, mime_type: 'text/csv' }
+      mail(to: user.notification_email_for(@project.group), subject: subject("Exported merge requests")) do |format|
+        format.html { render layout: 'mailer' }
+        format.text { render layout: 'mailer' }
+      end
+    end
+
     private
 
     def setup_merge_request_mail(merge_request_id, recipient_id, present: false)