ff0bbef0ab121420eb1ecaea34436b2ee487af8b
Stan Hu <stanhu@gmail.com>
2020-10-17 05:08:02 -0700

    Gracefully recover from deleted LFS file
    If an LFS object exists in the database but the underlying file has been
    deleted, pushing that same object from another project may result in a
    confusing "403 Access Forbidden" error.
    
    Now, if the file no longer exists, we replace the file entry with the
    newly-uploaded file so that we can gracefully recover from this case.
    
    Closes https://gitlab.com/gitlab-org/gitlab/-/issues/25852

--- app/controllers/repositories/lfs_storage_controller.rb
+++ app/controllers/repositories/lfs_storage_controller.rb
@@ -62,7 +62,10 @@ module Repositories
     # rubocop: disable CodeReuse/ActiveRecord
     def store_file!(oid, size)
       object = LfsObject.find_by(oid: oid, size: size)
-      unless object&.file&.exists?
+
+      if object
+        replace_file!(object) unless object.file&.exists?
+      else
         object = create_file!(oid, size)
       end
 
@@ -77,6 +80,16 @@ module Repositories
       return unless uploaded_file.is_a?(UploadedFile)
 
       LfsObject.create!(oid: oid, size: size, file: uploaded_file)
+    end
+
+    def replace_file!(lfs_object)
+      uploaded_file = params[:file]
+
+      raise UploadedFile::InvalidPathError unless uploaded_file.is_a?(UploadedFile)
+
+      Gitlab::AppJsonLogger.info(message: "LFS file replaced because it did not exist", oid: oid, size: size)
+      lfs_object.file = uploaded_file
+      lfs_object.save!
     end
 
     def link_to_project!(object)
