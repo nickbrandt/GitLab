ea07db725d37758887e43c7dae9c2e466bf6ce35
fjsanpedro <fjlopez@gitlab.com>
2020-10-19 15:45:37 +0000

    Add sitemap generator for .com and gitlab-org group
    In this commit we add a small functionality to generate the sitemap
    for the `gitlab-org` group just in the .com environment.
    
    The idea behind this is to analyze whether a broader sitemap
    would improve the search crawling results.

---
+++ ee/spec/lib/gitlab/sitemaps/sitemap_file_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Gitlab::Sitemaps::SitemapFile do
+  subject do
+    described_class.new.tap do |file|
+      file.add_elements("https://gitlab.com")
+    end
+  end
+
+  describe '#render' do
+    it 'generates a valid sitemap file' do
+      freeze_time do
+        content = subject.render
+        expected_content = <<~EOS
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+          <url>
+            <loc>https://gitlab.com</loc>
+            <lastmod>#{Date.today.iso8601}</lastmod>
+          </url>
+        </urlset>
+        EOS
+
+        expect(content).to eq expected_content
+      end
+    end
+  end
+
+  describe '#save' do
+    it 'returns if no elements has been provided' do
+      expect(File).not_to receive(:write)
+
+      described_class.new.save # rubocop: disable Rails/SaveBang
+    end
+
+    it 'stores the content in the public root folder' do
+      expect(subject).to receive(:render).and_call_original
+
+      expect(File).to receive(:write).with(
+        File.join(Rails.public_path, 'sitemap.xml'),
+        anything)
+
+      subject.save # rubocop: disable Rails/SaveBang
+    end
+  end
+end
