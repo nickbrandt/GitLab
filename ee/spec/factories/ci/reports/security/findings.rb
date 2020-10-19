44e783bbffa797bd435b6dd0cfb0f43589e50432
Peter Leitzen <pleitzen@gitlab.com>
2020-10-19 13:58:57 +0200

    Fix cop FactoryBot/InlineAssociation for security findings/reports

--- ee/spec/factories/ci/reports/security/findings.rb
+++ ee/spec/factories/ci/reports/security/findings.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :ci_reports_security_finding, class: '::Gitlab::Ci::Reports::Security::Finding' do
     compare_key { "#{identifiers.first&.external_type}:#{identifiers.first&.external_id}:#{location.fingerprint}" }
     confidence { :medium }
-    identifiers { Array.new(1) { FactoryBot.build(:ci_reports_security_identifier) } }
+    identifiers { Array.new(1) { association(:ci_reports_security_identifier) } }
     location factory: :ci_reports_security_locations_sast
     metadata_version { 'sast:1.0' }
     name { 'Cipher with no integrity' }
@@ -36,7 +36,7 @@ FactoryBot.define do
     skip_create
 
     trait :dynamic do
-      location { FactoryBot.build(:ci_reports_security_locations_sast, :dynamic) }
+      location { association(:ci_reports_security_locations_sast, :dynamic) }
     end
 
     initialize_with do
