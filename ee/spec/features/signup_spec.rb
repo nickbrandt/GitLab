c8ad6097aad34a8658af3b8f7ea450679c0e48ca
Nicolas Dular <ndular@gitlab.com>
2020-10-20 14:31:12 +0200

    Split sign in and sign up pages
    Instead of having tabs to switch between sign in and sign up pages, we
    want to have two separate pages. This separation was already part of an
    experiment and is now the default experience.

--- ee/spec/features/signup_spec.rb
+++ ee/spec/features/signup_spec.rb
@@ -16,12 +16,11 @@ RSpec.describe 'Signup on EE' do
   context 'for Gitlab.com' do
     before do
       expect(Gitlab).to receive(:com?).and_return(true).at_least(:once)
+      visit new_user_registration_path
     end
 
     context 'when the user sets it up for the company' do
       it 'creates the user and sets the email_opted_in field truthy' do
-        visit root_path
-
         fill_in_signup_form
         click_button "Register"
 
@@ -39,8 +38,6 @@ RSpec.describe 'Signup on EE' do
 
     context 'when the user checks the opt-in to email updates box' do
       it 'creates the user and sets the email_opted_in field truthy' do
-        visit root_path
-
         fill_in_signup_form
         click_button "Register"
 
@@ -59,8 +56,6 @@ RSpec.describe 'Signup on EE' do
 
     context 'when the user does not check the opt-in to email updates box' do
       it 'creates the user and sets the email_opted_in field falsey' do
-        visit root_path
-
         fill_in_signup_form
         click_button "Register"
 
@@ -77,8 +72,6 @@ RSpec.describe 'Signup on EE' do
     end
 
     it 'redirects to step 2 of the signup process, sets the role and setup for company and redirects back' do
-      visit new_user_registration_path
-
       fill_in_signup_form
       click_button 'Register'
       visit new_project_path
@@ -99,11 +92,10 @@ RSpec.describe 'Signup on EE' do
   context 'not for Gitlab.com' do
     before do
       expect(Gitlab).to receive(:com?).and_return(false).at_least(:once)
+      visit new_user_registration_path
     end
 
     it 'does not have a opt-in checkbox, it creates the user and sets email_opted_in to falsey' do
-      visit root_path
-
       expect(page).not_to have_selector("[name='new_user_email_opted_in']")
 
       fill_in_signup_form
