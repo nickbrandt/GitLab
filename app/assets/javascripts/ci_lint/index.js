3231c4132bae2bbbf273b19d7dc0e13404979343
Miguel Rincon <mrincon@gitlab.com>
2020-10-20 17:19:50 +0200

    Check for missing reference in job.only.refs
    This change adds a check in case job.only is missing in the resolver,
    so that axios does not fail with an exception.

--- app/assets/javascripts/ci_lint/index.js
+++ app/assets/javascripts/ci_lint/index.js
@@ -13,23 +13,24 @@ const resolvers = {
         valid: data.valid,
         errors: data.errors,
         warnings: data.warnings,
-        jobs: data.jobs.map(job => ({
-          name: job.name,
-          stage: job.stage,
-          beforeScript: job.before_script,
-          script: job.script,
-          afterScript: job.after_script,
-          tagList: job.tag_list,
-          environment: job.environment,
-          when: job.when,
-          allowFailure: job.allow_failure,
-          only: {
-            refs: job.only.refs,
-            __typename: 'CiLintJobOnlyPolicy',
-          },
-          except: job.except,
-          __typename: 'CiLintJob',
-        })),
+        jobs: data.jobs.map(job => {
+          const only = job.only ? { refs: job.only.refs, __typename: 'CiLintJobOnlyPolicy' } : null;
+
+          return {
+            name: job.name,
+            stage: job.stage,
+            beforeScript: job.before_script,
+            script: job.script,
+            afterScript: job.after_script,
+            tagList: job.tag_list,
+            environment: job.environment,
+            when: job.when,
+            allowFailure: job.allow_failure,
+            only,
+            except: job.except,
+            __typename: 'CiLintJob',
+          };
+        }),
         __typename: 'CiLintContent',
       }));
     },
