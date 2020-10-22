45c0783f203c9106d6ba298e09a60f93257245c8
Savas Vedova <svedova@gitlab.com>
2020-10-22 13:31:15 +0300

    Rewrite tests using mock apollo client
    - Move mock_data into a separate folder

---
+++ ee/spec/frontend/security_dashboard/mock_data/index.js
@@ -0,0 +1,120 @@
+export const mockProjectsWithSeverityCounts = () => [
+  {
+    id: 'gid://gitlab/Project/1',
+    name: 'Gitlab Test',
+    nameWithNamespace: 'Gitlab Org / Gitlab Test',
+    securityDashboardPath: '/gitlab-org/gitlab-test/-/security/dashboard',
+    fullPath: 'gitlab-org/gitlab-test',
+    avatarUrl: null,
+    path: 'gitlab-test',
+    vulnerabilitySeveritiesCount: {
+      critical: 2,
+      high: 0,
+      info: 4,
+      low: 3,
+      medium: 0,
+      unknown: 1,
+    },
+  },
+  {
+    id: 'gid://gitlab/Project/2',
+    name: 'Gitlab Shell',
+    nameWithNamespace: 'Gitlab Org / Gitlab Shell',
+    securityDashboardPath: '/gitlab-org/gitlab-shell/-/security/dashboard',
+    fullPath: 'gitlab-org/gitlab-shell',
+    avatarUrl: null,
+    path: 'gitlab-shell',
+    vulnerabilitySeveritiesCount: {
+      critical: 0,
+      high: 2,
+      info: 2,
+      low: 1,
+      medium: 1,
+      unknown: 2,
+    },
+  },
+  {
+    id: 'gid://gitlab/Project/4',
+    name: 'Gitlab Perfectly Secure',
+    nameWithNamespace: 'Gitlab Org / Perfectly Secure',
+    securityDashboardPath: '/gitlab-org/gitlab-perfectly-secure/-/security/dashboard',
+    fullPath: 'gitlab-org/gitlab-perfectly-secure',
+    avatarUrl: null,
+    path: 'gitlab-perfectly-secure',
+    vulnerabilitySeveritiesCount: {
+      critical: 0,
+      high: 0,
+      info: 0,
+      low: 0,
+      medium: 0,
+      unknown: 0,
+    },
+  },
+  {
+    id: 'gid://gitlab/Project/5',
+    name: 'Gitlab Perfectly Secure 2 ',
+    nameWithNamespace: 'Gitlab Org / Perfectly Secure 2',
+    securityDashboardPath: '/gitlab-org/gitlab-perfectly-secure-2/-/security/dashboard',
+    fullPath: 'gitlab-org/gitlab-perfectly-secure-2',
+    avatarUrl: null,
+    path: 'gitlab-perfectly-secure-2',
+    vulnerabilitySeveritiesCount: {
+      critical: 0,
+      high: 0,
+      info: 0,
+      low: 0,
+      medium: 0,
+      unknown: 0,
+    },
+  },
+];
+
+const projectsMemoized = mockProjectsWithSeverityCounts();
+const vulnerabilityGrades = [
+  {
+    grade: 'F',
+    projects: {
+      nodes: [projectsMemoized[0]],
+    },
+  },
+  {
+    grade: 'D',
+    projects: {
+      nodes: [projectsMemoized[1]],
+    },
+  },
+  {
+    grade: 'C',
+    projects: {
+      nodes: [projectsMemoized[0], projectsMemoized[1]],
+    },
+  },
+  {
+    grade: 'B',
+    projects: {
+      nodes: [projectsMemoized[1]],
+    },
+  },
+  {
+    grade: 'A',
+    projects: {
+      nodes: [projectsMemoized[2], projectsMemoized[3]],
+    },
+  },
+];
+
+export const mockGroupVulnerabilityGrades = () => ({
+  data: {
+    group: {
+      vulnerabilityGrades,
+    },
+  },
+});
+
+export const mockInstanceVulnerabilityGrades = () => ({
+  data: {
+    instanceSecurityDashboard: {
+      vulnerabilityGrades,
+    },
+  },
+});
