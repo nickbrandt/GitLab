33acb7a22968d07736a437631a1a3d57ec5257c2
Eugenia Grieff <egrieff@gitlab.com>
2020-10-29 18:02:22 +0000

    Add email notification for requirements cvs import
    - Add mailer to notify user of imported
    requirements
    - Add email templates
    - Add tests

--- ee/app/services/requirements_management/import_csv_service.rb
+++ ee/app/services/requirements_management/import_csv_service.rb
@@ -7,5 +7,9 @@ module RequirementsManagement
     def create_issuable_class
       RequirementsManagement::CreateRequirementService
     end
+
+    def email_results_to_user
+      Notify.import_requirements_csv_email(@user.id, @project.id, @results).deliver_later
+    end
   end
 end
