5c43e14e37d899ac7e52854f93ed629c7678b96e
sfang97 <sfang@gitlab.com>
2020-10-29 11:24:46 -0500

    Add specs for pass case

--- ee/spec/services/resource_access_tokens/create_service_spec.rb
+++ ee/spec/services/resource_access_tokens/create_service_spec.rb
@@ -5,6 +5,10 @@ require 'spec_helper'
 RSpec.describe ResourceAccessTokens::CreateService do
   subject { described_class.new(user, resource).execute }
 
+  let_it_be(:user) { create(:user) }
+  let_it_be(:project) { create(:project, :private) }
+  let_it_be(:params) { {} }
+
   shared_examples 'token creation fails' do
     let(:resource) { create(:project, group: group)}
 
@@ -28,6 +32,28 @@ RSpec.describe ResourceAccessTokens::CreateService do
     end
   end
 
+  shared_examples 'token creation succeeds' do
+    let(:resource) { create(:project, group: group)}
+
+    before do
+      resource.add_maintainer(user)
+    end
+
+    it 'does not cause an error' do
+      response = subject
+
+      expect(response.error?).to be false
+    end
+
+    it 'adds the project bot as a member' do
+      expect { subject }.to change { resource.members.count }.by(1)
+    end
+
+    it 'creates a project bot user' do
+      expect { subject }.to change { User.bots.count }.by(1)
+    end
+  end
+
   describe '#execute' do
     context 'with enforced group managed account enabled' do
       let(:group) { create(:group_with_managed_accounts, :private) }
@@ -38,7 +64,7 @@ RSpec.describe ResourceAccessTokens::CreateService do
         stub_licensed_features(group_saml: true)
       end
 
-      it_behaves_like 'token creation fails'
+      it_behaves_like 'token creation succeeds'
     end
 
     context "for SAML enabled groups" do
@@ -49,6 +75,20 @@ RSpec.describe ResourceAccessTokens::CreateService do
 
       before do
         stub_licensed_features(group_saml: true)
+      end
+
+      it_behaves_like 'token creation succeeds'
+    end
+
+    context "when access provisioning fails" do
+      let(:group) { create(:group, :private) }
+      let(:unpersisted_user) { create(:user, :project_bot) }
+
+      before do
+        unpersisted_user.destroy
+        allow_next_instance_of(ResourceAccessTokens::CreateService) do |service|
+          allow(service).to receive(:create_membership).and_return(unpersisted_user)
+        end
       end
 
       it_behaves_like 'token creation fails'
