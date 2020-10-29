559d1addd74c6ab145cbf0553b60547b54132254
sfang97 <sfang@gitlab.com>
2020-10-29 09:09:02 -0500

    Move lets into individual contexts

--- ee/spec/services/resource_access_tokens/create_service_spec.rb
+++ ee/spec/services/resource_access_tokens/create_service_spec.rb
@@ -6,9 +6,7 @@ RSpec.describe ResourceAccessTokens::CreateService do
   subject { described_class.new(user, resource).execute }
 
   shared_examples 'token creation fails' do
-    let(:group) { create(:group_with_managed_accounts, :private) }
     let(:resource) { create(:project, group: group)}
-    let(:user) { create(:user, :group_managed, managing_group: group) }
 
     before do
       resource.add_maintainer(user)
@@ -32,6 +30,9 @@ RSpec.describe ResourceAccessTokens::CreateService do
 
   describe '#execute' do
     context 'with enforced group managed account enabled' do
+      let(:group) { create(:group_with_managed_accounts, :private) }
+      let(:user) { create(:user, :group_managed, managing_group: group) }
+
       before do
         stub_feature_flags(group_managed_accounts: true)
         stub_licensed_features(group_saml: true)
@@ -41,6 +42,11 @@ RSpec.describe ResourceAccessTokens::CreateService do
     end
 
     context "for SAML enabled groups" do
+      let(:group) { create(:group, :private) }
+      let!(:saml_provider) { create(:saml_provider, group: group, enforced_sso: true) }
+      let(:identity) { create(:group_saml_identity, saml_provider: saml_provider) }
+      let(:user) { identity.user }
+
       before do
         stub_licensed_features(group_saml: true)
       end
