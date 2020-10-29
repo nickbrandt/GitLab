ec71eaa032e7cf012f2018a7440027b7037c88b9
Philip Cunningham <pcunningham@gitlab.com>
2020-10-29 09:48:21 +0000

    Reject active on-demand DAST scan when unvalidated
    Prevent users from running active scans against unvalidated targets.

--- ee/app/graphql/mutations/dast_on_demand_scans/create.rb
+++ ee/app/graphql/mutations/dast_on_demand_scans/create.rb
@@ -30,69 +30,55 @@ module Mutations
       def resolve(full_path:, dast_site_profile_id:, **args)
         project = authorized_find_project!(full_path: full_path)
 
-        # TODO: remove explicit coercion once compatibility layer is removed
-        # See: https://gitlab.com/gitlab-org/gitlab/-/issues/257883
-        dast_site_profile_id = ::Types::GlobalIDType[::DastSiteProfile].coerce_isolated_input(dast_site_profile_id)
+        dast_site_profile = find_dast_site_profile(project, dast_site_profile_id)
+        dast_scanner_profile = find_dast_scanner_profile(project, args[:dast_scanner_profile_id])
 
-        dast_site_profile = find_dast_site_profile(project: project, dast_site_profile_id: dast_site_profile_id)
-        dast_site = dast_site_profile.dast_site
-        dast_scanner_profile = find_dast_scanner_profile(project: project, dast_scanner_profile_id: args[:dast_scanner_profile_id])
+        response = create_on_demand_dast_scan(project, dast_site_profile, dast_scanner_profile)
 
-        result = ::Ci::RunDastScanService.new(
-          project, current_user
-        ).execute(
-          branch: project.default_branch,
-          target_url: dast_site.url,
-          spider_timeout: dast_scanner_profile&.spider_timeout,
-          target_timeout: dast_scanner_profile&.target_timeout,
-          full_scan_enabled: dast_scanner_profile&.full_scan_enabled?,
-          use_ajax_spider: dast_scanner_profile&.use_ajax_spider,
-          show_debug_messages: dast_scanner_profile&.show_debug_messages
-        )
+        return { errors: response.errors } if response.error?
 
-        if result.success?
-          success_response(project: project, pipeline: result.payload)
-        else
-          error_response(result)
-        end
+        { errors: [], pipeline_url: response.payload.fetch(:pipeline_url) }
       end
 
       private
 
       # rubocop: disable CodeReuse/ActiveRecord
-      def find_dast_site_profile(project:, dast_site_profile_id:)
+      def find_dast_site_profile(project, dast_site_profile_id)
+        # TODO: remove explicit coercion once compatibility layer is removed
+        # See: https://gitlab.com/gitlab-org/gitlab/-/issues/257883
+        dast_site_profile_id = ::Types::GlobalIDType[::DastSiteProfile].coerce_isolated_input(dast_site_profile_id)
+
         DastSiteProfilesFinder.new(project_id: project.id, id: dast_site_profile_id.model_id)
           .execute
           .first!
       end
       # rubocop: enable CodeReuse/ActiveRecord
 
-      def find_dast_scanner_profile(project:, dast_scanner_profile_id:)
+      # rubocop: disable CodeReuse/ActiveRecord
+      def find_dast_scanner_profile(project, dast_scanner_profile_id)
         return unless dast_scanner_profile_id
 
         # TODO: remove explicit coercion once compatibility layer is removed
         # See: https://gitlab.com/gitlab-org/gitlab/-/issues/257883
         dast_scanner_profile_id = ::Types::GlobalIDType[::DastScannerProfile]
-          .coerce_isolated_input(dast_scanner_profile_id)
+                                    .coerce_isolated_input(dast_scanner_profile_id)
 
-        project
-          .dast_scanner_profiles
-          .find(dast_scanner_profile_id.model_id)
+        DastScannerProfilesFinder.new(
+          project_ids: [project.id],
+          ids: [dast_scanner_profile_id.model_id]
+        ).execute.first!
       end
+      # rubocop: enable CodeReuse/ActiveRecord
 
-      def success_response(project:, pipeline:)
-        pipeline_url = Rails.application.routes.url_helpers.project_pipeline_url(
-          project,
-          pipeline
-        )
-        {
-          errors: [],
-          pipeline_url: pipeline_url
-        }
-      end
-
-      def error_response(result)
-        { errors: result.errors }
+      def create_on_demand_dast_scan(project, dast_site_profile, dast_scanner_profile)
+        ::DastOnDemandScans::CreateService.new(
+          container: project,
+          current_user: current_user,
+          params: {
+            dast_site_profile: dast_site_profile,
+            dast_scanner_profile: dast_scanner_profile
+          }
+        ).execute
       end
     end
   end
