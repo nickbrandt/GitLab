444486efd030342fad75cf031e82035e121aecc4
mfluharty <mfluharty@gitlab.com>
2020-10-30 17:12:03 -0600

    Add component skeleton

--- ee/app/assets/javascripts/analytics/repository_analytics/components/group_repository_analytics.vue
+++ ee/app/assets/javascripts/analytics/repository_analytics/components/group_repository_analytics.vue
@@ -1,12 +1,14 @@
 <script>
 import { s__ } from '~/locale';
 import glFeatureFlagsMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
+import TestCoverageSummary from './test_coverage_summary.vue';
 import TestCoverageTable from './test_coverage_table.vue';
 import DownloadTestCoverage from './download_test_coverage.vue';
 
 export default {
   name: 'GroupRepositoryAnalytics',
   components: {
+    TestCoverageSummary,
     TestCoverageTable,
     DownloadTestCoverage,
   },
@@ -27,6 +29,7 @@ export default {
     <h4 data-testid="test-coverage-header">
       {{ $options.text.codeCoverageHeader }}
     </h4>
+    <test-coverage-summary />
     <test-coverage-table v-if="shouldShowCoverageReport" class="gl-mb-5" />
     <download-test-coverage />
   </div>
---
+++ ee/app/assets/javascripts/analytics/repository_analytics/components/test_coverage_summary.vue
@@ -0,0 +1,21 @@
+<script>
+import MetricCard from '~/analytics/shared/components/metric_card.vue';
+
+export default {
+  name: 'TestCoverageSummary',
+  components: {
+    MetricCard,
+  },
+};
+</script>
+<template>
+  <metric-card
+    :title="__('Overall Activity')"
+    :metrics="[
+      { key: 1, value: 45, unit: '', label: 'Projects with Tests' },
+      { key: 2, value: 72, unit: '%', label: 'Average Coverage by Job' },
+      { key: 3, value: 103, unit: '', label: 'Total Number of Coverages' },
+    ]"
+    :is-loading="false"
+  />
+</template>
