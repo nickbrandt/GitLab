d16e1a49e77a81380afc17e053ecd49a740abd26
mfluharty <mfluharty@gitlab.com>
2020-11-02 13:34:45 -0700

    Wire up the GraphQL query
    i18n-ify strings

--- ee/app/assets/javascripts/analytics/repository_analytics/components/test_coverage_summary.vue
+++ ee/app/assets/javascripts/analytics/repository_analytics/components/test_coverage_summary.vue
@@ -1,21 +1,79 @@
 <script>
+import { s__ } from '~/locale';
 import MetricCard from '~/analytics/shared/components/metric_card.vue';
+import getGroupTestCoverage from '../graphql/queries/get_group_test_coverage.query.graphql';
 
 export default {
   name: 'TestCoverageSummary',
   components: {
     MetricCard,
   },
+  inject: {
+    groupFullPath: {
+      default: '',
+    },
+  },
+  apollo: {
+    group: {
+      query: getGroupTestCoverage,
+      variables() {
+        return {
+          groupFullPath: this.groupFullPath,
+          startDate: new Date(Date.now() - 604800000), // one week ago
+        };
+      },
+      result(res) {
+        const { projectCount, averageCoverage, coverageCount } =
+          res.data?.group?.codeCoverageActivities?.nodes?.[0] || {};
+
+        this.projectCount = projectCount;
+        this.averageCoverage = averageCoverage;
+        this.coverageCount = coverageCount;
+      },
+      error() {
+        this.hasError = true;
+        this.projectCount = null;
+        this.averageCoverage = null;
+        this.coverageCount = null;
+      },
+      watchLoading(isLoading) {
+        this.isLoading = isLoading;
+      },
+    },
+  },
+  data() {
+    return {
+      projectCount: null,
+      averageCoverage: null,
+      coverageCount: null,
+      hasError: false,
+      isLoading: false,
+    };
+  },
+  computed: {
+    metrics() {
+      return [
+        {
+          key: 'projectCount',
+          value: this.projectCount,
+          label: s__('RepositoriesAnalytics|Projects with Tests'),
+        },
+        {
+          key: 'averageCoverage',
+          value: this.averageCoverage,
+          unit: '%',
+          label: s__('RepositoriesAnalytics|Average Coverage by Job'),
+        },
+        {
+          key: 'coverageCount',
+          value: this.coverageCount,
+          label: s__('RepositoriesAnalytics|Total Number of Coverages'),
+        },
+      ];
+    },
+  },
 };
 </script>
 <template>
-  <metric-card
-    :title="__('Overall Activity')"
-    :metrics="[
-      { key: 1, value: 45, unit: '', label: 'Projects with Tests' },
-      { key: 2, value: 72, unit: '%', label: 'Average Coverage by Job' },
-      { key: 3, value: 103, unit: '', label: 'Total Number of Coverages' },
-    ]"
-    :is-loading="false"
-  />
+  <metric-card :title="__('Overall Activity')" :metrics="metrics" :is-loading="isLoading" />
 </template>
