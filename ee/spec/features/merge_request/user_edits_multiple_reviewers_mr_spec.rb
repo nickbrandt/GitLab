3bf388888882aca098659d71fb6e84ba3daa371f
Samantha Ming <sming@gitlab.com>
2020-11-02 16:51:12 -0800

    Display which approval rules match a given reviewer
    Issue: https://gitlab.com/gitlab-org/gitlab/-/issues/233736

--- ee/spec/features/merge_request/user_edits_multiple_reviewers_mr_spec.rb
+++ ee/spec/features/merge_request/user_edits_multiple_reviewers_mr_spec.rb
@@ -10,4 +10,28 @@ RSpec.describe 'Merge request > User edits MR with multiple reviewers' do
   end
 
   it_behaves_like 'multiple reviewers merge request', 'updates', 'Save changes'
+
+  context 'user approval rules', :js do
+    let(:rule_name) { 'some-custom-rule' }
+    let(:user) { create(:admin) }
+    let!(:mr_rule) { create(:approval_merge_request_rule, merge_request: merge_request, users: [user], name: rule_name, approvals_required: 1 )}
+
+    it 'is not shown in assignee dropdown' do
+      find('.js-assignee-search').click
+      wait_for_requests
+
+      page.within '.dropdown-menu-assignee' do
+        expect(page).not_to have_content(rule_name)
+      end
+    end
+
+    it 'is shown in reviewer dropdown' do
+      find('.js-reviewer-search').click
+      wait_for_requests
+
+      page.within '.dropdown-menu-reviewer' do
+        expect(page).to have_content(rule_name)
+      end
+    end
+  end
 end
