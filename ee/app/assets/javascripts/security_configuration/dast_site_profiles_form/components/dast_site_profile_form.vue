406360c8a9ba4f72d5f26116afd0ec7e05b15f4e
David Pisek <dpisek@gitlab.com>
2020-11-04 14:32:15 +0000

    Add auth-section component
    * Adds new component that includes the authentication form
    * Adds new component to site-profile form component

--- ee/app/assets/javascripts/security_configuration/dast_site_profiles_form/components/dast_site_profile_form.vue
+++ ee/app/assets/javascripts/security_configuration/dast_site_profiles_form/components/dast_site_profile_form.vue
@@ -10,6 +10,7 @@ import {
   GlModal,
   GlToggle,
 } from '@gitlab/ui';
+import { initFormField } from 'ee/security_configuration/utils';
 import * as Sentry from '~/sentry/wrapper';
 import { __, s__ } from '~/locale';
 import { redirectTo } from '~/lib/utils/url_utility';
@@ -25,12 +26,6 @@ import dastSiteValidationQuery from '../graphql/dast_site_validation.query.graph
 import { DAST_SITE_VALIDATION_STATUS, DAST_SITE_VALIDATION_POLL_INTERVAL } from '../constants';
 
 const { PENDING, INPROGRESS, PASSED, FAILED } = DAST_SITE_VALIDATION_STATUS;
-
-const initField = value => ({
-  value,
-  state: null,
-  feedback: null,
-});
 
 export default {
   name: 'DastSiteProfileForm',
@@ -71,8 +66,8 @@ export default {
       state: false,
       showValidation: false,
       fields: {
-        profileName: initField(name),
-        targetUrl: initField(targetUrl),
+        profileName: initFormField({ value: name }),
+        targetUrl: initFormField({ value: targetUrl }),
       },
     };
 
