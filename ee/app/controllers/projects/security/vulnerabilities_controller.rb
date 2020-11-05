68680f99c72d05c3c23ba0d1434e4cc016523d1b
Jonathan Schafer <jschafer@gitlab.com>
2020-11-05 16:41:05 -0600

    Add build from vulnerability service
    Move issue build to its own service as the issue requested
    Fix vulnerabilities_controller

--- ee/app/controllers/projects/security/vulnerabilities_controller.rb
+++ ee/app/controllers/projects/security/vulnerabilities_controller.rb
@@ -22,14 +22,7 @@ module Projects
       def new_issue
         @project = vulnerability.project
 
-        issue_params = {
-          title: "Investigate vulnerability: #{vulnerability.title}",
-          description: render_description(vulnerability),
-          confidential: true
-        }
-
-        @issue = @notable = ::Issues::BuildService
-          .new(@project, current_user, issue_params).execute
+        @issue = ::Issues::BuildFromVulnerabilityService.new(@project, current_user, { vulnerability: @vulnerability }).execute
 
         render '/projects/issues/new'
       end
@@ -42,17 +35,6 @@ module Projects
 
       alias_method :issuable, :vulnerability
       alias_method :noteable, :vulnerability
-
-      def issue_serializer
-        IssueSerializer.new(current_user: current_user)
-      end
-
-      def render_description(vulnerability)
-        ApplicationController.render(
-          template: 'vulnerabilities/issue_description.md.erb',
-          locals: { vulnerability: vulnerability.present }
-        )
-      end
     end
   end
 end
