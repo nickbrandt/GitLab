363759a988f1b2649a0264428b239427a7a6f410
Guillaume Grossetie <ggrossetie@gmail.com>
2020-11-06 13:22:36 +0100

    Integrate Asciidoctor Kroki gem

--- lib/gitlab/kroki.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Gitlab
-  module Kroki
-    # QUESTION: should we use the asciidoctor-kroki gem?
-    DIAGRAM_TYPES = %w(plantuml ditaa graphviz blockdiag seqdiag actdiag nwdiag packetdiag rackdiag c4plantuml erd mermaid nomnoml svgbob umlet vega vegalite wavedrom).freeze
-  end
-end
