da7233d40d56522a9513e438d62c25dd2855854e
Małgorzata Ksionek <mksionek@gitlab.com>
2020-11-06 19:26:20 +0100

    Add cr remarks

--- spec/migrations/ensure_namespace_settings_creation_spec.rb
+++ spec/migrations/ensure_namespace_settings_creation_spec.rb
@@ -8,9 +8,37 @@ RSpec.describe EnsureNamespaceSettingsCreation do
     let(:namespaces) { table(:namespaces) }
     let(:namespace_settings) { table(:namespace_settings) }
     let!(:namespace) { namespaces.create!(name: 'gitlab', path: 'gitlab-org') }
+    let!(:namespace_2) { namespaces.create!(name: 'gitlab', path: 'gitlab-org2') }
 
     it 'migrates namespaces without namespace_settings' do
-      expect { migrate! }.to change { namespace_settings.count }.from(0).to(1)
+      stub_const("#{described_class.name}::BATCH_SIZE", 2)
+
+      Sidekiq::Testing.fake! do
+        freeze_time do
+          migrate!
+
+          expect(described_class::MIGRATION)
+            .to be_scheduled_delayed_migration(2.minutes.to_i, namespace.id, namespace_2.id)
+        end
+      end
+    end
+
+    it 'schedules migrations in batches ' do
+      stub_const("#{described_class.name}::BATCH_SIZE", 2)
+
+      namespace_3 = namespaces.create!(name: 'gitlab', path: 'gitlab-org3')
+      namespace_4 = namespaces.create!(name: 'gitlab', path: 'gitlab-org4')
+
+      Sidekiq::Testing.fake! do
+        freeze_time do
+          migrate!
+
+          expect(described_class::MIGRATION)
+            .to be_scheduled_delayed_migration(2.minutes.to_i, namespace.id, namespace_2.id)
+          expect(described_class::MIGRATION)
+            .to be_scheduled_delayed_migration(4.minutes.to_i, namespace_3.id, namespace_4.id)
+        end
+      end
     end
   end
 end
