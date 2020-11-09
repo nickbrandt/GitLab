9098031da28f6bf74fb450982d78838d13a03125
Zack Cuddy <zcuddy@gitlab.com>
2020-11-09 10:16:04 +0000

    Global Search - Left Sidebar
    This creates a search facet sidebar
    for issues and merge requests.
    
    This also converts the
    status and confidential
    filter from a dropdown
    to a radio button.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -23,10 +23,6 @@ class SearchController < ApplicationController
     search_term_present && !params[:project_id].present?
   end
 
-  before_action do
-    push_frontend_feature_flag(:search_facets)
-  end
-
   layout 'search'
 
   feature_category :global_search
