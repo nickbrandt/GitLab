6199360e02a4794b913f9a8282fd22845451ea6b
Jackie Fraser <jfraser@gitlab.com>
2020-11-09 12:03:57 -0500

    Change "Contact Sales to upgrade" link on License

--- ee/spec/views/admin/licenses/_info.html.haml_spec.rb
+++ ee/spec/views/admin/licenses/_info.html.haml_spec.rb
@@ -11,22 +11,22 @@ RSpec.describe 'admin/licenses/_info' do
     context 'when plan can be upgraded' do
       let(:license) { create(:license, plan: License::STARTER_PLAN) }
 
-      it 'shows "How to upgrade" link' do
+      it 'shows "Contact Sales to upgrade" link' do
         render
 
-        expect(rendered).to have_content('Plan: Starter - How to upgrade')
-        expect(rendered).to have_link('How to upgrade')
+        expect(rendered).to have_content('Plan: Starter - Contact Sales to upgrade')
+        expect(rendered).to have_link('Contact Sales to upgrade')
       end
     end
 
     context 'when plan can not be upgraded' do
       let(:license) { create(:license, plan: License::ULTIMATE_PLAN) }
 
-      it 'does not show "How to upgrade" link' do
+      it 'does not show "Contact Sales to upgrade" link' do
         render
 
         expect(rendered).to have_content('Plan: Ultimate')
-        expect(rendered).not_to have_link('How to upgrade')
+        expect(rendered).not_to have_link('Contact Sales to upgrade')
       end
     end
   end
