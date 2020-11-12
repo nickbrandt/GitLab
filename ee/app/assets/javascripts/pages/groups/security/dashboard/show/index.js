c1ae4bef098b8c47da0316c52cea6636e44a527d
Jannik Lehmann <jlehmann@gitlab.com>
2020-11-12 17:28:04 +0100

    Remove DOMContentLoaded Eventlisteners
    Removing DOMContentLoaded Eventlisteners from
    index files since the scripts
    are embedded using the defer tag.

--- ee/app/assets/javascripts/pages/groups/security/dashboard/show/index.js
+++ ee/app/assets/javascripts/pages/groups/security/dashboard/show/index.js
@@ -1,6 +1,4 @@
 import initSecurityCharts from 'ee/security_dashboard/security_charts_init';
 import { DASHBOARD_TYPES } from 'ee/security_dashboard/store/constants';
 
-document.addEventListener('DOMContentLoaded', () => {
-  initSecurityCharts(document.getElementById('js-group-security-dashboard'), DASHBOARD_TYPES.GROUP);
-});
+initSecurityCharts(document.getElementById('js-group-security-dashboard'), DASHBOARD_TYPES.GROUP);
