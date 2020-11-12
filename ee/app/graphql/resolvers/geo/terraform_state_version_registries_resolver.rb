6cd6c54cf5c04e309f59d946aeb7b075c4f2c3f8
Alex Ives <aives@gitlab.com>
2020-11-12 16:42:22 -0600

    Add resolver types to registry resolvers
    Relates to https://gitlab.com/gitlab-org/gitlab/issues/281697

--- ee/app/graphql/resolvers/geo/terraform_state_version_registries_resolver.rb
+++ ee/app/graphql/resolvers/geo/terraform_state_version_registries_resolver.rb
@@ -3,6 +3,8 @@
 module Resolvers
   module Geo
     class TerraformStateVersionRegistriesResolver < BaseResolver
+      type ::Types::Geo::GeoNodeType.connection_type, null: true
+
       include RegistriesResolver
     end
   end
