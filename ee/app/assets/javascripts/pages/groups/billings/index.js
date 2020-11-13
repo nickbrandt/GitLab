48cdba9c2c989d5be86d362e7bb8bc4b021baa51
Jannik Lehmann <jlehmann@gitlab.com>
2020-11-13 17:34:38 +0100

    Remove DOMContentLoaded Eventlisteners
    Removing DOMContentLoaded Eventlisteners from
    index files since the scripts
    are embedded using the defer tag.

--- ee/app/assets/javascripts/pages/groups/billings/index.js
+++ ee/app/assets/javascripts/pages/groups/billings/index.js
@@ -1,8 +1,5 @@
 import initSubscriptions from 'ee/billings';
 import PersistentUserCallout from '~/persistent_user_callout';
 
-document.addEventListener('DOMContentLoaded', () => {
-  PersistentUserCallout.factory(document.querySelector('.js-gold-trial-callout'));
-
-  initSubscriptions();
-});
+PersistentUserCallout.factory(document.querySelector('.js-gold-trial-callout'));
+initSubscriptions();
