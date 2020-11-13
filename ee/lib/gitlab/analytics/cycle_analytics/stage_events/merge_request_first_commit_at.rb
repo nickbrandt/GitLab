8343b553ba72c91c8d49f95c71f2d00703cb9615
Adam Hegyi <ahegyi@gitlab.com>
2020-11-13 02:37:34 +0000

    Update code stage calculation in VSA
    This change updates the calculation in value stream analytics. For the
    code stage, the start event timestamp can be first_commit_at (new) or
    first_mentioned_in_commit_at.
    
    The change also introduces a separate stage where only the
    first_commit_at is used.

---
+++ ee/lib/gitlab/analytics/cycle_analytics/stage_events/merge_request_first_commit_at.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module Analytics
+    module CycleAnalytics
+      module StageEvents
+        class MergeRequestFirstCommitAt < MetricsBasedStageEvent
+          def self.name
+            s_("CycleAnalyticsEvent|Merge request first commit time")
+          end
+
+          def self.identifier
+            :merge_request_first_commit_at
+          end
+
+          def object_type
+            MergeRequest
+          end
+
+          def timestamp_projection
+            mr_metrics_table[:first_commit_at]
+          end
+        end
+      end
+    end
+  end
+end
