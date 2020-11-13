809b200529510f9a3f6f43c411913257401e93c7
Jannik Lehmann <jlehmann@gitlab.com>
2020-11-13 11:39:03 +0000

    Remove DOMContentLoaded eventlistener

--- ee/app/assets/javascripts/pages/security/dashboard/settings/index.js
+++ ee/app/assets/javascripts/pages/security/dashboard/settings/index.js
@@ -1,5 +1,3 @@
 import initInstanceSecurityDashboardSettings from 'ee/security_dashboard/instance_dashboard_settings_init';
 
-document.addEventListener('DOMContentLoaded', () => {
-  initInstanceSecurityDashboardSettings(document.getElementById('js-security'));
-});
+initInstanceSecurityDashboardSettings(document.getElementById('js-security'));
