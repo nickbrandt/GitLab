3cb1ac27b6b058fbc998c126fd159907bcb8bfb0
Jannik Lehmann <jlehmann@gitlab.com>
2020-11-13 20:03:32 +0000

    Remove DOMContentLoaded Eventlisteners
    Removing DOMContentLoaded Eventlisteners from
    index files since the scripts
    are embedded using the defer tag.

--- ee/app/assets/javascripts/pages/analytics/productivity_analytics/index.js
+++ ee/app/assets/javascripts/pages/analytics/productivity_analytics/index.js
@@ -1,5 +1,3 @@
 import initProductivityAnalyticsApp from 'ee/analytics/productivity_analytics';
 
-document.addEventListener('DOMContentLoaded', () => {
-  initProductivityAnalyticsApp();
-});
+initProductivityAnalyticsApp();
--- ee/app/assets/javascripts/pages/registrations/projects/new/index.js
+++ ee/app/assets/javascripts/pages/registrations/projects/new/index.js
@@ -2,8 +2,6 @@ import mountProgressBar from 'ee/registrations/projects/new';
 import initProjectVisibilitySelector from '~/project_visibility';
 import initProjectNew from '~/projects/project_new';
 
-document.addEventListener('DOMContentLoaded', () => {
-  mountProgressBar();
-  initProjectVisibilitySelector();
-  initProjectNew.bindEvents();
-});
+mountProgressBar();
+initProjectVisibilitySelector();
+initProjectNew.bindEvents();
