79318ba9eedfbfccc6cb62acf5f3f2df4d35d18b
Jannik Lehmann <jlehmann@gitlab.com>
2020-11-16 13:54:24 +0100

    Remove DOMContentLoaded Eventlisteners
    Removing DOMContentLoaded Eventlisteners from
    index files since the scripts
    are embedded using the defer tag.

--- ee/app/assets/javascripts/pages/groups/ldap_group_links/index.js
+++ ee/app/assets/javascripts/pages/groups/ldap_group_links/index.js
@@ -1,7 +1,5 @@
 import initLDAPGroupsSelect from 'ee/ldap_groups_select';
 import initLDAPGroupLinks from 'ee/groups/ldap_group_links';
 
-document.addEventListener('DOMContentLoaded', () => {
-  initLDAPGroupsSelect();
-  initLDAPGroupLinks();
-});
+initLDAPGroupsSelect();
+initLDAPGroupLinks();
--- ee/app/assets/javascripts/pages/groups/sso/index.js
+++ ee/app/assets/javascripts/pages/groups/sso/index.js
@@ -1,7 +1,5 @@
 import UsernameValidator from '~/pages/sessions/new/username_validator';
 import initConfirmDangerModal from '~/confirm_danger_modal';
 
-document.addEventListener('DOMContentLoaded', () => {
-  new UsernameValidator(); // eslint-disable-line no-new
-  initConfirmDangerModal();
-});
+new UsernameValidator(); // eslint-disable-line no-new
+initConfirmDangerModal();
