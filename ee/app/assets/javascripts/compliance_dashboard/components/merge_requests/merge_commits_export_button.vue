e7e244f1c4527d577f1f738d041fda6c9f6f3fc7
Jiaan Louw <3468028-jiaan@users.noreply.gitlab.com>
2020-11-16 13:15:04 +0000

    Add export for merge commit custody report
    This adds a export dropdown to the existing list all
    merge commits button in the Compliance Dashboard, it
    allows for a merge commit-specific chain of custody
    report to be generate for the provided commit hash.

--- ee/app/assets/javascripts/compliance_dashboard/components/merge_requests/merge_commits_export_button.vue
+++ ee/app/assets/javascripts/compliance_dashboard/components/merge_requests/merge_commits_export_button.vue
@@ -1,10 +1,26 @@
 <script>
-import { GlButton, GlTooltip } from '@gitlab/ui';
+import {
+  GlButton,
+  GlDropdown,
+  GlDropdownForm,
+  GlForm,
+  GlFormGroup,
+  GlFormInput,
+  GlTooltip,
+} from '@gitlab/ui';
+
+import { isValidSha1Hash } from '~/lib/utils/text_utility';
 import { __ } from '~/locale';
+import { INPUT_DEBOUNCE, CUSTODY_REPORT_PARAMETER } from '../../constants';
 
 export default {
   components: {
     GlButton,
+    GlDropdown,
+    GlDropdownForm,
+    GlForm,
+    GlFormGroup,
+    GlFormInput,
     GlTooltip,
   },
   props: {
@@ -13,33 +29,85 @@ export default {
       type: String,
     },
   },
+  data() {
+    return {
+      validMergeCommitHash: null,
+      listMergeCommitsButton: null,
+    };
+  },
+  computed: {
+    mergeCommitButtonDisabled() {
+      return !this.validMergeCommitHash;
+    },
+  },
+  mounted() {
+    this.listMergeCommitsButton = this.$refs.listMergeCommitsButton;
+  },
+  methods: {
+    onInput(value) {
+      this.validMergeCommitHash = isValidSha1Hash(value);
+    },
+  },
   strings: {
+    mergeCommitInputLabel: __('Merge commit SHA'),
+    mergeCommitInvalidMessage: __('Invalid hash'),
+    mergeCommitButtonText: __('Export commit custody report'),
     listMergeCommitsButtonText: __('List of all merge commits'),
     exportAsCsv: __('Export as CSV'),
     csvSizeLimit: __('(max size 15 MB)'),
   },
-  data() {
-    return {
-      button: null,
-    };
-  },
-  mounted() {
-    this.button = this.$refs.button;
-  },
+  inputDebounce: INPUT_DEBOUNCE,
+  custodyReportParamater: CUSTODY_REPORT_PARAMETER,
 };
 </script>
 
 <template>
   <div>
-    <gl-button
-      ref="button"
-      :href="mergeCommitsCsvExportPath"
-      icon="export"
-      class="gl-align-self-center"
+    <gl-dropdown split>
+      <template #button-content>
+        <gl-button
+          ref="listMergeCommitsButton"
+          class="gl-p-0!"
+          category="tertiary"
+          icon="export"
+          :href="mergeCommitsCsvExportPath"
+        >
+          {{ $options.strings.listMergeCommitsButtonText }}
+        </gl-button>
+      </template>
+      <gl-dropdown-form>
+        <gl-form :action="mergeCommitsCsvExportPath" method="GET">
+          <gl-form-group
+            :label="$options.strings.mergeCommitInputLabel"
+            :invalid-feedback="$options.strings.mergeCommitInvalidMessage"
+            :state="validMergeCommitHash"
+            label-size="sm"
+            label-for="merge-commits-export-custody-report"
+          >
+            <gl-form-input
+              id="merge-commits-export-custody-report"
+              :name="$options.custodyReportParamater"
+              :debounce="$options.inputDebounce"
+              @input="onInput"
+            />
+          </gl-form-group>
+          <gl-button
+            :disabled="mergeCommitButtonDisabled"
+            type="submit"
+            variant="success"
+            data-test-id="merge-commit-submit-button"
+            class="gl-hover-text-white!"
+            >{{ $options.strings.mergeCommitButtonText }}</gl-button
+          >
+        </gl-form>
+      </gl-dropdown-form>
+    </gl-dropdown>
+    <gl-tooltip
+      v-if="listMergeCommitsButton"
+      :target="listMergeCommitsButton"
+      boundary="viewport"
+      placement="top"
     >
-      {{ $options.strings.listMergeCommitsButtonText }}
-    </gl-button>
-    <gl-tooltip v-if="button" :target="button" boundary="viewport" placement="top">
       <p class="gl-my-0">{{ $options.strings.exportAsCsv }}</p>
       <p class="gl-my-0">{{ $options.strings.csvSizeLimit }}</p>
     </gl-tooltip>
--- ee/spec/frontend/compliance_dashboard/components/merge_requests/merge_commits_export_button_spec.js
+++ ee/spec/frontend/compliance_dashboard/components/merge_requests/merge_commits_export_button_spec.js
@@ -1,38 +1,40 @@
-import { shallowMount } from '@vue/test-utils';
-import { GlButton } from '@gitlab/ui';
+import { mount, shallowMount } from '@vue/test-utils';
+import { GlFormInput, GlForm, GlFormGroup } from '@gitlab/ui';
 
 import MergeCommitsExportButton from 'ee/compliance_dashboard/components/merge_requests/merge_commits_export_button.vue';
+import { INPUT_DEBOUNCE, CUSTODY_REPORT_PARAMETER } from 'ee/compliance_dashboard/constants';
 
 const CSV_EXPORT_PATH = '/merge_commit_reports';
 
 describe('MergeCommitsExportButton component', () => {
   let wrapper;
 
-  const findCsvExportButton = () => wrapper.find(GlButton);
+  const findCommitForm = () => wrapper.find(GlForm);
+  const findCommitInput = () => wrapper.find(GlFormInput);
+  const findCommitInputGroup = () => wrapper.find(GlFormGroup);
+  const findCommitInputFeedback = () => wrapper.find('.invalid-feedback');
+  const findCommitExportButton = () => wrapper.find('[data-test-id="merge-commit-submit-button"]');
+  const findCsvExportButton = () => wrapper.find({ ref: 'listMergeCommitsButton' });
 
-  const createComponent = (props = {}) => {
-    return shallowMount(MergeCommitsExportButton, {
+  const createComponent = ({ mountFn = shallowMount, data = {} } = {}) => {
+    return mountFn(MergeCommitsExportButton, {
       propsData: {
         mergeCommitsCsvExportPath: CSV_EXPORT_PATH,
-        ...props,
       },
+      data: () => data,
     });
   };
-
-  beforeEach(() => {
-    wrapper = createComponent();
-  });
 
   afterEach(() => {
     wrapper.destroy();
   });
 
-  describe('Merge commit CSV export button', () => {
-    it('matches the snapshot', () => {
-      expect(wrapper.element).toMatchSnapshot();
+  describe('Merge commit CSV export all button', () => {
+    beforeEach(() => {
+      wrapper = createComponent({ mountFn: mount });
     });
 
-    it('renders the merge commits csv export button', () => {
+    it('renders the button', () => {
       expect(findCsvExportButton().exists()).toBe(true);
     });
 
@@ -42,6 +44,68 @@ describe('MergeCommitsExportButton component', () => {
 
     it('links to the csv download path', () => {
       expect(findCsvExportButton().attributes('href')).toEqual(CSV_EXPORT_PATH);
+    });
+  });
+
+  describe('Merge commit custody report', () => {
+    beforeEach(() => {
+      wrapper = createComponent();
+    });
+
+    it('renders the input label', () => {
+      expect(findCommitInputGroup().attributes('label')).toBe('Merge commit SHA');
+    });
+
+    it('sets the input debounce time', () => {
+      expect(findCommitInput().attributes('debounce')).toEqual(INPUT_DEBOUNCE.toString());
+    });
+
+    it('sets the input name', () => {
+      expect(findCommitInput().attributes('name')).toEqual(CUSTODY_REPORT_PARAMETER);
+    });
+
+    it('sets the form action to the csv download path', () => {
+      expect(findCommitForm().attributes('action')).toEqual(CSV_EXPORT_PATH);
+    });
+
+    it('sets the invalid input feedback message', () => {
+      wrapper = createComponent({ mountFn: mount });
+
+      expect(findCommitInputFeedback().text()).toBe('Invalid hash');
+    });
+
+    describe('when the commit input is valid', () => {
+      beforeEach(() => {
+        wrapper = createComponent({
+          mountFn: mount,
+          data: { validMergeCommitHash: true },
+        });
+      });
+
+      it('shows that the input is valid', () => {
+        expect(findCommitInputGroup().classes('is-invalid')).toBe(false);
+      });
+
+      it('enables the submit button', () => {
+        expect(findCommitExportButton().props('disabled')).toBe(false);
+      });
+    });
+
+    describe('when the commit input is invalid', () => {
+      beforeEach(() => {
+        wrapper = createComponent({
+          mountFn: mount,
+          data: { validMergeCommitHash: false },
+        });
+      });
+
+      it('shows that the input is invalid', () => {
+        expect(findCommitInputGroup().classes('is-invalid')).toBe(true);
+      });
+
+      it('disables the submit button', () => {
+        expect(findCommitExportButton().props('disabled')).toBe(true);
+      });
     });
   });
 });
