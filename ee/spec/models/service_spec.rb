c2a4bcdf17228c5c9ea121e78101633cf08356d8
Arturo Herrero <arturo.herrero@gmail.com>
2020-11-16 09:46:53 +0000

    Test available services name for EE services

--- ee/spec/models/service_spec.rb
+++ ee/spec/models/service_spec.rb
@@ -3,6 +3,17 @@
 require 'spec_helper'
 
 RSpec.describe Service do
+  let(:ee_project_services) do
+    %w[
+      github
+      jenkins
+    ]
+  end
+
+  describe '.available_services_names' do
+    it { expect(described_class.available_services_names).to include(*ee_project_services) }
+  end
+
   describe '.project_specific_services_names' do
     before do
       allow(::Gitlab).to receive(:com?).and_return(com)
@@ -11,13 +22,13 @@ RSpec.describe Service do
     context 'when not on gitlab.com' do
       let(:com) { false }
 
-      it { expect(described_class.project_specific_services_names).to contain_exactly('github', 'jenkins') }
+      it { expect(described_class.project_specific_services_names).to match_array(ee_project_services) }
     end
 
     context 'when on gitlab.com' do
       let(:com) { true }
 
-      it { expect(described_class.project_specific_services_names).to contain_exactly('github', 'gitlab_slack_application', 'jenkins') }
+      it { expect(described_class.project_specific_services_names).to match_array(ee_project_services + ['gitlab_slack_application']) }
     end
   end
 end
