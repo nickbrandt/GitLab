230a0cfbcd52d5037d10adacf2585b1e76862254
Max Woolf <mwoolf@gitlab.com>
2020-11-17 14:04:04 +0000

    Add destroyservice to compliance frameworks
    Adds a new service to handle destruction
    of compliance frameworks

---
+++ ee/app/policies/compliance_management/framework_policy.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module ComplianceManagement
+  class FrameworkPolicy < BasePolicy
+    delegate { @subject.namespace }
+
+    condition(:custom_compliance_frameworks_enabled) do
+      License.feature_available?(:custom_compliance_frameworks)
+    end
+
+    rule { can?(:owner_access) & custom_compliance_frameworks_enabled }.policy do
+      enable :manage_compliance_framework
+    end
+  end
+end
---
+++ ee/spec/policies/compliance_management/framework_policy_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe ComplianceManagement::FrameworkPolicy do
+  let_it_be(:framework) { create(:compliance_framework) }
+  let(:user) { framework.namespace.owner }
+
+  subject { described_class.new(user, framework) }
+
+  context 'feature is licensed' do
+    before do
+      stub_licensed_features(custom_compliance_frameworks: true)
+    end
+
+    context 'user is namespace owner' do
+      it { is_expected.to be_allowed(:manage_compliance_framework) }
+    end
+
+    context 'user is group owner' do
+      let_it_be(:group) { create(:group) }
+      let_it_be(:framework) { create(:compliance_framework, namespace: group) }
+      let_it_be(:user) { create(:user) }
+
+      before do
+        group.add_owner(user)
+      end
+
+      it { is_expected.to be_allowed(:manage_compliance_framework) }
+    end
+
+    context 'user is not namespace owner' do
+      let(:user) { build(:user) }
+
+      it { is_expected.to be_disallowed(:manage_compliance_framework) }
+    end
+
+    context 'user is an admin', :enable_admin_mode do
+      let(:user) { build(:admin) }
+
+      it { is_expected.to be_allowed(:manage_compliance_framework) }
+    end
+  end
+
+  context 'feature is unlicensed' do
+    before do
+      stub_licensed_features(custom_compliance_frameworks: false)
+    end
+
+    it { is_expected.to be_disallowed(:manage_compliance_framework) }
+  end
+end
