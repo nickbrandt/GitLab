8fb5f42b7f683815c8929cf4a1f05238132156a2
Markus Koller <mkoller@gitlab.com>
2020-11-17 10:55:41 +0100

    Fix repository clone panel for wikis
    This fixes a small bug with wikis where the dropdown label wasn't
    updated correctly when switching the protocol.
    
    Since we want to reuse this for group wikis, this also generalizes the
    code a bit by:
    
    - Moving the JS initializer into its own file.
    - Moving the wiki specs for the clone panel into shared examples, and
      extending it to verify the dropdown switching behaviour.
    - Using `container` rather than `project` in variable names.
    - Using `repositories` rather than `project code` in UI strings.
    - Renaming `enabled_project_button` to `enabled_protocol_button`, to
      make the intent clearer.

--- ee/app/helpers/ee/button_helper.rb
+++ ee/app/helpers/ee/button_helper.rb
@@ -5,38 +5,38 @@ module EE
     extend ::Gitlab::Utils::Override
 
     override :ssh_clone_button
-    def ssh_clone_button(project, append_link: true)
+    def ssh_clone_button(container, append_link: true)
       return super unless ::Gitlab::Geo.secondary?
 
       if ::Gitlab::CurrentSettings.user_show_add_ssh_key_message? &&
           current_user.try(:require_ssh_key?)
-        dropdown_description = _("You won't be able to pull or push project code via SSH until you add an SSH key to your profile")
+        dropdown_description = s_("MissingSSHKeyWarningLink|You won't be able to pull or push repositories via SSH until you add an SSH key to your profile")
       end
 
-      append_url = project.ssh_url_to_repo if append_link
-      geo_url = geo_primary_http_url_to_repo(project)
+      append_url = container.ssh_url_to_repo if append_link
+      geo_url = geo_primary_http_url_to_repo(container)
 
       dropdown_item_with_description('SSH', dropdown_description, href: append_url, data: { primary_url: geo_url, clone_type: 'ssh' })
     end
 
     override :http_clone_button
-    def http_clone_button(project, append_link: true)
+    def http_clone_button(container, append_link: true)
       return super unless ::Gitlab::Geo.secondary?
 
       protocol = gitlab_config.protocol.upcase
       dropdown_description = http_dropdown_description(protocol)
-      append_url = project.http_url_to_repo if append_link
-      geo_url = geo_primary_http_url_to_repo(project)
+      append_url = container.http_url_to_repo if append_link
+      geo_url = geo_primary_http_url_to_repo(container)
 
       dropdown_item_with_description(protocol, dropdown_description, href: append_url, data: { primary_url: geo_url, clone_type: 'http' })
     end
 
-    def kerberos_clone_button(project)
+    def kerberos_clone_button(container)
       klass = 'kerberos-selector has-tooltip'
 
       content_tag :a, 'KRB5',
         class: klass,
-        href: project.kerberos_url_to_repo,
+        href: container.kerberos_url_to_repo,
         data: {
           html: 'true',
           placement: 'right',
