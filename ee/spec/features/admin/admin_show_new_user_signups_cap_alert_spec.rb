9f93fd4d0f76c2186bb080a0253c0936d2004c77
Vitaly Slobodin <vslobodin@gitlab.com>
2020-11-19 14:14:40 +0000

    Add banner for new user signups cap alert
    Add banner for notyfing admins about reached
    cap for new user signups.

---
+++ ee/spec/features/admin/admin_show_new_user_signups_cap_alert_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'displays new user signups cap alert', :js do
+  let_it_be(:admin) { create(:admin) }
+  let(:help_page_href) { help_page_path('user/admin_area/settings/sign_up_restrictions.md') }
+  let(:expected_content) { 'Your instance has reached its user cap' }
+
+  context 'when reached active users cap' do
+    before do
+      allow(User).to receive(:billable).and_return((0..9))
+      allow(Gitlab::CurrentSettings.current_application_settings).to receive(:new_user_signups_cap).and_return(9)
+
+      gitlab_sign_in(admin)
+    end
+
+    it 'displays and dismiss alert' do
+      expect(page).to have_content(expected_content)
+      expect(page).to have_link('usage caps', href: help_page_href)
+
+      visit root_dashboard_path
+      find('.js-new-user-signups-cap-reached .gl-alert-dismiss').click
+
+      expect(page).not_to have_content(expected_content)
+      expect(page).not_to have_link('usage caps', href: help_page_href)
+    end
+  end
+end
---
+++ ee/spec/requests/callout_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'callout alerts' do
+  include Capybara::RSpecMatchers
+
+  describe 'new_user_signups_cap_reached' do
+    let_it_be(:user) { create(:admin) }
+    let(:enabled) { true }
+    let(:billable_users) { [double(:billable_user)] }
+    let(:help_page_href) { help_page_path('user/admin_area/settings/sign_up_restrictions.md') }
+    let(:expected_content) { 'Your instance has reached its user cap' }
+
+    shared_examples_for 'a visible alert' do
+      it 'shows the alert' do
+        get root_dashboard_path
+
+        expect(response.body).to include(expected_content)
+        expect(response.body).to have_link('usage caps', href: help_page_href)
+      end
+    end
+
+    shared_examples_for 'a hidden alert' do
+      it 'does not show the alert' do
+        get root_dashboard_path
+
+        expect(response.body).not_to include(expected_content)
+      end
+    end
+
+    before do
+      stub_feature_flags(admin_new_user_signups_cap: enabled)
+      stub_application_setting(new_user_signups_cap: 1)
+      allow(User).to receive(:billable).and_return(billable_users)
+
+      login_as(user)
+    end
+
+    context 'when cap reached' do
+      it_behaves_like 'a visible alert'
+    end
+
+    context 'when cap not reached' do
+      let(:billable_users) { [] }
+
+      it_behaves_like 'a hidden alert'
+    end
+
+    context 'when feature disabled' do
+      let(:enabled) { false }
+
+      it_behaves_like 'a hidden alert'
+    end
+
+    context 'when user is not admin' do
+      let_it_be(:user) { create(:user) }
+
+      it_behaves_like 'a hidden alert'
+    end
+  end
+end
