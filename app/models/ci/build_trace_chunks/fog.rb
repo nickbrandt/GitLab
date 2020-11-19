3a14d340e5b9420508c42c330f1c3cc66015b4d6
Stan Hu <stanhu@gmail.com>
2020-11-18 20:53:11 -0800

    Introduce ci_live_trace_use_fog_attributes feature flag
    This commit reverts some of the previous commit to be more amenable for
    adding a feature feature flag to avoid introducing a bug in the CI live
    trace functionality.

--- app/models/ci/build_trace_chunks/fog.rb
+++ app/models/ci/build_trace_chunks/fog.rb
@@ -4,7 +4,7 @@ module Ci
   module BuildTraceChunks
     class Fog
       def available?
-        object_store_config.enabled?
+        object_store.enabled
       end
 
       def data(model)
@@ -14,7 +14,15 @@ module Ci
       end
 
       def set_data(model, new_data)
-        files.create(create_attributes(model, new_data))
+        if Feature.enabled?(:ci_live_trace_use_fog_attributes)
+          files.create(create_attributes(model, new_data))
+        else
+          # TODO: Support AWS S3 server side encryption
+          files.create({
+            key: key(model),
+            body: new_data
+          })
+        end
       end
 
       def append_data(model, new_data, offset)
@@ -67,13 +75,13 @@ module Ci
       def bucket_name
         return unless available?
 
-        object_store_config.bucket
+        object_store.remote_directory
       end
 
       def connection
         return unless available?
 
-        @connection ||= ::Fog::Storage.new(object_store_config.credentials)
+        @connection ||= ::Fog::Storage.new(object_store.connection.to_hash.deep_symbolize_keys)
       end
 
       def fog_directory
@@ -84,8 +92,12 @@ module Ci
         @files ||= fog_directory.files
       end
 
-      def object_store_raw_config
+      def object_store
         Gitlab.config.artifacts.object_store
+      end
+
+      def object_store_raw_config
+        object_store
       end
 
       def object_store_config
