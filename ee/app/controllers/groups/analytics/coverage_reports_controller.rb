7cff9f31e64288b29db3fe48bb3b3d3a0f9dd5d0
drew cimino <dcimino@gitlab.com>
2020-11-20 17:30:01 -0500

    Double-splat a kwargs hash

--- ee/app/controllers/groups/analytics/coverage_reports_controller.rb
+++ ee/app/controllers/groups/analytics/coverage_reports_controller.rb
@@ -11,7 +11,7 @@ class Groups::Analytics::CoverageReportsController < Groups::Analytics::Applicat
   def index
     respond_to do |format|
       format.csv do
-        track_event(:download_code_coverage_csv, download_tracker_params)
+        track_event(:download_code_coverage_csv, **download_tracker_params)
         send_data(render_csv(report_results), type: 'text/csv; charset=utf-8')
       end
     end
