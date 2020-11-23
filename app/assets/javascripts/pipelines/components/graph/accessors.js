3ec8f408e67305f8ab5888052d580f67ff77bea9
Sarah GP <sarah.groff.palermo@gmail.com>
2020-11-23 11:30:33 +0100

    Changes for accessors
    Squashes 22 commits for rebase and applies
    previous changes
    
    Adjusts mounting funcs, adds query, specs

--- app/assets/javascripts/pipelines/components/graph/accessors.js
+++ app/assets/javascripts/pipelines/components/graph/accessors.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { REST, GRAPHQL } from './constants';
 
 export const accessors = {
@@ -8,3 +9,30 @@ export const accessors = {
     groupId: 'name',
   },
 };
+=======
+import { get } from 'lodash';
+import { REST, GRAPHQL } from './constants';
+
+const accessors = {
+  [REST]: {
+    detailsPath: 'details_path',
+    groupId: 'id',
+    hasDetails: 'has_details',
+    pipelineStatus: ['details', 'status'],
+    sourceJob: ['source_job', 'name'],
+  },
+  [GRAPHQL]: {
+    detailsPath: 'detailsPath',
+    groupId: 'name',
+    hasDetails: 'hasDetails',
+    pipelineStatus: 'status',
+    sourceJob: ['sourceJob', 'name'],
+  },
+};
+
+const accessValue = (dataMethod, prop, item) => {
+  return get(item, accessors[dataMethod][prop]);
+};
+
+export { accessors, accessValue };
+>>>>>>> 5a4bf1b75d7 (Changes for accessors)
