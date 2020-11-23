9a705346a201d6f0ef5b41c426db68bb507e1125
dcouture <dcouture@gitlab.com>
2020-11-23 12:07:15 -0500

    Rename helper to reflect its new role
    It handles more than the `defer` attribute now so it's
    renamed to GitlabScriptTagHelper

--- app/helpers/gitlab_script_tag_helper.rb
+++ app/helpers/gitlab_script_tag_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module DeferScriptTagHelper
+module GitlabScriptTagHelper
   # Override the default ActionView `javascript_include_tag` helper to support page specific deferred loading.
   # PLEASE NOTE: `defer` is also critical so that we don't run JavaScript entrypoints before the DOM is ready.
   # Please see https://gitlab.com/groups/gitlab-org/-/epics/4538#note_432159769.
