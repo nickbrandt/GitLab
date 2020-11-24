6254a0bb1cda433b8fda002439ec7829cb580a93
Coung Ngo <cngo@gitlab.com>
2020-11-24 13:09:40 +0000

    Add epic autocompletion to GfmAutocomplete component
    Add epic autocompletion as part of migrating from jQuery-based
    at.js to tribute

---
+++ ee/app/assets/javascripts/vue_shared/components/gfm_autocomplete/utils.js
@@ -0,0 +1,24 @@
+import { escape } from 'lodash';
+import {
+  GfmAutocompleteType as GfmAutocompleteTypeFoss,
+  tributeConfig as tributeConfigFoss,
+} from '~/vue_shared/components/gfm_autocomplete/utils';
+
+export const GfmAutocompleteType = {
+  ...GfmAutocompleteTypeFoss,
+  Epics: 'epics',
+};
+
+export const tributeConfig = {
+  ...tributeConfigFoss,
+
+  [GfmAutocompleteType.Epics]: {
+    config: {
+      trigger: '&',
+      fillAttr: 'iid',
+      lookup: value => `${value.iid}${value.title}`,
+      menuItemTemplate: ({ original }) =>
+        `<small>${original.iid}</small> ${escape(original.title)}`,
+    },
+  },
+};
---
+++ ee/spec/frontend/vue_shared/components/gfm_autocomplete/utils_spec.js
@@ -0,0 +1,31 @@
+import {
+  GfmAutocompleteType,
+  tributeConfig,
+} from 'ee/vue_shared/components/gfm_autocomplete/utils';
+
+describe('ee gfm_autocomplete/utils', () => {
+  describe('epics config', () => {
+    const epicsConfig = tributeConfig[GfmAutocompleteType.Epics].config;
+    const epic = {
+      id: null,
+      iid: 123456,
+      title: "Epic title <script>alert('hi')</script>",
+    };
+
+    it('uses & as the trigger', () => {
+      expect(epicsConfig.trigger).toBe('&');
+    });
+
+    it('inserts the iid on autocomplete selection', () => {
+      expect(epicsConfig.fillAttr).toBe('iid');
+    });
+
+    it('searches using both the iid and title', () => {
+      expect(epicsConfig.lookup(epic)).toBe(`${epic.iid}${epic.title}`);
+    });
+
+    it('shows the iid and title in the menu item', () => {
+      expect(epicsConfig.menuItemTemplate({ original: epic })).toMatchSnapshot();
+    });
+  });
+});
