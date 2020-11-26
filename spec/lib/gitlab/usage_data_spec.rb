ae4b6341a53620bcea00fd3bbc8072d331805823
Francisco Javier López <fjlopez@gitlab.com>
2020-11-26 14:41:50 +0000

    Track SSE edit action
    In this commit we track the edit action for the SSE, when an
    MR is created through it. For that we track the request referer.

--- spec/lib/gitlab/usage_data_spec.rb
+++ spec/lib/gitlab/usage_data_spec.rb
@@ -1122,6 +1122,12 @@ RSpec.describe Gitlab::UsageData, :aggregate_failures do
 
       counter.track_web_ide_edit_action(author: user3, time: time - 3.days)
       counter.track_snippet_editor_edit_action(author: user3)
+
+      counter.track_sse_edit_action(author: user1)
+      counter.track_sse_edit_action(author: user1)
+      counter.track_sse_edit_action(author: user2)
+      counter.track_sse_edit_action(author: user3)
+      counter.track_sse_edit_action(author: user2, time: time - 3.days)
     end
 
     it 'returns the distinct count of user actions within the specified time period' do
@@ -1134,7 +1140,8 @@ RSpec.describe Gitlab::UsageData, :aggregate_failures do
           action_monthly_active_users_web_ide_edit: 2,
           action_monthly_active_users_sfe_edit: 2,
           action_monthly_active_users_snippet_editor_edit: 2,
-          action_monthly_active_users_ide_edit: 3
+          action_monthly_active_users_ide_edit: 3,
+          action_monthly_active_users_sse_edit: 3
         }
       )
     end
