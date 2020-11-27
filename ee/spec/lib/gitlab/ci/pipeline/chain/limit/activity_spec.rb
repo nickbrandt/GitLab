410d76d67bc939a617531cc83f7394e60db8bfde
Hordur Freyr Yngvason <hfyngvason@gitlab.com>
2020-11-27 17:26:36 +0000

    Add limits for deployments per pipeline
    See https://gitlab.com/gitlab-org/gitlab/-/issues/24087

--- ee/spec/lib/gitlab/ci/pipeline/chain/limit/activity_spec.rb
+++ ee/spec/lib/gitlab/ci/pipeline/chain/limit/activity_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe ::Gitlab::Ci::Pipeline::Chain::Limit::Activity do
 
     it 'logs the error' do
       expect(Gitlab::ErrorTracking).to receive(:track_exception).with(
-        instance_of(EE::Gitlab::Ci::Limit::LimitExceededError),
+        instance_of(Gitlab::Ci::Limit::LimitExceededError),
         project_id: project.id, plan: namespace.actual_plan_name
       )
 
