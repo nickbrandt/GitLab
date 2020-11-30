d5bbc2422e3477f229d2348ff01fd6667e1aad57
Tim Zallmann <tzallmann@gitlab.com>
2020-11-30 17:58:06 +0100

    Added bootstrap collapse also for tree shaking

--- app/assets/javascripts/commons/bootstrap.js
+++ app/assets/javascripts/commons/bootstrap.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 // bootstrap jQuery plugins
 import 'bootstrap/js/dist/alert';
 import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/popover';
