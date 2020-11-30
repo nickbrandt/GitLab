6622130a6f328c826133a2a86d256a5f73db104c
Tim Zallmann <tzallmann@gitlab.com>
2020-11-30 14:34:20 +0100

    Imported also bootstrap popover

--- app/assets/javascripts/commons/bootstrap.js
+++ app/assets/javascripts/commons/bootstrap.js
@@ -5,8 +5,9 @@ import 'bootstrap/js/dist/alert';
 import 'bootstrap/js/dist/button';
 import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/popover';
 import 'bootstrap/js/dist/tooltip';
+import 'bootstrap/js/dist/tab';
 
 // custom jQuery functions
 $.fn.extend({
