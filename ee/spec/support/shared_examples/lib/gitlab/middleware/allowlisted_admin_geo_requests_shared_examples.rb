e9bd175c9593198c5c2ac57bd403d21e15436ae3
Aakriti Gupta <agupta@gitlab.com>
2020-12-01 11:47:53 +0100

    Remove redundant expectation in test

--- ee/spec/support/shared_examples/lib/gitlab/middleware/allowlisted_admin_geo_requests_shared_examples.rb
+++ ee/spec/support/shared_examples/lib/gitlab/middleware/allowlisted_admin_geo_requests_shared_examples.rb
@@ -3,7 +3,6 @@
 RSpec.shared_examples 'allowlisted /admin/geo requests' do
   shared_examples 'allowlisted request' do |request_type, request_url|
     it "expects a #{request_type.upcase} request to #{request_url} to be allowed" do
-      expect(Rails.application.routes).to receive(:recognize_path).and_call_original
       response = request.send(request_type, request_url)
 
       expect(response).not_to be_redirect
