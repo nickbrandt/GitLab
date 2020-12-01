20e1817a58bb6ac0dea54ee07f416c0a54359419
Fabio Pitino <fpitino@gitlab.com>
2020-12-01 18:48:46 +0000

    Move shared_runners_remaining_minutes to Ci::Minutes::Quota
    Namespace#shared_runners_remaining_minutes_percent moved to
    Ci::Minutes::Quota#percent_total_minutes_remaining.
    
    Dropped unused method shared_runners_remaining_minutes_below_threshold?

--- ee/app/models/ee/namespace.rb
+++ ee/app/models/ee/namespace.rb
@@ -249,17 +249,6 @@ module EE
         actual_shared_runners_minutes_limit.nonzero?
     end
 
-    def shared_runners_remaining_minutes_percent
-      return 0 if shared_runners_remaining_minutes.to_f <= 0
-      return 0 if actual_shared_runners_minutes_limit.to_f == 0
-
-      (shared_runners_remaining_minutes.to_f * 100) / actual_shared_runners_minutes_limit.to_f
-    end
-
-    def shared_runners_remaining_minutes_below_threshold?
-      shared_runners_remaining_minutes_percent.to_i <= last_ci_minutes_usage_notification_level.to_i
-    end
-
     def any_project_with_shared_runners_enabled?
       all_projects.with_shared_runners.any?
     end
@@ -427,10 +416,6 @@ module EE
         start_date: created_at,
         seats: 0
       )
-    end
-
-    def shared_runners_remaining_minutes
-      [actual_shared_runners_minutes_limit.to_f - ci_minutes_quota.total_minutes_used.to_f, 0].max
     end
 
     def total_repository_size_excess_calculation(repository_size_limit, project_level: true)
--- ee/spec/models/project_spec.rb
+++ ee/spec/models/project_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe Project do
 
     it { is_expected.to delegate_method(:actual_shared_runners_minutes_limit).to(:shared_runners_limit_namespace) }
     it { is_expected.to delegate_method(:shared_runners_minutes_limit_enabled?).to(:shared_runners_limit_namespace) }
-    it { is_expected.to delegate_method(:shared_runners_remaining_minutes_below_threshold?).to(:shared_runners_limit_namespace) }
 
     it { is_expected.to delegate_method(:closest_gitlab_subscription).to(:namespace) }
 
