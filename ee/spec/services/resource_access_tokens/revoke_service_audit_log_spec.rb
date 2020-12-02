3fb6618a5722475fa4b4b8a77094481c2ba473e8
serenafang <sfang@gitlab.com>
2020-12-02 23:48:24 +0000

    New spec file
    Add ee resource token revoke spec file

--- ee/spec/services/resource_access_tokens/revoke_service_audit_log_spec.rb
+++ ee/spec/services/resource_access_tokens/revoke_service_audit_log_spec.rb
@@ -2,18 +2,18 @@
 
 require 'spec_helper'
 
-RSpec.describe PersonalAccessTokens::RevokeService do
+RSpec.describe ResourceAccessTokens::RevokeService do
   describe '#execute' do
     subject { service.execute }
 
-    let(:user) { create(:user) }
+    let(:user) { create(:user, :project_bot) }
     let(:token) { create(:personal_access_token, user: user) }
     let(:service) { described_class.new(user, token: token) }
 
     it 'creates audit logs' do
       expect(::AuditEventService)
         .to receive(:new)
-        .with(user, user, action: :custom, custom_message: "Revoked personal access token with id #{token.id}")
+        .with(user, user, action: :custom, custom_message: "Revoked project access token with id #{token.id}")
         .and_call_original
 
       subject
