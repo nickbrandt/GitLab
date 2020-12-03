3eb079ea51cdf5c6ae0f5d1a753919cc7c422f2c
David O'Regan <doregan@gitlab.com>
2020-12-03 16:04:15 +0000

    Add rotation modal
    Add the base modal for adding
    a new rotation in oncall
    schedules.

---
+++ ee/spec/frontend/oncall_schedule/mocks/apollo_mock.js
@@ -0,0 +1,16 @@
+export const participants = [
+  {
+    id: '1',
+    username: 'test',
+    name: 'test',
+    avatar: '',
+    avatarUrl: '',
+  },
+  {
+    id: '2',
+    username: 'hello',
+    name: 'hello',
+    avatar: '',
+    avatarUrl: '',
+  },
+];
