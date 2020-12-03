3431e39bb724ae17fa872890ad04a6ef64434393
Doug Stull <dstull@gitlab.com>
2020-12-03 11:56:56 -0500

    Remove unnecessary js loading for ldap from admin/groups/edit

--- ee/app/assets/javascripts/pages/admin/groups/edit/index.js
+++ ee/app/assets/javascripts/pages/admin/groups/edit/index.js
@@ -1,7 +1,3 @@
-import initLDAPGroupsSelect from 'ee/ldap_groups_select';
-import initLDAPGroupLinks from 'ee/groups/ldap_group_links';
 import { pipelineMinutes } from '../../users/pipeline_minutes';
 
-initLDAPGroupsSelect();
-initLDAPGroupLinks();
 pipelineMinutes();
