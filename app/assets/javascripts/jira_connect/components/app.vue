c4fd3cc5898af527c3eddb05630f6ecbe5ec6a05
Justin Ho <hduong@gitlab.com>
2020-12-09 20:42:51 +0700

    Add path to namespaces API endpoint
    - Add global translations in <templates> tags
    - Add GitLab UI setup code (as per GitLab UI readme)
    - Add guard clauses to prevent errors while working on
    local development.

--- app/assets/javascripts/jira_connect/components/app.vue
+++ app/assets/javascripts/jira_connect/components/app.vue
@@ -1,6 +1,12 @@
 <script>
 export default {
   name: 'JiraConnectApp',
+  props: {
+    namespacesEndpoint: {
+      type: String,
+      required: true,
+    },
+  },
   computed: {
     state() {
       return this.$root.$data.state || {};
@@ -11,6 +17,7 @@ export default {
   },
 };
 </script>
+
 <template>
   <div></div>
 </template>
--- app/assets/javascripts/jira_connect/index.js
+++ app/assets/javascripts/jira_connect/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import $ from 'jquery';
+import setConfigs from '@gitlab/ui/dist/config';
 import App from './components/app.vue';
+import Translate from '~/vue_shared/translate';
 
 const store = {
   state: {
@@ -27,12 +29,14 @@ const initJiraFormHandlers = () => {
     alert(error);
   };
 
-  AP.getLocation((location) => {
-    $('.js-jira-connect-sign-in').each(function updateSignInLink() {
-      const updatedLink = `${$(this).attr('href')}?return_to=${location}`;
-      $(this).attr('href', updatedLink);
+  if (typeof AP.getLocation === 'function') {
+    AP.getLocation(location => {
+      $('.js-jira-connect-sign-in').each(function updateSignInLink() {
+        const updatedLink = `${$(this).attr('href')}?return_to=${location}`;
+        $(this).attr('href', updatedLink);
+      });
     });
-  });
+  }
 
   $('#add-subscription-form').on('submit', function onAddSubscriptionForm(e) {
     const actionUrl = $(this).attr('action');
@@ -71,9 +75,18 @@ const initJiraFormHandlers = () => {
 };
 
 function initJiraConnect() {
+  setConfigs();
+  Vue.use(Translate);
+
   const el = document.querySelector('.js-jira-connect-app');
 
   initJiraFormHandlers();
+
+  if (!el) {
+    return null;
+  }
+
+  const { namespacesEndpoint } = el.dataset;
 
   return new Vue({
     el,
@@ -81,7 +94,11 @@ function initJiraConnect() {
       state: store.state,
     },
     render(createElement) {
-      return createElement(App, {});
+      return createElement(App, {
+        props: {
+          namespacesEndpoint,
+        },
+      });
     },
   });
 }
