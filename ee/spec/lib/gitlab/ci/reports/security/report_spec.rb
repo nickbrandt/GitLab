d0951b236999aea9564811dd04a328183cfb0994
Mehmet Emin INAC <minac@gitlab.com>
2020-12-11 05:29:09 +0100

    Extend Security::Report interface with project methods

--- ee/spec/lib/gitlab/ci/reports/security/report_spec.rb
+++ ee/spec/lib/gitlab/ci/reports/security/report_spec.rb
@@ -5,10 +5,13 @@ require 'spec_helper'
 RSpec.describe Gitlab::Ci::Reports::Security::Report do
   let_it_be(:pipeline) { create(:ci_pipeline) }
 
-  let(:report) { described_class.new('sast', pipeline, created_at) }
   let(:created_at) { 2.weeks.ago }
 
+  subject(:report) { described_class.new('sast', pipeline, created_at) }
+
   it { expect(report.type).to eq('sast') }
+  it { is_expected.to delegate_method(:project).to(:pipeline) }
+  it { is_expected.to delegate_method(:id).to(:project).with_prefix }
 
   describe '#add_scanner' do
     let(:scanner) { create(:ci_reports_security_scanner, external_id: 'find_sec_bugs') }
