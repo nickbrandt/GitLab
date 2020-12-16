bb5ec122f8ef546ba980886c76085561f6667fd1
Alex Kalderimis <akalderimis@gitlab.com>
2020-12-16 17:24:17 +0000

    Change type of CiJob.needs
    This changes the type of `CiJob.needs` from `JobType.connection_type`
    to `BuildNeedType.connection_type`. This is because `Build#needs` does
    not return a relation of builds, but one of `Ci::BuildNeed`.
    
    This change is a backwards compatible bug-fix.
    Any query that works now will continue to work.

--- app/graphql/types/ci/stage_type.rb
+++ app/graphql/types/ci/stage_type.rb
@@ -9,6 +9,7 @@ module Types
       field :name, GraphQL::STRING_TYPE, null: true,
         description: 'Name of the stage'
       field :groups, Ci::GroupType.connection_type, null: true,
+        extras: [:lookahead],
         description: 'Group of jobs for the stage'
       field :detailed_status, Types::Ci::DetailedStatusType, null: true,
             description: 'Detailed status of the stage'
@@ -18,24 +19,42 @@ module Types
       end
 
       # Issues one query per pipeline
-      def groups
-        BatchLoader::GraphQL.for([object.pipeline, object]).batch(default_value: []) do |keys, loader|
+      def groups(lookahead:)
+        needs_selected = %i[nodes jobs nodes]
+          .reduce(lookahead) { |q, f| q.selection(f) }
+          .selects?(:needs)
+        key = [object.pipeline, object, needs_selected]
+
+        BatchLoader::GraphQL.for(key).batch(default_value: []) do |keys, loader|
           by_pipeline = keys.group_by(&:first)
+          include_needs = keys.any? { |k| k[2] }
 
           by_pipeline.each do |pl, key_group|
             project = pl.project
             stages = key_group.map(&:second).uniq
             indexed = stages.index_by(&:id)
-            results = pl.latest_statuses.where(stage_id: stages.map(&:id)) # rubocop: disable CodeReuse/ActiveRecord
 
-            results.group_by(&:stage_id).each do |stage_id, statuses|
+            jobs_for_pipeline(pl, stages.map(&:id), include_needs).each do |stage_id, statuses|
               stage = indexed[stage_id]
               groups = ::Ci::Group.fabricate(project, stage, statuses)
-              loader.call([pl, stage], groups)
+              # we don't know (and do not care) whether this set of jobs was
+              # loaded with needs preloaded as part of the key.
+              [true, false].each { |b| loader.call([pl, stage, b], groups) }
             end
           end
         end
       end
+
+      private
+
+      # rubocop: disable CodeReuse/ActiveRecord
+      def jobs_for_pipeline(pipeline, stage_ids, include_needs)
+        results = pipeline.latest_statuses.where(stage_id: stage_ids)
+        results = results.preload(:needs) if include_needs
+
+        results.group_by(&:stage_id)
+      end
+      # rubocop: enable CodeReuse/ActiveRecord
     end
   end
 end
