8d99ce595944f555069aa3133b5e5dec25af8096
Alper Akgun <aakgun@gitlab.com>
2020-12-17 10:53:13 +0000

    Add optional "start a trial" in experimental free SaaS signup
    Growth conversion experiment behind a feature flag

--- ee/app/assets/javascripts/pages/trials/country_select.js
+++ ee/app/assets/javascripts/pages/trials/country_select.js
@@ -4,21 +4,24 @@ import { __ } from '~/locale';
 import { deprecatedCreateFlash as Flash } from '~/flash';
 
 const selectElement = document.getElementById('country_select');
-const { countriesEndPoint, selectedOption } = selectElement.dataset;
 
-axios
-  .get(countriesEndPoint)
-  .then(({ data }) => {
-    // fill #country_select element with array of <option>s
-    data.forEach(([name, code]) => {
-      const option = document.createElement('option');
-      option.value = code;
-      option.text = name;
+if (selectElement?.dataset) {
+  const { countriesEndPoint, selectedOption } = selectElement.dataset;
 
-      selectElement.appendChild(option);
-    });
-    $(selectElement)
-      .val(selectedOption)
-      .trigger('change.select2');
-  })
-  .catch(() => new Flash(__('Error loading countries data.')));
+  axios
+    .get(countriesEndPoint)
+    .then(({ data }) => {
+      // fill #country_select element with array of <option>s
+      data.forEach(([name, code]) => {
+        const option = document.createElement('option');
+        option.value = code;
+        option.text = name;
+
+        selectElement.appendChild(option);
+      });
+      $(selectElement)
+        .val(selectedOption)
+        .trigger('change.select2');
+    })
+    .catch(() => new Flash(__('Error loading countries data.')));
+}
