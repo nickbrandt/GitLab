4243c149c04da9f21b48903fa0a7ea6af17a7429
Robert May <rmay@gitlab.com>
2020-12-17 13:56:41 +0000

    Add spec coverage of timestamp setting

--- spec/services/resource_events/change_state_service_spec.rb
+++ spec/services/resource_events/change_state_service_spec.rb
@@ -30,6 +30,17 @@ RSpec.describe ResourceEvents::ChangeStateService do
 
         expect_event_source(event, source)
       end
+
+      it "sets the created_at timestamp from the system_note_timestamp" do
+        Timecop.freeze do
+          resource.system_note_timestamp = Time.at(43)
+
+          described_class.new(user: user, resource: resource).execute(status: state, mentionable_source: source)
+          event = resource.resource_state_events.last
+
+          expect(event.created_at).to eq(Time.at(43))
+        end
+      end
     end
   end
 
