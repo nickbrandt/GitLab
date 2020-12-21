65756a48bb5b5e8d04b034ee26a1296256395f95
Mehmet Emin INAC <minac@gitlab.com>
2020-12-21 10:36:14 +0100

    Truncate the sample data to prevent validation failure

--- ee/spec/factories/dast_site_profiles.rb
+++ ee/spec/factories/dast_site_profiles.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :dast_site_profile do
     sequence :name do |i|
-      "#{FFaker::Product.product_name} - #{i}"
+      "#{FFaker::Product.product_name.truncate(200)} - #{i}"
     end
 
     before(:create) do |dast_site_profile|
