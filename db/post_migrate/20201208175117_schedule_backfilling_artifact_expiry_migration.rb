3aa9c110ba58526ee0c936032cfe91a25ac91e97
Andreas Brandl <abrandl@gitlab.com>
2020-12-21 17:33:18 +0100

    Adjust scheduling migration

--- db/post_migrate/20201208175117_schedule_backfilling_artifact_expiry_migration.rb
+++ db/post_migrate/20201208175117_schedule_backfilling_artifact_expiry_migration.rb
@@ -4,25 +4,35 @@ class ScheduleBackfillingArtifactExpiryMigration < ActiveRecord::Migration[6.0]
   include Gitlab::Database::MigrationHelpers
 
   DOWNTIME = false
-  INDEX_NAME = 'expired_artifacts_temp_index'
-  INDEX_CONDITION = 'expire_at IS NULL'
+  SWITCH_DATE = Time.utc(2020, 6, 22).freeze
+  INDEX_NAME = 'expired_artifacts_temp_index'.freeze
+  INDEX_CONDITION = "expire_at IS NULL AND created_at < '#{SWITCH_DATE}'"
 
   disable_ddl_transaction!
+
+  class JobArtifact < ActiveRecord::Base
+    include EachBatch
+
+    self.table_name = 'ci_job_artifacts'
+
+    scope :without_expiry_date, -> { where(expire_at: nil) }
+    scope :before_switch, -> { where('created_at < ?', SWITCH_DATE) }
+  end
 
   def up
     # Create temporary index for expired artifacts
     # Needs to be removed in a later migration
-    add_concurrent_index(:ci_job_artifacts, :expire_at, where: INDEX_CONDITION, name: INDEX_NAME)
+    add_concurrent_index(:ci_job_artifacts, %i(id created_at), where: INDEX_CONDITION, name: INDEX_NAME)
 
     queue_background_migration_jobs_by_range_at_intervals(
-      ::Ci::JobArtifact.where(expire_at: nil),
+      JobArtifact.without_expiry_date.before_switch,
       ::Gitlab::BackgroundMigration::BackfillArtifactExpiryDate,
       2.minutes,
-      batch_size: 100_000
+      batch_size: 200_000
     )
   end
 
   def down
-    # no-op
+    remove_concurrent_index_by_name :ci_job_artifacts, INDEX_NAME
   end
 end
