42a500c7acfbdad89f182bab6bb2b8746b8e97a8
mfluharty <mfluharty@gitlab.com>
2020-12-22 15:29:13 +0000

    Display status reason as subheading
    Backend returns a reason for the status
    Why not put it underneath the error message in the widget

--- app/assets/javascripts/reports/codequality_report/store/mutations.js
+++ app/assets/javascripts/reports/codequality_report/store/mutations.js
@@ -14,12 +14,14 @@ export default {
   },
   [types.RECEIVE_REPORTS_SUCCESS](state, data) {
     state.hasError = false;
+    state.statusReason = '';
     state.isLoading = false;
     state.newIssues = data.newIssues;
     state.resolvedIssues = data.resolvedIssues;
   },
-  [types.RECEIVE_REPORTS_ERROR](state) {
+  [types.RECEIVE_REPORTS_ERROR](state, error) {
     state.isLoading = false;
     state.hasError = true;
+    state.statusReason = error?.response?.data?.status_reason;
   },
 };
