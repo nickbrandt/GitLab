354e5ecb51c68478d13743232d609fd9ce369caf
Justin Ho <hduong@gitlab.com>
2020-12-24 13:52:04 +0700

    Add pagination for groups listing

--- app/assets/javascripts/jira_connect/components/groups_list.vue
+++ app/assets/javascripts/jira_connect/components/groups_list.vue
@@ -1,15 +1,17 @@
 <script>
-import { GlTabs, GlTab, GlLoadingIcon } from '@gitlab/ui';
+import { GlTabs, GlTab, GlLoadingIcon, GlPagination } from '@gitlab/ui';
 import axios from '~/lib/utils/axios_utils';
 import { __ } from '~/locale';
 import { deprecatedCreateFlash as createFlash } from '~/flash';
 import GroupsListItem from './groups_list_item.vue';
+import { parseIntPagination, normalizeHeaders } from '~/lib/utils/common_utils';
 
 export default {
   components: {
     GlTabs,
     GlTab,
     GlLoadingIcon,
+    GlPagination,
     GroupsListItem,
   },
   props: {
@@ -22,26 +24,37 @@ export default {
     return {
       namespaces: null,
       filter: '',
+      isLoading: true,
+      page: 1,
+      perPage: 2,
+      totalItems: 0,
     };
   },
-  computed: {
-    filteredNamespaces() {
-      return this.namespaces.filter(n => n.name.toLowerCase().includes(this.filter.toLowerCase()));
-    },
-  },
-
   mounted() {
     this.loadGroups();
   },
 
   methods: {
     loadGroups() {
+      this.isLoading = true;
+
       axios
-        .get(this.namespacesEndpoint)
+        .get(this.namespacesEndpoint, {
+          params: {
+            page: this.page,
+            per_page: this.perPage,
+          },
+        })
         .then(response => {
+          const { page, total } = parseIntPagination(normalizeHeaders(response.headers));
+          this.page = page;
+          this.totalItems = total;
           this.namespaces = response.data;
         })
-        .catch(() => createFlash(__('There was a problem fetching groups.')));
+        .catch(() => createFlash(__('There was a problem fetching groups.')))
+        .finally(() => {
+          this.isLoading = false;
+        });
     },
   },
 };
@@ -49,22 +62,35 @@ export default {
 <template>
   <gl-tabs class="fork-groups gl-px-5">
     <gl-tab :title="__('Groups and subgroups')">
-      <gl-loading-icon v-if="!namespaces" size="md" class="gl-mt-3" />
-      <template v-else-if="namespaces.length === 0">
-        <div class="gl-text-center">
-          <div class="h5">{{ __('No available groups to fork the project.') }}</div>
-          <p class="gl-mt-5">
-            {{ __('You must have permission to create a project in a group before forking.') }}
-          </p>
-        </div>
+      <gl-loading-icon v-if="isLoading" size="md" class="gl-mt-3" />
+      <template v-else>
+        <template v-if="namespaces.length === 0">
+          <div class="gl-text-center">
+            <!-- TODO: Check copy -->
+            <div class="h5">{{ __('No available namespaces.') }}</div>
+            <p class="gl-mt-5">
+              <!-- TODO: Check copy -->
+              {{ __('You must have owner or maintainer permissions to link namespaces.') }}
+            </p>
+          </div>
+        </template>
+        <template v-else>
+          <ul class="groups-list group-list-tree gl-list-style-none gl-pl-0">
+            <groups-list-item
+              v-for="namespace in namespaces"
+              :key="namespace.id"
+              :group="namespace"
+            />
+          </ul>
+        </template>
       </template>
-      <ul v-else class="groups-list group-list-tree gl-list-style-none gl-pl-0">
-        <groups-list-item
-          v-for="namespace in filteredNamespaces"
-          :key="namespace.id"
-          :group="namespace"
-        />
-      </ul>
+      <gl-pagination
+        v-if="totalItems > perPage && namespaces.length > 0"
+        v-model="page"
+        :per-page="perPage"
+        :total-items="totalItems"
+        @input="loadGroups"
+      />
     </gl-tab>
   </gl-tabs>
 </template>
