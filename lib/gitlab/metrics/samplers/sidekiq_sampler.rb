739c2452dd7dc961031c51c61a86544c5cf7e3c3
Matthias Kaeppler <mkaeppler@gitlab.com>
2020-12-31 17:13:45 +0100

    Fetch queue size and latency in a single call

--- lib/gitlab/metrics/samplers/sidekiq_sampler.rb
+++ lib/gitlab/metrics/samplers/sidekiq_sampler.rb
@@ -15,27 +15,55 @@ module Gitlab
           super
           @stats = Sidekiq::Stats.new
           @metrics = {
-            sidekiq_queue_size: ::Gitlab::Metrics.gauge(:sidekiq_queue_size, 'The current length of the queue')
+            sidekiq_queue_size:    ::Gitlab::Metrics.gauge(:sidekiq_queue_size, 'The current length of the queue'),
+            sidekiq_queue_latency: ::Gitlab::Metrics.gauge(:sidekiq_queue_latency_seconds, 'Time elapsed since the oldest job was enqueued')
           }
         end
 
         def sample
           labels = {}
 
-          sample_queue_sizes(labels)
+          sample_queue_stats(labels)
         end
 
         private
 
         # Cost: 2 Redis roundtrips.
         # - 1 request to obtain all queue names
-        # - N pipelined requests to read all lengths per queue
-        #
-        # See https://github.com/mperham/sidekiq/blob/6c94a9dd9f3ccbbd107c6ad2336c26a305020a25/lib/sidekiq/api.rb#L132-L147
-        def sample_queue_sizes(labels)
-          @stats.queues.each do |queue, length|
-            @metrics[:sidekiq_queue_size].set(labels.merge(name: queue, queue: queue), length)
+        # - N pipelined requests to read all jobs per queue
+        def sample_queue_stats(labels)
+          queue_stats = Sidekiq.redis do |conn|
+            queues = conn.sscan_each("queues").to_a
+
+            jobs = conn.pipelined do
+              queues.each do |queue|
+                conn.lrange("queue:#{queue}", -1, -1)
+              end
+            end
+
+            queues.zip(jobs).to_h do |queue, queue_jobs|
+              [queue, {
+                length: queue_jobs.size,
+                latency: queue_latency_from_jobs(queue_jobs)
+              }]
+            end
           end
+
+          queue_stats.each do |queue, stats|
+            @metrics[:sidekiq_queue_size].set(labels.merge(name: queue, queue: queue), stats[:length])
+            @metrics[:sidekiq_queue_latency].set(labels.merge(name: queue, queue: queue), stats[:latency])
+          end
+        end
+
+        # Cf. https://github.com/mperham/sidekiq/blob/6c94a9dd9f3ccbbd107c6ad2336c26a305020a25/lib/sidekiq/api.rb#L227-L241
+        def queue_latency_from_jobs(job_list)
+          entry = job_list.first
+          return 0 unless entry
+
+          job = Sidekiq.load_json(entry)
+          now = Time.now.to_f
+          thence = job["enqueued_at"] || now
+          now - thence
         end
       end
     end
