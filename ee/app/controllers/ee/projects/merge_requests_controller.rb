f955ba0ae4cff47230333f982ebca1ee3e2ce52d
Fernando <farias@gitlab.com>
2021-01-04 02:35:48 -0600

    Remove coverage_fuzzing_mr_widget feature flag
    * Remove YML, usages, and update unit tests
    
    Remove feature flag reference from controller spec

--- ee/app/controllers/ee/projects/merge_requests_controller.rb
+++ ee/app/controllers/ee/projects/merge_requests_controller.rb
@@ -11,7 +11,6 @@ module EE
         before_action only: [:show] do
           push_frontend_feature_flag(:anonymous_visual_review_feedback)
           push_frontend_feature_flag(:missing_mr_security_scan_types, @project)
-          push_frontend_feature_flag(:coverage_fuzzing_mr_widget, @project, default_enabled: true)
         end
 
         before_action :whitelist_query_limiting_ee_merge, only: [:merge]
