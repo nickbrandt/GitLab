0d45b816f17dca0ece5659fba71080d1038cab1d
Scott Hampton <shampton@gitlab.com>
2021-01-11 16:05:34 -0700

    Add usage ping for repo analytics page
    Usage ping for every user that views the repo
    analytics page.

--- ee/app/controllers/groups/analytics/repository_analytics_controller.rb
+++ ee/app/controllers/groups/analytics/repository_analytics_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class Groups::Analytics::RepositoryAnalyticsController < Groups::Analytics::ApplicationController
+  include RedisTracking
   layout 'group'
 
   before_action :load_group
   before_action -> { check_feature_availability!(:group_repository_analytics) }
   before_action -> { authorize_view_by_action!(:read_group_repository_analytics) }
+  track_redis_hll_event :show, name: 'i_testing_group_code_coverage_visit_total', feature: :usage_data_i_testing_group_code_coverage_visit_total, feature_default_enabled: true
 
   def show
     track_event(**pageview_tracker_params)
