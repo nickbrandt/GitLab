44926ce2ea0580a8bb6c368e5b2beee451d1702c
Phil Hughes <me@iamphill.com>
2021-01-12 11:19:06 +0000

    Moves the auto merge state to GraphQL
    Moves the auto merge failed and enabled states into
    GraphQL for its state data.
    
    Closes https://gitlab.com/gitlab-org/gitlab/-/issues/235715

--- ee/app/assets/javascripts/vue_merge_request_widget/mixins/auto_merge.js
+++ ee/app/assets/javascripts/vue_merge_request_widget/mixins/auto_merge.js
@@ -8,28 +8,27 @@ import { s__ } from '~/locale';
 export default {
   computed: {
     statusTextBeforeAuthor() {
-      if (this.mr.autoMergeStrategy === MT_MERGE_STRATEGY) {
+      if (this.autoMergeStrategy === MT_MERGE_STRATEGY) {
         return s__('mrWidget|Added to the merge train by');
       }
 
       return s__('mrWidget|Set by');
     },
     statusTextAfterAuthor() {
-      if (this.mr.autoMergeStrategy === MTWPS_MERGE_STRATEGY && this.mr.mergeTrainsCount === 0) {
+      const { mergeTrainsCount } = this.glFeatures.mergeRequestWidgetGraphql ? this.state : this.mr;
+
+      if (this.autoMergeStrategy === MTWPS_MERGE_STRATEGY && mergeTrainsCount === 0) {
         return s__('mrWidget|to start a merge train when the pipeline succeeds');
-      } else if (
-        this.mr.autoMergeStrategy === MTWPS_MERGE_STRATEGY &&
-        this.mr.mergeTrainsCount !== 0
-      ) {
+      } else if (this.autoMergeStrategy === MTWPS_MERGE_STRATEGY && mergeTrainsCount !== 0) {
         return s__('mrWidget|to be added to the merge train when the pipeline succeeds');
-      } else if (this.mr.autoMergeStrategy === MWPS_MERGE_STRATEGY) {
+      } else if (this.autoMergeStrategy === MWPS_MERGE_STRATEGY) {
         return s__('mrWidget|to be merged automatically when the pipeline succeeds');
       }
 
       return '';
     },
     cancelButtonText() {
-      if (this.mr.autoMergeStrategy === MT_MERGE_STRATEGY) {
+      if (this.autoMergeStrategy === MT_MERGE_STRATEGY) {
         return s__('mrWidget|Remove from merge train');
       }
 
