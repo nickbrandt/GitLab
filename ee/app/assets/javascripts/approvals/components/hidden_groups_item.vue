d5b08bd51d08029cc7c98f6127aae85bd6c5ba13
Dave Pisek <dpisek@gitlab.com>
2021-01-12 15:29:54 +1100

    Add specs for hidden groups item component
    * Make sure we display the correct icons
    * Check for the correct text
    * Test that the help-tooltip is wired up

--- ee/app/assets/javascripts/approvals/components/hidden_groups_item.vue
+++ ee/app/assets/javascripts/approvals/components/hidden_groups_item.vue
@@ -14,7 +14,7 @@ export default {
 <template>
   <div class="d-flex align-items-center">
     <div class="square s24 d-flex-center mr-2 text-tertiary">
-      <gl-icon name="folder-o" :size="16" />
+      <gl-icon name="folder-o" :size="16" data-testid="folder-icon" />
     </div>
     <span>{{ __('Private group(s)') }}</span>
     <gl-icon
@@ -23,6 +23,7 @@ export default {
       class="ml-1 gl-text-gray-500"
       name="question-o"
       :size="16"
+      data-testid="help-icon"
     />
   </div>
 </template>
