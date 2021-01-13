ddc31df1efb1d83c3d5f4bad1266a9053236308d
Tiger <twatson@gitlab.com>
2021-01-13 15:50:20 +1100

    Add rake task to migrate Terraform states to object storage
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/50740

--- app/models/terraform/state_version.rb
+++ app/models/terraform/state_version.rb
@@ -10,6 +10,7 @@ module Terraform
 
     scope :ordered_by_version_desc, -> { order(version: :desc) }
     scope :with_files_stored_locally, -> { where(file_store: Terraform::StateUploader::Store::LOCAL) }
+    scope :preload_state, -> { includes(:terraform_state) }
 
     default_value_for(:file_store) { StateUploader.default_store }
 
