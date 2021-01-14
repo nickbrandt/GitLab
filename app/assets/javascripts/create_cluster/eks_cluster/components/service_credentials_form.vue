e38774078738bf2256ea7a50cfbea3508ff6285d
Veethika Mishra <6115425-v_mishra@users.noreply.gitlab.com>
2021-01-14 07:17:06 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- app/assets/javascripts/create_cluster/eks_cluster/components/service_credentials_form.vue
+++ app/assets/javascripts/create_cluster/eks_cluster/components/service_credentials_form.vue
@@ -106,7 +106,7 @@ export default {
         )
       }}
     </p>
-    <gl-alert v-if="createRoleError" class="js-invalid-credentials" variant="danger">
+    <gl-alert v-if="createRoleError" class="js-invalid-credentials gl-mb-5" variant="danger" dismissible="false">
       {{ createRoleError }}
     </gl-alert>
     <div class="form-row">
