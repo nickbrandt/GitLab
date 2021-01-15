57d0029253118501d891db2033cbc16ba3ff8047
Alex Kalderimis <akalderimis@gitlab.com>
2021-01-15 17:52:58 +0000

    Fix epic assignment in the createIssue mutation
    This was broken in a few ways, but most noteably by the change
    in the method name in the FOSS class which is overridden in EE.
    
    This change introduces fixes for this, and a wider range of tests
    (missing regression tests were the reason this was not detected),
    and marks the method as overriding the FOSS implementation, guaranteeing
    that we will notice if a similar upstream change happens again.
    
    Issue: https://gitlab.com/gitlab-org/gitlab/-/issues/298840

--- ee/app/graphql/ee/mutations/issues/create.rb
+++ ee/app/graphql/ee/mutations/issues/create.rb
@@ -5,6 +5,7 @@ module EE
     module Issues
       module Create
         extend ActiveSupport::Concern
+        extend ::Gitlab::Utils::Override
 
         prepended do
           include ::Mutations::Issues::CommonEEMutationArguments
@@ -14,10 +15,21 @@ module EE
                    description: 'The ID of an epic to associate the issue with.'
         end
 
+        override :resolve
+        def resolve(**args)
+          super
+        rescue ActiveRecord::RecordNotFound => e
+          { errors: [e.message], issue: nil }
+        end
+
         private
 
-        def create_issue_params(params)
-          params[:epic_id] &&= params[:epic_id]&.model_id
+        override :build_create_issue_params
+        def build_create_issue_params(params)
+          # TODO: remove this line when the compatibility layer is removed
+          # See: https://gitlab.com/gitlab-org/gitlab/-/issues/257883
+          params[:epic_id] = ::Types::GlobalIDType[::Epic].coerce_isolated_input(params[:epic_id]) if params[:epic_id]
+          params[:epic_id] = params[:epic_id]&.model_id if params.key?(:epic_id)
 
           super(params)
         end
