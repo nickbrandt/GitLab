fe29393b9029f573b7b1fe0b174b06fe87b395e2
Francisco Javier López <fjlopez@gitlab.com>
2021-01-15 13:19:39 +0100

    Import/Export Group Wikis
    In this commit we add the capability to import and export
    group wikis inside the existing group export file.

---
+++ ee/spec/lib/ee/gitlab/import_export/wiki_repo_saver_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Gitlab::ImportExport::WikiRepoSaver do
+  let_it_be(:user) { create(:user) }
+  let_it_be(:group) do
+    create(:group, :wiki_repo).tap do |g|
+      g.add_owner(user)
+    end
+  end
+
+  let(:exportable) { group }
+  let(:shared) { Gitlab::ImportExport::Shared.new(group) }
+
+  subject { described_class.new(exportable: exportable, shared: shared) }
+
+  describe 'bundles a group wiki Git repo' do
+    let!(:group_wiki) { GroupWiki.new(group, user) }
+    let(:export_path) { "#{Dir.tmpdir}/group_tree_saver_spec" }
+
+    before do
+      allow_next_instance_of(Gitlab::ImportExport) do |instance|
+        allow(instance).to receive(:storage_path).and_return(export_path)
+      end
+
+      group_wiki.create_page('index', 'test content')
+    end
+
+    after do
+      FileUtils.rm_rf(export_path)
+    end
+
+    it 'bundles the repo successfully' do
+      expect(subject.save).to be true
+      expect(File.exist?(subject.send(:bundle_full_path))).to eq true
+    end
+
+    context 'when the repo is empty' do
+      let!(:group) { create(:group) }
+
+      it 'bundles the repo successfully' do
+        expect(subject.save).to be true
+      end
+    end
+  end
+
+  describe '#bundle_filename' do
+    context 'when exportable is a group' do
+      it 'returns the right filename for group wikis' do
+        expect(subject.send(:bundle_filename)).to eq ::Gitlab::ImportExport.group_wiki_repo_bundle_filename(exportable.id)
+      end
+    end
+
+    context 'when exportable is a project' do
+      let(:exportable) { build_stubbed(:project) }
+
+      it 'returns the right filename for project wikis' do
+        expect(subject.send(:bundle_filename)).to eq ::Gitlab::ImportExport.wiki_repo_bundle_filename
+      end
+    end
+  end
+end
---
+++ ee/spec/services/ee/groups/import_export/import_service_spec.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Groups::ImportExport::ImportService do
+  let_it_be(:import_file) { fixture_file_upload('ee/spec/fixtures/group_export_with_wikis.tar.gz') }
+  let_it_be(:user) { create(:user) }
+  let_it_be_with_reload(:group) do
+    create(:group).tap do |g|
+      g.add_owner(user)
+    end
+  end
+
+  subject(:import_service) { described_class.new(group: group, user: user) }
+
+  before do
+    ImportExportUpload.create!(group: group, import_file: import_file)
+  end
+
+  context 'when group_wikis feature is enabled' do
+    it 'imports group and descendant wiki repositories', :aggregate_failures do
+      stub_licensed_features(group_wikis: true)
+
+      expect_next_instance_of(
+        Gitlab::ImportExport::Group::GroupAndDescendantsRepoRestorer,
+        group: group,
+        shared: instance_of(Gitlab::ImportExport::Shared),
+        tree_restorer: instance_of(Gitlab::ImportExport::Group::TreeRestorer)
+      ) do |restorer|
+        expect(restorer).to receive(:restore).and_call_original
+      end
+
+      import_service.execute
+
+      expect(group.wiki.repository_exists?).to be true
+      expect(group.wiki.list_pages.first.title).to eq 'home'
+
+      group.descendants.each do |subgroup|
+        expect(subgroup.wiki.repository_exists?).to be true
+        expect(subgroup.wiki.list_pages.first.title).to eq "home_#{subgroup.path}"
+      end
+    end
+
+    context 'when feature flag :group_wiki_import_export is disabled' do
+      it 'does not export group wiki repositories' do
+        stub_feature_flags(group_wiki_import_export: false)
+
+        expect(::Gitlab::ImportExport::Group::GroupAndDescendantsRepoRestorer).not_to receive(:new)
+
+        import_service.execute
+      end
+    end
+
+    context 'when export file not in ndjson format' do
+      let(:import_file) { fixture_file_upload('spec/fixtures/legacy_group_export.tar.gz') }
+
+      it 'does not export group wiki repositories' do
+        expect(::Gitlab::ImportExport::Group::GroupAndDescendantsRepoRestorer).not_to receive(:new)
+
+        import_service.execute
+      end
+    end
+  end
+
+  context 'when group_wikis feature is not enabled' do
+    it 'does not call the group wiki restorer' do
+      expect(::Gitlab::ImportExport::RepoRestorer).not_to receive(:new)
+
+      expect { import_service.execute }.not_to raise_error(Gitlab::ImportExport::Error)
+    end
+  end
+end
