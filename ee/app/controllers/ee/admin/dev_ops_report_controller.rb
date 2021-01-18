c8092cdc1740974c15112aa4a1f49d9672b45131
Ash McKenzie <amckenzie@gitlab.com>
2021-01-18 18:38:49 +1100

    Fix default_enabled value must be a boolean

--- ee/app/controllers/ee/admin/dev_ops_report_controller.rb
+++ ee/app/controllers/ee/admin/dev_ops_report_controller.rb
@@ -7,7 +7,7 @@ module EE
         feature_already_in_use = ::Analytics::DevopsAdoption::Segment.any?
 
         ::License.feature_available?(:devops_adoption) &&
-          ::Feature.enabled?(:devops_adoption_feature, default_enabled: feature_already_in_use)
+          (feature_already_in_use || ::Feature.enabled?(:devops_adoption_feature, default_enabled: false))
       end
     end
   end
