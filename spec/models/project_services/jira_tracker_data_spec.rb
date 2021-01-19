39835a3e617b186fde66ee351a9ae3525246a627
Arturo Herrero <arturo.herrero@gmail.com>
2021-01-19 17:01:41 +0000

    Add Jira proxy settings columns
    We need to store the proxy_address, proxy_port, proxy_username and
    proxy_password.

--- spec/models/project_services/jira_tracker_data_spec.rb
+++ spec/models/project_services/jira_tracker_data_spec.rb
@@ -3,13 +3,18 @@
 require 'spec_helper'
 
 RSpec.describe JiraTrackerData do
-  let(:service) { build(:jira_service) }
-
-  describe 'Associations' do
+  describe 'associations' do
     it { is_expected.to belong_to(:service) }
   end
 
   describe 'deployment_type' do
     it { is_expected.to define_enum_for(:deployment_type).with_values([:unknown, :server, :cloud]).with_prefix(:deployment) }
+  end
+
+  describe 'proxy settings' do
+    it { is_expected.to validate_length_of(:proxy_address).is_at_most(2048) }
+    it { is_expected.to validate_length_of(:proxy_port).is_at_most(5) }
+    it { is_expected.to validate_length_of(:proxy_username).is_at_most(255) }
+    it { is_expected.to validate_length_of(:proxy_password).is_at_most(255) }
   end
 end
