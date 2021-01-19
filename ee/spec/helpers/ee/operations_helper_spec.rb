120fa9d9d99433bc55309ef8ae69f71582b38e82
Vitali Tatarintev <vtatarintev@gitlab.com>
2021-01-19 15:25:17 +0100

    Pass the custom mapping fields to frontend
    Pass alert fields and types as custom mapping attributes to frontend

--- ee/spec/helpers/ee/operations_helper_spec.rb
+++ ee/spec/helpers/ee/operations_helper_spec.rb
@@ -73,7 +73,7 @@ RSpec.describe OperationsHelper, :routing do
   end
 
   describe '#alerts_settings_data' do
-    subject { helper.alerts_settings_data }
+    subject(:alerts_settings_data) { helper.alerts_settings_data }
 
     describe 'Multiple Integrations Support' do
       before do
@@ -84,12 +84,37 @@ RSpec.describe OperationsHelper, :routing do
         let(:multi_integrations) { true }
 
         it { is_expected.to include('multi_integrations' => 'true') }
+
+        context 'with multiple_http_integrations_custom_mapping feature flag enabled' do
+          before do
+            stub_feature_flags(multiple_http_integrations_custom_mapping: true)
+          end
+
+          it 'has the correct list of fields', :aggregate_failures do
+            fields = Gitlab::Json.parse(alerts_settings_data['alert_fields'])
+
+            expect(fields.count).to eq(10)
+            expect(fields.first.keys).to eq(%w[name label types])
+            expect(fields.map { |f| f['name'] }).to match_array(
+              %w[title description start_time end_time service monitoring_tool hosts severity fingerprint gitlab_environment_name]
+            )
+          end
+        end
+
+        context 'with multiple_http_integrations_custom_mapping feature flag disabled' do
+          before do
+            stub_feature_flags(multiple_http_integrations_custom_mapping: false)
+          end
+
+          it { is_expected.not_to have_key('alert_fields') }
+        end
       end
 
       context 'when not available' do
         let(:multi_integrations) { false }
 
         it { is_expected.to include('multi_integrations' => 'false') }
+        it { is_expected.not_to have_key('alert_fields') }
       end
     end
   end
