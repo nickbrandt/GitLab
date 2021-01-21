ec9a5dc810039e769fdb6f0914174594f06103a2
Mireya Andres <mandres@gitlab.com>
2021-01-21 11:47:09 +0000

    Enable Merge Train checkbox based on Merge Pipelines checkbox
    The Merge Train checkbox is enabled only when the Merge Pipelines
    checkbox is enabled and checked. The initial state is set by querying
    `ciCdSettings` via GraphQL.

--- ee/app/assets/javascripts/pages/projects/edit/index.js
+++ ee/app/assets/javascripts/pages/projects/edit/index.js
@@ -3,6 +3,7 @@
 import '~/pages/projects/edit';
 import mountApprovals from 'ee/approvals/mount_project_settings';
 import initProjectAdjournedDeleteButton from 'ee/projects/project_adjourned_delete_button';
+import initMergeOptionSettings from 'ee/pages/projects/edit/merge_options';
 import UsersSelect from '~/users_select';
 import UserCallout from '~/user_callout';
 import groupsSelect from '~/groups_select';
@@ -15,3 +16,4 @@ new UserCallout({ className: 'js-mr-approval-callout' });
 mountApprovals(document.getElementById('js-mr-approvals-settings'));
 
 initProjectAdjournedDeleteButton();
+initMergeOptionSettings();
