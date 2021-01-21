0cdc512566ace25335d3ea1a575e1ba280b9f7e4
Stan Hu <stanhu@gmail.com>
2021-01-21 21:51:07 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- lib/gitlab/cluster/puma_worker_killer_initializer.rb
+++ lib/gitlab/cluster/puma_worker_killer_initializer.rb
@@ -35,7 +35,7 @@ module Gitlab
           # regularly rather than rely on OOM behavior for periodic restarting.
           config.rolling_restart_frequency = 43200 # 12 hours in seconds.
 
-          # Spread the rolling restarts out over 1 hour to avoid too many simulatneous
+          # Spread the rolling restarts out over 1 hour to avoid too many simultaneous
           # process startups.
           config.rolling_restart_splay_seconds = 0.0..3600.0 # 0 to 1 hour in seconds.
 
