f1ef965c24a61d4b065109146112b23c73ad701f
Philip Cunningham <pcunningham@gitlab.com>
2021-01-21 07:03:54 +0000

    Add DastScan model, dast_scans table and tests
    Adds new model that groups DastSiteProfile and
    DastScannerProfile.

---
+++ ee/app/models/dast/profile.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Dast
+  class Profile < ApplicationRecord
+    self.table_name = 'dast_profiles'
+
+    belongs_to :project
+    belongs_to :dast_site_profile
+    belongs_to :dast_scanner_profile
+
+    validates :description, length: { maximum: 255 }
+    validates :name, length: { maximum: 255 }, uniqueness: { scope: :project_id }
+    validates :project_id, :dast_site_profile_id, :dast_scanner_profile_id, presence: true
+
+    validate :project_ids_match
+
+    private
+
+    def project_ids_match
+      association_project_id_matches(dast_site_profile)
+      association_project_id_matches(dast_scanner_profile)
+    end
+
+    def association_project_id_matches(association)
+      return if association.nil?
+
+      unless project_id == association.project_id
+        errors.add(:project_id, "must match #{association.class.underscore}.project_id")
+      end
+    end
+  end
+end
