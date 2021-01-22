98bdb53a0b047e3cda609adf750067f0dbaba17c
Zack Cuddy <zcuddy@gitlab.com>
2021-01-22 08:09:27 +0000

    Global Search - Search Form Vue
    This MR changes the remaining search
    form into Vue.
    
    This allows us to remove the custom hook ins
    for the text highlighting.
    
    This will also allow us to remove the
    individual bootstrapping of
    the Project/Group Filter.

--- ee/spec/frontend/search/highlight_blob_search_result_spec.js
+++ ee/spec/frontend/search/highlight_blob_search_result_spec.js
@@ -2,6 +2,7 @@ import setHighlightClass from 'ee/search/highlight_blob_search_result';
 
 const fixture = 'ee/search/blob_search_result.html';
 const ceFixture = 'search/blob_search_result.html';
+const searchKeyword = 'Send'; // spec/frontend/fixtures/search.rb#79
 
 describe('ee/search/highlight_blob_search_result', () => {
   preloadFixtures(fixture, ceFixture);
@@ -10,7 +11,7 @@ describe('ee/search/highlight_blob_search_result', () => {
   it('highlights lines with search term occurrence', () => {
     loadFixtures(ceFixture);
 
-    setHighlightClass();
+    setHighlightClass(searchKeyword);
 
     expect(document.querySelectorAll('.blob-result .hll').length).toBe(4);
   });
@@ -19,7 +20,7 @@ describe('ee/search/highlight_blob_search_result', () => {
   it('highlights lines which have been identified by Elasticsearch', () => {
     loadFixtures(fixture);
 
-    setHighlightClass();
+    setHighlightClass(searchKeyword);
 
     expect(document.querySelectorAll('.blob-result .hll').length).toBe(3);
   });
