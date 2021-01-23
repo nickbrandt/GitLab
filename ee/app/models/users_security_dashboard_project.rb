b003363ed283e805e1871f5ba1961565bbd63a7c
Thong Kuah <tkuah@gitlab.com>
2021-01-23 23:29:45 +1300

    Suppress composite primary key warning
    These are all known cases, and there's no harm in the composite pk, so
    we can stop emit this warnings for these models

--- ee/app/models/users_security_dashboard_project.rb
+++ ee/app/models/users_security_dashboard_project.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersSecurityDashboardProject < ApplicationRecord
+  extend SuppressCompositePrimaryKeyWarning
+
   SECURITY_DASHBOARD_PROJECTS_LIMIT = 1000
 
   belongs_to :project
