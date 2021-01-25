ebfc54b0b25dfb9d21aa5d2bdd9f22586cd60e3b
Francisco Javier López <fjlopez@gitlab.com>
2021-01-25 17:19:50 +0100

    Include RepositoryStorageMove concern in RepositoryStorageMove
    In this commit we add the necessary logic to the
    RepositoryStorageMove class in order to deal with repository
    storage moves.

--- ee/app/models/groups/repository_storage_move.rb
+++ ee/app/models/groups/repository_storage_move.rb
@@ -5,11 +5,26 @@
 # balance storage capacity.
 module Groups
   class RepositoryStorageMove < ApplicationRecord
+    extend ::Gitlab::Utils::Override
+    include RepositoryStorageMovable
+
     self.table_name = 'group_repository_storage_moves'
 
     belongs_to :container, class_name: 'Group', inverse_of: :repository_storage_moves, foreign_key: :group_id
     alias_attribute :group, :container
 
     scope :with_groups, -> { includes(container: :route) }
+
+    override :schedule_repository_storage_update_worker
+    def schedule_repository_storage_update_worker
+      # No-op. It will be implemented in https://gitlab.com/gitlab-org/gitlab/-/issues/299059
+    end
+
+    private
+
+    override :error_key
+    def error_key
+      :group
+    end
   end
 end
--- ee/spec/models/groups/repository_storage_move_spec.rb
+++ ee/spec/models/groups/repository_storage_move_spec.rb
@@ -3,7 +3,10 @@
 require 'spec_helper'
 
 RSpec.describe Groups::RepositoryStorageMove, type: :model do
-  describe 'associations' do
-    it { is_expected.to belong_to(:container).class_name('Group') }
+  it_behaves_like 'handles repository moves', check_worker: false do
+    let_it_be_with_refind(:container) { create(:group) }
+
+    let(:repository_storage_factory_key) { :group_repository_storage_move }
+    let(:error_key) { :group }
   end
 end
