29e753c10b6710e1be960a9e606414c155e42d94
Francisco Javier López <fjlopez@gitlab.com>
2021-01-27 17:54:42 +0100

    Add worker to enqueue repository storage moves
    In this commit we introduce a worker that will handle
    group repository storage through the group update service.

---
+++ ee/app/workers/groups/update_repository_storage_worker.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Groups
+  class UpdateRepositoryStorageWorker # rubocop:disable Scalability/IdempotentWorker
+    extend ::Gitlab::Utils::Override
+    include ::UpdateRepositoryStorageWorker
+
+    private
+
+    override :find_repository_storage_move
+    def find_repository_storage_move(repository_storage_move_id)
+      ::Groups::RepositoryStorageMove.find(repository_storage_move_id)
+    end
+
+    override :find_container
+    def find_container(container_id)
+      ::Group.find(container_id)
+    end
+
+    override :update_repository_storage
+    def update_repository_storage(repository_storage_move)
+      ::Groups::UpdateRepositoryStorageService.new(repository_storage_move).execute
+    end
+  end
+end
--- ee/spec/models/groups/repository_storage_move_spec.rb
+++ ee/spec/models/groups/repository_storage_move_spec.rb
@@ -3,10 +3,11 @@
 require 'spec_helper'
 
 RSpec.describe Groups::RepositoryStorageMove, type: :model do
-  it_behaves_like 'handles repository moves', check_worker: false do
+  it_behaves_like 'handles repository moves' do
     let_it_be_with_refind(:container) { create(:group) }
 
     let(:repository_storage_factory_key) { :group_repository_storage_move }
     let(:error_key) { :group }
+    let(:repository_storage_worker) { Groups::UpdateRepositoryStorageWorker }
   end
 end
