4d242d8531874e13f6d787d00c2c8b7279e331a2
Kassio Borges <kborges@gitlab.com>
2021-01-28 19:46:32 +0000

    Simplify BulkImports::Pipeline::Context
    As the Gitlab Migration Grows (BulkImports) more and more developers
    have to access the Pipeline context data, like the entity group. To make
    this easier, the Context now has syntax-sugar methods for the most
    accessed data.

--- ee/spec/lib/ee/bulk_imports/importers/group_importer_spec.rb
+++ ee/spec/lib/ee/bulk_imports/importers/group_importer_spec.rb
@@ -4,16 +4,10 @@ require 'spec_helper'
 
 RSpec.describe BulkImports::Importers::GroupImporter do
   let(:user) { create(:user) }
-  let(:bulk_import) { create(:bulk_import) }
+  let(:bulk_import) { create(:bulk_import, user: user) }
   let(:bulk_import_entity) { create(:bulk_import_entity, :started, bulk_import: bulk_import) }
   let(:bulk_import_configuration) { create(:bulk_import_configuration, bulk_import: bulk_import) }
-  let(:context) do
-    BulkImports::Pipeline::Context.new(
-      current_user: user,
-      entity: bulk_import_entity,
-      configuration: bulk_import_configuration
-    )
-  end
+  let(:context) { BulkImports::Pipeline::Context.new(bulk_import_entity) }
 
   subject { described_class.new(bulk_import_entity) }
 
