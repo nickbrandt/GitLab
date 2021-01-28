8911c985e894e10e1ffc3b02aa25d5425376d5b1
Fernando Arias <farias@gitlab.com>
2021-01-28 08:27:07 +0000

    Show request/reply empty body
    * Update unit tests

--- ee/app/assets/javascripts/vue_shared/security_reports/components/helpers.js
+++ ee/app/assets/javascripts/vue_shared/security_reports/components/helpers.js
@@ -1,3 +1,16 @@
 import { EMPTY_BODY_MESSAGE } from './constants';
 
-export const bodyWithFallBack = (body) => body || EMPTY_BODY_MESSAGE;
+/**
+ * A helper function which validates the passed
+ * in body string.
+ *
+ * It returns an empty string if the body has explicitly
+ * been passed in as an empty string, a fallback
+ * message if the body is null / undefined, else
+ * it will return the original body string.
+ *
+ * @param {String} body the body message
+ *
+ * @return {String} the validated body message
+ */
+export const bodyWithFallBack = (body) => (body === '' ? '' : body || EMPTY_BODY_MESSAGE);
