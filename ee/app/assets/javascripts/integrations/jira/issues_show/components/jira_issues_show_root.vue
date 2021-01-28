d13c20260b5f8545f21f7c06555393e58defdc0e
Justin Ho <hduong@gitlab.com>
2021-01-28 16:14:06 +0700

    Add basic elements and process data
    Add page breadcrumbs (in HAML)

--- ee/app/assets/javascripts/integrations/jira/issues_show/components/jira_issues_show_root.vue
+++ ee/app/assets/javascripts/integrations/jira/issues_show/components/jira_issues_show_root.vue
@@ -1,8 +1,14 @@
 <script>
 import { fetchIssue } from 'ee/integrations/jira/issues_show/api';
+import IssuableShow from '~/issuable_show/components/issuable_show_root.vue';
+
+import { convertObjectPropsToCamelCase } from '~/lib/utils/common_utils';
 
 export default {
   name: 'JiraIssuesShow',
+  components: {
+    IssuableShow,
+  },
   inject: {
     issuesShowPath: {
       default: '',
@@ -10,15 +16,21 @@ export default {
   },
   data() {
     return {
+      isLoading: true,
       issue: {},
     };
   },
   async mounted() {
-    this.issue = await fetchIssue(this.issuesShowPath);
+    this.issue = convertObjectPropsToCamelCase(await fetchIssue(this.issuesShowPath), {
+      deep: true,
+    });
+    this.isLoading = false;
   },
 };
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <issuable-show v-if="!isLoading" :issuable="issue" :enable-edit="false"></issuable-show>
+  </div>
 </template>
