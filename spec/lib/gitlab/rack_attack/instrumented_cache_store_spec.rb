15f83c860d14b482f1c5a135d9c0df18e6230713
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2021-01-29 23:42:17 +0700

    Fix ruby2 keyword argument warnings in rack attack tests
    Issue https://gitlab.com/gitlab-com/gl-infra/scalability/-/issues/751

--- spec/lib/gitlab/rack_attack/instrumented_cache_store_spec.rb
+++ spec/lib/gitlab/rack_attack/instrumented_cache_store_spec.rb
@@ -9,22 +9,20 @@ RSpec.describe Gitlab::RackAttack::InstrumentedCacheStore do
 
   subject { described_class.new(upstream_store: store)}
 
-  where(:operation, :params) do
-    :fetch | [:key]
-    :fetch | [:key]
-    :read | [:key]
-    :read | [:key]
-    :read_multi | [:key_1, :key_2, :key_3]
-    :write_multi | [{ key_1: 1, key_2: 2, key_3: 3 }]
-    :fetch_multi | [:key_1, :key_2, :key_3]
-    :write | [:key, :value, { option_1: 1 }]
-    :delete | [:key]
-    :exist? | [:key, { option_1: 1 }]
-    :delete_matched | [/^key$/, { option_1: 1 }]
-    :increment | [:key, 1]
-    :decrement | [:key, 1]
-    :cleanup | []
-    :clear | []
+  where(:operation, :params, :test_proc) do
+    :fetch | [:key] | ->(s) { s.fetch(:key) }
+    :read | [:key] | ->(s) { s.read(:key) }
+    :read_multi | [:key_1, :key_2, :key_3] | ->(s) { s.read_multi(:key_1, :key_2, :key_3) }
+    :write_multi | [{ key_1: 1, key_2: 2, key_3: 3 }] | ->(s) { s.write_multi(key_1: 1, key_2: 2, key_3: 3) }
+    :fetch_multi | [:key_1, :key_2, :key_3] | ->(s) { s.fetch_multi(:key_1, :key_2, :key_3) {} }
+    :write | [:key, :value, { option_1: 1 }] | ->(s) { s.write(:key, :value, option_1: 1) }
+    :delete | [:key] | ->(s) { s.delete(:key) }
+    :exist? | [:key, { option_1: 1 }] | ->(s) { s.exist?(:key, option_1: 1) }
+    :delete_matched | [/^key$/, { option_1: 1 }] | ->(s) { s.delete_matched(/^key$/, option_1: 1 ) }
+    :increment | [:key, 1] | ->(s) { s.increment(:key, 1) }
+    :decrement | [:key, 1] | ->(s) { s.decrement(:key, 1) }
+    :cleanup | [] | ->(s) { s.cleanup }
+    :clear | [] | ->(s) { s.clear }
   end
 
   with_them do
@@ -36,7 +34,7 @@ RSpec.describe Gitlab::RackAttack::InstrumentedCacheStore do
           event = ActiveSupport::Notifications::Event.new(*args)
         end
 
-        subject.send(operation, *params) {}
+        test_proc.call(subject)
       ensure
         ActiveSupport::Notifications.unsubscribe(subscriber) if subscriber
       end
@@ -59,7 +57,7 @@ RSpec.describe Gitlab::RackAttack::InstrumentedCacheStore do
         end
 
         begin
-          subject.send(operation, *params) {}
+          test_proc.call(subject)
         rescue => e
           exception = e
         end
@@ -77,13 +75,15 @@ RSpec.describe Gitlab::RackAttack::InstrumentedCacheStore do
     end
 
     it 'delegates to the upstream store' do
+      allow(store).to receive(operation).and_call_original
+
       if params.empty?
         expect(store).to receive(operation).with(no_args)
       else
         expect(store).to receive(operation).with(*params)
       end
 
-      subject.send(operation, *params)
+      test_proc.call(subject)
     end
   end
 end
