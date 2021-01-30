16df6b5a1c0d9d7a8c49e923d706a61715033bed
syasonik <syasonik@gitlab.com>
2021-01-29 20:25:42 -0500

    Only email when triggered or resolved

--- ee/spec/support/shared_examples/alert_notification_service_shared_examples.rb
+++ ee/spec/support/shared_examples/alert_notification_service_shared_examples.rb
@@ -1,29 +1,19 @@
 # frozen_string_literal: true
 
+# Requires `users` and `fingerprint` to be defined
 RSpec.shared_examples 'Alert Notification Service sends notification email to on-call users' do
   let(:notification_service) { instance_double(NotificationService) }
 
-  context 'with oncall schedules enabled' do
-    before do
-      stub_licensed_features(oncall_schedules: project)
-    end
+  it 'sends a notification' do
+    expect(NotificationService).to receive(:new).and_return(notification_service)
 
-    it 'sends a notification' do
-      expect(NotificationService).to receive(:new).and_return(notification_service)
+    expect(notification_service)
+      .to receive_message_chain(:async, :notify_oncall_users_of_alert)
+      .with(
+        users,
+        having_attributes(class: AlertManagement::Alert, fingerprint: fingerprint)
+      )
 
-      expect(notification_service)
-        .to receive_message_chain(:async, :notify_oncall_users_of_alert)
-        .with(*notification_args)
-
-      expect(subject).to be_success
-    end
-  end
-
-  context 'with oncall schedules disabled' do
-    it 'does not notify the on-call users' do
-      expect(NotificationService).not_to receive(:new)
-
-      expect(subject).to be_success
-    end
+    expect(subject).to be_success
   end
 end
