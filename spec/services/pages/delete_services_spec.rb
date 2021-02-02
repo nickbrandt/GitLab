d7b74a289759d14cf6437fa39913f260e9deb1bc
Vladimir Shushlin <v.shushlin@gmail.com>
2021-02-02 15:56:49 +0300

    Clean up pages delete service specs
    This feature flag was removed a long time ago

--- spec/services/pages/delete_services_spec.rb
+++ spec/services/pages/delete_services_spec.rb
@@ -3,35 +3,25 @@
 require 'spec_helper'
 
 RSpec.describe Pages::DeleteService do
-  shared_examples 'remove pages' do
-    let_it_be(:project) { create(:project, path: "my.project")}
-    let_it_be(:admin) { create(:admin) }
-    let_it_be(:domain) { create(:pages_domain, project: project) }
-    let_it_be(:service) { described_class.new(project, admin)}
+  let_it_be(:project) { create(:project, path: "my.project")}
+  let_it_be(:admin) { create(:admin) }
+  let_it_be(:domain) { create(:pages_domain, project: project) }
+  let_it_be(:service) { described_class.new(project, admin)}
 
-    it 'deletes published pages' do
-      expect_any_instance_of(Gitlab::PagesTransfer).to receive(:rename_project).and_return true
-      expect(PagesWorker).to receive(:perform_in).with(5.minutes, :remove, project.namespace.full_path, anything)
+  it 'deletes published pages' do
+    expect_any_instance_of(Gitlab::PagesTransfer).to receive(:rename_project).and_return true
+    expect(PagesWorker).to receive(:perform_in).with(5.minutes, :remove, project.namespace.full_path, anything)
 
-      Sidekiq::Testing.inline! { service.execute }
+    Sidekiq::Testing.inline! { service.execute }
 
-      expect(project.reload.pages_metadatum.deployed?).to be(false)
-    end
-
-    it 'deletes all domains' do
-      expect(project.pages_domains.count).to be 1
-
-      Sidekiq::Testing.inline! { service.execute }
-
-      expect(project.reload.pages_domains.count).to be 0
-    end
+    expect(project.reload.pages_metadatum.deployed?).to be(false)
   end
 
-  context 'with feature flag enabled' do
-    before do
-      expect(PagesRemoveWorker).to receive(:perform_async).and_call_original
-    end
+  it 'deletes all domains' do
+    expect(project.pages_domains.count).to be 1
 
-    it_behaves_like 'remove pages'
+    Sidekiq::Testing.inline! { service.execute }
+
+    expect(project.reload.pages_domains.count).to be 0
   end
 end
