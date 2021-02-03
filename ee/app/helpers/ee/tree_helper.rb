3ee2cd7da51f915a027715e909ba750d3c959cb0
jboyson <jboyson@gitlab.com>
2021-02-03 11:05:25 -0600

    Remove unused tree files
    These tree files were refactored into vue components and never got
    removed

--- ee/app/helpers/ee/tree_helper.rb
+++ ee/app/helpers/ee/tree_helper.rb
@@ -4,15 +4,6 @@ module EE
   module TreeHelper
     extend ::Gitlab::Utils::Override
 
-    override :tree_content_data
-    def tree_content_data(logs_path, project, path)
-      super.merge({
-        "path-locks-available" => project.feature_available?(:file_locks).to_s,
-        "path-locks-toggle" => toggle_project_path_locks_path(project),
-        "path-locks-path" => path
-      })
-    end
-
     override :vue_file_list_data
     def vue_file_list_data(project, ref)
       super.merge({
