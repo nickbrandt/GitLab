77f63da4957d4c20073f9dd5710241d4ce2d7d24
Rajat Jain <rjain@gitlab.com>
2021-02-03 11:05:58 +0530

    Use reference for Epic in autocompletion
    Use reference for Epics in autocompletion

--- ee/spec/frontend/gfm_auto_complete_spec.js
+++ ee/spec/frontend/gfm_auto_complete_spec.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import GfmAutoCompleteEE from 'ee/gfm_auto_complete';
 import { TEST_HOST } from 'helpers/test_constants';
+import GfmAutoComplete from '~/gfm_auto_complete';
 
 describe('GfmAutoCompleteEE', () => {
   const dataSources = {
@@ -16,5 +17,23 @@ describe('GfmAutoCompleteEE', () => {
 
   it('should have enableMap', () => {
     expect(instance.enableMap).not.toBeNull();
+  });
+
+  describe('Issues.templateFunction', () => {
+    it('should return html with id and title', () => {
+      expect(GfmAutoComplete.Issues.templateFunction({ id: 42, title: 'Sample Epic' })).toBe(
+        '<li><small>42</small> Sample Epic</li>',
+      );
+    });
+
+    it('should replace id with reference if reference is set', () => {
+      expect(
+        GfmAutoComplete.Issues.templateFunction({
+          id: 42,
+          title: 'Another Epic',
+          reference: 'foo&42',
+        }),
+      ).toBe('<li><small>foo&42</small> Another Epic</li>');
+    });
   });
 });
