7da5792faf060c1656a3ec347c6c8a6d7d161080
Paul Slaughter <pslaughter@gitlab.com>
2021-02-03 09:52:41 -0600

    Use fake date in spec environment
    - This makes sure the fake date is
      truly applied everywhere and
      fixes an issue where jsdom was
      not aware of the fake date.

--- spec/frontend/environment.js
+++ spec/frontend/environment.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { ErrorWithStack } = require('jest-util');
 const JSDOMEnvironment = require('jest-environment-jsdom');
 const { TEST_HOST } = require('./__helpers__/test_constants');
+const { useFakeDate } = require('./__helpers__/fake_date');
 
 const ROOT_PATH = path.resolve(__dirname, '../..');
 
@@ -11,6 +12,9 @@ class CustomEnvironment extends JSDOMEnvironment {
   constructor(config, context) {
     // Setup testURL so that window.location is setup properly
     super({ ...config, testURL: TEST_HOST }, context);
+
+    // This lets jsdom use the fake date for things like document.cookie
+    useFakeDate();
 
     Object.assign(context.console, {
       error(...args) {
