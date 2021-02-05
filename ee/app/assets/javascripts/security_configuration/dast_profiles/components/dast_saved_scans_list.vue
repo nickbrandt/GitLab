755b86a17f1432bdf9bf2ff9cf732720859f002e
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-02-05 17:31:43 -0500

    Implement Run & Delete scan actions
    Adds the ability to run or delete a saved DAST scans from the DAST
    profiles library.

--- ee/app/assets/javascripts/security_configuration/dast_profiles/components/dast_saved_scans_list.vue
+++ ee/app/assets/javascripts/security_configuration/dast_profiles/components/dast_saved_scans_list.vue
@@ -1,20 +1,66 @@
 <script>
+import { GlButton } from '@gitlab/ui';
+import { redirectTo } from '~/lib/utils/url_utility';
+import createFlash from '~/flash';
+import { ERROR_RUN_SCAN, ERROR_MESSAGES } from 'ee/on_demand_scans/settings';
+import dastProfileRunMutation from '../graphql/dast_profile_run.mutation.graphql';
 import ProfilesList from './dast_profiles_list.vue';
 import ScanTypeBadge from './dast_scan_type_badge.vue';
 
 export default {
   components: {
+    GlButton,
     ProfilesList,
     ScanTypeBadge,
+  },
+  props: {
+    fullPath: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    async runScan({ id }) {
+      try {
+        const {
+          dastProfileRun: { pipelineUrl, errors },
+        } = await this.$apollo.mutate({
+          mutation: dastProfileRunMutation,
+          variables: {
+            input: {
+              fullPath: this.fullPath,
+              id,
+            },
+          },
+        });
+
+        if (errors.length) {
+          this.handleError();
+        } else {
+          redirectTo(pipelineUrl);
+        }
+      } catch (error) {
+        this.handleError(error);
+      }
+    },
+    handleError(error) {
+      createFlash({ message: ERROR_MESSAGES[ERROR_RUN_SCAN], error, captureError: true });
+    },
   },
 };
 </script>
 
 <template>
-  <profiles-list v-bind="$attrs" v-on="$listeners">
+  <profiles-list :full-path="fullPath" v-bind="$attrs" v-on="$listeners">
     <!-- eslint-disable-next-line vue/valid-v-slot -->
     <template #cell(dastScannerProfile.scanType)="{ value }">
       <scan-type-badge :scan-type="value" />
+    </template>
+
+    <template #actions="{ profile }">
+      <gl-button size="small" data-testid="dast-scan-run-button" @click="runScan(profile)">{{
+        s__('DastProfiles|Run scan')
+      }}</gl-button>
     </template>
   </profiles-list>
 </template>
--- ee/spec/frontend/security_configuration/dast_profiles/components/dast_saved_scans_list_spec.js
+++ ee/spec/frontend/security_configuration/dast_profiles/components/dast_saved_scans_list_spec.js
@@ -2,7 +2,14 @@ import { mount, shallowMount } from '@vue/test-utils';
 import { merge } from 'lodash';
 import Component from 'ee/security_configuration/dast_profiles/components/dast_saved_scans_list.vue';
 import ProfilesList from 'ee/security_configuration/dast_profiles/components/dast_profiles_list.vue';
+import waitForPromises from 'helpers/wait_for_promises';
+import { extendedWrapper } from 'helpers/vue_test_utils_helper';
+import { redirectTo } from '~/lib/utils/url_utility';
+import createFlash from '~/flash';
 import { savedScans } from '../mocks/mock_data';
+
+jest.mock('~/lib/utils/url_utility');
+jest.mock('~/flash');
 
 describe('EE - DastSavedScansList', () => {
   let wrapper;
@@ -24,13 +31,15 @@ describe('EE - DastSavedScansList', () => {
   };
 
   const wrapperFactory = (mountFn = shallowMount) => (options = {}) => {
-    wrapper = mountFn(
-      Component,
-      merge(
-        {
-          propsData: defaultProps,
-        },
-        options,
+    wrapper = extendedWrapper(
+      mountFn(
+        Component,
+        merge(
+          {
+            propsData: defaultProps,
+          },
+          options,
+        ),
       ),
     );
   };
@@ -66,5 +75,66 @@ describe('EE - DastSavedScansList', () => {
     findProfileList().vm.$emit('input');
 
     expect(inputHandler).toHaveBeenCalled();
+  });
+
+  describe('run scan', () => {
+    it('redirects to the running pipeline page on success', async () => {
+      const pipelineUrl = '/pipeline/url';
+      createFullComponent({
+        propsData: { profiles: savedScans },
+        mocks: {
+          $apollo: {
+            mutate: jest.fn().mockResolvedValue({
+              dastProfileRun: {
+                pipelineUrl,
+                errors: [],
+              },
+            }),
+          },
+        },
+      });
+      wrapper.findByTestId('dast-scan-run-button').trigger('click');
+      await waitForPromises();
+
+      expect(redirectTo).toHaveBeenCalledWith(pipelineUrl);
+      expect(createFlash).not.toHaveBeenCalled();
+    });
+
+    it('create a flash error on failure', async () => {
+      createFullComponent({
+        propsData: { profiles: savedScans },
+        mocks: {
+          $apollo: {
+            mutate: jest.fn().mockRejectedValue(),
+          },
+        },
+      });
+      wrapper.findByTestId('dast-scan-run-button').trigger('click');
+      await waitForPromises();
+
+      expect(createFlash).toHaveBeenCalled();
+      expect(redirectTo).not.toHaveBeenCalled();
+    });
+
+    it('create a flash error if the API responds with errors-as-data', async () => {
+      createFullComponent({
+        propsData: { profiles: savedScans },
+        mocks: {
+          $apollo: {
+            mutate: jest.fn().mockResolvedValue({
+              dastProfileRun: {
+                pipelineUrl: null,
+                errors: ['error-as-data'],
+              },
+            }),
+          },
+        },
+      });
+      wrapper.findByTestId('dast-scan-run-button').trigger('click');
+      await waitForPromises();
+
+      expect(createFlash).toHaveBeenCalled();
+      expect(redirectTo).not.toHaveBeenCalled();
+    });
   });
 });
