dc9c2c3781f6f3cbd28600ecae48ab31b728d1b7
jejacks0n <jjackson@gitlab.com>
2021-02-08 15:47:24 -0700

    Removes the cache and adds it to the gem
    - This also introduces some tests to make sure that our cache config
    stays intact with the configuration. We can probably remove this at some
    point however.

--- config/initializers/gitlab_experiment.rb
+++ config/initializers/gitlab_experiment.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Gitlab::Experiment.configure do |config|
+  config.define_singleton_method(:_default_cache) do
+    Gitlab::Experiment::Cache::RedisHashStore.new(
+      pool: ->(&block) { Gitlab::Redis::SharedState.with { |redis| block.call(redis) } }
+    )
+  end
+
   config.base_class = 'ApplicationExperiment'
-  config.cache = ApplicationExperiment::Cache.new
+  config.cache = config._default_cache
 end
