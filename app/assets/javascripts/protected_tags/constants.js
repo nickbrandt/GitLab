32b43af3eb6a5c428144cc65a0f04763812e0a60
Austin Regnery <aregnery@gitlab.com>
2021-02-10 16:54:34 -0500

    Updated flash message

--- app/assets/javascripts/protected_tags/constants.js
+++ app/assets/javascripts/protected_tags/constants.js
@@ -1,3 +1,3 @@
 import { s__ } from '~/locale';
 
-export const FAILED_TO_UPDATE_TAG_MESSSAGE = s__('ProjectSettings|Failed to update tag!');
+export const FAILED_TO_UPDATE_TAG_MESSAGE = s__('ProjectSettings|Failed to update tag!');
