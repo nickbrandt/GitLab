ee11a0eedbdc25aef45a6880fe6f415a014b1490
Serena Fang <sfang@gitlab.com>
2021-02-11 15:08:31 -0600

    Do not expose name if user is project bot

--- lib/api/entities/user_safe.rb
+++ lib/api/entities/user_safe.rb
@@ -3,7 +3,8 @@
 module API
   module Entities
     class UserSafe < Grape::Entity
-      expose :id, :name, :username
+      expose :id, :username
+      expose :name, unless: ->(user) { user.project_bot? }
     end
   end
 end
