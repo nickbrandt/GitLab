df14dfae354e72a68de2fc0e96045f78197bfa8a
Maxime Orefice <morefice@gitlab.com>
2021-02-16 19:58:49 +0000

    Add daily group coverage data new finder
    This commit introduces a new finder to agregate our daily coverage
    data at the group level. This is premium feature so all the code
    is included in ee.

--- ee/spec/controllers/groups/analytics/coverage_reports_controller_spec.rb
+++ ee/spec/controllers/groups/analytics/coverage_reports_controller_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe Groups::Analytics::CoverageReportsController do
   let_it_be(:project) { create(:project, namespace: group) }
   let_it_be(:ref_path) { 'refs/heads/master' }
 
-  let_it_be(:first_coverage) { create_daily_coverage('rspec', project, 79.0, '2020-03-09') }
-  let_it_be(:last_coverage) { create_daily_coverage('karma', project, 95.0, '2020-03-10') }
+  let_it_be(:first_coverage) { create_daily_coverage('rspec', project, 79.0, '2020-03-09', group) }
+  let_it_be(:last_coverage) { create_daily_coverage('karma', project, 95.0, '2020-03-10', group) }
 
   let_it_be(:valid_request_params) do
     {
@@ -59,71 +59,96 @@ RSpec.describe Groups::Analytics::CoverageReportsController do
         stub_licensed_features(group_coverage_reports: true)
       end
 
-      it 'responds 200 with CSV coverage data', :snowplow do
-        get :index, params: valid_request_params
-
-        expect_snowplow_event(
-          category: described_class.name,
-          action: 'download_code_coverage_csv',
-          label: 'group_id',
-          value: group.id
-        )
-
-        expect(response).to have_gitlab_http_status(:ok)
-        expect(csv_response).to eq([
-          %w[date group_name project_name coverage],
-          [last_coverage.date.to_s, last_coverage.group_name, project.name, last_coverage.data['coverage'].to_s],
-          [first_coverage.date.to_s, first_coverage.group_name, project.name, first_coverage.data['coverage'].to_s]
-        ])
-      end
-
-      context 'with a project_id filter' do
-        let(:params) { valid_request_params.merge(project_ids: [project.id]) }
+      context 'when feature coverage_data_new_finder is enabled' do
+        before do
+          stub_feature_flags(coverage_data_new_finder: true)
+        end
 
         it 'responds 200 with CSV coverage data' do
-          expect(Ci::DailyBuildGroupReportResultsByGroupFinder).to receive(:new).with({
-            group: group,
-            current_user: user,
-            project_ids: [project.id.to_s],
-            start_date: Date.parse('2020-03-01'),
-            end_date: Date.parse('2020-03-31'),
-            ref_path: ref_path
-          }).and_call_original
-
-          get :index, params: params
-        end
-      end
-
-      context 'when ref_path is nil' do
-        let(:ref_path) { nil }
-
-        it 'responds HTTP 200' do
           get :index, params: valid_request_params
 
           expect(response).to have_gitlab_http_status(:ok)
-          expect(csv_response.size).to eq(3)
+          expect(csv_response).to eq([
+            %w[date group_name project_name coverage],
+            [last_coverage.date.to_s, last_coverage.group_name, project.name, last_coverage.data['coverage'].to_s],
+            [first_coverage.date.to_s, first_coverage.group_name, project.name, first_coverage.data['coverage'].to_s]
+          ])
         end
       end
 
-      it 'executes the same number of queries regardless of the number of records returned' do
-        control = ActiveRecord::QueryRecorder.new do
+      context 'when feature coverage_data_new_finder is disabled' do
+        before do
+          stub_feature_flags(coverage_data_new_finder: false)
+        end
+
+        it 'responds 200 with CSV coverage data', :snowplow do
           get :index, params: valid_request_params
+
+          expect_snowplow_event(
+            category: described_class.name,
+            action: 'download_code_coverage_csv',
+            label: 'group_id',
+            value: group.id
+          )
+
+          expect(response).to have_gitlab_http_status(:ok)
+          expect(csv_response).to eq([
+            %w[date group_name project_name coverage],
+            [last_coverage.date.to_s, last_coverage.group_name, project.name, last_coverage.data['coverage'].to_s],
+            [first_coverage.date.to_s, first_coverage.group_name, project.name, first_coverage.data['coverage'].to_s]
+          ])
         end
 
-        expect(CSV.parse(response.body).length).to eq(3)
+        context 'with a project_id filter' do
+          let(:params) { valid_request_params.merge(project_ids: [project.id]) }
 
-        create_daily_coverage('rspec', project, 79.0, '2020-03-10')
+          it 'responds 200 with CSV coverage data' do
+            expect(Ci::DailyBuildGroupReportResultsByGroupFinder).to receive(:new).with({
+              group: group,
+              current_user: user,
+              project_ids: [project.id.to_s],
+              start_date: Date.parse('2020-03-01'),
+              end_date: Date.parse('2020-03-31'),
+              ref_path: ref_path
+            }).and_call_original
 
-        expect { get :index, params: valid_request_params }.not_to exceed_query_limit(control)
+            get :index, params: params
 
-        expect(csv_response.length).to eq(4)
-      end
+            expect(response).to have_gitlab_http_status(:ok)
+          end
+        end
 
-      context 'with an invalid format' do
-        it 'responds 404' do
-          get :index, params: valid_request_params.merge(format: :json)
+        context 'when ref_path is nil' do
+          let(:ref_path) { nil }
 
-          expect(response).to have_gitlab_http_status(:not_found)
+          it 'responds HTTP 200' do
+            get :index, params: valid_request_params
+
+            expect(response).to have_gitlab_http_status(:ok)
+            expect(csv_response.size).to eq(3)
+          end
+        end
+
+        it 'executes the same number of queries regardless of the number of records returned' do
+          control = ActiveRecord::QueryRecorder.new do
+            get :index, params: valid_request_params
+          end
+
+          expect(CSV.parse(response.body).length).to eq(3)
+
+          create_daily_coverage('rspec', project, 79.0, '2020-03-10')
+
+          expect { get :index, params: valid_request_params }.not_to exceed_query_limit(control)
+
+          expect(csv_response.length).to eq(4)
+        end
+
+        context 'with an invalid format' do
+          it 'responds 404' do
+            get :index, params: valid_request_params.merge(format: :json)
+
+            expect(response).to have_gitlab_http_status(:not_found)
+          end
         end
       end
     end
@@ -131,14 +156,15 @@ RSpec.describe Groups::Analytics::CoverageReportsController do
 
   private
 
-  def create_daily_coverage(group_name, project, coverage, date)
+  def create_daily_coverage(group_name, project, coverage, date, group = nil)
     create(
       :ci_daily_build_group_report_result,
       project: project,
       ref_path: ref_path,
       group_name: group_name,
       data: { 'coverage' => coverage },
-      date: date
+      date: date,
+      group: group
     )
   end
 end
