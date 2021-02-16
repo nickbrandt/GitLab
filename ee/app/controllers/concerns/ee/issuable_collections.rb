c590705323bda61fc90dcae8522559aab06cb414
Gary Holtz <gholtz@gitlab.com>
2021-02-16 12:12:07 +0000

    Fix N+1 spec to test correctly
    Move it to request spec since we are transitioning controller
    specs to request specs. Also, query counter should be used in
    request specs as it is possible to return false positives when
    used in controller specs.

--- ee/app/controllers/concerns/ee/issuable_collections.rb
+++ ee/app/controllers/concerns/ee/issuable_collections.rb
@@ -9,7 +9,7 @@ module EE
     def preload_for_collection
       @preload_for_collection ||= case collection_type
                                   when 'MergeRequest'
-                                    super.push(:approval_rules)
+                                    super + [approval_rules: [:users, :group_users], approval_project_rules: [:users, :group_users]]
                                   when 'Issue'
                                     super.push(*issue_preloads)
                                   else
