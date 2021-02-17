33c12b9a05eb5a30e9fb947f732b3340139f1d3f
Natalia Tepluhina <ntepluhina@gitlab.com>
2021-02-17 08:07:34 +0000

    Created the assignees widget
    Revert "Created the first exposed method"
    
    This reverts commit 097a584f99be3aad4c793133684183c43a029a3d.
    Replaced search query with shared one
    
    Abstracted parent entity assignees
    
    Abstracted setting assignees
    
    Abstracted widget in shared component
    
    - reverted changes to BoardAssigneesDropDown
    - fixed a few styles
    Added hardcoded widget to issue
    
    Passed correct injections
    
    Added a watcher to hande initial state
    
    Fixed participants loading state
    
    Finished issue sidebar
    
    Renamed issue assignees queries
    
    - changed issue and mutation name to snake_case
    Connected a correct query for MR
    
    Added MR assignees mutation
    
    Fixed update for MRs
    
    Fixed MR avatarUrl casing
    
    Added an exposed method and demo
    
    Changed assignees to participants
    
    Removed rebase artifacts
    
    Apply 1 suggestion(s) to 1 file(s)
    Started widget refactoring
    
    Fixed sidebar issue mutation
    
    Added ID to queries
    
    Added fixes for MR assignees
    
    Adjusted the code to swimlanes
    
    Exposed assignees state
    
    Nullified observable on destroyed
    
    Fixed a race condition
    
    Added a fallback with initial assignees
    
    Reverted changes with assinees state
    
    Silenced loading state with initial assignees
    
    Splited widget for CE and EE
    
    Fixed emitted event parameter
    
    Passed issuable type to assignees component
    
    Uncommented fetch policies
    Added default slot directive
    Removed board_assignees_dropdown
    Fixed actions spec
    Added temporary translation
    
    Fixed sidebar assignees spec
    Fixed an import
    
    Removed doubled imports
    
    Refactored a component to have a mixin
    
    Removed changed to Noteable note
    
    Removed sidebar widget for issues and MRs
    
    Fixed linter errors
    
    Reverted changes to sidebar assignees spec
    
    Refactored widget to use issuable types
    
    - moved issuable types queries to constants
    - changed query variables
    Added a widget to board sidebar
    
    Fixed project path to be calculated from issue
    
    Fixed board to use widget
    
    Fixed assignees text
    
    Fixed board sidebar spec
    
    Fixed translations file
    
    Fixed unassigning and assign self
    
    Fixed feature spec for boards
    
    Fixed ee board spec
    
    Fixed ee sidebar spec
    Created tests for widget
    
    Added tests for initial prop
    
    Fixed initial assignees tests
    
    Added spec for fetching assignees
    
    Added todos for tests
    
    Added error handling and test
    
    Added a test for mutation
    
    Fixed loading participants test
    
    Added tests for selected and unselected
    
    Added tests for selecting
    
    Tested a mutation error
    
    Fixed finder helpers
    
    Finished test abstraction
    
    Regenerated a translation file
    Added participants to mutation responses
    
    Updated a test with successful mutation
    
    Apply 1 suggestion(s) to 1 file(s)
    Apply 1 suggestion(s) to 1 file(s)
    Apply 1 suggestion(s) to 1 file(s)
    Added issuable type import
    
    Fixed computed property and validator
    
    Removed unnecessary root path injection
    
    Added changelog entry
    
    Fixed a spec
    
    Apply 1 suggestion(s) to 1 file(s)
    Added searchUsers to data
    
    Apply 1 suggestion(s) to 1 file(s)
    Renamed assigneesWidget exposed state
    
    Removed an unused method from boards helper
    
    Apply 1 suggestion(s) to 1 file(s)
    Apply 1 suggestion(s) to 1 file(s)
    Fixed a default value for createComponent
    
    Removed jquery code for dropdown
    
    Added a logic for escape button
    
    Added focusing a search

--- ee/spec/frontend/sidebar/mock_data.js
+++ ee/spec/frontend/sidebar/mock_data.js
@@ -73,3 +73,120 @@ export const mockMutationResponse = {
     },
   },
 };
+
+export const issuableQueryResponse = {
+  data: {
+    project: {
+      issuable: {
+        __typename: 'Issue',
+        id: 'gid://gitlab/Issue/1',
+        iid: '1',
+        participants: {
+          nodes: [
+            {
+              id: 'gid://gitlab/User/1',
+              avatarUrl:
+                'https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80\u0026d=identicon',
+              name: 'Administrator',
+              username: 'root',
+              webUrl: '/root',
+            },
+            {
+              id: 'gid://gitlab/User/2',
+              avatarUrl:
+                'https://www.gravatar.com/avatar/a95e5b71488f4b9d69ce5ff58bfd28d6?s=80\u0026d=identicon',
+              name: 'Jacki Kub',
+              username: 'francina.skiles',
+              webUrl: '/franc',
+            },
+          ],
+        },
+        assignees: {
+          nodes: [
+            {
+              id: 'gid://gitlab/User/2',
+              avatarUrl:
+                'https://www.gravatar.com/avatar/a95e5b71488f4b9d69ce5ff58bfd28d6?s=80\u0026d=identicon',
+              name: 'Jacki Kub',
+              username: 'francina.skiles',
+              webUrl: '/franc',
+            },
+          ],
+        },
+      },
+    },
+  },
+};
+
+export const searchQueryResponse = {
+  data: {
+    users: {
+      nodes: [
+        {
+          id: '1',
+          avatarUrl: '/avatar',
+          name: 'root',
+          username: 'root',
+          webUrl: 'root',
+        },
+        {
+          id: '3',
+          avatarUrl: '/avatar',
+          name: 'rookie',
+          username: 'rookie',
+          webUrl: 'rookie',
+        },
+      ],
+    },
+  },
+};
+
+export const updateIssueAssigneesMutationResponse = {
+  data: {
+    issueSetAssignees: {
+      issue: {
+        id: 'gid://gitlab/Issue/1',
+        iid: '1',
+        assignees: {
+          nodes: [
+            {
+              __typename: 'User',
+              id: 'gid://gitlab/User/1',
+              avatarUrl:
+                'https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80\u0026d=identicon',
+              name: 'Administrator',
+              username: 'root',
+              webUrl: '/root',
+            },
+          ],
+          __typename: 'UserConnection',
+        },
+        participants: {
+          nodes: [
+            {
+              __typename: 'User',
+              id: 'gid://gitlab/User/1',
+              avatarUrl:
+                'https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80\u0026d=identicon',
+              name: 'Administrator',
+              username: 'root',
+              webUrl: '/root',
+            },
+            {
+              __typename: 'User',
+              id: 'gid://gitlab/User/2',
+              avatarUrl:
+                'https://www.gravatar.com/avatar/a95e5b71488f4b9d69ce5ff58bfd28d6?s=80\u0026d=identicon',
+              name: 'Jacki Kub',
+              username: 'francina.skiles',
+              webUrl: '/franc',
+            },
+          ],
+          __typename: 'UserConnection',
+        },
+        __typename: 'Issue',
+      },
+      __typename: 'IssueSetAssigneesPayload',
+    },
+  },
+};
