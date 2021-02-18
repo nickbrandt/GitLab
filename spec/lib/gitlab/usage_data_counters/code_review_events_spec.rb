af5196e9534f7c126628f72022ef8416db520d1a
Mikołaj Wawrzyniak <mwawrzyniak@gitlab.com>
2021-02-18 11:55:54 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- spec/lib/gitlab/usage_data_counters/code_review_events_spec.rb
+++ spec/lib/gitlab/usage_data_counters/code_review_events_spec.rb
@@ -5,12 +5,10 @@ require 'spec_helper'
 # If this spec fails, we need to add the new code review event to the correct aggregated metric
 RSpec.describe 'Code review events' do
   it 'the aggregated metrics contain all the code review metrics' do
-    wildcard = Gitlab::Usage::Metrics::Aggregates::AGGREGATED_METRICS_PATH
-    aggregated_events = Dir[wildcard].each_with_object([]) do |path, metrics|
-      metrics.push(*YAML.safe_load(File.read(path), aliases: true)&.map(&:with_indifferent_access))
-    end
+    path = Rails.root.join('lib/gitlab/usage_data_counters/aggregated_metrics/code_review.yml')
+    aggregated_events = YAML.safe_load(File.read(path), aliases: true)&.map(&:with_indifferent_access)
+
     code_review_aggregated_events = aggregated_events
-      .select { |event| event['name'].include?('code_review') }
       .map { |event| event['events'] }
       .flatten
       .uniq
