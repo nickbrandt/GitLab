86bc58a1879c8d2356c894152367ef21d4f46195
Robert May <rmay@gitlab.com>
2021-02-18 12:12:23 +0000

    Forcibly load X509 cert
    Forcibly loads the OpenSSL::X509::DEFAULT_CERT_FILE if it exists,
    as this is seemingly ignored by the OpenSSL library.

--- lib/gitlab/x509/signature.rb
+++ lib/gitlab/x509/signature.rb
@@ -52,6 +52,10 @@ module Gitlab
         strong_memoize(:cert_store) do
           store = OpenSSL::X509::Store.new
           store.set_default_paths
+
+          # Forcibly load the default cert file because the OpenSSL library seemingly ignores it
+          store.add_file(OpenSSL::X509::DEFAULT_CERT_FILE) if File.exist?(OpenSSL::X509::DEFAULT_CERT_FILE)
+
           # valid_signing_time? checks the time attributes already
           # this flag is required, otherwise expired certificates would become
           # unverified when notAfter within certificate attribute is reached
