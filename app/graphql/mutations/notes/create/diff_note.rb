0826e33201107127845921825e74c3d91d92c2ba
Phil Hughes <me@iamphill.com>
2021-02-22 15:12:09 +0000

    Fixed failing rubocop

--- app/graphql/mutations/notes/create/diff_note.rb
+++ app/graphql/mutations/notes/create/diff_note.rb
@@ -18,11 +18,12 @@ module Mutations
           # This may be able to be done using InputUnions in the future
           # if this RFC is merged:
           # https://github.com/graphql/graphql-spec/blob/master/rfcs/InputUnion.md
+
           if args[:position].to_hash.values_at(:old_line, :new_line).compact.blank?
             raise Gitlab::Graphql::Errors::ArgumentError,
                   'position oldLine or newLine arguments are required'
           end
-  
+
           super(**args)
         end
 
