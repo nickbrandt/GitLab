2cbc9d235c2cf248aca966b5f575b4740ed3bb41
Justin Ho <hduong@gitlab.com>
2021-02-25 01:49:36 +0700

    Add issue due date to Jira issue details sidebar
    - Always show the due date field
    - Show "None" if no value is available
    - Add "Past due" text if due date is in the past
    - Show for both collapsed and expanded sidebar

--- ee/app/assets/javascripts/integrations/jira/issues_show/components/sidebar/jira_issues_sidebar_root.vue
+++ ee/app/assets/javascripts/integrations/jira/issues_show/components/sidebar/jira_issues_sidebar_root.vue
@@ -3,12 +3,14 @@ import { labelsFilterParam } from 'ee/integrations/jira/issues_show/constants';
 
 import LabelsSelect from '~/vue_shared/components/sidebar/labels_select_vue/labels_select_root.vue';
 import Assignee from './assignee.vue';
+import IssueDueDate from './issue_due_date.vue';
 import IssueReference from './issue_reference.vue';
 
 export default {
   name: 'JiraIssuesSidebar',
   components: {
     Assignee,
+    IssueDueDate,
     IssueReference,
     LabelsSelect,
   },
@@ -30,10 +32,10 @@ export default {
   computed: {
     assignee() {
       // Jira issues have at most 1 assignee
-      return (this.issue?.assignees || [])[0];
+      return (this.issue.assignees || [])[0];
     },
     reference() {
-      return this.issue?.references?.relative;
+      return this.issue.references?.relative;
     },
   },
   labelsFilterParam,
@@ -43,6 +45,8 @@ export default {
 <template>
   <div>
     <assignee class="block" :assignee="assignee" />
+
+    <issue-due-date :due-date="issue.dueDate" />
 
     <labels-select
       :selected-labels="issue.labels"
