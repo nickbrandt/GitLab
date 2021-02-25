efe1beb20d9ad323eb6980b950845e9b81338582
Florie Guibert <fguibert@gitlab.com>
2021-02-25 10:06:47 +0000

    Add label list to Epic board
    Add new label list to epic board using add column

--- ee/spec/features/epic_boards/epic_boards_spec.rb
+++ ee/spec/features/epic_boards/epic_boards_spec.rb
@@ -8,16 +8,19 @@ RSpec.describe 'epic boards', :js do
 
   let_it_be(:epic_board) { create(:epic_board, group: group) }
   let_it_be(:label) { create(:group_label, group: group, name: 'Label1') }
+  let_it_be(:label2) { create(:group_label, group: group, name: 'Label2') }
   let_it_be(:label_list) { create(:epic_list, epic_board: epic_board, label: label, position: 0) }
   let_it_be(:backlog_list) { create(:epic_list, epic_board: epic_board, list_type: :closed) }
   let_it_be(:closed_list) { create(:epic_list, epic_board: epic_board, list_type: :backlog) }
 
   let_it_be(:epic1) { create(:epic, group: group, labels: [label], title: 'Epic1') }
   let_it_be(:epic2) { create(:epic, group: group, title: 'Epic2') }
+  let_it_be(:epic3) { create(:epic, group: group, labels: [label2], title: 'Epic3') }
 
   context 'display epics in board' do
     before do
       stub_licensed_features(epics: true)
+      group.add_maintainer(user)
       sign_in(user)
       visit_epic_boards_page
     end
@@ -34,10 +37,14 @@ RSpec.describe 'epic boards', :js do
       end
     end
 
-    it 'displays one epic in Open list' do
+    it 'displays two epics in Open list' do
       page.within("[data-board-type='backlog']") do
-        expect(page).to have_selector('.board-card', count: 1)
+        expect(page).to have_selector('.board-card', count: 2)
         page.within(first('.board-card')) do
+          expect(page).to have_content('Epic3')
+        end
+
+        page.within('.board-card:nth-child(2)') do
           expect(page).to have_content('Epic2')
         end
       end
@@ -50,6 +57,21 @@ RSpec.describe 'epic boards', :js do
           expect(page).to have_content('Epic1')
         end
       end
+    end
+
+    it 'creates new column for label containing labeled issue' do
+      click_button 'Create list'
+      wait_for_all_requests
+
+      page.within("[data-testid='board-add-new-column']") do
+        find('label', text: label2.title).click
+        click_button 'Add'
+      end
+
+      wait_for_all_requests
+
+      expect(page).to have_selector('.board', text: label2.title)
+      expect(find('.board:nth-child(3) .board-card')).to have_content(epic3.title)
     end
   end
 
