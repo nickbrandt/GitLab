2b68d4d3dd4803382f058b82fb3cbca82eb234d7
Vasilii Iakliushin <viakliushin@gitlab.com>
2021-03-01 16:41:09 +0100

    Introduce MarkerRange class
    Contributes to https://gitlab.com/gitlab-org/gitlab/-/issues/16950
    
    MarkerRange behaves like a Range but can additionally keep information
    about the type of the change. It helps to identify if changed
    characters were removed or added.

---
+++ lib/gitlab/marker_range.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# It is a Range object extended with `mode` attribute
+# MarkerRange not only keeps information about changed characters, but also
+# the type of changes
+module Gitlab
+  class MarkerRange < Range
+    DELETION = :deletion
+    ADDITION = :addition
+
+    # Converts Range object to MarkerRange class
+    def self.from_range(range)
+      return range if range.is_a?(self)
+
+      new(range.begin, range.end, exclude_end: range.exclude_end?)
+    end
+
+    def initialize(first, last, exclude_end: false, mode: nil)
+      super(first, last, exclude_end)
+      @mode = mode
+    end
+
+    attr_reader :mode
+  end
+end
--- spec/lib/gitlab/diff/highlight_spec.rb
+++ spec/lib/gitlab/diff/highlight_spec.rb
@@ -50,7 +50,7 @@ RSpec.describe Gitlab::Diff::Highlight do
       end
 
       it 'highlights and marks added lines' do
-        code = %Q{+<span id="LC9" class="line" lang="ruby">      <span class="k">raise</span> <span class="no"><span class="idiff left">RuntimeError</span></span><span class="p"><span class="idiff">,</span></span><span class="idiff right"> </span><span class="s2">"System commands must be given as an array of strings"</span></span>\n}
+        code = %Q{+<span id="LC9" class="line" lang="ruby">      <span class="k">raise</span> <span class="no"><span class="idiff left addition">RuntimeError</span></span><span class="p"><span class="idiff addition">,</span></span><span class="idiff right addition"> </span><span class="s2">"System commands must be given as an array of strings"</span></span>\n}
 
         expect(subject[5].rich_text).to eq(code)
       end
@@ -93,7 +93,7 @@ RSpec.describe Gitlab::Diff::Highlight do
       end
 
       it 'marks added lines' do
-        code = %q{+      raise <span class="idiff left right">RuntimeError, </span>&quot;System commands must be given as an array of strings&quot;}
+        code = %q{+      raise <span class="idiff left right addition">RuntimeError, </span>&quot;System commands must be given as an array of strings&quot;}
 
         expect(subject[5].rich_text).to eq(code)
         expect(subject[5].rich_text).to be_html_safe
