ba5d43cde11e2686fc954c7ae47af3a74373bd00
Etienne Baqué <ebaque@gitlab.com>
2021-03-01 13:21:47 +0300

    Updated pages path initializer

--- lib/gitlab/pages/settings.rb
+++ lib/gitlab/pages/settings.rb
@@ -5,6 +5,14 @@ module Gitlab
     class Settings < ::SimpleDelegator
       DiskAccessDenied = Class.new(StandardError)
 
+      def self.build_path_for(path_value)
+        if !path_value && path_value.to_s == 'false'
+          false
+        else
+          ::Settings.absolute(path_value || File.join(::Settings.shared['path'], "pages"))
+        end
+      end
+
       def path
         if ::Gitlab::Runtime.web_server? && !::Gitlab::Runtime.test_suite?
           raise DiskAccessDenied
--- spec/lib/gitlab/pages/settings_spec.rb
+++ spec/lib/gitlab/pages/settings_spec.rb
@@ -21,4 +21,32 @@ RSpec.describe Gitlab::Pages::Settings do
       end
     end
   end
+
+  describe '.build_path_for' do
+    subject(:build_path) { described_class.build_path_for(path_value) }
+
+    context 'when path_value is false' do
+      let(:path_value) { false }
+
+      it 'returns false' do
+        expect(build_path).to be_falsey
+      end
+    end
+
+    context 'when path value is actual false string' do
+      let(:path_value) { 'false' }
+
+      it 'returns path with false as a directory' do
+        expect(build_path).to eq(Settings.absolute(path_value))
+      end
+    end
+
+    context 'when path value is nil' do
+      let(:path_value) { nil }
+
+      it 'returns the default path' do
+        expect(build_path).to eq(Settings.absolute(File.join(Settings.shared['path'], "pages")))
+      end
+    end
+  end
 end
