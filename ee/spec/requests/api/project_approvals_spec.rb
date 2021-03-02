394b57bce11c08a6a8d9639e9788517f4a0b205d
Kerri Miller <kerrizor@kerrizor.com>
2021-03-02 04:58:35 -0800

    Expose boolean predicate methods in API

--- ee/spec/requests/api/project_approvals_spec.rb
+++ ee/spec/requests/api/project_approvals_spec.rb
@@ -20,6 +20,18 @@ RSpec.describe API::ProjectApprovals do
         get api(url, user)
       end
 
+      it 'does not return null values for unmodified boolean fields' do
+        expect(project.disable_overriding_approvers_per_merge_request).to be_nil
+        expect(project.merge_requests_author_approval).to be_nil
+        expect(project.merge_requests_disable_committers_approval).to be_nil
+        expect(project.require_password_to_approve).to be_nil
+
+        expect(json_response["disable_overriding_approvers_per_merge_request"]).to be false
+        expect(json_response["merge_requests_author_approval"]).to be false
+        expect(json_response["merge_requests_disable_committers_approval"]).to be false
+        expect(json_response["require_password_to_approve"]).to be false
+      end
+
       it 'returns 200 status' do
         expect(response).to have_gitlab_http_status(:ok)
       end
