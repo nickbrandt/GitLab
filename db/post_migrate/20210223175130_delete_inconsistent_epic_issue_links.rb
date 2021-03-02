339acf2abadb8df8ab60b6f05e44438b2f83a50c
Eugenia Grieff <egrieff@gitlab.com>
2021-03-02 18:25:31 +0000

    Use EachBatch in group table
    Include ancestor and descendant examples in specs

--- db/post_migrate/20210223175130_delete_inconsistent_epic_issue_links.rb
+++ db/post_migrate/20210223175130_delete_inconsistent_epic_issue_links.rb
@@ -2,20 +2,28 @@
 
 class DeleteInconsistentEpicIssueLinks < ActiveRecord::Migration[6.0]
   DOWNTIME = false
-  BATCH_SIZE = 1_000
-  class EpicIssue < ActiveRecord::Base
-    include ::EachBatch
+  BATCH_SIZE = 100
 
-    self.table_name = 'epic_issues'
-    belongs_to :epic
-    belongs_to :issue
+  class Epic < ActiveRecord::Base
+    include ::EachBatch
+    self.table_name = 'epics'
+
+    has_many :epic_issues
+    scope :with_issues, -> { joins(:epic_issues) }
   end
+
+  class EpicIssue < ActiveRecord::Base
+    self.table_name = 'epic_issues'
+  end
+
+  disable_ddl_transaction!
 
   def up
     return unless ::Gitlab.ee?
 
-    Group.where(type: 'Group').joins(:epics).distinct.find_each do |group|
-      delete_broken_epic_issue_links(group)
+    Epic.with_issues.select('group_id').distinct.each_batch(of: BATCH_SIZE) do |batch|
+      group_ids = batch.pluck(:group_id).join(',')
+      delete_broken_epic_issue_links(group_ids)
     end
   end
 
@@ -25,9 +33,25 @@ class DeleteInconsistentEpicIssueLinks < ActiveRecord::Migration[6.0]
 
   private
 
-  def delete_broken_epic_issue_links(group)
-    EpicIssue.joins(:epic).where(epics: { group_id: group })
-      .joins(issue: :project).where.not(projects: { namespace_id: group.self_and_descendants })
-      .each_batch(of: BATCH_SIZE) { |batch| batch.delete_all }
+  def delete_broken_epic_issue_links(group_ids)
+    invalid_links = ApplicationRecord.connection.execute(<<-SQL.squish)
+      SELECT epic_issues.* FROM epic_issues
+      INNER JOIN epics ON epics.id = epic_issues.epic_id
+      INNER JOIN issues ON issues.id = epic_issues.issue_id
+      INNER JOIN projects ON projects.id = issues.project_id
+      WHERE epics.group_id IN (#{group_ids})
+        AND projects.namespace_id NOT IN (WITH RECURSIVE base_and_descendants AS (
+          (SELECT namespaces.* FROM namespaces
+            WHERE namespaces.type = 'Group'
+            AND namespaces.id IN (#{group_ids}))
+          UNION
+          (SELECT namespaces.* FROM namespaces, base_and_descendants
+            WHERE namespaces.type = 'Group'
+            AND namespaces.parent_id = base_and_descendants.id))
+          SELECT namespaces.id
+          FROM base_and_descendants AS namespaces);
+    SQL
+
+    EpicIssue.where(id: invalid_links.pluck('id')).delete_all
   end
 end
--- ee/spec/migrations/delete_inconsistent_epic_issue_links_spec.rb
+++ ee/spec/migrations/delete_inconsistent_epic_issue_links_spec.rb
@@ -4,38 +4,61 @@ require 'spec_helper'
 require Rails.root.join('db', 'post_migrate', '20210223175130_delete_inconsistent_epic_issue_links')
 
 RSpec.describe DeleteInconsistentEpicIssueLinks, :migration, :sidekiq do
-  let(:users) { table(:users) }
-  let(:namespaces) { table(:namespaces) }
-  let(:projects) { table(:projects) }
-  let(:issues) { table(:issues) }
-  let(:epics) { table(:epics) }
-  let(:epic_issues) { table(:epic_issues) }
+  let_it_be(:users) { table(:users) }
+  let_it_be(:namespaces) { table(:namespaces) }
+  let_it_be(:projects) { table(:projects) }
+  let_it_be(:issues) { table(:issues) }
+  let_it_be(:epics) { table(:epics) }
+  let_it_be(:epic_issues) { table(:epic_issues) }
 
   let(:user) { users.create!(name: 'root', email: 'root@example.com', username: 'root', projects_limit: 0) }
   let(:group1) { namespaces.create!(name: 'group1', path: 'group1', type: 'Group') }
-  let(:group2) { namespaces.create!(name: 'group2', path: 'group2', type: 'Group') }
   let(:project_a) { projects.create!(name: 'project-a', path: 'project-a', namespace_id: group1.id, visibility_level: 0) }
   let(:project_b) { projects.create!(name: 'project-b', path: 'project-b', namespace_id: group1.id, visibility_level: 0) }
 
-  let(:epic) { epics.create!(iid: 1, group_id: group1.id, author_id: user.id, title: 'any', title_html: 'any') }
+  before do
+    epic = epics.create!(iid: 1, group_id: group1.id, author_id: user.id, title: 'any', title_html: 'any')
+    issue1, issue2, issue3 = (1..3).map { |i| issues.create!(issue_type: 0, project_id: project_a.id) }
+    issue4 = issues.create!(issue_type: 0, project_id: project_b.id)
 
-  let(:issue1) { issues.create!(issue_type: 0, project_id: project_a.id) }
-  let(:issue2) { issues.create!(issue_type: 0, project_id: project_a.id) }
-  let(:issue3) { issues.create!(issue_type: 0, project_id: project_a.id) }
-  let(:issue4) { issues.create!(issue_type: 0, project_id: project_b.id) }
-
-  let!(:epic_issue1) { epic_issues.create!(issue_id: issue1.id, epic_id: epic.id) }
-  let!(:epic_issue2) { epic_issues.create!(issue_id: issue2.id, epic_id: epic.id) }
-  let!(:epic_issue3) { epic_issues.create!(issue_id: issue3.id, epic_id: epic.id) }
-  let!(:epic_issue4) { epic_issues.create!(issue_id: issue4.id, epic_id: epic.id) }
+    [issue1, issue2, issue3, issue4].each do |issue|
+      epic_issues.create!(issue_id: issue.id, epic_id: epic.id)
+    end
+  end
 
   describe '#up' do
-    before do
-      project_a.update!(namespace_id: group2.id) # Simulate transferring project-a to group2
+    context "when the issue's group is different to the epic's group" do
+      before do
+        group2 = namespaces.create!(name: 'group2', path: 'group2', type: 'Group')
+        project_a.update!(namespace_id: group2.id)
+      end
+
+      it 'deletes epic issue links' do
+        expect { migrate! }.to change { epic_issues.count }.by(-3)
+      end
     end
 
-    it 'cleanups invalid epic issue links' do
-      expect { migrate! }.to change { epic_issues.count }.by(-3)
+    context "when the issue's group is a descendant of the epic's group " do
+      before do
+        subgroup = namespaces.create!(name: 'subgroup', path: 'group1/subgroup', type: 'Group', parent_id: group1.id)
+        project_a.update!(namespace_id: subgroup.id)
+      end
+
+      it 'does not delete epic issue links' do
+        expect { migrate! }.not_to change { epic_issues.count }
+      end
+    end
+
+    context "when the issue's group is an ancestor of the epic's group" do
+      before do
+        parent1 = namespaces.create!(name: 'parent1', path: 'parent1', type: 'Group')
+        group1.update!(parent_id: parent1.id)
+        project_a.update!(namespace_id: parent1.id)
+      end
+
+      it 'deletes epic issue links' do
+        expect { migrate! }.to change { epic_issues.count }.by(-3)
+      end
     end
   end
 end
