dc11e5b7f4d4bd8c276796f4d21a5e126c3ff212
Mark Florian <mflorian@gitlab.com>
2021-03-03 11:35:52 +0000

    Add pinning test for current implementation
    Here we set up a [pinning test](http://wiki.c2.com/?PinningTests) to
    lock down the complete behaviour of the current implementation of the
    Security Configuration page.
    
    Because the state of the configuration table is completely determined by
    the `data-` attributes of the mount element, it's easy to mock various
    scenarios, just by copying the returned markup from the page of
    a running instance, e.g., production.
    
    Three projects from production are used to drive the tests:
    
    1. `gitlab-org/gitlab-ui` (this has some manually enabled scanners)
    1. `markrian/qr` (this has _no_ enabled scanners)
    1. `markrian-test/test-public-project` (this has some scanners enabled
       via Auto DevOps)
    
    Since these test establish the current behaviour, the tests pass by
    definition.

---
+++ ee/spec/frontend/security_configuration/index_spec.js
@@ -0,0 +1,59 @@
+import Vue, { nextTick } from 'vue';
+import { initSecurityConfiguration } from 'ee/security_configuration';
+import { resetHTMLFixture } from 'helpers/fixtures';
+import { someEnabledEl, noneEnabledEl, someEnabledWithAutoDevOpsEl } from './mock_data';
+
+const errorsAndWarnings = [];
+
+Vue.config.errorHandler = Vue.config.warnHandler = (error, vm, info) => {
+  errorsAndWarnings.push(`Vue error/warning: Message: ${error}
+
+vm.name: ${vm.name}
+info: ${JSON.stringify(info)}
+  `);
+};
+
+// Not using GlFeatureFlagsPlugin because it eagerly reads from gon, whereas
+// this is lazy. This allows the gon.features to be changed before each test.
+Vue.mixin({
+  provide() {
+    return {
+      glFeatures: { ...((window.gon && window.gon.features) || {}) },
+    };
+  },
+});
+
+describe('Security Configuration App', () => {
+  describe.each`
+    context                                | mountElHtml
+    ${'some enabled scanners (gitlab-ui)'} | ${someEnabledEl}
+    ${'no enabled scanners'}               | ${noneEnabledEl}
+    ${'some enabled by ADO'}               | ${someEnabledWithAutoDevOpsEl}
+  `('given $context', ({ mountElHtml }) => {
+    beforeEach(() => {
+      setFixtures(mountElHtml);
+
+      const el = document.querySelector('#js-security-configuration');
+      gon.features = {
+        // Mimic production; this will be removed in https://gitlab.com/gitlab-org/gitlab/-/issues/235135
+        sastConfigurationUi: true,
+
+        // This is a temporary flag just for this spike. Used to switch between
+        // re-architected implementation and original.
+        newSecurityConfiguration: false,
+      };
+      initSecurityConfiguration(el);
+
+      return nextTick();
+    });
+
+    afterEach(() => {
+      resetHTMLFixture();
+    });
+
+    it('matches the snapshot', () => {
+      expect(errorsAndWarnings).toEqual([]);
+      expect(document.body).toMatchSnapshot();
+    });
+  });
+});
