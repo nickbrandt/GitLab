4d7255b99af1b89b34945592b803da8a91b58066
Małgorzata Ksionek <mksionek@gitlab.com>
2021-03-03 19:38:19 +0100

    Make crypto-helper generic class
    Add new way of saving encrypted tokens
    
    Stop testing not used read only method
    
    WIP on new encryption
    
    Add working spec
    
    Add specs about new way of encrypting/decrypting tokens
    
    Refactor encryption methods into own class
    
    Modify specs to mirror new code structure
    
    Add specs for the new class
    
    Fix crypto-helper spec
    
    Add cr remarks
    
    Add nonce size condition
    
    Fix naming in specs from form_encrypted_token to encrypt_token
    
    Clean up crypto helper class

---
+++ app/models/concerns/token_authenticatable_strategies/encryption_helper.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module TokenAuthenticatableStrategies
+  class EncryptionHelper
+    DYNAMIC_NONCE_IDENTIFIER = "|"
+    NONCE_SIZE = 12
+
+    def self.encrypt_token(token)
+      iv = ::Digest::SHA256.hexdigest(token).bytes.take(NONCE_SIZE).pack('c*')
+      token = Gitlab::CryptoHelper.aes256_gcm_encrypt(token, nonce: iv)
+      "#{DYNAMIC_NONCE_IDENTIFIER}#{token}#{iv}"
+    end
+
+    def self.decrypt_token(encrypted_token)
+      return unless encrypted_token
+
+      if encrypted_token[0] == DYNAMIC_NONCE_IDENTIFIER && encrypted_token.size > NONCE_SIZE + 1
+        iv = encrypted_token[-NONCE_SIZE..-1]
+        token = encrypted_token[1...-NONCE_SIZE]
+        Gitlab::CryptoHelper.aes256_gcm_decrypt(token, nonce: iv)
+      else
+        Gitlab::CryptoHelper.aes256_gcm_decrypt(encrypted_token, nonce: Gitlab::CryptoHelper::AES256_GCM_IV_STATIC)
+      end
+    end
+  end
+end
