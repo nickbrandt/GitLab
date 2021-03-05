fcf2ef6f18ca879e408ef43a051a6c44697e82b6
Doug Stull <dstull@gitlab.com>
2021-03-05 14:21:34 -0500

    Updates view specs for merge request show
    - for prep in extending them in subsequent MR
    - gain performance by using let_it_be

--- ee/spec/views/projects/merge_requests/show.html.haml_spec.rb
+++ ee/spec/views/projects/merge_requests/show.html.haml_spec.rb
@@ -3,19 +3,10 @@
 require 'spec_helper'
 
 RSpec.describe 'projects/merge_requests/show.html.haml' do
-  before do
-    allow(view).to receive(:experiment_enabled?).and_return(false)
-  end
-
-  include_context 'closed merge request show action'
+  include_context 'merge request show action'
 
   context 'when merge request is created by a GitLab team member' do
-    let(:user) { create(:user) }
-
-    include_context 'gitlab team member'
-
     before do
-      allow(Gitlab).to receive(:com?).and_return(true)
       allow(user).to receive(:gitlab_employee?).and_return(true)
     end
 
