bcc383ec7a0c7bedc06ca652ae336e2bd1269fc0
Robert Hunt <rhunt@gitlab.com>
2021-03-05 10:31:08 +0000

    Moved the isNumeric function to a shared util
    The isNumeric function was being used in two different places. This
    moves that function to a shared util and updates the places to use
    the new shared util instead. It also moves the test to the shard util
    as well

--- ee/app/assets/javascripts/groups/settings/compliance_frameworks/utils.js
+++ ee/app/assets/javascripts/groups/settings/compliance_frameworks/utils.js
@@ -1,10 +1,7 @@
 import Api from '~/api';
 import httpStatus from '~/lib/utils/http_status';
+import { isNumeric } from '~/lib/utils/number_utils';
 import { EDIT_PATH_ID_FORMAT, PIPELINE_CONFIGURATION_PATH_FORMAT } from './constants';
-
-const isNumeric = (value) => {
-  return !Number.isNaN(parseInt(value, 10));
-};
 
 export const injectIdIntoEditPath = (path, id) => {
   if (!path.match(EDIT_PATH_ID_FORMAT) || !isNumeric(id)) {
