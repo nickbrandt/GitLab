05a8a521235ed48651bd7e6e1a9931340957f57f
Simon Knox <simon@gitlab.com>
2021-03-06 01:45:40 +0000

    Remove remove from board button
    This isn't implemented in graphql yet, and is a bit
    broken in regular boards.

--- ee/app/assets/javascripts/boards/components/board_sidebar.js
+++ ee/app/assets/javascripts/boards/components/board_sidebar.js
@@ -1,10 +1,8 @@
 import Weight from 'ee/sidebar/components/weight/weight.vue';
 import BoardSidebar from '~/boards/components/board_sidebar';
-import RemoveBtn from './sidebar/remove_issue';
 
 export default BoardSidebar.extend({
   components: {
-    RemoveBtn,
     Weight,
   },
 });
