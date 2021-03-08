443974be215b65b0bbe8fced5b8652ca583fea21
Emily Ring <ering@gitlab.com>
2021-03-08 23:56:22 +0000

    Updated cluster token name migration
    Renamed migrations since master now has a conflicting migration
    Added missing db/schema_migrations/20210303165302

--- db/structure.sql
+++ db/structure.sql
@@ -19952,6 +19952,9 @@ ALTER TABLE ONLY chat_names
 ALTER TABLE ONLY chat_teams
     ADD CONSTRAINT chat_teams_pkey PRIMARY KEY (id);
 
+ALTER TABLE cluster_agent_tokens
+    ADD CONSTRAINT check_0fb634d04d CHECK ((name IS NOT NULL)) NOT VALID;
+
 ALTER TABLE vulnerability_scanners
     ADD CONSTRAINT check_37608c9db5 CHECK ((char_length(vendor) <= 255)) NOT VALID;
 
