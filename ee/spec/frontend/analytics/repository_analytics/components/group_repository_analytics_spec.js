ef2efb83be660cb477a4bbdf8ec106cc53225827
Miranda Fluharty <mfluharty@gitlab.com>
2021-03-08 23:03:10 +0000

    Track group coverage visits in vue instead
    Count unique users in the same way as other group::testing metrics
    So we can use the unique user counts in aggregated metrics

--- ee/spec/frontend/analytics/repository_analytics/components/group_repository_analytics_spec.js
+++ ee/spec/frontend/analytics/repository_analytics/components/group_repository_analytics_spec.js
@@ -1,26 +1,30 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import DownloadTestCoverage from 'ee/analytics/repository_analytics/components/download_test_coverage.vue';
-import GroupRepositoryAnalytics from 'ee/analytics/repository_analytics/components/group_repository_analytics.vue';
+import GroupRepositoryAnalytics, {
+  VISIT_EVENT_FEATURE_FLAG,
+  VISIT_EVENT_NAME,
+} from 'ee/analytics/repository_analytics/components/group_repository_analytics.vue';
+import Api from '~/api';
 
 const localVue = createLocalVue();
+jest.mock('~/api.js');
 
 describe('Group repository analytics app', () => {
   let wrapper;
 
-  const createComponent = () => {
-    wrapper = shallowMount(GroupRepositoryAnalytics, { localVue });
+  const createComponent = (glFeatures = {}) => {
+    wrapper = shallowMount(GroupRepositoryAnalytics, { localVue, provide: { glFeatures } });
   };
-
-  beforeEach(() => {
-    createComponent();
-  });
 
   afterEach(() => {
     wrapper.destroy();
-    wrapper = null;
   });
 
   describe('test coverage', () => {
+    beforeEach(() => {
+      createComponent();
+    });
+
     it('renders test coverage header', () => {
       const header = wrapper.find('[data-testid="test-coverage-header"]');
 
@@ -29,6 +33,28 @@ describe('Group repository analytics app', () => {
 
     it('renders the download test coverage component', () => {
       expect(wrapper.find(DownloadTestCoverage).exists()).toBe(true);
+    });
+  });
+
+  describe('usage ping events', () => {
+    describe('with the feature flag enabled', () => {
+      beforeEach(() => {
+        createComponent({ [VISIT_EVENT_FEATURE_FLAG]: true });
+      });
+
+      it('tracks a visit event on mount', () => {
+        expect(Api.trackRedisHllUserEvent).toHaveBeenCalledWith(VISIT_EVENT_NAME);
+      });
+    });
+
+    describe('with the feature flag disabled', () => {
+      beforeEach(() => {
+        createComponent({ [VISIT_EVENT_FEATURE_FLAG]: false });
+      });
+
+      it('does not track a visit event on mount', () => {
+        expect(Api.trackRedisHllUserEvent).not.toHaveBeenCalled();
+      });
     });
   });
 });
