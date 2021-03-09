0ccb6aa077dab03f3cf1fb075430c850e5b0c912
Vijay Hawoldar <vhawoldar@gitlab.com>
2021-03-09 14:19:49 +0000

    Separate GitlabSubscriptions module from class name
    To be consistent with other module definitions

--- ee/app/services/gitlab_subscriptions/fetch_subscription_plans_service.rb
+++ ee/app/services/gitlab_subscriptions/fetch_subscription_plans_service.rb
@@ -1,63 +1,65 @@
 # frozen_string_literal: true
-class GitlabSubscriptions::FetchSubscriptionPlansService
-  URL = "#{EE::SUBSCRIPTIONS_URL}/gitlab_plans".freeze
+module GitlabSubscriptions
+  class FetchSubscriptionPlansService
+    URL = "#{EE::SUBSCRIPTIONS_URL}/gitlab_plans".freeze
 
-  def initialize(plan:, namespace_id: nil)
-    @plan = plan
-    @namespace_id = namespace_id
-  end
-
-  def execute
-    cached { send_request }
-  end
-
-  private
-
-  def send_request
-    response = Gitlab::HTTP.get(
-      URL,
-      allow_local_requests: true,
-      query: { plan: @plan, namespace_id: @namespace_id },
-      headers: { 'Accept' => 'application/json' }
-    )
-
-    Gitlab::Json.parse(response.body).map { |plan| Hashie::Mash.new(plan) }
-  rescue => e
-    Gitlab::AppLogger.info "Unable to connect to GitLab Customers App #{e}"
-
-    nil
-  end
-
-  def cached
-    if plans_data = cache.read(cache_key)
-      plans_data
-    else
-      cache.fetch(cache_key, force: true, expires_in: 1.day) { yield }
+    def initialize(plan:, namespace_id: nil)
+      @plan = plan
+      @namespace_id = namespace_id
     end
-  end
 
-  def cache
-    Rails.cache
-  end
+    def execute
+      cached { send_request }
+    end
 
-  def cache_key
-    if Feature.enabled?(:pnp_subscription_plan_cache_key)
-      if @namespace_id.present?
-        "pnp-subscription-plan-#{@plan}-#{@namespace_id}"
+    private
+
+    def send_request
+      response = Gitlab::HTTP.get(
+        URL,
+        allow_local_requests: true,
+        query: { plan: @plan, namespace_id: @namespace_id },
+        headers: { 'Accept' => 'application/json' }
+      )
+
+      Gitlab::Json.parse(response.body).map { |plan| Hashie::Mash.new(plan) }
+    rescue => e
+      Gitlab::AppLogger.info "Unable to connect to GitLab Customers App #{e}"
+
+      nil
+    end
+
+    def cached
+      if plans_data = cache.read(cache_key)
+        plans_data
       else
-        "pnp-subscription-plan-#{@plan}"
+        cache.fetch(cache_key, force: true, expires_in: 1.day) { yield }
       end
-    elsif Feature.enabled?(:subscription_plan_cache_key)
-      if @namespace_id.present?
-        "subscription-plan-#{@plan}-#{@namespace_id}"
+    end
+
+    def cache
+      Rails.cache
+    end
+
+    def cache_key
+      if Feature.enabled?(:pnp_subscription_plan_cache_key)
+        if @namespace_id.present?
+          "pnp-subscription-plan-#{@plan}-#{@namespace_id}"
+        else
+          "pnp-subscription-plan-#{@plan}"
+        end
+      elsif Feature.enabled?(:subscription_plan_cache_key)
+        if @namespace_id.present?
+          "subscription-plan-#{@plan}-#{@namespace_id}"
+        else
+          "subscription-plan-#{@plan}"
+        end
       else
-        "subscription-plan-#{@plan}"
-      end
-    else
-      if @namespace_id.present?
-        "subscription-plans-#{@plan}-#{@namespace_id}"
-      else
-        "subscription-plans-#{@plan}"
+        if @namespace_id.present?
+          "subscription-plans-#{@plan}-#{@namespace_id}"
+        else
+          "subscription-plans-#{@plan}"
+        end
       end
     end
   end
