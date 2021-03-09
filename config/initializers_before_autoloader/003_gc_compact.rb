b5fcc9f9a09a826b736adf8758252bab7e8e0055
Aleksei Lipniagov <alipniagov@gitlab.com>
2021-03-09 14:23:19 +0300

    Fix rubocop offences

--- config/initializers_before_autoloader/003_gc_compact.rb
+++ config/initializers_before_autoloader/003_gc_compact.rb
@@ -8,6 +8,7 @@
 # only without `GC.compact` (still invoking 4 GC cycles).
 # Refer to for details: https://github.com/puma/puma/blob/80274413b04fae77cac7a7fecab7d6e89204343b/lib/puma/util.rb#L27
 
+# rubocop:disable Rails/Output
 module NakayoshiForkCompacting
   module MonkeyPatch
     def compact
@@ -18,4 +19,3 @@ module NakayoshiForkCompacting
 end
 
 GC.singleton_class.prepend NakayoshiForkCompacting::MonkeyPatch
-
