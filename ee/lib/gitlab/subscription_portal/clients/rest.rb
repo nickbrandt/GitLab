ba20f4e4b1c2dc23da01c2291df0150b546bbb30
Vijay Hawoldar <vhawoldar@gitlab.com>
2021-03-10 01:53:28 +0000

    Refactor Gitlab::SubscriptionPortal::Client into concerns
    Our subscriptions portal (CustomersDot) is growing it's
    support for GraphQL. Currently all queries are routed through
    one Client class, this commit splits it into REST and Graphql
    concerns accordingly to make it more manageable

---
+++ ee/lib/gitlab/subscription_portal/clients/rest.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module SubscriptionPortal
+    module Clients
+      module REST
+        extend ActiveSupport::Concern
+
+        class_methods do
+          def generate_trial(params)
+            http_post("trials", admin_headers, params)
+          end
+
+          def create_customer(params)
+            http_post("api/customers", admin_headers, params)
+          end
+
+          def create_subscription(params, email, token)
+            http_post("subscriptions", customer_headers(email, token), params)
+          end
+
+          def payment_form_params(payment_type)
+            http_get("payment_forms/#{payment_type}", admin_headers)
+          end
+
+          def payment_method(id)
+            http_get("api/payment_methods/#{id}", admin_headers)
+          end
+
+          private
+
+          def base_url
+            EE::SUBSCRIPTIONS_URL
+          end
+
+          def http_get(path, headers)
+            response = Gitlab::HTTP.get("#{base_url}/#{path}", headers: headers)
+
+            parse_response(response)
+          rescue *Gitlab::HTTP::HTTP_ERRORS => e
+            { success: false, data: { errors: e.message } }
+          end
+
+          def http_post(path, headers, params = {})
+            response = Gitlab::HTTP.post("#{base_url}/#{path}", body: params.to_json, headers: headers)
+
+            parse_response(response)
+          rescue *Gitlab::HTTP::HTTP_ERRORS => e
+            { success: false, data: { errors: e.message } }
+          end
+        end
+      end
+    end
+  end
+end
---
+++ ee/spec/lib/gitlab/subscription_portal/clients/rest_spec.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Gitlab::SubscriptionPortal::Clients::REST do
+  let(:client) { Gitlab::SubscriptionPortal::Client }
+  let(:http_response) { nil }
+  let(:http_method) { :post }
+  let(:gitlab_http_response) do
+    double(code: http_response.code, response: http_response, body: {}, parsed_response: {})
+  end
+
+  shared_examples 'when response is successful' do
+    let(:http_response) { Net::HTTPSuccess.new(1.0, '201', 'OK') }
+
+    it 'has a successful status' do
+      allow(Gitlab::HTTP).to receive(http_method).and_return(gitlab_http_response)
+
+      expect(subject[:success]).to eq(true)
+    end
+  end
+
+  shared_examples 'when response code is 422' do
+    let(:http_response) { Net::HTTPUnprocessableEntity.new(1.0, '422', 'Error') }
+
+    it 'has a unprocessable entity status' do
+      allow(Gitlab::HTTP).to receive(http_method).and_return(gitlab_http_response)
+
+      expect(subject[:success]).to eq(false)
+    end
+  end
+
+  shared_examples 'when response code is 500' do
+    let(:http_response) { Net::HTTPServerError.new(1.0, '500', 'Error') }
+
+    it 'has a server error status' do
+      allow(Gitlab::HTTP).to receive(http_method).and_return(gitlab_http_response)
+
+      expect(subject[:success]).to eq(false)
+    end
+  end
+
+  describe '#create_trial_account' do
+    subject do
+      client.generate_trial({})
+    end
+
+    it_behaves_like 'when response is successful'
+    it_behaves_like 'when response code is 422'
+    it_behaves_like 'when response code is 500'
+  end
+
+  describe '#create_subscription' do
+    subject do
+      client.create_subscription({}, 'customer@mail.com', 'token')
+    end
+
+    it_behaves_like 'when response is successful'
+    it_behaves_like 'when response code is 422'
+    it_behaves_like 'when response code is 500'
+  end
+
+  describe '#create_customer' do
+    subject do
+      client.create_customer({})
+    end
+
+    it_behaves_like 'when response is successful'
+    it_behaves_like 'when response code is 422'
+    it_behaves_like 'when response code is 500'
+  end
+
+  describe '#payment_form_params' do
+    subject do
+      client.payment_form_params('cc')
+    end
+
+    let(:http_method) { :get }
+
+    it_behaves_like 'when response is successful'
+    it_behaves_like 'when response code is 422'
+    it_behaves_like 'when response code is 500'
+  end
+
+  describe '#payment_method' do
+    subject do
+      client.payment_method('1')
+    end
+
+    let(:http_method) { :get }
+
+    it_behaves_like 'when response is successful'
+    it_behaves_like 'when response code is 422'
+    it_behaves_like 'when response code is 500'
+  end
+end
