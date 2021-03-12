5a5169f86a357b5ed994ffc2ed29a917980eaad1
Allison Browne <abrowne@gitlab.com>
2021-03-12 14:12:38 -0500

    Namespace existing Ci::DestroyExpiredJobArtifactsService
    Namespace Ci::DestroyExpiredJobArtifactsService as
    Ci::JobArtifacts::DestroyExpiredService

--- app/services/ci/job_artifacts/destroy_expired_service.rb
+++ app/services/ci/job_artifacts/destroy_expired_service.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Ci
-  class DestroyExpiredJobArtifactsService
+  class JobArtifacts::DestroyExpiredService
     include ::Gitlab::ExclusiveLeaseHelpers
     include ::Gitlab::LoopHelpers
 
