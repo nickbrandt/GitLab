731c40a0de290157f5d6d203d3689309ffbf718a
Allison Browne <abrowne@gitlab.com>
2021-03-12 20:12:10 +0000

    Refactor: Extract JobArtifactsDestroyAsyncService
    Extract parallel batch destruction of artifacts for
    reuse in project destruction

--- ee/app/services/ee/ci/job_artifacts_destroy_batch_service.rb
+++ ee/app/services/ee/ci/job_artifacts_destroy_batch_service.rb
@@ -2,17 +2,17 @@
 
 module EE
   module Ci
-    module DestroyExpiredJobArtifactsService
+    module JobArtifactsDestroyBatchService
       extend ::Gitlab::Utils::Override
 
       private
 
-      override :destroy_related_records_for
-      def destroy_related_records_for(artifacts)
-        destroy_security_findings_for(artifacts)
+      override :destroy_related_records
+      def destroy_related_records(artifacts)
+        destroy_security_findings(artifacts)
       end
 
-      def destroy_security_findings_for(artifacts)
+      def destroy_security_findings(artifacts)
         job_ids = artifacts.map(&:job_id)
 
         ::Security::Finding.by_build_ids(job_ids).delete_all
---
+++ ee/spec/services/ee/ci/job_artifacts_destroy_batch_service_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Ci::JobArtifactsDestroyBatchService do
+  describe '.execute' do
+    subject { service.execute }
+
+    let(:service) { described_class.new(Ci::JobArtifact.all, pick_up_at: Time.current) }
+
+    let_it_be(:artifact) { create(:ci_job_artifact) }
+    let_it_be(:security_scan) { create(:security_scan, build: artifact.job) }
+    let_it_be(:security_finding) { create(:security_finding, scan: security_scan) }
+
+    it 'destroys all expired artifacts' do
+      expect { subject }.to change { Ci::JobArtifact.count }.by(-1)
+                        .and change { Security::Finding.count }.from(1).to(0)
+    end
+  end
+end
