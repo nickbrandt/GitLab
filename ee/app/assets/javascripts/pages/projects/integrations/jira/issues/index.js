02b8cd0913982fc25714661b4fd78ca89138f97f
Justin Ho <hduong@gitlab.com>
2021-03-12 14:12:16 +0700

    Change selector to use class instead of id

--- ee/app/assets/javascripts/pages/projects/integrations/jira/issues/index.js
+++ ee/app/assets/javascripts/pages/projects/integrations/jira/issues/index.js
@@ -1,3 +1,3 @@
 import initJiraIssuesList from 'ee/integrations/jira/issues_list/jira_issues_list_bundle';
 
-initJiraIssuesList({ mountPointSelector: '#js-jira-issues-list' });
+initJiraIssuesList({ mountPointSelector: '.js-jira-issues-list' });
