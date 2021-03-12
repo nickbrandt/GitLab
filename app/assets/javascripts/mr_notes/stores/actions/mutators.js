433ae04f9404fc006911e9473d1287a370d9e819
Thomas Randolph <trandolph@gitlab.com>
2021-03-12 15:27:14 -0700

    Add mutator action that ingests endpoints

--- app/assets/javascripts/mr_notes/stores/actions/mutators.js
+++ app/assets/javascripts/mr_notes/stores/actions/mutators.js
@@ -5,3 +5,7 @@ import types from '../mutation_types';
 export function setActiveTab({ commit }, tab) {
   commit(types.SET_ACTIVE_TAB, tab);
 }
+
+export function setEndpoints({ commit }, endpoints) {
+  commit(types.SET_ENDPOINTS, endpoints);
+}
