5f6e219ea83fd93bbb5e775532e64b49bdf42e55
Reuben Pereira <rpereira@gitlab.com>
2021-03-13 23:06:53 +0530

    Update readers of projects.container_registry_enabled
    Update readers of projects.container_registry_enabled to read
    project_features.container_registry_access_level.
    
    Writers will continue writing to projects.container_registry_enabled.
    The before_update callback on the Project model and the before_create
    on ProjectFeature model will copy the value to
    project_features.container_registry_access_level.

--- app/models/project.rb
+++ app/models/project.rb
@@ -412,8 +412,9 @@ class Project < ApplicationRecord
     :wiki_access_level, :snippets_access_level, :builds_access_level,
     :repository_access_level, :pages_access_level, :metrics_dashboard_access_level, :analytics_access_level,
     :operations_enabled?, :operations_access_level, :security_and_compliance_access_level,
-    :container_registry_access_level,
+    :container_registry_access_level, :container_registry_enabled?,
     to: :project_feature, allow_nil: true
+  alias_method :container_registry_enabled, :container_registry_enabled?
   delegate :show_default_award_emojis, :show_default_award_emojis=,
     :show_default_award_emojis?,
     to: :project_setting, allow_nil: true
@@ -552,7 +553,6 @@ class Project < ApplicationRecord
   scope :include_project_feature, -> { includes(:project_feature) }
   scope :with_service, ->(service) { joins(service).eager_load(service) }
   scope :with_shared_runners, -> { where(shared_runners_enabled: true) }
-  scope :with_container_registry, -> { where(container_registry_enabled: true) }
   scope :inside_path, ->(path) do
     # We need routes alias rs for JOIN so it does not conflict with
     # includes(:route) which we use in ProjectsFinder.
