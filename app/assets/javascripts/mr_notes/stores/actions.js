71d146f1cd865c778274b40750a65879cde4dc9a
Thomas Randolph <trandolph@gitlab.com>
2021-03-15 13:55:01 -0600

    Revert back to single-file actions

--- app/assets/javascripts/mr_notes/stores/actions.js
+++ app/assets/javascripts/mr_notes/stores/actions.js
@@ -1,1 +1,9 @@
-export * from './actions/mutators';
+import types from './mutation_types';
+
+export function setActiveTab({ commit }, tab) {
+  commit(types.SET_ACTIVE_TAB, tab);
+}
+
+export function setEndpoints({ commit }, endpoints) {
+  commit(types.SET_ENDPOINTS, endpoints);
+}
--- spec/frontend/mr_notes/stores/actions_spec.js
+++ spec/frontend/mr_notes/stores/actions_spec.js
@@ -1,5 +1,5 @@
 import testAction from 'helpers/vuex_action_helper';
-import { setEndpoints } from '~/mr_notes/stores/actions/mutators';
+import { setEndpoints } from '~/mr_notes/stores/actions';
 import mutationTypes from '~/mr_notes/stores/mutation_types';
 
 describe('MR Notes Mutator Actions', () => {
