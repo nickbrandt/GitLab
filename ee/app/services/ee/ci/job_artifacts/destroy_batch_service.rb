aea6929900ad9f4242253e69aa99ba79e2a51fa6
Allison Browne <abrowne@gitlab.com>
2021-03-15 14:14:29 -0400

    Switch to nested module and class definition
    Switch to nested module and classes instead of compact
    to follow this guideline:
    
    https://github.com/rubocop/ruby-style-guide\#namespace-definition

--- ee/app/services/ee/ci/job_artifacts/destroy_batch_service.rb
+++ ee/app/services/ee/ci/job_artifacts/destroy_batch_service.rb
@@ -2,20 +2,22 @@
 
 module EE
   module Ci
-    module JobArtifacts::DestroyBatchService
-      extend ::Gitlab::Utils::Override
+    module JobArtifacts
+      module DestroyBatchService
+        extend ::Gitlab::Utils::Override
 
-      private
+        private
 
-      override :destroy_related_records
-      def destroy_related_records(artifacts)
-        destroy_security_findings(artifacts)
-      end
+        override :destroy_related_records
+        def destroy_related_records(artifacts)
+          destroy_security_findings(artifacts)
+        end
 
-      def destroy_security_findings(artifacts)
-        job_ids = artifacts.map(&:job_id)
+        def destroy_security_findings(artifacts)
+          job_ids = artifacts.map(&:job_id)
 
-        ::Security::Finding.by_build_ids(job_ids).delete_all
+          ::Security::Finding.by_build_ids(job_ids).delete_all
+        end
       end
     end
   end
