b67fa5b42a7a66042ccdb08e28f5b89dc45cf000
charlie ablett <cablett@gitlab.com>
2021-03-15 17:31:36 +1300

    Track epic destroy note via usage ping
    - Add usage ping metrics files

--- ee/app/services/ee/notes/destroy_service.rb
+++ ee/app/services/ee/notes/destroy_service.rb
@@ -11,6 +11,14 @@ module EE
 
         ::Analytics::RefreshCommentsData.for_note(note)&.execute(force: true)
         ::Gitlab::StatusPage.trigger_publish(project, current_user, note)
+
+        track_note_removal_usage_epics if note.for_epic?
+      end
+
+      private
+
+      def track_note_removal_usage_epics
+        ::Gitlab::UsageDataCounters::EpicActivityUniqueCounter.track_epic_note_destroyed_action(author: current_user)
       end
     end
   end
