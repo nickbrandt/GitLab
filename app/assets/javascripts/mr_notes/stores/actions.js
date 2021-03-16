9a326fa7076f88348b7a8892768b71a7a86a6b44
Thomas Randolph <trandolph@gitlab.com>
2021-03-16 00:06:17 -0600

    Add a mutator action to set the MR Metadata

--- app/assets/javascripts/mr_notes/stores/actions.js
+++ app/assets/javascripts/mr_notes/stores/actions.js
@@ -7,3 +7,7 @@ export function setActiveTab({ commit }, tab) {
 export function setEndpoints({ commit }, endpoints) {
   commit(types.SET_ENDPOINTS, endpoints);
 }
+
+export function setMrMetadata({ commit }, metadata) {
+  commit(types.SET_MR_METADATA, metadata);
+}
--- spec/frontend/mr_notes/stores/actions_spec.js
+++ spec/frontend/mr_notes/stores/actions_spec.js
@@ -1,5 +1,5 @@
 import testAction from 'helpers/vuex_action_helper';
-import { setEndpoints } from '~/mr_notes/stores/actions';
+import { setEndpoints, setMrMetadata } from '~/mr_notes/stores/actions';
 import mutationTypes from '~/mr_notes/stores/mutation_types';
 
 describe('MR Notes Mutator Actions', () => {
@@ -15,6 +15,26 @@ describe('MR Notes Mutator Actions', () => {
           {
             type: mutationTypes.SET_ENDPOINTS,
             payload: endpoints,
+          },
+        ],
+        [],
+        done,
+      );
+    });
+  });
+
+  describe('setMrMetadata', () => {
+    it('should trigger the SET_MR_METADATA state mutation', (done) => {
+      const mrMetadata = { propA: 'a', propB: 'b' };
+
+      testAction(
+        setMrMetadata,
+        mrMetadata,
+        {},
+        [
+          {
+            type: mutationTypes.SET_MR_METADATA,
+            payload: mrMetadata,
           },
         ],
         [],
