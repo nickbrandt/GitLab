3a8cfe96e9d535785ab28576a7c4691808894019
Francisco Javier López <fjlopez@gitlab.com>
2021-03-18 11:11:51 +0100

    Add sitemap generation for Gitlab.com and gitlab-org namespace
    In this commit we remove the feature flag that enables the sitemap
    generation for the `gitlab-org` namespace in .com.

--- ee/spec/controllers/sitemap_controller_spec.rb
+++ ee/spec/controllers/sitemap_controller_spec.rb
@@ -24,22 +24,10 @@ RSpec.describe SitemapController do
       let(:dot_com) { true }
 
       context 'with an authenticated user' do
-        let(:flag_value) { true }
-
         before do
-          stub_feature_flags(gitlab_org_sitemap: flag_value)
-
           allow(Sitemap::CreateService).to receive_message_chain(:new, :execute).and_return(result)
 
           subject
-        end
-
-        shared_examples 'gitlab_org_sitemap flag is disabled' do
-          let(:flag_value) { false }
-
-          it 'returns :not_found' do
-            expect(response).to have_gitlab_http_status(:not_found)
-          end
         end
 
         context 'when the sitemap generation raises an error' do
@@ -49,8 +37,6 @@ RSpec.describe SitemapController do
             expect(response).to have_gitlab_http_status(:ok)
             expect(response.body).to include('<error>foo</error>')
           end
-
-          it_behaves_like 'gitlab_org_sitemap flag is disabled'
         end
 
         context 'when the sitemap was created suscessfully' do
@@ -60,8 +46,6 @@ RSpec.describe SitemapController do
             expect(response).to have_gitlab_http_status(:ok)
             expect(response.body).to eq('foo')
           end
-
-          it_behaves_like 'gitlab_org_sitemap flag is disabled'
         end
       end
     end
