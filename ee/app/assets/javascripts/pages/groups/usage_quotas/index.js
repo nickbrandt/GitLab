0e9829a8c5b989747de6e1ce44075cdf5d1a83a8
Ammar Alakkad <am.alakkad@gmail.com>
2021-03-22 16:50:08 +0300

    Duplicate storage counter init to other

--- ee/app/assets/javascripts/pages/groups/usage_quotas/index.js
+++ ee/app/assets/javascripts/pages/groups/usage_quotas/index.js
@@ -1,3 +1,4 @@
+import otherStorageCounter from 'ee/other_storage_counter';
 import storageCounter from 'ee/storage_counter';
 import LinkedTabs from '~/lib/utils/bootstrap_linked_tabs';
 import initSearchSettings from '~/search_settings';
@@ -9,6 +10,17 @@ if (document.querySelector('#js-storage-counter-app')) {
   new LinkedTabs({
     defaultAction: '#pipelines-quota-tab',
     parentEl: '.js-storage-tabs',
+    hashedTabs: true,
+  });
+}
+
+if (document.querySelector('#js-other-storage-counter-app')) {
+  otherStorageCounter();
+
+  // eslint-disable-next-line no-new
+  new LinkedTabs({
+    defaultAction: '#pipelines-quota-tab',
+    parentEl: '.js-other-storage-tabs',
     hashedTabs: true,
   });
 }
