08264c828753e9068c2f2b9a640dc7e415149761
Fernando <farias@gitlab.com>
2021-03-24 18:07:14 -0500

    Add corpus management bundle

--- ee/app/assets/javascripts/security_configuration/corpus_management/corpus_management_bundle.js
+++ ee/app/assets/javascripts/security_configuration/corpus_management/corpus_management_bundle.js
@@ -1,5 +1,11 @@
+import { defaultDataIdFromObject } from 'apollo-cache-inmemory';
 import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import createDefaultClient from '~/lib/graphql';
 import CorpusManagement from './components/corpus_management.vue';
+import resolvers from './graphql/resolvers';
+
+Vue.use(VueApollo);
 
 export default () => {
   const el = document.querySelector('.js-corpus-management');
@@ -8,16 +14,26 @@ export default () => {
     return undefined;
   }
 
+  const defaultClient = createDefaultClient(resolvers, {
+    cacheConfig: {
+      dataIdFromObject: (object) => {
+        return object.id || defaultDataIdFromObject(object);
+      },
+    },
+  });
+
   const {
-    dataset: { projectFullPath },
+    dataset: { projectFullPath, corpusHelpPath },
   } = el;
 
   const props = {
     projectFullPath,
+    corpusHelpPath,
   };
 
   return new Vue({
     el,
+    apolloProvider: new VueApollo({ defaultClient }),
     render(h) {
       return h(CorpusManagement, {
         props,
