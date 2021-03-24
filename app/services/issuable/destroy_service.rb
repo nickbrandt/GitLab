9123434eccd160966eb2f3c30acdc08c58036a26
Patrick Bajao <ebajao@gitlab.com>
2021-03-24 19:53:13 +0800

    Delete all associated todos without N+1
    When deleting an issuable (Issue or MergeRequest), the associated
    Todo records need to be deleted as well.
    
    Before this change, it was calling Todo#destroy on each associated
    todo that results to N+1 issues.

--- app/services/issuable/destroy_service.rb
+++ app/services/issuable/destroy_service.rb
@@ -5,10 +5,27 @@ module Issuable
     def execute(issuable)
       TodoService.new.destroy_target(issuable) do |issuable|
         if issuable.destroy
+          delete_all_todos(issuable)
           issuable.update_project_counter_caches
           issuable.assignees.each(&:invalidate_cache_counts)
         end
       end
+    end
+
+    private
+
+    def delete_all_todos(issuable)
+      # To follow https://docs.gitlab.com/ee/development/foreign_keys.html#dependent-removals,
+      # we are deleting associated Todo records here instead of having it
+      # in `has_many :todos` definition.
+      #
+      # Need to call `delete_all` on `Todo` collection instead of `todos`
+      # association of issuable because the latter triggers an UPDATE
+      # query and results in a `PG::NotNullViolation` error.
+      Todo
+        .for_target(issuable.id)
+        .for_type(issuable.class.name)
+        .delete_all
     end
   end
 end
--- spec/services/issuable/destroy_service_spec.rb
+++ spec/services/issuable/destroy_service_spec.rb
@@ -9,6 +9,20 @@ RSpec.describe Issuable::DestroyService do
   subject(:service) { described_class.new(project, user) }
 
   describe '#execute' do
+    shared_examples_for 'service deleting todos' do
+      it 'destroys associated todos' do
+        create(:todo, target: issuable, user: user, author: user, project: project)
+
+        control_count = ActiveRecord::QueryRecorder.new { service.execute(issuable) }.count
+
+        create(:todo, target: other_issuable, user: user, author: user, project: project)
+        create(:todo, target: other_issuable, user: user, author: user, project: project)
+
+        expect { service.execute(other_issuable) }.not_to exceed_query_limit(control_count)
+        expect(other_issuable.todos.count).to eq(0)
+      end
+    end
+
     context 'when issuable is an issue' do
       let!(:issue) { create(:issue, project: project, author: user, assignees: [user]) }
 
@@ -32,6 +46,11 @@ RSpec.describe Issuable::DestroyService do
       it 'invalidates the issues count cache for the assignees' do
         expect_any_instance_of(User).to receive(:invalidate_cache_counts).once
         service.execute(issue)
+      end
+
+      it_behaves_like 'service deleting todos' do
+        let(:issuable) { issue }
+        let(:other_issuable) { create(:issue, project: project, author: user, assignees: [user]) }
       end
     end
 
@@ -58,6 +77,11 @@ RSpec.describe Issuable::DestroyService do
 
         expect { service.execute(merge_request) }
           .to change { user.todos_pending_count }.from(1).to(0)
+      end
+
+      it_behaves_like 'service deleting todos' do
+        let(:issuable) { merge_request }
+        let(:other_issuable) { create(:merge_request, target_project: project, source_project: project, author: user, assignees: [user]) }
       end
     end
   end
