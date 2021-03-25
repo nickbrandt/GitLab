657de9c8aa5627d5286fb24370d1b0fa66e5bc2c
Dylan Griffith <dyl.griffith@gmail.com>
2021-03-25 04:47:31 +0000

    Merge branch 'remove-direct-mimemagic-dependency' into 'master'
    Replace mimemagic dependency and introduce a Gitlab::Utils::MimeType class
    
    See merge request gitlab-org/gitlab!57387

--- lib/gitlab/utils/mime_type.rb
+++ lib/gitlab/utils/mime_type.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
+require 'magic'
 
 # This wraps calls to a gem which support mime type detection.
-# Currently uses `mimemagic`, but that will be replaced shortly.
+# We use the `ruby-magic` gem instead of `mimemagic` due to licensing issues
 module Gitlab
   module Utils
     class MimeType
@@ -9,13 +10,14 @@ module Gitlab
         def from_io(io)
           return unless io.is_a?(IO) || io.is_a?(StringIO)
 
-          MimeMagic.by_magic(io).try(:type)
+          mime_type = File.magic(io, Magic::MIME_TYPE)
+          mime_type == 'inode/x-empty' ? nil : mime_type
         end
 
         def from_string(string)
           return unless string.is_a?(String)
 
-          MimeMagic.by_magic(string)
+          string.type
         end
       end
     end
