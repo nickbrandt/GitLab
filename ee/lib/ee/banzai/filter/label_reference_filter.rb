a2e4911c830857d4af5986e9b9fd682bda490ce4
Mario Celi <mcelicalderon@gitlab.com>
2021-03-26 14:52:42 -0500

    Escape HTML on scoped labels tooltip

--- ee/lib/ee/banzai/filter/label_reference_filter.rb
+++ ee/lib/ee/banzai/filter/label_reference_filter.rb
@@ -10,11 +10,17 @@ module EE
         def data_attributes_for(text, parent, object, link_content: false, link_reference: false)
           return super unless object.scoped_label?
 
-          # Enabling HTML tooltips for scoped labels here but we do not need to do any additional
-          # escaping because the label's tooltips are already stripped of dangerous HTML
+          # Enabling HTML tooltips for scoped labels here and additional escaping is done in `object_link_title`
           super.merge!(
             html: true
           )
+        end
+
+        override :object_link_title
+        def object_link_title(object, matches)
+          return super unless object.scoped_label?
+
+          ERB::Util.html_escape(super)
         end
       end
     end
