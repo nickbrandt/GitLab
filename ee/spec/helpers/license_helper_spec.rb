a94aa40d30ab576af6f6009f1cca3cbe3d604fdf
Angelo Gulina <agulina@gitlab.com>
2021-03-26 08:22:07 +0000

    Add licenses helper

--- ee/spec/helpers/license_helper_spec.rb
+++ ee/spec/helpers/license_helper_spec.rb
@@ -78,4 +78,24 @@ RSpec.describe LicenseHelper do
       end
     end
   end
+
+  describe '#cloud_license_view_data' do
+    context 'when there is a current license' do
+      it 'returns the data for the view' do
+        custom_plan = 'custom plan'
+        license = double('License', plan: custom_plan)
+        allow(License).to receive(:current).and_return(license)
+
+        expect(cloud_license_view_data).to eq({ current_plan_title: 'Custom Plan' })
+      end
+    end
+
+    context 'when there is no current license' do
+      it 'returns the data for the view' do
+        allow(License).to receive(:current).and_return(nil)
+
+        expect(cloud_license_view_data).to eq({ current_plan_title: 'Core' })
+      end
+    end
+  end
 end
