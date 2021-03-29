a785cfa865d2e4ca98a455a0110f713e5419f807
Etienne Baqué <ebaque@gitlab.com>
2021-03-29 14:58:10 +0300

    Updated condition to raise DiskAccessDenied
    Updated pages_path in Project class as well.
    Updated tests accordingly.

--- spec/lib/gitlab/pages/settings_spec.rb
+++ spec/lib/gitlab/pages/settings_spec.rb
@@ -16,8 +16,34 @@ RSpec.describe Gitlab::Pages::Settings do
         allow(::Gitlab::Runtime).to receive(:web_server?).and_return(true)
       end
 
-      it 'raises a DiskAccessDenied exception' do
-        expect { subject }.to raise_error(described_class::DiskAccessDenied)
+      it 'logs a DiskAccessDenied error' do
+        expect(Gitlab::ErrorTracking).to receive(:track_exception).with(
+          instance_of(described_class::DiskAccessDenied)
+        )
+
+        subject
+      end
+    end
+
+    context 'when local_store settings does not exist yet' do
+      before do
+        allow(Settings.pages).to receive(:local_store).and_return(nil)
+      end
+
+      it { is_expected.to eq('the path') }
+    end
+
+    context 'when local store exists but legacy storage is disabled' do
+      before do
+        allow(Settings.pages.local_store).to receive(:enabled).and_return(false)
+      end
+
+      it 'logs a DiskAccessDenied error' do
+        expect(Gitlab::ErrorTracking).to receive(:track_exception).with(
+          instance_of(described_class::DiskAccessDenied)
+        )
+
+        subject
       end
     end
   end
