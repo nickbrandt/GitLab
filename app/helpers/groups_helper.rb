d5f2b32a51c8e26ac454e8beb0a335708d607556
Markus Koller <mkoller@gitlab.com>
2021-03-29 19:28:21 +0000

    Avoid N+1 queries in breadcrumbs

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -120,7 +120,9 @@ module GroupsHelper
     @has_group_title = true
     full_title = []
 
-    group.ancestors.reverse.each_with_index do |parent, index|
+    ancestors = group.ancestors.with_route
+
+    ancestors.reverse_each.with_index do |parent, index|
       if index > 0
         add_to_breadcrumb_dropdown(group_title_link(parent, hidable: false, show_avatar: true, for_dropdown: true), location: :before)
       else
