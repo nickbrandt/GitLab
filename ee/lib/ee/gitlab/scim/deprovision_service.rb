71e99cfa52f71c430715333f77046e2dc08fc776
Serena Fang <sfang@gitlab.com>
2021-03-31 03:12:46 +0000

    Return errors if deprovision unsuccessful
    Return errors before updating active false
    
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/57178
    
    https://gitlab.com/gitlab-org/gitlab/-/issues/321883
    https://gitlab.com/gitlab-org/gitlab/-/issues/325729

--- ee/lib/ee/gitlab/scim/deprovision_service.rb
+++ ee/lib/ee/gitlab/scim/deprovision_service.rb
@@ -13,23 +13,29 @@ module EE
         end
 
         def execute
+          return error(_("Could not remove %{user} from %{group}. User is not a group member.") % { user: user.name, group: group.name }) unless group_membership
+          return error(_("Could not remove %{user} from %{group}. Cannot remove last group owner.") % { user: user.name, group: group.name }) if group.last_owner?(user)
+
           ScimIdentity.transaction do
             identity.update!(active: false)
             remove_group_access
           end
+
+          ServiceResponse.success(message: _("User %{user} was removed from %{group}.") % { user: user.name, group: group.name })
         end
 
         private
 
         def remove_group_access
-          return unless group_membership
-          return if group.last_owner?(user)
-
           ::Members::DestroyService.new(user).execute(group_membership)
         end
 
         def group_membership
-          @group_membership ||= group.group_member(user)
+          @group_membership ||= group.all_group_members.with_user(user).first
+        end
+
+        def error(message)
+          ServiceResponse.error(message: message)
         end
       end
     end
--- ee/spec/lib/ee/gitlab/scim/deprovision_service_spec.rb
+++ ee/spec/lib/ee/gitlab/scim/deprovision_service_spec.rb
@@ -4,36 +4,89 @@ require 'spec_helper'
 
 RSpec.describe ::EE::Gitlab::Scim::DeprovisionService do
   describe '#execute' do
-    let_it_be(:identity) { create(:scim_identity, active: true) }
-    let_it_be(:group) { identity.group }
-    let_it_be(:user) { identity.user }
+    let(:identity) { create(:scim_identity, active: true) }
+    let(:group) { identity.group }
+    let(:user) { identity.user }
 
     let(:service) { described_class.new(identity) }
 
-    it 'deactivates scim identity' do
-      service.execute
+    context 'when user is successfully removed' do
+      before do
+        create(:group_member, group: group, user: user, access_level: GroupMember::REPORTER)
+      end
 
-      expect(identity.active).to be false
+      it 'deactivates scim identity' do
+        expect { service.execute }.to change { identity.active }.from(true).to(false)
+      end
+
+      it 'removes group access' do
+        service.execute
+
+        expect(group.all_group_members.pluck(:user_id)).not_to include(user.id)
+      end
+
+      it 'returns the successful deprovision message' do
+        response = service.execute
+
+        expect(response.message).to include("User #{user.name} was removed from #{group.name}.")
+      end
     end
 
-    it 'removes group access' do
-      create(:group_member, group: group, user: user, access_level: GroupMember::REPORTER)
+    context 'with minimal access role' do
+      before do
+        stub_licensed_features(minimal_access_role: true)
+        create(:group_member, group: group, user: user, access_level: ::Gitlab::Access::MINIMAL_ACCESS)
+      end
 
-      service.execute
+      it 'deactivates scim identity' do
+        expect { service.execute }.to change { identity.active }.from(true).to(false)
+      end
 
-      expect(group.members.pluck(:user_id)).not_to include(user.id)
+      it 'removes group access' do
+        service.execute
+
+        expect(group.all_group_members.pluck(:user_id)).not_to include(user.id)
+      end
+
+      it 'returns the successful deprovision message' do
+        response = service.execute
+
+        expect(response.message).to include("User #{user.name} was removed from #{group.name}.")
+      end
     end
 
-    it 'does not remove the last owner' do
-      create(:group_member, group: group, user: user, access_level: GroupMember::OWNER)
+    context 'when user is not successfully removed' do
+      context 'when user is the last owner' do
+        before do
+          create(:group_member, group: group, user: user, access_level: GroupMember::OWNER)
+        end
 
-      service.execute
+        it 'does not remove the last owner' do
+          service.execute
 
-      expect(identity.group.members.pluck(:user_id)).to include(user.id)
-    end
+          expect(identity.group.members.pluck(:user_id)).to include(user.id)
+        end
 
-    it 'does not change group membership when the user is not a member' do
-      expect { service.execute }.not_to change { group.members.count }
+        it 'returns the last group owner error' do
+          response = service.execute
+
+          expect(response.error?).to be true
+          expect(response.errors).to include("Could not remove #{user.name} from #{group.name}. Cannot remove last group owner.")
+        end
+      end
+
+      context 'when user is not a group member' do
+        it 'does not change group membership when the user is not a member' do
+          expect { service.execute }.not_to change { group.members.count }
+        end
+
+        it 'returns the group membership error' do
+          response = service.execute
+
+          expect(response.error?).to be true
+          expect(response.errors).to include("Could not remove #{user.name} from #{group.name}. User is not a group member.")
+        end
+      end
     end
   end
 end
