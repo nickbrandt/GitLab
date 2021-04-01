879d1d26e35435f5bce9e1ffd31c73e90d461b0b
pburdette <pburdette@gitlab.com>
2021-04-01 09:02:25 -0400

    Add authorization test
    Test for auth on the project
    jobs resolver.

--- spec/graphql/resolvers/project_jobs_resolver_spec.rb
+++ spec/graphql/resolvers/project_jobs_resolver_spec.rb
@@ -5,29 +5,41 @@ require 'spec_helper'
 RSpec.describe Resolvers::ProjectJobsResolver do
   include GraphqlHelpers
 
-  let_it_be(:project) { create(:project, :repository, :public) }
-  let_it_be(:irrelevant_project) { create(:project, :repository, :public) }
+  let_it_be(:project) { create(:project, :repository) }
+  let_it_be(:irrelevant_project) { create(:project, :repository) }
   let_it_be(:pipeline) { create(:ci_pipeline, project: project) }
   let_it_be(:irrelevant_pipeline) { create(:ci_pipeline, project: irrelevant_project) }
+  let_it_be(:build_one) { create(:ci_build, :success, name: 'Build One', pipeline: pipeline) }
+  let_it_be(:build_two) { create(:ci_build, :success, name: 'Build Two', pipeline: pipeline) }
+  let_it_be(:build_three) { create(:ci_build, :failed, name: 'Build Three', pipeline: pipeline) }
 
-  let(:current_user) { create(:user) }
-  let(:build_one) { create(:ci_build, :success, name: 'Build One', pipeline: pipeline) }
-  let(:build_two) { create(:ci_build, :success, name: 'Build Two', pipeline: pipeline) }
-  let(:build_three) { create(:ci_build, :failed, name: 'Build Three', pipeline: pipeline) }
   let(:irrelevant_build) { create(:ci_build, name: 'Irrelevant Build', pipeline: irrelevant_pipeline)}
   let(:args) { {} }
+  let(:current_user) { create(:user) }
 
   subject { resolve_jobs(args) }
 
   describe '#resolve' do
-    context 'with statuses argument' do
-      let(:args) { { statuses: [Types::Ci::JobStatusEnum.coerce_isolated_input('SUCCESS')] } }
+    context 'with authorized user' do
+      before do
+        project.add_developer(current_user)
+      end
 
-      it { is_expected.to contain_exactly(build_one, build_two) }
+      context 'with statuses argument' do
+        let(:args) { { statuses: [Types::Ci::JobStatusEnum.coerce_isolated_input('SUCCESS')] } }
+
+        it { is_expected.to contain_exactly(build_one, build_two) }
+      end
+
+      context 'without statuses argument' do
+        it { is_expected.to contain_exactly(build_one, build_two, build_three) }
+      end
     end
 
-    context 'without statuses argument' do
-      it { is_expected.to contain_exactly(build_one, build_two, build_three) }
+    context 'with unauthorized user' do
+      let(:current_user) { nil }
+
+      it { is_expected.to be_nil }
     end
   end
 
