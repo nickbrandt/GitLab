7928fb956d445e33cf2cd08aa734543a7b9aa37d
Dmitry Gruzd <dgruzd@gitlab.com>
2021-04-01 16:59:57 +0300

    Add data consistency for Elastic::BulkCronWorker

--- ee/app/workers/elastic_index_bulk_cron_worker.rb
+++ ee/app/workers/elastic_index_bulk_cron_worker.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-class ElasticIndexBulkCronWorker
+class ElasticIndexBulkCronWorker # rubocop:disable Scalability/IdempotentWorker
   include Elastic::BulkCronWorker
 
   feature_category :global_search
-  idempotent!
   urgency :throttled
+  # Even though this worker is idempotent, until https://gitlab.com/gitlab-org/gitlab/-/issues/325291 is done
+  # we can't use it with read-only database replicas
+  data_consistency :delayed, feature_flag: :load_balancing_for_bulk_cron_workers
 
   private
 
