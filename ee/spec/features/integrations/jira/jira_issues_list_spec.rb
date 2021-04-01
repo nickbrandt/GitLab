e23afd33dfd0707c4ecb255904e9e49ad97fc052
Justin Ho <hduong@gitlab.com>
2021-04-01 14:25:20 +0700

    Remove feature flag "jira_issues_list"
    Backport of changes made in this MR
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/56465

--- ee/spec/features/integrations/jira/jira_issues_list_spec.rb
+++ ee/spec/features/integrations/jira/jira_issues_list_spec.rb
@@ -5,13 +5,14 @@ require 'spec_helper'
 RSpec.describe 'Jira issues list' do
   let_it_be(:project, refind: true) { create(:project) }
   let_it_be(:jira_integration) { create(:jira_service, project: project, issues_enabled: true, project_key: 'GL') }
+
   let(:user) { create(:user) }
 
   before do
     stub_licensed_features(jira_issues_integration: true)
-    stub_feature_flags(jira_issues_list: false)
     project.add_user(user, :developer)
     sign_in(user)
+    stub_request(:get, /.*jira.example.com.*/)
   end
 
   context 'when jira_issues_integration licensed feature is not available' do
@@ -27,7 +28,7 @@ RSpec.describe 'Jira issues list' do
     end
   end
 
-  it 'renders "Create new issue" button' do
+  it 'renders "Create new issue" button', :js do
     visit project_integrations_jira_issues_path(project)
 
     expect(page).to have_link('Create new issue in Jira')
