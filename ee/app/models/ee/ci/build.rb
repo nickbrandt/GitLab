32898ed7b94340f1bebcfe066e045cefd0921f3d
alinamihaila <amihaila@gitlab.com>
2021-04-02 12:58:09 +0300

    Remove extra feature flag check
      - Remove feature flag check from ee ci::build
      model
      - This is checked in Gitlab::UsageDataCounters::HLLRedisCounter

--- ee/app/models/ee/ci/build.rb
+++ ee/app/models/ee/ci/build.rb
@@ -184,7 +184,6 @@ module EE
       end
 
       def track_ci_secrets_management_usage
-        return unless ::Feature.enabled?(:usage_data_i_ci_secrets_management_vault_build_created, default_enabled: true)
         return unless ci_secrets_management_available? && secrets?
 
         ::Gitlab::UsageDataCounters::HLLRedisCounter.track_event('i_ci_secrets_management_vault_build_created', values: user_id)
