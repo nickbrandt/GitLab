edaf424a61301707162e68b612d29c3353b2e5cd
Jarka Košanová <jarka@gitlab.com>
2021-04-07 23:01:56 +0000

    Track epic labels change action on usage ping
    https://gitlab.com/gitlab-org/gitlab/-/issues/292253

--- ee/app/services/ee/resource_events/change_labels_service.rb
+++ ee/app/services/ee/resource_events/change_labels_service.rb
@@ -6,6 +6,13 @@ module EE
       extend ActiveSupport::Concern
       extend ::Gitlab::Utils::Override
 
+      override :execute
+      def execute(added_labels: [], removed_labels: [])
+        super
+
+        ::Gitlab::UsageDataCounters::EpicActivityUniqueCounter.track_epic_labels_changed_action(author: user) if resource.is_a?(Epic)
+      end
+
       override :resource_column
       def resource_column(resource)
         resource.is_a?(Epic) ? :epic_id : super
