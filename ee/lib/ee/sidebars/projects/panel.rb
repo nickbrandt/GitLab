745ef5ce9f25168758add6d308963df9d4e41531
Francisco Javier López <fjlopez@gitlab.com>
2021-04-07 12:33:42 +0200

    Add External Issue Tracker, Jira, and Labels menus
    This commits move the External Issue Tracker, Jira,
    and Labels menus from the HAML view to the new refactor.

---
+++ ee/lib/ee/sidebars/projects/panel.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module EE
+  module Sidebars
+    module Projects
+      module Panel
+        extend ::Gitlab::Utils::Override
+
+        override :configure_menus
+        def configure_menus
+          super
+
+          if jira_menu.render?
+            replace_menu(::Sidebars::Projects::Menus::ExternalIssueTrackerMenu, jira_menu)
+          end
+        end
+
+        private
+
+        def jira_menu
+          @jira_menu ||= ::Sidebars::Projects::Menus::JiraMenu.new(context)
+        end
+      end
+    end
+  end
+end
