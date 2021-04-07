b0a70c812133e6d43d91bf859e0f12b4069eb2fe
jejacks0n <jjackson@gitlab.com>
2021-04-07 13:52:33 -0600

    Feature test to ensure experiments publish
    - Experiments, when run during the request cycle are pushed into the
    client using Gon. This adds a test to ensure that this continues to
    work.

--- spec/features/admin/admin_abuse_reports_spec.rb
+++ spec/features/admin/admin_abuse_reports_spec.rb
@@ -15,8 +15,42 @@ RSpec.describe "Admin::AbuseReports", :js do
     describe 'if a user has been reported for abuse' do
       let!(:abuse_report) { create(:abuse_report, user: user) }
 
+      describe 'with experimentation tracking' do
+        # This is part of a set of tests that ensure that tracking remains
+        # consistent at the front end layer. Since we don't want to actually
+        # introduce an experiment in real code, we're going to simulate it
+        # here.
+        before do
+          stub_experiments(null_hypothesis: :candidate)
+        end
+
+        it 'publishes the experiments that have been run to the client', :experiment do
+          allow_next_instance_of(Admin::AbuseReportsController) do |instance|
+            allow(instance).to receive(:index).and_wrap_original do |original|
+              instance.experiment(:null_hypothesis, user: instance.current_user) do |e|
+                e.use { original.call }
+                e.try { original.call }
+              end
+            end
+          end
+
+          visit admin_abuse_reports_path
+
+          expect(page).to have_content('Abuse Reports')
+
+          published_experiments = page.evaluate_script('window.gon.experiment')
+          expect(published_experiments).to eq(
+            null_hypothesis: {
+              experiment: 'null_hypothesis',
+              key: 'c934a2885104c464e13bfae61b7d9dc2',
+              variant: 'candidate'
+            }.as_json
+          )
+        end
+      end
+
       describe 'in the abuse report view' do
-        it 'presents information about abuse report' do
+        it 'presents information about abuse report', :experiment do
           visit admin_abuse_reports_path
 
           expect(page).to have_content('Abuse Reports')
