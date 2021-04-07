41e9595688f1531a36105f1a37d8a475e67b82ef
Terri Chu <tchu@gitlab.com>
2021-04-07 14:06:17 +0000

    Reduce queries on group search API for blobs scope [RUN ALL RSPEC] [RUN AS-IF-FOSS]

--- ee/spec/requests/api/search_spec.rb
+++ ee/spec/requests/api/search_spec.rb
@@ -134,26 +134,51 @@ RSpec.describe API::Search, factory_default: :keep do
         it 'avoids N+1 queries' do
           control = ActiveRecord::QueryRecorder.new { get api(endpoint, user), params: { scope: 'commits', search: 'folder' } }
 
-          project_2 = create(:project, :public, :repository, :wiki_repo, name: 'awesome project 2')
-          project_3 = create(:project, :public, :repository, :wiki_repo, name: 'awesome project 3')
+          project_2 = create(:project, :public, :repository, :wiki_repo, group: group, name: 'awesome project 2')
           project_2.repository.index_commits_and_blobs
-          project_3.repository.index_commits_and_blobs
+          3.times do |i|
+            commit_sha = project.repository.create_file(user, "#{i}", "folder #{i}", message: "committing folder #{i}", branch_name: 'master')
+            project.repository.commit(commit_sha)
+          end
+          project.repository.index_commits_and_blobs
 
           ensure_elasticsearch_index!
 
-          # Some N+1 queries still exist
-          expect { get api(endpoint, user), params: { scope: 'commits', search: 'folder' } }.not_to exceed_query_limit(control).with_threshold(9)
+          # N+1 queries still exist (ci_pipelines)
+          expect { get api(endpoint, user), params: { scope: 'commits', search: 'folder' } }.not_to exceed_query_limit(control).with_threshold(5)
+          # support global, group, and project search results expected counts
+          expected_count = level == :project ? 5 : 7
+          expect(json_response.count).to be expected_count
         end
       end
 
       context 'for blobs scope' do
         it_behaves_like 'response is correct', schema: 'public_api/v4/blobs' do
           before do
-            get api(endpoint, user), params: { scope: 'blobs', search: 'monitors' }
+            get api(endpoint, user), params: { scope: 'blobs', search: 'folder' }
           end
         end
 
         it_behaves_like 'pagination', scope: 'blobs'
+
+        it 'avoids N+1 queries' do
+          control = ActiveRecord::QueryRecorder.new { get api(endpoint, user), params: { scope: 'blobs', search: 'Issue team' } }
+
+          project_2 = create(:project, :public, :repository, :wiki_repo, group: group, name: 'awesome project 2')
+          project_2.repository.index_commits_and_blobs
+          3.times do |i|
+            commit_sha = project.repository.create_file(user, "#{i}", "Issue team #{i}", message: "#{i}", branch_name: 'master')
+            project.repository.commit(commit_sha)
+          end
+
+          project.repository.index_commits_and_blobs
+          ensure_elasticsearch_index!
+
+          expect { get api(endpoint, user), params: { scope: 'blobs', search: 'Issue team' } }.not_to exceed_query_limit(control)
+          # support global, group, and project search results expected counts
+          expected_count = level == :project ? 6 : 9
+          expect(json_response.count).to be expected_count
+        end
 
         context 'filters' do
           def results_filenames
