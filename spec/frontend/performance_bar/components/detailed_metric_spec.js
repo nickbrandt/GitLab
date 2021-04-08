b5357b2b2e97ce36665ce93c6a7bee51ac2f0f94
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2021-04-08 14:22:37 +0700

    Refactor frontend tests

--- spec/frontend/performance_bar/components/detailed_metric_spec.js
+++ spec/frontend/performance_bar/components/detailed_metric_spec.js
@@ -120,80 +120,70 @@ describe('detailedMetric', () => {
       });
     });
 
-    describe('when the details have summaryOptions > hideTotal option', () => {
-      beforeEach(() => {
-        createComponent({
-          currentRequest: {
-            details: {
-              gitaly: {
-                duration: '123ms',
-                calls: 456,
-                details: requestDetails,
-                warnings: ['gitaly calls: 456 over 30'],
-                summaryOptions: {
-                  hideTotal: true,
-                },
+    describe('when the details have summaryOptions option', () => {
+      const gitalyDetails = {
+        duration: '123ms',
+        calls: 456,
+        details: requestDetails,
+        warnings: ['gitaly calls: 456 over 30'],
+      };
+
+      describe('when the details have summaryOptions > hideTotal option', () => {
+        beforeEach(() => {
+          createComponent({
+            currentRequest: {
+              details: {
+                gitaly: { ...gitalyDetails, summaryOptions: { hideTotal: true } },
               },
             },
-          },
+          });
+        });
+
+        it('displays a summary section', () => {
+          expect(findAllSummaryItems()).toEqual(['Total duration 123ms']);
         });
       });
 
-      it('displays a summary section', () => {
-        expect(findAllSummaryItems()).toEqual(['Total duration 123ms']);
-      });
-    });
-
-    describe('when the details have summaryOptions > hideDuration option', () => {
-      beforeEach(() => {
-        createComponent({
-          currentRequest: {
-            details: {
-              gitaly: {
-                duration: '123ms',
-                calls: 456,
-                details: requestDetails,
-                warnings: ['gitaly calls: 456 over 30'],
-                summaryOptions: {
-                  hideDuration: true,
-                },
+      describe('when the details have summaryOptions > hideDuration option', () => {
+        beforeEach(() => {
+          createComponent({
+            currentRequest: {
+              details: {
+                gitaly: { ...gitalyDetails, summaryOptions: { hideDuration: true } },
               },
             },
-          },
+          });
+        });
+
+        it('displays a summary section', () => {
+          expect(findAllSummaryItems()).toEqual(['Total 456']);
         });
       });
 
-      it('displays a summary section', () => {
-        expect(findAllSummaryItems()).toEqual(['Total 456']);
-      });
-    });
-
-    describe('when the details have both summary and summaryOptions field', () => {
-      beforeEach(() => {
-        createComponent({
-          currentRequest: {
-            details: {
-              gitaly: {
-                duration: '123ms',
-                calls: 456,
-                details: requestDetails,
-                warnings: ['gitaly calls: 456 over 30'],
-                summary: {
-                  'In controllers': 100,
-                  'In middlewares': 20,
-                },
-                summaryOptions: {
-                  hideDuration: true,
-                  hideTotal: true,
+      describe('when the details have both summary and summaryOptions field', () => {
+        beforeEach(() => {
+          createComponent({
+            currentRequest: {
+              details: {
+                gitaly: {
+                  ...gitalyDetails,
+                  summary: {
+                    'In controllers': 100,
+                    'In middlewares': 20,
+                  },
+                  summaryOptions: {
+                    hideDuration: true,
+                    hideTotal: true,
+                  },
                 },
               },
             },
-          },
+          });
         });
-      });
 
-      it('displays a summary section', () => {
-        expect(findAllSummaryItems()).toEqual(['In controllers 100', 'In middlewares 20']);
+        it('displays a summary section', () => {
+          expect(findAllSummaryItems()).toEqual(['In controllers 100', 'In middlewares 20']);
+        });
       });
     });
 
