a298cf3736af823aa1acfa33e12b685b1aa22871
Fernando <farias@gitlab.com>
2021-04-08 17:50:19 -0500

    Corpus upload wrapper

--- ee/app/assets/javascripts/security_configuration/corpus_management/components/corpus_upload.vue
+++ ee/app/assets/javascripts/security_configuration/corpus_management/components/corpus_upload.vue
@@ -1,33 +1,72 @@
 <script>
-import { GlButton, GlSprintf } from '@gitlab/ui';
+import { GlButton, GlModal, GlModalDirective } from '@gitlab/ui';
+import CorpusUploadModal from 'ee/security_configuration/corpus_management/components/corpus_upload_modal.vue';
 import { numberToHumanSize } from '~/lib/utils/number_utils';
 import { s__ } from '~/locale';
+import addCorpusMutation from '../graphql/mutations/add_corpus.mutation.graphql';
+import getCorpusesQuery from '../graphql/queries/get_corpuses.query.graphql';
 
 export default {
   components: {
     GlButton,
-    GlSprintf,
+    GlModal,
+    CorpusUploadModal,
   },
+  directives: {
+    GlModalDirective,
+  },
+  inject: ['projectFullPath','corpusHelpPath'],
+  apollo: {
+    states: {
+      query: getCorpusesQuery,
+      variables() {
+        return {
+          projectPath: this.projectFullPath,
+          ...this.cursor,
+        };
+      },
+      update: (data) => {
+        return data;
+      },
+      error() {
+        this.states = null;
+      },
+    },
+  },   
+  modal: {
+    actionCancel: {
+      text: s__('Cancel'),
+    },  
+  },  
   props: {
     totalSize: {
       type: Number,
       required: true,
     },
   },
-  i18n: {
-    totalSize: s__('CorpusManagement|Total Size: %{totalSize}'),
-    newCorpus: s__('CorpusManagement|New corpus'),
+  methods: {
+    addCorpus() {
+      this.$apollo.mutate({
+        mutation: addCorpusMutation,
+        variables: { name: "New Upload", projectPath: this.projectFullPath },
+      })
+    },
   },
   computed: {
     formattedFileSize() {
       return numberToHumanSize(this.totalSize);
     },
+    isUploaded() {
+      return this.states?.uploadState.progress === 100;
+    },  
+    actionPrimaryProps() {
+      return {
+        text: s__('Add'),
+        attributes: { 'data-testid': 'modal-confirm', disabled: !this.isUploaded},
+      }  
+    } 
   },
-  methods: {
-    newCorpus() {
-      this.$emit('newcorpus');
-    },
-  },
+
 };
 </script>
 <template>
@@ -35,15 +74,25 @@ export default {
     class="gl-h-11 gl-bg-gray-10 gl-display-flex gl-justify-content-space-between gl-align-items-center"
   >
     <div class="gl-ml-5">
-      <gl-sprintf :message="$options.i18n.totalSize">
-        <template #totalSize>
-          <span class="gl-font-weight-bold">{{ formattedFileSize }}</span>
-        </template>
-      </gl-sprintf>
+      {{ s__('CorpusManagement|Total Size:') }}
+      <span class="gl-font-weight-bold">{{ formattedFileSize }}</span>
     </div>
 
-    <gl-button class="gl-mr-5" category="primary" variant="confirm" @click="newCorpus">
-      {{ this.$options.i18n.newCorpus }}
-    </gl-button>
+    <gl-button class="gl-mr-5" variant="success" v-gl-modal-directive="`corpus-upload-modal`">
+      {{ s__('CorpusManagement|New corpus') }}
+    </gl-button>  
+
+    <gl-modal
+      modal-id="corpus-upload-modal"
+      title="New corpus"
+      size="sm"
+      @primary="addCorpus"
+      :action-primary="actionPrimaryProps"
+      :action-cancel="$options.modal.actionCancel"      
+    >
+      <corpus-upload-modal 
+      />
+    </gl-modal> 
+
   </div>
 </template>
