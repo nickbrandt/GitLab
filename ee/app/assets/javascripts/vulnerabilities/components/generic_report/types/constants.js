33051a378f8d721b5fdf01161885b2596b02fdb5
David Pisek <dpisek@gitlab.com>
2021-04-08 13:21:00 +0000

    Add recursive list component to generic reports section on vulnerability details page

--- ee/app/assets/javascripts/vulnerabilities/components/generic_report/types/constants.js
+++ ee/app/assets/javascripts/vulnerabilities/components/generic_report/types/constants.js
@@ -1,3 +1,4 @@
+export const REPORT_TYPE_LIST = 'list';
 export const REPORT_TYPE_URL = 'url';
 
-export const REPORT_TYPES = [REPORT_TYPE_URL];
+export const REPORT_TYPES = [REPORT_TYPE_LIST, REPORT_TYPE_URL];
