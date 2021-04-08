c8a031af41fd653c97ebf7c2aa4407713470dd82
Mayra Cabrera <mcabrera@gitlab.com>
2021-04-08 18:00:12 -0500

    Moves method to ci/pipeline_editor_helper
    Also make sure original version is returned on EE unless the feature is
    available

---
+++ ee/spec/helpers/ee/ci/pipeline_editor_helper_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe EE::Ci::PipelineEditorHelper do
+  let_it_be(:project) { create(:project, :public, :repository) }
+
+  describe '#js_pipeline_editor_data' do
+    before do
+      stub_licensed_features(coverage_fuzzing: true)
+
+      allow(helper)
+        .to receive(:namespace_project_new_merge_request_path)
+        .and_return('/mock/project/-/merge_requests/new')
+
+      allow(helper)
+        .to receive(:image_path)
+        .and_return('foo')
+    end
+
+    subject(:pipeline_editor_data) { helper.js_pipeline_editor_data(project) }
+
+    it 'returns ee specific values' do
+      expect(pipeline_editor_data.keys).to include('api-fuzzing-configuration-path')
+    end
+  end
+end
