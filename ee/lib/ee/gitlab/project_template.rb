41ece763c74b11989f091b1d15df9a35416a7941
Austin Regnery <aregnery@gitlab.com>
2021-04-09 16:24:38 +0000

    Update HIPAA logo for project templates

--- ee/lib/ee/gitlab/project_template.rb
+++ ee/lib/ee/gitlab/project_template.rb
@@ -10,7 +10,7 @@ module EE
 
         def localized_ee_templates_table
           [
-            ::Gitlab::ProjectTemplate.new('hipaa_audit_protocol', 'HIPAA Audit Protocol', _('A project containing issues for each audit inquiry in the HIPAA Audit Protocol published by the U.S. Department of Health & Human Services'), 'https://gitlab.com/gitlab-org/project-templates/hipaa-audit-protocol')
+            ::Gitlab::ProjectTemplate.new('hipaa_audit_protocol', 'HIPAA Audit Protocol', _('A project containing issues for each audit inquiry in the HIPAA Audit Protocol published by the U.S. Department of Health & Human Services'), 'https://gitlab.com/gitlab-org/project-templates/hipaa-audit-protocol', 'illustrations/logos/asklepian.svg')
           ].freeze
         end
 
