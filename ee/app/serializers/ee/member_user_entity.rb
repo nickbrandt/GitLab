511aef55e4abef910e99d66b7df1e48ed121de1e
Sean Arnold <sarnold@gitlab.com>
2021-04-12 12:04:56 +1200

    Return unique schedules for member user
    Move changleog

--- ee/app/serializers/ee/member_user_entity.rb
+++ ee/app/serializers/ee/member_user_entity.rb
@@ -8,6 +8,10 @@ module EE
       unexpose :gitlab_employee
       unexpose :email
       expose :oncall_schedules, with: ::IncidentManagement::OncallScheduleEntity
+
+      def oncall_schedules
+        object.oncall_schedules.uniq
+      end
     end
   end
 end
