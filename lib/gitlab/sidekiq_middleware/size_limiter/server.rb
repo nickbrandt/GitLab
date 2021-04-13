33ee1aaa58a5b05eb3a624c95ae68067b6ed8015
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2021-04-13 18:16:08 +0700

    Implement size limiter server middleware to restore the offloaded payload

---
+++ lib/gitlab/sidekiq_middleware/size_limiter/server.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module SidekiqMiddleware
+    module SizeLimiter
+      class Server
+        def call(worker, job, queue)
+          # Note: offloaded should be handled from the server side regardless of the track mode
+          # Note 2: The logger is independent from the middleware stack. Hence, offloaded jobs will have `args = null`. Is that acceptable?
+          # Handle action mailer
+          job_offloaded = job.delete('offloaded')
+          if job_offloaded
+            uploader = BackgroundJobPayloadUploader.new(
+              jid: job['jid'],
+              class: worker.class.name
+            )
+            # What if the uploader fails to load the job?
+            job['args'] = Sidekiq.load_json(uploader.load!)
+          end
+
+          job_succeeded = false
+          # Add some metrics right here to track the offloading activities
+          yield
+          job_succeeded = true
+        ensure
+          # What if removal fails?
+          # What if the job is sent to the death queue?
+          uploader.remove! if job_succeeded && job_offloaded
+        end
+      end
+    end
+  end
+end
