3a1623c039d7d1387fce4a4a5d7556b841cb92ec
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2021-04-13 17:13:12 +0700

    Implement an uploader to offload the job payloads

---
+++ app/uploaders/background_job_payload_uploader.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+class BackgroundJobPayloadUploader < GitlabUploader
+  include ObjectStorage::Concern
+
+  storage_options Gitlab.config.background_job_payloads
+
+  alias_method :upload, :model
+
+  def filename
+    "#{model['jid']}-payload"
+  end
+
+  def store_dir
+    dynamic_segment
+  end
+
+  def move_to_store
+    false
+  end
+
+  def move_to_cache
+    false
+  end
+
+  def save!
+    tmp = Tempfile.new(filename)
+    File.open(tmp.path, "w") { |f| f.write model['args'].to_json }
+    file = {
+      tempfile:     tmp,
+      filename:     filename,
+      content_type: 'application/json'
+    }
+    store!(file)
+
+    true
+  end
+
+  def load!
+    retrieve_from_store!(filename)
+    file.read
+  end
+
+  def object_store
+    ObjectStorage::Store::REMOTE
+  end
+
+  private
+
+  def dynamic_segment
+    model['class']
+  end
+end
