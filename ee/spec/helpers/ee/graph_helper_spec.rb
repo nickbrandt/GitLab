0b3d3fbfc55b0fc906138dcfa0deccc682fe768c
Nathan Friend <nathan@gitlab.com>
2021-04-13 14:55:55 -0400

    Deduplicate DORA chart visiblity flags
    This commit merges two flags used for controlling the visibility of DORA
    charts - `should_render_deployment_frequency_charts` and
    `should_render_lead_time_charts` - into one:
    `should_render_dora_charts`.

--- ee/spec/helpers/ee/graph_helper_spec.rb
+++ ee/spec/helpers/ee/graph_helper_spec.rb
@@ -15,37 +15,13 @@ RSpec.describe EE::GraphHelper do
     allow(self).to receive(:can?).with(current_user, :read_dora4_analytics, project).and_return(is_user_authorized)
   end
 
-  describe '#should_render_deployment_frequency_charts' do
+  describe '#should_render_dora_charts' do
     shared_examples 'returns true' do
-      it { expect(should_render_deployment_frequency_charts).to be(true) }
+      it { expect(should_render_dora_charts).to be(true) }
     end
 
     shared_examples 'returns false' do
-      it { expect(should_render_deployment_frequency_charts).to be(false) }
-    end
-
-    it_behaves_like 'returns true'
-
-    context 'when the feature is not available' do
-      let(:is_feature_licensed) { false }
-
-      it_behaves_like 'returns false'
-    end
-
-    context 'when the user does not have permission' do
-      let(:is_user_authorized) { false }
-
-      it_behaves_like 'returns false'
-    end
-  end
-
-  describe '#should_render_lead_time_charts' do
-    shared_examples 'returns true' do
-      it { expect(should_render_lead_time_charts).to be(true) }
-    end
-
-    shared_examples 'returns false' do
-      it { expect(should_render_lead_time_charts).to be(false) }
+      it { expect(should_render_dora_charts).to be(false) }
     end
 
     it_behaves_like 'returns true'
