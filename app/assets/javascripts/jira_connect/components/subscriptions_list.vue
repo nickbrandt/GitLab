557624588e8d18c25a81a22528d099d8d4deea58
Justin Ho <hduong@gitlab.com>
2021-04-13 14:09:06 +0700

    Add empty state and remove old HAML views
    Add changelog

--- app/assets/javascripts/jira_connect/components/subscriptions_list.vue
+++ app/assets/javascripts/jira_connect/components/subscriptions_list.vue
@@ -1,5 +1,6 @@
 <script>
-import { GlButton, GlTable } from '@gitlab/ui';
+import { GlButton, GlEmptyState, GlTable } from '@gitlab/ui';
+import { isEmpty } from 'lodash';
 import { removeSubscription } from '~/jira_connect/api';
 import { reloadPage } from '~/jira_connect/utils';
 import { __, s__ } from '~/locale';
@@ -9,6 +10,7 @@ import GroupItemName from './group_item_name.vue';
 export default {
   components: {
     GlButton,
+    GlEmptyState,
     GlTable,
     GroupItemName,
     TimeagoTooltip,
@@ -33,7 +35,14 @@ export default {
       label: '',
     },
   ],
+  i18n: {
+    emptyTitle: s__('Integrations|No linked namespaces'),
+    emptyDescription: s__(
+      'Integrations|Namespaces are the GitLab groups and subgroups you link to this Jira instance.',
+    ),
+  },
   methods: {
+    isEmpty,
     onClick(item) {
       removeSubscription(item.unlink_path)
         .then(() => {
@@ -47,17 +56,24 @@ export default {
 </script>
 
 <template>
-  <gl-table :items="subscriptions" :fields="$options.fields">
-    <template #cell(name)="{ item }">
-      <group-item-name :group="item.group" />
-    </template>
-    <template #cell(created_at)="{ item }">
-      <timeago-tooltip :time="item.created_at" />
-    </template>
-    <template #cell(actions)="{ item }">
-      <gl-button category="secondary" :loading="false" @click.prevent="onClick(item)">{{
-        __('Unlink')
-      }}</gl-button>
-    </template>
-  </gl-table>
+  <div>
+    <gl-empty-state
+      v-if="isEmpty(subscriptions)"
+      :title="$options.i18n.emptyTitle"
+      :description="$options.i18n.emptyDescription"
+    />
+    <gl-table v-else :items="subscriptions" :fields="$options.fields">
+      <template #cell(name)="{ item }">
+        <group-item-name :group="item.group" />
+      </template>
+      <template #cell(created_at)="{ item }">
+        <timeago-tooltip :time="item.created_at" />
+      </template>
+      <template #cell(actions)="{ item }">
+        <gl-button category="secondary" :loading="false" @click.prevent="onClick(item)">{{
+          __('Unlink')
+        }}</gl-button>
+      </template>
+    </gl-table>
+  </div>
 </template>
