0cf2754ede4c0b185b4312b28d558dcf7fa505ff
jejacks0n <jjackson@gitlab.com>
2021-04-13 13:45:05 -0600

    Move the gitlab-experiment test to a new file

--- spec/features/admin/admin_abuse_reports_spec.rb
+++ spec/features/admin/admin_abuse_reports_spec.rb
@@ -15,40 +15,6 @@ RSpec.describe "Admin::AbuseReports", :js do
     describe 'if a user has been reported for abuse' do
       let!(:abuse_report) { create(:abuse_report, user: user) }
 
-      describe 'with experimentation tracking' do
-        # This is part of a set of tests that ensure that tracking remains
-        # consistent at the front end layer. Since we don't want to actually
-        # introduce an experiment in real code, we're going to simulate it
-        # here.
-        before do
-          stub_experiments(null_hypothesis: :candidate)
-        end
-
-        it 'publishes the experiments that have been run to the client', :experiment do
-          allow_next_instance_of(Admin::AbuseReportsController) do |instance|
-            allow(instance).to receive(:index).and_wrap_original do |original|
-              instance.experiment(:null_hypothesis, user: instance.current_user) do |e|
-                e.use { original.call }
-                e.try { original.call }
-              end
-            end
-          end
-
-          visit admin_abuse_reports_path
-
-          expect(page).to have_content('Abuse Reports')
-
-          published_experiments = page.evaluate_script('window.gon.experiment')
-          expect(published_experiments).to include({
-            'null_hypothesis' => {
-              'experiment' => 'null_hypothesis',
-              'key' => anything,
-              'variant' => 'candidate'
-            }
-          })
-        end
-      end
-
       describe 'in the abuse report view' do
         it 'presents information about abuse report' do
           visit admin_abuse_reports_path
