e609b44fbb7ccc0fbf1faaccc0fe8a349d5f99e5
Nicolas Dular <ndular@gitlab.com>
2021-04-14 20:50:47 +0200

    Change unless style and fix typo

--- spec/workers/namespaces/in_product_marketing_emails_worker_spec.rb
+++ spec/workers/namespaces/in_product_marketing_emails_worker_spec.rb
@@ -5,22 +5,20 @@ require 'spec_helper'
 RSpec.describe Namespaces::InProductMarketingEmailsWorker, '#perform' do
   using RSpec::Parameterized::TableSyntax
 
-  # Running these tests in EE would call the overriden method, which we can't test in CE.
-  # EE is covered in a separate EE spec.
-  unless Gitlab.ee?
-    context 'not on gitlab.com' do
-      let(:is_gitlab_com) { false }
+  # Running this in EE would call the overridden method, which can't be tested in CE.
+  # The EE code is covered in a separate EE spec.
+  context 'not on gitlab.com', unless: Gitlab.ee? do
+    let(:is_gitlab_com) { false }
 
-      where(:in_product_marketing_emails_enabled, :experiment_active, :executes_service) do
-        true     | true     | 1
-        true     | false    | 1
-        false    | false    | 0
-        false    | true     | 0
-      end
+    where(:in_product_marketing_emails_enabled, :experiment_active, :executes_service) do
+      true     | true     | 1
+      true     | false    | 1
+      false    | false    | 0
+      false    | true     | 0
+    end
 
-      with_them do
-        it_behaves_like 'in-product marketing email'
-      end
+    with_them do
+      it_behaves_like 'in-product marketing email'
     end
   end
 
