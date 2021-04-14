8151fdd46fa24bcd89cd2ea719a48de8fa99882f
Francisco Javier López <fjlopez@gitlab.com>
2021-04-14 12:23:20 +0200

    Add Labels menu

--- app/models/sidebars/projects/panel.rb
+++ app/models/sidebars/projects/panel.rb
@@ -12,6 +12,7 @@ module Sidebars
         add_menu(Sidebars::Projects::Menus::Repository::Menu.new(context))
         add_menu(Sidebars::Projects::Menus::Issues::Menu.new(context))
         add_menu(Sidebars::Projects::Menus::ExternalIssueTracker::Menu.new(context))
+        add_menu(Sidebars::Projects::Menus::Labels::Menu.new(context))
       end
 
       override :render_raw_menus_partial
