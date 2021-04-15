a87528dddd0ad618d747bd8fcb3f92ce6723a949
Simon Knox <simon@gitlab.com>
2021-04-15 02:50:55 +0000

    Enable board_new_list and iteration_board_lists feature flags by default

--- ee/app/services/ee/boards/issues/move_service.rb
+++ ee/app/services/ee/boards/issues/move_service.rb
@@ -39,7 +39,7 @@ module EE
             milestone_id: milestone_id
           }
 
-          movement_args[:sprint_id] = iteration_id(issue) if ::Feature.enabled?(:iteration_board_lists, parent)
+          movement_args[:sprint_id] = iteration_id(issue) if ::Feature.enabled?(:iteration_board_lists, parent, default_enabled: :yaml)
 
           movement_args
         end
