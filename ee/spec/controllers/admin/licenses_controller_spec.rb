1a278b5e00e4f4269f8487c4aef31c0817550d9f
Tyler Amos <tamos@gitlab.com>
2021-04-21 17:52:09 +0000

    Adjust sync_seat_link action to respond with JSON
    This allows the FE to leverage async actions instead of a full page
    refresh.

--- ee/spec/controllers/admin/licenses_controller_spec.rb
+++ ee/spec/controllers/admin/licenses_controller_spec.rb
@@ -115,22 +115,22 @@ RSpec.describe Admin::LicensesController do
     context 'with seat link enabled' do
       let(:seat_link_enabled) { true }
 
-      it 'redirects with a successful message' do
-        post :sync_seat_link
+      it 'returns a success response' do
+        post :sync_seat_link, format: :json
 
-        expect(response).to redirect_to(admin_license_path)
-        expect(flash[:notice]).to eq('Your license was successfully synced.')
+        expect(response).to have_gitlab_http_status(:ok)
+        expect(json_response).to eq({ 'success' => true })
       end
     end
 
     context 'with seat link disabled' do
       let(:seat_link_enabled) { false }
 
-      it 'redirects with an error message' do
-        post :sync_seat_link
+      it 'returns a failure response' do
+        post :sync_seat_link, format: :json
 
-        expect(response).to redirect_to(admin_license_path)
-        expect(flash[:error]).to match('There was an error when trying to sync your license.')
+        expect(response).to have_gitlab_http_status(:unprocessable_entity)
+        expect(json_response).to eq({ 'success' => false })
       end
     end
   end
