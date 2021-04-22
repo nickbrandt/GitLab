98b63c71aa4ae228ce38b03a6814fdf1b1aa7f44
Daniel Tian <dtian@gitlab.com>
2021-04-22 00:48:26 -1000

    Use querystring as source of truth for filters on vulnerability report

--- ee/app/assets/javascripts/security_dashboard/components/shared/filters/scanner_filter.vue
+++ ee/app/assets/javascripts/security_dashboard/components/shared/filters/scanner_filter.vue
@@ -93,8 +93,6 @@ export default {
       this.selectedOptions = options.every((option) => this.selectedSet.has(option))
         ? without(this.selectedOptions, ...options)
         : union(this.selectedOptions, options);
-
-      this.updateQuerystring();
     },
   },
 };
