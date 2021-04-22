2861c45fd7ac94c40288fe347e9b880e65aa4f24
Terri Chu <tchu@gitlab.com>
2021-04-22 09:16:10 +0000

    Search: Always show scope tabs regardless of tab selected

--- ee/spec/controllers/ee/search_controller_spec.rb
+++ ee/spec/controllers/ee/search_controller_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe SearchController do
+RSpec.describe SearchController, :elastic do
   let_it_be(:user) { create(:user) }
 
   before do
@@ -10,7 +10,7 @@ RSpec.describe SearchController do
   end
 
   describe 'GET #show' do
-    context 'unique users tracking', :elastic do
+    context 'unique users tracking' do
       before do
         stub_ee_application_setting(elasticsearch_search: true, elasticsearch_indexing: true)
         allow(Gitlab::UsageDataCounters::HLLRedisCounter).to receive(:track_event)
@@ -69,6 +69,62 @@ RSpec.describe SearchController do
           end
         end
       end
+    end
+
+    shared_examples 'renders the elasticsearch tabs if elasticsearch is enabled' do
+      using RSpec::Parameterized::TableSyntax
+
+      render_views
+
+      subject { get :show, params: request_params, format: :html }
+
+      where(:scope) { %w[projects issues merge_requests milestones epics notes blobs commits wiki_blobs users] }
+
+      with_them do
+        context 'when elasticsearch is enabled' do
+          before do
+            stub_ee_application_setting(elasticsearch_search: true, elasticsearch_indexing: true)
+          end
+
+          it 'shows the elasticsearch tabs' do
+            subject
+
+            expect(response.body).to have_link('Code')
+            expect(response.body).to have_link('Wiki')
+            expect(response.body).to have_link('Comments')
+            expect(response.body).to have_link('Commits')
+          end
+        end
+
+        context 'when elasticsearch is disabled' do
+          before do
+            stub_ee_application_setting(elasticsearch_search: false, elasticsearch_indexing: false)
+          end
+
+          it 'does not show the elasticsearch tabs' do
+            subject
+
+            expect(response.body).not_to have_link('Code')
+            expect(response.body).not_to have_link('Wiki')
+            expect(response.body).not_to have_link('Comments')
+            expect(response.body).not_to have_link('Commits')
+          end
+        end
+      end
+    end
+
+    context 'global search' do
+      let(:request_params) { { scope: scope, search: 'term' } }
+
+      it_behaves_like 'renders the elasticsearch tabs if elasticsearch is enabled'
+    end
+
+    context 'group search' do
+      let_it_be(:group) { create(:group) }
+
+      let(:request_params) { { group_id: group.id, scope: scope, search: 'term' } }
+
+      it_behaves_like 'renders the elasticsearch tabs if elasticsearch is enabled'
     end
   end
 end
