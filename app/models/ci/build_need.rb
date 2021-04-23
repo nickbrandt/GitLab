f89dbba3b3e859a55d693ba0c0ab9654f9711b12
Andreas Brandl <abrandl@gitlab.com>
2021-04-23 12:19:04 +0200

    Add comment to describe workaround context

--- app/models/ci/build_need.rb
+++ app/models/ci/build_need.rb
@@ -18,6 +18,9 @@ module Ci
     scope :scoped_build, -> { where('ci_builds.id=ci_build_needs.build_id') }
     scope :artifacts, -> { where(artifacts: true) }
 
+    # TODO: Remove once build_id_convert_to_bigint is not an "ignored" column anymore (see .ignore_columns above)
+    # There is a database-side trigger to populate this column. This is unexpected in the context
+    # of cloning an instance, e.g. when retrying the job. Hence we exclude the ignored column explicitly here.
     def attributes
       super.except('build_id_convert_to_bigint')
     end
