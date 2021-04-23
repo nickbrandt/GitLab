788b4ce3f860a2aee59114f012fbc93d7acb6aed
Jonathan Schafer <jschafer@gitlab.com>
2021-04-23 16:07:41 +0000

    Add FindingEvidenceResponse model

--- ee/app/models/vulnerabilities/finding_evidence.rb
+++ ee/app/models/vulnerabilities/finding_evidence.rb
@@ -7,5 +7,6 @@ module Vulnerabilities
     belongs_to :finding, class_name: 'Vulnerabilities::Finding', inverse_of: :finding_evidences, foreign_key: 'vulnerability_occurrence_id', optional: false
 
     has_many :requests, class_name: 'Vulnerabilities::FindingEvidenceRequest', inverse_of: :finding_evidence, foreign_key: 'vulnerability_finding_evidence_id'
+    has_many :responses, class_name: 'Vulnerabilities::FindingEvidenceResponse', inverse_of: :finding_evidence, foreign_key: 'vulnerability_finding_evidence_id'
   end
 end
