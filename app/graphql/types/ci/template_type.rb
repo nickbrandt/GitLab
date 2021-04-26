961b0b5258161fbd064759636b14071172a9f2ca
Matija Čupić <matteeyah@gmail.com>
2021-04-26 14:14:44 +0200

    Add CI template field to project GraphQL type
    Changelog: added

---
+++ app/graphql/types/ci/template_type.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Types
+  module Ci
+    # rubocop: disable Graphql/AuthorizeTypes
+    class TemplateType < BaseObject
+      graphql_name 'CiTemplate'
+
+      field :name, GraphQL::STRING_TYPE, null: false,
+        description: 'Name of the CI template.'
+      field :content, GraphQL::STRING_TYPE, null: false,
+        description: 'Contents of the CI template.'
+    end
+  end
+end
