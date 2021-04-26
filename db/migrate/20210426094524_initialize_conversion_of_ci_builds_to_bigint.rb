d6fcc60d82ac8c5e5f62b0819a78d4532d288e0a
Andreas Brandl <abrandl@gitlab.com>
2021-04-26 17:14:17 +0200

    Use rollback helpers for pk migration

--- db/migrate/20210426094524_initialize_conversion_of_ci_builds_to_bigint.rb
+++ db/migrate/20210426094524_initialize_conversion_of_ci_builds_to_bigint.rb
@@ -12,11 +12,6 @@ class InitializeConversionOfCiBuildsToBigint < ActiveRecord::Migration[6.0]
   end
 
   def down
-    trigger_name = rename_trigger_name(TABLE, COLUMNS, TARGET_COLUMNS)
-    remove_rename_triggers_for_postgresql TABLE, trigger_name
-
-    TARGET_COLUMNS.each do |column|
-      remove_column TABLE, column
-    end
+    revert_initialize_conversion_of_integer_to_bigint(TABLE, COLUMNS)
   end
 end
--- db/post_migrate/20210426094549_backfill_ci_builds_for_bigint_conversion.rb
+++ db/post_migrate/20210426094549_backfill_ci_builds_for_bigint_conversion.rb
@@ -5,20 +5,19 @@ class BackfillCiBuildsForBigintConversion < ActiveRecord::Migration[6.0]
 
   DOWNTIME = false
 
+  TABLE = :ci_builds
+  COLUMNS = %i(id stage_id).freeze
+
   def up
     return unless should_run?
 
-    backfill_conversion_of_integer_to_bigint :ci_builds, %i(id stage_id), batch_size: 15000, sub_batch_size: 100
+    backfill_conversion_of_integer_to_bigint TABLE, COLUMNS, batch_size: 15000, sub_batch_size: 100
   end
 
   def down
     return unless should_run?
 
-    Gitlab::Database::BackgroundMigration::BatchedMigration
-      .where(job_class_name: 'CopyColumnUsingBackgroundMigrationJob')
-      .where(table_name: 'ci_builds', column_name: 'id')
-      .where('job_arguments = ?', [%w[id stage_id], %w[id_convert_to_bigint stage_id_convert_to_bigint]].to_json)
-      .delete_all
+    revert_backfill_conversion_of_integer_to_bigint TABLE, COLUMNS
   end
 
   private
