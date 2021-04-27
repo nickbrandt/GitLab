9b02b9f6ed5686466a5f5200fe5fe6d03dd10654
Jacob Vosmaer <jacob@gitlab.com>
2021-04-27 18:19:52 +0200

    Only count running jobs in LimitedCapacity::Worker
    The old implementation of LimitedCapacity::Worker relies on querying
    the number of jobs enqueued for the worker it applies to. In
    https://gitlab.com/groups/gitlab-com/gl-infra/-/epics/447 we are
    working towards a situation where you can no longer get this number
    from Sidekiq (in an efficient way, at least).
    
    This commit changes the capacity checking logic to only use the set of
    "running jobs". The capacity check now happens in exactly one place,
    namely LimitedCapacity::Worker#perform.
    LimitedCapacity::Worker.perform_with_capacity now always schedules
    max_running_jobs jobs. Sometimes these will be no-ops but that is OK,
    we expect less than 0.01 no-op jobs per second on GitLab.com.

--- app/workers/concerns/limited_capacity/worker.rb
+++ app/workers/concerns/limited_capacity/worker.rb
@@ -55,26 +55,14 @@ module LimitedCapacity
       def perform_with_capacity(*args)
         worker = self.new
         worker.remove_failed_jobs
-        worker.report_prometheus_metrics(*args)
-        required_jobs_count = worker.required_jobs_count(*args)
 
-        arguments = Array.new(required_jobs_count) { args }
+        arguments = Array.new(worker.max_running_jobs) { args }
         self.bulk_perform_async(arguments) # rubocop:disable Scalability/BulkPerformWithContext
       end
     end
 
     def perform(*args)
-      return unless has_capacity?
-
-      job_tracker.register(jid)
-      report_running_jobs_metrics
-      perform_work(*args)
-    rescue StandardError => exception
-      raise
-    ensure
-      job_tracker.remove(jid)
-      report_prometheus_metrics(*args)
-      re_enqueue(*args) unless exception
+      perform_registered(*args) if job_tracker.register(jid, max_running_jobs)
     end
 
     def perform_work(*args)
@@ -89,21 +77,6 @@ module LimitedCapacity
       raise NotImplementedError
     end
 
-    def has_capacity?
-      remaining_capacity > 0
-    end
-
-    def remaining_capacity
-      [
-        max_running_jobs - running_jobs_count - self.class.queue_size,
-        0
-      ].max
-    end
-
-    def has_work?(*args)
-      remaining_work_count(*args) > 0
-    end
-
     def remove_failed_jobs
       job_tracker.clean_up
     end
@@ -114,18 +87,22 @@ module LimitedCapacity
       set_metric(:max_running_jobs_gauge, max_running_jobs)
     end
 
+    private
+
+    def perform_registered(*args)
+      report_running_jobs_metrics
+      perform_work(*args)
+    rescue StandardError => exception
+      raise
+    ensure
+      job_tracker.remove(jid)
+      report_prometheus_metrics(*args)
+      re_enqueue(*args) unless exception
+    end
+
     def report_running_jobs_metrics
       set_metric(:running_jobs_gauge, running_jobs_count)
     end
-
-    def required_jobs_count(*args)
-      [
-        remaining_work_count(*args),
-        remaining_capacity
-      ].min
-    end
-
-    private
 
     def running_jobs_count
       job_tracker.count
@@ -138,8 +115,7 @@ module LimitedCapacity
     end
 
     def re_enqueue(*args)
-      return unless has_capacity?
-      return unless has_work?(*args)
+      return unless remaining_work_count(*args) > 0
 
       self.class.perform_async(*args)
     end
