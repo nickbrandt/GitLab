4f9d7f600c058c6789114c4b52b96773f80122ef
Mehmet Emin INAC <minac@gitlab.com>
2021-04-27 18:17:22 +0200

    Initial commit

---
+++ ee/lib/api/helpers/security_and_compliance_permissions.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module API
+  module Helpers
+    module SecurityAndCompliancePermissions
+      extend ActiveSupport::Concern
+
+      included do
+        before do
+        end
+      end
+    end
+  end
+end
