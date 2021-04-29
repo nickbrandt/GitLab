56016b11b8aba0bdbaf377e936d9913d83426fc2
Arturo Herrero <arturo.herrero@gmail.com>
2021-04-29 08:39:35 +0100

    Declare WebHook model in the migration

--- db/migrate/20210428151144_update_invalid_web_hooks.rb
+++ db/migrate/20210428151144_update_invalid_web_hooks.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class UpdateInvalidWebHooks < ActiveRecord::Migration[6.0]
+  class WebHook < ActiveRecord::Base
+    self.table_name = 'web_hooks'
+  end
+
   def up
     WebHook.where(type: 'ProjectHook')
            .where.not(project_id: nil)
