56f59fc084c9ceab26ad096f16bbb13f750a094e
Arturo Herrero <arturo.herrero@gmail.com>
2021-04-30 09:19:42 +0100

    Use each_batch to improve query performance

--- db/migrate/20210428151144_update_invalid_web_hooks.rb
+++ db/migrate/20210428151144_update_invalid_web_hooks.rb
@@ -2,14 +2,18 @@
 
 class UpdateInvalidWebHooks < ActiveRecord::Migration[6.0]
   class WebHook < ActiveRecord::Base
+    include EachBatch
+
     self.table_name = 'web_hooks'
   end
 
   def up
-    WebHook.where(type: 'ProjectHook')
-           .where.not(project_id: nil)
-           .where.not(group_id: nil)
-           .update_all(group_id: nil)
+    WebHook.each_batch(of: 10_000, column: :id) do |relation|
+      relation.where(type: 'ProjectHook')
+              .where.not(project_id: nil)
+              .where.not(group_id: nil)
+              .update_all(group_id: nil)
+    end
   end
 
   def down
