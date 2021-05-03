b9aa07da519b5b3b9c803380999b20c6a57aa9f7
Alex Kalderimis <akalderimis@gitlab.com>
2021-05-03 15:35:16 +0100

    Add columns and indices on web hooks
    This adds three new columns on web-hooks: The two columns are
    
    - recent_failures: a counter of recent 4xx level failures
    - backoff_count: a counter of times we have backed-off
    - disabled_until: a timestamp for when we can next use the hook
    
    One new index is added, to aid finding enabled web-hooks,
    on `(project_id, recent_failures)`, which is used
    when selecting active project hooks.

---
+++ db/migrate/20210504085144_add_index_on_web_hook_project_id_recent_failures.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# See https://docs.gitlab.com/ee/development/migration_style_guide.html
+# for more information on how to write migrations for GitLab.
+
+class AddIndexOnWebHookProjectIdRecentFailures < ActiveRecord::Migration[6.0]
+  include Gitlab::Database::MigrationHelpers
+
+  INDEX_NAME = 'index_web_hooks_on_project_id_recent_failures'
+
+  disable_ddl_transaction!
+
+  def up
+    add_concurrent_index(:web_hooks, [:project_id, :recent_failures], name: INDEX_NAME)
+  end
+
+  def down
+    remove_concurrent_index_by_name(:web_hooks, INDEX_NAME)
+  end
+end
