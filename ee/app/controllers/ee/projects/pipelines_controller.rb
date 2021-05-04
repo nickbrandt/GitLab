57e0b450585ad2d5f3cb102c1cf855801f4a7ddd
Bob Van Landuyt <bob@gitlab.com>
2021-05-04 00:53:55 +0000

    Separate feature categories in PipelinesController

--- ee/app/controllers/ee/projects/pipelines_controller.rb
+++ ee/app/controllers/ee/projects/pipelines_controller.rb
@@ -12,6 +12,10 @@ module EE
           push_frontend_feature_flag(:usage_data_i_testing_full_code_quality_report_total, project, default_enabled: true)
           push_frontend_feature_flag(:pipeline_security_dashboard_graphql, project, type: :development, default_enabled: :yaml)
         end
+
+        feature_category :license_compliance, [:licenses]
+        feature_category :vulnerability_management, [:security]
+        feature_category :code_quality, [:codequality_report]
       end
 
       def security
