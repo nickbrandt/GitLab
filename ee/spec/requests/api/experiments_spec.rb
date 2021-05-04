c59e824a17ec6f1b0fd081593b5bd570a4dbd329
jejacks0n <jjackson@gitlab.com>
2021-05-04 16:24:15 -0600

    Update gitlab-experiment to 0.5.4

--- ee/spec/requests/api/experiments_spec.rb
+++ ee/spec/requests/api/experiments_spec.rb
@@ -140,7 +140,7 @@ RSpec.describe API::Experiments do
           end
 
           it 'publishes into a collection of experiments that have been run in the request' do
-            pending 'requires gitlab-experiment >= 0.5.4'
+            pending 'requires gitlab-experiment >= 0.5.4 -- resolved in a follow up MR'
 
             get api('/experiments', user)
 
