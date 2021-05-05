f82fbb4eb4853b91ce73b3d07d7a1dcfdeefd6aa
Małgorzata Ksionek <mksionek@gitlab.com>
2021-05-05 12:59:11 +0200

    Add regenerated string file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1121,7 +1121,7 @@ class User < ApplicationRecord
     Gitlab::CurrentSettings.password_authentication_enabled_for_git? && !password_based_omniauth_user?
   end
 
-  #method overriden in EE
+  # method overriden in EE
   def user_authorized_by_provisioning_group?
     false
   end
