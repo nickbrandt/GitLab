fd5d7989f0046b1855ea096b9728a68613fb9b84
Mike Greiling <mike@pixelcog.com>
2021-05-05 00:05:19 -0500

    Remove old namespace select dropdown implementation

--- app/assets/javascripts/pages/admin/projects/index.js
+++ app/assets/javascripts/pages/admin/projects/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import { parseBoolean } from '~/lib/utils/common_utils';
 import { mergeUrlParams } from '~/lib/utils/url_utility';
-import OldNamespaceSelect from '~/namespace_select';
 import ProjectsList from '~/projects_list';
 import RemoveMemberModal from '~/vue_shared/components/remove_member_modal.vue';
 import NamespaceSelect from './components/namespace_select.vue';
@@ -23,10 +22,6 @@ function mountRemoveMemberModal() {
 mountRemoveMemberModal();
 
 new ProjectsList(); // eslint-disable-line no-new
-
-document
-  .querySelectorAll('.js-old-namespace-select')
-  .forEach((dropdown) => new OldNamespaceSelect({ dropdown }));
 
 function mountNamespaceSelect() {
   const el = document.querySelector('.js-namespace-select');
