db58b29afcf18b46c5d844a1c086454e394d8079
Illya Klymov <xanf@xanf.me>
2021-05-06 10:15:29 +0300

    Update group creation UI
    - introduce card-based UI
    
    Changelog: changed

---
+++ app/assets/javascripts/pages/groups/new/components/create_group_description_details.vue
@@ -0,0 +1,49 @@
+<script>
+import { GlSprintf, GlLink } from '@gitlab/ui';
+import { helpPagePath } from '~/helpers/help_page_helper';
+
+export default {
+  components: {
+    GlLink,
+    GlSprintf,
+  },
+  computed: {
+    groupsHelpPath() {
+      return helpPagePath('user/group/index');
+    },
+
+    subgroupsHelpPath() {
+      return helpPagePath('user/group/subgroups/index');
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p>
+      <gl-sprintf
+        :message="
+          s__(
+            'GroupsNew|%{linkStart}Groups%{linkEnd} allow you to manage and collaborate across multiple projects. Members of a group have access to all of its projects. Groups can also be nested by creating subgroups.',
+          )
+        "
+      >
+        <template #link="{ content }">
+          <gl-link :href="groupsHelpPath" target="_blank">{{ content }}</gl-link>
+        </template>
+      </gl-sprintf>
+    </p>
+    <p>
+      <gl-sprintf
+        :message="
+          s__('GroupsNew|Groups can also be nested by creating %{linkStart}subgroups%{linkEnd}.')
+        "
+      >
+        <template #link="{ content }">
+          <gl-link :href="subgroupsHelpPath" target="_blank">{{ content }}</gl-link>
+        </template>
+      </gl-sprintf>
+    </p>
+  </div>
+</template>
