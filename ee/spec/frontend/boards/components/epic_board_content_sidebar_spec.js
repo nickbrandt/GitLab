3da6019dd5d43d9a24d912815d525750cd63908d
Florie Guibert <fguibert@gitlab.com>
2021-05-06 07:09:18 +0000

    Epic boards - Display epic participants in sidebar

--- ee/spec/frontend/boards/components/epic_board_content_sidebar_spec.js
+++ ee/spec/frontend/boards/components/epic_board_content_sidebar_spec.js
@@ -4,8 +4,11 @@ import Vuex from 'vuex';
 import EpicBoardContentSidebar from 'ee_component/boards/components/epic_board_content_sidebar.vue';
 import { stubComponent } from 'helpers/stub_component';
 import BoardSidebarLabelsSelect from '~/boards/components/sidebar/board_sidebar_labels_select.vue';
+import BoardSidebarTitle from '~/boards/components/sidebar/board_sidebar_title.vue';
 import { ISSUABLE } from '~/boards/constants';
 import SidebarConfidentialityWidget from '~/sidebar/components/confidential/sidebar_confidentiality_widget.vue';
+import SidebarDateWidget from '~/sidebar/components/date/sidebar_date_widget.vue';
+import SidebarParticipantsWidget from '~/sidebar/components/participants/sidebar_participants_widget.vue';
 import SidebarSubscriptionsWidget from '~/sidebar/components/subscriptions/sidebar_subscriptions_widget.vue';
 import { mockEpic } from '../mock_data';
 
@@ -59,30 +62,42 @@ describe('EpicBoardContentSidebar', () => {
   });
 
   it('confirms we render GlDrawer', () => {
-    expect(wrapper.find(GlDrawer).exists()).toBe(true);
+    expect(wrapper.findComponent(GlDrawer).exists()).toBe(true);
   });
 
   it('does not render GlDrawer when isSidebarOpen is false', () => {
     createStore({ mockGetters: { isSidebarOpen: () => false } });
     createComponent();
 
-    expect(wrapper.find(GlDrawer).exists()).toBe(false);
+    expect(wrapper.findComponent(GlDrawer).exists()).toBe(false);
   });
 
   it('applies an open attribute', () => {
-    expect(wrapper.find(GlDrawer).props('open')).toBe(true);
+    expect(wrapper.findComponent(GlDrawer).props('open')).toBe(true);
   });
 
   it('renders BoardSidebarLabelsSelect', () => {
-    expect(wrapper.find(BoardSidebarLabelsSelect).exists()).toBe(true);
+    expect(wrapper.findComponent(BoardSidebarLabelsSelect).exists()).toBe(true);
+  });
+
+  it('renders BoardSidebarTitle', () => {
+    expect(wrapper.findComponent(BoardSidebarTitle).exists()).toBe(true);
   });
 
   it('renders SidebarConfidentialityWidget', () => {
-    expect(wrapper.find(SidebarConfidentialityWidget).exists()).toBe(true);
+    expect(wrapper.findComponent(SidebarConfidentialityWidget).exists()).toBe(true);
+  });
+
+  it('renders 2 SidebarDateWidget', () => {
+    expect(wrapper.findAll(SidebarDateWidget)).toHaveLength(2);
+  });
+
+  it('renders SidebarParticipantsWidget', () => {
+    expect(wrapper.findComponent(SidebarParticipantsWidget).exists()).toBe(true);
   });
 
   it('renders SidebarSubscriptionsWidget', () => {
-    expect(wrapper.find(SidebarSubscriptionsWidget).exists()).toBe(true);
+    expect(wrapper.findComponent(SidebarSubscriptionsWidget).exists()).toBe(true);
   });
 
   describe('when we emit close', () => {
