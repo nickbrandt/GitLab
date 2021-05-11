65bb88edb14f769d5a12d3cac97cb31f41852158
Marc Shaw <mshaw@gitlab.com>
2021-05-11 15:45:12 +1200

    Revert "Merge branch '321625-epic_boards-redirect' into 'master'"
    This reverts commit 186c51b54447b1199a2967c6ca226a8bb3e0b310, reversing
    changes made to a09996b3f19c933d211ea49dc687a610815cbfc1.

--- ee/app/finders/boards/epic_boards_visits_finder.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Boards
-  class EpicBoardsVisitsFinder < VisitsFinder
-    def recent_visit_model
-      Boards::EpicBoardRecentVisit
-    end
-  end
-end
--- ee/spec/factories/boards/epic_board_recent_visits.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :epic_board_recent_visit, class: 'Boards::EpicBoardRecentVisit' do
-    user
-    group
-    epic_board
-  end
-end
