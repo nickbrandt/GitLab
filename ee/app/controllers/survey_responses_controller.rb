677c632969ee2af1d4dfc8c2ab18142f67601818
Alex Buijs <abuijs@gitlab.com>
2021-05-12 09:59:21 +0000

    Add Calendly invite link to survey response page [RUN ALL RSPEC] [RUN AS-IF-FOSS]

--- ee/app/controllers/survey_responses_controller.rb
+++ ee/app/controllers/survey_responses_controller.rb
@@ -2,20 +2,24 @@
 
 class SurveyResponsesController < ApplicationController
   SURVEY_RESPONSE_SCHEMA_URL = 'iglu:com.gitlab/survey_response/jsonschema/1-0-0'
+  CALENDLY_INVITE_LINK = 'https://calendly.com/mkarampalas/gitlab-user-onboarding-research'
+
+  before_action :track_response, only: :index
+  before_action :set_invite_link, only: :index
 
   skip_before_action :authenticate_user!
 
   feature_category :navigation
 
   def index
-    track_response if Gitlab.com?
-
     render layout: false
   end
 
   private
 
   def track_response
+    return unless Gitlab.dev_env_or_com?
+
     data = {
       survey_id: to_number(params[:survey_id]),
       instance_id: to_number(params[:instance_id]),
@@ -33,5 +37,13 @@ class SurveyResponsesController < ApplicationController
 
   def to_number(param)
     param.to_i if param&.match?(/^\d+$/)
+  end
+
+  def set_invite_link
+    return unless Gitlab.dev_env_or_com?
+    return unless Gitlab::Utils.to_boolean(params[:show_invite_link])
+    return unless Feature.enabled?(:calendly_invite_link)
+
+    @invite_link = CALENDLY_INVITE_LINK
   end
 end
