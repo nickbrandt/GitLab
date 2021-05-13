6bec51ffe5c96720cdd4cecfdbfa725b41b5f649
Luke Duncalfe <lduncalfe@eml.cc>
2021-05-13 17:48:39 +1200

    Move issue tracker integrations to Integrations::
    https://gitlab.com/gitlab-org/gitlab/-/issues/201855

---
+++ ee/app/models/ee/integrations/issue_tracker.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module EE
+  module Integrations
+    module IssueTracker
+      def create_cross_reference_note
+        # implement inside child
+      end
+    end
+  end
+end
--- ee/app/models/ee/issue_tracker_service.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module EE
-  module IssueTrackerService
-    def create_cross_reference_note
-      # implement inside child
-    end
-  end
-end
