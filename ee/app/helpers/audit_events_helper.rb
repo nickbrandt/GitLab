bcd4a2898392d039bd1ac6edcb6c94de7ff65cc6
Tan Le <tle@gitlab.com>
2021-05-13 19:34:39 +1000

    Remove unused audit events helper methods
    These methods were no longer called since we moved from server template
    to front-end rendering.

--- ee/app/helpers/audit_events_helper.rb
+++ ee/app/helpers/audit_events_helper.rb
@@ -2,40 +2,26 @@
 
 module AuditEventsHelper
   FILTER_TOKEN_TYPES = {
-      user: :user,
-      group: :group,
-      project: :project,
-      member: :member
+    user: :user,
+    group: :group,
+    project: :project,
+    member: :member
   }.freeze
 
   def admin_audit_event_tokens
-    [{ type: FILTER_TOKEN_TYPES[:user] }, { type: FILTER_TOKEN_TYPES[:group] }, { type: FILTER_TOKEN_TYPES[:project] }].freeze
+    [
+      { type: FILTER_TOKEN_TYPES[:user] },
+      { type: FILTER_TOKEN_TYPES[:group] },
+      { type: FILTER_TOKEN_TYPES[:project] }
+    ].freeze
   end
 
   def group_audit_event_tokens(group_id)
-    [{ type: FILTER_TOKEN_TYPES[:member], group_id: group_id }]
+    [{ type: FILTER_TOKEN_TYPES[:member], group_id: group_id }].freeze
   end
 
   def project_audit_event_tokens(project_path)
-    [{ type: FILTER_TOKEN_TYPES[:member], project_path: project_path }]
-  end
-
-  def human_text(details)
-    return details[:custom_message] if details[:custom_message]
-
-    details.map { |key, value| select_keys(key, value) }.join(" ").humanize
-  end
-
-  def select_keys(key, value)
-    if key =~ /^(author|target)_.*/
-      ""
-    elsif key.to_s == 'ip_address' && value.blank?
-      ""
-    elsif key =~ /^expiry_(from|to)$/ && value.blank?
-      "#{key} <strong>never expires</strong>"
-    else
-      "#{key} <strong>#{value}</strong>"
-    end
+    [{ type: FILTER_TOKEN_TYPES[:member], project_path: project_path }].freeze
   end
 
   def export_url
