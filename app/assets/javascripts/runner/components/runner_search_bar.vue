1a128307248f6de3a3adb4b4aaad4d7833430553
Miguel Rincon <mrincon@gitlab.com>
2021-05-14 12:01:53 +0200

    Add filtered search to runner

---
+++ app/assets/javascripts/runner/components/runner_search_bar.vue
@@ -0,0 +1,48 @@
+<script>
+import { __ } from '~/locale';
+import FilteredSearchBar from '~/vue_shared/components/filtered_search_bar/filtered_search_bar_root.vue';
+
+export default {
+  components: {
+    FilteredSearchBar,
+  },
+  data() {
+    const sortOptions = [
+      {
+        id: 1,
+        title: __('Created date'),
+        sortDirection: {
+          descending: 'created_desc',
+          ascending: 'created_asc',
+        },
+      },
+      {
+        id: 2,
+        title: __('Last contact'),
+        sortDirection: {
+          descending: 'updated_desc',
+          ascending: 'updated_asc',
+        },
+      },
+    ];
+
+    return {
+      searchTokens: [],
+      searchSortOptions: sortOptions,
+      searchInitialSortBy: sortOptions[0].sortDirection.descending,
+    };
+  },
+};
+</script>
+
+<template>
+  <filtered-search-bar
+    v-bind="$attrs"
+    recent-searches-storage-key="runners"
+    :sort-options="searchSortOptions"
+    :initial-sort-by="searchInitialSortBy"
+    :tokens="searchTokens"
+    :search-input-placeholder="__('Search or filter results...')"
+    v-on="$listeners"
+  />
+</template>
--- app/assets/javascripts/runner/runner_list/runner_list_app.vue
+++ app/assets/javascripts/runner/runner_list/runner_list_app.vue
@@ -1,6 +1,7 @@
 <script>
 import RunnerDescription from '../components/runner_description.vue';
 import RunnerList from '../components/runner_list.vue';
+import RunnerSearchBar from '../components/runner_search_bar.vue';
 import RunnerManualSetupHelp from '../components/runner_manual_setup_help.vue';
 import getRunnersQuery from '../graphql/get_runners.query.graphql';
 
@@ -8,6 +9,7 @@ export default {
   components: {
     RunnerDescription,
     RunnerList,
+    RunnerSearchBar,
     RunnerManualSetupHelp,
   },
   props: {
@@ -41,6 +43,9 @@ export default {
         <runner-manual-setup-help :registration-token="registrationToken" />
       </div>
     </div>
+
+    <!-- TODO Add filter search bar -->
+    <runner-search-bar namespace="admin" />
 
     <!-- TODO Add an empty state for no runners -->
     <runner-list :runners="runners" :loading="$apollo.queries.runners.loading" />
