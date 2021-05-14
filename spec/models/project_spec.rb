0d346291f613e7e3191e844d4bbd4a1f06a71a09
Igor Drozdov <idrozdov@gitlab.com>
2021-05-14 13:21:03 +0300

    Add post migration to cleanup orphaned records
    The migration cleans up lfs_objects_projects orphaned records
    
    The number of lfs_objects_projects on SaSS: 73747276
    Batch size: 10000
    Interval: 2 minutes
    
    The job is going to execute: ~10 days
    
    Changelog: added
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/61787

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6456,6 +6456,10 @@ RSpec.describe Project, factory_default: :keep do
       it 'lfs_objects_projects associations are deleted along with project' do
         expect { project.delete }.to change(LfsObjectsProject, :count).by(-2)
       end
+
+      it 'lfs_objects associations are unchanged when the assicated project is removed' do
+        expect { project.delete }.not_to change(LfsObject, :count)
+      end
     end
 
     context 'when project has no associated LFS objects' do
