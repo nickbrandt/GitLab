fde4980ff66aa713c63ff31a28294240df872eb1
Jan Provaznik <jprovaznik@gitlab.com>
2021-05-17 11:37:16 +0200

    Avoid N+1 queries in epic reference
    * We can avoid N+1 in epic autocomplete by preloading epic groups.
    * We can also optimize epic cross reference by using just group id instead
    of using epic's group (and group's projects).

--- ee/spec/requests/ee/groups/autocomplete_sources_spec.rb
+++ ee/spec/requests/ee/groups/autocomplete_sources_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe Groups::AutocompleteSourcesController do
+RSpec.describe 'groups autocomplete' do
   let_it_be(:user) { create(:user) }
   let_it_be_with_reload(:group) { create(:group, :private) }
   let_it_be(:epic) { create(:epic, group: group) }
@@ -32,9 +32,8 @@ RSpec.describe Groups::AutocompleteSourcesController do
     with_them do
       it 'returns the correct response', :aggregate_failures do
         issues = Array(expected).flat_map { |sym| public_send(sym) }
-        params = { group_id: group, issue_types: issue_types }.compact
 
-        get :issues, params: params
+        get issues_group_autocomplete_sources_path(group, issue_types: issue_types)
 
         expect(response).to have_gitlab_http_status(:ok)
         expect(json_response).to be_an(Array)
@@ -47,18 +46,28 @@ RSpec.describe Groups::AutocompleteSourcesController do
 
   describe '#epics' do
     it 'returns 200 status' do
-      get :epics, params: { group_id: group }
+      get epics_group_autocomplete_sources_path(group)
 
       expect(response).to have_gitlab_http_status(:ok)
     end
 
     it 'returns the correct response' do
-      get :epics, params: { group_id: group }
+      get epics_group_autocomplete_sources_path(group)
 
       expect(json_response).to be_an(Array)
       expect(json_response.first).to include(
         'iid' => epic.iid, 'title' => epic.title, 'reference' => epic.to_reference(epic.group)
       )
+    end
+
+    it 'avoids N+1 queries' do
+      control = ActiveRecord::QueryRecorder.new(skip_cached: false) { get epics_group_autocomplete_sources_path(group) }
+
+      create_list(:epic, 3, group: group)
+
+      expect do
+        get epics_group_autocomplete_sources_path(group)
+      end.not_to exceed_all_query_limit(control)
     end
   end
 
@@ -72,13 +81,13 @@ RSpec.describe Groups::AutocompleteSourcesController do
     end
 
     it 'returns 200 status' do
-      get :vulnerabilities, params: { group_id: group }
+      get vulnerabilities_group_autocomplete_sources_path(group)
 
       expect(response).to have_gitlab_http_status(:ok)
     end
 
     it 'returns the correct response', :aggregate_failures do
-      get :vulnerabilities, params: { group_id: group }
+      get vulnerabilities_group_autocomplete_sources_path(group)
 
       expect(json_response).to be_an(Array)
       expect(json_response.first).to include(
@@ -89,13 +98,13 @@ RSpec.describe Groups::AutocompleteSourcesController do
 
   describe '#commands' do
     it 'returns 200 status' do
-      get :commands, params: { group_id: group, type: 'Epic', type_id: epic.iid }
+      get commands_group_autocomplete_sources_path(group, type: 'Epic', type_id: epic.iid)
 
       expect(response).to have_gitlab_http_status(:ok)
     end
 
     it 'returns the correct response' do
-      get :commands, params: { group_id: group, type: 'Epic', type_id: epic.iid }
+      get commands_group_autocomplete_sources_path(group, type: 'Epic', type_id: epic.iid)
 
       expect(json_response).to be_an(Array)
       expect(json_response).to include(
@@ -107,7 +116,7 @@ RSpec.describe Groups::AutocompleteSourcesController do
     end
 
     it 'handles new epics' do
-      get :commands, params: { group_id: group, type: 'Epic', type_id: nil }
+      get commands_group_autocomplete_sources_path(group, type: 'Epic', type_id: nil)
 
       expect(json_response).to be_an(Array)
       expect(json_response).to include(
