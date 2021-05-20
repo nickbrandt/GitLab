c6ed9500c8d0ae9a51b0a0278cd7454c18ed75b6
Alper Akgun <aakgun@gitlab.com>
2021-05-20 17:13:08 +0300

    Optionally start a trial during free signup
    Experiment cleanup
    
    Changelog: added

--- ee/spec/views/registrations/groups/new.html.haml_spec.rb
+++ ee/spec/views/registrations/groups/new.html.haml_spec.rb
@@ -4,14 +4,12 @@ require 'spec_helper'
 
 RSpec.describe 'registrations/groups/new' do
   let_it_be(:user) { create(:user) }
-  let_it_be(:trial_during_signup) { false }
   let_it_be(:group) { create(:group) }
   let_it_be(:trial_onboarding_flow) { false }
 
   before do
     assign(:group, group)
     allow(view).to receive(:current_user).and_return(user)
-    allow(view).to receive(:experiment_enabled?).with(:trial_during_signup).and_return(trial_during_signup)
     allow(view).to receive(:in_trial_onboarding_flow?).and_return(trial_onboarding_flow)
 
     render
@@ -19,29 +17,9 @@ RSpec.describe 'registrations/groups/new' do
 
   subject { rendered }
 
-  context 'feature flag trial_during_signup is enabled' do
-    let_it_be(:trial_during_signup) { true }
-
-    it 'shows trial form and hides invite members' do
-      is_expected.to have_content('Company name')
-      is_expected.not_to have_selector('.js-invite-members')
-    end
-
-    context 'in trial onboarding' do
-      let_it_be(:trial_onboarding_flow) { true }
-
-      it 'hides trial form and shows invite members' do
-        is_expected.not_to have_content('Company name')
-        is_expected.to have_selector('.js-invite-members')
-      end
-    end
-  end
-
-  context 'feature flag trial_during_signup is disabled' do
-    it 'hides trial form and shows invite members' do
-      is_expected.not_to have_content('Company name')
-      is_expected.to have_selector('.js-invite-members')
-    end
+  it 'shows trial form and hides invite members' do
+    is_expected.to have_content('Company name')
+    is_expected.not_to have_selector('.js-invite-members')
   end
 
   it 'shows the progress bar' do
@@ -50,6 +28,11 @@ RSpec.describe 'registrations/groups/new' do
 
   context 'in trial onboarding' do
     let_it_be(:trial_onboarding_flow) { true }
+
+    it 'hides trial form and shows invite members' do
+      is_expected.not_to have_content('Company name')
+      is_expected.to have_selector('.js-invite-members')
+    end
 
     it 'hides the progress bar' do
       expect(rendered).not_to have_selector('#progress-bar')
