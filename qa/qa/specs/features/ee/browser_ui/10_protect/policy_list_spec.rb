256ad32f2a4ceffd23593de5a1732b1a3f447d2a
Alexander Turinske <aturinske@gitlab.com>
2021-05-21 16:28:22 +0000

    Move let variable out of before block

--- qa/qa/specs/features/ee/browser_ui/10_protect/policy_list_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/10_protect/policy_list_spec.rb
@@ -3,22 +3,22 @@
 module QA
   RSpec.describe 'Protect' do
     describe 'Threat Monitoring Policy List page' do
-      before do
-        let(:project) do
-          Resource::Project.fabricate_via_api! do |project|
-            project.name = Runtime::Env.auto_devops_project_name || 'project-with-protect'
-            project.description = 'Project with Protect'
-            project.auto_devops_enabled = false
-            project.initialize_with_readme = true
-          end
+      let(:project) do
+        Resource::Project.fabricate_via_api! do |project|
+          project.name = Runtime::Env.auto_devops_project_name || 'project-with-protect'
+          project.description = 'Project with Protect'
+          project.auto_devops_enabled = false
+          project.initialize_with_readme = true
         end
       end
 
-      it 'can load Threat Monitoring page and view the policy alert list', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1786' do
+      before do       
         Flow::Login.sign_in
 
         project.visit!
+      end
 
+      it 'can load Threat Monitoring page and view the policy alert list', testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/1786' do
         Page::Project::Menu.perform(&:click_on_threat_monitoring)
 
         EE::Page::Project::ThreatMonitoring::AlertsList.perform do |alerts_list|
