01676fbe260b9aceaf5799edd968802875fbe4e3
Sean Arnold <sarnold@gitlab.com>
2021-05-31 11:37:14 +0000

    Limit rotations when removing members to those accessible to the member

--- ee/app/models/ee/members_preloader.rb
+++ ee/app/models/ee/members_preloader.rb
@@ -11,6 +11,7 @@ module EE
       users = members.map(&:user)
       ActiveRecord::Associations::Preloader.new.preload(users, group_saml_identities: :saml_provider)
       ActiveRecord::Associations::Preloader.new.preload(users, oncall_participants: { rotation: :schedule })
+      ActiveRecord::Associations::Preloader.new.preload(members, user: :oncall_schedules)
     end
   end
 end
