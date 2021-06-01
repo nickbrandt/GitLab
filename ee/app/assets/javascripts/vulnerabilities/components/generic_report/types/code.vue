fadd6e5e3bf17fc40e609e94c4be24213e735086
Dave Pisek <dpisek@gitlab.com>
2021-06-01 11:13:58 +1000

    Show generic 'code' type on vuln details
    Adds a new Vue component to render the generic vulnerability report's
    'code' type on both the vulnerability details page and the pipeline
    security modal.

---
+++ ee/app/assets/javascripts/vulnerabilities/components/generic_report/types/code.vue
@@ -0,0 +1,18 @@
+<script>
+import CodeBlock from '~/vue_shared/components/code_block.vue';
+
+export default {
+  components: {
+    CodeBlock,
+  },
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+<template>
+  <code-block :code="value" />
+</template>
--- ee/app/assets/javascripts/vulnerabilities/components/generic_report/types/constants.js
+++ ee/app/assets/javascripts/vulnerabilities/components/generic_report/types/constants.js
@@ -10,6 +10,7 @@ export const REPORT_TYPES = {
   moduleLocation: 'module-location',
   fileLocation: 'file-location',
   table: 'table',
+  code: 'code',
 };
 
 const REPORT_TYPE_TO_COMPONENT_MAP = {
@@ -22,6 +23,7 @@ const REPORT_TYPE_TO_COMPONENT_MAP = {
   [REPORT_TYPES.moduleLocation]: () => import('./module_location.vue'),
   [REPORT_TYPES.fileLocation]: () => import('./file_location.vue'),
   [REPORT_TYPES.table]: () => import('./table.vue'),
+  [REPORT_TYPES.code]: () => import('./code.vue'),
 };
 
 export const getComponentNameForType = (reportType) =>
