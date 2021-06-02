a7337266dd61ced6f27d1763d4e890c359401288
pburdette <pburdette@gitlab.com>
2021-06-02 14:55:51 -0400

    Add danger for merge trains
    If pipeline failed for merge train
    show red danger button.
    
    Changelog: added

--- ee/spec/frontend/vue_mr_widget/components/states/mr_widget_ready_to_merge_spec.js
+++ ee/spec/frontend/vue_mr_widget/components/states/mr_widget_ready_to_merge_spec.js
@@ -86,6 +86,8 @@ describe('ReadyToMerge', () => {
     findMergeTrainHelperText().find('[data-testid="pipeline-link"]');
   const findMergeTrainDocumentationLink = () =>
     findMergeTrainHelperText().find('[data-testid="documentation-link"]');
+  const findFailedPipelineMergeTrainText = () =>
+    wrapper.find('[data-testid="failed-pipeline-merge-train-text"]');
 
   afterEach(() => {
     if (wrapper?.destroy) {
@@ -421,6 +423,34 @@ describe('ReadyToMerge', () => {
 
     it('should show a custom message that explains the conflict', () => {
       expect(findPipelineConflictMessage().text()).toBe(PIPELINE_MUST_SUCCEED_CONFLICT_TEXT);
+    });
+  });
+
+  describe('Merge button variant', () => {
+    it('danger variant and failed text should show if pipeline failed', () => {
+      factory({
+        isPipelineFailed: true,
+        preferredAutoMergeStrategy: MT_MERGE_STRATEGY,
+        availableAutoMergeStrategies: [MT_MERGE_STRATEGY],
+        hasCI: true,
+        onlyAllowMergeIfPipelineSucceeds: false,
+      });
+
+      expect(findMergeButton().attributes('variant')).toBe('danger');
+      expect(findFailedPipelineMergeTrainText().exists()).toBe(true);
+    });
+
+    it('confirm variant and failed text should not show if pipeline passed', () => {
+      factory({
+        preferredAutoMergeStrategy: MT_MERGE_STRATEGY,
+        availableAutoMergeStrategies: [MT_MERGE_STRATEGY],
+        hasCI: true,
+        onlyAllowMergeIfPipelineSucceeds: false,
+        ciStatus: 'success',
+      });
+
+      expect(findMergeButton().attributes('variant')).toBe('confirm');
+      expect(findFailedPipelineMergeTrainText().exists()).toBe(false);
     });
   });
 });
