a52e7d803bf76c87a287042c8aa830ae4d727560
Jason Goodman <jgoodman@gitlab.com>
2021-06-03 05:19:58 +0000

    Add Prevent Sharing Groups Column to Namespace Settings Table

--- db/structure.sql
+++ db/structure.sql
@@ -14954,6 +14954,7 @@ CREATE TABLE namespace_settings (
     delayed_project_removal boolean,
     resource_access_token_creation_allowed boolean DEFAULT true NOT NULL,
     lock_delayed_project_removal boolean DEFAULT false NOT NULL,
+    prevent_sharing_groups_outside_hierarchy boolean DEFAULT false NOT NULL,
     CONSTRAINT check_0ba93c78c7 CHECK ((char_length(default_branch_name) <= 255))
 );
 
