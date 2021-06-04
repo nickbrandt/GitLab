22e2f903c821e54ce6d4b4b749a009d14abc4a13
Tan Le <tle@gitlab.com>
2021-06-04 14:40:42 +1000

    Fix XSS on audit log for feature flag actions
    An adversary can craft a malicious link in the feature flag
    description. This action creates an audit event which is then presented
    to an administrator. Clicking on the link will grant admin role to the
    adversary.
    
    This change ensures that:
    
    - HTML tags are removed before rendering the audit log actions
    - HTML tags are removed in feature flag related audit event messages
    - HTML tags are removed when saving `custom_message` in audit events
    
    Changelog: security

--- ee/spec/frontend/audit_events/components/table_cells/html_table_cell_spec.js
+++ ee/spec/frontend/audit_events/components/table_cells/html_table_cell_spec.js
@@ -3,21 +3,13 @@ import { shallowMount } from '@vue/test-utils';
 import HtmlTableCell from 'ee/audit_events/components/table_cells/html_table_cell.vue';
 
 describe('HtmlTableCell component', () => {
-  it('should show <strong> tags if present in the HTML provided', () => {
-    const html = '<strong>Test HTML</strong>';
-    const wrapper = shallowMount(HtmlTableCell, {
-      propsData: { html },
-    });
-
-    expect(wrapper.html()).toBe(`<span>${html}</span>`);
-  });
-
   it('should not include tags that are not in the allowed list but should keep the content', () => {
-    const html = '<a href="https://magic.url/">Link</a> <i>Test</i> <h1>HTML</h1>';
+    const html =
+      '<a href="https://magic.url/">Link</a> <i>Test</i> <h1>HTML</h1> <strong>Flex</strong>';
     const wrapper = shallowMount(HtmlTableCell, {
       propsData: { html },
     });
 
-    expect(wrapper.html()).toBe('<span>Link Test HTML</span>');
+    expect(wrapper.html()).toBe('<span>Link Test HTML Flex</span>');
   });
 });
