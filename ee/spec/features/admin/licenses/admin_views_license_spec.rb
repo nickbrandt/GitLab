bfecbf6317986affee244438e47e594070b3919c
Vitaly Slobodin <vslobodin@gitlab.com>
2021-06-07 14:09:31 +0300

    Add feature test to License view

--- ee/spec/features/admin/licenses/admin_views_license_spec.rb
+++ ee/spec/features/admin/licenses/admin_views_license_spec.rb
@@ -150,4 +150,50 @@ RSpec.describe "Admin views license" do
       end
     end
   end
+
+  describe 'qrtly reconciliation alert', :js do
+    shared_examples 'a visible alert' do
+      it 'displays an alert' do
+        expect(page).to have_selector('[data-testid="qrtly-reconciliation-alert"]')
+      end
+    end
+
+    shared_examples 'a hidden alert' do
+      it 'does not display an alert' do
+        expect(page).not_to have_selector('[data-testid="qrtly-reconciliation-alert"]')
+      end
+    end
+
+    context 'on dotcom' do
+      before do
+        allow(Gitlab).to receive(:com?).and_return(true)
+        visit(admin_license_path)
+      end
+
+      it_behaves_like 'a hidden alert'
+    end
+
+    context 'on self-managed' do
+      before do
+        allow(Gitlab).to receive(:ee?).and_return(true)
+      end
+
+      context 'when qrtly reconciliation is available' do
+        before do
+          create(:upcoming_reconciliation, :self_managed)
+          visit(admin_license_path)
+        end
+
+        it_behaves_like 'a visible alert'
+      end
+
+      context 'when qrtly reconciliation is not available' do
+        before do
+          visit(admin_license_path)
+        end
+
+        it_behaves_like 'a hidden alert'
+      end
+    end
+  end
 end
