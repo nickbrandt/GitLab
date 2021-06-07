ee9b0c0ccc14975faa3a7c799e49dcf331f34c51
Fabio Pitino <fpitino@gitlab.com>
2021-06-07 17:12:28 +0100

    Add project setting to toggle job token scope
    Add column job_token_scope_enabled to ProjectCiCdSetting.
    Use setting when checking inclusion of project in scope.
    
    Changelog: added

--- spec/factories/ci/job_token/scope_links.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :ci_job_token_scope_link, class: 'Ci::JobToken::ScopeLink' do
-    source_project factory: :project
-    target_project factory: :project
-    added_by factory: :user
-  end
-end
