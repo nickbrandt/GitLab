089a4ef65399e2ac02e73bcc98ebbbb61029ea47
Vitaly Slobodin <vslobodin@gitlab.com>
2021-06-08 10:17:38 +0300

    Add unit tests for BillingAddress

--- ee/spec/frontend/vue_shared/purchase_flow/spec_helper.js
+++ ee/spec/frontend/vue_shared/purchase_flow/spec_helper.js
@@ -1,10 +1,11 @@
+import { merge } from 'lodash';
 import activeStepQuery from 'ee/vue_shared/purchase_flow/graphql/queries/active_step.query.graphql';
 import stepListQuery from 'ee/vue_shared/purchase_flow/graphql/queries/step_list.query.graphql';
 import resolvers from 'ee/vue_shared/purchase_flow/graphql/resolvers';
 import createMockApollo from 'helpers/mock_apollo_helper';
 
-export function createMockApolloProvider(stepList, initialStepIndex = 0) {
-  const mockApollo = createMockApollo([], resolvers);
+export function createMockApolloProvider(stepList, initialStepIndex = 0, additionalResolvers = {}) {
+  const mockApollo = createMockApollo([], merge({}, resolvers, additionalResolvers));
   mockApollo.clients.defaultClient.cache.writeQuery({
     query: stepListQuery,
     data: { stepList },
