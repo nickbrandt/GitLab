855c03a08c0a63fdb2c61d770620fb6918d63f7d
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2021-06-10 13:50:11 +0000

    Add a new line to separate the setup and exercise phases

--- ee/spec/lib/ee/api/entities/ci/minutes/additional_pack_spec.rb
+++ ee/spec/lib/ee/api/entities/ci/minutes/additional_pack_spec.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 RSpec.describe ::EE::API::Entities::Ci::Minutes::AdditionalPack do
   it 'contains the correct attributes', :aggregate_failures do
     pack = build(:ci_minutes_additional_pack)
+
     entity = described_class.new(pack).as_json
 
     expect(entity[:expires_at]).to eq pack.expires_at
