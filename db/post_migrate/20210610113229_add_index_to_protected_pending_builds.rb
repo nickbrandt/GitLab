2b458201ccc2b27d0d1d2c12fcbd8e6bdfd6b377
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2021-06-11 12:28:38 +0200

    Index protected pending builds only partially

--- db/post_migrate/20210610113229_add_index_to_protected_pending_builds.rb
+++ db/post_migrate/20210610113229_add_index_to_protected_pending_builds.rb
@@ -3,12 +3,12 @@
 class AddIndexToProtectedPendingBuilds < ActiveRecord::Migration[6.1]
   include Gitlab::Database::MigrationHelpers
 
-  INDEX_NAME = 'index_ci_pending_builds_on_protected'
+  INDEX_NAME = 'index_ci_pending_builds_id_on_protected_partial'
 
   disable_ddl_transaction!
 
   def up
-    add_concurrent_index :ci_pending_builds, :protected, name: INDEX_NAME
+    add_concurrent_index :ci_pending_builds, :id, where: 'protected = true', name: INDEX_NAME
   end
 
   def down
