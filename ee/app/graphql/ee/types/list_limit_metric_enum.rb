f86dfc014e03645980c64e7340b3cd5d3461f030
Jonathan Schafer <jschafer@gitlab.com>
2021-06-11 09:27:36 -0500

    Add descriptions for ListLimitMetricEnum

--- ee/app/graphql/ee/types/list_limit_metric_enum.rb
+++ ee/app/graphql/ee/types/list_limit_metric_enum.rb
@@ -6,9 +6,9 @@ module EE
       graphql_name 'ListLimitMetric'
       description 'List limit metric setting'
 
-      value 'all_metrics'
-      value 'issue_count'
-      value 'issue_weights'
+      value 'all_metrics', description: 'Limit list by number and total weight of issues.'
+      value 'issue_count', description: 'Limit list by number of issues.'
+      value 'issue_weights', description: 'Limit list by total weight of issues.'
     end
   end
 end
