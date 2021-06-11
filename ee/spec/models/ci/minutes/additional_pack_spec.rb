1647549088bed44b12388deb82828695dc290c7a
Vijay Hawoldar <vhawoldar@gitlab.com>
2021-06-11 12:26:07 +0100

    Add validation to additional pack purchase_xid
    For GitLab.com we should attempt to ensure the uniqueness
    of Ci::Minutes::AdditionalPack purchase_xid values
    
    Changelog: changed
    EE: true

--- ee/spec/models/ci/minutes/additional_pack_spec.rb
+++ ee/spec/models/ci/minutes/additional_pack_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe Ci::Minutes::AdditionalPack do
   end
 
   describe 'validations' do
+    subject(:additional_pack) { build(:ci_minutes_additional_pack) }
+
     it { is_expected.to validate_presence_of(:namespace) }
     it { is_expected.to validate_presence_of(:number_of_minutes) }
     it { is_expected.to validate_length_of(:purchase_xid).is_at_most(32) }
@@ -19,6 +21,7 @@ RSpec.describe Ci::Minutes::AdditionalPack do
 
       it { is_expected.to validate_presence_of(:expires_at) }
       it { is_expected.to validate_presence_of(:purchase_xid) }
+      it { is_expected.to validate_uniqueness_of(:purchase_xid) }
     end
 
     context 'when self-managed' do
@@ -28,6 +31,7 @@ RSpec.describe Ci::Minutes::AdditionalPack do
 
       it { is_expected.not_to validate_presence_of(:purchase_xid) }
       it { is_expected.not_to validate_presence_of(:expires_at) }
+      it { is_expected.not_to validate_uniqueness_of(:purchase_xid) }
     end
   end
 end
