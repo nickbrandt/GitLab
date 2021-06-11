08224b499634f96617eb38e054109c945352d72c
Tan Le <tle@gitlab.com>
2021-06-11 11:18:06 +1000

    Convert SAML module to class

--- ee/app/services/group_saml/saml_provider/create_service.rb
+++ ee/app/services/group_saml/saml_provider/create_service.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
+require_dependency 'group_saml/saml_provider/base_service'
+
 module GroupSaml
   module SamlProvider
-    class CreateService
-      include BaseService
-
+    class CreateService < BaseService
       def initialize(current_user, group, params:)
         @group = group
         super(current_user, group.build_saml_provider, params: params)
