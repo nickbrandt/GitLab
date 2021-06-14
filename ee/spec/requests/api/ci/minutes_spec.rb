d8d3b217f72f75a3332163d2ac4a90be00621492
Vijay Hawoldar <vhawoldar@gitlab.com>
2021-06-14 12:02:42 +0100

    Allow changing AdditionalPack namespace
    To support a customer changing a the namespace for their
    purchased additional Ci minute packs, this commit will add
    a new REST API endpoint and service
    
    Changelog: added
    EE: true

--- ee/spec/requests/api/ci/minutes_spec.rb
+++ ee/spec/requests/api/ci/minutes_spec.rb
@@ -3,6 +3,14 @@
 require 'spec_helper'
 
 RSpec.describe API::Ci::Minutes do
+  shared_examples 'not found error' do
+    it 'returns an error' do
+      subject
+
+      expect(response).to have_gitlab_http_status(:not_found)
+    end
+  end
+
   describe 'POST /namespaces/:id/minutes' do
     let_it_be(:namespace) { create(:namespace) }
     let_it_be(:user) { create(:user) }
@@ -32,11 +40,7 @@ RSpec.describe API::Ci::Minutes do
       context 'when the namespace cannot be found' do
         let(:namespace_id) { non_existing_record_id }
 
-        it 'returns an error' do
-          post_minutes
-
-          expect(response).to have_gitlab_http_status(:not_found)
-        end
+        it_behaves_like 'not found error'
       end
 
       context 'when the additional pack does not exist' do
@@ -74,6 +78,95 @@ RSpec.describe API::Ci::Minutes do
 
           expect(response).to have_gitlab_http_status(:bad_request)
           expect(response.body).to include('Unable to save additional pack')
+        end
+      end
+    end
+  end
+
+  describe 'PATCH /namespaces/:id/minutes/move/:target_id' do
+    let_it_be(:namespace) { create(:group) }
+    let_it_be(:target) { create(:group, owner: namespace.owner) }
+    let_it_be(:unrelated_target) { create(:group) }
+    let_it_be(:child_namespace) { create(:group, :nested) }
+    let_it_be(:user) { create(:user) }
+
+    let(:namespace_id) { namespace.id }
+    let(:target_id) { target.id }
+
+    before do
+      namespace.add_owner(user)
+      target.add_owner(user)
+    end
+
+    subject(:move_packs) { patch api("/namespaces/#{namespace_id}/minutes/move/#{target_id}", user) }
+
+    context 'when unauthorized' do
+      it 'returns an error' do
+        move_packs
+
+        expect(response).to have_gitlab_http_status(:forbidden)
+      end
+    end
+
+    context 'when authorized' do
+      let_it_be(:user) { create(:admin) }
+
+      context 'when the namespace cannot be found' do
+        let(:namespace_id) { non_existing_record_id }
+
+        it_behaves_like 'not found error'
+      end
+
+      context 'when the target namespace cannot be found' do
+        let(:target_id) { non_existing_record_id }
+
+        it_behaves_like 'not found error'
+      end
+
+      context 'when the namespace is not a top-level namespace' do
+        let(:namespace_id) { child_namespace.id }
+
+        it 'returns an error' do
+          move_packs
+
+          expect(response).to have_gitlab_http_status(:bad_request)
+          expect(response.body).to include('Namespace must be a top-level namespace')
+        end
+      end
+
+      context 'when the target namespace is not a top-level namespace' do
+        let(:target_id) { child_namespace.id }
+
+        it 'returns an error' do
+          move_packs
+
+          expect(response).to have_gitlab_http_status(:bad_request)
+          expect(response.body).to include('Target namespace must be a top-level namespace')
+        end
+      end
+
+      context 'when the target namespace is not owned by the same user' do
+        let(:target_id) { unrelated_target.id }
+
+        it 'returns an error' do
+          move_packs
+
+          expect(response).to have_gitlab_http_status(:bad_request)
+          expect(response.body).to include('Both namespaces must share the same owner')
+        end
+      end
+
+      context 'when the transfer is successful' do
+        let(:target_id) { target.id }
+
+        before do
+          create(:ci_minutes_additional_pack, namespace: namespace)
+        end
+
+        it 'moves the packs and returns an accepted response' do
+          expect { move_packs }.to change(target.ci_minutes_additional_packs, :count).by(1)
+
+          expect(response).to have_gitlab_http_status(:accepted)
         end
       end
     end
