1537a8a3f7ca958984ec590c566b1e2725fcbf61
Michael Eddington <meddington@gitlab.com>
2021-06-14 21:18:47 +0000

    Promote unstable API Fuzzing CI template to stable (breaking)

--- ee/spec/lib/ee/gitlab/ci/templates/templates_spec.rb
+++ ee/spec/lib/ee/gitlab/ci/templates/templates_spec.rb
@@ -4,12 +4,9 @@ require 'spec_helper'
 
 RSpec.describe "CI YML Templates" do
   using RSpec::Parameterized::TableSyntax
-
   subject { Gitlab::Ci::YamlProcessor.new(content).execute }
 
-  where(:template_name) do
-    Gitlab::Template::GitlabCiYmlTemplate.all.map(&:full_name)
-  end
+  let(:all_templates) { Gitlab::Template::GitlabCiYmlTemplate.all.map(&:full_name) }
 
   before do
     stub_feature_flags(
@@ -19,27 +16,24 @@ RSpec.describe "CI YML Templates" do
       redirect_to_latest_template_jobs_browser_performance_testing: false)
   end
 
-  with_them do
-    let(:content) do
-      if template_name == 'Security/DAST-API.gitlab-ci.yml'
-        # The DAST-API template purposly excludes a stages
-        # definition.
+  shared_examples 'require default stages to be included' do
+    it 'require default stages to be included' do
+      expect(subject.stages).to include(*Gitlab::Ci::Config::Entry::Stages.default)
+    end
+  end
 
-        <<~EOS
-          include:
-            - template: #{template_name}
+  context 'that support autodevops' do
+    non_autodevops_templates = [
+      'Security/DAST-API.gitlab-ci.yml',
+      'Security/API-Fuzzing.gitlab-ci.yml'
+    ]
 
-          stages:
-            - build
-            - test
-            - deploy
-            - dast
+    where(:template_name) do
+      all_templates - non_autodevops_templates
+    end
 
-          concrete_build_implemented_by_a_user:
-            stage: test
-            script: do something
-        EOS
-      else
+    with_them do
+      let(:content) do
         <<~EOS
           include:
             - template: #{template_name}
@@ -49,14 +43,104 @@ RSpec.describe "CI YML Templates" do
             script: do something
         EOS
       end
+
+      it 'are valid with default stages' do
+        expect(subject).to be_valid
+      end
+
+      include_examples 'require default stages to be included'
+    end
+  end
+
+  context 'that do not support autodevops' do
+    context 'when DAST API template' do
+      # The DAST API template purposly excludes a stages
+      # definition.
+
+      let(:template_name) { 'Security/DAST-API.gitlab-ci.yml' }
+
+      context 'with default stages' do
+        let(:content) do
+          <<~EOS
+            include:
+              - template: #{template_name}
+
+            concrete_build_implemented_by_a_user:
+              stage: test
+              script: do something
+          EOS
+        end
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'with defined stages' do
+        let(:content) do
+          <<~EOS
+            include:
+              - template: #{template_name}
+
+            stages:
+              - build
+              - test
+              - deploy
+              - dast
+
+            concrete_build_implemented_by_a_user:
+              stage: test
+              script: do something
+          EOS
+        end
+
+        it { is_expected.to be_valid }
+
+        include_examples 'require default stages to be included'
+      end
     end
 
-    it 'is valid' do
-      expect(subject).to be_valid
-    end
+    context 'when API Fuzzing template' do
+      # The API Fuzzing template purposly excludes a stages
+      # definition.
 
-    it 'require default stages to be included' do
-      expect(subject.stages).to include(*Gitlab::Ci::Config::Entry::Stages.default)
+      let(:template_name) { 'Security/API-Fuzzing.gitlab-ci.yml' }
+
+      context 'with default stages' do
+        let(:content) do
+          <<~EOS
+            include:
+              - template: #{template_name}
+
+            concrete_build_implemented_by_a_user:
+              stage: test
+              script: do something
+          EOS
+        end
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'with defined stages' do
+        let(:content) do
+          <<~EOS
+            include:
+              - template: #{template_name}
+
+            stages:
+              - build
+              - test
+              - deploy
+              - fuzz
+
+            concrete_build_implemented_by_a_user:
+              stage: test
+              script: do something
+          EOS
+        end
+
+        it { is_expected.to be_valid }
+
+        include_examples 'require default stages to be included'
+      end
     end
   end
 end
