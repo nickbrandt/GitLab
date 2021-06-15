86573c3fb3122535e8597ed85169a1d96d44fb44
Francisco Javier López <fjlopez@gitlab.com>
2021-06-15 19:37:21 +0200

    Fix CI/CD > Pipelines active routes
    In this commit we add more route to indicate when
    the `CI/CD > Pipelines` menu item should be active.
    
    Changelog: fixed

--- ee/spec/features/projects/active_tabs_spec.rb
+++ ee/spec/features/projects/active_tabs_spec.rb
@@ -3,17 +3,11 @@
 require 'spec_helper'
 
 RSpec.describe 'Project active tab' do
-  let(:user) { create :user }
-  let(:project) { create(:project, :repository) }
+  let_it_be(:project) { create(:project, :repository) }
 
-  def click_tab(title)
-    page.within '.sidebar-top-level-items > .active' do
-      click_link(title)
-    end
-  end
+  let(:user) { project.owner }
 
   before do
-    project.add_maintainer(user)
     sign_in(user)
   end
 
@@ -37,5 +31,38 @@ RSpec.describe 'Project active tab' do
 
     it_behaves_like 'page has active tab', _('Analytics')
     it_behaves_like 'page has active sub tab', _('Code Review')
+  end
+
+  context 'on project CI/CD' do
+    context 'browsing Pipelines tabs' do
+      let_it_be(:pipeline) { create(:ci_pipeline, project: project) }
+
+      context 'Security tab' do
+        before do
+          visit security_project_pipeline_path(project, pipeline)
+        end
+
+        it_behaves_like 'page has active tab', _('CI/CD')
+        it_behaves_like 'page has active sub tab', _('Pipelines')
+      end
+
+      context 'Licenses tab' do
+        before do
+          visit licenses_project_pipeline_path(project, pipeline)
+        end
+
+        it_behaves_like 'page has active tab', _('CI/CD')
+        it_behaves_like 'page has active sub tab', _('Pipelines')
+      end
+
+      context 'Code Quality tab' do
+        before do
+          visit codequality_report_project_pipeline_path(project, pipeline)
+        end
+
+        it_behaves_like 'page has active tab', _('CI/CD')
+        it_behaves_like 'page has active sub tab', _('Pipelines')
+      end
+    end
   end
 end
