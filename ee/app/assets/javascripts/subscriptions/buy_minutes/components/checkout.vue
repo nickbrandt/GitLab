20ef8174c3bfbba96b35635a5f5b5fcc60f30033
Vitaly Slobodin <vslobodin@gitlab.com>
2021-06-15 13:58:12 +0300

    Create a copy of PaymentMethod cmp with GQL

--- ee/app/assets/javascripts/subscriptions/buy_minutes/components/checkout.vue
+++ ee/app/assets/javascripts/subscriptions/buy_minutes/components/checkout.vue
@@ -1,13 +1,14 @@
 <script>
 import ProgressBar from 'ee/registrations/components/progress_bar.vue';
 import { STEPS, SUBSCRIPTON_FLOW_STEPS } from 'ee/registrations/constants';
-import STATE_QUERY from 'ee/subscriptions/graphql/queries/state.query.graphql';
+import stateQuery from 'ee/subscriptions/graphql/queries/state.query.graphql';
 import { s__ } from '~/locale';
 import BillingAddress from './checkout/billing_address.vue';
 import SubscriptionDetails from './checkout/subscription_details.vue';
+import PaymentMethod from './checkout/payment_method.vue';
 
 export default {
-  components: { ProgressBar, SubscriptionDetails, BillingAddress },
+  components: { ProgressBar, SubscriptionDetails, BillingAddress, PaymentMethod },
   props: {
     plans: {
       type: Array,
@@ -16,7 +17,7 @@ export default {
   },
   apollo: {
     isNewUser: {
-      query: STATE_QUERY,
+      query: stateQuery,
     },
   },
   currentStep: STEPS.checkout,
@@ -37,6 +38,7 @@ export default {
       <h2 class="gl-mt-6 gl-mb-7 gl-mb-lg-5">{{ $options.i18n.checkout }}</h2>
       <subscription-details :plans="plans" />
       <billing-address />
+      <payment-method />
     </div>
   </div>
 </template>
