7c30991b53efb1478c06cbcee1ea80625a2d3449
Brandon Labuschagne <blabuschagne@gitlab.com>
2021-06-18 11:02:04 +0200

    Remove add button from Devops Adoption
    Along with this, remove all of the dormant
    code associated to the devops adoption
    add / remove modal
    
    Changelog: fixed
    EE: true

--- ee/spec/frontend/analytics/devops_report/devops_adoption/components/devops_adoption_empty_state_spec.js
+++ ee/spec/frontend/analytics/devops_report/devops_adoption/components/devops_adoption_empty_state_spec.js
@@ -1,10 +1,7 @@
-import { GlEmptyState, GlButton } from '@gitlab/ui';
-import { shallowMount, mount } from '@vue/test-utils';
+import { GlEmptyState } from '@gitlab/ui';
+import { shallowMount } from '@vue/test-utils';
 import DevopsAdoptionEmptyState from 'ee/analytics/devops_report/devops_adoption/components/devops_adoption_empty_state.vue';
-import {
-  DEVOPS_ADOPTION_STRINGS,
-  DEVOPS_ADOPTION_SEGMENT_MODAL_ID,
-} from 'ee/analytics/devops_report/devops_adoption/constants';
+import { DEVOPS_ADOPTION_STRINGS } from 'ee/analytics/devops_report/devops_adoption/constants';
 
 const emptyStateSvgPath = 'illustrations/monitoring/getting_started.svg';
 
@@ -12,9 +9,9 @@ describe('DevopsAdoptionEmptyState', () => {
   let wrapper;
 
   const createComponent = (options = {}) => {
-    const { stubs = {}, props = {}, func = shallowMount } = options;
+    const { stubs = {}, props = {} } = options;
 
-    return func(DevopsAdoptionEmptyState, {
+    return shallowMount(DevopsAdoptionEmptyState, {
       provide: {
         emptyStateSvgPath,
       },
@@ -27,7 +24,6 @@ describe('DevopsAdoptionEmptyState', () => {
   };
 
   const findEmptyState = () => wrapper.find(GlEmptyState);
-  const findEmptyStateAction = () => findEmptyState().find(GlButton);
 
   afterEach(() => {
     wrapper.destroy();
@@ -47,44 +43,5 @@ describe('DevopsAdoptionEmptyState', () => {
 
     expect(emptyState.props('title')).toBe(DEVOPS_ADOPTION_STRINGS.emptyState.title);
     expect(emptyState.props('description')).toBe(DEVOPS_ADOPTION_STRINGS.emptyState.description);
-  });
-
-  describe('action button', () => {
-    it('displays an overridden action button', () => {
-      wrapper = createComponent({ stubs: { GlEmptyState } });
-
-      const actionButton = findEmptyStateAction();
-
-      expect(actionButton.exists()).toBe(true);
-      expect(actionButton.text()).toBe(DEVOPS_ADOPTION_STRINGS.emptyState.button);
-    });
-
-    it('is enabled when there is group data', () => {
-      wrapper = createComponent({ stubs: { GlEmptyState } });
-
-      const actionButton = findEmptyStateAction();
-
-      expect(actionButton.props('disabled')).toBe(false);
-    });
-
-    it('is disabled when there is no group data', () => {
-      wrapper = createComponent({ stubs: { GlEmptyState }, props: { hasGroupsData: false } });
-
-      const actionButton = findEmptyStateAction();
-
-      expect(actionButton.props('disabled')).toBe(true);
-    });
-
-    it('calls the gl-modal show', async () => {
-      wrapper = createComponent({ func: mount });
-
-      const actionButton = findEmptyStateAction();
-      const rootEmit = jest.spyOn(wrapper.vm.$root, '$emit');
-
-      actionButton.trigger('click');
-
-      expect(rootEmit.mock.calls[0][0]).toContain('show');
-      expect(rootEmit.mock.calls[0][1]).toBe(DEVOPS_ADOPTION_SEGMENT_MODAL_ID);
-    });
   });
 });
