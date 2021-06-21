bd50783809194e48e91148d1f13dcb4cc1de0711
Max Woolf <mwoolf@gitlab.com>
2021-06-21 15:58:09 +0100

    Refactor expired SSH key error messages
    Updates the profiles_helper to rely on model logic
    to display certain SSH expiration messages in EE.
    
    EE: true

--- ee/spec/models/ee/key_spec.rb
+++ ee/spec/models/ee/key_spec.rb
@@ -24,6 +24,33 @@ RSpec.describe Key do
     end
   end
 
+  describe 'only_expired_and_enforced?' do
+    using RSpec::Parameterized::TableSyntax
+
+    where(:expired, :enforced, :valid, :result) do
+      true | true | true | true
+      true | false | true | false
+      false | true | true | false
+      true | true | false | true
+      false | false | true | false
+    end
+
+    with_them do
+      before do
+        stub_licensed_features(enforce_ssh_key_expiration: true)
+        stub_ee_application_setting(enforce_ssh_key_expiration: enforced)
+      end
+
+      it 'checks if ssh key expiration is enforced' do
+        key = create(:personal_key)
+        key.update_attribute(:expires_at, 3.days.ago) if expired
+        key.update_attribute(:title, nil) unless valid
+
+        expect(key.only_expired_and_enforced?).to be(result)
+      end
+    end
+  end
+
   describe '.expiration_enforced?' do
     using RSpec::Parameterized::TableSyntax
 
