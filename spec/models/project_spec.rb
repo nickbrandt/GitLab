d4322db97a7a2a6edf338fb161326bd7f3191c4e
Mark Lapierre <mlapierre@gitlab.com>
2021-06-23 11:07:05 +1000

    Skip attributes defined in EE code

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -658,7 +658,16 @@ RSpec.describe Project, factory_default: :keep do
     it { is_expected.to delegate_method(:container_registry_enabled?).to(:project_feature) }
     it { is_expected.to delegate_method(:container_registry_access_level).to(:project_feature) }
 
-    include_examples 'ci_cd_settings delegation'
+    include_examples 'ci_cd_settings delegation' do
+      # Skip attributes defined in EE code
+      let(:exclude_attributes) do
+        %w(
+          merge_pipelines_enabled
+          merge_trains_enabled
+          auto_rollback_enabled
+        )
+      end
+    end
 
     describe '#ci_forward_deployment_enabled?' do
       it_behaves_like 'a ci_cd_settings predicate method', prefix: 'ci_' do
--- spec/support/shared_examples/models/project_ci_cd_settings_shared_examples.rb
+++ spec/support/shared_examples/models/project_ci_cd_settings_shared_examples.rb
@@ -4,7 +4,7 @@ RSpec.shared_examples 'ci_cd_settings delegation' do
   context 'when ci_cd_settings is destroyed but project is not' do
     it 'allows methods delegated to ci_cd_settings to be nil', :aggregate_failures do
       project = create(:project)
-      attributes = project.ci_cd_settings.attributes.keys - %w(id project_id)
+      attributes = project.ci_cd_settings.attributes.keys - %w(id project_id) - exclude_attributes
       project.ci_cd_settings.destroy
       project.reload
       attributes.each do |attr|
