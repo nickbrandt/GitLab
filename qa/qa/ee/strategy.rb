41a057c584d049bbc8ac2a960014c75414566663
Sanad Liaquat (Personal) <sliaquat@gitlab.com>
2021-06-23 18:47:09 +0500

    Use seperate retry for login page and license creation

--- qa/qa/ee/strategy.rb
+++ qa/qa/ee/strategy.rb
@@ -18,11 +18,13 @@ module QA
         end
 
         QA::Support::Retrier.retry_on_exception do
-          Page::Main::Menu.perform(&:sign_out_if_signed_in)
+          QA::Runtime::Browser.visit(:gitlab, QA::Page::Main::Login)
+        end
 
-          QA::Runtime::Browser.visit(:gitlab, QA::Page::Main::Login) do
-            EE::Resource::License.fabricate!(ENV['EE_LICENSE'])
-          end
+        QA::Support::Retrier.retry_on_exception do
+          QA::Page::Main::Menu.perform(&:sign_out_if_signed_in)
+
+          EE::Resource::License.fabricate!(ENV['EE_LICENSE'])
         end
       end
     end
