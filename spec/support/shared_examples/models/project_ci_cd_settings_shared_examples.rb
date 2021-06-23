7c174603cbf508fbcf2a8ceb79e47112c7e0deda
Mark Lapierre <mlapierre@gitlab.com>
2021-06-23 14:30:14 +1000

    Add undefined variable and use ! with destroy

--- spec/support/shared_examples/models/project_ci_cd_settings_shared_examples.rb
+++ spec/support/shared_examples/models/project_ci_cd_settings_shared_examples.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'ci_cd_settings delegation' do
+  let(:exclude_attributes) { [] }
+
   context 'when ci_cd_settings is destroyed but project is not' do
     it 'allows methods delegated to ci_cd_settings to be nil', :aggregate_failures do
       project = create(:project)
       attributes = project.ci_cd_settings.attributes.keys - %w(id project_id) - exclude_attributes
-      project.ci_cd_settings.destroy
+      project.ci_cd_settings.destroy!
       project.reload
       attributes.each do |attr|
         method = project.respond_to?("ci_#{attr}") ? "ci_#{attr}" : attr
