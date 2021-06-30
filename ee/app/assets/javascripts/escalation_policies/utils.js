744fb186c2902c9060021fa690ac3e207aadc25e
Tristan Read <tread@gitlab.com>
2021-06-30 18:08:16 +0000

    Convert minutes to seconds for escalation policies

--- ee/app/assets/javascripts/escalation_policies/utils.js
+++ ee/app/assets/javascripts/escalation_policies/utils.js
@@ -17,8 +17,33 @@ export const isNameFieldValid = (name) => {
 export const getRulesValidationState = (rules) => {
   return rules.map((rule) => {
     return {
-      isTimeValid: parseInt(rule.elapsedTimeSeconds, 10) >= 0,
+      isTimeValid: parseInt(rule.elapsedTimeMinutes, 10) >= 0,
       isScheduleValid: Boolean(rule.oncallScheduleIid),
     };
   });
 };
+
+/**
+ * Serializes a rule by converting elapsed minutes to seconds
+ * @param {Object} rule
+ *
+ * @returns {Object} rule
+ */
+export const serializeRule = ({ elapsedTimeMinutes, ...ruleParams }) => ({
+  ...ruleParams,
+  elapsedTimeSeconds: elapsedTimeMinutes * 60,
+});
+
+/**
+ * Parses a policy by converting elapsed seconds to minutes
+ * @param {Object} policy
+ *
+ * @returns {Object} policy
+ */
+export const parsePolicy = (policy) => ({
+  ...policy,
+  rules: policy.rules.map(({ elapsedTimeSeconds, ...ruleParams }) => ({
+    ...ruleParams,
+    elapsedTimeMinutes: elapsedTimeSeconds / 60,
+  })),
+});
