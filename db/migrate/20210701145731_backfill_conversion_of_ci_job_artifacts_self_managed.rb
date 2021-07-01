fff09f705ba27279e77b454caae04ef804ebad56
Alex Ives <aives@gitlab.com>
2021-07-01 10:03:27 -0500

    Ci_job_artifacts bigint conversion for self managed
    - In addition to gitlab.com, the bigint conversion has to
      run on self managed instances
    
    Relates to https://gitlab.com/gitlab-org/gitlab/issues/325615
    Changelog: other

---
+++ db/migrate/20210701145731_backfill_conversion_of_ci_job_artifacts_self_managed.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class BackfillConversionOfCiJobArtifactsSelfManaged < ActiveRecord::Migration[6.1]
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  def up
+    return unless should_run?
+
+    backfill_conversion_of_integer_to_bigint :ci_job_artifacts, %i(id job_id), batch_size: 15000, sub_batch_size: 100
+  end
+
+  def down
+    return unless should_run?
+
+    Gitlab::Database::BackgroundMigration::BatchedMigration
+      .where(job_class_name: 'CopyColumnUsingBackgroundMigrationJob')
+      .where(table_name: 'ci_job_artifacts', column_name: 'id')
+      .where(job_arguments: [%w[id job_id], %w[id_convert_to_bigint job_id_convert_to_bigint]].to_json)
+      .delete_all
+  end
+
+  private
+
+  def should_run?
+    # This already ran on Gitlab.com as part of 13.12 see
+    # db/post_migrate/20210420121149_backfill_conversion_of_ci_job_artifacts.rb
+    !(Gitlab.dev_or_test_env? || Gitlab.com?)
+  end
+end
