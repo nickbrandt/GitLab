85b40579c17d1f2c55b6f9673ffa76865864e67a
Doug Stull <dstull@gitlab.com>
2021-07-02 15:04:01 -0400

    Add helpful text to URL group validation and limit text
    - better form validation.
    
    Changelog: other
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/65369
    EE: true

--- spec/frontend/import_entities/import_groups/components/import_table_row_spec.js
+++ spec/frontend/import_entities/import_groups/components/import_table_row_spec.js
@@ -68,6 +68,7 @@ describe('import table row', () => {
       propsData: {
         availableNamespaces: availableNamespacesFixture,
         groupPathRegex: /.*/,
+        groupUrlErrorMessage: 'Please choose a group URL with no special characters or spaces.',
         ...props,
       },
     });
@@ -196,7 +197,9 @@ describe('import table row', () => {
         groupPathRegex: /^[a-zA-Z]+$/,
       });
 
-      expect(wrapper.text()).toContain('Please choose a group URL with no special characters.');
+      expect(wrapper.text()).toContain(
+        'Please choose a group URL with no special characters or spaces.',
+      );
     });
 
     it('reports invalid group name if relevant validation error exists', async () => {
