4318951005761ec37fccd06d266740c824d1f93d
Andrejs Cunskis <acunskis@gitlab.com>
2021-07-05 16:18:06 +0300

    Add github project import via api

--- qa/qa/resource/project_imported_from_github.rb
+++ qa/qa/resource/project_imported_from_github.rb
@@ -5,6 +5,8 @@ require 'securerandom'
 module QA
   module Resource
     class ProjectImportedFromGithub < Resource::Project
+      attribute :github_repo_id
+
       def fabricate!
         self.import = true
 
@@ -16,9 +18,33 @@ module QA
         end
 
         Page::Project::Import::Github.perform do |import_page|
-          import_page.add_personal_access_token(@github_personal_access_token)
-          import_page.import!(@github_repository_path, @name)
+          import_page.add_personal_access_token(github_personal_access_token)
+          import_page.import!(github_repository_path, name)
         end
+      end
+
+      def fabricate_via_api!
+        super
+      rescue ResourceURLMissingError
+        "#{Runtime::Scenario.gitlab_address}/#{group.full_path}/#{name}"
+      end
+
+      def api_post_path
+        '/import/github'
+      end
+
+      def api_post_body
+        {
+          repo_id: github_repo_id,
+          new_name: name,
+          target_namespace: group.full_path,
+          personal_access_token: github_personal_access_token,
+          ci_cd_only: false
+        }
+      end
+
+      def transform_api_resource(api_resource)
+        api_resource
       end
     end
   end
---
+++ qa/qa/specs/features/api/1_manage/import_github_repo_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module QA
+  RSpec.describe 'Manage', :github, :requires_admin do
+    describe 'Project import' do
+      let!(:api_client) { Runtime::API::Client.as_admin }
+      let!(:group) { Resource::Group.fabricate_via_api! { |resource| resource.api_client = api_client } }
+      let!(:user) do
+        Resource::User.fabricate_via_api! do |resource|
+          resource.api_client = api_client
+          resource.hard_delete_on_api_removal = true
+        end
+      end
+
+      let(:imported_project) do
+        Resource::ProjectImportedFromGithub.fabricate_via_api! do |project|
+          project.name = 'imported-project'
+          project.group = group
+          project.github_personal_access_token = Runtime::Env.github_access_token
+          project.github_repository_path = 'gitlab-qa-github/test-project'
+          project.api_client = api_client
+          project.github_repo_id = '310217317'
+        end
+      end
+
+      before do
+        group.add_member(user, Resource::Members::AccessLevel::MAINTAINER)
+      end
+
+      after do
+        user.remove_via_api!
+      end
+
+      it 'imports Github repo via api' do
+        imported_project.reload! # import the project and reload all fields
+
+        expect { imported_project.reload!.import_status }.to eventually_eq('finished').within(duration: 90)
+      end
+    end
+  end
+end
