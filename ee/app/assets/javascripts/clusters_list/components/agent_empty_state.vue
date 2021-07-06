06301ff327c7709fffa350548082d30c43fc664f
Anna Vovchenko <avovchenko@gitlab.com>
2021-07-06 12:19:05 +0000

    Empty state frontend

--- ee/app/assets/javascripts/clusters_list/components/agent_empty_state.vue
+++ ee/app/assets/javascripts/clusters_list/components/agent_empty_state.vue
@@ -1,5 +1,5 @@
 <script>
-import { GlButton, GlEmptyState, GlLink, GlSprintf } from '@gitlab/ui';
+import { GlButton, GlEmptyState, GlLink, GlSprintf, GlAlert } from '@gitlab/ui';
 
 export default {
   components: {
@@ -7,11 +7,25 @@ export default {
     GlEmptyState,
     GlLink,
     GlSprintf,
+    GlAlert,
   },
   props: {
     image: {
       type: String,
       required: true,
+    },
+    projectPath: {
+      type: String,
+      required: true,
+    },
+    hasConfigurations: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    repositoryPath() {
+      return `/${this.projectPath}`;
     },
   },
 };
@@ -21,9 +35,10 @@ export default {
   <gl-empty-state
     :svg-path="image"
     :title="s__('ClusterAgents|Integrate Kubernetes with a GitLab Agent')"
+    class="empty-state--agent"
   >
     <template #description>
-      <p>
+      <p class="mw-460 gl-mx-auto">
         <gl-sprintf
           :message="
             s__(
@@ -39,7 +54,7 @@ export default {
         </gl-sprintf>
       </p>
 
-      <p>
+      <p class="mw-460 gl-mx-auto">
         <gl-sprintf
           :message="
             s__(
@@ -57,10 +72,40 @@ export default {
           </template>
         </gl-sprintf>
       </p>
+
+      <gl-alert
+        v-if="!hasConfigurations"
+        variant="warning"
+        class="gl-mb-5 text-left"
+        :dismissible="false"
+      >
+        {{
+          s__(
+            'ClusterAgents|To install an Agent you should create an agent directory in the Repository first. We recommend that you add the Agent configuration to the directory before you start the installation process.',
+          )
+        }}
+
+        <template #actions>
+          <gl-button
+            category="primary"
+            variant="info"
+            href="https://docs.gitlab.com/ee/user/clusters/agent/#define-a-configuration-repository"
+            target="_blank"
+            class="gl-ml-0!"
+          >
+            {{ s__('ClusterAgents|Read more about getting started') }}
+          </gl-button>
+          <gl-button category="secondary" variant="info" :href="repositoryPath">
+            {{ s__('ClusterAgents|Go to the repository') }}
+          </gl-button>
+        </template>
+      </gl-alert>
     </template>
 
     <template #actions>
       <gl-button
+        :disabled="!hasConfigurations"
+        data-testid="integration-primary-button"
         category="primary"
         variant="success"
         href="https://docs.gitlab.com/ee/user/clusters/agent/#get-started-with-gitops-and-the-gitlab-agent"
