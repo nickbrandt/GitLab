cbee51e6c36f74d8a3e8a22949b1b8bb0330b747
Vitaly Slobodin <vslobodin@gitlab.com>
2021-07-06 18:26:54 +0300

    Revert "Merge branch 'health-widget-apollo' into 'master'"
    This reverts commit 9d839940dcab13a32ef09ab077a6f37ae16963d6, reversing
    changes made to e20ad592df0b93ad45b5bd4215c07a486fe11250.

--- ee/app/assets/javascripts/sidebar/components/status/sidebar_status.vue
+++ ee/app/assets/javascripts/sidebar/components/status/sidebar_status.vue
@@ -1,9 +1,8 @@
 <script>
-import produce from 'immer';
+import { mapGetters } from 'vuex';
 import createFlash from '~/flash';
-import { __, sprintf } from '~/locale';
+import { __ } from '~/locale';
 import { OPENED, REOPENED } from '~/notes/constants';
-import { healthStatusQueries } from '../../constants';
 import Status from './status.vue';
 
 export default {
@@ -11,134 +10,27 @@ export default {
     Status,
   },
   props: {
-    issuableType: {
-      type: String,
+    mediator: {
       required: true,
-    },
-    iid: {
-      type: String,
-      required: true,
-    },
-    fullPath: {
-      type: String,
-      required: true,
-    },
-    canUpdate: {
-      type: Boolean,
-      required: false,
-      default: false,
+      type: Object,
+      validator(mediatorObject) {
+        return Boolean(mediatorObject.store);
+      },
     },
   },
   computed: {
+    ...mapGetters(['getNoteableData']),
     isOpen() {
-      return this.issuableData?.state === OPENED || this.issuableData?.state === REOPENED;
-    },
-    isLoading() {
-      return this.$apollo.queries.issuableData.loading;
-    },
-    healthStatus() {
-      return this.issuableData?.healthStatus;
-    },
-  },
-  apollo: {
-    issuableData: {
-      query() {
-        return healthStatusQueries[this.issuableType].query;
-      },
-      variables() {
-        return {
-          fullPath: this.fullPath,
-          iid: this.iid,
-        };
-      },
-      update(data) {
-        return {
-          healthStatus: data.workspace?.issuable?.healthStatus,
-          state: data.workspace?.issuable?.state,
-        };
-      },
-      result({ data }) {
-        this.$emit('issuableData', {
-          healthStatus: data.workspace?.issuable?.healthStatus,
-          state: data.workspace?.issuable?.state,
-        });
-      },
-      error() {
-        createFlash({
-          message: sprintf(
-            __('Something went wrong while setting %{issuableType} health status.'),
-            {
-              issuableType: this.issuableType,
-            },
-          ),
-        });
-      },
+      return this.getNoteableData.state === OPENED || this.getNoteableData.state === REOPENED;
     },
   },
   methods: {
     handleDropdownClick(status) {
-      return this.$apollo
-        .mutate({
-          mutation: healthStatusQueries[this.issuableType].mutation,
-          variables: {
-            projectPath: this.fullPath,
-            iid: this.iid,
-            healthStatus: status,
-          },
-          update: (
-            _,
-            {
-              data: {
-                updateIssue: {
-                  issue: { healthStatus },
-                },
-              },
-            },
-          ) => {
-            const { defaultClient: client } = this.$apollo.provider.clients;
-
-            const queryVariables = { fullPath: this.fullPath, iid: this.iid };
-
-            const sourceData = client.readQuery({
-              query: healthStatusQueries[this.issuableType].query,
-              variables: queryVariables,
-            });
-
-            const data = produce(sourceData, (draftData) => {
-              draftData.workspace.issuable.healthStatus = healthStatus;
-            });
-
-            client.writeQuery({
-              query: healthStatusQueries[this.issuableType].query,
-              variables: queryVariables,
-              data,
-            });
-          },
-          optimisticResponse: {
-            __typename: 'Mutation', // eslint-disable-line @gitlab/require-i18n-strings
-            updateIssue: {
-              issue: {
-                healthStatus: status,
-                __typename: 'Issue', // eslint-disable-line @gitlab/require-i18n-strings
-              },
-              errors: [],
-              __typename: 'UpdateIssuePayload',
-            },
-          },
-        })
-        .then(({ data: { updateIssue } = {} } = {}) => {
-          const error = updateIssue?.errors[0];
-          if (error) {
-            createFlash({ message: error });
-          }
-        })
-        .catch(() => {
-          createFlash({
-            message: sprintf(__('Error occurred while updating the %{issuableType} status'), {
-              issuableType: this.issuableType,
-            }),
-          });
+      this.mediator.updateStatus(status).catch(() => {
+        createFlash({
+          message: __('Error occurred while updating the issue status'),
         });
+      });
     },
   },
 };
@@ -147,9 +39,9 @@ export default {
 <template>
   <status
     :is-open="isOpen"
-    :is-editable="canUpdate"
-    :is-fetching="isLoading"
-    :status="healthStatus"
+    :is-editable="mediator.store.editable"
+    :is-fetching="mediator.store.isFetching.status"
+    :status="mediator.store.status"
     @onDropdownClick="handleDropdownClick"
   />
 </template>
