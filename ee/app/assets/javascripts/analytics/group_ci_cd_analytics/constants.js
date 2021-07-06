672747385fb4dcbe3e522187ec72c1e4cf1d5ddd
Nathan Friend <nathan@gitlab.com>
2021-07-06 07:49:07 +0000

    Hide group-level DORA charts if not licensed
    This commit fixed a bug that allowed the DORA charts to render even if
    the DORA feature wasn't available.
    
    Changelog: fixed
    EE: true

--- ee/app/assets/javascripts/analytics/group_ci_cd_analytics/constants.js
+++ ee/app/assets/javascripts/analytics/group_ci_cd_analytics/constants.js
@@ -1,3 +1,1 @@
 export const STAT_ERROR_PLACEHOLDER = '-';
-
-export const TABS = ['release-statistics', 'deployment-frequency', 'lead-time'];
--- ee/spec/helpers/ee/graph_helper_spec.rb
+++ ee/spec/helpers/ee/graph_helper_spec.rb
@@ -4,16 +4,15 @@ require 'spec_helper'
 
 RSpec.describe EE::GraphHelper do
   let_it_be(:current_user) { create(:user) }
+  let_it_be_with_refind(:group) { create(:group) }
+  let_it_be(:project) { create(:project, :private) }
 
-  let(:project) { create(:project, :private) }
   let(:is_feature_licensed) { true }
   let(:is_user_authorized) { true }
 
   before do
     stub_licensed_features(dora4_analytics: is_feature_licensed)
     self.instance_variable_set(:@current_user, current_user)
-    self.instance_variable_set(:@project, project)
-    allow(self).to receive(:can?).with(current_user, :read_dora4_analytics, project).and_return(is_user_authorized)
   end
 
   describe '#should_render_dora_charts' do
@@ -25,18 +24,38 @@ RSpec.describe EE::GraphHelper do
       it { expect(should_render_dora_charts).to be(false) }
     end
 
-    it_behaves_like 'returns true'
+    shared_examples '#should_render_dora_charts for a specific type of container' do
+      it_behaves_like 'returns true'
 
-    context 'when the feature is not available' do
-      let(:is_feature_licensed) { false }
+      context 'when the feature is not available' do
+        let(:is_feature_licensed) { false }
 
-      it_behaves_like 'returns false'
+        it_behaves_like 'returns false'
+      end
+
+      context 'when the user does not have permission' do
+        let(:is_user_authorized) { false }
+
+        it_behaves_like 'returns false'
+      end
     end
 
-    context 'when the user does not have permission' do
-      let(:is_user_authorized) { false }
+    context 'when serving the project-level DORA page' do
+      before do
+        self.instance_variable_set(:@project, project)
+        allow(self).to receive(:can?).with(current_user, :read_dora4_analytics, project).and_return(is_user_authorized)
+      end
 
-      it_behaves_like 'returns false'
+      it_behaves_like '#should_render_dora_charts for a specific type of container'
+    end
+
+    context 'when serving the group-level DORA page' do
+      before do
+        self.instance_variable_set(:@group, group)
+        allow(self).to receive(:can?).with(current_user, :read_dora4_analytics, group).and_return(is_user_authorized)
+      end
+
+      it_behaves_like '#should_render_dora_charts for a specific type of container'
     end
   end
 end
