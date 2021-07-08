a17fc0601df12612f339a24402537229da37a1df
Jonathan Schafer <jschafer@gitlab.com>
2021-07-08 15:55:26 -0500

    Update asset to be many-to-one

--- ee/app/models/vulnerabilities/finding/evidence/asset.rb
+++ ee/app/models/vulnerabilities/finding/evidence/asset.rb
@@ -6,7 +6,7 @@ module Vulnerabilities
       class Asset < ApplicationRecord
         self.table_name = 'vulnerability_finding_evidence_assets'
 
-        belongs_to :evidence, class_name: 'Vulnerabilities::Finding::Evidence', inverse_of: :asset, foreign_key: 'vulnerability_finding_evidence_id', optional: false
+        belongs_to :evidence, class_name: 'Vulnerabilities::Finding::Evidence', inverse_of: :assets, foreign_key: 'vulnerability_finding_evidence_id', optional: false
 
         validates :type, length: { maximum: 2048 }
         validates :name, length: { maximum: 2048 }
--- ee/spec/models/vulnerabilities/finding/evidence/asset_spec.rb
+++ ee/spec/models/vulnerabilities/finding/evidence/asset_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe Vulnerabilities::Finding::Evidence::Asset do
-  it { is_expected.to belong_to(:evidence).class_name('Vulnerabilities::Finding::Evidence').inverse_of(:asset).required }
+  it { is_expected.to belong_to(:evidence).class_name('Vulnerabilities::Finding::Evidence').inverse_of(:assets).required }
 
   it { is_expected.to validate_length_of(:type).is_at_most(2048) }
   it { is_expected.to validate_length_of(:name).is_at_most(2048) }
