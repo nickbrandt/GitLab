183a78beda522d69506e7f9c96e45561e748f67f
Olena Horal-Koretska <ohoralkoretska@gitlab.com>
2021-07-08 19:13:45 +0300

    Handle schedule removal - simple case

--- ee/spec/frontend/escalation_policies/escalation_policy_spec.js
+++ ee/spec/frontend/escalation_policies/escalation_policy_spec.js
@@ -1,9 +1,9 @@
-import { GlSprintf } from '@gitlab/ui';
+import { GlSprintf, GlIcon } from '@gitlab/ui';
 import { shallowMount } from '@vue/test-utils';
 import { cloneDeep } from 'lodash';
 import EditEscalationPolicyModal from 'ee/escalation_policies/components/add_edit_escalation_policy_modal.vue';
 import DeleteEscalationPolicyModal from 'ee/escalation_policies/components/delete_escalation_policy_modal.vue';
-import EscalationPolicy from 'ee/escalation_policies/components/escalation_policy.vue';
+import EscalationPolicy, { i18n } from 'ee/escalation_policies/components/escalation_policy.vue';
 
 import {
   deleteEscalationPolicyModalId,
@@ -16,10 +16,10 @@ describe('EscalationPolicy', () => {
   let wrapper;
   const escalationPolicy = parsePolicy(cloneDeep(mockPolicies[0]));
 
-  const createComponent = () => {
+  const createComponent = (policy = escalationPolicy) => {
     wrapper = shallowMount(EscalationPolicy, {
       propsData: {
-        policy: escalationPolicy,
+        policy,
         index: 0,
       },
       stubs: {
@@ -38,9 +38,22 @@ describe('EscalationPolicy', () => {
 
   const findDeleteModal = () => wrapper.findComponent(DeleteEscalationPolicyModal);
   const findEditModal = () => wrapper.findComponent(EditEscalationPolicyModal);
+  const findWarningIcon = () => wrapper.findComponent(GlIcon);
 
-  it('renders policy with rules', () => {
+  it('renders a policy with rules', () => {
     expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('renders a policy without rules', () => {
+    const policyWithoutRules = {
+      id: mockPolicies[0].id,
+      name: mockPolicies[0].name,
+      description: mockPolicies[0].description,
+      rules: [],
+    };
+    createComponent(policyWithoutRules);
+    expect(findWarningIcon().exists()).toBe(true);
+    expect(wrapper.text()).toContain(i18n.noRules);
   });
 
   describe('Modals', () => {
