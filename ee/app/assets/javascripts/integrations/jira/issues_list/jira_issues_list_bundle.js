b5a559aeab38769888723f78c435b98af311666d
Justin Ho <hduong@gitlab.com>
2021-07-08 15:21:01 +0700

    Migrate urlParamsToObject to queryToObject
    Reduce technical debt in Jira issues list app.
    
    We need to use the `gatherArrays` options since we still use
    the Rails syntax for labels like this `labels[]=`.

--- ee/app/assets/javascripts/integrations/jira/issues_list/jira_issues_list_bundle.js
+++ ee/app/assets/javascripts/integrations/jira/issues_list/jira_issues_list_bundle.js
@@ -2,8 +2,7 @@ import Vue from 'vue';
 
 import { IssuableStates } from '~/issuable_list/constants';
 import { convertObjectPropsToCamelCase } from '~/lib/utils/common_utils';
-// eslint-disable-next-line import/no-deprecated
-import { urlParamsToObject } from '~/lib/utils/url_utility';
+import { queryToObject } from '~/lib/utils/url_utility';
 
 import JiraIssuesListApp from './components/jira_issues_list_root.vue';
 import apolloProvider from './graphql';
@@ -22,10 +21,12 @@ export default function initJiraIssuesList({ mountPointSelector }) {
   } = mountPointEl.dataset;
 
   const initialFilterParams = Object.assign(
-    // eslint-disable-next-line import/no-deprecated
-    convertObjectPropsToCamelCase(urlParamsToObject(window.location.search.substring(1)), {
-      dropKeys: ['scope', 'utf8', 'state', 'sort'], // These keys are unsupported/unnecessary
-    }),
+    convertObjectPropsToCamelCase(
+      queryToObject(window.location.search.substring(1), { gatherArrays: true }),
+      {
+        dropKeys: ['scope', 'utf8', 'state', 'sort'], // These keys are unsupported/unnecessary
+      },
+    ),
   );
 
   return new Vue({
