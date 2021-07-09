de82b75a26d21d96384398844741bab8608edbf3
Marcel Amirault <4155490-marcel.amirault@users.noreply.gitlab.com>
2021-07-09 14:32:20 +0000

    Update warnings for ci minute quotas
    Make it clear that it's a namespace, not just a group.
    
    Changelog: changed
    EE: true

--- ee/app/models/ci/minutes/notification.rb
+++ ee/app/models/ci/minutes/notification.rb
@@ -79,14 +79,14 @@ module Ci
       end
 
       def exceeded_message
-        s_("Pipelines|Group %{namespace_name} has exceeded its pipeline minutes quota. " \
-          "Unless you buy additional pipeline minutes, no new jobs or pipelines in its projects will run.") %
+        s_("Pipelines|The %{namespace_name} namespace has exceeded its pipeline minutes quota. " \
+          "Buy additional pipeline minutes, or no new jobs or pipelines in its projects will run.") %
           { namespace_name: context.namespace_name }
       end
 
       def threshold_message
-        s_("Pipelines|Group %{namespace_name} has %{percentage}%% or less Shared Runner Pipeline" \
-          " minutes remaining.  Once it runs out, no new jobs or pipelines in its projects will run.") %
+        s_("Pipelines|The %{namespace_name} namespace has %{percentage}%% or less Shared Runner Pipeline" \
+          " minutes remaining. After it runs out, no new jobs or pipelines in its projects will run.") %
           {
             namespace_name: context.namespace_name,
             percentage: PERCENTAGES[stage]
