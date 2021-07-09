782d0bffc9e011aee4a63aab1948d73ee283e531
Sanad Liaquat (Personal) <sliaquat@gitlab.com>
2021-07-09 14:05:17 +0500

    Fix license fabrication

--- qa/qa/ee/resource/license.rb
+++ qa/qa/ee/resource/license.rb
@@ -7,7 +7,7 @@ module QA
         def fabricate!(license)
           QA::Page::Main::Login.perform(&:sign_in_using_admin_credentials)
           QA::Page::Main::Menu.perform(&:go_to_admin_area)
-          QA::Page::Admin::Menu.perform(&:click_license_menu_link)
+          QA::Page::Admin::Menu.perform(&:click_subscription_menu_link)
 
           EE::Page::Admin::License.perform do |license_page|
             license_page.add_new_license(license) unless license_page.license?
