549a9afd5597843c68db3e0da42d37e8e6d239cc
alinamihaila <amihaila@gitlab.com>
2021-07-12 20:53:54 +0300

    Submit service ping if product intelligence enabled

--- ee/spec/services/ee/service_ping/permit_data_categories_service_spec.rb
+++ ee/spec/services/ee/service_ping/permit_data_categories_service_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper'
 
 RSpec.describe ServicePing::PermitDataCategoriesService do
+  using RSpec::Parameterized::TableSyntax
+
   describe '#execute' do
     subject(:permitted_categories) { described_class.new.execute }
 
@@ -93,6 +95,40 @@ RSpec.describe ServicePing::PermitDataCategoriesService do
             expect(permitted_categories).to match_array(%w[])
           end
         end
+      end
+    end
+  end
+
+  describe '#product_intelligence_enabled?' do
+    where(:usage_ping_enabled, :customer_service_enabled, :requires_usage_stats_consent, :expected_product_intelligence_enabled) do
+      # Customer service enabled
+      true  | true  | false | true
+      false | true  | true  | false
+      false | true  | false | true
+      true  | true  | true  | false
+
+      # Customer service disabled
+      true  | false | false | true
+      true  | false | true  | false
+      false | false | false | false
+      false | false | true  | false
+
+      # When there is no license it should have same behaviour as ce
+      true  | nil | false | true
+      false | nil | false | false
+      false | nil | true  | false
+      true  | nil | true  | false
+    end
+
+    with_them do
+      before do
+        allow(User).to receive(:single_user).and_return(double(:user, requires_usage_stats_consent?: requires_usage_stats_consent))
+        stub_config_setting(usage_ping_enabled: usage_ping_enabled)
+        create_current_license(usage_ping_required_metrics_enabled: customer_service_enabled)
+      end
+
+      it 'has the correct product_intelligence_enabled?' do
+        expect(described_class.new.product_intelligence_enabled?).to eq(expected_product_intelligence_enabled)
       end
     end
   end
