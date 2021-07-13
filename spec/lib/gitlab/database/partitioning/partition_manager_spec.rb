01210db7b783cf716f240f8dec536bbda41299f8
Heinrich Lee Yu <heinrich@gitlab.com>
2021-07-13 11:09:45 +0000

    Apply formatting suggestions to spec

--- spec/lib/gitlab/database/partitioning/partition_manager_spec.rb
+++ spec/lib/gitlab/database/partitioning/partition_manager_spec.rb
@@ -120,6 +120,7 @@ RSpec.describe Gitlab::Database::Partitioning::PartitionManager do
       before do
         stub_feature_flags(partition_pruning_dry_run: true)
       end
+
       it 'detaches each extra partition' do
         extra_partitions.each { |p| expect(manager).to receive(:detach_one_partition).with(p) }
 
@@ -149,6 +150,7 @@ RSpec.describe Gitlab::Database::Partitioning::PartitionManager do
       before do
         stub_feature_flags(partition_pruning_dry_run: false)
       end
+
       it 'returns immediately' do
         expect(manager).not_to receive(:detach)
 
