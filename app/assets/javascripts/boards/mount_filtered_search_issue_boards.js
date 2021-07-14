582485a8d0c718bcef91b666aa68d00080ca4a48
Scott Stern <sstern@gitlab.com>
2021-07-14 01:45:17 +0000

    Add new filtered search to issue boards [RUN ALL RSPEC] [RUN AS-IF-FOSS]

--- app/assets/javascripts/boards/mount_filtered_search_issue_boards.js
+++ app/assets/javascripts/boards/mount_filtered_search_issue_boards.js
@@ -1,14 +1,13 @@
 import Vue from 'vue';
-import EpicFilteredSearch from 'ee_component/boards/components/epic_filtered_search.vue';
+import IssueBoardFilteredSearch from '~/boards/components/issue_board_filtered_search.vue';
 import store from '~/boards/stores';
 import { convertObjectPropsToCamelCase } from '~/lib/utils/common_utils';
-// eslint-disable-next-line import/no-deprecated
-import { urlParamsToObject } from '~/lib/utils/url_utility';
+import { queryToObject } from '~/lib/utils/url_utility';
 
 export default (apolloProvider) => {
-  const el = document.getElementById('js-board-filtered-search');
-  // eslint-disable-next-line import/no-deprecated
-  const rawFilterParams = urlParamsToObject(window.location.search);
+  const el = document.getElementById('js-issue-board-filtered-search');
+  const rawFilterParams = queryToObject(window.location.search, { gatherArrays: true });
+
   const initialFilterParams = {
     ...convertObjectPropsToCamelCase(rawFilterParams, {}),
   };
@@ -25,7 +24,7 @@ export default (apolloProvider) => {
     store, // TODO: https://gitlab.com/gitlab-org/gitlab/-/issues/324094
     apolloProvider,
     render: (createElement) =>
-      createElement(EpicFilteredSearch, {
+      createElement(IssueBoardFilteredSearch, {
         props: { fullPath: store.state?.fullPath || '', boardType: store.state?.boardType || '' },
       }),
   });
