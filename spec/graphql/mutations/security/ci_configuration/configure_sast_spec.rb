dfa9326d47d8f12aaec2afd67e0b435345674f65
Adam Cohen <acohen@gitlab.com>
2021-07-14 18:01:15 +1000

    Remove duplication from ConfigureSast

--- spec/graphql/mutations/security/ci_configuration/configure_sast_spec.rb
+++ spec/graphql/mutations/security/ci_configuration/configure_sast_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe Mutations::Security::CiConfiguration::ConfigureSast do
   subject { resolve(described_class, args: { project_path: project.full_path, configuration: {} }, ctx: { current_user: user }) }
 
   include_examples 'graphql mutations security ci configuration'
+
+  it 'raises an error if the configuration parameter is not provided' do
+    expect { resolve(described_class, args: { project_path: project.full_path }, ctx: { current_user: user }) }.to raise_error(ArgumentError)
+  end
 end
