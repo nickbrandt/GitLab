c42d8dfe77a17e7d91462e9195f28d56f33a4d73
Denys Mishunov <dmishunov@gitlab.com>
2021-07-14 11:11:01 +0200

    Supply project data to integration tests

--- spec/frontend_integration/ide/helpers/start.js
+++ spec/frontend_integration/ide/helpers/start.js
@@ -1,20 +1,28 @@
 /* global monaco */
 
 import { TEST_HOST } from 'helpers/test_constants';
+import { projectData } from 'jest/ide/mock_data';
 import { initIde } from '~/ide';
 import extendStore from '~/ide/stores/extend';
 import { IDE_DATASET } from './mock_data';
 
 export default (container, { isRepoEmpty = false, path = '', mrId = '' } = {}) => {
   const projectName = isRepoEmpty ? 'lorem-ipsum-empty' : 'lorem-ipsum';
+  const projectNamespaced = `gitlab-test/${projectName}`;
+  const project = Object.assign({}, projectData, {
+    path_with_namespace: projectNamespaced,
+  });
   const pathSuffix = mrId ? `merge_requests/${mrId}` : `tree/master/-/${path}`;
 
   global.jsdom.reconfigure({
-    url: `${TEST_HOST}/-/ide/project/gitlab-test/${projectName}/${pathSuffix}`,
+    url: `${TEST_HOST}/-/ide/project/${projectNamespaced}/${pathSuffix}`,
   });
 
   const el = document.createElement('div');
-  Object.assign(el.dataset, IDE_DATASET);
+  Object.assign(el.dataset, {
+    ...IDE_DATASET,
+    project: JSON.stringify(project),
+  });
   container.appendChild(el);
   const vm = initIde(el, { extendStore });
 
